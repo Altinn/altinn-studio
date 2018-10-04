39050931035c4531f203ac2bf58245deb94f5544
Kylstad, Nina <nina.kylstad@accenture.com>
2018-10-04 14:15:18 +0200

    forgot to add filet to last commit

--- src/react-apps/ux-editor/src/components/GenericComponent.tsx
+++ src/react-apps/ux-editor/src/components/GenericComponent.tsx
@@ -6,13 +6,27 @@ export interface IGenericComponentProps {
   isValid: boolean;
   formData: any;
   designMode: boolean;
+  thirdPartyComponents?: any;
   handleDataChange: (callbackValue: any) => void;
   getTextResource: (key: string) => string;
 }
 
 class GenericComponent extends React.Component<IGenericComponentProps> {
 
+  public renderThirdPartyComponent = (): JSX.Element => {
+    const [packageName, component] = this.props.component.title.split('.');
+    if (!this.props.thirdPartyComponents || !this.props.thirdPartyComponents[packageName]
+      || !this.props.thirdPartyComponents[packageName][component]) {
+      return null;
+    }
+    return this.props.thirdPartyComponents[packageName][component];
+  }
+  
   public render() {
+    if (this.props.component.component === 'ThirdParty') {
+      return this.renderThirdPartyComponent();
+    }
+
     const TagName = components.find((c: any) => c.name === this.props.component.component).Tag;
     const text = this.props.designMode ? this.props.component.title
       : this.props.getTextResource(this.props.component.title);
