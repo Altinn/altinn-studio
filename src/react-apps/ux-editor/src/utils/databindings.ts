958703cd2d828aa412ea58b5363c23ace9f12066
Kylstad, Nina <nina.kylstad@accenture.com>
2018-10-10 12:56:19 +0200

    Fix issue when saving form data

--- src/react-apps/ux-editor/src/utils/databindings.ts
+++ src/react-apps/ux-editor/src/utils/databindings.ts
@@ -7,7 +7,7 @@ import {object} from 'dot-object';
  * @param formData the complete datamodel in store
  */
 export function convertDataBindingToModel(formData: any, dataModelElements: IDataModelFieldElement[]): any {
-  return object(formData);
+  return object(Object.assign({}, formData));
 }
 
 export interface IData {
