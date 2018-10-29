b367935d16140b3b79f01ea51bfb8b4aa34c5640
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2018-10-29 12:15:49 +0100

    Created new web api project to save and get data from the cosmos db emulator

---
+++ src/AltinnCore/AltinnCore.Runtime.Db/Repository/IFormDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AltinnCore.Runtime.Db.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltinnCore.Runtime.Db.Repository
+{
+    /// <summary>
+    /// the interface contains methods for operations on form data document
+    /// </summary>
+    public interface IFormDataRepository
+    {
+        Task<FormData> GetFormDataFromCollectionAsync(string reporteeId, string reporteeElementId, string formId);
+        Task<FormData> InsertFormDataIntoCollectionAsync(FormData item);
+        Task<FormData> UpdateFormDataInCollectionAsync(string id, FormData item);
+    }
+}
