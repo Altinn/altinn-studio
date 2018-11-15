4eeb1fb637741e7ca0a033cd2a72c5a60349ba3a
trymen <44166090+trymen@users.noreply.github.com>
2018-11-15 10:30:17 +0100

    Added prod baseUrl, added return-to-home-page afterEach hook to common.js (#398)

--- src/test/Testcafe/page-objects/loginPage.js
+++ src/test/Testcafe/page-objects/loginPage.js
@@ -4,6 +4,7 @@ import { Selector, t } from 'testcafe';
 export default class LoginPage {
   constructor() {
     //repository landing page
+    this.welcomeLoginButton = Selector('[value ^= "Logg inn"]');
     this.userInput = Selector('#user_name');
     this.passwordInput = Selector('#password');
     this.loginButton = Selector('.ui.green.button');
