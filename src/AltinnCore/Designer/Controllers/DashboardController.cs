b13455f58f7c5252d87ccbabd940aac3dd160bd2
Kylstad, Nina <nina.kylstad@accenture.com>
2018-11-19 13:00:51 +0100

    Updated designer to work with new react structure

---
+++ src/AltinnCore/Designer/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltinnCore.Designer.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
