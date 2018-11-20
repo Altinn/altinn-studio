6f2d4bcd1a0dc35d61c60d0b46d2479704be80eb
Rune T. Larsen <rune@hjemmekino.no>
2018-11-20 12:58:19 +0100

    Added more tests for home controller

--- src/AltinnCore/UnitTest/Designer/HomeControllerTests.cs
+++ src/AltinnCore/UnitTest/Designer/HomeControllerTests.cs
@@ -3,12 +3,15 @@ using AltinnCore.Common.Models;
 using AltinnCore.Common.Services.Interfaces;
 using AltinnCore.Designer.Controllers;
 using AltinnCore.RepositoryClient.Model;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace AltinnCore.UnitTest.Designer
@@ -47,6 +50,9 @@ namespace AltinnCore.UnitTest.Designer
             Assert.Equal(7, model.Repositories.Count);
         }
 
+        /// <summary>
+        /// Test verifies that non cloned 
+        /// </summary>
         [Fact]
         public void Index_GetSevenReposFromOrgWhereFourIsClonedLocallyOnlyLocallyClonedListed()
         {
@@ -74,6 +80,172 @@ namespace AltinnCore.UnitTest.Designer
             Assert.Equal(4, model.Repositories.Count);
         }
 
+
+        /// <summary>
+        /// This test verifies that user is redirected to start page if user is not logged in
+        /// </summary>
+        [Fact]
+        public void StartPage_NotLoggedInUser()
+        {
+            // Arrange
+            Moq.Mock<IRepository> moqRepository = new Mock<IRepository>();
+            Moq.Mock<ILogger<HomeController>> moqLogger = new Mock<ILogger<HomeController>>();
+            Moq.Mock<IHttpContextAccessor> moqHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            Moq.Mock<IOptions<ServiceRepositorySettings>> moqServiceRepositorySettings = GetMoqServiceRepositorySettings();
+         
+            Moq.Mock<IGitea> moqGiteaWrappeer = GetMoqGiteaWrapperForIndexTest();
+            Moq.Mock<ISourceControl> moqSourceControl = GetMoqSourceControlForIndexTest();
+
+            DefaultHttpContext httpContext = new DefaultHttpContext();
+
+            var cookies = new[] { "i_like_gitea=234543556"};
+
+            httpContext.Request.Headers["Cookie"] = cookies;
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
+            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            { ControllerContext = controllerContext } ;
+
+            // Act
+            ActionResult result = controller.StartPage();
+
+            // Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("StartPage", viewResult.ViewName);
+        }
+
+
+        /// <summary>
+        /// This test verifies that user is redirected to start page if user is not logged in
+        /// </summary>
+        [Fact]
+        public void StartPage_LoggedInUser()
+        {
+            // Arrange
+            Moq.Mock<IRepository> moqRepository = new Mock<IRepository>();
+            Moq.Mock<ILogger<HomeController>> moqLogger = new Mock<ILogger<HomeController>>();
+            Moq.Mock<IHttpContextAccessor> moqHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            Moq.Mock<IOptions<ServiceRepositorySettings>> moqServiceRepositorySettings = GetMoqServiceRepositorySettings();
+
+            Moq.Mock<IGitea> moqGiteaWrappeer = GetMoqGiteaWrapperForIndexTest();
+            Moq.Mock<ISourceControl> moqSourceControl = GetMoqSourceControlForIndexTest();
+
+            User user = new User();
+            moqGiteaWrappeer.Setup(g => g.GetCurrentUser(It.IsAny<string>())).ReturnsAsync(user);
+
+            DefaultHttpContext httpContext = new DefaultHttpContext();
+
+            var cookies = new[] { "i_like_gitea=234543556" };
+
+            httpContext.Request.Headers["Cookie"] = cookies;
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
+            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            { ControllerContext = controllerContext };
+
+            // Act
+            ActionResult result = controller.StartPage();
+
+            // Assert
+            RedirectToActionResult redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+            Assert.Equal("Home", redirectResult.ControllerName);
+        }
+
+        /// <summary>
+        /// This tests logs inn a a user
+        /// </summary>
+        [Fact]
+        public void Login_LogInUser()
+        {
+            // Arrange
+            Moq.Mock<IRepository> moqRepository = new Mock<IRepository>();
+            Moq.Mock<ILogger<HomeController>> moqLogger = new Mock<ILogger<HomeController>>();
+            Moq.Mock<IHttpContextAccessor> moqHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            Moq.Mock<IOptions<ServiceRepositorySettings>> moqServiceRepositorySettings = GetMoqServiceRepositorySettings();
+
+            Moq.Mock<IGitea> moqGiteaWrappeer = GetMoqGiteaWrapperForIndexTest();
+            Moq.Mock<ISourceControl> moqSourceControl = GetMoqSourceControlForIndexTest();
+
+            Mock<IServiceProvider> serviceProviderMock = GetServiceProviderMock();
+
+            User user = new User();
+            user.Login = "Test";
+            moqGiteaWrappeer.Setup(g => g.GetCurrentUser(It.IsAny<string>())).ReturnsAsync(user);
+
+            DefaultHttpContext httpContext = new DefaultHttpContext() { RequestServices = serviceProviderMock.Object };
+
+            var cookies = new[] { "i_like_gitea=234543556" };
+
+            httpContext.Request.Headers["Cookie"] = cookies;
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
+            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            { ControllerContext = controllerContext };
+
+            // Act
+           var result = controller.Login();
+
+            // Assert
+            LocalRedirectResult redirectResult = Assert.IsType<LocalRedirectResult>(result.Result);
+            Assert.Equal("/", redirectResult.Url);
+        }
+
+
+        /// <summary>
+        /// Unit test that verifies user logout
+        /// </summary>
+        [Fact]
+        public void Login_LogOutUser()
+        {
+            // Arrange
+            Moq.Mock<IRepository> moqRepository = new Mock<IRepository>();
+            Moq.Mock<ILogger<HomeController>> moqLogger = new Mock<ILogger<HomeController>>();
+            Moq.Mock<IHttpContextAccessor> moqHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            Moq.Mock<IOptions<ServiceRepositorySettings>> moqServiceRepositorySettings = GetMoqServiceRepositorySettings();
+
+            Moq.Mock<IGitea> moqGiteaWrappeer = GetMoqGiteaWrapperForIndexTest();
+            Moq.Mock<ISourceControl> moqSourceControl = GetMoqSourceControlForIndexTest();
+
+            Mock<IServiceProvider> serviceProviderMock = GetServiceProviderMock();
+
+            User user = new User();
+            user.Login = "Test";
+            moqGiteaWrappeer.Setup(g => g.GetCurrentUser(It.IsAny<string>())).ReturnsAsync(user);
+
+            DefaultHttpContext httpContext = new DefaultHttpContext() { RequestServices = serviceProviderMock.Object };
+
+            var cookies = new[] { "i_like_gitea=234543556" };
+
+            httpContext.Request.Headers["Cookie"] = cookies;
+            var controllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
+            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            { ControllerContext = controllerContext };
+
+            // Act
+            var result = controller.Logout();
+
+            // Assert
+            LocalRedirectResult redirectResult = Assert.IsType<LocalRedirectResult>(result.Result);
+            Assert.Equal("/user/logout", redirectResult.Url);
+        }
 
 
 
@@ -105,6 +277,35 @@ namespace AltinnCore.UnitTest.Designer
             Moq.Mock<IGitea> moqGiteaWrappeer = new Mock<IGitea>();
             moqGiteaWrappeer.Setup(gitea => gitea.SearchRepository(It.IsAny<bool>(), It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(search);
             return moqGiteaWrappeer;
+        }
+
+        private Moq.Mock<IOptions<ServiceRepositorySettings>> GetMoqServiceRepositorySettings()
+        {
+            ServiceRepositorySettings settings = new ServiceRepositorySettings();
+            settings.GiteaCookieName = "i_like_gitea";
+            settings.ForceGiteaAuthentication = true;
+            Moq.Mock<IOptions<ServiceRepositorySettings>> moqServiceRepositorySettings = new Mock<IOptions<ServiceRepositorySettings>>();
+            moqServiceRepositorySettings.Setup(r => r.Value).Returns(settings);
+            return moqServiceRepositorySettings;
+        }
+
+        private Mock<IServiceProvider> GetServiceProviderMock()
+        {
+            Mock<IAuthenticationService> authServiceMock = new Mock<IAuthenticationService>();
+            authServiceMock
+                .Setup(_ => _.SignInAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<ClaimsPrincipal>(), It.IsAny<AuthenticationProperties>()))
+                .Returns(Task.FromResult((object)null));
+
+            authServiceMock
+            .Setup(_ => _.SignOutAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<AuthenticationProperties>()))
+            .Returns(Task.FromResult((object)null));
+
+            Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                .Returns(authServiceMock.Object);
+
+            return serviceProviderMock;
         }
 
     }
