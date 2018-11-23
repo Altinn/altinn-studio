632dcff536f422c1a2ebb987c24b183527e40118
Rune T. Larsen <rune@hjemmekino.no>
2018-11-23 09:40:57 +0100

    Fixed stylecop

--- src/AltinnCore/UnitTest/Designer/HomeControllerTests.cs
+++ src/AltinnCore/UnitTest/Designer/HomeControllerTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using AltinnCore.Common.Configuration;
 using AltinnCore.Common.Models;
 using AltinnCore.Common.Services.Interfaces;
@@ -9,9 +12,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
-using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace AltinnCore.UnitTest.Designer
@@ -90,7 +90,6 @@ namespace AltinnCore.UnitTest.Designer
             Assert.Equal(4, model.Repositories.Count);
         }
 
-
         /// <summary>
         /// This test verifies that user is redirected to start page if user is not logged in
         /// </summary>
@@ -108,17 +107,16 @@ namespace AltinnCore.UnitTest.Designer
 
             DefaultHttpContext httpContext = new DefaultHttpContext();
 
-            var cookies = new[] { "i_like_gitea=234543556"};
+            var cookies = new[] { "i_like_gitea=234543556" };
 
             httpContext.Request.Headers["Cookie"] = cookies;
             var controllerContext = new ControllerContext()
             {
-                HttpContext = httpContext
+                HttpContext = httpContext,
             };
 
-            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
-            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
-            { ControllerContext = controllerContext } ;
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object, moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            { ControllerContext = controllerContext };
 
             // Act
             ActionResult result = controller.StartPage();
@@ -127,7 +125,6 @@ namespace AltinnCore.UnitTest.Designer
             ViewResult viewResult = Assert.IsType<ViewResult>(result);
             Assert.Equal("StartPage", viewResult.ViewName);
         }
-
 
         /// <summary>
         /// This test verifies that user is redirected to start page if user is not logged in
@@ -154,11 +151,16 @@ namespace AltinnCore.UnitTest.Designer
             httpContext.Request.Headers["Cookie"] = cookies;
             var controllerContext = new ControllerContext()
             {
-                HttpContext = httpContext
+                HttpContext = httpContext,
             };
 
-            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
-            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(
+            moqRepository.Object,
+            moqLogger.Object,
+            moqServiceRepositorySettings.Object,
+            moqGiteaWrappeer.Object,
+            moqHttpContextAccessor.Object,
+            moqSourceControl.Object)
             { ControllerContext = controllerContext };
 
             // Act
@@ -198,21 +200,25 @@ namespace AltinnCore.UnitTest.Designer
             httpContext.Request.Headers["Cookie"] = cookies;
             var controllerContext = new ControllerContext()
             {
-                HttpContext = httpContext
+                HttpContext = httpContext,
             };
 
-            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
-            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(
+            moqRepository.Object,
+            moqLogger.Object,
+            moqServiceRepositorySettings.Object,
+            moqGiteaWrappeer.Object,
+            moqHttpContextAccessor.Object,
+            moqSourceControl.Object)
             { ControllerContext = controllerContext };
 
             // Act
-           var result = controller.Login();
+            Task<IActionResult> result = controller.Login();
 
             // Assert
             LocalRedirectResult redirectResult = Assert.IsType<LocalRedirectResult>(result.Result);
             Assert.Equal("/", redirectResult.Url);
         }
-
 
         /// <summary>
         /// Unit test that verifies user logout
@@ -242,11 +248,16 @@ namespace AltinnCore.UnitTest.Designer
             httpContext.Request.Headers["Cookie"] = cookies;
             var controllerContext = new ControllerContext()
             {
-                HttpContext = httpContext
+                HttpContext = httpContext,
             };
 
-            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
-            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(
+                moqRepository.Object,
+                moqLogger.Object,
+                moqServiceRepositorySettings.Object,
+                moqGiteaWrappeer.Object,
+                moqHttpContextAccessor.Object,
+                moqSourceControl.Object)
             { ControllerContext = controllerContext };
 
             // Act
@@ -285,14 +296,19 @@ namespace AltinnCore.UnitTest.Designer
             httpContext.Request.Headers["Cookie"] = cookies;
             var controllerContext = new ControllerContext()
             {
-                HttpContext = httpContext
+                HttpContext = httpContext,
             };
 
-            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(moqRepository.Object, moqLogger.Object,
-            moqServiceRepositorySettings.Object, moqGiteaWrappeer.Object, moqHttpContextAccessor.Object, moqSourceControl.Object)
+            AltinnCore.Designer.Controllers.HomeController controller = new AltinnCore.Designer.Controllers.HomeController(
+            moqRepository.Object,
+            moqLogger.Object,
+            moqServiceRepositorySettings.Object,
+            moqGiteaWrappeer.Object,
+            moqHttpContextAccessor.Object,
+            moqSourceControl.Object)
             { ControllerContext = controllerContext };
 
-           AppKey key = new AppKey() { Key = "12345" };
+            AppKey key = new AppKey() { Key = "12345" };
 
             // Act
             IActionResult result = controller.AppToken(key);
@@ -360,6 +376,5 @@ namespace AltinnCore.UnitTest.Designer
 
             return serviceProviderMock;
         }
-
     }
 }
