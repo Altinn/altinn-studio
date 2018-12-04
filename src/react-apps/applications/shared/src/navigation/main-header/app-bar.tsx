0a8305b137fe1eb36e03c30e95615338fe9288fc
Kylstad, Nina <nina.kylstad@accenture.com>
2018-12-04 11:57:50 +0100

    Move modal to generic component, clean up

--- src/react-apps/applications/shared/src/navigation/main-header/app-bar.tsx
+++ src/react-apps/applications/shared/src/navigation/main-header/app-bar.tsx
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import classNames from 'classnames';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import 'typeface-roboto';
 
 import ProfileMenu from '../../profile-menu';
 
