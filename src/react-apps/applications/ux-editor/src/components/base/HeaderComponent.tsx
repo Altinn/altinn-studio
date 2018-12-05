ba7245a41b197a1205289e1f9afd46e606a908e3
nkylstad <nkylstad@gmail.com>
2018-12-05 09:49:23 +0100

    400 component title sbl (#527)
    * Updated header component classes and elements
    
    * updated unit tests for HeaderComponent

--- src/react-apps/applications/ux-editor/src/components/base/HeaderComponent.tsx
+++ src/react-apps/applications/ux-editor/src/components/base/HeaderComponent.tsx
@@ -13,15 +13,15 @@ export class HeaderComponent extends React.Component<IHeaderProps, IHeaderState>
   public renderHeader(): JSX.Element {
     switch (this.props.size) {
       case ('S'): {
-        return <h3 className='a-pageTitle' id={this.props.component.id}>{this.props.text}</h3>;
+        return <h4 className='a-sectionSubTitle' id={this.props.component.id}>{this.props.text}</h4>;
       }
 
       case ('M'): {
-        return <h2 className='a-fontBold a-pageTitle' id={this.props.component.id}>{this.props.text}</h2>;
+        return <h3 className='a-sectionSubTitle' id={this.props.component.id}>{this.props.text}</h3>;
       }
 
       default: {
-        return <h1 className='a-fontBold a-pageTitle' id={this.props.component.id}>{this.props.text}</h1>;
+        return <h2 className='a-sectionTitle' id={this.props.component.id}>{this.props.text}</h2>;
       }
     }
   }
