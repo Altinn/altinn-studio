dbc2e9ac9b999ce9eede062d8399f09d2461bf10
hanne.kottum <hanne.kottum@capgemini.com>
2018-12-11 07:39:18 +0100

    made card clickable and tabable

--- src/react-apps/applications/dashboard/src/services/serviceCard.tsx
+++ src/react-apps/applications/dashboard/src/services/serviceCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, Grid, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
 import * as moment from 'moment';
@@ -28,6 +28,10 @@ const styles = {
     height: '154px',
     maxHeight: '154px',
     minHeight: '154px',
+    '&:hover': {
+      background: '#e5e5e5',
+      cursor: 'pointer',
+    }
   },
   iconStyling: {
     fontSize: '35px',
@@ -46,77 +50,83 @@ class ServiceCard extends React.Component<IServiceCardComponentProps, IServiceCa
     return moment(new Date(date)).format('DD.MM.YYYY');
   }
 
+  public openService = () => {
+    window.location.href = `/designer/${this.props.service.full_name}`;
+  }
+
   public render() {
     const { classes, service } = this.props;
     return (
       <Card elevation={0} className={classNames(classes.card)}>
-        <CardContent>
-          <Grid container={true} spacing={8}>
-            <Grid item={true} xl={10} lg={10} md={10} sm={10} xs={10}>
-              <Typography
-                variant='subtitle2'
-                className={classNames(classes.displayInlineBlock, classes.width100)}
-                noWrap={true}
-              >
-                {service.name}
-              </Typography>
-            </Grid>
-            <Grid item={true} xl={1} lg={1} md={1} sm={1} xs={1}>
-              <i
-                className={classNames(classes.iconStyling,
-                  { ['ai ai-corp']: service.owner.UserType === 2 },
-                  { ['ai ai-private']: service.owner.UserType !== 2 })}
-                aria-hidden='true'
-              />
-            </Grid>
-            <Grid item={true} xl={1} lg={1} md={1} sm={1} xs={1}>
-              <i
-                className={classNames(classes.iconStyling,
-                  { ['ai ai-read']: service.permissions.push === false },
-                  { ['ai ai-write']: service.permissions.push === true })}
-                aria-hidden='true'
-              />
-            </Grid>
-            <Grid
-              item={true}
-              className={classNames(
-                classes.displayInlineBlock,
-                classes.width100,
-                classes.height)}
-            >
-              <Typography variant='body1' gutterBottom={true} className={classes.width100}>
-                <TruncateMarkup lines={2}>
-                  <span>
-                    {service.description}
-                  </span>
-                </TruncateMarkup>
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container={true} spacing={0} direction='row'>
-            <Grid item={true} xl={6} lg={6} md={6} sm={6} xs={6}>
-              <Typography
-                variant='body2'
-                className={classNames(classes.displayInlineBlock, classes.width100)}
-                noWrap={true}
-              >
-                {service.owner.full_name || service.owner.login}
-              </Typography>
-            </Grid>
-            <Grid item={true} xl={6} lg={6} md={6} sm={6} xs={6}>
-              <Typography
-                variant='body2'
+        <CardActionArea onClick={this.openService}>
+          <CardContent>
+            <Grid container={true} spacing={8}>
+              <Grid item={true} xl={10} lg={10} md={10} sm={10} xs={10}>
+                <Typography
+                  variant='subtitle2'
+                  className={classNames(classes.displayInlineBlock, classes.width100)}
+                  noWrap={true}
+                >
+                  {service.name}
+                </Typography>
+              </Grid>
+              <Grid item={true} xl={1} lg={1} md={1} sm={1} xs={1}>
+                <i
+                  className={classNames(classes.iconStyling,
+                    { ['ai ai-corp']: service.owner.UserType === 2 },
+                    { ['ai ai-private']: service.owner.UserType !== 2 })}
+                  aria-hidden='true'
+                />
+              </Grid>
+              <Grid item={true} xl={1} lg={1} md={1} sm={1} xs={1}>
+                <i
+                  className={classNames(classes.iconStyling,
+                    { ['ai ai-read']: service.permissions.push === false },
+                    { ['ai ai-write']: service.permissions.push === true })}
+                  aria-hidden='true'
+                />
+              </Grid>
+              <Grid
+                item={true}
                 className={classNames(
                   classes.displayInlineBlock,
                   classes.width100,
-                  classes.textToRight)}
-                noWrap={true}
+                  classes.height)}
               >
-                Endret: {this.formatDate(service.updated_at)}
-              </Typography>
+                <Typography variant='body1' gutterBottom={true} className={classes.width100}>
+                  <TruncateMarkup lines={2}>
+                    <span>
+                      {service.description}
+                    </span>
+                  </TruncateMarkup>
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
+            <Grid container={true} spacing={0} direction='row'>
+              <Grid item={true} xl={6} lg={6} md={6} sm={6} xs={6}>
+                <Typography
+                  variant='body2'
+                  className={classNames(classes.displayInlineBlock, classes.width100)}
+                  noWrap={true}
+                >
+                  {service.owner.full_name || service.owner.login}
+                </Typography>
+              </Grid>
+              <Grid item={true} xl={6} lg={6} md={6} sm={6} xs={6}>
+                <Typography
+                  variant='body2'
+                  className={classNames(
+                    classes.displayInlineBlock,
+                    classes.width100,
+                    classes.textToRight)}
+                  noWrap={true}
+                >
+                  Endret: {this.formatDate(service.updated_at)}
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
       </Card>
     );
   }
