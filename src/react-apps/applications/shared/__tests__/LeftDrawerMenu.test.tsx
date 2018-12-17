c64556b206fa463081c5f10aaa6bfa247cae0cf7
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2018-12-17 09:04:50 +0100

    Fixed PR comments

--- src/react-apps/applications/shared/__tests__/LeftDrawerMenu.test.tsx
+++ src/react-apps/applications/shared/__tests__/LeftDrawerMenu.test.tsx
@@ -3,156 +3,213 @@ import { MemoryRouter } from 'react-router';
 import * as renderer from 'react-test-renderer';
 import LeftDrawerMenu from '../src/navigation/drawer/LeftDrawerMenu';
 
-describe('render left drawer menu', () => {
-  let mockMenuType: string;
-  let mockactiveLeftMenuSelection: string;
+describe('LeftDrawerMenu.tsx', () => {
+  describe('render left drawer menu', () => {
+    describe('when mockmenutype is create', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-  it('should render left drawer menu for create option in header that matches the snapshot', () => {
-    mockMenuType = 'create';
-    mockactiveLeftMenuSelection = 'gui';
+      beforeEach(() => {
+        mockMenuType = 'create';
+        mockactiveLeftMenuSelection = 'gui';
+      });
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+      it('should render left drawer menu for create option in header that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-  it('should render left drawer menu for about option in header that matches the snapshot', () => {
-    mockMenuType = 'about';
-    mockactiveLeftMenuSelection = 'about';
+    describe('when mockmenutype is about', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+      beforeEach(() => {
+        mockMenuType = 'about';
+        mockactiveLeftMenuSelection = 'about';
+      });
 
-  it('should render left drawer menu for language option in header that matches the snapshot', () => {
-    mockMenuType = 'language';
-    mockactiveLeftMenuSelection = 'language';
+      it('should render left drawer menu for about option in header that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+    describe('when mockmenutype is language', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-  it('should render left drawer menu for test option in header that matches the snapshot', () => {
-    mockMenuType = 'test';
-    mockactiveLeftMenuSelection = 'test';
+      beforeEach(() => {
+        mockMenuType = 'language';
+        mockactiveLeftMenuSelection = 'language';
+      });
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+      it('should render left drawer menu for language option in header that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-  it('should render left drawer menu for publish option in header that matches the snapshot', () => {
-    mockMenuType = 'publish';
-    mockactiveLeftMenuSelection = 'publish';
+    describe('when mockmenutype is test', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
-});
+      beforeEach(() => {
+        mockMenuType = 'test';
+        mockactiveLeftMenuSelection = 'test';
+      });
 
-describe('returns empty div for null,undefined or invalid  property values', () => {
-  let mockMenuType: string;
-  let mockactiveLeftMenuSelection: string;
+      it('should render left drawer menu for test option in header that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-  it('should return an empty div when the menutype is undefined) that matches the snapshot', () => {
-    mockMenuType = undefined;
-    mockactiveLeftMenuSelection = undefined;
+    describe('when mockmenutype is publish', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+      beforeEach(() => {
+        mockMenuType = 'publish';
+        mockactiveLeftMenuSelection = 'publish';
+      });
 
-  it('should return an empty div when the menutype is null that matches the snapshot', () => {
-    mockMenuType = null;
-    mockactiveLeftMenuSelection = null;
+      it('should render left drawer menu for publish option in header that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+    describe('when mockmenutype is undefined', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-  it('should return an empty div when the menutype is empty string that matches the snapshot', () => {
-    mockMenuType = '';
-    mockactiveLeftMenuSelection = '';
+      beforeEach(() => {
+        mockMenuType = undefined;
+        mockactiveLeftMenuSelection = undefined;
+      });
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
-  });
+      it('should return an empty div when the menutype is undefined) that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
 
-  it('should return empty div when the menutype is not valid(doesnt exist in the menu setting)', () => {
-    mockMenuType = 'testmock';
-    mockactiveLeftMenuSelection = 'testmock';
+    describe('when mockmenutype is null', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
 
-    const rendered =
-      renderer.create(
-        <MemoryRouter>
-          <LeftDrawerMenu
-            menuType={mockMenuType}
-            activeLeftMenuSelection={mockactiveLeftMenuSelection}
-          />
-        </MemoryRouter>,
-      );
-    expect(rendered).toMatchSnapshot();
+      beforeEach(() => {
+        mockMenuType = null;
+        mockactiveLeftMenuSelection = null;
+      });
+
+      it('should return an empty div when the menutype is null that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
+
+    describe('when mockmenutype is empty string', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
+
+      beforeEach(() => {
+        mockMenuType = '';
+        mockactiveLeftMenuSelection = '';
+      });
+
+      it('should return an empty div when the menutype is empty string that matches the snapshot', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
+
+    describe('when mock value is invalid menutype', () => {
+      let mockMenuType: string;
+      let mockactiveLeftMenuSelection: string;
+
+      beforeEach(() => {
+        mockMenuType = 'testmock';
+        mockactiveLeftMenuSelection = 'testmock';
+      });
+
+      it('should return empty div when the menutype is not valid(doesnt exist in the menu setting)', () => {
+        const rendered =
+          renderer.create(
+            <MemoryRouter>
+              <LeftDrawerMenu
+                menuType={mockMenuType}
+                activeLeftMenuSelection={mockactiveLeftMenuSelection}
+              />
+            </MemoryRouter>,
+          );
+        expect(rendered).toMatchSnapshot();
+      });
+    });
   });
 });
