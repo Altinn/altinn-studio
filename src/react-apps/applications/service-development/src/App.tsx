56430c98cc61dda18c99a301c1ec4a0dea307877
Mats Gimming Myhre <mats.myhre@capgemini.com>
2018-12-18 20:30:06 +0100

    216 header review fixes (#612)
    * First version of loopable configuration rendering test.
    
    * Exact version of Typescript in all package.json. Removed Typescript from Lerna root.
    
    * Commit.
    
    * Added showBreadcrumbOnTablet prop to AppBarComponent. Refactored tests.
    
    * Modified TabletDrawerMenu to show logoutButton when in tabletmode, if prop is set.
    
    * Fixed linting errors.
    
    * Fixed linting errors.

--- src/react-apps/applications/service-development/src/App.tsx
+++ src/react-apps/applications/service-development/src/App.tsx
@@ -64,10 +64,10 @@ class App extends React.Component<WithStyles<typeof styles>, any> {
                       {...props}
                       org={org}
                       service={service}
+                      showBreadcrumbOnTablet={true}
                       showSubHeader={true}
                       activeSubHeaderSelection={route.activeSubHeaderSelection}
                       activeLeftMenuSelection={route.activeLeftMenuSelection}
-                      backgroundColor={theme.altinnPalette.primary.white}
                     />}
                   />
                 ))}
