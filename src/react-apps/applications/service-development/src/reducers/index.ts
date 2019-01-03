4c2a642847f2e9f0000daefa4b918dff97cade78
Mats Gimming Myhre <mats.myhre@capgemini.com>
2019-01-03 13:59:03 +0100

    Implemented fetchLanguage in Service-Development. Updated tests. (#717)

--- src/react-apps/applications/service-development/src/reducers/index.ts
+++ src/react-apps/applications/service-development/src/reducers/index.ts
@@ -1,13 +1,16 @@
 import { combineReducers, Reducer, ReducersMapObject } from 'redux';
-import serviceDevelopmentReducer, {IServiceDevelopmentState} from '../reducers/serviceDevelopmentReducer';
+import serviceDevelopmentReducer, { IServiceDevelopmentState } from '../reducers/serviceDevelopmentReducer';
+import languageReducer, { IFetchedLanguageState } from '../utils/fetchLanguage/languageReducer';
 
 export interface IServiceDevelopmentReducers
   extends IServiceDevelopmentNameSpace<
+  Reducer<IFetchedLanguageState>,
   Reducer<IServiceDevelopmentState>
   >,
   ReducersMapObject { }
 
 const reducers: IServiceDevelopmentReducers = {
+  language: languageReducer,
   serviceDevelopment: serviceDevelopmentReducer,
 };
 
