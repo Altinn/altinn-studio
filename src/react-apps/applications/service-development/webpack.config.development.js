ca2111064baf96bcfdde00820695ec5604bee6ea
Kylstad, Nina <nina.kylstad@accenture.com>
2019-01-07 10:49:00 +0100

    got basic syntax highlighting working

--- src/react-apps/applications/service-development/webpack.config.development.js
+++ src/react-apps/applications/service-development/webpack.config.development.js
@@ -1,24 +1,16 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin
-//const MonacoPlugin = require('monaco-editor-webpack-plugin');
+const MonacoPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   devtool: 'eval',
-  entry: {
-    "service-development": "./src/index.tsx",
-    "editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
-    "json.worker": 'monaco-editor/esm/vs/language/json/json.worker.js',
-    "css.worker": 'monaco-editor/esm/vs/language/css/css.worker.js',
-    "html.worker": 'monaco-editor/esm/vs/language/html/html.worker.js',
-    "ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker.js',
-  },
+  entry: "./src/index.tsx",
   output: {
-    globalObject: 'self',
-    filename: "[name].js",
     path: path.resolve(__dirname, 'dist'),
+    filename: "service-development.js"
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".scss"],
@@ -93,9 +85,10 @@ module.exports = {
       filename: "service-development.css",
     }),
     new CheckerPlugin(),
-    // new MonacoPlugin({
-    //   output: path.join('..','js', 'react')
-    // }),
+    new MonacoPlugin({
+      output: path.join('..','js', 'react'),
+      languages: ['typescript', 'csharp']
+    }),
   ],
   devServer: {
     historyApiFallback: true,
