1e3779a04fa042f6c1919728ed211e91b499e22d
sekeberg <steffen.ekeberg@soprasteria.com>
2019-01-09 12:32:46 +0100

    Added missing nullcheck.

--- src/react-apps/applications/ux-editor/src/utils/apiConnection.ts
+++ src/react-apps/applications/ux-editor/src/utils/apiConnection.ts
@@ -23,6 +23,9 @@ export function getCodeListConnectionForDatamodelBinding(dataModelId: string, co
   Returns the index of a given code list name, or -1 if not found
 */
 export function getCodeListIndexByName(name: string, codeLists: ICodeListListElement[]) {
+  if (!codeLists) {
+    return -1;
+  }
   for (let i = 0; i < codeLists.length; i++) {
     if (codeLists[i].codeListName === name) {
       return i;
