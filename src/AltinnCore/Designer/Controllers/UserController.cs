9d4ad1257fe3527232fffb20ab436ef37f1847d6
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2019-01-10 13:44:01 +0100

    #701 - Missing authentication on controllers (#775)
    * Added authrorization.
    
    * Unwanted char
    
    * Auth at controller level not individual endpoints.

--- src/AltinnCore/Designer/Controllers/UserController.cs
+++ src/AltinnCore/Designer/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AltinnCore.Common.Configuration;
 using AltinnCore.Common.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -13,6 +14,7 @@ namespace AltinnCore.Designer.Controllers
     /// <summary>
     /// API controller for User functionality
     /// </summary>
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly IGitea _giteaApi;
