a45ead9be8aff6d65a0dcc7ed34a901fefd1a5d6
Kylstad, Nina <nina.kylstad@accenture.com>
2019-01-15 09:51:31 +0100

    Fix failing build

--- src/react-apps/applications/service-development/webpack.config.production.js
+++ src/react-apps/applications/service-development/webpack.config.production.js
@@ -10,6 +10,7 @@ module.exports = {
   devtool: false,
   entry: "./src/index.tsx",
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: "service-development.js"
   },
   resolve: {
