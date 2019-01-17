a42c4006b204d787f4ae080c0f5ffd9944fa3353
hanne.kottum <hanne.kottum@capgemini.com>
2019-01-17 15:39:25 +0100

    fix build fail

--- src/AltinnCore/Common/Services/Implementation/GiteaAPIWrapper.cs
+++ src/AltinnCore/Common/Services/Implementation/GiteaAPIWrapper.cs
@@ -467,27 +467,6 @@ namespace AltinnCore.Common.Services.Implementation
             return false;
         }
 
-        private bool IsLocalRepo(string org, string service)
-        {
-            string localServiceRepoFolder = _settings.GetServicePath(org, service, AuthenticationHelper.GetDeveloperUserName(_httpContextAccessor.HttpContext));
-            if (Directory.Exists(localServiceRepoFolder))
-            {
-                try
-                {
-                    using (LibGit2Sharp.Repository repo = new LibGit2Sharp.Repository(localServiceRepoFolder))
-                    {
-                        return true;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return false;
-                }
-            }
-
-            return false;
-        }
-
         private string GetStringFromHtmlContent(string htmlContent, string inputSearchTextBefore, string inputSearchTextAfter)
         {
             int start = htmlContent.IndexOf(inputSearchTextBefore);
