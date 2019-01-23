334c365df56320c2e8f8c60e306e0d9460b47ff5
Milford <malin.milford@gmail.com>
2019-01-23 11:10:19 +0100

    added calculation folder to path

--- src/AltinnCore/Common/Services/Implementation/RepositorySI.cs
+++ src/AltinnCore/Common/Services/Implementation/RepositorySI.cs
@@ -706,7 +706,7 @@ namespace AltinnCore.Common.Services.Implementation
                 serviceImplementationPath,
                 File.ReadAllText(serviceImplementationPath).Replace(oldRoot ?? CodeGeneration.DefaultServiceModelName, original.Elements.Values.First(el => el.ParentElement == null).TypeName));
 
-            string calculationHandlerPath = implementationDirectory + _settings.CalculationHandlerFileName;
+            string calculationHandlerPath = implementationDirectory + "Calculation/" + _settings.CalculationHandlerFileName;
             File.WriteAllText(
                 calculationHandlerPath,
                 File.ReadAllText(calculationHandlerPath).Replace(oldRoot ?? CodeGeneration.DefaultServiceModelName, original.Elements.Values.First(el => el.ParentElement == null).TypeName));
