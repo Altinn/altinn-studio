97db45bf641a81060922105ebe5e72b2811b20f1
malmil <37180955+malmil@users.noreply.github.com>
2019-02-06 09:06:44 +0100

    fix for #804 deselecting elements (#1055)
    * fix for #804 deselecting elements
    
    * Object.keys instead of Object.entries
    
    * id not needed
    
    * tsconfig.json change not needed

--- src/react-apps/applications/ux-editor/src/components/FormComponent.tsx
+++ src/react-apps/applications/ux-editor/src/components/FormComponent.tsx
@@ -49,6 +49,7 @@ export interface IFormElementProps extends IProvidedProps {
 export interface IFormElementState {
   component: FormComponentType;
   activeList: any[];
+  wrapperRef: any;
 }
 
 /**
@@ -64,7 +65,34 @@ class FormComponent extends React.Component<
     this.state = {
       component: _props.component,
       activeList: _props.activeList,
+      wrapperRef: '',
     };
+  }
+
+  public componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+  }
+
+  public setWrapperRef = (node: any) => {
+    if (node) {
+      this.setState({
+        wrapperRef: node.parentElement.parentElement,
+      });
+    }
+  }
+
+  /*
+  * Handle all types of clicks.
+  * Tracks if the click is outside of the formComponent
+  */
+  public handleClick = () => {
+    const key: any = Object.keys(this.props.order)[0];
+    const order = this.props.order[key].indexOf(this.props.id);
+
+    if (this.state.wrapperRef && !this.state.wrapperRef.contains(event.target) &&
+        order === 0) {
+      this.handleActiveListChange({});
+    }
   }
 
   /**
@@ -168,7 +196,11 @@ class FormComponent extends React.Component<
   }
 
   public handleActiveListChange = (obj: any) => {
-    FormDesignerActionDispatchers.updateActiveList(obj, this.props.activeList);
+    if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+      FormDesignerActionDispatchers.deleteActiveListAction();
+    } else  {
+      FormDesignerActionDispatchers.updateActiveList(obj, this.props.activeList);
+    }
     this.props.sendListToParent(this.props.activeList);
   }
 
@@ -194,6 +226,7 @@ class FormComponent extends React.Component<
     const key: any = Object.keys(this.props.order)[0];
     const order = this.props.order[key].indexOf(this.props.id);
     return (
+      <div ref={this.setWrapperRef}>
       <EditContainer
         component={this.props.component}
         id={this.props.id}
@@ -208,6 +241,7 @@ class FormComponent extends React.Component<
           {this.renderComponent()}
         </div>
       </EditContainer>
+      </div>
     );
   }
 
