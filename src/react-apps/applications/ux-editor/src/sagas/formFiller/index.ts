d0a2d68d579d21b9ee9e544dcb19d0989c3661f4
Kylstad, Nina <nina.kylstad@accenture.com>
2019-02-19 15:02:40 +0100

    merge with master - missing file

--- src/react-apps/applications/ux-editor/src/sagas/formFiller/index.ts
+++ src/react-apps/applications/ux-editor/src/sagas/formFiller/index.ts
@@ -1,11 +1,11 @@
 import { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 import {  watchCompleteAndSendInFormSaga,
-  watchFetchFormDataSaga,
-  watchResetFormDataSaga,
-  watchRunSingleFieldValidationSaga,
-  watchSubmitFormDataSaga,
-  watchUpdateFormDataSaga } from './formFillerSagas';
+          watchFetchFormDataSaga,
+          watchResetFormDataSaga,
+          watchRunSingleFieldValidationSaga,
+          watchSubmitFormDataSaga,
+          watchUpdateFormDataSaga } from './formFillerSagas';
 
 export default function*(): SagaIterator {
   yield fork(watchUpdateFormDataSaga);
