56914488e2c89ef8d53311df179c0fd4cb53992b
lorang92 <steffen.ekeberg@soprasteria.com>
2019-02-21 15:50:44 +0100

    Removed runtime tasks from Designer gulpfile.

--- src/AltinnCore/Designer/gulpfile.js
+++ src/AltinnCore/Designer/gulpfile.js
@@ -24,11 +24,9 @@ const jsServDevMonacoWorker1 = '../../react-apps/applications/service-developmen
 const jsServDevMonacoWorker2 = '../../react-apps/applications/service-development/js/react/typescript.worker.js';
 const jsDashboardFile = '../../react-apps/applications/dashboard/dist/dashboard.js';
 const jsUiEditorFile = '../../react-apps/applications/ux-editor/dist/react-app.js';
-const jsRuntimeFile = '../../react-apps/applications/runtime/dist/runtime.js';
 const cssServDevFile = '../../react-apps/applications/service-development/dist/service-development.css';
 const cssDashboardFile = '../../react-apps/applications/dashboard/dist/dashboard.css';
 const cssUiEditorFile = '../../react-apps/applications/ux-editor/dist/react-app.css';
-const cssRuntimeFile = '../../react-apps/applications/runtime/dist/runtime.css';
 
 let jsWatcher = null;
 let cssWatcher = null;
@@ -120,7 +118,6 @@ function copyReactJs(cb) {
   copyDashboardJs();
   copyServDevJs();
   copyUiEditorJs();
-  copyRuntimeJs();
   cb();
   return;
 }
@@ -129,7 +126,6 @@ function copyReactCss(cb) {
   copyDashboardCss();
   copyServDevCss();
   copyUiEditorCss();
-  copyRuntimeCss();
   cb();
   return;
 }
@@ -161,13 +157,6 @@ function copyUiEditorJs() {
   return;
 }
 
-function copyRuntimeJs() {
-  setTimeout(function () {
-    gulp.src(jsRuntimeFile).pipe(gulp.dest('./wwwroot/designer/js/react'));
-  }, 1000);
-  return;
-}
-
 function copyDashboardCss() {
   setTimeout(function () {
     gulp.src(cssDashboardFile).pipe(gulp.dest('./wwwroot/designer/css/react'));
@@ -185,13 +174,6 @@ function copyServDevCss() {
 function copyUiEditorCss() {
   setTimeout(function () {
     gulp.src(cssUiEditorFile).pipe(gulp.dest('./wwwroot/designer/css/react'));
-  }, 1000);
-  return;
-}
-
-function copyRuntimeCss() {
-  setTimeout(function () {
-    gulp.src(cssRuntimeFile).pipe(gulp.dest('./wwwroot/designer/css/react'));
   }, 1000);
   return;
 }
@@ -249,15 +231,6 @@ function setupWatchers(cb) {
     }
   }, 1000);
 
-  var checkRuntimeJsFile = setInterval(function () {
-    if (fs.existsSync(jsRuntimeFile)) {
-      jsWatcher = chokidar.watch(jsRuntimeFile);
-      // jsWatcher.on('ready', copyReactJs);
-      jsWatcher.on('change', copyRuntimeJs);
-      clearInterval(checkRuntimeJsFile);
-    }
-  }, 1000);
-
   var checkDashboardCssFile = setInterval(function () {
     if (fs.existsSync(cssDashboardFile)) {
       cssWatcher = chokidar.watch(cssDashboardFile);
@@ -282,15 +255,6 @@ function setupWatchers(cb) {
       // cssWatcher.on('ready', copyReactCss);
       cssWatcher.on('change', copyUiEditorCss);
       clearInterval(checkUiEditorCssFile);
-    }
-  }, 1000);
-
-  var checkRuntimeCssFile = setInterval(function () {
-    if (fs.existsSync(cssRuntimeFile)) {
-      cssWatcher = chokidar.watch(cssRuntimeFile);
-      // cssWatcher.on('ready', copyReactCss);
-      cssWatcher.on('change', copyRuntimeCss);
-      clearInterval(checkRuntimeCssFile);
     }
   }, 1000);
 
@@ -348,13 +312,6 @@ gulp.task('build-ux-editor', gulp.series(
   'copy-files'
 ));
 
-gulp.task('build-runtime', gulp.series(
-  run('npm run build', {
-    cwd: '../../react-apps/applications/runtime',
-  }),
-  'copy-files'
-));
-
 gulp.task('install-react-app-dependencies', gulp.series(
   run('lerna bootstrap --hoist', {
     cwd: '../../react-apps',
@@ -370,9 +327,6 @@ gulp.task('default', gulp.series([
   }),
   run('npm run build', {
     cwd: '../../react-apps/applications/ux-editor',
-  }),
-  run('npm run build', {
-    cwd: '../../react-apps/applications/runtime',
   }),
   'copy-files'
 ]));
