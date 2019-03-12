71b36392fc77d8e4d312c9cfe804f8764f3f258e
Milford <malin.milford@capgemini.com>
2019-03-12 10:28:53 +0100

    fix to only cache when reloading page. + globalsuppressions for readability rules

---
+++ src/AltinnCore/Runtime/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+// This file is used by Code Analysis to maintain suppressed messages
+
+[module: SuppressMessage("Microsoft.StyleCop.CSharp.ReadabilityRules", "SA1309:FieldNamesMustNotBeginWithUnderscore")]
+[module: SuppressMessage("Microsoft.StyleCop.CSharp.ReadabilityRules", "SA1101:PrefixLocalCallsWithThis")]
