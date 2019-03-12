3b0f7da9c4b0485a731bd4646a6c87c0184d8fe6
Milford <malin.milford@capgemini.com>
2019-03-12 10:51:47 +0100

    SA1515 blink lines fix

--- src/AltinnCore/Runtime/GlobalSuppressions.cs
+++ src/AltinnCore/Runtime/GlobalSuppressions.cs
@@ -3,4 +3,3 @@ using System.Diagnostics.CodeAnalysis;
 // This file is used by Code Analysis to maintain suppressed messages.
 [module: SuppressMessage("Microsoft.StyleCop.CSharp.ReadabilityRules", "SA1309:FieldNamesMustNotBeginWithUnderscore", Justification = "using _ takes less space")]
 [module: SuppressMessage("Microsoft.StyleCop.CSharp.ReadabilityRules", "SA1101:PrefixLocalCallsWithThis", Justification = "using _ takes less space")]
-
