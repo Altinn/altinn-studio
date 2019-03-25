92ff814afce580439a15f49ce37d05bcb77b6326
hanne.kottum <hanne.kottum@capgemini.com>
2019-03-25 08:46:40 +0100

    trying to fix db sln

--- src/Altinn.Platform/Storage/Controllers/DebugController.cs
+++ src/Altinn.Platform/Storage/Controllers/DebugController.cs
@@ -2,17 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Altinn.Platform.Storage.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Altinn.Platform.Storage.Controllers
 {
     [Route("api/v1/[controller]")]
     public class DebugController : Controller
     {
+        private readonly AzureCosmosSettings _cosmosettings;
+
+        public DebugController(IOptions<AzureCosmosSettings> cosmosettings)
+        {
+            _cosmosettings = cosmosettings.Value;
+        }
+
         [HttpGet("debug")]
         public async Task<ActionResult> Debug() 
         {
-            return Ok("test");
+            return Ok(_cosmosettings.EndpointUri);
         }
     }
 }
