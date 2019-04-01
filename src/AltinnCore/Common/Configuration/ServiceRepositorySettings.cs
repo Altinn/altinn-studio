a67a21165f53af20767c9702ebc1f783685e1eff
lorang92 <steffen.ekeberg@soprasteria.com>
2019-04-01 14:45:40 +0200

    Fixed after code review.

--- src/AltinnCore/Common/Configuration/ServiceRepositorySettings.cs
+++ src/AltinnCore/Common/Configuration/ServiceRepositorySettings.cs
@@ -671,7 +671,7 @@ namespace AltinnCore.Common.Configuration
         /// <param name="service">The name of the service</param>
         /// <param name="developer">The name of the developer of the service</param>
         /// <param name="partyId">The party id of the test user</param>
-        /// <param name="baseEndpoint">If the path should contain the pod to pod link, defaults to true. If set to false the path contains the altinn studio endpoint</param>
+        /// <param name="baseEndpoint"> Will be used as the url base if supplied, defaults to null.</param>
         /// <returns>The url path to the runtime api</returns>
         public string GetRuntimeAPIPath(string nameOfMethod, string org, string service, string developer, int partyId = 0, string baseEndpoint = null)
         {
