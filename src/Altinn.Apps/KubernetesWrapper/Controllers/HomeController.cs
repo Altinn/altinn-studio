3cc87d5ade8c0a53cb4bb601cda0564d5942051e
lorang92 <steffen.ekeberg@soprasteria.com>
2019-04-03 11:47:05 +0200

    Added dummy endpoint

---
+++ src/Altinn.Apps/KubernetesWrapper/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KubernetesWrapper.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KubernetesWrapper.Controllers
+{
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "hello";
+        }
+    }
+}
