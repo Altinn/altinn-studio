4ec946bfa83685ce765a5f780d2a5244f3da5085
hanne.kottum <hanne.kottum@capgemini.com>
2019-04-08 16:23:28 +0200

    here we go again

--- src/Altinn.Platform/Altinn.Platform.Profile/Profile/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Profile/Profile/Startup.cs
@@ -54,7 +54,7 @@ namespace Altinn.Platform.Profile
             if (Environment.GetEnvironmentVariable("GeneralSettings__ShouldUseMock") != null)
             {
                 _logger.LogWarning("GeneralSettings__ShouldUseMock is not null " + Environment.GetEnvironmentVariable("GeneralSettings__ShouldUseMock"));
-                shouldUseMock = Environment.GetEnvironmentVariable("GeneralSettings__ShouldUseMock") == "\"true\"" ? true : false;
+                shouldUseMock = bool.Parse(Environment.GetEnvironmentVariable("GeneralSettings__ShouldUseMock"));
             }
             else
             {
