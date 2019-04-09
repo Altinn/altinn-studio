52570e99ed78135293157dcb39c02a2a165e7300
trymen <trym.nielsen@soco.no>
2019-04-09 09:38:00 +0200

    Updated runtime components, added test skeleton for instantiating test

--- src/test/Testcafe/page-objects/runTimePage.js
+++ src/test/Testcafe/page-objects/runTimePage.js
@@ -1,15 +1,19 @@
 import { Selector, t, ClientFunction } from 'testcafe';
-import { createJsxSelfClosingElement } from 'typescript';
-
 
 export default class RunTimePage {
   constructor() {
     this.openManualTestWindow = Selector('#manual-test-button');
-    this.testUsers = Selector('.list-group.action-list-group');
+    //this.privatPerson = Selector("ul").child(0);
+    this.testUsers = [
+      Selector('ul').child(0),
+      Selector('ul').child(1),
+      Selector('ul').child(2),
+      Selector('ul').child(3)
+    ];
     this.languageSelection = Selector('#reporteeLanguageSelect');
-    this.changeLanguageButton = Selector(".btn.btn-primary").withAttribute('value', 'Oppdater språk');
+    this.changeLanguageButton = Selector('.btn.btn-primary').withAttribute('value', 'Oppdater språk');
     this.prefillData = Selector('#PrefillList');
-    this.startNewButton = Selector(".btn.btn-primary").withAttribute('value', 'Start ny');
-    this.backToAltinnStudio = Selector(".btn.btn-primary").withAttribute('value', 'Tilbake til Altinn Studio');
+    this.startNewButton = Selector('.btn.btn-primary').withAttribute('value', 'Start ny');
+    this.backToAltinnStudio = Selector('.btn.btn-primary').withAttribute('value', 'Tilbake til Altinn Studio');
   }
 }
--- src/test/Testcafe/runtime_tests/runtime-tests.js
+++ src/test/Testcafe/runtime_tests/runtime-tests.js
@@ -3,6 +3,7 @@ import { Selector, t } from 'testcafe';
 import CommonPage from '../page-objects/common';
 import DashBoard from '../page-objects/DashboardPage';
 import LoginPage from '../page-objects/loginPage';
+import RunTimePage from '../page-objects/runTimePage';
 import DesignerPage from '../page-objects/designerPage';
 import TestData from '../TestData';
 
@@ -10,6 +11,7 @@ let app = new App();
 let dash = new DashBoard();
 let loginPage = new LoginPage();
 let common = new CommonPage();
+let runtime = new RunTimePage();
 let designer = new DesignerPage();
 let runtimeUser = new TestData('AutoTest', 'automatictestaltinn@brreg.no', 'test123', 'basic');
 
@@ -19,14 +21,16 @@ fixture('Regression tests of services in runtime')
   .beforeEach(async t => {
     //Testdata and other testing context
     t.ctx.serviceName = "runtime_auto_test";
-    await common.login(testUser.userEmail, testUser.password, loginPage);
+    await common.login(runtimeUser.userEmail, runtimeUser.password, loginPage);
   })
 
-test.skip('Instantiate a service in runtime', async () => {
+test.only('Instantiate a service in runtime', async () => {
     await t
       .navigateTo(app.baseUrl + 'designer/AutoTest/runtime_auto_test#/aboutservice')
-      .hover(designer.testeNavigationTab)
+      .click(designer.testeNavigationTab)
       .hover(designer.leftDrawerMenu)
-      .click()
-      .wait(1000);
+      .click(designer.testeLeftMenuItems[0])
+      .click(runtime.openManualTestWindow)
+      .click(runtime.testUsers[0])
+      .click(runtime.startNewButton)
 });
