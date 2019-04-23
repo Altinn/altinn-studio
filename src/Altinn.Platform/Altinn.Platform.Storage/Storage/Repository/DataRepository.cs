e22406da6d57a745a783212a3c36510b3018648e
David Skogan <david.skogan@gmail.com>
2019-04-23 15:06:55 +0200

    refactor dataRepository

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Repository/DataRepository.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Repository/DataRepository.cs
@@ -29,12 +29,14 @@ namespace Altinn.Platform.Storage.Repository
         private static DocumentClient _client;
         private readonly AzureCosmosSettings _cosmosettings;
         private readonly AzureStorageConfiguration _storageConfiguration;
+        private CloudBlobClient blobClient;
+        private CloudBlobContainer container;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DataRepository"/> class
         /// </summary>
-        /// <param name="cosmosettings">the configuration settings for cosmos database</param>
-        /// <param name="storageConfiguration">the storage configuration</param>
+        /// <param name="cosmosettings">the configuration settings for azure cosmos database</param>
+        /// <param name="storageConfiguration">the storage configuration for azure blob storage</param>
         public DataRepository(IOptions<AzureCosmosSettings> cosmosettings, IOptions<AzureStorageConfiguration> storageConfiguration)
         {
             // Retrieve configuration values from appsettings.json
@@ -49,19 +51,13 @@ namespace Altinn.Platform.Storage.Repository
                 _databaseUri,
                 new DocumentCollection { Id = _cosmosettings.Collection }).GetAwaiter().GetResult();
             _storageConfiguration = storageConfiguration.Value;
-        }
 
-        public async Task<bool> CreateDataInStorage(Stream fileStream, string fileName)
-        {
+            // connect to azure blob storage
             StorageCredentials storageCredentials = new StorageCredentials(_storageConfiguration.AccountName, _storageConfiguration.AccountKey);
             CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
-            CloudBlobClient blobClient = CreateBlobClient(storageCredentials, storageAccount);
 
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfiguration.StorageContainer);
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
-
-            await blockBlob.UploadFromStreamAsync(fileStream);
-            return await Task.FromResult(true);
+            blobClient = CreateBlobClient(storageCredentials, storageAccount);
+            container = blobClient.GetContainerReference(_storageConfiguration.StorageContainer);
         }
 
         private CloudBlobClient CreateBlobClient(StorageCredentials storageCredentials, CloudStorageAccount storageAccount)
@@ -80,28 +76,41 @@ namespace Altinn.Platform.Storage.Repository
             return blobClient;
         }
 
+        /// <summary>
+        /// Creates a file in blob storage.
+        /// </summary>
+        /// <param name="fileStream">The file stream to read from</param>
+        /// <param name="fileName">The file name to writ to</param>
+        /// <returns></returns>
+        public async Task<bool> CreateDataInStorage(Stream fileStream, string fileName)
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            await blockBlob.UploadFromStreamAsync(fileStream);
+            return await Task.FromResult(true);
+        }
+       
+        /// <summary>
+        /// Updates a file in blob storage.
+        /// </summary>
+        /// <param name="fileStream">the stream to update from</param>
+        /// <param name="fileName">the name of the file to update</param>
+        /// <returns></returns>
         public async Task<bool> UpdateDataInStorage(Stream fileStream, string fileName)
         {
-            StorageCredentials storageCredentials = new StorageCredentials(_storageConfiguration.AccountName, _storageConfiguration.AccountKey);
-            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
-
-            CloudBlobClient blobClient = CreateBlobClient(storageCredentials, storageAccount);
-
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfiguration.StorageContainer);
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
             await blockBlob.UploadFromStreamAsync(fileStream);
             return await Task.FromResult(true);
         }
 
+        /// <summary>
+        /// Retrieves a file as a stream from blob storage.
+        /// </summary>
+        /// <param name="fileName">the file to retrieve</param>
+        /// <returns></returns>
         public async Task<Stream> GetDataInStorage(string fileName)
         {
-            StorageCredentials storageCredentials = new StorageCredentials(_storageConfiguration.AccountName, _storageConfiguration.AccountKey);
-            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
-            
-            CloudBlobClient blobClient = CreateBlobClient(storageCredentials, storageAccount);
-           
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfiguration.StorageContainer);
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
             var memoryStream = new MemoryStream();
@@ -110,14 +119,13 @@ namespace Altinn.Platform.Storage.Repository
             return memoryStream;
         }
 
+        /// <summary>
+        /// Deletes a file in blob storage.
+        /// </summary>
+        /// <param name="fileName">the file to delete</param>
+        /// <returns></returns>
         public async Task<bool> DeleteDataInStorage(string fileName)
-        {
-            StorageCredentials storageCredentials = new StorageCredentials(_storageConfiguration.AccountName, _storageConfiguration.AccountKey);
-            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
-
-            CloudBlobClient blobClient = CreateBlobClient(storageCredentials, storageAccount);
-
-            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfiguration.StorageContainer);
+        {           
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
 
             bool result = await blockBlob.DeleteIfExistsAsync();
