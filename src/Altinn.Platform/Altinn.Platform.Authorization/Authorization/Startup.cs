a13e25df117fa95e6577e74de4088b1c0cadf58d
Stephanie Buadu <stephanie.buadu@avanade.com>
2019-04-25 08:27:23 +0200

    Simplified configureservices

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Startup.cs
@@ -36,15 +36,10 @@ namespace Altinn.Platform.Authorization
         /// </summary>
         /// <param name="services">the service configuration.</param>
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
+        {   
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().AddControllersAsServices();
+            services.AddSingleton(Configuration);
         }
 
         /// <summary>
