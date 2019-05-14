5af3d4e510dad0367ef267aaa37f5b9bc0a35d38
hanne.kottum <hanne.kottum@capgemini.com>
2019-05-14 09:55:59 +0200

    started

---
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Altinn.Platform.Authentication.Controllers
+{
+    /// <summary>
+    /// Handles the authentication of requests to platform
+    /// </summary>
+    [Route("authentication/api/v1/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthenticationController"/> class
+        /// </summary>
+        /// <param name="logger">the logger</param>
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Request that handles the form authentication cookie from SBL
+        /// </summary>
+        /// <returns>redirect to correct url based on the validation of the form authentication sbl cookie</returns>
+        [HttpPost]
+        public Task<ActionResult> Post()
+        {
+            Request.Cookies["d"];
+            Request.Cookies
+            Request.Cookies["cookiename"];
+            return Ok();
+            
+        }
+    }
+}
