5461626dcfa4a6e4586ed5c04459ec76308881ee
hannekot <hannekottum@gmail.com>
2019-05-20 15:11:03 +0200

    Feature/1334 jwt authentication endpoint (#1767)
    * Added authentication controller
    
    * Deletet common altinncoreclaim to use JWTCookieAuthentication instead
    
    * added key vault
    
    * Added authentication level
    
    * last one
    
    * added cert
    
    * added pfx
    
    * Removed excess ; and added vscode specific files to gitignore

--- src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Extensions.Logging;
@@ -33,6 +36,38 @@ namespace Altinn.Platform.Authentication
         /// <returns>The web host builder</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+                config.SetBasePath(basePath);
+                config.AddJsonFile(basePath + "altinn-appsettings/altinn-appsettings-secret.json", optional: true, reloadOnChange: true);
+                if (basePath == "/")
+                {
+                    config.AddJsonFile(basePath + "app/appsettings.json", optional: false, reloadOnChange: true);
+                }
+                else
+                {
+                    config.AddJsonFile(Directory.GetCurrentDirectory() + "/appsettings.json", optional: false, reloadOnChange: true);
+                }
+
+                config.AddEnvironmentVariables();
+                config.AddCommandLine(args);
+                IConfiguration stageOneConfig = config.Build();
+                string appId = stageOneConfig.GetValue<string>("KvSetting:ClientId");
+                string tenantId = stageOneConfig.GetValue<string>("KvSetting:TenantId");
+                string appKey = stageOneConfig.GetValue<string>("KvSetting:ClientSecret");
+                string keyVaultEndpoint = stageOneConfig.GetValue<string>("KvSetting:SecretUri");
+                if (!string.IsNullOrEmpty(appId) && !string.IsNullOrEmpty(tenantId)
+                    && !string.IsNullOrEmpty(appKey) && !string.IsNullOrEmpty(keyVaultEndpoint))
+                {
+                    AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider($"RunAs=App;AppId={appId};TenantId={tenantId};AppKey={appKey}");
+                    KeyVaultClient keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+                    config.AddAzureKeyVault(
+                        keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                }
+            })
             .ConfigureLogging((hostingContext, logging) =>
             {
                 logging.ClearProviders();
