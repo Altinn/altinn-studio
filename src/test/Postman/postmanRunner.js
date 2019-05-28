2ac59653e92ea9d3530780b0337bce9994a84247
trymen <trym.nielsen@soco.no>
2019-05-28 13:42:23 +0200

    Added postman config files and run library file

---
+++ src/test/Postman/postmanRunner.js
@@ -0,0 +1,11 @@
+var newman = require('newman'); // require newman in your project
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./Events.postman_collection.json'),
+    environment: 'Platform_Altinn_Cloud.postman_environment.json',
+    reporters: 'junit'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
