f98647c945c007a50fb514839c4e6638c2eb380e
lorang92 <steffen.ekeberg@soprasteria.com>
2019-06-05 13:57:25 +0200

    Updated button component tests.

--- src/react-apps/applications/runtime/__tests__/components/base/ButtonComponent.test.tsx
+++ src/react-apps/applications/runtime/__tests__/components/base/ButtonComponent.test.tsx
@@ -1,10 +1,9 @@
 /* tslint:disable:jsx-wrap-multiline */
 import 'jest';
 import * as React from 'react';
-import * as renderer from 'react-test-renderer';
 
 import { mount, shallow } from 'enzyme';
-import { ButtonComponent } from '../../../src/components/base/ButtonComponent';
+import { ButtonComponentClass } from '../../../src/components/base/ButtonComponent';
 
 describe('>>> components/base/ButtonComponent.tsx', () => {
   let mockId: string;
@@ -12,28 +11,24 @@ describe('>>> components/base/ButtonComponent.tsx', () => {
   let formDataCount: number;
   let mockHandleDataChange: (value: any) => void;
   let mockDisabled: boolean;
-  let mockUnsavedChanges: boolean;
-  let mockValidationResults: any;
 
   beforeEach(() => {
     mockId = 'mock-id';
     mockHandleDataChange = (data: any) => null;
     mockDisabled = false;
-    mockUnsavedChanges = false;
-    mockValidationResults = {};
     mockText = 'Submit form';
     formDataCount = 0;
   });
 
   it('+++ should submit button should have text \'Submit form\'', () => {
     const wrapper = shallow(
-      <ButtonComponent
+      <ButtonComponentClass
         id={mockId}
         text={mockText}
         handleDataChange={mockHandleDataChange}
         disabled={mockDisabled}
         unsavedChanges={true}
-        validationResults={mockValidationResults}
+        validations={{}}
         formDataCount={formDataCount}
       />,
     );
@@ -44,13 +39,13 @@ describe('>>> components/base/ButtonComponent.tsx', () => {
 
   it('+++ should disable submit button and add class disabled if there are unsaved changes', () => {
     const wrapper = mount(
-      <ButtonComponent
+      <ButtonComponentClass
         id={mockId}
         text={mockText}
         handleDataChange={mockHandleDataChange}
         disabled={mockDisabled}
+        validations={{}}
         unsavedChanges={true}
-        validationResults={mockValidationResults}
         formDataCount={formDataCount}
       />,
     );
