f93fe4575a8a3862309088f2e778d33885f4afbf
trymen <trym.nielsen@soco.no>
2019-06-07 10:44:54 +0200

    retest

--- src/test/Postman/postmanRunner.sh
+++ src/test/Postman/postmanRunner.sh
@@ -1,5 +1,5 @@
 #Naive runner for newman postman cli tool. Add a new line with your new collection and environment
 npm install -g newman
-newman run ./collections/Altinn-platform-SBL-integration.postman_collection.json -e ./environments/Altinn-platform-SBL-bridge.postman_environment -r junit
-newman run ./collections/Events.postman_collection.json -e ./environments/Platform_Altinn_Cloud.postman_environment -r junit
-newman run ./collections/Events.postman_test_run.json -e ./environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run src/test/Postman/collections/Altinn-platform-SBL-integration.postman_collection.json -e src/test/Postman/environments/Altinn-platform-SBL-bridge.postman_environment -r junit
+newman run src/test/Postman/collections/Events.postman_collection.json -e src/test/Postman/environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run src/test/Postman/collections/Events.postman_test_run.json -e src/test/Postman/environments/Platform_Altinn_Cloud.postman_environment -r junit
