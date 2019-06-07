12f18f0f725623ccb9ea0828ee40ce3fa95f004d
trymen <trym.nielsen@soco.no>
2019-06-07 10:40:31 +0200

    Added npm install -g to bash script

--- src/test/Postman/postmanRunner.sh
+++ src/test/Postman/postmanRunner.sh
@@ -1,4 +1,5 @@
 #Naive runner for newman postman cli tool. Add a new line with your new collection and environment
+npm install -g newman
 newman run ./collections/Altinn-platform-SBL-integration.postman_collection.json -e ./environments/Altinn-platform-SBL-bridge.postman_environment -r junit
 newman run ./collections/Events.postman_collection.json -e ./environments/Platform_Altinn_Cloud.postman_environment -r junit
 newman run ./collections/Events.postman_test_run.json -e ./environments/Platform_Altinn_Cloud.postman_environment -r junit
