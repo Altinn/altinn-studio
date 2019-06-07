ae59983a7256a64afe3360a268b490e5dbe8ccd9
trymen <trym.nielsen@soco.no>
2019-06-07 13:00:32 +0200

    fixed path variable

--- src/test/Postman/postmanRunner.sh
+++ src/test/Postman/postmanRunner.sh
@@ -1,6 +1,6 @@
 #Naive runner for newman postman cli tool. Add a new line with your new collection and environment
-pwd=$(pwd)
+PATH="`dirname \"$0\"`"
 npm install -g newman
-newman run $pwd/collections/Altinn-platform-SBL-integration.postman_collection.json -e pwd/environments/Altinn-platform-SBL-bridge.postman_environment -r junit
-newman run $pwd/collections/Events.postman_collection.json -e pwd/environments/Platform_Altinn_Cloud.postman_environment -r junit
-newman run $pwd/collections/Events.postman_test_run.json -e pwd/environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run $PATH/collections/Altinn-platform-SBL-integration.postman_collection.json -e $PATH/environments/Altinn-platform-SBL-bridge.postman_environment -r junit
+newman run $PATH/collections/Events.postman_collection.json -e $PATH/environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run $PATH/collections/Events.postman_test_run.json -e $PATH/environments/Platform_Altinn_Cloud.postman_environment -r junit
