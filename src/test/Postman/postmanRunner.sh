b5cf3f841fc2c06273a4c7ae3b91d0992b7a51ee
trymen <trym.nielsen@soco.no>
2019-06-07 12:25:06 +0200

    Added pwd variable to find collections folder

--- src/test/Postman/postmanRunner.sh
+++ src/test/Postman/postmanRunner.sh
@@ -1,5 +1,6 @@
 #Naive runner for newman postman cli tool. Add a new line with your new collection and environment
+pwd=$(pwd)
 npm install -g newman
-newman run src/test/Postman/collections/Altinn-platform-SBL-integration.postman_collection.json -e src/test/Postman/environments/Altinn-platform-SBL-bridge.postman_environment -r junit
-newman run src/test/Postman/collections/Events.postman_collection.json -e src/test/Postman/environments/Platform_Altinn_Cloud.postman_environment -r junit
-newman run src/test/Postman/collections/Events.postman_test_run.json -e src/test/Postman/environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run $pwd/collections/Altinn-platform-SBL-integration.postman_collection.json -e pwd/environments/Altinn-platform-SBL-bridge.postman_environment -r junit
+newman run $pwd/collections/Events.postman_collection.json -e pwd/environments/Platform_Altinn_Cloud.postman_environment -r junit
+newman run $pwd/collections/Events.postman_test_run.json -e pwd/environments/Platform_Altinn_Cloud.postman_environment -r junit
