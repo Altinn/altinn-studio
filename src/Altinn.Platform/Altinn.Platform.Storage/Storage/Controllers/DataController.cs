25cd96b2d3d0d77074aa7df2d7c5201644487632
David Skogan <david.skogan@gmail.com>
2019-06-13 09:20:02 +0200

    fixed compiler bug

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Controllers/DataController.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Controllers/DataController.cs
@@ -307,7 +307,7 @@ namespace Altinn.Platform.Storage.Controllers
                 LastChangedBy = User.Identity.Name,
                 LastChangedDateTime = creationTime,
 
-                SelfLinks = new ResourceLinks()
+                DataLinks = new ResourceLinks()
                 {
                     Apps = dataLink,
                 },
