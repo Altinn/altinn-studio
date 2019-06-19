376163019b8ed0a9814abdbe2f0cbe7ed8b491c9
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2019-06-19 15:20:41 +0200

    Bug/1936 accessing app (#2018)
    * Added logging
    
    * Marking messages with origin project

--- src/Altinn.Platform/Altinn.Platform.Register/Register/Services/Implementation/PartiesWrapper.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Register/Services/Implementation/PartiesWrapper.cs
@@ -36,8 +36,8 @@ namespace Altinn.Platform.Register.Services.Implementation
         {
             Party party = null;
             DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Party));
-            Uri endpointUrl = new Uri($"{_generalSettings.GetApiBaseUrl()}parties/{partyId}");
-            _logger.LogInformation($" /// Access app test /// Sending request to bridge endpoint: {endpointUrl}");
+            Uri endpointUrl = new Uri($"{_generalSettings.GetApiBaseUrl()}/parties/{partyId}");
+            _logger.LogInformation($"// REGISTER Sending request to bridge endpoint: {endpointUrl}");
             using (HttpClient client = HttpApiHelper.GetApiClient())
             {
                 HttpResponseMessage response = await client.GetAsync(endpointUrl);
@@ -48,7 +48,7 @@ namespace Altinn.Platform.Register.Services.Implementation
                 }
                 else
                 {
-                    _logger.LogError($"New msg: Getting party with party Id {partyId} failed with statuscode {response.StatusCode}");
+                    _logger.LogError($"// REGISTER Getting party with party Id {partyId} failed with statuscode {response.StatusCode}");
                 }
             }
 
