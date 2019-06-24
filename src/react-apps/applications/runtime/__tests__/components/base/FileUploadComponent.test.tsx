9a11f6e11e9dd4e33d59f8c4492e93757ba23a17
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2019-06-24 12:48:00 +0200

    bug/1764-max-attachments (#2034)
    * Fixed buggy file upload comp.
    
    * Updated snapshot.
    
    * Added tests, fixed code smells.
    
    * Updated snpashot.
    
    * Updated as any.
    
    * Added validations test.
    
    * Added test for showFileUpload

--- src/react-apps/applications/runtime/__tests__/components/base/FileUploadComponent.test.tsx
+++ src/react-apps/applications/runtime/__tests__/components/base/FileUploadComponent.test.tsx
@@ -1,15 +1,15 @@
+/* tslint:disable:jsx-wrap-multiline */
 import { mount } from 'enzyme';
 import 'jest';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import * as renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
-import { FileUploadComponent, FileUploadComponentClass, getFileUploadComponentValidations } from '../../../src/components/base/FileUploadComponent';
+import { bytesInOneMB, FileUploadComponent, FileUploadComponentClass, getFileUploadComponentValidations } from '../../../src/components/base/FileUploadComponent';
 import { mapAttachmentListApiResponseToAttachments } from '../../../src/utils/attachment';
 
 describe('>>> components/base/FileUploadComponent.tsx', () => {
   let mockDisplayMode: string;
-  let mockHasCustomFileEndings: boolean;
   let mockId: string;
   let mockIsValid: boolean;
   let mockMaxFileSizeInMB: number;
@@ -46,11 +46,16 @@ describe('>>> components/base/FileUploadComponent.tsx', () => {
     mockMaxNumberOfAttachments = 4;
     mockMinNumberOfAttachments = 1;
     mockMaxFileSizeInMB = 2;
-    mockHasCustomFileEndings = false;
     mockDisplayMode = 'simple';
     mockIsValid = true;
     mockReadOnly = false;
-    mockFileList = [{ name: 'mock-name.txt', lastModified: null, size: 100, slice: null, type: null }];
+    mockFileList = [
+      { name: 'mock-name-1.txt', lastModified: null, size: 100, slice: null, type: null },
+      { name: 'mock-name-2.txt', lastModified: null, size: 100, slice: null, type: null },
+      { name: 'mock-name-3.txt', lastModified: null, size: 100, slice: null, type: null },
+      { name: 'mock-name-4.txt', lastModified: null, size: 200 * bytesInOneMB, slice: null, type: null },
+      { name: 'mock-name-5.txt', lastModified: null, size: 200 * bytesInOneMB, slice: null, type: null },
+    ];
     mockStore = createStore(mockInitialState);
   });
 
@@ -108,6 +113,73 @@ describe('>>> components/base/FileUploadComponent.tsx', () => {
     expect(instance.state.validations.length).toBe(0);
     instance.onDrop([], mockFileList);
     expect(instance.state.validations.length).toBe(1);
+  });
+
+  it('+++ should not upload any files if number of files are greater than max files', () => {
+    const wrapper = mount(
+      <FileUploadComponentClass
+        displayMode={mockDisplayMode}
+        id={mockId}
+        isValid={mockIsValid}
+        language={{}}
+        maxFileSizeInMB={mockMaxFileSizeInMB}
+        maxNumberOfAttachments={mockMaxNumberOfAttachments}
+        minNumberOfAttachments={mockMinNumberOfAttachments}
+        readOnly={mockReadOnly}
+        attachments={mockAttachments}
+      />,
+    );
+    const instance = wrapper.instance() as FileUploadComponentClass;
+    const spy = jest.spyOn(instance, 'setState');
+    instance.onDrop(mockFileList, []);
+    const call = spy.mock.calls[0][0] as any;
+    expect(call.attachments.length).toBe(mockAttachments.length);
+  });
+
+  it('+++ should upload all files if number of accepted files are less then max allowed files', () => {
+    const mockAccepted = [{ name: 'mock-name-1.txt', lastModified: null, size: 100, slice: null, type: null }];
+    const wrapper = mount(
+      <FileUploadComponentClass
+        displayMode={mockDisplayMode}
+        id={mockId}
+        isValid={mockIsValid}
+        language={{}}
+        maxFileSizeInMB={mockMaxFileSizeInMB}
+        maxNumberOfAttachments={mockMaxNumberOfAttachments}
+        minNumberOfAttachments={mockMinNumberOfAttachments}
+        readOnly={mockReadOnly}
+        attachments={mockAttachments}
+      />,
+    );
+    const instance = wrapper.instance() as FileUploadComponentClass;
+    const spy = jest.spyOn(instance, 'setState');
+    instance.onDrop(mockAccepted, []);
+    const call = spy.mock.calls[0][0] as any;
+    expect(call.attachments.length).toBe(mockAttachments.length + mockAccepted.length);
+  });
+
+  it('+++ should add validation messages if file is rejected', () => {
+    const wrapper = mount(
+      <FileUploadComponentClass
+        displayMode={mockDisplayMode}
+        id={mockId}
+        isValid={mockIsValid}
+        language={{}}
+        maxFileSizeInMB={10}
+        maxNumberOfAttachments={10}
+        minNumberOfAttachments={mockMinNumberOfAttachments}
+        readOnly={mockReadOnly}
+        attachments={mockAttachments}
+      />,
+    );
+    const instance = wrapper.instance() as FileUploadComponentClass;
+    const spy = jest.spyOn(instance, 'setState');
+    instance.onDrop([], mockFileList);
+    const call = spy.mock.calls[0][0] as any;
+    expect(call.validations.length).toBe(mockFileList.length);
+    // tslint:disable-next-line: max-line-length
+    expect(call.validations[0]).toBe('form_filler.file_uploader_validation_error_general_1 mock-name-1.txt form_filler.file_uploader_validation_error_general_2');
+    expect(call.validations[3]).toBe('mock-name-4.txt form_filler.file_uploader_validation_error_file_size');
   });
 
   it('+++ should trigger onDelete on when delete is clicked and update state to deleting for that attachment', () => {
@@ -215,4 +287,22 @@ describe('>>> components/base/FileUploadComponent.tsx', () => {
       ({ simpleBinding: { errors: ['Noe gikk galt under slettingen av filen, prøv igjen senere.'], warnings: [] } });
   });
 
+  it('+++ should not show file upload when max files is reached', () => {
+    const wrapper = mount(
+      <FileUploadComponentClass
+        displayMode={mockDisplayMode}
+        id={mockId}
+        isValid={mockIsValid}
+        language={{}}
+        maxFileSizeInMB={mockMaxFileSizeInMB}
+        maxNumberOfAttachments={3}
+        minNumberOfAttachments={mockMinNumberOfAttachments}
+        readOnly={mockReadOnly}
+        attachments={mockAttachments}
+      />,
+    );
+    const instance = wrapper.instance() as FileUploadComponentClass;
+    const result = instance.shouldShowFileUpload();
+    expect(result).toBe(false);
+  });
 });
