adc71cf4c255d023d9185e1dd71b64f779db0f89
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2019-07-08 15:10:52 +0200

    Added org and app to app url

--- src/react-apps/applications/runtime/src/features/form/containers/index.tsx
+++ src/react-apps/applications/runtime/src/features/form/containers/index.tsx
@@ -27,35 +27,35 @@ export default (props) => {
   React.useEffect(() => {
     const { org, service, reportee } = window as IAltinnWindow;
     LanguageActions.fetchLanguage(
-      `${window.location.origin}/api/Language/GetLanguageAsJSON`,
+      `${window.location.origin}/${org}/${service}/api/Language/GetLanguageAsJSON`,
       'nb',
     );
     FormDataModelActions.fetchDataModel(
-      `${window.location.origin}/api/metadata/${org}/${service}/ServiceMetaData`,
+      `${window.location.origin}/${org}/${service}/api/metadata/${org}/${service}/ServiceMetaData`,
     );
     FormLayoutActions.fetchFormLayout(
-      `${window.location.origin}/api/resource/${org}/${service}/FormLayout.json`,
+      `${window.location.origin}/${org}/${service}/api/resource/${org}/${service}/FormLayout.json`,
     );
     FormDataActions.fetchFormData(
-      `${window.location.origin}/api/${reportee}/${org}/${service}/Index/${instanceId}`,
+      `${window.location.origin}/${org}/${service}/api/${reportee}/${org}/${service}/Index/${instanceId}`,
     );
     FormRuleActions.fetchRuleModel(
-      `${window.location.origin}/api/resource/${org}/${service}/RuleHandler.js`,
+      `${window.location.origin}/${org}/${service}/api/resource/${org}/${service}/RuleHandler.js`,
     );
     FormWorkflowActions.getCurrentState(
       // tslint:disable-next-line:max-line-length
-      `${window.location.origin}/api/workflow/${reportee}/${org}/${service}/GetCurrentState?instanceId=${instanceId}`,
+      `${window.location.origin}/${org}/${service}/api/workflow/${reportee}/${org}/${service}/GetCurrentState?instanceId=${instanceId}`,
     );
 
     FormDynamicActions.fetchFormDynamics(
-      `${window.location.origin}/api/resource/${org}/${service}/ServiceConfigurations.json`,
+      `${window.location.origin}/${org}/${service}/api/resource/${org}/${service}/ServiceConfigurations.json`,
     );
     FormResourceActions.fetchFormResource(
-      `${window.location.origin}/api/textresources/${org}/${service}`,
+      `${window.location.origin}/${org}/${service}/api/textresources/${org}/${service}`,
     );
 
     ProfileActions.fetchProfile(
-      `${window.location.origin}/api/v1/profile/user`,
+      `${window.location.origin}/${org}/${service}/api/v1/profile/user`,
     );
 
     AttachmentActions.fetchAttachments();
