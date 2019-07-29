b5df4dbd08e3f7283949c899a27b985eabbfec50
Stephanie Buadu <stephanie.buadu@avanade.com>
2019-07-29 12:25:28 +0200

    Added missing xml comment

--- src/AltinnCore/ServiceLibrary/Models/ProfileSettingPreference.cs
+++ src/AltinnCore/ServiceLibrary/Models/ProfileSettingPreference.cs
@@ -1,37 +1,31 @@
 using AltinnCore.ServiceLibrary.ServiceMetadata;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AltinnCore.ServiceLibrary.Models
 {
-
+    /// <summary>
+    /// Class describing a users profile setting preferences
+    /// </summary>
     public class ProfileSettingPreference
     {
         /// <summary>
         /// Gets or sets the user ID that owns the preference
         /// </summary>
-        [DataMember]
         public int UserId { get; set; }
 
         /// <summary>
         /// Gets or sets the Language type available to the user in Altinn
         /// </summary>
-        [DataMember]
         public Language Language { get; set; }
 
         /// <summary>
         /// Gets or sets the user's preselected reportee
         /// </summary>
-        [DataMember]
         public int PreSelectedReporteeId { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the users want 
         /// to be asked for the reportee on every form submission
         /// </summary>
-        [DataMember]
         public bool DoNotPromptForReportee { get; set; }
-
     }
 }
