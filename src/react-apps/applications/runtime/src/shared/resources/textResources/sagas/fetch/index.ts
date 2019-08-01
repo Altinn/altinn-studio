b59ecc34a1962689288a6ff4f3ef709c7d742ba9
lorang92 <steffen.ekeberg@soprasteria.com>
2019-08-01 14:11:37 +0200

    Added text for textResources, some cleanup

--- src/react-apps/applications/runtime/src/shared/resources/textResources/sagas/fetch/index.ts
+++ src/react-apps/applications/runtime/src/shared/resources/textResources/sagas/fetch/index.ts
@@ -1,14 +1,13 @@
 import { SagaIterator } from 'redux-saga';
 import { call, takeLatest } from 'redux-saga/effects';
 import { get } from '../../../../../utils/networking';
-import { getTextResourcesUrl } from '../../../../../utils/urlHelper';
+import { textResourcesUrl } from '../../../../../utils/urlHelper';
 import TextResourcesActions from '../../actions';
 import { FETCH_TEXT_RESOURCES } from '../../actions/types';
 
 function* fetchTextResources(): SagaIterator {
   try {
-    const url = getTextResourcesUrl();
-    const resource = yield call(get, url);
+    const resource = yield call(get, textResourcesUrl);
     yield call(TextResourcesActions.fetchTextResourcesFulfilled, resource.language, resource.resources);
   } catch (err) {
     yield call(TextResourcesActions.fetchTextResourcesRejected, err);
--- src/react-apps/applications/runtime/src/utils/urlHelper.ts
+++ src/react-apps/applications/runtime/src/utils/urlHelper.ts
@@ -1,10 +1,6 @@
 const altinnWindow = window as any;
 const { origin, org, service, reportee } = altinnWindow;
 
-export const getVerifySubscriptionUrl = (): string => {
-  return `${origin}/api/v1/${org}/${service}/verifySubscription?partyId=${reportee}`;
-};
+export const verifySubscriptionUrl = `${origin}/api/v1/${org}/${service}/verifySubscription?partyId=${reportee}`;
 
-export const getTextResourcesUrl = (): string => {
-  return  `${origin}/${org}/${service}/api/textresources`;
-};
+export const textResourcesUrl = `${origin}/${org}/${service}/api/textresources`;
