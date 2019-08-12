2e79551f4b47d67f062d3fb0c819f0ab35e1e21f
Stephanie Buadu <stephanie.buadu@avanade.com>
2019-08-12 16:31:39 +0200

    Implement SBLClient, added request to sbl when party is updated in app backend

--- src/AltinnCore/Common/Services/Interfaces/IAuthorization.cs
+++ src/AltinnCore/Common/Services/Interfaces/IAuthorization.cs
@@ -5,23 +5,30 @@ using AltinnCore.ServiceLibrary.Models;
 namespace AltinnCore.Common.Services.Interfaces
 {
     /// <summary>
-    /// Interface for authorization functionality
+    /// Interface for authorization functionality.
     /// </summary>
     public interface IAuthorization
     {
         /// <summary>
-        /// Returns the list of parties that user has any rights for
+        /// Returns the list of parties that user has any rights for.
         /// </summary>
-        /// <param name="userId">The userId</param>
-        /// <returns>List of parties</returns>
+        /// <param name="userId">The userId.</param>
+        /// <returns>List of parties.</returns>
         List<Party> GetPartyList(int userId);
 
         /// <summary>
-        /// Verifies that the selected party is contained in the user's party list
+        /// Verifies that the selected party is contained in the user's party list.
         /// </summary>
-        /// <param name="userId">The user id"</param>
-        /// <param name="partyId">The party id"</param>
+        /// <param name="userId">The user id.</param>
+        /// <param name="partyId">The party id.</param>
         /// <returns> Boolean indicating whether or not the user can represent the selected party.</returns>
         Task<bool?> ValidateSelectedParty(int userId, int partyId);
+
+        /// <summary>
+        /// Notifies SBL that selected party has been updated.
+        /// </summary>
+        /// <param name="partyId">The party id.</param>
+        /// <returns> Boolean indicating whether or not the update was successful in SBL.</returns>
+        Task<bool> UpdateSelectedParty(int partyId);
     }
 }
