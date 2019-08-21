904a6121cd8622b17f8c5971d965350bb18d44eb
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2019-08-21 09:15:34 +0200

    removing models that are duplicated from servicelibrary (#2356)

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Services/Interface/IParties.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Services/Interface/IParties.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Authorization.Interface.Models;
+using AltinnCore.ServiceLibrary.Models;
 
 namespace Altinn.Platform.Authorization.Services.Interface
 {
