4077ed969d59e1e1debf730f806d2dee6cd21274
Hans Oluf Waaler <hans.waaler@brreg.no>
2019-08-26 10:06:51 +0200

    Add string extensions for file name sanitizations.

---
+++ src/AltinnCore/Common/Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AltinnCore.Common.Helpers.Extensions
+{
+    /// <summary>
+    /// Extensions to facilitate sanitization of string values
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Santize the input variable as a file name. If the parameter throwException is set to true, it will throw an invalidargumentexception
+        /// if the variable contains invalid characters. When it is set to false, it will replace the characters with '-'.
+        /// </summary>
+        /// <param name="input">The input variable to be sanitized</param>
+        /// <param name="throwExceptionOnInvalidCharacters">Throw exception if invalid characters are found</param>
+        /// <returns></returns>
+        public static string ToFileName(this string input, bool throwExceptionOnInvalidCharacters = true)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            var illegalFileNameCharacters = System.IO.Path.GetInvalidFileNameChars();
+            if (throwExceptionOnInvalidCharacters)
+            {
+                if (illegalFileNameCharacters.Any(ic => input.Any(i => ic == i)))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(input));
+                }
+
+                return input;
+            }
+
+            return illegalFileNameCharacters.Aggregate(input, (current, c) => current.Replace(c, '-'));
+        }
+    }
+}
---
+++ src/AltinnCore/UnitTest/Common/StringExtensionsTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AltinnCore.Common.Helpers.Extensions;
+using Xunit;
+
+namespace AltinnCore.UnitTest.Common
+{
+    /// <summary>
+    /// String Extensions related tests
+    /// </summary>
+    public class StringExtensionsTest
+    {
+        /// <summary>
+        /// Proper santization when using ToFileName without exceptions.
+        /// </summary>
+        [Fact]
+        public void ToFileNameReplaceSlashWithDash()
+        {
+            var invalidFileNames = new string[] { "/test/", "\\test\\" };
+
+            foreach (var invalidFileName in invalidFileNames)
+            {
+                var sanitizedName = invalidFileName.ToFileName(throwExceptionOnInvalidCharacters: false);
+                Assert.Equal("-test-", sanitizedName);
+            }
+
+        }
+
+        /// <summary>
+        /// Throws exception when an invalid character is encountered.
+        /// </summary>
+        [Fact]
+        public void ToFileNameThrowsException()
+        {
+            var invalidFileNames = new string[] { "/test/", "\\test\\" };
+
+            foreach (var invalidFileName in invalidFileNames)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => invalidFileName.ToFileName());
+            }
+        }
+    }
+}
