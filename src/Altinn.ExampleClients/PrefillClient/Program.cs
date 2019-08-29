dfa91ed620a34540f9210c5442043f231350191b
Kvernevik <cap-tkver@ai-dev.brreg.no>
2019-08-29 13:51:18 +0200

    Restructured to accept flags on arguments, added error writing to file in separate method.

--- src/Altinn.ExampleClients/PrefillClient/Program.cs
+++ src/Altinn.ExampleClients/PrefillClient/Program.cs
@@ -15,31 +15,43 @@ namespace Altinn.Clients.PrefillClient
     {
         static void Main(string[] args)
         {
-            Debug.WriteLine("Starting program");
-
-            // Incoming parameters in string[] args: [appId(*)] [endPoint(*)] [input directory(?)]
+            Console.WriteLine("Starting program.");
 
             string appId = null, endPoint = null, specifiedDirectory = null;
 
-            if (args == null || args.Length < 2)
+            if (args.Length > 3)
             {
-                Console.WriteLine("Did not find the parameters as prerequisites specified, please run the program again including these.");
-
-                System.Environment.Exit(0);
-            }
-            else if (args.Length == 2)
-            { 
-                appId = args[0];
-                endPoint = args[1];
-                // No input directory specified, use the application installation directory where the XML files will be situated.
-                specifiedDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string parameter = args[i].ToLower();
+                    switch (parameter)
+                    {
+                        case "-a":
+                            appId = args[++i];
+                            Console.WriteLine($"App ID: {appId}");
+                            break;
+                        case "-e":
+                            endPoint = args[++i];
+                            Console.WriteLine($"Endpoint: {endPoint}");
+                            break;
+                        case "-d":
+                            specifiedDirectory = args[++i];
+                            Console.WriteLine($"Specified directory: {specifiedDirectory}");
+                            break;
+                    }
+                }
             }
             else
             {
-                // More than two parameters: The input directory is specified by user
-                appId = args[0];
-                endPoint = args[1];
-                specifiedDirectory = args[2];
+                Console.WriteLine("Did not find the parameters as prerequisites specified, please run the program again including these.");
+                System.Environment.Exit(0);
+            }
+
+            if (string.IsNullOrEmpty(specifiedDirectory))
+            {
+                // No input directory specified, use the application installation directory where the XML files then is supposed to be situated.
+                specifiedDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                Console.WriteLine($"Specified directory: {specifiedDirectory}");
             }
             
             string [] xmlFilePaths = Directory.GetFiles(specifiedDirectory, "*.xml");
@@ -47,7 +59,6 @@ namespace Altinn.Clients.PrefillClient
             if (xmlFilePaths == null || xmlFilePaths.Length < 1)
             {
                 Console.WriteLine("Please add the XML files in your input directory, and run the program again.");
-                // TO DO: Write error to file in specified directory.
                 System.Environment.Exit(0);
             }
             
@@ -55,15 +66,13 @@ namespace Altinn.Clients.PrefillClient
             HttpClient client = new HttpClient();
 
             string xmlFileName, personNumber;
-            string errorFile = Path.Combine(specifiedDirectory, "error.txt");
 
             foreach (string xmlFilePath in xmlFilePaths)
             {
-                // Get the filename only
-                personNumber = xmlFilePath.Split("\\").Last().Split(".").First();
+                xmlFileName = xmlFilePath.Split("\\").Last();
 
                 // Get the person number which is the XML filename.
-                // personNumber = xmlFileName.Split(".").First();
+                personNumber = xmlFileName.Split(".").First();
 
                 Instance instanceTemplate = new Instance()
                 {
@@ -83,9 +92,8 @@ namespace Altinn.Clients.PrefillClient
                     
                     if (!response.IsSuccessStatusCode)
                     {
+                        WriteLogFile(response.ReasonPhrase, personNumber, specifiedDirectory);
                         Console.WriteLine($"Failed to instanciate and save prefill on person number {personNumber}.");
-                        //Debug.WriteLine($"Failed to instanciate and save prefill on person number {personNumber}.");
-                        //Debug.WriteLine($"{response.StatusCode} - {response.ReasonPhrase}");
                     }
                     else
                     {
@@ -98,36 +106,30 @@ namespace Altinn.Clients.PrefillClient
                 }
                 catch (Exception e)
                 {
-                    if (!File.Exists(errorFile))
-                    {
-                        File.Create(Path.Combine(errorFile)).Dispose();
-                    }
-
-                    using (StreamWriter sw = File.AppendText(errorFile))
-                    {
-                        sw.WriteLine("============= Error Logging ===========");
-                        sw.WriteLine($"=========== Start ============= {DateTime.Now}");
-                        sw.WriteLine($"Failed to instanciate and save prefill on person number {personNumber}.");
-                        sw.WriteLine($"Error Message: {e.Message}");
-                        sw.WriteLine($"=========== End ============= {DateTime.Now}");
-                    }
-                    
+                    WriteLogFile(e.Message, personNumber, specifiedDirectory);
                     Console.WriteLine($"Failed to instanciate and save prefill on person number {personNumber}.");
                 }
             }
         }
 
+        private static void WriteLogFile(string message, string personNumber, string specifiedDirectory)
+        {
+            string errorFile = Path.Combine(specifiedDirectory, "error.txt");
 
-        
+            if (!File.Exists(errorFile))
+            {
+                File.Create(Path.Combine(errorFile)).Dispose();
+            }
+
+            using (StreamWriter sw = File.AppendText(errorFile))
+            {
+                sw.WriteLine("============= Error Logging ===========");
+                sw.WriteLine($"=========== Start ============= {DateTime.Now}");
+                sw.WriteLine($"Failed to instanciate and save prefill on person number {personNumber}.");
+                sw.WriteLine($"Error Message: {message}");
+                sw.WriteLine($"=========== End ============= {DateTime.Now}");
+            }
+
+        }
     }
 }
-
-
-    /*
-    //client.Timeout = new TimeSpan(0, 0, 3);
-    //string prefix = "https://platform.at21.altinn.cloud/storage/api/v1";
-    //string prefix = "http://localhost:5010/storage/api/v1";
-    //Uri uri = new Uri(prefix + "/instances?appId=tdd/cat");
-
-    // endPoint could be like this: "http://localhost:5010/storage/api/v1/instances"
-    */
