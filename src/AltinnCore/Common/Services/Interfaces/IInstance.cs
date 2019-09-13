d73f3c42a68a0799f0dfc57d65aa3e81331a1b9d
David Skogan <david.skogan@gmail.com>
2019-09-13 08:40:25 +0200

    Moved deprecation message to InstantiateInstance

--- src/AltinnCore/Common/Services/Interfaces/IInstance.cs
+++ src/AltinnCore/Common/Services/Interfaces/IInstance.cs
@@ -16,7 +16,6 @@ namespace AltinnCore.Common.Services.Interfaces
         /// <summary>
         /// Gets the instance
         /// </summary>
-        [Obsolete("Method is deprecated, please use CreateInstance instead")]
         Task<Instance> GetInstance(string appName, string org, int instanceOwnerId, Guid instanceId);
 
         /// <summary>
@@ -27,6 +26,7 @@ namespace AltinnCore.Common.Services.Interfaces
         /// <summary>
         /// Stores the form data
         /// </summary>
+        [Obsolete("Method is deprecated, please use CreateInstance instead")]
         Task<Instance> InstantiateInstance(StartServiceModel startServiceModel, object serviceModel, IServiceImplementation serviceImplementation);
 
         /// <summary>
