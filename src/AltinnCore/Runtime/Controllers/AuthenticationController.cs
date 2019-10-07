f1eb678c403229c25506807050880e0e0a379cfe
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2019-10-07 14:59:24 +0200

    Added domain and expiration time for cookie

--- src/AltinnCore/Runtime/Controllers/AuthenticationController.cs
+++ src/AltinnCore/Runtime/Controllers/AuthenticationController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AltinnCore.Common.Configuration;
 using AltinnCore.Common.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -49,10 +51,16 @@ namespace AltinnCore.Runtime.Controllers
 
                 _logger.LogInformation($"token from platform{token}");
 
+                CookieOptions runtimeCookieSetting = new CookieOptions
+                {
+                    Domain = "at21.altinn.cloud",
+                    Expires = DateTime.UtcNow.AddMinutes(30),
+                };
+
                 if (!string.IsNullOrWhiteSpace(token))
                 {
                     _logger.LogInformation($"Starting to Append runtime cookie with token from platform{token}");
-                    HttpContext.Response.Cookies.Append(Common.Constants.General.RuntimeCookieName, token);
+                    HttpContext.Response.Cookies.Append(Common.Constants.General.RuntimeCookieName, token, runtimeCookieSetting);
                     _logger.LogInformation($"Appended runtime cookie with token from platform{token}");                  
                 }
             }
