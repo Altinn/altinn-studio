9bc482ac1562cd1eb221268d3a85a037f2a410a7
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2019-10-07 19:13:34 +0200

    Reverted the keep-alive functionality, uncommented it

--- src/react-apps/applications/runtime/src/App.tsx
+++ src/react-apps/applications/runtime/src/App.tsx
@@ -11,49 +11,49 @@ import LanguageActions from './shared/resources/language/languageActions';
 import PartyActions from './shared/resources/party/partyActions';
 import ProfileActions from './shared/resources/profile/profileActions';
 import TextResourcesActions from './shared/resources/textResources/actions';
-// import { get } from './utils/networking';
+import { get } from './utils/networking';
 import {
-//  getEnvironmentLoginUrl,
+  getEnvironmentLoginUrl,
   languageUrl,
   profileApiUrl,
-//  refreshJwtTokenUrl,
+  refreshJwtTokenUrl,
 } from './utils/urlHelper';
 
 const theme = createMuiTheme(AltinnAppTheme);
 
 // 1 minute = 60.000ms
-// const TEN_MINUTE_IN_MILLISECONDS: number = 60000 * 10;
+const TEN_MINUTE_IN_MILLISECONDS: number = 60000 * 10;
 
 export default function() {
-  // let lastRefreshTokenTimestamp: number = 0;
+  let lastRefreshTokenTimestamp: number = 0;
 
-  // function setUpEventListeners() {
-  //   window.addEventListener('mousemove', refreshJwtToken);
-  //   window.addEventListener('scroll', refreshJwtToken);
-  //   window.addEventListener('onfocus', refreshJwtToken);
-  // }
+  function setUpEventListeners() {
+    window.addEventListener('mousemove', refreshJwtToken);
+    window.addEventListener('scroll', refreshJwtToken);
+    window.addEventListener('onfocus', refreshJwtToken);
+  }
 
-  // function removeEventListeners() {
-  //   window.removeEventListener('mousemove', refreshJwtToken);
-  //   window.removeEventListener('scroll', refreshJwtToken);
-  //   window.removeEventListener('onfocus', refreshJwtToken);
-  // }
+  function removeEventListeners() {
+    window.removeEventListener('mousemove', refreshJwtToken);
+    window.removeEventListener('scroll', refreshJwtToken);
+    window.removeEventListener('onfocus', refreshJwtToken);
+  }
 
-  // function refreshJwtToken() {
-  //   const timeNow = Date.now();
-  //   if ((timeNow - lastRefreshTokenTimestamp) > TEN_MINUTE_IN_MILLISECONDS) {
-  //     lastRefreshTokenTimestamp = timeNow;
-  //     get(refreshJwtTokenUrl)
-  //     .catch((err) => {
-  //       // Most likely the user has an expired token, so we redirect to the login-page
-  //       try {
-  //         window.location.href = getEnvironmentLoginUrl();
-  //       } catch (error) {
-  //         console.error(err, error);
-  //       }
-  //     });
-  //   }
-  // }
+  function refreshJwtToken() {
+    const timeNow = Date.now();
+    if ((timeNow - lastRefreshTokenTimestamp) > TEN_MINUTE_IN_MILLISECONDS) {
+      lastRefreshTokenTimestamp = timeNow;
+      get(refreshJwtTokenUrl)
+      .catch((err) => {
+        // Most likely the user has an expired token, so we redirect to the login-page
+        try {
+          window.location.href = getEnvironmentLoginUrl();
+        } catch (error) {
+          console.error(err, error);
+        }
+      });
+    }
+  }
 
   React.useEffect(() => {
     TextResourcesActions.fetchTextResources();
@@ -61,10 +61,10 @@ export default function() {
     LanguageActions.fetchLanguage(languageUrl, 'nb');
     ApplicationMetadataActions.getApplicationMetadata();
     PartyActions.getParties();
-    // setUpEventListeners();
-    // return function cleanup() {
-    //  removeEventListeners();
-    // };
+    setUpEventListeners();
+    return function cleanup() {
+     removeEventListeners();
+    };
   }, []);
 
   return (
