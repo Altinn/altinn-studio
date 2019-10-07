575b677e0c76a9086c5dd5f73ed6f90657d32a1b
jezpoz <jesfor94@gmail.com>
2019-10-07 09:00:22 +0200

    fix another bug with one slap

--- src/react-apps/applications/runtime/src/shared/resources/party/getParties/getPartiesSagas.ts
+++ src/react-apps/applications/runtime/src/shared/resources/party/getParties/getPartiesSagas.ts
@@ -3,34 +3,12 @@ import { call, select, takeLatest } from 'redux-saga/effects';
 import { IRuntimeState } from 'src/types';
 import { IParty } from '../';
 import { get } from '../../../../utils/networking';
+import { findSelectedParty } from '../../../../utils/partyUtils';
 import { allPartiesUrl, currentPartyUrl, validPartiesUrl } from '../../../../utils/urlHelper';
 import PartyActions from '../partyActions';
 import * as GetPartyActionTypes from './getPartiesActionTypes';
 
 const SelectedPartySelector = ((state: IRuntimeState) => state.party.selectedParty);
-
-function findPartyByPartyId(party: IParty, partyId: string) {
-  if (party.partyId === partyId) {
-    return party;
-  } else if (party.childParties && party.childParties.length > 0) {
-    for (const childParty of party.childParties) {
-      return findPartyByPartyId(childParty, partyId);
-    }
-  } else {
-    return null;
-  }
-}
-
-function findSelectedParty(parties: IParty[], partyId: string): IParty {
-  let selectedParty: IParty = null;
-  for (const party of parties) {
-    selectedParty = findPartyByPartyId(party, partyId);
-    if (selectedParty !== null) {
-      break;
-    }
-  }
-  return selectedParty;
-}
 
 function* getPartiesSaga(): SagaIterator {
   try {
