63b9d99c618cd5b1c2c3385680c02fce5b45366f
Dhanalakshmi Gopalswamy <d.gopalswamy@avanade.com>
2019-10-11 09:33:25 +0200

    Fix for sonar cloud comments

--- src/Altinn.Platform/Altinn.Platform.Register/Register/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Register/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Altinn.Platform.Register.Configuration;
 using Altinn.Platform.Register.Services.Implementation;
 using Altinn.Platform.Register.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Altinn.Platform.Register
 {
