36a767a31c52b3bd16989497074ca0abedbadbac
Stephanie Buadu <stephanie.buadu@avanade.com>
2019-10-13 18:48:35 +0200

    initial commit

--- src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Startup.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Net;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.IdentityModel.Tokens;
 using AltinnCore.Authentication.JwtCookie;
 using AltinnCore.Common.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Altinn.Platform.Receipt
 {
@@ -58,11 +58,10 @@ namespace Altinn.Platform.Receipt
                     options.Cookie.Name = "AltinnStudioRuntime";
                 });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddMvc().AddControllersAsServices();
             services.AddSingleton(Configuration);
             services.Configure<GeneralSettings>(Configuration.GetSection("GeneralSettings"));
             services.Configure<PlatformSettings>(Configuration.GetSection("PlatformSettings"));
+            services.AddControllers();
         }
 
         /// <summary>
@@ -70,7 +69,7 @@ namespace Altinn.Platform.Receipt
         /// </summary>
         /// <param name="app">the application builder.</param>
         /// <param name="env">the hosting environment.</param>
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             string runtimeMode = string.Empty;
             if (Environment.GetEnvironmentVariable("GeneralSettings__RuntimeMode") != null)
@@ -115,17 +114,19 @@ namespace Altinn.Platform.Receipt
                     response.Redirect(($"{authenticationEndpoint}authentication?goto={url}"));
                 }
             });
-            app.UseMvc(routes =>
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
                     name: "languageRoute",
-                    template: "receipt/api/v1/{controller}/{action=Index}",
+                    pattern: "receipt/api/v1/{controller}/{action=Index}",
                     defaults: new { controller = "Language" },
                     constraints: new
                     {
                         controller = "Language",
-                    }
-                );
+                    });
             });
         }
     }
