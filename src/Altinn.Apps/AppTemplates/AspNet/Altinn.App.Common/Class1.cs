f41383bdd0c74c48c796ee4ad55da91fb2586c77
Rune T. Larsen <rune@hjemmekino.no>
2019-10-16 09:20:30 +0200

    Initial commit of concept of new app template

---
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Altinn.App.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Altinn.App.Services
+{
+    public class Class1
+    {
+    }
+}
