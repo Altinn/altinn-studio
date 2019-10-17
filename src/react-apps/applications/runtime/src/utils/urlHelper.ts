3fa03471a57127fb528d05b6bf563168f4a36a6b
Truls Clark <truls.clark@capgemini.com>
2019-10-17 14:15:13 +0200

    added exported var appPath, changed instantiateURL to new api, updated vars with appath

--- src/react-apps/applications/runtime/src/utils/urlHelper.ts
+++ src/react-apps/applications/runtime/src/utils/urlHelper.ts
@@ -2,24 +2,26 @@ const altinnWindow = window as any;
 const { org, app, reportee } = altinnWindow;
 const origin = window.location.origin;
 
+export const appPath = `${origin}/${org}/${app}`;
+
 export const verifySubscriptionUrl = `${origin}/api/v1/${org}/${app}/verifySubscription?partyId=${reportee}`;
-export const languageUrl = `${origin}/${org}/${app}/api/Language/GetLanguageAsJSON`;
-export const profileApiUrl = `${origin}/${org}/${app}/api/v1/profile/user`;
-export const applicationMetadataApiUrl = `${origin}/${org}/${app}/api/v1/applicationmetadata`;
-export const textResourcesUrl = `${origin}/${org}/${app}/api/textresources`;
+export const languageUrl = `${appPath}/api/Language/GetLanguageAsJSON`;
+export const profileApiUrl = `${appPath}/api/v1/profile/user`;
+export const applicationMetadataApiUrl = `${appPath}/api/v1/applicationmetadata`;
+export const textResourcesUrl = `${appPath}/texts?lang=nb`;
 export const updateCookieUrl: (partyId: string) => string = (partyId: string) => `
-  ${origin}/${org}/${app}/api/v1/parties/${partyId}
+  ${appPath}/api/v1/parties/${partyId}
 `;
 export const validPartiesUrl: string =
-  `${origin}/${org}/${app}/api/v1/parties?allowedtoinstantiatefilter=true`;
+  `${appPath}/api/v1/parties?allowedtoinstantiatefilter=true`;
 export const allPartiesUrl: string =
-`${origin}/${org}/${app}/api/v1/parties?allowedtoinstantiatefilter=false`;
-export const instantiateUrl: string = `${origin}/${org}/${app}/Instance/InstantiateApp`;
-export const currentPartyUrl: string = `${origin}/${org}/${app}/api/authorization/parties/current`;
-export const instancesControllerUrl: string = `${origin}/${org}/${app}/instances`;
-export const partySelectionUrl: string = `${origin}/${org}/${app}/#/partyselection`;
-export const refreshJwtTokenUrl: string = `${origin}/${org}/${app}/api/authentication/keepAlive`;
-export const reactErrorPage: string = `${origin}/${org}/${app}/#/error`;
+`${appPath}/api/v1/parties?allowedtoinstantiatefilter=false`;
+export const instantiateUrl: string = `${appPath}/instances`;
+export const currentPartyUrl: string = `${appPath}/api/authorization/parties/current`;
+export const instancesControllerUrl: string = `${appPath}/instances`;
+export const partySelectionUrl: string = `${appPath}/#/partyselection`;
+export const refreshJwtTokenUrl: string = `${appPath}/api/authentication/keepAlive`;
+export const reactErrorPage: string = `${appPath}/#/error`;
 
 export const getEnvironmentLoginUrl: () => string = () => {
   // First split away the protocol 'https://' and take the last part. Then split on dots.
@@ -31,6 +33,7 @@ export const getEnvironmentLoginUrl: () => string = () => {
     return `https://platform${domainSplitted[2]}.${domainSplitted[3]}` +
       `/authentication/api/v1/authentication?goto=${window.location.href}`;
   } else {
+    // TODO: what if altinn3?
     throw new Error('Unknown domain');
   }
 };
