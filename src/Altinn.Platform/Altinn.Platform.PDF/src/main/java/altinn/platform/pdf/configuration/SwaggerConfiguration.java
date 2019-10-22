65be7083f5376b63868a6ebbfd646e06d4aa55f3
lorang92 <steffen.ekeberg@soprasteria.com>
2019-10-22 10:48:08 +0200

    final tuning of env variables

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/configuration/SwaggerConfiguration.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/configuration/SwaggerConfiguration.java
@@ -1,6 +1,5 @@
 package altinn.platform.pdf.configuration;
 
-import altinn.platform.pdf.services.BasicLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,25 +7,17 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.ServletContext;
-import java.util.logging.Level;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
   @Bean
-  public Docket api(ServletContext servletContext) {
+  public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-      .pathProvider(new RelativePathProvider(servletContext) {
-      @Override
-      public String getApplicationBasePath() {
-        return "pdf" + super.getApplicationBasePath();
-      }
-      })
       .host(System.getenv("SWAGGER_BASE_URL"))
       .select()
       .apis(RequestHandlerSelectors.basePackage("altinn.platform.pdf"))
