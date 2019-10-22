4cfdb556097f0844f4c95851efd6ea00755b6f8c
Stephanie Buadu <stephanie.buadu@avanade.com>
2019-10-22 09:22:04 +0200

    blob infrastructure set up

---
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Configuration/AzureStorageConfiguration.cs
@@ -0,0 +1,28 @@
+namespace Altinn.Platform.Authorization.Configuration
+{
+    /// <summary>
+    /// Settings for Azure storage
+    /// </summary>
+    public class AzureStorageConfiguration
+    {
+        /// <summary>
+        /// storage account name
+        /// </summary>
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// storage account key
+        /// </summary>
+        public string AccountKey { get; set; }
+
+        /// <summary>
+        /// name of the storage container in the storage account
+        /// </summary>
+        public string StorageContainer { get; set; }
+
+        /// <summary>
+        /// url for the blob end point
+        /// </summary>
+        public string BlobEndPoint { get; set; }
+    }
+}
