b467d2849275be41e4457fd2b4ff644b7be7d1bc
lorang92 <steffen.ekeberg@soprasteria.com>
2019-10-23 13:55:55 +0200

    added title, updated snapshot

--- src/react-apps/applications/shared/src/components/AltinnSearchInput.tsx
+++ src/react-apps/applications/shared/src/components/AltinnSearchInput.tsx
@@ -54,9 +54,10 @@ export class AltinnSearchInput extends React.Component<IAltinnSearchInputCompone
             startAdornment:
               <InputAdornment
                 position={'end'}
+
                 classes={{ root: classNames(classes.searchBoxIcon) }}
               >
-                <i className={'fa fa-search'} />
+                <i className={'fa fa-search'} title="Search icon"/>
               </InputAdornment>,
             classes: { root: classNames(classes.searchBoxInput) },
           }}
