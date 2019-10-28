b375107d78fa582afe57f35b6a8e415f8dee5267
Terje Holene <terje.holene@gmail.com>
2019-10-28 15:15:53 +0100

    Removed test using outdated token

--- src/Altinn.Platform/Altinn.Platform.Authentication/Altinn.Platform.Authentication.IntegrationTests/Controllers/AuthenticationControllerTest.cs
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Altinn.Platform.Authentication.IntegrationTests/Controllers/AuthenticationControllerTest.cs
@@ -38,34 +38,6 @@ namespace Altinn.Platform.Authentication.IntegrationTests
         /// Test of method <see cref="AuthenticationController.AuthenticateOrganisation"/>.
         /// </summary>
         [Fact]
-        public async Task OrganisationAuthenticationWithMaskinportenToken()
-        {
-            string externalToken = "eyJraWQiOiJjWmswME1rbTVIQzRnN3Z0NmNwUDVGSFpMS0pzdzhmQkFJdUZiUzRSVEQ0IiwiYWxnIjoiUlMyNTYifQ.eyJhdWQiOiJodHRwczpcL1wvdHQwMi5hbHRpbm4ubm9cL21hc2tpbnBvcnRlbi1hcGlcLyIsInNjb3BlIjoiYWx0aW5uOmluc3RhbmNlcy5yZWFkIGFsdGlubjppbnN0YW5jZXMud3JpdGUiLCJpc3MiOiJodHRwczpcL1wvb2lkYy12ZXIyLmRpZmkubm9cL2lkcG9ydGVuLW9pZGMtcHJvdmlkZXJcLyIsImNsaWVudF9hbXIiOiJ2aXJrc29taGV0c3NlcnRpZmlrYXQiLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiZXhwIjoxNTcyMDAzNzE0LCJpYXQiOjE1NzE5OTE3MTQsImNsaWVudF9pZCI6IjBkZTE5ZjdhLWY1ZmEtNDVkMS04NzRjLTNkMmU4OGNlOTdkOSIsImNsaWVudF9vcmdubyI6Ijk3NDc2MDY3MyIsImp0aSI6IkJjTklBdVpLWGRqcEVDbW13YWxBbS1wY0JwMGlOYzU2VDZlWGhseE5CWkUiLCJjb25zdW1lciI6eyJhdXRob3JpdHkiOiJpc282NTIzLWFjdG9yaWQtdXBpcyIsIklEIjoiMDE5Mjo5NzQ3NjA2NzMifX0.OHUF7aSlxKFpVzK8j_NVLvA3sALKwlIXzwhb4K2WyAo9bmlHNhp5nW7qjJssXRpNb-btgJn1wcKnK4H-0FE2wj82OmJKDxIcfDgy3vvdAwiTLaG-wbZqZxSpRIErl7FMbRAotwqe1ioPEQLXOO17LZv5MOwUkp54ftf9RLv3jd01WViESWwPDbcKd5jfLC5HRvnta_RHUBsOHwH_of7ivOPmwBmP23MIj6m3KQkYLahPNaPU4TLoywrUUmaEvL4zOXqhhMu0_D_k49S0ZtDTU80ItEEH9-ra-nFdpqu_Y5IeeQ-xc2Lt3n2-K9ff5MN9Xk3o1yR22epbNkSYafxcRQ";
-            HttpClient client = GetClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", externalToken);
-
-            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "/authentication/api/v1/convert");
-
-            // Act
-            HttpResponseMessage response = await client.SendAsync(requestMessage);
-
-            // Assert
-            string token = await response.Content.ReadAsStringAsync();
-
-            ClaimsPrincipal principal = JwtTokenMock.ValidateToken(token);
-
-            Assert.NotNull(principal);
-
-            Assert.True(principal.HasClaim(c => c.Type == "org"));
-
-            Assert.Equal("974760673", principal.Claims.First(c => c.Type == "orgNumber").Value);
-            Assert.Equal("brg", principal.Claims.First(c => c.Type == "org").Value);
-        }
-
-        /// <summary>
-        /// Test of method <see cref="AuthenticationController.AuthenticateOrganisation"/>.
-        /// </summary>
-        [Fact]
         public async Task OrganisationAuthentication()
         {
             // Arrange
@@ -105,20 +77,6 @@ namespace Altinn.Platform.Authentication.IntegrationTests
             Assert.NotNull(principal);            
 
             Assert.True(principal.HasClaim(c => c.Type == "org"));
-        }
-
-        private HttpClient GetClient()
-        {
-            string projectDir = Directory.GetCurrentDirectory();
-            string configPath = Path.Combine(projectDir, "appsettings.json");
-
-            HttpClient client = factory.WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureTestServices(services => { services.AddSingleton<ISigningKeysRetriever, SigningKeysRetriever>(); });
-                builder.ConfigureAppConfiguration((context, conf) => { conf.AddJsonFile(configPath); });
-            }).CreateClient();
-
-            return client;
         }
 
         private HttpClient GetTestClient()
