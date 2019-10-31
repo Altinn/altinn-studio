4850ccb6d5cb529b674c0f9a99a3e59733668e3b
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2019-10-31 14:45:00 +0100

    Removed sass-loader

--- src/react-apps/applications/service-development/webpack.config.production.js
+++ src/react-apps/applications/service-development/webpack.config.production.js
@@ -36,8 +36,7 @@ module.exports = {
         test: /\.scss$/,
         use: [
           "style-loader",
-          "css-loader",
-          "sass-loader"
+          "css-loader"
         ]
       },
       {
