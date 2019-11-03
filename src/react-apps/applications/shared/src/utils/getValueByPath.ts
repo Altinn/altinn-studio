9bbebf8cad15d427712dbfc8e53a8157869041f3
Mats Gimming Myhre <mats.myhre@capgemini.com>
2019-11-03 09:52:17 +0100

    Fixed LGTM for getValueByPath

--- src/react-apps/applications/shared/src/utils/getValueByPath.ts
+++ src/react-apps/applications/shared/src/utils/getValueByPath.ts
@@ -1,5 +1,6 @@
 // Example
 // getValueByPath(obj, 'some.path.to.value', null)
+// does not work with arrays
 
 export const getValueByPath = (value: string, path: string, defaultValue: any) => {
   return String(path).split('.').reduce((accVal, currVal: any) => {
