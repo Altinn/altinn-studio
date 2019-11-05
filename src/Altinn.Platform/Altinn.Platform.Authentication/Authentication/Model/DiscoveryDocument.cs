37fc36ee4453592e30be6bd4885f84f29d4608a7
Terje Holene <terje.holene@gmail.com>
2019-11-05 13:35:58 +0100

    Adding documentation to DiscoveryDocument

--- src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Model/DiscoveryDocument.cs
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Model/DiscoveryDocument.cs
@@ -50,42 +50,81 @@ namespace Altinn.Platform.Authentication.Model
         [JsonPropertyName("check_session_iframe")]
         public string CheckSessionIframe { get; set; }
 
+        /// <summary>
+        /// URL for the revocation endpoint.
+        /// </summary>
         [JsonPropertyName("revocation_endpoint")]
         public string RevocationEndpoint { get; set; }
 
+        /// <summary>
+        /// URL for the introspection endpoint.
+        /// </summary>
         [JsonPropertyName("introspection_endpoint")]
         public string IntrospectionEndpoint { get; set; }
 
+        /// <summary>
+        /// Value indicating whether there is a front channel mechanism for logout.
+        /// </summary>
         [JsonPropertyName("frontchannel_logout_supported")]
         public bool? FrontchannelLogoutSupported { get; set; }
 
+        /// <summary>
+        /// Value indicating wheter there is a front channel mechanism for session logout.
+        /// </summary>
         [JsonPropertyName("frontchannel_logout_session_supported")]
         public bool? FrontchannelLogoutSessionSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported scopes.
+        /// </summary>
         [JsonPropertyName("scopes_supported")]
         public string[] ScopesSupported { get; set; }
-        
+
+        /// <summary>
+        /// Array of supported claims.
+        /// </summary>
         [JsonPropertyName("claims_supported")]
         public string[] ClaimsSupported { get; set; }
-        
+
+        /// <summary>
+        /// Array of supported response types.
+        /// </summary>
         [JsonPropertyName("response_types_supported")]
         public string[] ResponseTypesSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported response modes.
+        /// </summary>
         [JsonPropertyName("response_modes_supported")]
         public string[] ResponseModesSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported grant types.
+        /// </summary>
         [JsonPropertyName("grant_types_supported")]
         public string[] GrantTypesSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported subject types.
+        /// </summary>
         [JsonPropertyName("subject_types_supported")]
         public string[] SubjectTypesSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported signing algorithms.
+        /// </summary>
         [JsonPropertyName("id_token_signing_alg_values_supported")]
         public string[] IdTokenSigningAlgValuesSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported authentication methods on the token endpoint.
+        /// </summary>
         [JsonPropertyName("token_endpoint_auth_methods_supported")]
         public string[] TokenEndpointAuthMethodsSupported { get; set; }
 
+        /// <summary>
+        /// Array of supported code challenge methods.
+        /// </summary>
         [JsonPropertyName("code_challenge_methods_supported")]
         public string[] CodeChallengeMethodsSupported { get; set; }
     }
