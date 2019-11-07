44e181a9df8ce2051e5438da2e1b9604864809d5
Mats Gimming Myhre <mats.myhre@capgemini.com>
2019-11-07 19:01:00 +0100

    Hide table when deploy history is empty.

--- src/react-apps/applications/service-development/src/features/appPublish/components/appDeploymentComponent.tsx
+++ src/react-apps/applications/service-development/src/features/appPublish/components/appDeploymentComponent.tsx
@@ -492,62 +492,78 @@ const AppDeploymentComponent = (props: IReceiptContainerProps) => {
           </Grid>
 
           <Grid item={true} className={classes.deploymentListGrid}>
-            <Typography>
-            {getParsedLanguageFromKey('app_deploy_table.deployed_version_history', language, [envName.toUpperCase()])}
-            </Typography>
-            <div className={classes.tableWrapper}>
-              <Table
-                stickyHeader={true}
-                className={classes.table}
-                size='small'
-                aria-label={getParsedLanguageFromKey('app_deploy_table.deploy_table_aria', language, [envName], true)}
-              >
-                <TableHead>
-                  <TableRow className={classes.tableRow}>
-                    <TableCell className={classes.colorBlack}>
-                      <Typography>
-                        {getParsedLanguageFromKey('app_deploy_table.version_col', language)}
-                      </Typography>
-                    </TableCell>
-                    <TableCell className={classes.colorBlack}>
-                      <Typography>
-                        {getParsedLanguageFromKey('app_deploy_table.available_version_col', language)}
-                      </Typography>
-                    </TableCell>
-                    <Hidden mdDown={true}>
-                      <TableCell className={classes.colorBlack}>
-                        <Typography>
-                          {getParsedLanguageFromKey('app_deploy_table.deployed_by_col', language)}
-                        </Typography>
-                      </TableCell>
-                    </Hidden>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {succeededDeployHistory.map((deploy: any, index: number) => (
-                    <TableRow key={index} className={classes.tableRow}>
-                      <TableCell component='th' scope='row'>
-                        <Typography>
-                          {deploy.tagName}
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography>
-                          {moment(new Date(deploy.build.finished)).format('DD.MM.YY [kl.] HH:mm')}
-                        </Typography>
-                      </TableCell>
-                      <Hidden mdDown={true}>
-                        <TableCell>
+            {succeededDeployHistory.length === 0 ? (
+              <Typography>
+                {getParsedLanguageFromKey(
+                  'app_deploy_table.deployed_version_history_empty',
+                   language,
+                   [envName.toUpperCase()],
+                )}
+              </Typography>
+            ) : (
+              <>
+                <Typography>
+                  {getParsedLanguageFromKey(
+                    'app_deploy_table.deployed_version_history',
+                    language,
+                    [envName.toUpperCase()],
+                  )}
+                </Typography>
+                <div className={classes.tableWrapper}>
+                  <Table
+                    stickyHeader={true}
+                    className={classes.table}
+                    size='small'
+                    aria-label={getParsedLanguageFromKey('app_deploy_table.deploy_table_aria', language, [envName], true)}
+                  >
+                    <TableHead>
+                      <TableRow className={classes.tableRow}>
+                        <TableCell className={classes.colorBlack}>
                           <Typography>
-                            {deploy.createdBy}
+                            {getParsedLanguageFromKey('app_deploy_table.version_col', language)}
                           </Typography>
                         </TableCell>
-                      </Hidden>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
+                        <TableCell className={classes.colorBlack}>
+                          <Typography>
+                            {getParsedLanguageFromKey('app_deploy_table.available_version_col', language)}
+                          </Typography>
+                        </TableCell>
+                        <Hidden mdDown={true}>
+                          <TableCell className={classes.colorBlack}>
+                            <Typography>
+                              {getParsedLanguageFromKey('app_deploy_table.deployed_by_col', language)}
+                            </Typography>
+                          </TableCell>
+                        </Hidden>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {succeededDeployHistory.map((deploy: any, index: number) => (
+                        <TableRow key={index} className={classes.tableRow}>
+                          <TableCell component='th' scope='row'>
+                            <Typography>
+                              {deploy.tagName}
+                            </Typography>
+                          </TableCell>
+                          <TableCell>
+                            <Typography>
+                              {moment(new Date(deploy.build.finished)).format('DD.MM.YY [kl.] HH:mm')}
+                            </Typography>
+                          </TableCell>
+                          <Hidden mdDown={true}>
+                            <TableCell>
+                              <Typography>
+                                {deploy.createdBy}
+                              </Typography>
+                            </TableCell>
+                          </Hidden>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </>
+            )}
           </Grid>
 
         </Grid>
