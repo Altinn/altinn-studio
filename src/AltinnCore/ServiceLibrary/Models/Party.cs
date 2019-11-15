56f997eedb0695cbdecf30977f8acc381ff27774
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2019-11-15 12:32:06 +0100

    Refactor

--- src/AltinnCore/ServiceLibrary/Models/Party.cs
+++ src/AltinnCore/ServiceLibrary/Models/Party.cs
@@ -12,67 +12,56 @@ namespace AltinnCore.ServiceLibrary.Models
         /// <summary>
         /// Gets or sets the ID of the party
         /// </summary>
-        [JsonProperty(PropertyName = "partyId")]
         public int PartyId { get; set; }
 
         /// <summary>
         /// Gets or sets the type of party
         /// </summary>
-        [JsonProperty(PropertyName = "partyTypeName")]
         public PartyType PartyTypeName { get; set; }
 
         /// <summary>
         /// Gets the parties org number
         /// </summary>
-        [JsonProperty(PropertyName = "orgNumber")]
         public string OrgNumber { get; set; }
 
         /// <summary>
         /// Gets the parties ssn
         /// </summary>
-        [JsonProperty(PropertyName = "SSN")]
         public string SSN { get; set; }
 
         /// <summary>
         /// Gets or sets the UnitType
         /// </summary>
-        [JsonProperty(PropertyName = "unitType")]
         public string UnitType { get; set; }
 
         /// <summary>
         /// Gets or sets the Name
         /// </summary>
-        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the IsDeleted
         /// </summary>
-        [JsonProperty(PropertyName = "isDeleted")]
         public bool IsDeleted { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether if the reportee in the list is only there for showing the hierarchy (a parent unit with no access)
         /// </summary>
-        [JsonProperty(PropertyName = "onlyHierarchyElementWithNoAccess")]
         public bool OnlyHierarchyElementWithNoAccess { get; set; }
 
         /// <summary>
         /// Gets or sets the person details for this party (will only be set if the party type is Person)
         /// </summary>
-        [JsonProperty(PropertyName = "person")]
         public Person Person { get; set; }
 
         /// <summary>
         /// Gets or sets the organization details for this party (will only be set if the party type is Organization)
         /// </summary>
-        [JsonProperty(PropertyName = "organization")]
         public Organization Organization { get; set; }
 
         /// <summary>
         /// Gets or sets the value of ChildParties
         /// </summary>
-        [JsonProperty(PropertyName = "childParties")]
         public List<Party> ChildParties { get; set; }
     }
 }
