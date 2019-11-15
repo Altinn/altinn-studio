6741cedc19547ffc441f76910221434bdc462f39
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2019-11-15 16:42:39 +0100

    Fixed enum typing

--- src/react-apps/applications/runtime/src/utils/validation.ts
+++ src/react-apps/applications/runtime/src/utils/validation.ts
@@ -1,7 +1,7 @@
 import { getLanguageFromKey, getParsedLanguageFromKey } from '../../../shared/src/utils/language';
 import { IFormData } from '../features/form/data/reducer';
 import { ILayout, ILayoutComponent } from '../features/form/layout/';
-import { IValidationIssue, Severity} from '../types';
+import { IValidationIssue, Severity } from '../types';
 import { IComponentValidations, IDataModelFieldElement, IValidations } from '../types/global';
 import { getKeyWithoutIndex } from './databindings';
 
