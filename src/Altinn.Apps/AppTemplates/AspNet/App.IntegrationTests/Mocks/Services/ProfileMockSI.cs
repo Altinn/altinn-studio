82e95836df82137d4073474224d65af01d90d349
Rune T. Larsen <rune@hjemmekino.no>
2019-11-18 22:18:01 +0100

    Profile unit test

---
+++ src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Mocks/Services/ProfileMockSI.cs
@@ -0,0 +1,17 @@
+using Altinn.App.Services.Interface;
+using Altinn.App.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.IntegrationTestsRef.Mocks.Services
+{
+    public class ProfileMockSI : IProfile
+    {
+        public Task<UserProfile> GetUserProfile(int userId)
+        {
+            return Task.FromResult(new UserProfile() { UserId = userId });
+        }
+    }
+}
