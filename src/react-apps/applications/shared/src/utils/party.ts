600bdf7b84aa679cae8444deedebfabd052d3f97
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2019-11-21 11:24:53 +0100

    Now uses name instead of person.name/org.name

--- src/react-apps/applications/shared/src/utils/party.ts
+++ src/react-apps/applications/shared/src/utils/party.ts
@@ -4,15 +4,5 @@ export function renderPartyName(party: IParty) {
   if (!party) {
     return null;
   }
-  if (party.person) {
-    const user = party.person.firstName.concat(
-      ' ',
-      (party.person.middleName !== null ?
-        (party.person.middleName.concat(' ')) : ''),
-      party.person.lastName);
-    return user.toUpperCase();
-  } else if (party.organisation) {
-    return party.organisation.name.toUpperCase();
-  }
-  return null;
+  return party.name;
 }
