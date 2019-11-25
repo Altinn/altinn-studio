f92374930cbf994280db3ed01d011f0068fde90b
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2019-11-25 13:42:50 +0100

    fixed build errors (#3136)
    * fixed build errors
    
    * more errors

--- src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Mocks/Services/PepWithPDPAuthorizationMockSI.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Mocks/Services/PepWithPDPAuthorizationMockSI.cs
@@ -312,10 +312,5 @@ namespace App.IntegrationTests.Mocks.Services
             string unitTestFolder = Path.GetDirectoryName(new Uri(typeof(PepWithPDPAuthorizationMockSI).Assembly.CodeBase).LocalPath);
             return Path.Combine(unitTestFolder, @"..\..\..\Data\apps\" + org + @"\" + app + @"\config\authorization\");
         }
-
-        public Task<bool> GetDecisionForUnvalidateRequest(XacmlJsonRequest xacmlJsonRequest, ClaimsPrincipal user)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
