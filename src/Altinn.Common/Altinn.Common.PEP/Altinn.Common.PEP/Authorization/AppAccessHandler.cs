9ebac92c75909d7390554405f90b5f2a4067d3be
Kritsi <kristine.skje@live.no>
2019-11-26 09:04:01 +0100

    Feature/3027 custom pep instansiation (#3151)
    * new pep nuget version 0.0.7-alpha. Cleaned code, added more unittest
    
    * romoved code that crashed the project appref
    
    * updated pep nuget version and test fails
    
    * added Instantiate to startup and processController, called pdp and tests fails
    
    * new nuget 0.0.8-alpha, added disablepep in pepappsi
    
    * upgraded to 0.0.8-alpha pep nuget
    
    * updated pep nuget version in appref and set disablepep to true

--- src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/AppAccessHandler.cs
+++ src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Authorization/AppAccessHandler.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Altinn.Authorization.ABAC.Xacml;
 using Altinn.Authorization.ABAC.Xacml.JsonProfile;
 using Altinn.Common.PEP.Configuration;
 using Altinn.Common.PEP.Helpers;
@@ -12,7 +8,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
-using static Altinn.Authorization.ABAC.Constants.XacmlConstants;
 
 namespace Altinn.Common.PEP.Authorization
 {
@@ -27,15 +22,6 @@ namespace Altinn.Common.PEP.Authorization
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IPDP _pdp;
         private readonly GeneralSettings _generalSettings;
-        private const string XacmlResourceInstanceId = "urn:altinn:instance-id";
-        private const string XacmlResourceOrgId = "urn:altinn:org";
-        private const string XacmlResourceAppId = "urn:altinn:app";
-        private const string ParamInstanceOwnerId = "instanceOwnerId";
-        private const string ParamInstanceGuid = "instanceGuid";
-        private const string ParamApp = "app";
-        private const string ParamOrg = "org";
-        private const string defaultIssuer = "Altinn";
-        private const string defaultType = "string";
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AppAccessHandler"/> class.
@@ -71,114 +57,18 @@ namespace Altinn.Common.PEP.Authorization
             XacmlJsonRequest request = DecisionHelper.CreateXacmlJsonRequest(context, requirement, _httpContextAccessor.HttpContext.GetRouteData());
             XacmlJsonResponse response = await _pdp.GetDecisionForRequest(request);
 
-            if(response == null || response.Response == null)
+            if (response == null)
             {
-                context.Fail();
-                return;
+                throw new ArgumentNullException("response");
             }
 
-            List<XacmlJsonResult> results = response.Response;
-
-            // We request one thing and then only want one result
-            if (results.Count != 1)
+            if (!DecisionHelper.ValidateResponse(response.Response, context.User))
             {
                 context.Fail();
-            }
-
-            // Checks that the result is nothing else than "permit"
-            if (!results.First().Decision.Equals(XacmlContextDecision.Permit.ToString()))
-            {
-                context.Fail();
-            }
-
-            // Checks if the result contains obligation
-            if (results.First().Obligations != null && results.Count > 0)
-            {
-                List<XacmlJsonObligationOrAdvice> obligationList = results.First().Obligations;
-                XacmlJsonAttributeAssignment attributeMinLvAuth = obligationList.Select(a => a.AttributeAssignment.Find(a => a.Category.Equals("urn:altinn:minimum-authenticationlevel"))).FirstOrDefault();
-
-                // Checks if the obligation contains a minimum authentication level attribute
-                if (attributeMinLvAuth != null)
-                {
-                    string minAuthenticationLevel = attributeMinLvAuth.Value;
-                    string usersAuthenticationLevel = context.User.Claims.FirstOrDefault(c => c.Type.Equals("urn:altinn:minimum-authenticationlevel")).Value;
-
-                    // Checks that the user meets the minimum authentication level
-                    if (Convert.ToInt32(usersAuthenticationLevel) < Convert.ToInt32(minAuthenticationLevel))
-                    {
-                        context.Fail();
-                    }
-                }
             }
 
             context.Succeed(requirement);
             await Task.CompletedTask;
         }
-
-        /*private XacmlJsonRequest CreateXacmlJsonRequest(AuthorizationHandlerContext context, AppAccessRequirement requirement)
-        {
-            XacmlJsonRequest request = new XacmlJsonRequest();
-
-            request.AccessSubject = new List<XacmlJsonCategory>();
-            request.Action = new List<XacmlJsonCategory>();
-            request.Resource = new List<XacmlJsonCategory>();
-
-            request.AccessSubject.Add(CreateSubjectCategory(context.User.Claims));
-            request.Action.Add(CreateActionCategory(requirement.ActionType));
-            request.Resource.Add(CreateResourceCategory(_httpContextAccessor.HttpContext.GetRouteData()));
-
-            return request;
-        }
-
-        private XacmlJsonCategory CreateSubjectCategory(IEnumerable<Claim> claims)
-        {
-            XacmlJsonCategory subjectAttributes = new XacmlJsonCategory();
-            subjectAttributes.Attribute = new List<XacmlJsonAttribute>();
-
-            // Mapping all claims on user to attributes
-            foreach (Claim claim in claims)
-            {
-                subjectAttributes.Attribute.Add(CreateXacmlJsonAttribute(claim.Type, claim.Value, claim.ValueType, claim.Issuer));
-            }
-
-            return subjectAttributes;
-        }
-
-        private XacmlJsonCategory CreateActionCategory(string actionType)
-        {
-            XacmlJsonCategory actionAttributes = new XacmlJsonCategory();
-            actionAttributes.Attribute = new List<XacmlJsonAttribute>();
-            actionAttributes.Attribute.Add(CreateXacmlJsonAttribute(MatchAttributeIdentifiers.ActionId, actionType, defaultType, defaultIssuer));
-            return actionAttributes;
-        }
-
-        private XacmlJsonCategory CreateResourceCategory(RouteData routeData)
-        {
-            XacmlJsonCategory resourceAttributes = new XacmlJsonCategory();
-            resourceAttributes.Attribute = new List<XacmlJsonAttribute>();
-
-            string instanceGuid = routeData.Values[ParamInstanceGuid] as string;
-            string app = routeData.Values[ParamApp] as string;
-            string org = routeData.Values[ParamOrg] as string;
-            string instanceOwnerId = routeData.Values[ParamInstanceOwnerId] as string;
-
-            resourceAttributes.Attribute.Add(CreateXacmlJsonAttribute(XacmlResourceInstanceId, instanceOwnerId + "/" + instanceGuid, defaultType, defaultIssuer));
-            resourceAttributes.Attribute.Add(CreateXacmlJsonAttribute(XacmlResourceOrgId, org, defaultType, defaultIssuer));
-            resourceAttributes.Attribute.Add(CreateXacmlJsonAttribute(XacmlResourceAppId, app, defaultType, defaultIssuer));
-
-            return resourceAttributes;
-        }
-
-        private XacmlJsonAttribute CreateXacmlJsonAttribute(string attributeId, string value, string dataType, string issuer)
-        {
-            XacmlJsonAttribute xacmlJsonAttribute = new XacmlJsonAttribute();
-
-            xacmlJsonAttribute.AttributeId = attributeId;
-            xacmlJsonAttribute.Value = value;
-            xacmlJsonAttribute.DataType = dataType;
-            xacmlJsonAttribute.Issuer = issuer;
-
-            return xacmlJsonAttribute;
-        }*/
     }
 }
--- src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Configuration/GeneralSettings.cs
+++ src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Configuration/GeneralSettings.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Altinn.Common.PEP.Configuration
 {
     /// <summary>
