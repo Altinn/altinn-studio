a2dbb7cba2891413f80e4732a50acb560003c480
Terje Holene <terje.holene@gmail.com>
2019-12-05 14:55:27 +0100

    Update platform authentication to use version 1.0.6-alpha of JWTCookieAuthentication (#3268)
    Updated version of JWTCookieAuthentication has "caching" of certificate from key vault with refresh every 30 minutes.

---
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Altinn.Platform.Authentication.IntegrationTests/Fakes/JwtCookiePostConfigureOptionsStub.cs
@@ -0,0 +1,57 @@
+using System;
+
+using AltinnCore.Authentication.JwtCookie;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Altinn.Platform.Authentication.IntegrationTests.Fakes
+{
+    /// <summary>
+    /// Represents a stub for the <see cref="JwtCookiePostConfigureOptions"/> class to be used in integration tests.
+    /// </summary>
+    public class JwtCookiePostConfigureOptionsStub : IPostConfigureOptions<JwtCookieOptions>
+    {
+        /// <inheritdoc />
+        public void PostConfigure(string name, JwtCookieOptions options)
+        {
+            if (string.IsNullOrEmpty(options.Cookie.Name))
+            {
+                options.Cookie.Name = JwtCookieDefaults.CookiePrefix + name;
+            }
+
+            if (options.CookieManager == null)
+            {
+                options.CookieManager = new ChunkingCookieManager();
+            }
+
+            if (!options.LoginPath.HasValue)
+            {
+                options.LoginPath = JwtCookieDefaults.LoginPath;
+            }
+
+            if (!options.LogoutPath.HasValue)
+            {
+                options.LogoutPath = JwtCookieDefaults.LogoutPath;
+            }
+
+            if (!options.AccessDeniedPath.HasValue)
+            {
+                options.AccessDeniedPath = JwtCookieDefaults.AccessDeniedPath;
+            }
+
+            if (!options.MetadataAddress.EndsWith("/", StringComparison.Ordinal))
+            {
+                options.MetadataAddress += "/";
+            }
+
+            options.MetadataAddress += ".well-known/openid-configuration";
+            options.ConfigurationManager = new ConfigurationManagerStub(
+                options.MetadataAddress,
+                new OpenIdConnectConfigurationRetriever(),
+                new HttpDocumentRetriever());
+        }
+    }
+}
