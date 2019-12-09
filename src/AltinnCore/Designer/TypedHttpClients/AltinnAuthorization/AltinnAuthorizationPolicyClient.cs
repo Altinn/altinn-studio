840b43bd6ebbbce7276e8d93e9f2e9334899b076
DanRJ <daniel.rustadj@gmail.com>
2019-12-09 09:39:33 +0100

    Feature/2547 jwt authentication (#3277)
    * Expanded jwt authentication handler and add authentication to Platform.Storage
    
    * Remove logg
    
    * Downgraded to an actual package in nuget
    
    * Move runtime cookie name to appsettings
    
    * Change appsettings openid well known endpoint
    
    * Update JWTCookieAuthentication nuget package
    
    * Remove Authentication from Storage while we figure out how to use it
    
    * Add jwt from cert on requests to platform
    
    * Add maskinporten token auth, rename all typed http clients from service to client, change startup to add secrets after adding appsettings if env is development
    
    * Put certificate name in appsettings
    
    * Delete bunch of unused settings
    
    * Move token retriever to delegating handler and set altinn authentication token retriever to use dynamic host
    
    * Up nuget package JWTCookieAuthentication from 7 to 8, put back authentication
    
    * Change from ReadAsStringAsync to ReadAsync<string> because it does not add "\" to the string content
    
    * Add token validation parameters
    
    * Remove unused class
    
    * Remove uneccessary using statement
    
    * Update to JWTCookieAuth 9
    
    * Move Maskinporten related variables out to appsettings
    
    * Fix stylecop
    
    * Fix dotnet build errors in pipeline

---
+++ src/AltinnCore/Designer/TypedHttpClients/AltinnAuthorization/AltinnAuthorizationPolicyClient.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AltinnCore.Common.Configuration;
+using AltinnCore.Designer.Services.Models;
+using Microsoft.Extensions.Options;
+
+namespace AltinnCore.Designer.TypedHttpClients.AltinnAuthorization
+{
+    /// <summary>
+    /// AltinnAuthorizationPolicyClient
+    /// </summary>
+    public class AltinnAuthorizationPolicyClient : IAltinnAuthorizationPolicyClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly PlatformSettings _platformSettings;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpClient">HttpClient</param>
+        /// <param name="options">OptionsMonitor of type PlatformSettings</param>
+        public AltinnAuthorizationPolicyClient(
+            HttpClient httpClient,
+            IOptionsMonitor<PlatformSettings> options)
+        {
+            _httpClient = httpClient;
+            _platformSettings = options.CurrentValue;
+        }
+
+        /// <inheritdoc />
+        public async Task SavePolicy(string org, string app, string policyFile, EnvironmentModel environmentModel)
+        {
+            string uriString = $"https://{environmentModel.PlatformPrefix}.{environmentModel.Hostname}/{_platformSettings.ApiAuthorizationPolicyUri}";
+            Uri uri = new Uri($"{uriString}?org={org}&app={app}");
+
+            /*
+             * Have to create a HttpRequestMessage instead of using helper extension methods like _httpClient.PostAsync(...)
+             * because the base address can change on each request and after HttpClient gets initial base address,
+             * it is not advised (and not allowed) to change base address.
+             */
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uri)
+            {
+                Content = new StringContent(policyFile, Encoding.UTF8, "application/xml"),
+            };
+            await _httpClient.SendAsync(request);
+        }
+    }
+}
