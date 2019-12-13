5cfe41a1c1209f06e17581899fb0049861056bd9
Terje Holene <terje.holene@gmail.com>
2019-12-13 12:26:27 +0100

    Consolidate key vault logic across controllers in authentication (#3377)

--- src/Altinn.Platform/Altinn.Platform.Authentication/Altinn.Platform.Authentication.IntegrationTests/Fakes/JwtSigningCertificateProviderStub.cs
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Altinn.Platform.Authentication.IntegrationTests/Fakes/JwtSigningCertificateProviderStub.cs
@@ -1,36 +1,39 @@
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 using Altinn.Platform.Authentication.Services;
 using AltinnCore.Authentication.Constants;
+using AltinnCore.Authentication.Utils;
 
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Altinn.Platform.Authentication.IntegrationTests.Fakes
 {
     /// <summary>
     /// Represents a fake version of <see cref="SigningCredentialsProvider"/> to be used in testing.
     /// </summary>
-    public class SigningCredentialsProviderStub : ISigningCredentialsProvider
+    public class JwtSigningCertificateProviderStub : IJwtSigningCertificateProvider
     {
         private readonly CertificateSettings _certificateSettings;
 
         /// <summary>
-        /// Initialize a new instance of <see cref="SigningCredentialsProviderStub"/> with settings for accessing file system.
+        /// Initialize a new instance of <see cref="JwtSigningCertificateProviderStub"/> with settings for accessing file system.
         /// </summary>
         /// <param name="certificateSettings">Settings required to access a certificate stored on a file system.</param>
-        public SigningCredentialsProviderStub(IOptions<CertificateSettings> certificateSettings)
+        public JwtSigningCertificateProviderStub(IOptions<CertificateSettings> certificateSettings)
         {
             _certificateSettings = certificateSettings.Value;
         }
 
         /// <inheritdoc />
-        public async Task<SigningCredentials> GetSigningCredentials()
+        public async Task<List<X509Certificate2>> GetCertificates()
         {
             X509Certificate2 cert = new X509Certificate2(_certificateSettings.CertificatePath, _certificateSettings.CertificatePwd);
 
-            return await Task.FromResult(new X509SigningCredentials(cert, SecurityAlgorithms.RsaSha256));
+            List<X509Certificate2> certificates = new List<X509Certificate2> { cert };
+
+            return await Task.FromResult(certificates);
         }
     }
 }
