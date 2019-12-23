cb038006f4c99818fee5b958ee331cceac728ad3
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2019-12-23 11:34:08 +0100

    Updated jwt auth

---
+++ src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Configuration/GeneralSettings.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altinn.Platform.Receipt.Configuration
+{
+    /// <summary>
+    /// Settings for accessing bridge functionality
+    /// </summary>
+    public class GeneralSettings
+    {
+        /// <summary>
+        /// Open Id Connect Well known endpoint
+        /// </summary>
+        public string OpenIdWellKnownEndpoint { get; set; }
+
+        /// <summary>
+        /// Hostname
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        /// Name of the cookie for runtime
+        /// </summary>
+        public string RuntimeCookieName { get; set; }
+    }
+}
