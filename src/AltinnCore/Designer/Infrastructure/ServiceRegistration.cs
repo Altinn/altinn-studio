4b22bd7e602cab4aaa4f027fc28d31b6dbd88f00
Rune T. Larsen <rune@hjemmekino.no>
2020-01-07 09:57:44 +0100

    Removed unused stuff

--- src/AltinnCore/Designer/Infrastructure/ServiceRegistration.cs
+++ src/AltinnCore/Designer/Infrastructure/ServiceRegistration.cs
@@ -4,7 +4,6 @@ using AltinnCore.Common.Services.Implementation;
 using AltinnCore.Common.Services.Interfaces;
 using AltinnCore.Designer.Services;
 using AltinnCore.Designer.Services.Interfaces;
-using AltinnCore.ServiceLibrary.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,14 +22,6 @@ namespace AltinnCore.Designer.Infrastructure
         /// <param name="configuration">The configuration for the project</param>
         public static IServiceCollection RegisterServiceImplementations(this IServiceCollection services, IConfiguration configuration)
         {
-            // Adding services to Dependency Injection TODO: Make this environment specific
-            services.AddSingleton<IDSF, RegisterDSFStudioSI>();
-            services.AddSingleton<IER, RegisterERStudioSI>();
-            services.AddSingleton<IRegister, RegisterStudioSI>();
-            services.AddSingleton<IProfile, ProfileStudioSI>();
-
-            // services.AddSingleton<ICompilation, CompilationSI>();
-            // services.AddSingleton<IForm, FormStudioSI>();
             services.AddTransient<IRepository, RepositorySI>();
             services.AddTransient<ISourceControl, SourceControlSI>();
 
