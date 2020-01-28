e0c7698ec408a56febcef0c0e5ffa5581cc7ab2b
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2020-01-28 10:09:06 +0100

    Actually fixed data model name

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Interface/IPrefill.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Interface/IPrefill.cs
@@ -12,8 +12,8 @@ namespace Altinn.App.Services.Interface
         /// Prefills the data model based on the prefill json configuration file
         /// </summary>
         /// <param name="partyId">The partyId of the instance owner</param>
-        /// <param name="dataModel">The data model name</param>
-        /// <param name="dataModelName">The data model object</param>
+        /// <param name="dataModelName">The data model name</param>
+        /// <param name="dataModel">The data model object</param>
         /// <returns></returns>
         Task PrefillDataModel(string partyId, string dataModelName, object dataModel);
     }
