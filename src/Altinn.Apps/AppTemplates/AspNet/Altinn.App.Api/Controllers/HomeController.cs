dbe4c7f5d907a1888f315ff19a1ec919eb123b01
nkylstad <nkylstad@gmail.com>
2020-02-03 14:22:55 +0100

    Added authentication check to app home controller

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Api/Controllers/HomeController.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Api/Controllers/HomeController.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Web;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Altinn.App.Services.Configuration;
 
 namespace Altinn.App.Api.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IAntiforgery _antiforgery;
+        private readonly PlatformSettings _platformSettings;
 
-        public HomeController(IAntiforgery antiforgery)
+        public HomeController(
+          IAntiforgery antiforgery,
+          IOptions<PlatformSettings> platformSettings
+        )
         {
             _antiforgery = antiforgery;
+            _platformSettings = platformSettings.Value;
         }
 
         [Route("{org}/{app}/")]
@@ -25,9 +33,18 @@ namespace Altinn.App.Api.Controllers
                 HttpOnly = false // Make this cookie readable by Javascript.
             });
 
-            ViewBag.org = org;
-            ViewBag.app = app;
-            return PartialView("Index");
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.org = org;
+                ViewBag.app = app;
+                return PartialView("Index");
+            }
+            else
+            {
+                string goToUrl = HttpUtility.UrlEncode($"{Request.Host.ToUriComponent()}/{org}/{app}");
+                string redirectUrl = $"{_platformSettings.ApiAuthenticationEndpoint}authentication?goto={goToUrl}";
+                return Redirect(redirectUrl);
+            }
         }
     }
 }
