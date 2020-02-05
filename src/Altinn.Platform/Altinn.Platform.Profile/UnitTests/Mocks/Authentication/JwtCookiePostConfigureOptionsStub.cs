056789dfe0c2bbc3a8467d296adfee668cff7629
Rune T. Larsen <rune@hjemmekino.no>
2020-02-05 16:00:56 +0100

    Added unit tests

---
+++ src/Altinn.Platform/Altinn.Platform.Profile/UnitTests/Mocks/Authentication/JwtCookiePostConfigureOptionsStub.cs
@@ -0,0 +1,45 @@
+using System;
+
+using AltinnCore.Authentication.JwtCookie;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Altinn.Platform.Profile.UnitTests.Mocks.Authentication
+{
+    /// <summary>
+    /// Represents a stub for the <see cref="JwtCookiePostConfigureOptions"/> class to be used in integration tests.
+    /// </summary>
+    public class JwtCookiePostConfigureOptionsStub : IPostConfigureOptions<JwtCookieOptions>
+    {
+        /// <inheritdoc />
+        public void PostConfigure(string name, JwtCookieOptions options)
+        {
+            if (string.IsNullOrEmpty(options.JwtCookieName))
+            {
+                options.JwtCookieName = JwtCookieDefaults.CookiePrefix + name;
+            }
+
+            if (options.CookieManager == null)
+            {
+                options.CookieManager = new ChunkingCookieManager();
+            }
+
+            if (!string.IsNullOrEmpty(options.MetadataAddress))
+            {
+                if (!options.MetadataAddress.EndsWith("/", StringComparison.Ordinal))
+                {
+                    options.MetadataAddress += "/";
+                }
+            }
+
+            options.MetadataAddress += ".well-known/openid-configuration";
+            options.ConfigurationManager = new ConfigurationManagerStub(
+                options.MetadataAddress,
+                new OpenIdConnectConfigurationRetriever(),
+                new HttpDocumentRetriever());
+        }
+    }
+}
