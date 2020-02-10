788e44abea2c792d1c2e64885db04832e73936a6
nkylstad <nkylstad@gmail.com>
2020-02-10 16:00:47 +0100

    Add functionality for getting current party object

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/party/getParties/getPartiesActionTypes.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/party/getParties/getPartiesActionTypes.ts
@@ -4,3 +4,6 @@ const moduleName: string = 'PARTY';
 export const GET_PARTIES: string = `${moduleName}.GET_PARTIES`;
 export const GET_PARTIES_FULFILLED: string = `${moduleName}.GET_PARTIES_FULFILLED`;
 export const GET_PARTIES_REJECTED: string = `${moduleName}.GET_PARTIES_REJECTED`;
+
+// Get current party actions
+export const GET_CURRENT_PARTY: string = `${moduleName}.GET_CURRENT_PARTY`;
