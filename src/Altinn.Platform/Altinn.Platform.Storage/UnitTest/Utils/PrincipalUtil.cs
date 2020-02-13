b04cbf6e59f9f4afb574bb05e339ede65e330543
Terje Holene <terje.holene@gmail.com>
2020-02-13 14:22:12 +0100

    Moving tests using TestServer into UnitTest project

---
+++ src/Altinn.Platform/Altinn.Platform.Storage/UnitTest/Utils/PrincipalUtil.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Altinn.Platform.Storage.UnitTest.Mocks;
+using AltinnCore.Authentication.Constants;
+
+#pragma warning disable 1591
+#pragma warning disable SA1600
+
+namespace Altinn.Platform.Storage.UnitTest.Utils
+{
+    public static class PrincipalUtil
+    {
+        public static readonly string AltinnCoreClaimTypesOrg = "urn:altinn:org";
+        public static readonly string AltinnCoreClaimTypesOrgNumber = "urn:altinn:orgNumber";
+
+        public static string GetToken(int userId, int authenticationLevel = 2)
+        {
+            List<Claim> claims = new List<Claim>();
+            string issuer = "www.altinn.no";
+            claims.Add(new Claim(AltinnCoreClaimTypes.UserId, userId.ToString(), ClaimValueTypes.String, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.UserName, "UserOne", ClaimValueTypes.String, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.PartyID, userId.ToString(), ClaimValueTypes.Integer32, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticateMethod, "Mock", ClaimValueTypes.String, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticationLevel, authenticationLevel.ToString(), ClaimValueTypes.Integer32, issuer));
+
+            ClaimsIdentity identity = new ClaimsIdentity("mock");
+            identity.AddClaims(claims);
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+            string token = JwtTokenMock.GenerateToken(principal, new TimeSpan(1, 1, 1));
+
+            return token;
+        }
+
+        public static string GetOrgToken(string org, int orgNumber = 111111111, string scope = "altinn:appdeploy")
+        {
+            List<Claim> claims = new List<Claim>();
+            string issuer = "www.altinn.no";
+            claims.Add(new Claim(AltinnCoreClaimTypesOrg, org, ClaimValueTypes.String, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypesOrgNumber, orgNumber.ToString(), ClaimValueTypes.Integer32, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticateMethod, "Mock", ClaimValueTypes.String, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticationLevel, "3", ClaimValueTypes.Integer32, issuer));
+            claims.Add(new Claim("urn:altinn:scope", scope, ClaimValueTypes.String, "maskinporten"));
+
+            ClaimsIdentity identity = new ClaimsIdentity("mock-org");
+            identity.AddClaims(claims);
+            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+            string token = JwtTokenMock.GenerateToken(principal, new TimeSpan(1, 1, 1));
+
+            return token;
+        }
+    }
+}
