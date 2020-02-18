2511b6f96402c9612a3ebde07761c773ac3ff6c3
Rune T. Larsen <rune@hjemmekino.no>
2020-02-18 08:19:53 +0100

    Added sub claim for CSRF

--- src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Controllers/AuthenticationController.cs
+++ src/Altinn.Platform/Altinn.Platform.Authentication/Authentication/Controllers/AuthenticationController.cs
@@ -101,6 +101,7 @@ namespace Altinn.Platform.Authentication.Controllers
             {
                 List<Claim> claims = new List<Claim>();
                 string issuer = _generalSettings.PlatformEndpoint;
+                claims.Add(new Claim("sub", userAuthentication.UserID.ToString(), ClaimValueTypes.String, issuer));
                 claims.Add(new Claim(AltinnCoreClaimTypes.UserId, userAuthentication.UserID.ToString(), ClaimValueTypes.String, issuer));
                 claims.Add(new Claim(AltinnCoreClaimTypes.UserName, userAuthentication.Username, ClaimValueTypes.String, issuer));
                 claims.Add(new Claim(AltinnCoreClaimTypes.PartyID, userAuthentication.PartyID.ToString(), ClaimValueTypes.Integer32, issuer));
