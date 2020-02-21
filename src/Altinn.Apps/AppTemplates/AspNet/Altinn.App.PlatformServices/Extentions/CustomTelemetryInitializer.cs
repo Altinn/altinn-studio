57bdc12102d1cef29c1a7fe85e041bf364faa418
Nina Kylstad <nkylstad@gmail.com>
2020-02-21 11:42:33 +0100

    Add custom telemetry initializer for app insights (#3737)
    * Add custom telemetry initializer for app insights
    
    * update appinsights package version

---
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Extentions/CustomTelemetryInitializer.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Altinn.App.PlatformServices.Extentions
+{
+    /// <summary>
+    /// Set up custom telemetry for Application Insights
+    /// </summary>
+    public class CustomTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                string roleName = Environment.GetEnvironmentVariable("AppSettings__AppInsightsRoleName");
+                telemetry.Context.Cloud.RoleName = roleName;
+            }
+        }
+    }
+}
