d102aac8101481900bcff65a69ad9cf3980e1a1a
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2020-03-02 11:37:56 +0100

    bug fix (#3778)

--- src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Services/StorageWrapper.cs
+++ src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Services/StorageWrapper.cs
@@ -17,7 +17,7 @@ namespace Altinn.Platform.Receipt.Services.Interfaces
     public class StorageWrapper : IStorage
     {
         private readonly HttpClient _client;
-        private readonly HttpContext _context;
+        private readonly IHttpContextAccessor _contextAccessor;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="StorageWrapper"/> class
@@ -25,13 +25,13 @@ namespace Altinn.Platform.Receipt.Services.Interfaces
         public StorageWrapper(IHttpClientAccessor httpClientAccessor, IHttpContextAccessor httpContextAccessor)
         {
             _client = httpClientAccessor.StorageClient;
-            _context = httpContextAccessor.HttpContext;
+            _contextAccessor = httpContextAccessor;
         }
 
         /// <inheritdoc/>
         public async Task<Instance> GetInstance(int instanceOwnerId, Guid instanceGuid)
         {
-            string token = JwtTokenUtil.GetTokenFromContext(_context, "AltinnStudioRuntime");
+            string token = JwtTokenUtil.GetTokenFromContext(_contextAccessor.HttpContext, "AltinnStudioRuntime");
             JwtTokenUtil.AddTokenToRequestHeader(_client, token);
 
             string url = $"instances/{instanceOwnerId}/{instanceGuid}";
