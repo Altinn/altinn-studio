b863e55c85a3ec013e7f5fe1e112d84da581d407
Rune T. Larsen <rune@hjemmekino.no>
2020-03-16 20:39:39 +0100

    Added more validation tests

--- src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/ApiTests/SiriusApiTest.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/ApiTests/SiriusApiTest.cs
@@ -59,7 +59,7 @@ namespace App.IntegrationTests
         /// </summary>
         /// <returns></returns>
         [Fact]
-        public async Task Instance_Post_WithNæringOgSkattemelding()
+        public async Task Instance_Post_WithNæringOgSkattemelding_ValidateOk()
         {
             string token = PrincipalUtil.GetToken(1337);
 
@@ -130,6 +130,86 @@ namespace App.IntegrationTests
             HttpResponseMessage postresponseskattemelding = await client.PostAsync("/tdd/sirius/instances/" + instance.Id + "/data/?datatype=skattemelding", streamContentNæring);
             DataElement dataElementSkattemelding = (DataElement)JsonConvert.DeserializeObject(postresponseskattemelding.Content.ReadAsStringAsync().Result, typeof(DataElement));
 
+            // Validate instance
+            string url = "/tdd/sirius/instances/" + instance.Id + "/validate";
+            HttpResponseMessage responseValidation = await client.GetAsync(url);
+            string responseContentValidation = responseValidation.Content.ReadAsStringAsync().Result;
+            List<ValidationIssue> messages = (List<ValidationIssue>)JsonConvert.DeserializeObject(responseContentValidation, typeof(List<ValidationIssue>));
+
+            Assert.Empty(messages);
+            TestDataUtil.DeletInstanceAndData("tdd", "sirius", 1337, new Guid(instance.Id.Split('/')[1]));
+        }
+
+
+        /// <summary>
+        /// This test verifies that you can instansiate sirius app and add næringsoppgave and skattemdling
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task Instance_Post_WithNæringOgSkattemelding_MissingSkattemelding()
+        {
+            string token = PrincipalUtil.GetToken(1337);
+
+            HttpClient client = SetupUtil.GetTestClient(_factory, "tdd", "sirius");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/tdd/sirius/instances?instanceOwnerPartyId=1337")
+            {
+            };
+
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage);
+
+            string responseContent = response.Content.ReadAsStringAsync().Result;
+            Instance instance = JsonConvert.DeserializeObject<Instance>(responseContent);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.NotNull(instance);
+            Assert.Equal("1337", instance.InstanceOwner.PartyId);
+
+
+            // Get Data from Instance
+            HttpRequestMessage httpRequestMessageGetData = new HttpRequestMessage(HttpMethod.Get, "/tdd/sirius/instances/" + instance.Id + "/data/" + instance.Data[0].Id);
+            {
+            };
+
+            HttpResponseMessage responseData = await client.SendAsync(httpRequestMessageGetData);
+            string responseContentData = responseData.Content.ReadAsStringAsync().Result;
+
+
+            Skjema siriusMainForm = (Skjema)JsonConvert.DeserializeObject(responseContentData, typeof(Skjema));
+
+            // Modify the prefilled form. This would need to be replaced with the real sirius form
+            siriusMainForm.Permisjonsopplysningergrp8822 = new Permisjonsopplysningergrp8822();
+            siriusMainForm.Permisjonsopplysningergrp8822.AnsattEierandel20EllerMerdatadef33294 = new AnsattEierandel20EllerMerdatadef33294() { value = "50" };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Skjema));
+            using MemoryStream stream = new MemoryStream();
+
+            serializer.Serialize(stream, siriusMainForm);
+            stream.Position = 0;
+            StreamContent streamContent = new StreamContent(stream);
+            streamContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/xml");
+
+            HttpResponseMessage putresponse = await client.PutAsync("/tdd/sirius/instances/" + instance.Id + "/data/" + instance.Data[0].Id, streamContent);
+
+            // Add Næringsoppgave.xml
+            string næringsppgave = File.ReadAllText("Data/Files/data-element.xml");
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(næringsppgave);
+            //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+            MemoryStream næringsoppgavestream = new MemoryStream(byteArray);
+
+            StreamContent streamContentNæring = new StreamContent(næringsoppgavestream);
+            streamContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/xml");
+
+            HttpResponseMessage postresponseNæring = await client.PostAsync("/tdd/sirius/instances/" + instance.Id + "/data/?datatype=næringsoppgave", streamContentNæring);
+            DataElement dataElementNæring = (DataElement)JsonConvert.DeserializeObject(postresponseNæring.Content.ReadAsStringAsync().Result, typeof(DataElement));
+
+            //Validate instance
+            string url = "/tdd/sirius/instances/" + instance.Id + "/validate";
+            HttpResponseMessage responseValidation = await client.GetAsync(url);
+            string responseContentValidation = responseValidation.Content.ReadAsStringAsync().Result;
+            List<ValidationIssue> messages = (List<ValidationIssue>)JsonConvert.DeserializeObject(responseContentValidation, typeof(List<ValidationIssue>));
+
+            Assert.Single(messages);
             TestDataUtil.DeletInstanceAndData("tdd", "sirius", 1337, new Guid(instance.Id.Split('/')[1]));
         }
     }
