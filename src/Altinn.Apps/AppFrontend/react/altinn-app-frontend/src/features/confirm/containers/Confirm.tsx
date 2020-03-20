49390b756f303b01daf1b983c089470a60ec9782
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2020-03-20 10:42:42 +0100

    bug/show-errors-on-confirmation (#3869)
    * Update state on validations
    
    * Always enabled, but set validations on submit

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/confirm/containers/Confirm.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/confirm/containers/Confirm.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 import {AltinnButton} from 'altinn-shared/components/AltinnButton';
 import {getLanguageFromKey} from 'altinn-shared/utils/language';
 import ProcessDispatcher from '../../../shared/resources/process/processDispatcher';
-import { IAltinnWindow } from '../../../types';
+import { IAltinnWindow, IRuntimeState } from '../../../types';
 import { get } from '../../../utils/networking';
 import { getValidationUrl } from '../../../utils/urlHelper';
+import FormValidationActions from '../../form/validation/validationActions';
+import { mapDataElementValidationToRedux } from '../../../utils/validation';
+import { useSelector } from 'react-redux';
 
 export interface IConfirmProps {
   language: any;
@@ -12,26 +15,26 @@ export interface IConfirmProps {
   instanceGuid: any;
 }
 
-// export function getValidationStatus
-
 export function Confirm(props: IConfirmProps) {
   const { instanceId } = window as Window as IAltinnWindow;
-  const [canConfirm, setCanConfirm] = React.useState<boolean>()
-  const onClickConfirm = () => {
-    ProcessDispatcher.completeProcess();
-  }
+  const layout = useSelector((state: IRuntimeState) => state.formLayout.layout);
+  const textResources = useSelector((state: IRuntimeState) => state.textResources.resources);
 
-  React.useEffect(() => {
+  const onClickConfirm = () => {
     get(getValidationUrl(instanceId)).then((data: any) => {
-      setCanConfirm(data.length === 0);
+      if (data.length > 0) {
+        const mappedValidations = mapDataElementValidationToRedux(data, layout, textResources);
+        FormValidationActions.updateValidations(mappedValidations);
+      } else {
+        ProcessDispatcher.completeProcess();
+      }
     });
-  }, []);
+  }
 
   return (
   <AltinnButton
     btnText={getLanguageFromKey('confirm.button_text', props.language)}
     onClickFunction={onClickConfirm}
-    disabled={!canConfirm}
   />
   );
 }
