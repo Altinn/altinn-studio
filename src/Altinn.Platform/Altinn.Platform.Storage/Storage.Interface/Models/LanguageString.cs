26bce2071747ed0da1fe239609c42c08a159f2b1
Terje Holene <terje.holene@gmail.com>
2020-03-25 11:19:04 +0100

    Adding serialization decorations where missing. (#3885)
    Adding serialization decorations where missing.
    Added AllowedContributers to datatype.
    
    Co-authored-by: Stephanie Buadu <acn-sbuad@ai-dev.no>

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage.Interface/Models/LanguageString.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage.Interface/Models/LanguageString.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 
+using Newtonsoft.Json;
+
 namespace Altinn.Platform.Storage.Interface.Models
 {
     /// <summary>
     /// Represents a dictionary collection of translated texts where the key is a language id and the value is the text.
     /// </summary>
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class LanguageString : Dictionary<string, string>
     { 
     }
