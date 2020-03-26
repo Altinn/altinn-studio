fc1586c2d675ca3c05933b2fe6245c61e659fc68
Hans Oluf Waaler <aas-hwaal@ai-dev.brreg.no>
2020-03-26 12:18:35 +0100

    Use fail after each step

--- src/test/K6/src/tests/app/e2erf0002.js
+++ src/test/K6/src/tests/app/e2erf0002.js
@@ -1,4 +1,4 @@
-import { check, sleep } from "k6";
+import { check, sleep, fail } from "k6";
 import {addErrorCount} from "../../errorcounter.js";
 import * as appInstances from "../../api/app/instances.js"
 import * as appData from "../../api/app/data.js"
@@ -15,83 +15,108 @@ export const options = {
     thresholds:{
         "errors": ["count<1"]
     },
-    vus: 20,
-     stages: [                
-        { duration: '1m', target: 10 },
-        { duration: '10m', target: 20 }
-      ]  
+    vus: 50,
+     stages: [
+        { duration: '10m', target: 50 }
+      ]
 };
 
 //Tests for App API: RF-0002
 export default function() {
     var userNumber = (__VU - 1) % users.length;
-    
+
     //console.log("VU:" + userNumber + "/U:" + users[userNumber].username + "/P:" + users[userNumber].password );
-    var aspxauthCookie = setUpData.authenticateUser(users[userNumber].username, users[userNumber].password);  
-    const runtimeToken = setUpData.getAltinnStudioRuntimeToken(aspxauthCookie);    
+    var aspxauthCookie = setUpData.authenticateUser(users[userNumber].username, users[userNumber].password);
+    const runtimeToken = setUpData.getAltinnStudioRuntimeToken(aspxauthCookie);
     //var data = setUpData.getUserData(runtimeToken);
     setUpData.clearCookies();
     //var orgPartyId = postPartieslookup(runtimeToken, "OrgNo", data["orgNumber"])
-    //data.orgPartyId = JSON.parse(orgPartyId.body).partyId;    
-    const partyId = users[userNumber].partyid;  
-    var instanceId = "";    
-    var dataId = "";  
+    //data.orgPartyId = JSON.parse(orgPartyId.body).partyId;
+    const partyId = users[userNumber].partyid;
+    var instanceId = "";
+    var dataId = "";
     sleep(1);
 
     //Test to create an instance with App api and validate the response
     instanceId = appInstances.postInstance(runtimeToken, partyId);
     var success = check(instanceId, {
-        "E2E App POST Create Instance status is 201:": (r) => r.status === 201        
-      });  
+        "E2E App POST Create Instance status is 201:": (r) => r.status === 201
+      });
     addErrorCount(success);
     sleep(1);
-    
+
     if (instanceId.status === 201){
         dataId = appData.findDataId(instanceId.body);
-        instanceId = platformInstances.findInstanceId(instanceId.body); 
+        instanceId = platformInstances.findInstanceId(instanceId.body);
 
         //Test to edit a form data in an instance with App APi and validate the response
         var res = appData.putDataById(runtimeToken, partyId, instanceId, dataId, "default", instanceFormDataXml);
         success = check(res, {
-            "E2E PUT Edit Data by Id status is 201:": (r) => r.status === 201        
-        });  
+            "E2E PUT Edit Data by Id status is 201:": (r) => r.status === 201
+        });
+
         addErrorCount(success);
-        sleep(1);    
+        if (!success) {
+          fail("E2E PUT Edit Data by Id status is not set 201: " + JSON.stringify(res));
+        }
+
+        sleep(1);
 
         //Test to get validate instance and verify that validation of instance is ok
         res = appInstances.getValidateInstance(runtimeToken, partyId, instanceId);
         success = check(res, {
-            "E2E App GET Validate Instance validation OK:": (r) => (JSON.parse(r.body)).length === 0     
-        });  
+            "E2E App GET Validate Instance validation OK:": (r) => r.body.length > 0 && (JSON.parse(r.body)).length === 0
+        });
+
         addErrorCount(success);
+
+        if (!success) {
+          fail("E2E App GET Validate Instance is not OK: " + JSON.stringify(res));
+          console.log("Should not print");
+        }
+
         sleep(1);
 
         //Test to get next process of an app instance again and verify response code  to be 200
         res = appProcess.getNextProcess(runtimeToken, partyId, instanceId);
         sleep(1);
 
+        if (res.status !== 200) {
+            fail("Unable to get element: " + JSON.stringify(res));
+        }
+
         var nextElement = (JSON.parse(res.body))[0];
 
         //Test to move the process of an app instance to the next process element and verify response code to be 200
         res = appProcess.putNextProcess(runtimeToken, partyId, instanceId, nextElement);
         success = check(res, {
-            "E2E App PUT Move process to Next element status is 200:": (r) => r.status === 200      
-        });  
+            "E2E App PUT Move process to Next element status is 200:": (r) => r.status === 200
+        });
         addErrorCount(success);
+
+        if (!success) {
+          fail("E2E App PUT Move process to Next element status is 200: " + JSON.stringify(res));
+        }
+
         sleep(1);
 
         //Test to call get instance details and verify the presence of archived date
-        res = appInstances.getInstanceById(runtimeToken, partyId, instanceId);    
+        res = appInstances.getInstanceById(runtimeToken, partyId, instanceId);
         success = check(res, {
-            "E2E App Instance is archived:": (r) => (JSON.parse(r.body)).status.archived != null
-        }); 
+            "E2E App Instance is archived:": (r) => r.body.length > 0 && (JSON.parse(r.body)).status.archived != null
+        });
+
+        if (!success) {
+          fail("E2E App Instance is not archived. " + JSON.stringify(res));
+        }
+
         sleep(1);
 
-        deleteSblInstance(runtimeToken, partyId, instanceId, "true");    
-        sleep(1); 
+        deleteSblInstance(runtimeToken, partyId, instanceId, "true");
+        sleep(1);
     }else{
         console.log(__VU + "VU:" + users[__VU - 1].username);
         console.log(__VU + "/" + JSON.stringify(instanceId.status));
         console.log(__VU + "/" + JSON.stringify(instanceId.body));
-    };   
+    };
 };
