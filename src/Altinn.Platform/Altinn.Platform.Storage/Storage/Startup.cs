a294beb10614960bd3060fa2f95a7faa4d5acf43
Rune T. Larsen <rune@hjemmekino.no>
2020-04-03 15:41:57 +0200

    Code smell

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Startup.cs
@@ -22,7 +22,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
