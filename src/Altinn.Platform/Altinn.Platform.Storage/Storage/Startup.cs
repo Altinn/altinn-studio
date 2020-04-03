4c3cd845b84d2bd08f2aa3dbad587cef09d82e02
Rune T. Larsen <rune@hjemmekino.no>
2020-04-03 15:22:46 +0200

    Removed authorization config that messed up authorization

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Startup.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Startup.cs
@@ -77,14 +77,7 @@ namespace Altinn.Platform.Storage
         {
             _logger.Information("Startup // ConfigureServices");
 
-            services.AddControllers(config =>
-            {
-                AuthorizationPolicy policy = new AuthorizationPolicyBuilder()
-                    .AddAuthenticationSchemes(JwtCookieDefaults.AuthenticationScheme)
-                    .RequireAuthenticatedUser()
-                    .Build();
-                config.Filters.Add(new AuthorizeFilter(policy));
-            }).AddNewtonsoftJson();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddHttpClient<AuthorizationApiClient>();
 
