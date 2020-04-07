bbad81af710e8b40f7f04c506be2903a224ab611
Rune Tømmerås Larsen <rune@hjemmekino.no>
2020-04-07 13:36:33 +0200

    Removed serilog and setup default logging (#3960)
    * Removed serilog and setup default logging
    
    * Code stinks

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using AltinnCore.Authentication.Constants;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
@@ -11,20 +10,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Core;
-using Serilog.Extensions.Logging;
 
 namespace Altinn.Platform.Authorization
 {
     /// <summary>
     /// This is the main method for running this asp.net core application
     /// </summary>
-    public static class Program
+    public class Program
     {
-        private static readonly Logger _logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateLogger();
+        private static ILogger _logger; 
+
+        /// <summary>
+        /// Default protected constructor
+        /// </summary>
+        protected Program()
+        {
+        }
 
         /// <summary>
         /// The main method
@@ -32,6 +33,19 @@ namespace Altinn.Platform.Authorization
         /// <param name="args">The Arguments</param>
         public static void Main(string[] args)
         {
+            // Setup logging for the web host creation
+            var logFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("Altinn.Platform.Authorization.Program", LogLevel.Debug)
+                    .AddConsole()
+                    .AddEventLog();
+            });
+
+            _logger = logFactory.CreateLogger<Program>();
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
@@ -47,23 +61,50 @@ namespace Altinn.Platform.Authorization
                 string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
 
                 string basePathCurrentDirectory = Directory.GetCurrentDirectory();
-                _logger.Information($"Current directory is: {basePathCurrentDirectory}");
+                _logger.LogInformation($"Current directory is: {basePathCurrentDirectory}");
 
                 LoadConfigurationSettings(config, basePath, args);
             })
-            .ConfigureLogging((hostingContext, logging) =>
+            .ConfigureLogging(builder =>
             {
-                logging.ClearProviders();
-                LoggerConfiguration loggerConfig = new LoggerConfiguration().WriteTo.Console().MinimumLevel.Warning();
+                // The default ASP.NET Core project templates call CreateDefaultBuilder, which adds the following logging providers:
+                // Console, Debug, EventSource
+                // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-3.1
 
+                // Clear log providers
+                builder.ClearProviders();
+
+                // Setup up application insight if ApplicationInsightsKey is available
                 if (!string.IsNullOrEmpty(Startup.ApplicationInsightsKey))
                 {
-                    loggerConfig.WriteTo.ApplicationInsights(new TelemetryConfiguration(Startup.ApplicationInsightsKey), TelemetryConverter.Traces);
+                    // Add application insights https://docs.microsoft.com/en-us/azure/azure-monitor/app/ilogger
+                    // Providing an instrumentation key here is required if you're using
+                    // standalone package Microsoft.Extensions.Logging.ApplicationInsights
+                    // or if you want to capture logs from early in the application startup 
+                    // pipeline from Startup.cs or Program.cs itself.
+                    builder.AddApplicationInsights(Startup.ApplicationInsightsKey);
+
+                    // Optional: Apply filters to control what logs are sent to Application Insights.
+                    // The following configures LogLevel Information or above to be sent to
+                    // Application Insights for all categories.
+                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Warning);
+
+                    // Adding the filter below to ensure logs of all severity from Program.cs
+                    // is sent to ApplicationInsights.
+                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>(typeof(Program).FullName, LogLevel.Trace);
+
+                    // Adding the filter below to ensure logs of all severity from Startup.cs
+                    // is sent to ApplicationInsights.
+                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>(typeof(Startup).FullName, LogLevel.Trace);
                 }
-
-                Serilog.ILogger logger = loggerConfig.CreateLogger();
-
-                logging.AddProvider(new SerilogLoggerProvider(logger));
+                else
+                {
+                    // If not application insight is available log to console
+                    builder.AddFilter("Microsoft", LogLevel.Warning);
+                    builder.AddFilter("System", LogLevel.Warning);
+                    builder.AddConsole();
+                    builder.AddEventLog();
+                }
             })
             .UseStartup<Startup>();
 
@@ -75,7 +116,7 @@ namespace Altinn.Platform.Authorization
         /// <param name="args">programs arguments</param>
         public static void LoadConfigurationSettings(IConfigurationBuilder config, string basePath, string[] args)
         {
-            _logger.Information($"Program // Loading Configuration from basePath={basePath}");
+            _logger.LogInformation($"Program // Loading Configuration from basePath={basePath}");
 
             config.SetBasePath(basePath);
             string configJsonFile1 = $"{basePath}/altinn-appsettings/altinn-dbsettings-secret.json";
@@ -86,10 +127,10 @@ namespace Altinn.Platform.Authorization
                 configJsonFile2 = "/app/appsettings.json";
             }
 
-            _logger.Information($"Loading configuration file: '{configJsonFile1}'");
+            _logger.LogInformation($"Loading configuration file: '{configJsonFile1}'");
             config.AddJsonFile(configJsonFile1, optional: true, reloadOnChange: true);
 
-            _logger.Information($"Loading configuration file2: '{configJsonFile2}'");
+            _logger.LogInformation($"Loading configuration file2: '{configJsonFile2}'");
             config.AddJsonFile(configJsonFile2, optional: false, reloadOnChange: true);
 
             config.AddEnvironmentVariables();
@@ -108,7 +149,7 @@ namespace Altinn.Platform.Authorization
                 !string.IsNullOrEmpty(keyVaultSettings.ClientSecret) &&
                 !string.IsNullOrEmpty(keyVaultSettings.SecretUri))
             {
-                _logger.Information("Program // Configure key vault client // App");
+                _logger.LogInformation("Program // Configure key vault client // App");
 
                 string connectionString = $"RunAs=App;AppId={keyVaultSettings.ClientId};" +
                                           $"TenantId={keyVaultSettings.TenantId};" +
@@ -128,7 +169,7 @@ namespace Altinn.Platform.Authorization
                 }
                 catch (Exception vaultException)
                 {
-                   _logger.Error($"Unable to read application insights key {vaultException}");
+                   _logger.LogError($"Unable to read application insights key {vaultException}");
                 }
             }
         }
