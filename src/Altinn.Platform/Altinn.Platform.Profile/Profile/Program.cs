2f2fd7a11cba8b8a1900a91bb8a03ca6ebfbbba2
Rune T. Larsen <rune@hjemmekino.no>
2020-04-08 13:15:28 +0200

    Sorted usings

--- src/Altinn.Platform/Altinn.Platform.Profile/Profile/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Profile/Profile/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using AltinnCore.Authentication.Constants;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
