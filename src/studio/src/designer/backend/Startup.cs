03f0da6bd30526688d1cf5bb47a78ba99ec99b17
Rune T. Larsen <rune@hjemmekino.no>
2020-04-14 21:41:17 +0200

    Removed serilog and restructured app insight setup

--- src/studio/src/designer/backend/Startup.cs
+++ src/studio/src/designer/backend/Startup.cs
@@ -32,6 +32,11 @@ namespace Altinn.Studio.Designer
         private readonly ILogger<Startup> _logger;
 
         /// <summary>
+        /// The application insights key.
+        /// </summary>
+        internal static string ApplicationInsightsKey { get; set; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="Startup"/> class
         /// </summary>
         /// <param name="configuration">The configuration for designer</param>
