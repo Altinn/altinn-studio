c73a6cbb76f291d887c68471e787bfb079b6e49f
Rune T. Larsen <rune@hjemmekino.no>
2020-04-15 21:14:34 +0200

    Code smell

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Altinn.Platform.Storage
