2943f2e8866fddcf75af8a63558fd72e5d71b615
Rune T. Larsen <rune@hjemmekino.no>
2020-04-15 20:36:39 +0200

    Code smells

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Program.cs
@@ -1,9 +1,6 @@
 using System;
 using System.IO;
-using System.Threading.Tasks;
 using AltinnCore.Authentication.Constants;
-
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
