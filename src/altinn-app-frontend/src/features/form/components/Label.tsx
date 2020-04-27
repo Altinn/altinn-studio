6d9c1e7b88e2e3507ebb6a2b97ff71748e480edb
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2020-04-27 17:15:14 +0200

    bug/3940-hide-optional-on-readonly (#4055)
    * Hide optional tag on readonly
    
    * Added version

--- src/altinn-app-frontend/src/features/form/components/Label.tsx
+++ src/altinn-app-frontend/src/features/form/components/Label.tsx
@@ -8,6 +8,7 @@ export interface IFormLabelProps {
   id: string;
   language: any;
   required: boolean;
+  readOnly: boolean;
   helpTextProps: any;
 }
 
@@ -39,7 +40,7 @@ export default function Label(props: IFormLabelProps) {
       <Grid item={true}>
         <label className='a-form-label title-label' htmlFor={props.id}>
           {props.labelText}
-          {props.required ? null :
+          {(props.required || props.readOnly) ? null :
             <span className='label-optional'>
               ({getLanguageFromKey('general.optional', props.language)})
             </span>
