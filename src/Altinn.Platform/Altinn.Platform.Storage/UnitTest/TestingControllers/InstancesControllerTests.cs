23afa138e1cf6d5188f7fae7e9da7ce6d787f0c7
Rune T. Larsen <rune@hjemmekino.no>
2020-04-28 18:52:38 +0200

    Updated tests

--- src/Altinn.Platform/Altinn.Platform.Storage/UnitTest/TestingControllers/InstancesControllerTests.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/UnitTest/TestingControllers/InstancesControllerTests.cs
@@ -62,12 +62,12 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
             public async void Get_UserHasTooLowAuthLv_ReturnsStatusForbidden()
             {
                 // Arrange
-                int instanceOwnerPartyId = 1;
-                string instanceGuid = "cbdb00b1-4134-490d-b02b-3e33f7d8da33";
+                int instanceOwnerPartyId = 1337;
+                string instanceGuid = "20475edd-dc38-4ae0-bd64-1b20643f506c";
                 string requestUri = $"{BasePath}/{instanceOwnerPartyId}/{instanceGuid}";
 
                 HttpClient client = GetTestClient(_instanceRepository.Object);
-                string token = PrincipalUtil.GetToken(1, 0);
+                string token = PrincipalUtil.GetToken(1337, 0);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                 // Act
@@ -105,11 +105,9 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
             [Fact]
             public async void Get_One_Twice_Ok()
             {
-                RequestTracker.Clear();
-
                 // Arrange
                 int instanceOwnerPartyId = 1337;
-                string instanceGuid = "46133fb5-a9f2-45d4-90b1-f6d93ad40713";
+                string instanceGuid = "377efa97-80ee-4cc6-8d48-09de12cc273d";
                 string requestUri = $"{BasePath}/{instanceOwnerPartyId}/{instanceGuid}";
 
                 HttpClient client = GetTestClient(_instanceRepository.Object);
@@ -117,33 +115,33 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                 // Act
+                RequestTracker.Clear();
                 HttpResponseMessage response = await client.GetAsync(requestUri);
                 HttpResponseMessage response2 = await client.GetAsync(requestUri);
 
                 // Assert
+                Assert.Equal(1, RequestTracker.GetRequestCount("GetDecisionForRequest"));
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 string responseContent = await response.Content.ReadAsStringAsync();
                 Instance instance = (Instance)JsonConvert.DeserializeObject(responseContent, typeof(Instance));
                 Assert.Equal("1337", instance.InstanceOwner.PartyId);
                 Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
-
-                Assert.Equal(1, RequestTracker.GetRequestCount("GetDecisionForRequest"));
             }
 
             /// <summary>
-            /// Test case: Response is deny. 
+            /// Test case: User tries to access element that he is not authorized for
             /// Expected: Returns status forbidden.
             /// </summary>
             [Fact]
             public async void Get_ReponseIsDeny_ReturnsStatusForbidden()
             {
                 // Arrange
-                int instanceOwnerPartyId = 1;
-                string instanceGuid = "cbdb00b1-4134-490d-b02b-3e33f7d8da33";
+                int instanceOwnerPartyId = 1337;
+                string instanceGuid = "23d6aa98-df3b-4982-8d8a-8fe67a53b828";
                 string requestUri = $"{BasePath}/{instanceOwnerPartyId}/{instanceGuid}";
 
                 HttpClient client = GetTestClient(_instanceRepository.Object);
-                string token = PrincipalUtil.GetToken(-1);
+                string token = PrincipalUtil.GetToken(1, 3);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                 // Act
@@ -211,19 +209,13 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
             public async void Delete_UserHasTooLowAuthLv_ReturnsStatusForbidden()
             {
                 // Arrange
-                string org = "tdd";
-                string app = "test-applikasjon-1";
-                int instanceOwnerId = 1000;
-                string instanceGuid = "1916cd18-3b8e-46f8-aeaf-4bc3397ddd08";
-                string json = File.ReadAllText($"data/instances/{org}/{app}/{instanceOwnerId}/{instanceGuid}.json");
-                Instance instance = JsonConvert.DeserializeObject<Instance>(json);
-                _instanceRepository.Setup(r => r.GetOne(It.IsAny<string>(), It.IsAny<int>()))
-                .ReturnsAsync(instance);
-
+                int instanceOwnerId = 1337;
+                string instanceGuid = "7e6cc8e2-6cd4-4ad4-9ce8-c37a767677b5";
+                 
                 string requestUri = $"{BasePath}/{instanceOwnerId}/{instanceGuid}";
 
                 HttpClient client = GetTestClient(_instanceRepository.Object);
-                string token = PrincipalUtil.GetToken(1, 0);
+                string token = PrincipalUtil.GetToken(1337, 0);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                 // Act
@@ -234,26 +226,20 @@ namespace Altinn.Platform.Storage.UnitTest.TestingControllers
             }
 
             /// <summary>
-            /// Test case: Response is deny. 
+            /// Test case: User tries to delete a element it is not authorized to delete
             /// Expected: Returns status forbidden.
             /// </summary>
             [Fact]
             public async void Delete_ReponseIsDeny_ReturnsStatusForbidden()
             {
                 // Arrange
-                string org = "tdd";
-                string app = "test-applikasjon-1";
-                int instanceOwnerId = 1000;
-                string instanceGuid = "1916cd18-3b8e-46f8-aeaf-4bc3397ddd08";
-                string json = File.ReadAllText($"data/instances/{org}/{app}/{instanceOwnerId}/{instanceGuid}.json");
-                Instance instance = JsonConvert.DeserializeObject<Instance>(json);
-                _instanceRepository.Setup(r => r.GetOne(It.IsAny<string>(), It.IsAny<int>()))
-                    .ReturnsAsync(instance);
+                int instanceOwnerId = 1337;
+                string instanceGuid = "7e6cc8e2-6cd4-4ad4-9ce8-c37a767677b5";
 
                 string requestUri = $"{BasePath}/{instanceOwnerId}/{instanceGuid}";
 
                 HttpClient client = GetTestClient(_instanceRepository.Object);
-                string token = PrincipalUtil.GetToken(-1);
+                string token = PrincipalUtil.GetToken(1, 3);
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                 // Act
