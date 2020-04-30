77f78321a68fb5e6fc25049461bd0e2960694804
jeevananthank <jeevananthan.kesavan@accenture.com>
2020-04-30 16:11:42 +0200

    test for portal simulation

---
+++ src/test/K6/src/tests/app/rf0002portal.js
@@ -0,0 +1,113 @@
+import { check } from "k6";
+import {addErrorCount, printResponseToConsole} from "../../errorcounter.js";
+import * as appInstances from "../../api/app/instances.js"
+import * as appData from "../../api/app/data.js"
+import * as appProcess from "../../api/app/process.js"
+import * as platformInstances from "../../api/storage/instances.js"
+import * as setUpData from "../../setup.js";
+import * as appInstantiation from "../../api/app/instantiation.js"
+import * as appResources from "../../api/app/resources.js"
+
+let instanceFormDataXml = open("../../data/rf-0002.xml");
+let users = JSON.parse(open("../../data/users.json"));
+
+export const options = {
+    thresholds:{
+        "errors": ["count<1"]
+    }
+};
+
+//Tests for App API: RF-0002
+export default function() {
+    var userNumber = (__VU - 1) % users.length;
+    var aspxauthCookie = setUpData.authenticateUser(users[userNumber].username, users[userNumber].password);
+    const runtimeToken = setUpData.getAltinnStudioRuntimeToken(aspxauthCookie);
+    setUpData.clearCookies();
+    const partyId = users[userNumber].partyid;
+    var instanceId = "";
+    var dataId = "";
+    let res, success;
+
+    //Batch api calls before creating an app instance
+    res = appInstantiation.beforeInstanceCreation(runtimeToken, partyId);
+    for(var i = 0; i < res.length; i++){
+        success = check(res[i], {
+            "Batch request before app Instantiation:": (r) => r.status === 200
+          });
+        addErrorCount(success);
+        printResponseToConsole("Batch request before app Instantiation:", success, res[i]);
+    };
+
+    //Test to create an instance with App api and validate the response
+    res = appInstances.postInstance(runtimeToken, partyId);
+    success = check(res, {
+        "E2E App POST Create Instance status is 201:": (r) => r.status === 201
+      });
+    addErrorCount(success);    
+    printResponseToConsole("E2E App POST Create Instance:", success, res);
+    
+    dataId = appData.findDataId(res.body);
+    instanceId = platformInstances.findInstanceId(res.body);
+
+    //Test to get the current process of an app instance
+    res = appProcess.getCurrentProcess(runtimeToken, partyId, instanceId);
+    success = check(res, {
+        "Get Current process of instance:": (r) => r.status === 200
+    });
+    addErrorCount(success);    
+    printResponseToConsole("Get Current process of instance:", success, res);
+
+    //Test to get the form data xml by id
+    res = appData.getDataById(runtimeToken, partyId, instanceId, dataId);
+    success = check(res, {
+        "Get form data XML by id:": (r) => r.status === 200
+    });
+    addErrorCount(success);    
+    printResponseToConsole("Get form data XML by id:", success, res);
+
+    //Batch request to get the app resources
+    res = appResources.batchGetAppResources(runtimeToken);
+    for(var i = 0; i < res.length; i++){
+        success = check(res[i], {
+            "Batch request to get app resources:": (r) => r.status === 200
+          });
+        addErrorCount(success);
+        printResponseToConsole("Batch request to get app resources:", success, res[i]);
+    };
+
+    //Test to edit a form data in an instance with App APi and validate the response
+    for(var i = 0; i < 8; i++){
+        res = appData.putDataById(runtimeToken, partyId, instanceId, dataId, "default", instanceFormDataXml);
+        success = check(res, {
+            "E2E PUT Edit Data by Id status is 201:": (r) => r.status === 201
+        });
+        addErrorCount(success);    
+        printResponseToConsole("E2E PUT Edit Data by Id:", success, res);
+    };
+
+    //Test to get validate instance and verify that validation of instance is ok
+    res = appInstances.getValidateInstance(runtimeToken, partyId, instanceId);
+    success = check(res, {
+        "E2E App GET Validate Instance validation OK:": (r) => r.body && (JSON.parse(r.body)).length === 0
+    });
+    addErrorCount(success);    
+    printResponseToConsole("E2E App GET Validate Instance is not OK:", success, res);
+
+    //Test to move the process of an app instance to the next process element and verify response code to be 200
+    res = appProcess.putNextProcess(runtimeToken, partyId, instanceId, "EndEvent_1");
+    success = check(res, {
+        "E2E App PUT Move process to Next element status is 200:": (r) => r.status === 200
+    });
+    addErrorCount(success);
+    printResponseToConsole("E2E App PUT Move process to Next element:", success, res);
+
+    //Test to call get instance details and verify the presence of archived date
+    for(var i = 0; i < 2; i++){
+        res = appInstances.getInstanceById(runtimeToken, partyId, instanceId);
+        success = check(res, {
+            "E2E App Instance is archived:": (r) => r.body.length > 0 && (JSON.parse(r.body)).status.archived != null
+        });
+        addErrorCount(success);
+        printResponseToConsole("E2E App Instance is not archived:", success, res);
+    };
+};
