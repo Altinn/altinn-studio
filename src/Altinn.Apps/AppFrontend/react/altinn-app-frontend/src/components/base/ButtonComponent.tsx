96b4531d62b6d77eadc7d2e3b3f1e0d4481b5971
lorang92 <steffen.ekeberg@gmail.com>
2020-05-05 14:14:22 +0200

    eslint

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/ButtonComponent.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/ButtonComponent.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import * as React from 'react';
-import FormDataActions from '../../features/form/data/formDataActions';
-import { IAltinnWindow, IRuntimeState, } from './../../types';
 import { useSelector } from 'react-redux';
 import { getLanguageFromKey } from 'altinn-shared/utils/language';
 import { AltinnLoader } from 'altinn-shared/components';
+import { IAltinnWindow, IRuntimeState } from '../../types';
+import FormDataActions from '../../features/form/data/formDataActions';
 
 export interface IButtonProvidedProps {
   id: string;
@@ -21,72 +22,78 @@ export function ButtonComponent(props: IButtonProvidedProps) {
 
   const renderSubmitButton = () => {
     return (
-      <div className="col-4 pl-0">
-        {isSubmitting ? renderLoader() : 
-        <button
-          type='submit'
-          className={'a-btn a-btn-success'}
-          onClick={submitForm}
-          id={props.id}
-          style={{ marginBottom: '0' }}
-        >
-          {props.text}
-        </button>
-      }
+      <div className='col-4 pl-0'>
+        {isSubmitting ?
+          renderLoader() :
+          <button
+            type='submit'
+            className='a-btn a-btn-success'
+            onClick={submitForm}
+            id={props.id}
+            style={{ marginBottom: '0' }}
+          >
+            {props.text}
+          </button>
+        }
       </div>
     );
-  }
+  };
 
   const renderSaveButton = () => {
     return (
-      <div className="col-2 pl-0">
-      {isSaving ? renderLoader() :
-        <button
-          type='submit'
-          className={'a-btn a-btn-success'}
-          onClick={saveFormData}
-          id='saveBtn'
-          style={{ marginBottom: '0' }}
-        >
-          Lagre
-        </button>
-      }
+      <div className='col-2 pl-0'>
+        {isSaving ?
+          renderLoader() :
+          <button
+            type='submit'
+            className='a-btn a-btn-success'
+            onClick={saveFormData}
+            id='saveBtn'
+            style={{ marginBottom: '0' }}
+          >
+            Lagre
+          </button>
+        }
       </div>
     );
-  }
+  };
 
   const saveFormData = () => {
-    const { org, app, instanceId } = window as Window as IAltinnWindow;
+    const {
+      org, app, instanceId,
+    } = window as Window as IAltinnWindow;
     FormDataActions.submitFormData(
       `${window.location.origin}/${org}/${app}/api/${instanceId}`,
     );
-  }
+  };
 
   const renderLoader = () => {
     return (
       <AltinnLoader
         srContent={getLanguageFromKey('general.loading', props.language)}
         style={{
-        marginLeft: '40px',
-        marginTop: '2px',
-        height: '45px' // same height as button
+          marginLeft: '40px',
+          marginTop: '2px',
+          height: '45px', // same height as button
         }}
       />
-    )
-  }
+    );
+  };
 
   const submitForm = () => {
-    const {org, app, instanceId } = window as Window as IAltinnWindow;
+    const {
+      org, app, instanceId,
+    } = window as Window as IAltinnWindow;
     FormDataActions.submitFormData(
       `${window.location.origin}/${org}/${app}/api/${instanceId}`,
       'Complete',
     );
-  }
+  };
 
   return (
     <div className='container pl-0'>
       <div className='a-btn-group' style={{ marginTop: '3.6rem', marginBottom: '0' }}>
-        <div className="row">
+        <div className='row'>
           {autoSave === false && renderSaveButton()}
           {renderSubmitButton()}
         </div>
