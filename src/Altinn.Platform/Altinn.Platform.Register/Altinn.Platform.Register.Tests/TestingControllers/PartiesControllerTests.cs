b3ce606c1a8685ae6da6f470ab32d81c11e9b50c
Terje Holene <terje.holene@gmail.com>
2020-05-11 14:21:12 +0200

    Deleted GET operations with parameter from body (#4109)
    * Deleted GET operations with parameter from body
    
    * Added tests of OrganizationsController
    
    * More tests for improved coverage
    
    * Update to LocalTest

--- src/Altinn.Platform/Altinn.Platform.Register/Altinn.Platform.Register.Tests/TestingControllers/PartiesControllerTests.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Altinn.Platform.Register.Tests/TestingControllers/PartiesControllerTests.cs
@@ -5,11 +5,13 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Altinn.Platform.Register.IntegrationTest.Utils;
+
+using Altinn.Platform.Register.Tests.Utils;
 using Altinn.Platform.Register.Models;
 using Altinn.Platform.Register.Services.Interfaces;
-using Altinn.Platform.Storage.IntegrationTest.Mocks.Authentication;
+using Altinn.Platform.Register.Tests.Mocks.Authentication;
 using AltinnCore.Authentication.JwtCookie;
+
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
@@ -18,7 +20,7 @@ using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 
-namespace Altinn.Platform.Register.IntegrationTest.TestingControllers
+namespace Altinn.Platform.Register.Tests.TestingControllers
 {
     public class PartiesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
@@ -33,9 +35,73 @@ namespace Altinn.Platform.Register.IntegrationTest.TestingControllers
             _factory = factory;
         }
 
-        /// <summary>
-        /// Test that seeks to ensure that the lookup endpoint has model validation. It does not test model validation.
-        /// </summary>
+        [Fact]
+        public async Task GetParty_ValidTokenRequestForExistingParty_ReturnsParty()
+        {
+            string token = PrincipalUtil.GetToken(1);
+            int partyId = 6565;
+
+            // Arrange
+            Mock<IParties> partiesService = new Mock<IParties>();
+            partiesService.Setup(s => s.GetParty(It.Is<int>(o => o == partyId))).ReturnsAsync(new Party());
+
+            HttpClient client = GetTestClient(partiesService.Object);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync("/register/api/v1/parties/" + partyId);
+
+            // Assert
+            partiesService.VerifyAll();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            
+            Party actual = await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
+
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public async Task GetParty_ValidTokenRequestForNonExistingParty_ReturnsNotFound()
+        {
+            string token = PrincipalUtil.GetToken(1);
+            int partyId = 6565;
+
+            // Arrange
+            Mock<IParties> partiesService = new Mock<IParties>();
+            partiesService.Setup(s => s.GetParty(It.Is<int>(o => o == partyId))).ReturnsAsync((Party)null);
+
+            HttpClient client = GetTestClient(partiesService.Object);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync("/register/api/v1/parties/" + partyId);
+
+            // Assert
+            partiesService.VerifyAll();
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetParty_ExpiredToken_ReturnsUnAuthorized()
+        {
+            string token = PrincipalUtil.GetExpiredToken();
+            int partyId = 6565;
+
+            // Arrange
+            Mock<IParties> partiesService = new Mock<IParties>();
+
+            HttpClient client = GetTestClient(partiesService.Object);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync("/register/api/v1/parties/" + partyId);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
         [Fact]
         public async Task PostPartyLookup_ModelIsInvalid_ReturnsBadRequest()
         {
@@ -58,9 +124,6 @@ namespace Altinn.Platform.Register.IntegrationTest.TestingControllers
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
-        /// <summary>
-        /// Test that seeks to ensure that the lookup endpoint responds with NotFound if party lookup based on string value returns null.
-        /// </summary>
         [Fact]
         public async Task PostPartyLookup_InputIsSsn_BackendServiceRespondsWithNull_ControllerRespondsWithNotFound()
         {
@@ -82,17 +145,12 @@ namespace Altinn.Platform.Register.IntegrationTest.TestingControllers
             // Act
             HttpResponseMessage response = await client.PostAsync("/register/api/v1/parties/lookup", requestBody);
 
-            string responseCp = await response.Content.ReadAsStringAsync();
-
             // Assert
             partiesService.VerifyAll();
 
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
 
-        /// <summary>
-        /// Test that seeks to ensure that the lookup endpoint responds Ok and a party if the given organisation number is found.
-        /// </summary>
         [Fact]
         public async Task PostPartyLookup_InputIsOrgNo_BackendServiceRespondsWithParty_ControllerRespondsWithOkAndParty()
         {
@@ -128,6 +186,30 @@ namespace Altinn.Platform.Register.IntegrationTest.TestingControllers
             Party actual = await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
 
             Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public async Task PostPartyLookup_ExpiredToken_ReturnsUnAuthorized()
+        {
+            string token = PrincipalUtil.GetExpiredToken();
+
+            // Arrange
+            string OrgNo = "555000103";
+
+            Mock<IParties> partiesService = new Mock<IParties>();
+
+            HttpClient client = GetTestClient(partiesService.Object);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            PartyLookup lookUp = new PartyLookup { OrgNo = OrgNo }; 
+
+            StringContent requestBody = new StringContent(JsonSerializer.Serialize(lookUp), Encoding.UTF8, "application/json");
+
+            // Act
+            HttpResponseMessage response = await client.PostAsync("/register/api/v1/parties/lookup", requestBody);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
         private HttpClient GetTestClient(IParties partiesService)
--- src/Altinn.Platform/Altinn.Platform.Register/Altinn.Platform.Register.Tests/Utils/PrincipalUtil.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Altinn.Platform.Register.Tests/Utils/PrincipalUtil.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
-using Altinn.Platform.Register.IntegrationTest.Mocks;
+
+using Altinn.Platform.Register.Tests.Mocks;
 using AltinnCore.Authentication.Constants;
 
 #pragma warning disable 1591
 #pragma warning disable SA1600
 
-namespace Altinn.Platform.Register.IntegrationTest.Utils
+namespace Altinn.Platform.Register.Tests.Utils
 {
     public static class PrincipalUtil
     {
@@ -27,9 +28,20 @@ namespace Altinn.Platform.Register.IntegrationTest.Utils
             ClaimsIdentity identity = new ClaimsIdentity("mock");
             identity.AddClaims(claims);
             ClaimsPrincipal principal = new ClaimsPrincipal(identity);
-            string token = JwtTokenMock.GenerateToken(principal, new TimeSpan(1, 1, 1));
+            string token = JwtTokenMock.GenerateToken(principal, new TimeSpan(0, 0, 5));
 
             return token;
+        }
+
+        public static string GetExpiredToken()
+        {
+            return "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ4Q0VFNjAzMzEwMkYzMjQzMTk2NDc4QUYwNkZCNDNBMTc2NEQ4NDMiLCJ4NXQiOiJTTTdtQXpFQzh5UXhsa2"
+                   + "VLOEctME9oZGsyRU0iLCJ0eXAiOiJKV1QifQ.eyJ1cm46YWx0aW5uOnVzZXJpZCI6IjEiLCJ1cm46YWx0aW5uOnVzZXJuYW1lIjoiVXNlck9uZSI"
+                   + "sInVybjphbHRpbm46cGFydHlpZCI6MSwidXJuOmFsdGlubjphdXRoZW50aWNhdGVtZXRob2QiOiJNb2NrIiwidXJuOmFsdGlubjphdXRobGV2ZWw"
+                   + "iOjIsIm5iZiI6MTU4ODc2NjU4NSwiZXhwIjoxNTg4NzY2NTg5LCJpYXQiOjE1ODg3NjY1ODUsImF1ZCI6ImFsdGlubi5ubyJ9.JbwlNTwnCpafFZ"
+                   + "-452MM9ZvxTdkb2pMbPFsIOEqB0rvj62Xtex44fHW1Sf2mIld7UmEgw8Lfg-8qKz1SBXx-CYk_ZF-1g7suldEHqhjovQ8IQUwFMy8JaPbVJrZigI"
+                   + "2UI2myHAETj67YnKkAdImvEUPrJXYtRAOYzp4jH_GrFkGXe30sx3RIvCt2k5BFdsV2Q6kLXsH4Q0jpfJR0XkG1mhgojPc8es1no8XWB8yn8HZGgi"
+                   + "I9d4F2Edrs0nhCKs5JSFbdX5jVNAhYOw833yNxzSI5keFlRrCN_BXDSqdo9bn8joCwnCJ9fZ3kv_ieYKbMa0tgcN9lBM_KcGQU5EPxpA";
         }
 
         public static string GetOrgToken(string org, int orgNumber = 111111111, string scope = "altinn:appdeploy")
