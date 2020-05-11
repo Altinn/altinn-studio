3a75733a31550228dac3c8197466b14a05c0f218
Terje Holene <terje.holene@gmail.com>
2020-05-11 12:49:10 +0200

    Delete sneaky whitespace at the end of lines

--- src/development/LocalTest/Services/Register/Interface/IPersons.cs
+++ src/development/LocalTest/Services/Register/Interface/IPersons.cs
@@ -1,18 +1,18 @@
-using Altinn.Platform.Register.Models;	
-using System.Threading.Tasks;	
+using Altinn.Platform.Register.Models;
+using System.Threading.Tasks;
 
-namespace LocalTest.Services.Register.Interface	
-{	
-    /// <summary>	
-    /// Interface handling methods for operations related to persons	
-    /// </summary>	
-    public interface IPersons	
-    {	
-        /// <summary>	
-        /// Method that fetches a person based on a  security number	
-        /// </summary>	
-        /// <param name="ssn">The persons ssn</param>	
-        /// <returns></returns>	
-        Task<Person> GetPerson(string ssn);	
-    }	
+namespace LocalTest.Services.Register.Interface
+{
+    /// <summary>
+    /// Interface handling methods for operations related to persons
+    /// </summary>
+    public interface IPersons
+    {
+        /// <summary>
+        /// Method that fetches a person based on a  security number
+        /// </summary>
+        /// <param name="ssn">The persons ssn</param>
+        /// <returns></returns>
+        Task<Person> GetPerson(string ssn);
+    }
 }
