fecc9ada89a617fafeea28d559f5089854ba3567
Rune T. Larsen <rune@hjemmekino.no>
2020-05-22 15:27:56 +0200

    Fixed spelling

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Extentions/HttpClientExtension.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Extentions/HttpClientExtension.cs
@@ -17,15 +17,16 @@ namespace Altinn.App.PlatformServices.Extentions
         /// <param name="authorizationToken">the authorization token (jwt)</param>
         /// <param name="requestUri">The request Uri</param>
         /// <param name="content">The http content</param>
+        /// <param name="platformAccessToken">The platformAccess tokens</param>
         /// <returns>A HttpResponseMessage</returns>
-        public static Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string authorizationToken, string requestUri, HttpContent content, string plattformAccessToken = null)
+        public static Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string authorizationToken, string requestUri, HttpContent content, string platformAccessToken = null)
         {
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
                 request.Headers.Add("Authorization", "Bearer " + authorizationToken);
                 request.Content = content;
-                if (!string.IsNullOrEmpty(plattformAccessToken))
+                if (!string.IsNullOrEmpty(platformAccessToken))
                 {
-                    request.Headers.Add("PlatformAccessToken", plattformAccessToken);
+                    request.Headers.Add("PlatformAccessToken", platformAccessToken);
                 }
 
                 return httpClient.SendAsync(request, CancellationToken.None);
@@ -38,15 +39,16 @@ namespace Altinn.App.PlatformServices.Extentions
         /// <param name="authorizationToken">the authorization token (jwt)</param>
         /// <param name="requestUri">The request Uri</param>
         /// <param name="content">The http content</param>
+        /// <param name="platformAccessToken">The platformAccess tokens</param>
         /// <returns>A HttpResponseMessage</returns>
-        public static Task<HttpResponseMessage> PutAsync(this HttpClient httpClient, string authorizationToken, string requestUri, HttpContent content, string plattformAccessToken = null)
+        public static Task<HttpResponseMessage> PutAsync(this HttpClient httpClient, string authorizationToken, string requestUri, HttpContent content, string platformAccessToken = null)
         {
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, requestUri);
                 request.Headers.Add("Authorization", "Bearer " + authorizationToken);
                 request.Content = content;
-                if (!string.IsNullOrEmpty(plattformAccessToken))
+                if (!string.IsNullOrEmpty(platformAccessToken))
                 {
-                    request.Headers.Add("PlatformAccessToken", plattformAccessToken);
+                    request.Headers.Add("PlatformAccessToken", platformAccessToken);
                 }
 
                 return httpClient.SendAsync(request, CancellationToken.None);
@@ -59,14 +61,15 @@ namespace Altinn.App.PlatformServices.Extentions
         /// <param name="httpClient">The httpclient</param>
         /// <param name="authorizationToken">the authorization token (jwt)</param>
         /// <param name="requestUri">The request Uri</param>
+        /// <param name="platformAccessToken">The platformAccess tokens</param>
         /// <returns>A HttpResponseMessage</returns>
-        public static Task<HttpResponseMessage> GetAsync(this HttpClient httpClient, string authorizationToken, string requestUri, string plattformAccessToken = null)
+        public static Task<HttpResponseMessage> GetAsync(this HttpClient httpClient, string authorizationToken, string requestUri, string platformAccessToken = null)
         {
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUri);
             request.Headers.Add("Authorization", "Bearer " + authorizationToken);
-            if(!string.IsNullOrEmpty(plattformAccessToken))
+            if(!string.IsNullOrEmpty(platformAccessToken))
             {
-                request.Headers.Add("PlatformAccessToken", plattformAccessToken);
+                request.Headers.Add("PlatformAccessToken", platformAccessToken);
             }
 
             return httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, CancellationToken.None);
@@ -79,14 +82,15 @@ namespace Altinn.App.PlatformServices.Extentions
         /// <param name="httpClient">The httpclient</param>
         /// <param name="authorizationToken">the authorization token (jwt)</param>
         /// <param name="requestUri">The request Uri</param>
+        /// <param name="platformAccessToken">The platformAccess tokens</param>
         /// <returns>A HttpResponseMessage</returns>
-        public static Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient, string authorizationToken, string requestUri, string plattformAccessToken = null)
+        public static Task<HttpResponseMessage> DeleteAsync(this HttpClient httpClient, string authorizationToken, string requestUri, string platformAccessToken = null)
         {
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
             request.Headers.Add("Authorization", "Bearer " + authorizationToken);
-            if (!string.IsNullOrEmpty(plattformAccessToken))
+            if (!string.IsNullOrEmpty(platformAccessToken))
             {
-                request.Headers.Add("PlatformAccessToken", plattformAccessToken);
+                request.Headers.Add("PlatformAccessToken", platformAccessToken);
             }
 
             return httpClient.SendAsync(request, CancellationToken.None);
