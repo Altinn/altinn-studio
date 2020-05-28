2989595e2df74baf11821c1362c9b89dfab5d4e7
lorang92 <steffen.ekeberg@gmail.com>
2020-05-28 15:28:32 +0200

    Removed API connections all together

--- src/studio/src/designer/frontend/ux-editor/reducers/serviceConfigurationReducer/index.ts
+++ src/studio/src/designer/frontend/ux-editor/reducers/serviceConfigurationReducer/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers, Reducer } from 'redux';
-import apiReducer, { IApiState } from '../apiReducer';
 import conditionalRenderingReducer, { IConditionalRenderingState } from '../conditionalRenderingReducer';
 import manageServiceConfigurationReducer,
 { IManageServiceConfigurationState } from '../manageServiceConfigurationReducer';
 import ruleConnectionReducer, { IRuleConnectionState } from '../ruleConnectionReducer';
 
 export interface IServiceConfigurationState {
-  APIs: IApiState;
   ruleConnection: IRuleConnectionState;
   conditionalRendering: IConditionalRenderingState;
   manageServiceConfiguration: IManageServiceConfigurationState;
@@ -14,7 +12,6 @@ export interface IServiceConfigurationState {
 }
 
 const combinedReducers: Reducer<IServiceConfigurationState> = combineReducers({
-  APIs: apiReducer,
   ruleConnection: ruleConnectionReducer,
   conditionalRendering: conditionalRenderingReducer,
   manageServiceConfiguration: manageServiceConfigurationReducer,
