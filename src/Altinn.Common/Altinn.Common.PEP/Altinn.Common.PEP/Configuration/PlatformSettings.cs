bd1f3e22dfa4e5e0ce16e3ca343e3c11c7546bbe
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-05-28 09:19:30 +0200

    simplified get/set

--- src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Configuration/PlatformSettings.cs
+++ src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Configuration/PlatformSettings.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Altinn.Common.PEP.Configuration
 {
     /// <summary>
@@ -7,27 +5,14 @@ namespace Altinn.Common.PEP.Configuration
     /// </summary>
     public class PlatformSettings
     {
-        private string _subscriptionKey;
-        private string _apiAuthorizationEndpoint;
-
         /// <summary>
         /// Gets or sets the url for the API Authorization endpoint
         /// </summary>
-        public string ApiAuthorizationEndpoint
-        {
-            get => Environment.GetEnvironmentVariable("PlatformSettings__ApiAuthorizationEndpoint") ?? _apiAuthorizationEndpoint;
-            set => _apiAuthorizationEndpoint = value;
-        }
+        public string ApiAuthorizationEndpoint { get; set; }
 
         /// <summary>
         /// Gets or sets the subscription key value to use in requests against the platform.
         /// </summary>
-        public string SubscriptionKey
-        {
-            get => Environment.GetEnvironmentVariable("PlatformSettings__SubscriptionKey") ?? _subscriptionKey;
-            set => _subscriptionKey = value;
-        }
-
-
+        public string SubscriptionKey { get; set; }
     }
 }
