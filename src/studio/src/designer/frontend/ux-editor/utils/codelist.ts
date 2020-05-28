e3ff7032409bc7aae2e0eb89cfac5af23bb7f2b6
lorang92 <steffen.ekeberg@gmail.com>
2020-05-28 15:39:35 +0200

    Code smell

--- src/studio/src/designer/frontend/ux-editor/utils/codelist.ts
+++ src/studio/src/designer/frontend/ux-editor/utils/codelist.ts
@@ -1,7 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+
 /*
   Returns the index of a given code list name, or -1 if not found
 */
-export default function getCodeListIndexByName(name: string, codeLists: ICodeListListElement[]) {
+export function getCodeListIndexByName(name: string, codeLists: ICodeListListElement[]) {
   if (!codeLists) {
     return -1;
   }
