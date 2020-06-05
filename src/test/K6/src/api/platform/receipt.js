11e351a49f3d00c400bbd6765bf1b811d30785b0
Kesavan <acn-jkesa@ai-dev.no>
2020-06-05 08:18:51 +0200

    corrected typo

--- src/test/K6/src/api/platform/receipt.js
+++ src/test/K6/src/api/platform/receipt.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import * as config from "../../config.js";
 import * as header from "../../buildrequestheaders.js";
 
-//Request to platform receiipt component returns the response with instance details and party information
+//Request to platform receipt component and returns the response with instance details and party information
 export function getReceipt(partyId, instanceId, altinnStudioRuntimeCookie){    
     var endpoint = config.platformReceipt["receipt"] + "/" + partyId + "/" + instanceId + "?includeParty=true";
     var params = header.buildHearderWithRuntime(altinnStudioRuntimeCookie, "platform");
