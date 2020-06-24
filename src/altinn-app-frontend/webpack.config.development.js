bf66a62fd20951a9ac53a8989883861d8b281062
Nina Kylstad <nkylstad@gmail.com>
2020-06-24 14:57:02 +0200

    revert accidental commit (#4429)

--- src/altinn-app-frontend/webpack.config.development.js
+++ src/altinn-app-frontend/webpack.config.development.js
@@ -78,7 +78,6 @@ module.exports = {
         loader: MiniCssExtractPlugin.loader,
         options: {
           url: false,
-        
         }
       },
       {
@@ -107,7 +106,6 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "altinn-app-frontend.css",
-      ignoreOrder: false,
     }),
     new CheckerPlugin(),
   ],
