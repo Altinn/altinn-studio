199e377cc8e2916066a3a2d377858bb2749b7be7
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2020-07-03 14:10:07 +0200

    languag support up and running (#4488)
    * languag support up and running
    
    Co-authored-by: Stephanie Buadu <acn-sbuad@ai-dev.no>

--- src/shared/src/types/index.ts
+++ src/shared/src/types/index.ts
@@ -173,7 +173,7 @@ export interface IProfile {
 }
 
 export interface IProfileSettingPreference {
-  language: number;
+  language: string;
   preSelectedPartyId: number;
   doNotPromptForParty: boolean;
 }
