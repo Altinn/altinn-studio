81b93c50b3c41ad40ebb0a09a24132e3df9a7829
Terje Holene <terje.holene@gmail.com>
2020-07-09 09:54:52 +0200

    Udated packagereferences and deleted unused test project

--- src/Altinn.Platform/Altinn.Platform.Storage/IntegrationTest/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "This is test code", Scope = "member", Target = "~M:Altinn.Platform.Storage.Helpers.GenerateInstanceTestData.For1000InstanceOwners(System.Net.Http.HttpClient)~System.Boolean")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "This is test code", Scope = "member", Target = "~M:Altinn.Platform.Storage.IntegrationTest.Mocks.Authentication.ConfigurationManagerStub.#ctor(System.String,Microsoft.IdentityModel.Protocols.IConfigurationRetriever{Microsoft.IdentityModel.Protocols.OpenIdConnect.OpenIdConnectConfiguration},Microsoft.IdentityModel.Protocols.IDocumentRetriever)")]
