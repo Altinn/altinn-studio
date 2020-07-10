79ccda714c221b46be7a3594a8ab2c1f3709b133
Terje Holene <terje.holene@gmail.com>
2020-07-10 15:46:39 +0200

    Fixes to LocalTest. Removed EntityFramework and fixes for StyleCop (#4518)

--- src/development/LocalTest/Services/Authentication/Implementation/AuthenticationService.cs
+++ src/development/LocalTest/Services/Authentication/Implementation/AuthenticationService.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+
 using LocalTest.Services.Authentication.Interface;
+
 using Microsoft.IdentityModel.Tokens;
 
 namespace LocalTest.Services.Authentication.Implementation
@@ -15,7 +15,10 @@ namespace LocalTest.Services.Authentication.Implementation
         ///<inheritdoc/>
         public string GenerateToken(ClaimsPrincipal principal, int cookieValidityTime)
         {
-            List<X509Certificate2> certificates = new List<X509Certificate2> { new X509Certificate2("jwtselfsignedcert.pfx", "qwer1234") };
+            List<X509Certificate2> certificates = new List<X509Certificate2>
+            {
+                new X509Certificate2("jwtselfsignedcert.pfx", "qwer1234")
+            };
 
             TimeSpan tokenExpiry = new TimeSpan(0, cookieValidityTime, 0);
 
