b01499158610cd6f2117e065346bc4bc0c5654f9
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-07-27 11:38:25 +0200

    updated timer

--- src/Altinn.Platform/Altinn.Platform.Storage/DataCleanup/NightlyCleanup.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/DataCleanup/NightlyCleanup.cs
@@ -5,12 +5,19 @@ using Microsoft.Extensions.Logging;
 
 namespace Altinn.Platform.Storage.DataCleanup
 {
+    /// <summary>
+    /// Azure Function class for handling tasks related to nightly data cleanup.
+    /// </summary>
     public class NightlyCleanup
     {
+        /// <summary>
+        /// Runs nightly cleanup.
+        /// </summary>
+        /// <param name="timer">The trigger timer.</param>
+        /// <param name="log">The log</param>
         [FunctionName("NightlyCleanup")]
-        public static void Run([TimerTrigger("* */15 * * * 1-5")]TimerInfo timer, ILogger log)
+        public static void Run([TimerTrigger("0 0 */1 * * 1-5")]TimerInfo timer, ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
 }
