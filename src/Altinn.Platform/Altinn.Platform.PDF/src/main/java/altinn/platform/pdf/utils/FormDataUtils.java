8b2d94ca8bcc5a5baa5154e9d14d707dc6b97682
lorang92 <steffen.ekeberg@gmail.com>
2020-08-17 12:43:38 +0200

    Compliant xml parser

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/FormDataUtils.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/FormDataUtils.java
@@ -98,6 +98,9 @@ public class FormDataUtils {
     String xmlAsString = new String(xmlAsBytes, StandardCharsets.UTF_8);
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+    factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+    factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+    factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
     DocumentBuilder builder = factory.newDocumentBuilder();
     return builder.parse(new InputSource(new StringReader(xmlAsString)));
   }
