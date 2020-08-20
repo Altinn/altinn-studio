57a7479573695af82e257c0d2f1fffe8db5270ad
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-08-20 15:35:09 +0200

    missing using statement

--- src/Altinn.Platform/Altinn.Platform.Register/Register/Health/HealthTelemetryFilter.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Register/Health/HealthTelemetryFilter.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
