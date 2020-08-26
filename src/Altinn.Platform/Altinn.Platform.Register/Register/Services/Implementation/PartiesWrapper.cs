7afa0756257470b90c4c341267245561e5c1a279
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2020-08-26 13:08:04 +0200

    caching party for 5 minutes (#4661)
    * caching party for 5 minutes
    
    * fixed pr comments
    
    Co-authored-by: Stephanie Buadu <acn-sbuad@ai-dev.no>

--- src/Altinn.Platform/Altinn.Platform.Register/Register/Services/Implementation/PartiesWrapper.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Register/Services/Implementation/PartiesWrapper.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Altinn.Platform.Register.Configuration;
 using Altinn.Platform.Register.Models;
 using Altinn.Platform.Register.Services.Interfaces;
-
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -21,6 +21,8 @@ namespace Altinn.Platform.Register.Services.Implementation
         private readonly GeneralSettings _generalSettings;
         private readonly ILogger _logger;
         private readonly HttpClient _client;
+        private readonly IMemoryCache _memoryCache;
+        private const int _cacheTimeout = 5;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PartiesWrapper"/> class
@@ -28,23 +30,33 @@ namespace Altinn.Platform.Register.Services.Implementation
         /// <param name="httpClient">HttpClient from default httpclientfactory</param>
         /// <param name="generalSettings">the general settings</param>
         /// <param name="logger">the logger</param>
-        public PartiesWrapper(HttpClient httpClient, IOptions<GeneralSettings> generalSettings, ILogger<PartiesWrapper> logger)
+        /// <param name="memoryCache">the memory cache</param>
+        public PartiesWrapper(HttpClient httpClient, IOptions<GeneralSettings> generalSettings, ILogger<PartiesWrapper> logger, IMemoryCache memoryCache)
         {
             _generalSettings = generalSettings.Value;
             _logger = logger;
             _client = httpClient;
+            _memoryCache = memoryCache;
         }
 
         /// <inheritdoc />
         public async Task<Party> GetParty(int partyId)
         {
+            string cacheKey = $"PartyId:{partyId}";
+            if (_memoryCache.TryGetValue(cacheKey, out Party party))
+            {
+                return party;
+            }
+
             Uri endpointUrl = new Uri($"{_generalSettings.BridgeApiEndpoint}parties/{partyId}");
 
             HttpResponseMessage response = await _client.GetAsync(endpointUrl);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                return await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
+                party = await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
+                _memoryCache.Set(cacheKey, party, new TimeSpan(0, _cacheTimeout, 0));
+                return party;
             }
             else
             {
@@ -58,20 +70,22 @@ namespace Altinn.Platform.Register.Services.Implementation
         public async Task<Party> LookupPartyBySSNOrOrgNo(string lookupValue)
         {
             Uri endpointUrl = new Uri($"{_generalSettings.BridgeApiEndpoint}parties/lookupObject");
-       
+
             StringContent requestBody = new StringContent(JsonSerializer.Serialize(lookupValue), Encoding.UTF8, "application/json");
 
             HttpResponseMessage response = await _client.PostAsync(endpointUrl, requestBody);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                return await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
+                Party party = await JsonSerializer.DeserializeAsync<Party>(await response.Content.ReadAsStreamAsync());
+                _memoryCache.Set($"PartyId:{party.PartyId}", party, new TimeSpan(0, _cacheTimeout, 0));
+                return party;
             }
             else
             {
                 _logger.LogError($"Getting party by lookup value failed with statuscode {response.StatusCode}");
             }
-       
+
             return null;
         }
 
