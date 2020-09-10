43ec657783f2d5bf09c3a2f5f06e913a7e376849
Rune Tømmerås Larsen <rune@hjemmekino.no>
2020-09-10 10:01:09 +0200

    Removed unsused langauge stuff (#4749)
    * Removed unsused langauge stuff
    
    * Fixed stylecop issue. Really donmt know why it complains now

--- src/Altinn.Platform/Altinn.Platform.Authorization/Altinn.Authorization.ABAC/Xacml/JsonProfile/XacmlJsonRequests.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Altinn.Authorization.ABAC/Xacml/JsonProfile/XacmlJsonRequests.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace Altinn.Authorization.ABAC.Xacml.JsonProfile
 {
+    /// <summary>
+    /// Defines a list of json request
+    /// </summary>
     public class XacmlJsonRequests
     {
+        /// <summary>
+        /// A list of requests
+        /// </summary>
         public List<XacmlJsonRequest> Requests { get; set; }
     }
 }
