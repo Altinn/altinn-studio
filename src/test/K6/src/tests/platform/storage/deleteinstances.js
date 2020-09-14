c4b518971aba083c06d6bb09cbed7da061d10b85
jeevananthank <jeevananthan.kesavan@accenture.com>
2020-09-14 10:24:15 +0200

    fixed indefinite looping on delete instances

--- src/test/K6/src/tests/platform/storage/deleteinstances.js
+++ src/test/K6/src/tests/platform/storage/deleteinstances.js
@@ -1,6 +1,6 @@
 /* 
     Test data required: username and password, deployed app that requires level 2 login (reference app: ttd/apps-test)
-    Command: docker-compose run k6 run src/tests/platform/storage/deleteinstances.js -e env=*** -e org=*** -e username=*** -e userpwd=*** -e level2app=***
+    Command: docker-compose run k6 run src/tests/platform/storage/deleteinstances.js -e env=*** -e org=*** -e username=*** -e userpwd=*** -e level2app=*** -e appnames=app1;app2
 */
 
 import { check } from "k6";
@@ -12,6 +12,7 @@ const userName = __ENV.username;
 const userPassword = __ENV.userpwd;
 const appOwner = __ENV.org;
 const level2App = __ENV.level2app;
+let appNames = __ENV.appnames;
 
 export const options = {
     thresholds: {
@@ -30,12 +31,20 @@ export function setup() {
     return data;
 };
 
-//Hard delete instances under a party id
+//Hard delete instances under a party id based on supplied app names
 export default function (data) {
     const runtimeToken = data["RuntimeToken"];
-    const partyId = data["partyId"];    
-    var res, success;
+    const partyId = data["partyId"];
+    var res, success, instances;
     var instancesCount = 0;
+
+    try {
+        appNames = appNames.split(";");
+        appNames.push(level2App);        
+    } catch (error) {
+        appNames = [];
+        appNames.push(level2App);        
+    }
 
     do {
         //Find active instances under the party id to be deleted.
@@ -45,20 +54,23 @@ export default function (data) {
         });
         addErrorCount(success);
 
-        instancesCount = JSON.parse(res.body).length;
+        //Filter instances based on appName
+        instances = sbl.filterInstancesByAppName(appNames, res.body);        
+        instancesCount = instances.length;
 
         //hard delete all the instances fetched
         if (instancesCount > 0) {
-            sbl.hardDeleteManyInstances(runtimeToken, res.body, instancesCount);
+            sbl.hardDeleteManyInstances(runtimeToken, instances);
 
-            //Find more instances to loop through if instance count is > 0
+            //Find more instances to loop through
             res = sbl.getSblInstanceByParty(runtimeToken, partyId);
             success = check(res, {
                 "GET SBL Instance by Party status is 200:": (r) => r.status === 200
             });
             addErrorCount(success);
 
-            instancesCount = JSON.parse(res.body).length;
+            instances = sbl.filterInstancesByAppName(appNames, res.body);            
+            instancesCount = instances.length;            
         };
 
     } while (instancesCount > 0)
