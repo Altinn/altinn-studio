4bec113a9f2291f879e5fd1eed765d67b0a53819
jeevananthank <jeevananthan.kesavan@accenture.com>
2020-10-05 15:36:57 +0200

    updated appowner test and testcafe version

--- src/test/K6/src/tests/app/appowner/end2end.js
+++ src/test/K6/src/tests/app/appowner/end2end.js
@@ -4,8 +4,9 @@
     2. Installed appOwner certificate
     3. Send maskinporten token as environment variable after generating the token
 
-    This test script is end to end test of an app using app api
-    Test data required: username and password, deployed app that requires level 2 login (reference app: ttd/apps-test) to find the party id of the user to create an instance
+    This test script is end to end test of an app using app api for app owner where data upload is by user
+    Test data required: username and password, deployed app with two tasks that requires level 2 login 
+    (reference app: ttd/two-task-app) to find the party id of the user to create an instance
     and maskinporten token
     Command: docker-compose run k6 run src/tests/app/appowner/instances.js 
     -e env=*** -e org=*** -e username=*** -e userpwd=*** -e level2app=*** -e subskey=*** -e maskinporten=token
@@ -43,7 +44,8 @@ export function setup() {
     var altinnStudioRuntimeCookie = setUpData.getAltinnStudioRuntimeToken(aspxauthCookie);
     setUpData.clearCookies();
     var data = setUpData.getUserData(altinnStudioRuntimeCookie, appOwner, level2App);
-    
+    data.userRuntimeToken = altinnStudioRuntimeCookie; //send user token to use in updating instance data
+
     //get token for appowner: ttd
     altinnStudioRuntimeCookie = convertMaskinPortenToken(maskinPortenToken, "true");
     data.RuntimeToken = altinnStudioRuntimeCookie;
@@ -53,10 +55,10 @@ export function setup() {
     return data;
 };
 
-
 //End 2 end test of app api for an app with attachment component as an appowner
 export default function (data) {
-    const runtimeToken = data["RuntimeToken"];
+    const orgRuntimeToken = data["RuntimeToken"];
+    const userRuntimeToken = data["userRuntimeToken"];
     const partyId = data["partyId"];
     const attachmentDataType = data["attachmentDataType"];
     var instanceId = "";
@@ -64,7 +66,7 @@ export default function (data) {
     var res, success;
 
     //Test to create an instance with storage api and validate the response that created by is an app owner
-    res = appInstances.postInstance(runtimeToken, partyId, appOwner, level2App);
+    res = appInstances.postInstance(orgRuntimeToken, partyId, appOwner, level2App);
     success = check(res, {
         "POST Create Instance status is 201:": (r) => r.status === 201,
         "POST Create Instance Instance Id is not null:": (r) => JSON.parse(r.body).id != null
@@ -77,7 +79,7 @@ export default function (data) {
     };
 
     //Test to get an instance by id from storage and validate the response
-    res = appInstances.getInstanceById(runtimeToken, partyId, instanceId, appOwner, level2App);
+    res = appInstances.getInstanceById(orgRuntimeToken, partyId, instanceId, appOwner, level2App);
     success = check(res, {
         "GET Instance by Id status is 200:": (r) => r.status === 200,
         "CreatedBy of Instance is app owner:": (r) => JSON.parse(r.body).createdBy.toString().length === 9
@@ -85,7 +87,7 @@ export default function (data) {
     addErrorCount(success);
 
     //Test to update the sub status of an instance and validate the response
-    res = appInstances.putUpdateSubStatus(runtimeToken, partyId, instanceId, appOwner, level2App, "test", "test description");
+    res = appInstances.putUpdateSubStatus(orgRuntimeToken, partyId, instanceId, appOwner, level2App, "test", "test description");
     success = check(res, {
         "PUT Update sub status is 200:": (r) => r.status === 200,
         "Instance sub status is updated:": (r) => JSON.parse(r.body).status.substatus != null
@@ -93,40 +95,52 @@ export default function (data) {
     addErrorCount(success);
 
     //Test to edit a form data in an instance with App APi and validate the response
-    res = appData.putDataById(runtimeToken, partyId, instanceId, dataId, "default", instanceFormDataXml, appOwner, level2App);
+    res = appData.putDataById(userRuntimeToken, partyId, instanceId, dataId, "default", instanceFormDataXml, appOwner, level2App);
     success = check(res, {
         "PUT Edit Data by Id status is 201:": (r) => r.status === 201
     });
     addErrorCount(success);
 
     //upload a valid attachment to an instance with App API
-    res = appData.postData(runtimeToken, partyId, instanceId, attachmentDataType, pdfAttachment, appOwner, level2App);
+    res = appData.postData(userRuntimeToken, partyId, instanceId, attachmentDataType, pdfAttachment, appOwner, level2App);
     success = check(res, {
         "POST Upload attachment status is 201:": (r) => r.status === 201
     });
     addErrorCount(success);
 
     //Test to get validate instance and verify that validation of instance is ok
-    res = appInstances.getValidateInstance(runtimeToken, partyId, instanceId, appOwner, level2App);
+    res = appInstances.getValidateInstance(userRuntimeToken, partyId, instanceId, appOwner, level2App);
     success = check(res, {
         "App GET Validate Instance validation OK:": (r) => (JSON.parse(r.body)).length === 0
     });
     addErrorCount(success);
 
     //Test to get next process of an app instance again and verify response code  to be 200
-    res = appProcess.getNextProcess(runtimeToken, partyId, instanceId, appOwner, level2App);
+    res = appProcess.getNextProcess(userRuntimeToken, partyId, instanceId, appOwner, level2App);
 
     var nextElement = (JSON.parse(res.body))[0];
 
     //Test to move the process of an app instance to the next process element and verify response code to be 200
-    res = appProcess.putNextProcess(runtimeToken, partyId, instanceId, nextElement, appOwner, level2App);
+    res = appProcess.putNextProcess(userRuntimeToken, partyId, instanceId, nextElement, appOwner, level2App);
+    success = check(res, {
+        "App PUT Move process to Next element status is 200:": (r) => r.status === 200
+    });
+    addErrorCount(success);
+
+    //Test to get next process of an app instance again and verify response code  to be 200
+    res = appProcess.getNextProcess(orgRuntimeToken, partyId, instanceId, appOwner, level2App);
+
+    nextElement = (JSON.parse(res.body))[0];
+
+    //Test to move the process of an app instance to the next process element and verify response code to be 200
+    res = appProcess.putNextProcess(orgRuntimeToken, partyId, instanceId, nextElement, appOwner, level2App);
     success = check(res, {
         "App PUT Move process to Next element status is 200:": (r) => r.status === 200
     });
     addErrorCount(success);
 
     //Test to mark an instance as complete confirmed and validate the response
-    res = appInstances.postCompleteConfirmation(runtimeToken, partyId, instanceId, appOwner, level2App);
+    res = appInstances.postCompleteConfirmation(orgRuntimeToken, partyId, instanceId, appOwner, level2App);
     success = check(res, {
         "POST Complete instance is 200:": (r) => r.status === 200
     });
