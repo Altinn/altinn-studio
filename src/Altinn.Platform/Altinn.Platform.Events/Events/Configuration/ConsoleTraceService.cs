87c1ca9c5907b03886087ec706dfa6aca7e09dd4
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2020-10-07 12:13:29 +0200

    migrating db using yuniql (#4937)
    * migrating db using yuniql
    
    * tests running green
    
    * tests running green locally
    
    * connecting to db based on setting
    
    * configure workspace in config
    
    * updated summaries.
    
    * db name -> eventsdb
    
    * fixed typo in appsettings
    
    * updated db name
    
    Co-authored-by: Stephanie Buadu <acn-sbuad@ai-dev.no>

---
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Configuration/ConsoleTraceService.cs
@@ -0,0 +1,55 @@
+using System;
+using Yuniql.Extensibility;
+
+namespace Altinn.Platform.Events.Configuration
+{
+    /// <summary>
+    /// Copied from sample project.
+    /// </summary>
+    public class ConsoleTraceService : ITraceService
+    {
+        /// <summary>
+        /// Debug enabled 
+        /// </summary>
+        public bool IsDebugEnabled { get; set; } = false;
+
+        /// <summary>
+        /// Info
+        /// </summary>      
+        public void Info(string message, object payload = null)
+        {
+            var traceMessage = $"INF   {DateTime.UtcNow.ToString("o")}   {message}{Environment.NewLine}";
+            Console.Write(traceMessage);
+        }
+
+        /// <summary>
+        /// Error
+        /// </summary>
+        public void Error(string message, object payload = null)
+        {
+            var traceMessage = $"ERR   {DateTime.UtcNow.ToString("o")}   {message}{Environment.NewLine}";
+            Console.Write(traceMessage);
+        }
+
+        /// <summary>
+        /// Debug
+        /// </summary>
+        public void Debug(string message, object payload = null)
+        {
+            if (IsDebugEnabled)
+            {
+                var traceMessage = $"DBG   {DateTime.UtcNow.ToString("o")}   {message}{Environment.NewLine}";
+                Console.Write(traceMessage);
+            }
+        }
+
+        /// <summary>
+        /// Success
+        /// </summary>
+        public void Success(string message, object payload = null)
+        {
+            var traceMessage = $"INF   {DateTime.UtcNow.ToString("u")}   {message}{Environment.NewLine}";
+            Console.Write(traceMessage);
+        }
+    }
+}
---
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Migration/v0.00/01-setup-tables.sql
@@ -0,0 +1,41 @@
+-- SCHEMA: events
+
+CREATE SCHEMA IF NOT EXISTS events
+    AUTHORIZATION postgres;
+
+-- Table: events.events
+
+CREATE TABLE IF NOT EXISTS events.events
+(
+    sequenceno BIGSERIAL,
+    id character varying COLLATE pg_catalog."default" NOT NULL,
+    source character varying COLLATE pg_catalog."default" NOT NULL,
+    subject character varying COLLATE pg_catalog."default" NOT NULL,
+    "time" time with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    cloudevent text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT events_pkey PRIMARY KEY (sequenceno)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE events.events
+    OWNER to postgres;
+
+-- Procecure: insert_event
+
+
+CREATE OR REPLACE PROCEDURE events.insert_event(
+	id character varying,
+	source character varying, 
+	subject character varying, 
+	type character varying,
+	cloudevent text)
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+
+INSERT INTO events.events(id,source, subject, type,cloudevent)
+	VALUES ($1, $2, $3, $4, $5);
+END;
+$$;
