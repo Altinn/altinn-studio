93926edb92285ec844feb5b646ddb29c341f6a8b
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-10-08 10:22:12 +0200

    changed type of time in events table

---
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Migration/v0.01/01-setup-tables.sql
@@ -0,0 +1,18 @@
+-- SCHEMA: events
+
+DROP TABLE events.events
+
+CREATE TABLE IF NOT EXISTS events.events
+(
+    sequenceno BIGSERIAL,
+    id character varying COLLATE pg_catalog."default" NOT NULL,
+    source character varying COLLATE pg_catalog."default" NOT NULL,
+    subject character varying COLLATE pg_catalog."default" NOT NULL,
+    "time" timestamptz  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    cloudevent text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT events_pkey PRIMARY KEY (sequenceno)
+)
+
+TABLESPACE pg_default;
+
