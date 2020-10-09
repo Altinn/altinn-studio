dae3f8863e9f4e2a00c51e177b990fd6b770f866
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-10-09 10:12:51 +0200

    added data properties to the cloud event

--- src/Altinn.Platform/Altinn.Platform.Events/Events/Models/CloudEvent.cs
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Models/CloudEvent.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Net.Mime;
 using Newtonsoft.Json;
 
 namespace Altinn.Platform.Events.Models
 {
     /// <summary>
-    /// Class describing a CloudEvent
+    /// Represents a cloud event. Based on CloudEvent: https://github.com/cloudevents/spec/blob/v1.0/spec.md.
     /// </summary>
     [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class CloudEvent
@@ -38,7 +39,7 @@ namespace Altinn.Platform.Events.Models
         /// </summary>
         [JsonProperty(PropertyName = "subject")]
         public string Subject { get; set; }
-    
+
         /// <summary>
         /// Gets or sets the Time of the event
         /// </summary>
@@ -49,6 +50,27 @@ namespace Altinn.Platform.Events.Models
         /// Gets or sets the alternativesubject of the event
         /// </summary>
         [JsonProperty(PropertyName = "alternativesubject")]
-        public string Alternativesubject { get; set; }
+        public string AlternativeSubject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cloudEvent data content. The event payload.
+        /// The payload depends on the type and the dataschema.
+        /// </summary>
+        [JsonProperty(PropertyName = "data")]
+        public object Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cloudEvent dataschema attribute.
+        /// A link to the schema that the data attribute adheres to.
+        /// </summary>
+        [JsonProperty(PropertyName = "dataschema")]
+        public Uri DataSchema { get; set; }
+
+        /// <summary>
+        /// Gets or sets the cloudEvent datacontenttype attribute.
+        /// Content type of the data attribute value.
+        /// </summary>
+        [JsonProperty(PropertyName = "contenttype")]
+        public ContentType DataContentType { get; set; }
     }
 }
