aea3389c3bfcfbaddbac612e16089fdeffb59cc3
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-10-16 15:59:37 +0200

    added support for running it locally

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Implementation/KeyVaultAppSI.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Implementation/KeyVaultAppSI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Altinn.App.Services.Interface;
@@ -7,6 +8,7 @@ using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace Altinn.App.PlatformServices.Implementation
 {
@@ -17,6 +19,8 @@ namespace Altinn.App.PlatformServices.Implementation
     {
         private readonly string _connectionString;
         private readonly string _vaultUri;
+        private readonly bool _useMock;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="KeyVaultAppSI"/> class with a client using the credentials from the key vault settings.
         /// </summary>
@@ -29,11 +33,32 @@ namespace Altinn.App.PlatformServices.Implementation
                                 $"TenantId={keyVaultSettings.Value.TenantId};" +
                                 $"AppKey={keyVaultSettings.Value.ClientSecret}";
             _vaultUri = keyVaultSettings.Value.SecretUri;
+            _useMock = !Directory.GetParent(Directory.GetCurrentDirectory()).FullName.Equals("/");
         }
 
         /// </<inheritdoc/>>
         public async Task<X509Certificate2> GetCertificateAsync(string certificateId)
         {
+            if (_useMock)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), @"keyvault.json");
+                if (File.Exists(path))
+                {
+                    string jsonString = File.ReadAllText(path);
+                    JObject keyVault = JObject.Parse(jsonString);
+                    keyVault.TryGetValue(certificateId, out JToken token);
+
+                    if (token != null)
+                    {
+                        byte[] localCertBytes = Convert.FromBase64String(token.ToString());
+                        X509Certificate2 localCert = new X509Certificate2(localCertBytes);
+                        return localCert;
+                    }                   
+                }
+
+                return null;
+            }
+
             AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider(_connectionString);
             KeyVaultClient client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
@@ -46,8 +71,20 @@ namespace Altinn.App.PlatformServices.Implementation
         /// </<inheritdoc/>>
         public async Task<string> GetSecretAsync(string secretId)
         {
+            if (_useMock)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), @"keyVault.json");
+                if (File.Exists(path))
+                {
+                    string jsonString = File.ReadAllText(path);
+                    JObject keyVault = JObject.Parse(jsonString);
+                    keyVault.TryGetValue(secretId, out JToken token);
+                    return token != null ? token.ToString() : string.Empty;
+                }
 
-            // The AzureServiceTokenProvider has its own internal static collection of principals based on the connection string.
+                return string.Empty; 
+            }
+
             AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider(_connectionString);
             KeyVaultClient client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
