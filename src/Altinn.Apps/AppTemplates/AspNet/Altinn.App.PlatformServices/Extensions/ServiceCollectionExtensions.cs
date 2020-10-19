ca5d365a543edeaa3932c8fad40b0261025ecd78
Stephanie Buadu <acn-sbuad@ai-dev.no>
2020-10-19 16:19:49 +0200

    removed unused refremoved unused nuget

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Extensions/ServiceCollectionExtensions.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Extensions/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+
 namespace Altinn.App.PlatformServices.Extensions
 {
     public static class ServiceCollectionExtensions
