c7b56999dbf1e21a40e8d2aab8789b0a1319ac34
Terje Holene <terje.holene@gmail.com>
2020-10-26 15:17:54 +0100

    Revert "Retrying a build with missing element descriptions in Program.cs"
    This reverts commit 2d7f93a02275d8bfe54d5222c2e9aa912ccbe19d.

--- src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Program.cs
+++ src/Altinn.Common/Altinn.Common.PEP/Altinn.Common.PEP/Program.cs
@@ -3,13 +3,22 @@ using Microsoft.Extensions.Hosting;
 
 namespace Altinn.Common.PEP
 {
+    /// <summary>
+    /// The main entry point for the PEP, library?
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The main entry point for the PEP, library?
+        /// </summary>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Configure logging for setting up application. Temporary
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
