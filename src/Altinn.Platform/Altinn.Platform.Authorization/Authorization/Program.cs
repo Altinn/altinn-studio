a0e74c0b95a77ada85a2d5437f8908201c45c248
Terje Holene <terje.holene@gmail.com>
2020-10-27 09:14:29 +0100

    Removing logging to windows event log

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Program.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+
 using AltinnCore.Authentication.Constants;
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
@@ -8,7 +10,6 @@ using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Altinn.Platform.Authorization
@@ -109,7 +110,6 @@ namespace Altinn.Platform.Authorization
                     builder.AddFilter("Microsoft", LogLevel.Warning);
                     builder.AddFilter("System", LogLevel.Warning);
                     builder.AddConsole();
-                    builder.AddEventLog();
                 }
             })
             .UseStartup<Startup>();
