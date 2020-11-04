5b19d1c31d85d55db8890409a9e04ffde630b262
lorang92 <steffen.ekeberg@gmail.com>
2020-11-04 10:34:57 +0100

    Simplified layoutOrder logic

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/process-step/NavigationButtons.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/process-step/NavigationButtons.tsx
@@ -28,7 +28,7 @@ export function NavigationButtons(props: INavigationButtons) {
   const [disableBack, setDisableBack] = React.useState<boolean>(false);
   const [disableNext, setDisableNext] = React.useState<boolean>(false);
   const currentView = useSelector((state: IRuntimeState) => state.formLayout.uiConfig.currentView);
-  const orderedLayoutKeys = useSelector((state: IRuntimeState) => state.formLayout.uiConfig.layoutOrder || Object.keys(state.formLayout.layouts));
+  const orderedLayoutKeys = useSelector((state: IRuntimeState) => state.formLayout.uiConfig.layoutOrder);
   const textResources = useSelector((state: IRuntimeState) => state.textResources.resources);
   const language = useSelector((state: IRuntimeState) => state.language.language);
   const { next, previous } = useSelector(
