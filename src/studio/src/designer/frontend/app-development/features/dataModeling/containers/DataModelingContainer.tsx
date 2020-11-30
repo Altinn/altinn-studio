b8ffe692c82f3d40043e8822045edfda6d82ae65
nkylstad <nkylstad@gmail.com>
2020-11-30 09:22:11 +0100

    remove hard coding of model name

--- src/studio/src/designer/frontend/app-development/features/dataModeling/containers/DataModelingContainer.tsx
+++ src/studio/src/designer/frontend/app-development/features/dataModeling/containers/DataModelingContainer.tsx
@@ -3,33 +3,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import SchemaEditorApp from '@altinn/schema-editor/SchemaEditorApp';
 import { fetchDataModel, saveDataModel, setDataModelFilePath } from '../dataModelingSlice';
 
-// const filePath = 'App/models/RA-0678_M';
 export interface IDataModelingContainer {
   filePath: string;
 }
 
-function getDataModelFileType(applicationMetadata: any) {
+function getDataModelTypeName(applicationMetadata: any) {
   if (!applicationMetadata || !applicationMetadata.dataTypes) return undefined;
   const dataTypeWithLogic = applicationMetadata.dataTypes.find((dataType: any) => dataType.appLogic);
-  if (dataTypeWithLogic) {
-    return `App/models/${dataTypeWithLogic.id}`;
-  }
-  return undefined;
+  return dataTypeWithLogic.id;
 }
 
 function DataModelingContainer(): JSX.Element {
   const dispatch = useDispatch();
   const jsonSchema = useSelector((state: IServiceDevelopmentState) => state.dataModeling.schema);
-  const filePath = useSelector(
-    (state: IServiceDevelopmentState) => getDataModelFileType(state.applicationMetadataState.applicationMetadata),
+  const dataModelName = useSelector(
+    (state: IServiceDevelopmentState) => getDataModelTypeName(state.applicationMetadataState.applicationMetadata),
   );
 
   React.useEffect(() => {
-    if (filePath) {
-      dispatch(setDataModelFilePath({ filePath }));
+    if (dataModelName) {
+      dispatch(setDataModelFilePath({ filePath: `App/models/${dataModelName}` }));
       dispatch(fetchDataModel({}));
     }
-  }, [dispatch, filePath]);
+  }, [dispatch, dataModelName]);
 
   const onSaveSchema = (schema: any) => {
     dispatch(saveDataModel({ schema }));
@@ -39,7 +35,7 @@ function DataModelingContainer(): JSX.Element {
     <SchemaEditorApp
       schema={jsonSchema || {}}
       onSaveSchema={onSaveSchema}
-      rootItemId='#/definitions/RA-0678_M'
+      rootItemId={`#/definitions/${dataModelName}`}
     />
   );
 }
