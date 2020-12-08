12fef1dc95f8f0ba8a308f734f2888821145f8a9
jeevananthank <jeevananthan.kesavan@accenture.com>
2020-12-08 16:55:57 +0100

    added tests and updated cypress

--- src/test/cypress/e2e/integration/studio/designer.js
+++ src/test/cypress/e2e/integration/studio/designer.js
@@ -2,8 +2,10 @@
 
 import * as designer from "../../pageobjects/designer";
 import * as dashboard from "../../pageobjects/dashboard";
+import * as header from "../../pageobjects/header";
+import * as common from "../../pageobjects/common";
 
-context.skip("Designer", () => {
+context("Designer", () => {
   before(() => {
     cy.visit("/");    
     cy.studiologin(Cypress.env("userName"), Cypress.env("userPwd"));
@@ -12,14 +14,30 @@ context.skip("Designer", () => {
     cy.contains(header.menuItem, "Logout").click();
   });
   beforeEach(() => {
-    cy.visit("/");    
+    cy.visit("/");
     cy.studiologin(Cypress.env("userName"), Cypress.env("userPwd"));
     cy.get(dashboard.searchApp).type("designer");
     cy.contains(common.gridItem, "designer").click();
   });
 
   it("About App", () => {
-    
+    cy.contains(designer.appHeader, "Om appen").should('be.visible');
+    cy.contains(common.button, "Endre").click();
+    cy.get(designer.appName).clear().type("New app name");
+    cy.get(designer.appDescription).clear().type("App description");
+    cy.get(designer.appName).invoke("val").should("contain", "New app name");
+    cy.get(designer.appDescription).invoke("val").should("contain", "App description");
+  });
+
+  it("UI Editor", () => {
+    cy.get(designer.appMenu["edit"]).click();
+    cy.get(common.leftDrawer).trigger("mouseover", { force: true });
+    cy.get(designer.appEditorMenu["datamodel"]).should("be.visible");
+    cy.get(common.leftDrawer).trigger("mouseout");
+    cy.get(designer.shortAnswer).parents(designer.draggable).trigger("dragstart");
+    cy.get(designer.dragToArea).parents(designer.draggable).trigger("drop");
+    cy.get(designer.dragToArea).parent().siblings().find(designer.draggable).first().click();
+    cy.get(designer.deleteComponent).parents("button").click();
   });
 
 });
--- src/test/cypress/e2e/pageobjects/designer.js
+++ src/test/cypress/e2e/pageobjects/designer.js
@@ -13,3 +13,29 @@ export const failedCheck = "i[class*='ai-circle-exclamation']";
 export const successCheck = "i[class*='ai-check-circle']";
 
 export const at22Deploys = "#deploy-history-table-at22";
+
+//About app
+export const appHeader = "#altinn-column-layout-header";
+export const appName = "#administrationInputServicename_textField";
+export const appDescription = "#administrationInputDescription_textField";
+export const deleteChanges = "#reset-repo-button";
+
+export const appEditorMenu = {
+    "datamodel": "a[href='#/datamodel']",
+    "uiEditor": "a[href='#/ui-editor']",
+    "accessControl": "a[href='#/accesscontrol']"
+};
+
+//UI components
+export const dragToArea = ".col-12";
+export const draggable = "div[draggable='true']";
+export const shortAnswer = "i[class^='fa fa-short-answer']";
+export const longtAnswer = "i[class^='fa fa-long-answer']";
+export const checkBox = "i[class^='fa fa-checkbox']";
+export const radioDutton = "i[class^='fa fa-radio-button']";
+export const dropDown = "i[class^='fa fa-drop-down']";
+export const attachment = "i[class^='fa fa-attachment']";
+export const date = "i[class^='fa fa-date']";
+export const formButton = "i[class^='fa fa-button']";
+
+export const deleteComponent = ".fa-circletrash";
