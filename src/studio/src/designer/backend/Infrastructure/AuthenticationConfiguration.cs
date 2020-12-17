ce4d86d7a9c3dac105f8ecf6f26abc9c73641d70
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2020-12-17 09:00:45 +0100

    Bug/5352 explicit redirect (#5375)
    * explicit redirect
    
    * using host from general settings
    
    * moved  changes to startup
    
    * DI environment
    
    * changes
    
    * undid old changes on branch
    
    * removed unsed variable
    
    * injecting env in startup
    
    * IHostingEnvironment -> IwebHostingEnv
    
    * updated redirect login
    
    * simplified code

--- src/studio/src/designer/backend/Infrastructure/AuthenticationConfiguration.cs
+++ src/studio/src/designer/backend/Infrastructure/AuthenticationConfiguration.cs
@@ -1,6 +1,10 @@
 using Altinn.Studio.Designer.Authorization;
+using Altinn.Studio.Designer.Configuration;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Altinn.Studio.Designer.Infrastructure
 {
@@ -13,21 +17,31 @@ namespace Altinn.Studio.Designer.Infrastructure
         /// Extension method that configures authentication
         /// </summary>
         /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection for adding services.</param>
-        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services)
+        /// <param name="config">The configuration</param>
+        /// <param name="env">The web hosting environment</param>
+        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration config, IWebHostEnvironment env)
         {
+            GeneralSettings generalSettings = config.GetSection("GeneralSettings").Get<GeneralSettings>();
+
+            string schema = env.IsDevelopment() ? "http://" : "https://";
+            string loginUrl = $"{schema}{generalSettings.HostName}/Home/Login/";
+
             // Configure Authentication
             // Use [Authorize] to require login on MVC Controller Actions
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                 {
                     options.AccessDeniedPath = "/Home/NotAuthorized/";
-                    options.LoginPath = "/Home/Login/";
                     options.LogoutPath = "/Home/Logout/";
                     options.Cookie.Name = Altinn.Studio.Designer.Constants.General.DesignerCookieName;
                     options.Events = new CookieAuthenticationEvents
                     {
                         // Add Custom Event handler to be able to redirect users for authentication upgrade
                         OnRedirectToAccessDenied = NotAuthorizedHandler.RedirectToNotAuthorized,
+                        OnRedirectToLogin = async (context) =>
+                        {
+                            context.HttpContext.Response.Redirect(loginUrl);
+                        }
                     };
                 });
 
