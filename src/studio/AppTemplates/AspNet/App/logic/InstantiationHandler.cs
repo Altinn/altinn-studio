fb36c70fa18080b2d712f4fcb4aa0e977122750f
Rune T. Larsen <rune@hjemmekino.no>
2020-12-18 15:26:11 +0100

    Updated nuget version. Will not buid before nugets are created

--- src/studio/AppTemplates/AspNet/App/logic/InstantiationHandler.cs
+++ src/studio/AppTemplates/AspNet/App/logic/InstantiationHandler.cs
@@ -1,21 +1,26 @@
+using System.Threading.Tasks;
 using Altinn.App.Services.Interface;
 using Altinn.App.Services.Models.Validation;
 using Altinn.Platform.Storage.Interface.Models;
-using System.Threading.Tasks;
-// using Altinn.App.Models; // Uncomment this line to refer to app model(s)
+
+//// using Altinn.App.Models; // Uncomment this line to refer to app model(s)
 
 namespace Altinn.App.AppLogic
 {
+    /// <summary>
+    /// Represents a business logic class responsible for running logic related to instantiation.
+    /// </summary>
     public class InstantiationHandler
     {
         private IProfile _profileService;
         private IRegister _registerService;
 
         /// <summary>
-        /// Set up access to profile and register services
+        /// Initialize a new instance of the <see cref="InstantiationHandler"/> class with services
+        /// that can be used to access profile and register information.
         /// </summary>
-        /// <param name="profileService"></param>
-        /// <param name="registerService"></param>
+        /// <param name="profileService">A service with access to profile information</param>
+        /// <param name="registerService">A service with access to register information</param>
         public InstantiationHandler(IProfile profileService, IRegister registerService)
         {
             _profileService = profileService;
@@ -32,8 +37,7 @@ namespace Altinn.App.AppLogic
         /// }
         /// return null;
         /// </example>
-        /// <param name="instance"></param>
-        /// <param name="validationResults"></param>
+        /// <param name="instance">The instance being validated</param>
         /// <returns>The validation result object (null if no errors) </returns>
         public async Task<InstantiationValidationResult> RunInstantiationValidation(Instance instance)
         {
