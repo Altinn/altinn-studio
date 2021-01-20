c5962438e0d3cd72619d007a8437622c0fc372d8
Kylstad, Nina <nkylstad@gmail.com>
2021-01-20 09:39:50 +0100

    update url to widget on cdn

--- src/studio/src/designer/frontend/ux-editor/utils/widgetSettings.ts
+++ src/studio/src/designer/frontend/ux-editor/utils/widgetSettings.ts
@@ -1,5 +1,5 @@
 export const widgetSettings = {
   widgetUrls: [
-    'https://raw.githubusercontent.com/Altinn/altinn-cdn/feature/5233-message-widget/altinn-apps/widgets/message.json',
+    'https://altinncdn.no/altinn-apps/widgets/message.json',
   ],
 };
