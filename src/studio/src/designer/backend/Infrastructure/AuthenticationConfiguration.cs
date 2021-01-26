ca80fcfcd4ffab77f70f5c54ee8f49c0f1e7eb95
Terje Holene <terje.holene@gmail.com>
2021-01-26 13:37:02 +0100

    Fixing await warning in NotAuthorizedHandler.
    (Mostly done to trigger a new build.)

--- src/studio/src/designer/backend/Infrastructure/AuthenticationConfiguration.cs
+++ src/studio/src/designer/backend/Infrastructure/AuthenticationConfiguration.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace Altinn.Studio.Designer.Infrastructure
 {
@@ -41,6 +42,7 @@ namespace Altinn.Studio.Designer.Infrastructure
                         OnRedirectToLogin = async (context) =>
                         {
                             context.HttpContext.Response.Redirect(loginUrl);
+                            await Task.CompletedTask;
                         }
                     };
                 });
