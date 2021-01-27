eaa9ce03742dff50c408ebae25ff2b8d45f2d420
Kylstad, Nina <nkylstad@gmail.com>
2021-01-27 13:41:37 +0100

    rename method

--- src/studio/src/designer/frontend/ux-editor/sagas/manageServiceConfiguration/index.ts
+++ src/studio/src/designer/frontend/ux-editor/sagas/manageServiceConfiguration/index.ts
@@ -1,9 +1,9 @@
 import { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
-import { watchFetchJsonFileSaga, watchSaveJsonFileSaga } from './manageServiceConfigurationSagas';
+import { watchFetchServiceConfigurationSaga, watchSaveJsonFileSaga } from '../../features/serviceConfigurations/manageServiceConfigurations/manageServiceConfigurationSagas';
 
 // eslint-disable-next-line func-names
 export default function* (): SagaIterator {
-  yield fork(watchFetchJsonFileSaga);
+  yield fork(watchFetchServiceConfigurationSaga);
   yield fork(watchSaveJsonFileSaga);
 }
