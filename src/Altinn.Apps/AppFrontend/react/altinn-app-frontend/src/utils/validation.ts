635d077e44ea75f4f9f2728d77fcd671b4719d24
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-01-27 14:44:02 +0100

    Fixed mapping to redux for rep groups + nested rep groups

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/utils/validation.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/utils/validation.ts
@@ -659,7 +659,7 @@ export function mapDataElementValidationToRedux(
         if (c.type === 'FileUpload') {
           return c.id === validation.field;
         }
-        return Object.values(c.dataModelBindings).includes(validation.field);
+        return Object.values(c.dataModelBindings).includes(getKeyWithoutIndex(validation.field));
       });
       return !!foundInLayout;
     });
