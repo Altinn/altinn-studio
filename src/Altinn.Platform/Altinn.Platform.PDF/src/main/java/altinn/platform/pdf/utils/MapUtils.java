f617dc4df424f9ad097903fa365ba3a5764d34d3
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-01-29 15:10:42 +0100

    more generic names

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/MapUtils.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/MapUtils.java
@@ -6,18 +6,18 @@ public class MapUtils {
   private MapUtils() {
   }
 
-  public static String getLabelFromValue(Map<String, Map<String,String>> optionsDictionary, String optionsId, String value){
+  public static String getLabelFromValue(Map<String, Map<String,String>> dict, String outerKey, String innerValue){
 
-    Map<String,String> options = optionsDictionary.get(optionsId);
+    Map<String,String> options = dict.get(outerKey);
 
     if(options != null){
       for (Map.Entry<String, String> entry :options.entrySet()) {
-        if(entry.getValue().equals((value))){
+        if(entry.getValue().equals((innerValue))){
           return entry.getKey();
         }
       }
     }
 
-    return value;
+    return innerValue;
   }
 }
