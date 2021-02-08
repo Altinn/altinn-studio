dd1ea62e53afd0c4b974a32a98db784a75a98719
Terje Holene <terje.holene@gmail.com>
2021-02-08 15:06:32 +0100

    Added ILabelSettings (#5581)
    LabelSettings can control the visibility of the optional label of all components.

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/components/Legend.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/components/Legend.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
+import { ILabelSettings } from 'src/types';
 import { getLanguageFromKey } from 'altinn-shared/utils';
 import Description from './Description';
 import { HelpTextContainer } from './HelpTextContainer';
@@ -9,6 +10,7 @@ export interface IFormLegendProps {
   descriptionText: string;
   language: any;
   required: boolean;
+  labelSettings?: ILabelSettings;
   helpText: string;
   id: string;
 }
@@ -25,7 +27,7 @@ export default function Legend(props: IFormLegendProps) {
         htmlFor={props.id}
       >
         {props.labelText}
-        {props.required ?
+        {(props.labelSettings?.optionalIndicator === false || props.required) ?
           null
           :
           <span className='label-optional'>
