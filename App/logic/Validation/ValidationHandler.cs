550567f300405f51f72781299039edee0b061dd7
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-02-16 17:35:24 +0100

    updated validation on app

--- App/logic/Validation/ValidationHandler.cs
+++ App/logic/Validation/ValidationHandler.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Altinn.Platform.Storage.Interface.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-// using Altinn.App.Models; // Uncomment this line to refer to app model(s)
+using Altinn.App.Models; // Uncomment this line to refer to app model(s)
 
 
 namespace Altinn.App.AppLogic.Validation
@@ -17,49 +17,25 @@ namespace Altinn.App.AppLogic.Validation
             _httpContextAccessor = httpContextAccessor;
         }
 
-        /// <summary>
-        /// Handles all custom validations that are not covered by the data model validation.
-        /// </summary>
-        /// <remarks>
-        /// Validations that fail should be handled by updating the validation result object,
-        /// see example.
-        /// </remarks>
-        /// <param name="validationResults">Object to contain any validation results</param>
-        /// <example>
-        /// if (instance.GetType() == typeof([model class name])
-        /// {
-        ///     // Explicitly cast instance to correct model type
-        ///     [model class name] model = ([model class name])object;
-        ///
-        ///     // Perform validations
-        ///     if ([some condition])
-        ///     {
-        ///         validationResults.Add(new ValidationResult([error message], new List<string>() {[affected field id]} ));
-        ///     }
-        /// }
-        /// </example>
         public async Task ValidateData(object data, ModelStateDictionary validationResults)
         {
+            if (data.GetType() == typeof(Skjema))
+            {
+                Skjema model = (Skjema)data;
+                string middleName = model?.NyttNavngrp9313?.NyttNavngrp9314?.PersonMellomnavnNyttdatadef34759?.value;
+                string firstName = model?.NyttNavngrp9313?.NyttNavngrp9314?.PersonFornavnNyttdatadef34758?.value;
+                if (!string.IsNullOrEmpty(middleName) && middleName.Contains("test"))
+                {
+                    validationResults.AddModelError("NyttNavn-grp-9313.NyttNavn-grp-9314.PersonMellomnavnNytt-datadef-34759.value", "*WARNING*test er ikke en gyldig verdi");
+                }
+                if (!string.IsNullOrEmpty(firstName) && firstName.Contains("test"))
+                {
+                    validationResults.AddModelError("NyttNavn-grp-9313.NyttNavn-grp-9314.PersonFornavnNytt-datadef-34758.value", "test er ikke en gyldig verdi");
+                }
+            }
             await Task.CompletedTask;
         }
 
-        /// <summary>
-        /// Handles all custom validation regarding the state of the instance in a given task
-        /// </summary>
-        /// Validations that fail should be handled by updating the validation result object,
-        /// see example.
-        /// </remarks>
-        /// <example>
-        ///  if (taskId.Equals("Task_2"))
-        ///  {
-        ///     DateTime deadline = ((DateTime)instance.Created).AddDays(3);
-        ///     if (DateTime.UtcNow<deadline)
-        ///         {
-        ///             validationResults.AddModelError("flyttemelding", $"Avslutting av task 2 er kun gyldig mellom 08-15 på hverdager.");
-        ///          }
-        ///   }
-
-        /// </example>
         public async Task ValidateTask(Instance instance, string taskId, ModelStateDictionary validationResults)
         {
             await Task.CompletedTask;
