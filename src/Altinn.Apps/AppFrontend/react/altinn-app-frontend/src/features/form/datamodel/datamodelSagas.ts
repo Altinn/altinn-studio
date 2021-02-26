c1bb80984ecebb9067563b1ff63080cff5ffb769
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-02-26 15:59:23 +0100

    Code smell

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/datamodel/datamodelSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/datamodel/datamodelSagas.ts
@@ -2,6 +2,6 @@ import { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 import { watchFetchJsonSchemaSaga } from './fetch/fetchFormDatamodelSagas';
 
-export default function* formDataModelSagas(): SagaIterator {
+export default function* datamodelSagas(): SagaIterator {
   yield fork(watchFetchJsonSchemaSaga);
 }
