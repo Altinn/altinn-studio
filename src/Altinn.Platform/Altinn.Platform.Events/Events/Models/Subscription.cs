14780ee13c5c824eebbec7531833440bb346530b
Rune Tømmerås Larsen <rune@hjemmekino.no>
2021-03-01 05:48:54 +0100

    Feature/4940 api for subscription (#5737)
    * Added initial controller
    
    * Updates
    
    * Test and logic updates
    
    * Added test with missing source
    
    * Added scripts
    
    * Updated with more checks and tests
    
    * Code comments
    
    * Added functionality to get a subscription
    
    * Added mock functionality
    
    * Added more tests
    
    * Update
    
    * Added services and logic to check authorization for user
    
    * Improved documentation
    
    * Added grants
    
    * Update after feedback. Renamed to subscription from eventssubscripton
    
    * Updated name on id
    
    * Updates after comments
    
    * Updates after comments
    
    * Updated afte new tests and coments
    
    * Fixed mock

---
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Models/Subscription.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Altinn.Platform.Events.Models
+{
+    /// <summary>
+    /// Class that describes a events subscriptions
+    /// </summary>
+    public class Subscription
+    {
+        /// <summary>
+        /// Endpoint to receive matching events
+        /// </summary>
+        public Uri EndPoint { get; set; }
+
+        /// <summary>
+        /// Subscription Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Filter on source
+        /// </summary>
+        public string SourceFilter { get; set; }
+
+        /// <summary>
+        /// Filter on subject
+        /// </summary>
+        public string SubjectFilter { get; set; }
+
+        /// <summary>
+        /// Filter on alternative subject
+        /// </summary>
+        public string AlternativeSubjectFilter { get; set; }
+
+        /// <summary>
+        /// Filter for type. Starts with
+        /// </summary>
+        public string TypeFilter { get; set; }
+
+        /// <summary>
+        /// The events consumer
+        /// </summary>
+        public string Consumer { get; set; }
+
+        /// <summary>
+        /// Who created this subscription
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// When subscription was created
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Serializes the cloud event to a JSON string.
+        /// </summary>
+        /// <returns>Serialized cloud event</returns>
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions { IgnoreNullValues = true });
+        }
+
+        /// <summary>
+        /// Deserializes the subscriont to from JSON string.
+        /// </summary>
+        /// <returns>Cloud event</returns>
+        public static Subscription Deserialize(string jsonString)
+        {
+            return JsonSerializer.Deserialize<Subscription>(jsonString, new JsonSerializerOptions { });
+        }
+    }
+}
