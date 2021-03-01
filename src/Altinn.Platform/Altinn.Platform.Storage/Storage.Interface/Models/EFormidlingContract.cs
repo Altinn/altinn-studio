a4a5d7320b5ee88a6dd465b3860e917930f4f8e1
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2021-03-01 10:07:22 +0100

    added eFormidling to application model (#5764)
    * added eFormidling to application model
    
    * updated comment

---
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage.Interface/Models/EFormidlingContract.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace Altinn.Platform.Storage.Interface.Models
+{
+    /// <summary>
+    /// Defines the eFormidling shipment contract related to an application.
+    /// </summary>
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class EFormidlingContract
+    {
+        /// <summary>
+        /// Service identifier for the process
+        /// </summary>
+        /// <remarks>
+        /// Available services are DPO, DPV, DPF and DPI.
+        /// </remarks>
+        public string ServiceId { get; set; }
+
+        /// <summary>
+        /// Org number for the receiver of the shipment. 
+        /// </summary>
+        /// <remarks>
+        /// Should only be included if a single static receiver.
+        /// </remarks>
+        public string Receiver { get; set; }
+
+        /// <summary>
+        /// Process type
+        /// </summary>
+        /// <remarks>
+        /// Receiver must be able to support the process type. This is defined in receiver's capabilities.
+        /// </remarks>
+        public string Process { get; set; }
+
+        /// <summary>
+        /// List of ids for the data types to include in the shipment.
+        /// </summary>
+        public List<string> DataTypes { get; set; }
+    }
+}
