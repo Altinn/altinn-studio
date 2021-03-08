dc244027ca617a3c2c0ca4152d8a782fc3e5a582
tba76 <thomabak@gmail.com>
2021-03-08 16:25:23 +0100

    Fixed compare-fail on subjectfilter

--- src/Altinn.Platform/Altinn.Platform.Events/Events/Services/SubscriptionService.cs
+++ src/Altinn.Platform/Altinn.Platform.Events/Events/Services/SubscriptionService.cs
@@ -51,8 +51,8 @@ namespace Altinn.Platform.Events.Services
             List<Subscription> searchresult = await _repository.GetSubscriptionsByConsumer("/org/%");
             return searchresult.Where(s =>
                 s.SourceFilter.Equals(source) &&
-                s.SubjectFilter.Equals(subject) &&
-                (s.TypeFilter == null || type.StartsWith(s.TypeFilter))).ToList();
+                (s.SubjectFilter == null || s.SubjectFilter.Equals(subject)) &&
+                (s.TypeFilter == null || s.TypeFilter.Equals(type))).ToList();
         }
 
         /// <inheritdoc/>
