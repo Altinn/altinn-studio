19de5a90f40f6f9e05bed74f3f2d386ee48e1b4d
Trond Tufte <trondtufte89@gmail.com>
2021-03-12 12:33:08 +0100

    Added class for unittest

---
+++ src/Altinn.Common/Altinn.EFormidlingClient/Altinn.EFormidlingClient.Tests/UnitTest/EFormidlingClientUnitTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Altinn.EFormidlingClient.Tests.UnitTest
+{
+    /// <summary>
+    /// Represents a collection of unit test, testing the<see cref="EFormidlingClientUnitTest"/> class.
+    /// </summary>
+    public class EFormidlingClientUnitTest
+    {
+        /// <summary>
+        /// Tests..
+        /// </summary>
+        [Fact]
+        public void Read_Test_Data()
+        {
+        }
+    }
+
+}
