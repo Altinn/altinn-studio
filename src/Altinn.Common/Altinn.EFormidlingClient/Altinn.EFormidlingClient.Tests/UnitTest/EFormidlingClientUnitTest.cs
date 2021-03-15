87ff7aa82a3d3b8f53e8fe89b1f3909c792899b1
Trond Tufte <trondtufte89@gmail.com>
2021-03-15 15:28:27 +0100

    Added more tests

--- src/Altinn.Common/Altinn.EFormidlingClient/Altinn.EFormidlingClient.Tests/UnitTest/EFormidlingClientUnitTest.cs
+++ src/Altinn.Common/Altinn.EFormidlingClient/Altinn.EFormidlingClient.Tests/UnitTest/EFormidlingClientUnitTest.cs
@@ -1,23 +1,43 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Xml.Serialization;
+using Altinn.Common.EFormidlingClient;
+using Altinn.Common.EFormidlingClient.Configuration;
 using Altinn.Common.EFormidlingClient.Models;
 using Altinn.Common.EFormidlingClient.Models.SBD;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
 using Xunit;
 using Arkivmelding = Altinn.Common.EFormidlingClient.Models.Arkivmelding;
 
-namespace Altinn.EFormidlingClient.Tests.UnitTest
+namespace Altinn.EFormidlingClient.Tests.ClientUnitTest
 {
     /// <summary>
     /// Represents a collection of unit test, testing the<see cref="EFormidlingClientUnitTest"/> class.
     /// </summary>
-    public class EFormidlingClientUnitTest
+    public class EFormidlingClientUnitTest : IClassFixture<FixtureUnit>
     {
+        private readonly ServiceProvider _serviceProvider;
+
         /// <summary>
-        /// Test valid sbd json 
+        /// Initializes a new instance of the <see cref="EFormidlingClientUnitTest"/> class.
+        /// </summary>
+        /// <param name="fixture">Fixture for setup</param>
+        public EFormidlingClientUnitTest(FixtureUnit fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        /// <summary>
+        /// Test valid sbd json from file
         /// </summary>
         [Fact]
         public void Is_Valid_Json()
@@ -29,7 +49,7 @@ namespace Altinn.EFormidlingClient.Tests.UnitTest
         }
 
         /// <summary>
-        /// Test invalid sbd json
+        /// Test invalid sbd json from file
         /// </summary>
         [Fact]
         public void Is_Not_Valid_Json()
@@ -41,7 +61,7 @@ namespace Altinn.EFormidlingClient.Tests.UnitTest
         }
 
         /// <summary>
-        /// Test valid xml arkivmelding
+        /// Test valid xml arkivmelding from file
         /// </summary>
         [Fact]
         public void Is_Valid_Xml()
@@ -56,15 +76,127 @@ namespace Altinn.EFormidlingClient.Tests.UnitTest
         }
 
         /// <summary>
-        /// Tests
+        /// Tests not empty file arkivmelding
         /// </summary>
         [Fact]
-        public void Read_XML_Test_Data()
+        public void Read_Not_Empty_XML_Test_Data()
         {
             using (FileStream fs = File.OpenRead(@"TestData\arkivmelding.xml"))
             {
                 Assert.True(fs.Length > 0);
             }
+        }
+
+        /// <summary>
+        /// Tests valid config in appsettings
+        /// </summary>
+        [Fact]
+        public void Check_Valid_AppConfig()
+        {
+            var config = FixtureUnit.InitConfiguration();
+            var baseUrlSetting = config["EFormidlingClientSettings:BaseUrl"];
+            var baseUrlLocal = "http://localhost:9093/api/";
+
+            Assert.Equal(baseUrlLocal, baseUrlSetting);
+        }
+
+        /// <summary>
+        /// Tests invalid input to GetCapabilities()
+        /// </summary>
+        [Fact]
+        public async void Get_Capabilities_Invalid_Input()
+        {
+            var service = _serviceProvider.GetService<IEFormidlingClient>();
+            ArgumentNullException ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.GetCapabilities(string.Empty));
+        }
+
+        /// <summary>
+        /// Tests invald input to UploadAttachment()
+        /// </summary>
+        [Fact]
+        public async void Upload_Attachment_Invalid_Input()
+        {
+            var service = _serviceProvider.GetService<IEFormidlingClient>();
+            ArgumentNullException ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.UploadAttachment(null, string.Empty, string.Empty));
+        }
+
+        /// <summary>
+        /// Tests invalid input to CreateMessage()
+        /// </summary>
+        [Fact]
+        public async void Create_Message_Invalid_Input()
+        {
+            var service = _serviceProvider.GetService<IEFormidlingClient>();
+            ArgumentNullException ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.CreateMessage(null));
+        }
+
+        /// <summary>
+        /// Tests invalid input to SubscribeeFormiding()
+        /// </summary>
+        [Fact]
+        public async void SubscribeeFormidling_Invalid_Input()
+        {
+            var service = _serviceProvider.GetService<IEFormidlingClient>();
+            ArgumentNullException ex = await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.SubscribeeFormidling(null));
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FixtureUnit"/> class.
+    /// </summary>
+    public class FixtureUnit
+    {
+        /// <summary>
+        ///  Gets the ServiceProvider
+        /// </summary>
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        /// <summary>
+        ///  Gets the CustomGuid
+        /// </summary>
+        public string CustomGuid { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FixtureUnit"/> class.
+        /// </summary>
+        public FixtureUnit()
+        {
+            var serviceCollection = new ServiceCollection();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceCollection.Configure<EFormidlingClientSettings>(configuration.GetSection("EFormidlingClientSettings"));
+            serviceCollection.AddLogging(config =>
+            {
+                config.AddDebug();
+                config.AddConsole();
+            })
+                 .Configure<LoggerFilterOptions>(options =>
+                 {
+                     options.AddFilter<DebugLoggerProvider>(null, LogLevel.Debug);
+                     options.AddFilter<ConsoleLoggerProvider>(null, LogLevel.Debug);
+                 });
+
+            serviceCollection.AddTransient<HttpClient>();
+            _ = serviceCollection.AddTransient<IEFormidlingClient, Common.EFormidlingClient.EFormidlingClient>();
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            Guid obj = Guid.NewGuid();
+            CustomGuid = obj.ToString();
+        }
+
+        /// <summary>
+        ///  Gets the CustomGuid
+        /// </summary>
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return config;
         }
     }
 }
