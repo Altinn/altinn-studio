177dfab9c570e4dcd2c646e9596653545343c561
Trond Tufte <trondtufte89@gmail.com>
2021-03-16 22:24:05 +0100

    Added namespace to 'type' in basisregistrering

--- src/Altinn.Common/Altinn.EFormidlingClient/Altinn.EFormidlingClient/Models/Arkivmelding.cs
+++ src/Altinn.Common/Altinn.EFormidlingClient/Altinn.EFormidlingClient/Models/Arkivmelding.cs
@@ -276,7 +276,8 @@ namespace Altinn.Common.EFormidlingClient.Models
         /// <summary>
         ///  Gets or sets the Type
         /// </summary>
-        [XmlAttribute(AttributeName = "type")]
+        //[XmlAttribute(AttributeName = "type")]
+        [XmlAttribute(AttributeName = "type", Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
         public string Type { get; set; }
 
         /// <summary>
