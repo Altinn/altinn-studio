1ccc784dc31971e88068393cd1504ec9b0465565
Terje Holene <terje.holene@gmail.com>
2021-03-18 13:54:46 +0100

    Cleaning of filename coming from content-disposition

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Common/Helpers/Extensions/StringExtensions.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Common/Helpers/Extensions/StringExtensions.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
 
-namespace Altinn.App.Services.Helpers.Extensions
+namespace Altinn.App.Common.Helpers.Extensions
 {
     /// <summary>
     /// Extensions to facilitate sanitization of string values
@@ -9,11 +9,11 @@ namespace Altinn.App.Services.Helpers.Extensions
     public static class StringExtensions
     {
         /// <summary>
-        /// Sanitize the input as a file name. 
+        /// Sanitize the input as a file name.
         /// </summary>
-        /// <param name="input">The input variable to be sanitized</param>
-        /// <param name="throwExceptionOnInvalidCharacters">Throw exception instead of replacing invalid characters with '-'</param>
-        /// <returns></returns>
+        /// <param name="input">The input variable to be sanitized.</param>
+        /// <param name="throwExceptionOnInvalidCharacters">Throw exception instead of replacing invalid characters with '-'.</param>
+        /// <returns>A filename cleaned of any impurities.</returns>
         public static string AsFileName(this string input, bool throwExceptionOnInvalidCharacters = true)
         {
             if (string.IsNullOrWhiteSpace(input))
@@ -25,7 +25,7 @@ namespace Altinn.App.Services.Helpers.Extensions
             if (throwExceptionOnInvalidCharacters)
             {
                 if (illegalFileNameCharacters.Any(ic => input.Any(i => ic == i)))
-                {                    
+                {
                     throw new ArgumentOutOfRangeException(nameof(input));
                 }
 
