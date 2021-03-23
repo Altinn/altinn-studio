ae35f9774740555363e18ffe105a70dcc315b2ac
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-03-23 14:27:21 +0100

    Added support for help text in paragraph and header components

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/ParagraphComponent.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/ParagraphComponent.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+import { ITextResourceBindings } from 'src/features/form/layout';
+import { HelpTextContainer } from 'src/features/form/components/HelpTextContainer';
 
 export interface IParagraphProps {
   id: string;
   text: string;
+  textResourceBindings: ITextResourceBindings;
+  language: any;
+  getTextResource: (key: string) => string;
 }
 
 export function ParagraphComponent(props: IParagraphProps) {
@@ -13,11 +18,24 @@ export function ParagraphComponent(props: IParagraphProps) {
   };
 
   return (
-    <Typography
-      id={props.id}
-      style={style}
-    >
-      {props.text}
-    </Typography>
+    <Grid container={true} direction='row'>
+      <Grid item={true}>
+        <Typography
+          id={props.id}
+          style={style}
+        >
+          {props.text}
+        </Typography>
+      </Grid>
+      {props.textResourceBindings?.help &&
+      <Grid item={true} style={style}>
+        <HelpTextContainer
+          language={props.language}
+          id={props.id}
+          helpText={props.getTextResource(props.textResourceBindings.help)}
+        />
+      </Grid>
+      }
+    </Grid>
   );
 }
