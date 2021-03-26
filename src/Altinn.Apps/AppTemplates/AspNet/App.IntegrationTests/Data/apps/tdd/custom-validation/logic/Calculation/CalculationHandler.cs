5d221005f41754f0a226f439b67b2053efef9a81
Nina Kylstad <nkylstad@gmail.com>
2021-03-26 11:40:48 +0100

    updated DataController PUT method (#5943)
    * updated DataController PUT method
    
    * update comment
    
    * update package version and publish test version

--- src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Data/apps/tdd/custom-validation/logic/Calculation/CalculationHandler.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Data/apps/tdd/custom-validation/logic/Calculation/CalculationHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 
 #pragma warning disable SA1300 // Element should begin with upper-case letter
@@ -23,8 +24,34 @@ namespace App.IntegrationTests.Mocks.Apps.tdd.custom_validation
                 if (journalnummer != null && journalnummer == 1000)
                 {
                     model.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.Journalnummerdatadef33316.value = (decimal)journalnummer + 1;
+                    model.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317 = new IdentifikasjonsnummerKravdatadef33317
+                    {
+                        orid = 33317,
+                        value = "12345",
+                    };
                     changed = true;
                 }
+                else if (journalnummer != null && journalnummer == 1001)
+                {
+                    model.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.Journalnummerdatadef33316.value = (decimal)journalnummer - 1;
+                    model.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.TestRepeatinggrp123 = new List<Journalnummerdatadef33316>
+                    {
+                        new Journalnummerdatadef33316
+                        {
+                            orid = 1234,
+                            value = 555,
+                        },
+                        new Journalnummerdatadef33316
+                        {
+                            orid = 1234,
+                            value = 444,
+                        },
+                    };
+
+                    model.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317 = null;
+                    changed = true;
+                }
+
             }
 
             return changed;
