8d453d081f7f68523f6026c3baf7cfef5b37822f
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-04-07 17:02:56 +0200

    removed logging

--- src/test/cypress/e2e/integration/app-frontend/message.js
+++ src/test/cypress/e2e/integration/app-frontend/message.js
@@ -23,7 +23,6 @@ describe('Message', () => {
         data.instanceGuid = instanceId;
         instanceMetadata.data.push(data);
       });
-      cy.log(instanceMetadata);
       cy.intercept('GET', /[0-9]+\/*[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i, instanceMetadata);
     });
     cy.reload();
