dfead6bb45d0e177d3dc21129f6098437be47622
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-04-08 09:10:17 +0200

    removed sockjs mock

--- src/test/cypress/e2e/support/index.js
+++ src/test/cypress/e2e/support/index.js
@@ -8,17 +8,6 @@ import 'cypress-plugin-tab'
 import './start-app-instance'
 
 before(() => {
-  if (Cypress.env('environment') != "local") {
-    cy.intercept('GET', '/sockjs-node/*', {
-      statusCode: 200,
-      body: {
-        "websocket": false,
-        "origins": ["*:*"],
-        "cookie_needed": false,
-        "entropy": 568571552
-      }
-    });
-  };
   Cypress.on('uncaught:exception', (e, runnable) => {
     console.log('error', e)
     console.log('runnable', runnable)
