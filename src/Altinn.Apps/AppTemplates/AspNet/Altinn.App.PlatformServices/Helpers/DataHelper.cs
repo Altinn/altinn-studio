1b7293f0797a020bf63be16629d409f7c230d099
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-04-22 11:37:22 +0200

    added null value handling

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Helpers/DataHelper.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.PlatformServices/Helpers/DataHelper.cs
@@ -115,7 +115,7 @@ namespace Altinn.App.Services.Helpers
                 object propertyValue = property.GetValue(data, null);
                 if (isLastKey)
                 {
-                    return propertyValue.ToString();
+                    return propertyValue == null ? (string)propertyValue : propertyValue.ToString();
                 }
                 else
                 {
