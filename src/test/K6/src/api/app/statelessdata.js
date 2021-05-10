6837d0ff8dcaef921f3046bb80bb3e7054a61afa
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-05-10 16:44:12 +0200

    test implementation complete

--- src/test/K6/src/api/app/statelessdata.js
+++ src/test/K6/src/api/app/statelessdata.js
@@ -6,9 +6,8 @@ import * as header from "../../buildrequestheaders.js"
 export function postData(altinnStudioRuntimeCookie, dataType, appOwner, appName) {
     var endpoint = config.appApiBaseUrl(appOwner, appName) + config.buildAppApiUrls("", "", "", dataType, "statelessdata")
     var params = header.buildHearderWithRuntime(altinnStudioRuntimeCookie, "");    
-    return http.post(endpoint, data, params);
+    return http.post(endpoint, null, params);
 };
-
 
 //Api call to App Api:Data to edit a data by id of an app instance and returns response
 export function putDataByType(altinnStudioRuntimeCookie, dataType, data, appOwner, appName) {
--- src/test/K6/src/tests/app/statelessdata.js
+++ src/test/K6/src/tests/app/statelessdata.js
@@ -1,21 +1,20 @@
 /* 
   Test data required: username, password, app requiring level 2 login (reference app: ttd/apps-test)
-  command to run the test: docker-compose run k6 run /src/tests/app/statelessdata.js 
-  -e env=*** -e org=*** -e username=*** -e userpwd=*** -e level2app=***
+  command to run the test: 
+  docker-compose run k6 run /src/tests/app/statelessdata.js -e env=*** -e org=*** -e username=*** -e userpwd=*** -e level2app=*** -e appsaccesskey=***  
 */
 
 import { check } from "k6";
 import { addErrorCount } from "../../errorcounter.js";
-import * as stateless from "../../api/app/statelessdata"
+import * as stateless from  "../../api/app/statelessdata.js"
 import * as setUpData from "../../setup.js";
+import { generateJUnitXML, reportPath } from "../../report.js"
 
 const userName = __ENV.username;
 const userPassword = __ENV.userpwd;
 const appOwner = __ENV.org;
 const level2App = __ENV.level2app;
 let instanceFormDataXml = open("../../data/" + level2App + ".xml");
-let pdfAttachment = open("../../data/test_file_pdf.pdf", "b");
-
 
 export const options = {
     thresholds: {
@@ -38,19 +37,31 @@ export function setup() {
 //Tests for App API: Stateless data
 export default function (data) {
     const runtimeToken = data["RuntimeToken"];
+    var res, success; 
 
-    //Test to runs calculations on the provided data object and validate the data in the response
+    //Test to run calculations on the provided data object and validate the data in the response
     res = stateless.putDataByType(runtimeToken, "default", instanceFormDataXml, appOwner, level2App);
     success = check(res, {
-        "PUT Edit Data by Id status is 200:": (r) => r.status === 200
+        "PUT update stateless data object status is 200:": (r) => r.status === 200,
+        "PUT update stateless data object data field is updated": (r) => r.json("OpplysningerOmArbeidstakeren-grp-8819.Skjemainstans-grp-8854.IdentifikasjonsnummerKrav-datadef-33317.value") == "1705"    
 
         // parse data to ensure calculation has run.
     });
     addErrorCount(success);
+
+    //Test to create a new data object for a stateless app
+    res = stateless.postData(runtimeToken, "default", appOwner, level2App);
+    success = check(res, {
+        "POST create new stateless data object by type status is 200:": (r) => r.status === 200,
+        "POST create new stateless data object by type response is not empty": (r) => r.json("OpplysningerOmArbeidstakeren-grp-8819.Skjemainstans-grp-8854.IdentifikasjonsnummerKrav-datadef-33317.value") == "1234567890"    
+    });
+    
+   console.log(JSON.stringify(res.body));
+    addErrorCount(success);    
 };
 
 export function handleSummary(data) {
     let result = {};
-    result[reportPath("subscriptions")] = generateJUnitXML(data, "platform-events-subscription");
+    result[reportPath("statelessdata")] = generateJUnitXML(data, "app-statelessdata");
     return result;
 };
