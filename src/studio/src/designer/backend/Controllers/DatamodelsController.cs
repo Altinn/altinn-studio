e0761ca335639fce681ae9dbae99d3939d36336f
Kylstad, Nina <nkylstad@gmail.com>
2021-05-10 11:03:05 +0200

    remove unused import

--- src/studio/src/designer/backend/Controllers/DatamodelsController.cs
+++ src/studio/src/designer/backend/Controllers/DatamodelsController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Schema;
 
 using Altinn.Studio.Designer.Factories.ModelFactory;
