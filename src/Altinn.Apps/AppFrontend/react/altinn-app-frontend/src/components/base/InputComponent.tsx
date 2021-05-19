bf4bbb0eecbb73acc9062a1b33c9b3fb7c074add
Kylstad, Nina <nkylstad@gmail.com>
2021-05-19 15:26:36 +0200

    implement number formatting for input component

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/InputComponent.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/components/base/InputComponent.tsx
@@ -1,20 +1,76 @@
 import * as React from 'react';
 import '../../styles/shared.css';
 import classNames from 'classnames';
+import NumberFormat from 'react-number-format';
+import { Input } from '@material-ui/core';
 
-export interface IInputProps {
+export interface IInputBaseProps {
   id: string;
   readOnly: boolean;
   required: boolean;
-  formData: any;
+  formatting?: any;
   handleDataChange: (value: any) => void;
-  isValid?: boolean;
-  type?: string;
-  title?: string;
+}
+
+export interface IInputProps extends IInputBaseProps {
+  formData: any;
+  isValid?: boolean
+}
+
+export interface IBasicInputProps extends IInputBaseProps {
+  onDataChangeSubmit: () => void;
+  onDataChanged: (e: any) => void;
+  value: string;
+}
+
+export interface IFormattedNumberInputProps extends IInputBaseProps {
+  inputRef: ((el: HTMLInputElement) => void) | React.Ref<any>;
+  name: any;
+  onChange: (e: any) => void;
+}
+
+function NumberFormatCustom(props: IFormattedNumberInputProps) {
+  const {
+    inputRef,
+    onChange,
+    formatting,
+    ...rest
+  } = props;
+
+  return (
+    <NumberFormat
+      {...rest}
+      getInputRef={inputRef}
+      data-testid={`${props.id}-formatted-number`}
+      onValueChange={(values) => {
+        onChange({
+          target: {
+            name: props.name,
+            value: values.value,
+          },
+        });
+      }}
+      {...formatting}
+    />
+  );
+}
+
+export function BasicInputComponent(props: IBasicInputProps) {
+  const { formatting, ...rest } = props;
+  return (
+    <>
+      <input
+        data-testid={props.id}
+        {...rest}
+        {...formatting}
+      />
+    </>
+  );
 }
 
 export function InputComponent(props: IInputProps) {
   const [value, setValue] = React.useState(props.formData ? props.formData : '');
+  const { number, ...formatting } = props.formatting || {};
 
   React.useEffect(() => {
     setValue(props.formData ? props.formData : '');
@@ -29,21 +85,19 @@ export function InputComponent(props: IInputProps) {
   };
 
   return (
-    <>
-      <input
-        key={`input_${props.id}`}
-        id={props.id}
-        type={props.type}
-        onBlur={onDataChangeSubmit}
-        onChange={onDataChanged}
-        readOnly={props.readOnly}
-        required={props.required}
-        className={classNames('form-control',
-          { 'validation-error': !props.isValid, disabled: props.readOnly })}
-        value={value}
-        data-testid={props.id}
-        aria-describedby={`description-${props.id}`}
-      />
-    </>
+    <Input
+      key={`input_${props.id}`}
+      id={props.id}
+      onBlur={onDataChangeSubmit}
+      onChange={onDataChanged}
+      readOnly={props.readOnly}
+      required={props.required}
+      className={classNames('form-control',
+        { 'validation-error': !props.isValid, disabled: props.readOnly })}
+      value={value}
+      aria-describedby={`description-${props.id}`}
+      inputComponent={number ? NumberFormatCustom : BasicInputComponent}
+      inputProps={{ formatting }}
+    />
   );
 }
