b51c8cd589e7216483f2f3a4558f4bd568340af1
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-05-20 09:40:15 +0200

    testing report path logic

--- src/test/K6/src/report.js
+++ src/test/K6/src/report.js
@@ -49,7 +49,7 @@ export function generateJUnitXML(data, suiteName) {
  */
 export function reportPath(reportName) {
   var path = `src/reports/${reportName}.xml`;
-  console.log(__ENV.HOME);
-  if (__ENV.HOME && __ENV.HOME.includes('/home/k6')) path = `/${path}`;
+  console.log(JSON.stringify(__ENV));
+  if (!__ENV.OS) path = `/${path}`;
   return path;
 }
