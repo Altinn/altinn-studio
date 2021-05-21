19790bd1807ab5de6d23bb3fec5b5b70f5ef9ee8
Eirik Kvarstein <eirik.kvarstein@adera.no>
2021-05-21 14:50:40 +0200

    removed comment

--- src/studio/src/designer/frontend/packages/schema-editor/src/features/editor/schemaEditorSlice.ts
+++ src/studio/src/designer/frontend/packages/schema-editor/src/features/editor/schemaEditorSlice.ts
@@ -97,7 +97,6 @@ const schemaEditorSlice = createSlice({
         const index = path.lastIndexOf('/properties/');
         const parentPath = path.substring(0, index);
         const propertyName = path.substring(index + 12);
-        console.log(`parent: ${parentPath} property: ${propertyName}`);
         const removeFromItem = getUiSchemaItem(state.uiSchema, parentPath);
         if (removeFromItem) {
           const removeIndex = removeFromItem
