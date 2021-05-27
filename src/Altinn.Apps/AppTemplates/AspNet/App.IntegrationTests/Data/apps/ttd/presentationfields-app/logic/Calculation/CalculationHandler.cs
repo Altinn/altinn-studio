5bdb7b5bc8079c0991f828a456c54350cbe6d294
Rune T. Larsen <rune@hjemmekino.no>
2021-05-27 18:43:10 +0200

    updates

--- src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Data/apps/ttd/presentationfields-app/logic/Calculation/CalculationHandler.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/App.IntegrationTests/Data/apps/ttd/presentationfields-app/logic/Calculation/CalculationHandler.cs
@@ -22,6 +22,7 @@ namespace App.IntegrationTests.Mocks.Apps.Ttd.PresentationTextsApp
         /// }
         /// </example>
         /// <param name="instance">The data</param>
+        /// <param name="altinnAppContext">The context</param>
         public static async Task<bool> Calculate(object instance, IAltinnAppContext altinnAppContext)
         {
             AltinnAppContext appContext = altinnAppContext.GetContext();
