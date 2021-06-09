68f721a712d6ff13bbdef79f7dc53dffb3941138
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-06-09 10:10:22 +0200

    nuget 4.7.0-alpha

--- App/logic/Calculation/CalculationHandler.cs
+++ App/logic/Calculation/CalculationHandler.cs
@@ -3,39 +3,48 @@ using Altinn.App.Models; // <-- Uncomment this line to refer to app model(s)
 
 namespace Altinn.App.AppLogic.Calculation
 {
-    /// <summary>
-    /// Represents a business logic class responsible for running calculations on an instance.
-    /// </summary>
-    public class CalculationHandler
-    {        
-        public async Task<bool> Calculate(object instance)
+  /// <summary>
+  /// Represents a business logic class responsible for running calculations on an instance.
+  /// </summary>
+  public class CalculationHandler
+  {
+    public async Task<bool> Calculate(object instance)
+    {
+      if (instance.GetType() != typeof(Skjema))
+      {
+        return false;
+      }
+
+      Skjema skjema = (Skjema)instance;
+
+      if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == null)
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819 = skjema.OpplysningerOmArbeidstakerengrp8819 ?? new OpplysningerOmArbeidstakerengrp8819();
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 = new Skjemainstansgrp8854()
         {
-            if (instance.GetType() != typeof(Skjema))
-            {
-                return false;
-            }
+          IdentifikasjonsnummerKravdatadef33317 = new IdentifikasjonsnummerKravdatadef33317()
+          {
+            value = "1234567890"
+          }
+        };
 
-            Skjema skjema = (Skjema)instance;
-
-            if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == null)
-            {
-                skjema.OpplysningerOmArbeidstakerengrp8819 = skjema.OpplysningerOmArbeidstakerengrp8819 ?? new OpplysningerOmArbeidstakerengrp8819();
-                skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 = new Skjemainstansgrp8854()
-                {
-                    IdentifikasjonsnummerKravdatadef33317 = new IdentifikasjonsnummerKravdatadef33317()
-                    {
-                        value = "1234567890"
-                    }
-                };
-
-            }
-            else if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == "1337")
-            {
-                skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317.value = "1705";
-            }
-
-            return await Task.FromResult(true);
-        }           
-        
+      }
+      else if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == "1337")
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317.value = "1705";
+      }
+      if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.Journalnummerdatadef33316?.value == null)
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 = new Skjemainstansgrp8854()
+        {
+          Journalnummerdatadef33316 = new Journalnummerdatadef33316()
+          {
+            value = 1364
+          }
+        };
+      }
+      return await Task.FromResult(true);
     }
+
+  }
 }
