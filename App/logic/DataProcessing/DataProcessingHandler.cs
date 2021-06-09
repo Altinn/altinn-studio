d7544ce029a0d1ad0f69b19aa22f3cfd75485020
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-06-09 12:54:14 +0200

    added dataprocessing and removed calculation

---
+++ App/logic/DataProcessing/DataProcessingHandler.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Threading.Tasks;
+using Altinn.Platform.Storage.Interface.Models;
+
+using Altinn.App.Models;
+
+namespace Altinn.App.AppLogic.DataProcessing
+{
+  /// <summary>
+  /// Represents a business logic class responsible for running calculations on an instance.
+  /// </summary>
+  public class DataProcessingHandler
+  {
+    /// <summary>
+    /// Perform data processing on data read. When reading data from App API
+    /// </summary>
+    /// <example>
+    /// if (data.GetType() == typeof(Skjema)
+    /// {
+    ///     Skjema model = (Skjema)data;
+    ///     // Perform calculations and manipulation of data model here
+    /// }
+    /// </example>
+    /// <param name="instance">The instance that data belongs to</param>
+    /// <param name="dataId">The dataId for data if available</param>
+    /// <param name="data">The data as object</param>
+    public async Task<bool> ProcessDataRead(Instance instance, Guid? dataId, object data)
+    {
+      return await Task.FromResult(false);
+    }
+
+    /// <summary>
+    /// Perform data processing on data write. When posting and putting data against app
+    /// </summary>
+    /// <example>
+    /// if (data.GetType() == typeof(Skjema)
+    /// {
+    ///     Skjema model = (Skjema)data;
+    ///     // Perform calculations and manipulation of data model here
+    /// }
+    /// </example>
+    /// <param name="instance">The instance that data belongs to</param>
+    /// <param name="dataId">The dataId for data if available</param>
+    /// <param name="data">The data as object</param>
+    public async Task<bool> ProcessDataWrite(Instance instance, Guid? dataId, object data)
+    {
+      if (data.GetType() != typeof(Skjema))
+      {
+        return false;
+      }
+      Skjema skjema = (Skjema)data;
+
+      if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == null)
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819 = skjema.OpplysningerOmArbeidstakerengrp8819 ?? new OpplysningerOmArbeidstakerengrp8819();
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 = new Skjemainstansgrp8854()
+        {
+          IdentifikasjonsnummerKravdatadef33317 = new IdentifikasjonsnummerKravdatadef33317()
+          {
+            value = "1234567890"
+          }
+        };
+
+      }
+      else if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.IdentifikasjonsnummerKravdatadef33317?.value == "1337")
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317.value = "1705";
+      }
+      if (skjema?.OpplysningerOmArbeidstakerengrp8819?.Skjemainstansgrp8854?.Journalnummerdatadef33316?.value == null)
+      {
+        skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 = new Skjemainstansgrp8854()
+        {
+          Journalnummerdatadef33316 = new Journalnummerdatadef33316()
+          {
+            value = 1364
+          }
+        };
+      }
+      return await Task.FromResult(true);
+    }
+  }
+}
