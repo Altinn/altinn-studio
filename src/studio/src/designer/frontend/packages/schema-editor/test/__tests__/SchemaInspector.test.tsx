d375a37cac9cf04a7e5985b31547d654ea902d00
Eirik Kvarstein <eirik.kvarstein@adera.no>
2021-06-11 11:16:14 +0200

    Removed unused import.

--- src/studio/src/designer/frontend/packages/schema-editor/test/__tests__/SchemaInspector.test.tsx
+++ src/studio/src/designer/frontend/packages/schema-editor/test/__tests__/SchemaInspector.test.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { mount, ReactWrapper } from 'enzyme';
+import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import { Autocomplete } from '@material-ui/lab';
 import SchemaInspector from '../../src/components/SchemaInspector';
