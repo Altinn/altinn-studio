32f98e4fde56c073503f80bb3383c9700be467c2
Terje Holene <terje.holene@gmail.com>
2021-06-15 21:02:52 +0200

    Added DelegatingHandleerStub

---
+++ src/Altinn.Platform/Altinn.Platform.Profile/UnitTests/IntegrationTests/Utils/DelegatingHandlerStub.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Altinn.Platform.Profile.Tests.IntegrationTests.Utils
+{
+    public class DelegatingHandlerStub : DelegatingHandler
+    {
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+
+        public DelegatingHandlerStub()
+        {
+            _handlerFunc = (request, cancellationToken) => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+        }
+
+        public DelegatingHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
+        {
+            _handlerFunc = handlerFunc;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _handlerFunc(request, cancellationToken);
+        }
+    }
+}
