ba30b6d38d9754fda37349b976d2c88388f9a40c
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-06-18 10:37:10 +0200

    Fixed encoding on response

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/AltinnOrgUtils.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/AltinnOrgUtils.java
@@ -3,12 +3,15 @@ package altinn.platform.pdf.utils;
 import altinn.platform.pdf.models.AltinnOrg;
 import altinn.platform.pdf.models.AltinnOrgs;
 import altinn.platform.pdf.services.BasicLogger;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -24,13 +27,13 @@ public class AltinnOrgUtils {
    */
   public static String getOrgFullNameByShortName(String shortName, String language) {
     if (altinnOrgs == null || altinnOrgs.getOrgs() == null) {
-      return "";
+      return shortName;
     }
 
     AltinnOrg org = altinnOrgs.getOrgs().get(shortName);
 
     if (org == null) {
-      return "";
+      return shortName;
     }
 
     return org.getName().get(language);
@@ -40,22 +43,22 @@ public class AltinnOrgUtils {
    * Fetches the altinn orgs form our cdn
    */
   public static void fetchAltinnOrgs() {
+    HttpURLConnection connection = null;
     try {
       URL url = new URL("https://altinncdn.no/orgs/altinn-orgs.json");
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      String inputLine;
-      StringBuilder content = new StringBuilder();
-      while ((inputLine = in.readLine()) != null) {
-        content.append(inputLine);
+      connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")))) {
+          ObjectMapper mapper = new ObjectMapper();
+          mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+          altinnOrgs = mapper.readValue(in, AltinnOrgs.class);
       }
-      in.close();
-      Gson gson = new Gson();
-      altinnOrgs = gson.fromJson(content.toString(), AltinnOrgs.class);
-      con.disconnect();
     } catch (Exception e) {
       BasicLogger.log(Level.SEVERE, e.getMessage());
+    } finally {
+      if (connection != null) {
+        connection.disconnect();
+      }
     }
   }
 
