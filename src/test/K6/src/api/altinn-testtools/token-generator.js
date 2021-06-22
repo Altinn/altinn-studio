60efb260699ff1790ff2c3d1fa37d81d2164a4d8
Jeevananthan Kesavan <jeevananthan.kesavan@accenture.com>
2021-06-22 16:43:09 +0200

    support for getting personal token

--- src/test/K6/src/api/altinn-testtools/token-generator.js
+++ src/test/K6/src/api/altinn-testtools/token-generator.js
@@ -4,16 +4,30 @@ import * as config from '../../config.js';
 import * as support from '../../support.js';
 import { stopIterationOnFail } from '../../errorcounter.js';
 
-//Request to generate pdf from a json and returns the response
-export function getEnterpriseToken(userName, userPwd, queryParams) {
+/**
+ * only for test environments - api call to get enterprise or personal token with requested scopes and other params
+ * @param {string} tokenFor enterprise or personal
+ * @param {string} userName user name for basic authentication 
+ * @param {string} userPwd password for basic authentication
+ * @param {JSON} queryParams for enterprise: { env: 'at22', scopes: 'altinn:serviceowner/instances.read', org: 'ttd', orgNo: '991825827', }
+ * for personal: { env: 'at22', scopes: 'altinn:instances.read', userId: 123, partyId: 123, authLvl: 3, pid: '11 digit ssn', }
+ * @returns jwt token for enterprise or person with supplied params
+ */
+export function generateToken(tokenFor, userName, userPwd, queryParams) {
   const credentials = `${userName}:${userPwd}`;
   const encodedCredentials = encoding.b64encode(credentials);
-  var endpoint = `${config.tokenGenerator.getEnterpriseToken}${support.buildQueryParametersForEndpoint(queryParams)}`;
+  var endpoint;
+
+  if (tokenFor === 'enterprise') endpoint = config.tokenGenerator.getEnterpriseToken;
+  if (tokenFor === 'personal') endpoint = config.tokenGenerator.getPersonalToken;
+  endpoint += support.buildQueryParametersForEndpoint(queryParams);
+
   var params = {
     headers: {
       Authorization: `Basic ${encodedCredentials}`,
     },
   };
+
   var token = http.get(endpoint, params);
   if (token.status != 200) stopIterationOnFail('token gen failed', false, token);
   token = token.body;
