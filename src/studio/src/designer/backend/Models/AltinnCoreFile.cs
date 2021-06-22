140fdcaee75ca4d21ac4cfbcfe1438fa43828072
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-06-22 08:44:29 +0200

    Renamed parameters to be consistent.

--- src/studio/src/designer/backend/Models/AltinnCoreFile.cs
+++ src/studio/src/designer/backend/Models/AltinnCoreFile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using Altinn.Studio.Designer.Enums;
+using Altinn.Studio.Designer.Helpers;
 
 namespace Altinn.Studio.Designer.Models
 {
@@ -48,9 +49,9 @@ namespace Altinn.Studio.Designer.Models
         /// Creates a valid populated <see cref="AltinnCoreFile"/> object from a file path string.
         /// </summary>
         /// <param name="filePath">The complete pyshical path to the file.</param>
-        /// <param name="repositoryRootPath">Root path to calculate relative url's from. This is should be the root of the repository. If it's empty or isn't a part of the filePath parameter the RepositoryRelativeUrl will be left blank.</param>        
+        /// <param name="repositoryRootDirectory">Root path to calculate relative url's from. This is should be the root of the repository. If it's empty or isn't a part of the filePath parameter the RepositoryRelativeUrl will be left blank.</param>        
         /// <returns></returns>
-        public static AltinnCoreFile CreateFromPath(string filePath, string repositoryRootPath)
+        public static AltinnCoreFile CreateFromPath(string filePath, string repositoryRootDirectory)
         {
             var fileInfo = new FileInfo(filePath);
 
@@ -61,27 +62,25 @@ namespace Altinn.Studio.Designer.Models
                 FilePath = filePath,
                 FileName = fileInfo.Name,
                 FileType = fileInfo.Extension,
-                RepositoryRelativeUrl = GetRepositoryRelativeUrl(filePath, repositoryRootPath),
+                RepositoryRelativeUrl = GetRepositoryRelativeUrl(filePath, repositoryRootDirectory),
                 LastChanged = fileInfo.LastWriteTime
             };
         }
 
-        private static string GetRepositoryRelativeUrl(string filePath, string repositoryRootPath)
+        private static string GetRepositoryRelativeUrl(string filePath, string repositoryRootDirectory)
         {
-            if (string.IsNullOrEmpty(repositoryRootPath))
-            {
-                return string.Empty;
-            }
+            Guard.AssertNotNullOrEmpty(filePath, nameof(filePath));
+            Guard.AssertNotNullOrEmpty(repositoryRootDirectory, nameof(repositoryRootDirectory));
 
-            if (!filePath.Contains(repositoryRootPath))
-            {
-                return string.Empty;
-            }
-
-            var relativeFilePath = filePath.Replace(repositoryRootPath, string.Empty);
-            var relativeUrl = relativeFilePath.Replace(@"\", @"/").Replace(@"//", "/");
+            var relativeFilePath = filePath.Replace(repositoryRootDirectory, string.Empty);
+            string relativeUrl = MakeUrlSlashes(relativeFilePath);
 
             return relativeUrl;
+        }
+
+        private static string MakeUrlSlashes(string relativeFilePath)
+        {
+            return relativeFilePath.Replace(@"\", @"/").Replace(@"//", "/");
         }
 
         private static void AssertFileExists(FileInfo fileInfo)
