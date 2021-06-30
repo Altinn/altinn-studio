a1856ec9c6c600d1ccc39f30df01240e56a56ef8
Nina Kylstad <nkylstad@gmail.com>
2021-06-30 13:22:19 +0200

    Feature/6378 logo apps (#6486)
    * Support multiple logo colors
    
    * bump version
    
    * fix failing test
    
    * update snapshots
    
    * use default altinn logo for start page
    
    * Revert "use default altinn logo for start page"
    
    This reverts commit 48e3c8dd2e96c6f4d92dc3e5866b68cfa83ae169.

--- src/shared/__tests__/components/AltinnLogo.test.tsx
+++ src/shared/__tests__/components/AltinnLogo.test.tsx
@@ -4,12 +4,13 @@ import 'jest';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import AltinnLogo from '../../src/components/AltinnLogo';
+import altinnTheme from '../../src/theme/altinnAppTheme';
 
 describe('>>> AltinnLogo', () => {
   let mockColor: string;
 
   beforeEach(() => {
-    mockColor = '#FFF';
+    mockColor = altinnTheme.altinnPalette.primary.blueDarker;
   });
 
   it('+++ Should match snapshot', () => {
@@ -20,12 +21,24 @@ describe('>>> AltinnLogo', () => {
     );
     expect(rendered).toMatchSnapshot();
   });
-  it('+++ Should have correct color', () => {
+
+  it('+++ Should have correct color - blueDark', () => {
     const mountedAltinnLogo = mount(
       <AltinnLogo
         color={mockColor}
       />,
     );
-    expect(mountedAltinnLogo.find('#Desktop').props().fill).toEqual('#FFF');
+    expect(mountedAltinnLogo.find('#logo').getDOMNode().getAttribute('src')).toEqual('https://altinncdn.no/img/Altinn-logo-black.svg');
+    expect(mountedAltinnLogo.find('#logo').getDOMNode().getAttribute('class')).toEqual('logo logo-filter-022F51');
+  });
+
+  it('+++ Should have correct color - white', () => {
+    const mountedAltinnLogo = mount(
+      <AltinnLogo
+        color='white'
+      />,
+    );
+    expect(mountedAltinnLogo.find('#logo').getDOMNode().getAttribute('src')).toEqual('https://altinncdn.no/img/Altinn-logo-white.svg');
+    expect(mountedAltinnLogo.find('#logo').getDOMNode().getAttribute('class')).toEqual('logo');
   });
 });
