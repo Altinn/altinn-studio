c4ea84929e439339fd1ca660e00459200ae4f06f
Bendik Aune <bendik.aune@adera.no>
2021-07-02 10:32:15 +0200

    Moved types in separate files

--- src/studio/src/designer/DataModeling/Converter/Json/JsonSchemaToXmlSchemaConverter.cs
+++ src/studio/src/designer/DataModeling/Converter/Json/JsonSchemaToXmlSchemaConverter.cs
@@ -2134,35 +2134,4 @@ namespace Altinn.Studio.DataModeling.Converter.Json
             schema.Items.Add(annotation);
         }
     }
-
-    /// <summary>
-    /// Placeholder
-    /// </summary>
-    public static class KeywordListExtensions
-    {
-        /// <summary>
-        /// Placeholder
-        /// </summary>
-        public static bool AnyOf<T>(this IEnumerable<IJsonSchemaKeyword> keywords)
-        {
-            return keywords.Any(kw => kw is T);
-        }
-
-        /// <summary>
-        /// Placeholder
-        /// </summary>
-        public static bool AnyOf<T1, T2, T3, T4, T5, T6, T7>(this IEnumerable<IJsonSchemaKeyword> keywords)
-        {
-            return keywords.Any(kw => kw is T1 or T2 or T3 or T4 or T5 or T6 or T7);
-        }
-    }
-
-    /// <summary>
-    /// Placeholder
-    /// </summary>
-    public enum ComplexTypeContentType
-    {
-        ComplexContent,
-        SimpleContentExtensionOrRestriction
-    }
 }
