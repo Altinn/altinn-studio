be55f584b47e2d2acb414c2a99a9ca0a3ebc5e00
Bjørn Dybvik Langfors <bdl@digdir.no>
2021-07-06 10:22:39 +0200

    Add policy id

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Migration/v0.00/01-setup-tables.sql
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Migration/v0.00/01-setup-tables.sql
@@ -1,26 +1,33 @@
 -- Table: delegation.delegatedPolicy
 CREATE TABLE IF NOT EXISTS delegation.delegatedPolicy
 (
-    policyChangeId bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    altinnAppId character varying COLLATE pg_catalog."default" NOT NULL,
-    offeredByPartyId integer NOT NULL,
-    coveredByPartyId integer,
-    coveredByUserId integer,
-    performingUserId integer NOT NULL,
-    blobStoragePolicyPath character varying COLLATE pg_catalog."default" NOT NULL,
-    blobStorageVersionId character varying COLLATE pg_catalog."default",
-    created timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+  policyChangeId bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  policyId bigint NOT NULL,
+  altinnAppId character varying COLLATE pg_catalog."default" NOT NULL,
+  offeredByPartyId integer NOT NULL,
+  coveredByPartyId integer,
+  coveredByUserId integer,
+  performingUserId integer NOT NULL,
+  blobStoragePolicyPath character varying COLLATE pg_catalog."default" NOT NULL,
+  blobStorageVersionId character varying COLLATE pg_catalog."default",
+  created timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 TABLESPACE pg_default;
 
 -- Index: AltinnAppID_OfferedByPartyId_CoveredByUserId_CoveredByPartyId
 CREATE INDEX IF NOT EXISTS idx_altinnAppID_offeredByPartyId_coveredByUserId_coveredByPartyId
-    ON delegation.delegatedPolicy USING btree
-    (altinnAppId COLLATE pg_catalog."default" ASC NULLS LAST, offeredByPartyId ASC NULLS LAST, coveredByPartyId ASC NULLS LAST, coveredByUserId ASC NULLS LAST)
-    TABLESPACE pg_default;
+  ON delegation.delegatedPolicy USING btree
+  (altinnAppId COLLATE pg_catalog."default" ASC NULLS LAST, offeredByPartyId ASC NULLS LAST, coveredByPartyId ASC NULLS LAST, coveredByUserId ASC NULLS LAST)
+  TABLESPACE pg_default;
+
+-- Index: PolicyId
+CREATE INDEX IF NOT EXISTS idx_policyId
+  ON delegation.delegatedPolicy USING btree
+  (policyId ASC NULLS LAST)
+  TABLESPACE pg_default;
 
 -- Index: Created
 CREATE INDEX IF NOT EXISTS idx_created
-    ON delegation.delegatedPolicy USING btree
-    (created ASC NULLS LAST)
-    TABLESPACE pg_default;
+  ON delegation.delegatedPolicy USING btree
+  (created ASC NULLS LAST)
+  TABLESPACE pg_default;
--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Migration/v0.00/02-setup-procedures.sql
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Migration/v0.00/02-setup-procedures.sql
@@ -1,79 +1,127 @@
--- Procecure: insert_delegation
-CREATE OR REPLACE PROCEDURE delegation.insert_delegation(
-	_altinnAppId character varying,	
-	_offeredByPartyId integer,
-	_coveredByUserId integer,
-	_coveredByPartyId integer,
-	_performingUserId integer,
-	_blobStoragePolicyPath character varying,
-	_blobStorageVersionId character varying,
-	inout _policy_id bigint)
+-- Sequence: policyId
+CREATE SEQUENCE policyId START 1;
+
+-- Procecure: insert_new_delegation_policy
+CREATE OR REPLACE PROCEDURE delegation.insert_new_delegation_policy(
+  _altinnAppId character varying,
+  _offeredByPartyId integer,
+  _coveredByUserId integer,
+  _coveredByPartyId integer,
+  _performingUserId integer,
+  _blobStoragePolicyPath character varying,
+  _blobStorageVersionId character varying,
+  inout _policyId bigint)
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
 INSERT INTO delegation.delegatedPolicy(
+    policyId,
     altinnAppId, 
-	offeredByPartyId, coveredByUserId, coveredByPartyId, performingUserId,
+    offeredByPartyId, coveredByUserId, coveredByPartyId, performingUserId,
     blobStoragePolicyPath, blobStorageVersionId
 )
 VALUES (
-	_altinnAppId, 
-	_offeredByPartyId, _coveredByUserId, _coveredByPartyId, _performingUserId,
-	_blobStoragePolicyPath, _blobStorageVersionId
-) RETURNING policyId INTO _policy_id;
+  nextval('policyId'),
+  _altinnAppId,
+  _offeredByPartyId, _coveredByUserId, _coveredByPartyId, _performingUserId,
+  _blobStoragePolicyPath, _blobStorageVersionId
+) RETURNING policyId INTO _policyId;
 
 END;
 $BODY$;
 
 
--- Function: get_with_history
-CREATE OR REPLACE FUNCTION delegation.get_with_history(
-	IN _altinnAppId character varying,	
-	IN _offeredByPartyId integer,
-	IN _coveredByUserId integer,
-	IN _coveredByPartyId integer
+CREATE OR REPLACE PROCEDURE delegation.insert_delegation_policy_change(
+  _policyId bigint,
+  _altinnAppId character varying,
+  _offeredByPartyId integer,
+  _coveredByUserId integer,
+  _coveredByPartyId integer,
+  _performingUserId integer,
+  _blobStoragePolicyPath character varying,
+  _blobStorageVersionId character varying,
+  inout _policyChangeId bigint)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+INSERT INTO delegation.delegatedPolicy(
+    policyId,
+    altinnAppId,
+  offeredByPartyId, coveredByUserId, coveredByPartyId, performingUserId,
+    blobStoragePolicyPath, blobStorageVersionId
+)
+VALUES (
+  _policyId,
+  _altinnAppId,
+  _offeredByPartyId, _coveredByUserId, _coveredByPartyId, _performingUserId,
+  _blobStoragePolicyPath, _blobStorageVersionId
+) RETURNING policyChangeId INTO _policyChangeId;
+
+END;
+$BODY$;
+
+
+-- Function: get_all_changes
+CREATE OR REPLACE FUNCTION delegation.get_all_changes(
+  IN _altinnAppId character varying,
+  IN _offeredByPartyId integer,
+  IN _coveredByUserId integer,
+  IN _coveredByPartyId integer
     )
     RETURNS SETOF delegation.delegatedPolicy AS 
 $BODY$
-	SELECT * FROM delegation.delegatedPolicy
-	WHERE 
-	    altinnAppId = _altinnAppId
-	AND offeredByPartyId = _offeredByPartyId
-	AND (_offeredByPartyId IS NULL OR offeredByPartyId = _offeredByPartyId)
-	AND (_coveredByUserId IS NULL OR coveredByUserId = _coveredByUserId)
-	AND (_coveredByPartyId IS NULL OR coveredByPartyId = _coveredByPartyId)
+  SELECT * FROM delegation.delegatedPolicy
+  WHERE 
+      altinnAppId = _altinnAppId
+  AND offeredByPartyId = _offeredByPartyId
+  AND (_offeredByPartyId IS NULL OR offeredByPartyId = _offeredByPartyId)
+  AND (_coveredByUserId IS NULL OR coveredByUserId = _coveredByUserId)
+  AND (_coveredByPartyId IS NULL OR coveredByPartyId = _coveredByPartyId)
 $BODY$
 LANGUAGE sql;
 
 
--- Function: get_current
-CREATE OR REPLACE FUNCTION delegation.get_current(
-	_altinnAppId character varying,	
-	_offeredByPartyId integer,
-	_coveredByUserId integer,
-	_coveredByPartyId integer
+-- Function: get_current_change
+CREATE OR REPLACE FUNCTION delegation.get_current_change(
+  _altinnAppId character varying,
+  _offeredByPartyId integer,
+  _coveredByUserId integer,
+  _coveredByPartyId integer
 )
 RETURNS delegation.delegatedPolicy AS 
 $BODY$
-	SELECT * FROM delegation.delegatedPolicy
-	WHERE 
-	    altinnAppId = _altinnAppId
-	AND offeredByPartyId = _offeredByPartyId
-	AND (_offeredByPartyId IS NULL OR offeredByPartyId = _offeredByPartyId)
-	AND (_coveredByUserId IS NULL OR coveredByUserId = _coveredByUserId)
-	AND (_coveredByPartyId IS NULL OR coveredByPartyId = _coveredByPartyId)
-	ORDER BY created DESC LIMIT 1
+  SELECT * FROM delegation.delegatedPolicy
+  WHERE
+  altinnAppId = _altinnAppId
+  AND offeredByPartyId = _offeredByPartyId
+  AND (_offeredByPartyId IS NULL OR offeredByPartyId = _offeredByPartyId)
+  AND (_coveredByUserId IS NULL OR coveredByUserId = _coveredByUserId)
+  AND (_coveredByPartyId IS NULL OR coveredByPartyId = _coveredByPartyId)
+  ORDER BY created DESC LIMIT 1
 $BODY$
 LANGUAGE sql;
 
--- Function: get_single
-CREATE OR REPLACE FUNCTION delegation.get_single(
-	_policyChangeId bigint
+-- Function: get_single_policy_change
+CREATE OR REPLACE FUNCTION delegation.get_single_policy_change(
+  _policyChangeId bigint
+)
+RETURNS delegation.delegatedPolicy AS
+$BODY$
+  SELECT * FROM delegation.delegatedPolicy
+  WHERE
+      policyChangeId = _policyChangeId
+$BODY$
+LANGUAGE sql;
+
+
+-- Function: get_single_policy
+CREATE OR REPLACE FUNCTION delegation.get_single_policy(
+  _policyId bigint
 )
 RETURNS delegation.delegatedPolicy AS 
 $BODY$
-	SELECT * FROM delegation.delegatedPolicy
-	WHERE 
-	    policyChangeId = _policyChangeId
+  SELECT * FROM delegation.delegatedPolicy
+  WHERE
+      policyId = _policyId
 $BODY$
 LANGUAGE sql;
