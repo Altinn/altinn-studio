cca5ab5463036787f4f1815f347a08832cd41f50
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-07-07 14:36:09 +0200

    Moved helper method for compiling C# into it's own class.

---
+++ src/studio/src/designer/backend.Tests/Utils/Compiler.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Basic.Reference.Assemblies;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Designer.Tests.Utils
+{
+    public static class Compiler
+    {
+        public static Assembly CompileToAssembly(string csharpCode)
+        {
+            var syntaxTree = SyntaxFactory.ParseSyntaxTree(SourceText.From(csharpCode));
+
+            var compilation = CSharpCompilation.Create(Guid.NewGuid().ToString())
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .WithReferenceAssemblies(ReferenceAssemblyKind.Net50)
+                .AddReferences(MetadataReference.CreateFromFile(typeof(Microsoft.AspNetCore.Mvc.ModelBinding.BindNeverAttribute).GetTypeInfo().Assembly.Location))
+                .AddReferences(MetadataReference.CreateFromFile(typeof(Newtonsoft.Json.JsonPropertyAttribute).GetTypeInfo().Assembly.Location))
+                .AddSyntaxTrees(syntaxTree);
+
+            Assembly assembly = null;
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    var errors = new StringBuilder();
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        errors.AppendLine($"{diagnostic.Id}: {diagnostic.GetMessage()}");
+                    }
+
+                    throw new Exception($"Uh dude, you seem to have provocted some compilation errors with your code change. Please fix before merging! {errors}");
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    assembly = Assembly.Load(ms.ToArray());
+                }
+            }
+
+            return assembly;
+        }
+    }
+}
