18d34e4053b352c44119419dd48fca88ca3695ca
mjulstein <6736558+mjulstein@users.noreply.github.com>
2021-07-07 15:16:38 +0200

    fix to allow period in name

--- src/studio/src/designer/frontend/shared/features/dataModelling/functions/createDataModelMetadataOptions.ts
+++ src/studio/src/designer/frontend/shared/features/dataModelling/functions/createDataModelMetadataOptions.ts
@@ -2,7 +2,7 @@ const createDataModelMetadataOptions = (rootState: any) => {
   const { dataModelsMetadata } = rootState.dataModelsMetadataState;
   return dataModelsMetadata?.length
     ? dataModelsMetadata.flatMap((value: { fileName: string }) => {
-      const label = value?.fileName?.split('.')[0];
+      const label = value?.fileName?.split('.schema')[0];
       return label ? [{ value, label }] : [];
     }) : null;
 };
