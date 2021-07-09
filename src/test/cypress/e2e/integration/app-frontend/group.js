f2c5f0de08ecadc53b277bb0cdc8bc17aacc1e6a
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-07-09 11:18:27 +0200

    Test/updated app frontend tests (#6530)
    * added shouldbe visible clause on flaky tests
    
    * support starting different apps to test frontend
    
    * new tests for stateless app
    
    * removed preserve cookies on some tests
    
    * new test for client side validation
    
    * new test for redirecting user to error info page
    
    * fixed flaky tests
    
    * custom command to add group item
    
    * formatted
    
    * added beforeeach for retry
    
    * removed only clause
    
    * Update redirect.js

--- src/test/cypress/e2e/integration/app-frontend/group.js
+++ src/test/cypress/e2e/integration/app-frontend/group.js
@@ -8,12 +8,9 @@ const appFrontend = new AppFrontend();
 const mui = new Common();
 
 describe('Group', () => {
-  before(() => {
+  beforeEach(() => {
     cy.navigateToTask3();
     cy.get(appFrontend.group.showGroupToContinue).should('be.visible');
-  });
-  beforeEach(() => {
-    cy.preserveCookies();
   });
 
   it('Dynamics on group', () => {
@@ -22,23 +19,14 @@ describe('Group', () => {
     cy.get(appFrontend.group.addNewItem).should('exist').and('be.visible');
   });
 
-  it('Add Item to Group', () => {
-    cy.get(appFrontend.group.addNewItem).should('be.visible').click();
-    cy.get(appFrontend.group.currentValue).type('1').blur();
-    cy.get(appFrontend.group.newValue).type('2').blur();
-    cy.get(appFrontend.group.saveMainGroup).should('be.visible').click().should('not.exist');
+  it('Add and delete items on main and nested group', () => {
+    cy.get(appFrontend.group.showGroupToContinue).find('input').check();
+    cy.addItemToGroup(1, 2, 'automation');
     cy.get(appFrontend.group.mainGroup)
       .find(mui.tableBody)
       .then((table) => {
         cy.get(table).find(mui.tableElement).first().invoke('text').should('equal', '1');
         cy.get(table).find(mui.tableElement).eq(1).invoke('text').should('equal', '2');
-      });
-  });
-
-  it('Add Item to Nested Group', () => {
-    cy.get(appFrontend.group.mainGroup)
-      .find(mui.tableBody)
-      .then((table) => {
         cy.get(table).find(mui.tableElement).find(mui.buttonIcon).should('be.visible').click();
       });
     cy.get(appFrontend.group.mainGroup)
@@ -46,20 +34,10 @@ describe('Group', () => {
       .find(appFrontend.group.next)
       .should('be.visible')
       .click();
-    cy.get(appFrontend.group.addNewItem).should('be.visible').click();
-    cy.get(appFrontend.group.comments).type('automation');
-    cy.get(appFrontend.group.saveSubGroup).should('be.visible').click().should('not.exist');
     cy.get(appFrontend.group.subGroup)
       .find(mui.tableBody)
       .then((table) => {
         cy.get(table).find(mui.tableElement).first().invoke('text').should('equal', 'automation');
-      });
-  });
-
-  it('Delete Item from Nested Group', () => {
-    cy.get(appFrontend.group.subGroup)
-      .find(mui.tableBody)
-      .then((table) => {
         cy.get(table).find(mui.tableElement).find(mui.buttonIcon).should('be.visible').click();
       });
     cy.get(appFrontend.group.subGroup)
@@ -67,51 +45,21 @@ describe('Group', () => {
       .find(appFrontend.group.delete)
       .should('be.visible')
       .click();
-    cy.get(appFrontend.group.mainGroup)
-      .siblings(appFrontend.group.editContainer)
-      .find(appFrontend.group.back)
-      .should('be.visible')
-      .click();
-    cy.get(appFrontend.group.currentValue).should('be.visible');
-    cy.get(appFrontend.group.saveSubGroup).should('not.exist');
-    cy.get(appFrontend.group.saveMainGroup).should('be.visible').click().should('not.exist');
-  });
-
-  it('Delete Item from Main Group', () => {
-    cy.get(appFrontend.group.mainGroup)
-      .find(mui.tableBody)
-      .then((table) => {
-        cy.get(table).find(mui.tableElement).find(mui.buttonIcon).should('be.visible').click();
-      });
+    cy.get(appFrontend.group.subGroup).find(mui.tableElement).should('have.length', 0);
     cy.get(appFrontend.group.mainGroup)
       .siblings(appFrontend.group.editContainer)
       .find(appFrontend.group.delete)
       .should('be.visible')
       .click();
     cy.get(appFrontend.group.saveMainGroup).should('not.exist');
-    cy.get(appFrontend.group.mainGroup)
-      .find(mui.tableBody)
-      .then((table) => {
-        cy.get(table).get(mui.tableElement).should('have.length', 0);
-      });
+    cy.get(appFrontend.group.mainGroup).find(mui.tableElement).should('have.length', 0);
   });
 
   it('Calculation on Item in Main Group should update value', () => {
-    cy.get(appFrontend.group.addNewItem).should('be.visible').click();
-    cy.get(appFrontend.group.currentValue).type('1337').blur();
-    // CalculationHandler.cs for frontend-test changes 1337 to 1338.
+    cy.get(appFrontend.group.showGroupToContinue).find('input').check();
+    cy.get(appFrontend.group.addNewItem).focus().should('be.visible').click();
+    cy.get(appFrontend.group.currentValue).should('be.visible').type('1337').blur().tab();
+    // DataProcessingHandler.cs for frontend-test changes 1337 to 1338.
     cy.get(appFrontend.group.currentValue).should('have.value', 'NOK 1 338');
-    // Deletes value from group
-    cy.get(appFrontend.group.mainGroup)
-      .siblings(appFrontend.group.editContainer)
-      .find(appFrontend.group.delete)
-      .should('be.visible')
-      .click();
-    cy.get(appFrontend.group.saveMainGroup).should('not.exist');
-    cy.get(appFrontend.group.mainGroup)
-      .find(mui.tableBody)
-      .then((table) => {
-        cy.get(table).get(mui.tableElement).should('have.length', 0);
-      });
   });
 });
--- src/test/cypress/e2e/support/app-frontend.js
+++ src/test/cypress/e2e/support/app-frontend.js
@@ -10,7 +10,7 @@ const mui = new Common();
  * Start app instance of frontend-test and navigate to change name layout in task_2
  */
 Cypress.Commands.add('navigateToChangeName', () => {
-  cy.startAppInstance();
+  cy.startAppInstance(Cypress.env('multiData2Stage'));
   cy.get(appFrontend.closeButton).should('be.visible');
   cy.intercept('**/api/layoutsettings/changename').as('getLayoutChangeName');
   cy.get(appFrontend.sendinButton).then((button) => {
@@ -33,16 +33,16 @@ Cypress.Commands.add('completeChangeNameForm', (firstName, lastName) => {
   cy.get(appFrontend.changeOfName.currentName)
     .should('be.visible')
     .then(() => {
-      cy.get(appFrontend.changeOfName.newFirstName).type(firstName);
-      cy.get(appFrontend.changeOfName.newLastName).type(lastName);
-      cy.get(appFrontend.changeOfName.confirmChangeName).find('input').check();
-      cy.get(appFrontend.changeOfName.reasonRelationship).click().type('test');
+      cy.get(appFrontend.changeOfName.newFirstName).should('be.visible').type(firstName).blur();
+      cy.get(appFrontend.changeOfName.newLastName).should('be.visible').type(lastName).blur();
+      cy.get(appFrontend.changeOfName.confirmChangeName).should('be.visible').find('input').check();
+      cy.get(appFrontend.changeOfName.reasonRelationship).should('be.visible').click().type('test');
       cy.get(appFrontend.changeOfName.dateOfEffect)
         .siblings()
         .children(mui.buttonIcon)
         .click()
         .then(() => {
-          cy.get(mui.selectedDate).click();
+          cy.get(mui.selectedDate).should('be.visible').click();
         });
       cy.contains(mui.button, texts.next).click();
     });
@@ -67,17 +67,7 @@ Cypress.Commands.add('compelteTask3Form', () => {
   cy.get(appFrontend.group.showGroupToContinue).then((checkbox) => {
     cy.get(checkbox).should('be.visible').find('input').check();
   });
-  cy.get(appFrontend.group.addNewItem).should('be.visible').click();
-  cy.get(appFrontend.group.currentValue).type('1');
-  cy.get(appFrontend.group.newValue).type('2');
-  cy.get(appFrontend.group.mainGroup)
-    .siblings(appFrontend.group.editContainer)
-    .find(appFrontend.group.next)
-    .should('be.visible')
-    .click();
-  cy.get(appFrontend.group.addNewItem).should('be.visible').click();
-  cy.get(appFrontend.group.comments).type('automation');
-  cy.get(appFrontend.group.saveMainGroup).should('be.visible').click().should('not.exist');
+  cy.addItemToGroup(1, 2, 'automation');
   cy.contains(mui.button, texts.next).click();
   cy.get(appFrontend.group.sendersName).should('be.visible').type('automation');
   cy.contains(mui.button, texts.next).click();
@@ -88,4 +78,19 @@ Cypress.Commands.add('navigateToTask4', () => {
   cy.compelteTask3Form();
   cy.get(appFrontend.sendinButton).should('be.visible').click();
   cy.get(appFrontend.confirmContainer).should('be.visible');
+});
+
+Cypress.Commands.add('addItemToGroup', (oldValue, newValue, comment) => {
+  cy.get(appFrontend.group.addNewItem).should('be.visible').focus().click();
+  cy.get(appFrontend.group.currentValue).should('be.visible').type(oldValue).blur();
+  cy.get(appFrontend.group.newValue).should('be.visible').type(newValue).blur();
+  cy.get(appFrontend.group.mainGroup)
+    .siblings(appFrontend.group.editContainer)
+    .find(appFrontend.group.next)
+    .should('be.visible')
+    .click();
+  cy.get(appFrontend.group.addNewItem).should('be.visible').focus().click();
+  cy.get(appFrontend.group.comments).type(comment).blur();
+  cy.get(appFrontend.group.saveSubGroup).should('be.visible').click().should('not.exist');
+  cy.get(appFrontend.group.saveMainGroup).should('be.visible').click().should('not.exist');
 });
