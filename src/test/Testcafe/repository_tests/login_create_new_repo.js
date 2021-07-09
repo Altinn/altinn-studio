551c3973bb969b2cb17cf6473cb4b1b1afa10cc9
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-07-09 11:31:46 +0200

    updated studio tests (#6537)
    * delete local app changes when pull fails
    
    * added prettier, config and scripts
    
    * prettier formatted files

--- src/test/Testcafe/repository_tests/login_create_new_repo.js
+++ src/test/Testcafe/repository_tests/login_create_new_repo.js
@@ -15,37 +15,44 @@ let firstRun = true;
 
 fixture('adminster repos')
   .page(app.baseUrl)
-  .before(async t => {
+  .before(async (t) => {
     app.before();
   })
   .after(async () => {
     await app.after();
   })
-  .beforeEach(async t => {
+  .beforeEach(async (t) => {
     if (firstRun) {
       await t.useRole(AutoTestUser);
       await common.ensureUserHasNoRepos(testUser.userName, landingPage, repoPage);
       firstRun = false;
     }
   })
-  .afterEach(async t => {
+  .afterEach(async (t) => {
     common.returnToHomePage();
   });
 
 const getPageUrl = ClientFunction(() => window.location.href);
 
-test.skip('Login and create new repo', async t => {
+test.skip('Login and create new repo', async (t) => {
   await t
-    .expect(header.navBar.exists).ok({ timeout: 2500 })
-    .expect(landingPage.title.textContent).eql('Altinn studio')
+    .expect(header.navBar.exists)
+    .ok({ timeout: 2500 })
+    .expect(landingPage.title.textContent)
+    .eql('Altinn studio')
     .click(landingPage.repoLink)
-    .expect(common.getPageUrl()).contains('/explore/repos')
+    .expect(common.getPageUrl())
+    .contains('/explore/repos')
     .click(landingPage.createButton)
-    .expect(landingPage.newRepoButton.visible).ok({ timeout: 2500 })
+    .expect(landingPage.newRepoButton.visible)
+    .ok({ timeout: 2500 })
     .click(landingPage.newRepoButton)
-    .expect(common.getPageUrl()).contains('repo/create')
+    .expect(common.getPageUrl())
+    .contains('repo/create')
     .typeText(repoPage.title, 'automatedTestRepo')
-    .expect(repoPage.title.value).eql('automatedTestRepo')
+    .expect(repoPage.title.value)
+    .eql('automatedTestRepo')
     .click(repoPage.submitButton)
-    .expect(getPageUrl()).contains(testUser.userName + '/automatedTestRepo');
+    .expect(getPageUrl())
+    .contains(testUser.userName + '/automatedTestRepo');
 });
