b7b10951e6c95cef63a54dd5929b83ca6f0ba005
HansO <hans.waaler@digdir.no>
2021-07-12 10:12:03 +0200

    Add security headers to pipeline

---
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Api/Middleware/SecurityHeadersApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+
+namespace Altinn.App.Api.Middleware
+{
+    /// <summary>
+    /// Extensions for adding default security headerse middleware to the pipeline.
+    /// </summary>
+    public static class SecurityHeadersApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the security headers to the pipeline.
+        /// </summary>
+        /// <param name="builder">The application builder</param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseDefaultSecurityHeaders(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SecurityHeadersMiddleware>();
+        }
+    }
+}
