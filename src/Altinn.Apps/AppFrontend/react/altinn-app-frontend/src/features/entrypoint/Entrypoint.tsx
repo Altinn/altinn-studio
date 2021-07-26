f7c8b77aa5d5e8a32ec5846800ef06ea28aacee0
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-07-26 16:07:14 +0200

    Empty header

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/entrypoint/Entrypoint.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/entrypoint/Entrypoint.tsx
@@ -49,7 +49,7 @@ export default function Entrypoint() {
 
   return (
     <Presentation
-      header={applicationMetadata?.title?.nb}
+      header=''
       type={ProcessTaskType.Unknown}
     >
       <AltinnContentLoader width='100%' height='400'>
