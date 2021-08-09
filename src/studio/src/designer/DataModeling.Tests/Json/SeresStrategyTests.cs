7832ef9ccedce13176bcc35155f3b840530caf80
Bendik Aune <bendik.aune@adera.no>
2021-08-09 14:28:48 +0200

    Test for SERES Analyze, basic converter logic

---
+++ src/studio/src/designer/DataModeling.Tests/Json/SeresStrategyTests.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Altinn.Studio.DataModeling.Converter.Json;
+using Altinn.Studio.DataModeling.Converter.Json.Strategy;
+using Altinn.Studio.DataModeling.Json.Keywords;
+using FluentAssertions;
+using Json.Pointer;
+using Xunit;
+
+namespace DataModeling.Tests.Json
+{
+    public class SeresStrategyTests
+    {
+        [Theory]
+        [InlineData(@"Model\JsonSchema\SeresBasicSchema.json")]
+        public async Task Analyze_Seres_Converted_JsonSchema(string path)
+        {
+            JsonSchemaKeywords.RegisterXsdKeywords();
+
+            var schema = await ResourceHelpers.LoadJsonSchemaTestData(path);
+            var strategy = new JsonSchemaToXmlSchemaConverterSeresStrategy();
+
+            var metadata = strategy.AnalyzeSchema(schema);
+
+            metadata.GetCompatibleTypes(JsonPointer.Parse("#")).Should().Equal(CompatibleXsdType.Unknown);
+            metadata.GetCompatibleTypes(JsonPointer.Parse("#/oneOf/[0]")).Should().Equal(CompatibleXsdType.Unknown);
+            metadata.GetCompatibleTypes(JsonPointer.Parse("#/$defs/melding-modell")).Should().Equal(CompatibleXsdType.ComplexType);
+            metadata.GetCompatibleTypes(JsonPointer.Parse("#/$defs/melding-modell/properties/e1")).Should().Equal(CompatibleXsdType.SimpleType);
+        }
+    }
+}
--- src/studio/src/designer/DataModeling/Converter/Json/JsonSchemaXsdMetadata.cs
+++ src/studio/src/designer/DataModeling/Converter/Json/JsonSchemaXsdMetadata.cs
@@ -8,36 +8,63 @@ namespace Altinn.Studio.DataModeling.Converter.Json
     /// </summary>
     public class JsonSchemaXsdMetadata
     {
-        private static readonly IReadOnlyCollection<CompatibleXsdType> _empty = new List<CompatibleXsdType>().AsReadOnly();
+        private static readonly ISet<CompatibleXsdType> Empty = new HashSet<CompatibleXsdType>();
 
-        private Dictionary<JsonPointer, List<CompatibleXsdType>> _types;
+        private readonly Dictionary<JsonPointer, HashSet<CompatibleXsdType>> _types;
+
+        /// <summary>
+        /// Placeholder
+        /// </summary>
+        public string MessageName { get; set; }
+
+        /// <summary>
+        /// Placeholder
+        /// </summary>
+        public string MessageTypeName { get; set; }
 
         /// <summary>
         /// Construct a new instance of this type
         /// </summary>
         public JsonSchemaXsdMetadata()
         {
-            _types = new Dictionary<JsonPointer, List<CompatibleXsdType>>();
+            _types = new Dictionary<JsonPointer, HashSet<CompatibleXsdType>>();
         }
 
         /// <summary>
         /// Add compatible types to the give path
         /// </summary>
         /// <param name="path">the path to the element in JSON schema</param>
-        /// <param name="type">The type(s) to add as compatible</param>
-        public void AddCompatibleTypes(JsonPointer path, params CompatibleXsdType[] type)
+        /// <param name="types">The type(s) to add as compatible</param>
+        public void AddCompatibleTypes(JsonPointer path, params CompatibleXsdType[] types)
         {
+            if (!_types.TryGetValue(path, out var compatibleTypes))
+            {
+                compatibleTypes = new HashSet<CompatibleXsdType>();
+                _types.Add(path, compatibleTypes);
+            }
 
+            foreach (var type in types)
+            {
+                compatibleTypes.Add(type);
+            }
         }
 
         /// <summary>
         /// Remove compatible types from the give path
         /// </summary>
         /// <param name="path">the path to the element in JSON schema</param>
-        /// <param name="type">The type(s) to remove as compatible</param>
-        public void RemoveCompatibleTypes(JsonPointer path, params CompatibleXsdType[] type)
+        /// <param name="types">The type(s) to remove as compatible</param>
+        public void RemoveCompatibleTypes(JsonPointer path, params CompatibleXsdType[] types)
         {
+            if (!_types.TryGetValue(path, out var compatibleTypes))
+            {
+                return;
+            }
 
+            foreach (var type in types)
+            {
+                compatibleTypes.Remove(type);
+            }
         }
 
         /// <summary>
@@ -45,9 +72,9 @@ namespace Altinn.Studio.DataModeling.Converter.Json
         /// </summary>
         /// <param name="path">The path to the element</param>
         /// <returns>A list of compatible types for the JSON schema element</returns>
-        public IReadOnlyCollection<CompatibleXsdType> GetCompatibleTypes(JsonPointer path)
+        public ISet<CompatibleXsdType> GetCompatibleTypes(JsonPointer path)
         {
-            return _types.TryGetValue(path, out List<CompatibleXsdType> types) ? types.AsReadOnly() : _empty;
+            return _types.TryGetValue(path, out HashSet<CompatibleXsdType> types) ? types : Empty;
         } 
     }
 }
