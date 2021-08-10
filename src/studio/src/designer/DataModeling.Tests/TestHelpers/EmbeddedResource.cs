abf7eb6bc1e3e732822c4c1d97895ae87bd76b60
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-08-10 16:00:32 +0200

    Added XML validation.

---
+++ src/studio/src/designer/DataModeling.Tests/TestHelpers/EmbeddedResource.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DataModeling.Tests.TestHelpers
+{
+    public static class EmbeddedResource
+    {
+        public static string LoadDataFromEmbeddedResourceAsString(string resourceName)
+        {
+            var resourceStream = LoadDataFromEmbeddedResource(resourceName);
+
+            using StreamReader reader = new StreamReader(resourceStream);
+            string text = reader.ReadToEnd();
+
+            return text;
+        }
+
+        public static Stream LoadDataFromEmbeddedResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            Stream resource = assembly.GetManifestResourceStream(resourceName);
+
+            if (resource == null)
+            {
+                throw new InvalidOperationException("Unable to find test data embedded in the test assembly.");
+            }
+
+            return resource;
+        }
+    }
+}
---
+++ src/studio/src/designer/DataModeling.Tests/_TestData/Model/CSharp/melding.cs
@@ -0,0 +1,9 @@
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+namespace DataModeling.Tests._TestData.Model.CSharp
+{
+    public class melding
+    {
+        public string e1 { get; set; }
+    }
+}
+#pragma warning restore SA1300
