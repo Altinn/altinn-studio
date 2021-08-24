f9acbcd367ee89916bfd997317a9d807ddb97e5f
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-08-24 12:01:57 +0200

    removed using statements

--- src/Altinn.Platform/Altinn.Platform.Storage/Storage/Filters/IdentityTelemetryFilter.cs
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Filters/IdentityTelemetryFilter.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 using AltinnCore.Authentication.Constants;
 
