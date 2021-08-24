d9b706d9b47a6eab43034b8bdb548de4f304dd21
Stephanie Buadu <stephanie.buadu@avanade.com>
2021-08-24 09:30:43 +0200

    fixed import causing build error

--- src/studio/src/designer/backend/Services/Implementation/RepositorySI.cs
+++ src/studio/src/designer/backend/Services/Implementation/RepositorySI.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
-using Altinn.Platform.Storage.Interface.Models;
 using Altinn.Studio.Designer.Configuration;
 using Altinn.Studio.Designer.Enums;
 using Altinn.Studio.Designer.Factories.ModelFactory;
@@ -1197,7 +1196,7 @@ namespace Altinn.Studio.Designer.Services.Implementation
 
             await targetAppRepository.SearchAndReplaceInFile(".git/config", $"repos/{org}/{sourceRepository}.git", $"repos/{org}/{targetRepository}.git");
 
-            Application appMetadata = await targetAppRepository.GetApplicationMetadata();
+            PlatformStorageModels.Application appMetadata = await targetAppRepository.GetApplicationMetadata();
             appMetadata.Id = $"{org}/{targetRepository}";
             appMetadata.CreatedBy = developer;
             appMetadata.LastChangedBy = developer;
