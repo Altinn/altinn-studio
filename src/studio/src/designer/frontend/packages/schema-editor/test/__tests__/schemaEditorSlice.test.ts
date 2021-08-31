ce72cfa00e1911ba22045b95f3efd9b58be582fd
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-08-31 16:58:05 +0200

    Refactored

--- src/studio/src/designer/frontend/packages/schema-editor/test/__tests__/schemaEditorSlice.test.ts
+++ src/studio/src/designer/frontend/packages/schema-editor/test/__tests__/schemaEditorSlice.test.ts
@@ -1,7 +1,9 @@
-import reducer, { addRestriction, addProperty, deleteField, deleteProperty, initialState,
+import reducer, {
+  addRestriction, addProperty, deleteField, deleteProperty, initialState,
   setRestriction, setJsonSchema, setRestrictionKey, setPropertyName, setRef, setSelectedId, setUiSchema,
   updateJsonSchema, addEnum, setTitle, setDescription, setType, setRequired, deleteEnum,
-  setItems, promoteProperty, addRootItem, navigateToType, setSelectedTab } from '../../src/features/editor/schemaEditorSlice';
+  setItems, promoteProperty, addRootItem, navigateToType, setSelectedTab
+} from '../../src/features/editor/schemaEditorSlice';
 import { ISchemaState, UiSchemaItem } from '../../src/types';
 import { dataMock } from '../../src/mockData';
 import { getUiSchemaItem, resetUniqueNumber } from '../../src/utils';
@@ -161,15 +163,25 @@ describe('SchemaEditorSlice', () => {
     const payload = {
       path: '#/definitions/someField',
     };
-    const nextState = reducer({ ...state, selectedEditorTab: '1', selectedDefinitionNodeId: '#/definitions/someField' }, deleteProperty(payload));
+    const mockState = {
+      ...state,
+      selectedEditorTab: '1',
+      selectedDefinitionNodeId: '#/definitions/someField'
+    };
+    const nextState = reducer(mockState, deleteProperty(payload));
     expect(nextState.selectedDefinitionNodeId).toEqual('');
   });
 
-    it('resets selected id when deleting selected property', () => {
+  it('resets selected id when deleting selected property', () => {
     const payload = {
       path: '#/properties/someField',
     };
-    const nextState = reducer({ ...state, selectedEditorTab: '0', selectedPropertyNodeId: '#/properties/someField' }, deleteProperty(payload));
+    const mockState = {
+      ...state,
+      selectedEditorTab: '0',
+      selectedPropertyNodeId: '#/properties/someField'
+    };
+    const nextState = reducer(mockState, deleteProperty(payload));
     expect(nextState.selectedPropertyNodeId).toEqual('');
   });
 
