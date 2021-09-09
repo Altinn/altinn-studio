1a3b2ea2b3b1984eba974ce918880de487e23302
lompa94 <martinsolto@hotmail.com>
2021-09-09 09:14:28 +0200

    Changed prop name to "openByDefault" and added it to the edit object

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/containers/GroupContainer.tsx
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/containers/GroupContainer.tsx
@@ -89,7 +89,7 @@ export function GroupContainer({
       setMultiPageIndex(0);
     }
 
-    if (container.openDefault === true && repeatingGroupIndex === -1) {
+    if (container.edit?.openByDefault === true && repeatingGroupIndex === -1) {
       onClickAdd();
     }
   }, [container]);
