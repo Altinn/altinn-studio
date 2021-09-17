79d18b7a437dfd7fbdb7aff25774011291a85415
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-09-17 12:24:38 +0200

    Added back commentet out assert. Phew, still green.

--- src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaSeresAnalyzer.cs
+++ src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaSeresAnalyzer.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text.Json;
 using Altinn.Studio.DataModeling.Json.Keywords;
