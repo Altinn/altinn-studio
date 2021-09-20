1b1483c2547af02c4bf4455513bc7c21e1648a40
Ivar Tryti <oit@brreg.no>
2021-09-20 16:04:36 +0200

    Removed unused code

--- src/Altinn.Platform/Altinn.Platform.Authorization/IntegrationTests/Data/TestDataHelper.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/IntegrationTests/Data/TestDataHelper.cs
@@ -9,30 +9,6 @@ namespace Altinn.Platform.Authorization.IntegrationTests.Data
 {
     public static class TestDataHelper
     {
-        public static Rule GetRuleModel(int delegatedByUserId, int offeredByPartyId, string coveredBy, string coveredByAttributeType, string action, string org, string app, string task = null, string appresource = null)
-        {
-            Rule rule = new Rule
-            {
-                DelegatedByUserId = delegatedByUserId,
-                OfferedByPartyId = offeredByPartyId,
-                CoveredBy = new List<AttributeMatch> { new AttributeMatch { Id = coveredByAttributeType, Value = coveredBy } },
-                Resource = new List<AttributeMatch> { new AttributeMatch { Id = XacmlRequestAttribute.OrgAttribute, Value = org }, new AttributeMatch { Id = XacmlRequestAttribute.AppAttribute, Value = app } },
-                Action = new AttributeMatch { Id = XacmlConstants.MatchAttributeIdentifiers.ActionId, Value = action }
-            };
-
-            if (task != null)
-            {
-                rule.Resource.Add(new AttributeMatch { Id = XacmlRequestAttribute.TaskAttribute, Value = task });
-            }
-
-            if (appresource != null)
-            {
-                rule.Resource.Add(new AttributeMatch { Id = XacmlRequestAttribute.AppResourceAttribute, Value = appresource });
-            }
-
-            return rule;
-        }
-
         public static ResourceAction GetResourceActionModel(string action, IEnumerable<string> roles)
         {
             ResourceAction resourceAction = new ResourceAction // legg til Title
