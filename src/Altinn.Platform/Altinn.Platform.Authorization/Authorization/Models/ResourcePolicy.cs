ce76c97f7f16f156df5a31876d222fc9f939a04c
ivartryti <59682632+ivartryti@users.noreply.github.com>
2021-09-21 12:48:04 +0200

    Update src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Models/ResourcePolicy.cs
    Co-authored-by: Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Models/ResourcePolicy.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Models/ResourcePolicy.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace Altinn.Platform.Authorization.Models
 {
     /// <summary>
-    /// This model describes the actions that can be performed for a particular resource within a app policy
+    /// This model describes the actions that can be performed for a particular resource within an app policy
     /// </summary>
     public class ResourcePolicy
     {
