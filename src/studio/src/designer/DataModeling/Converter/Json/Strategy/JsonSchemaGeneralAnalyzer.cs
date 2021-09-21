ebe2d1c608990f1891c8178ac9bdb51203cabd86
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-09-21 10:12:33 +0200

    Initial analyzer class for general json schemas.

--- src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaGeneralAnalyzer.cs
+++ src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaGeneralAnalyzer.cs
@@ -1,3 +1,4 @@
+using Json.Pointer;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Converter.Json.Strategy
@@ -5,9 +6,25 @@ namespace Altinn.Studio.DataModeling.Converter.Json.Strategy
     /// <inheritdoc/>
     public class JsonSchemaGeneralAnalyzer : JsonSchemaAnalyzer, IJsonSchemaAnalyzer
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonSchemaGeneralAnalyzer"/> class.
+        /// </summary>
+        public JsonSchemaGeneralAnalyzer() : base()
+        {
+        }
+
         /// <inheritdoc/>
         public override JsonSchemaXsdMetadata AnalyzeSchema(JsonSchema schema)
         {
+            JsonSchema = schema;
+            Metadata = new JsonSchemaXsdMetadata();
+
+            Metadata.MessageName = string.Empty;
+            Metadata.MessageTypeName = string.Empty;
+            
+            DetermineRootModel(JsonSchema);
+            AnalyzeSchema(JsonPointer.Parse("#"), JsonSchema);
+
             return Metadata;
         }
     }
