685a3423655ee47f6b6a748934392fd1b08b063c
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-09-23 12:01:16 +0200

    Removed unused using statements.

--- src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaConverterStrategyFactory.cs
+++ src/studio/src/designer/DataModeling/Converter/Json/Strategy/JsonSchemaConverterStrategyFactory.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Altinn.Studio.DataModeling.Json.Keywords;
 using Altinn.Studio.DataModeling.Utils;
 using Json.Schema;
