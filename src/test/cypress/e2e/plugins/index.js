5727cca2447bfb40d49776762e212a4a36ce6cfe
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-10-06 16:36:07 +0200

    handle different configs for studio - app frontend

--- src/test/cypress/e2e/plugins/index.js
+++ src/test/cypress/e2e/plugins/index.js
@@ -1,21 +1,39 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-function getConfigurationByFile(file) {
-  const pathToJsonDataFile = path.resolve('e2e', 'config', `${file}.json`);
+function getConfigurationByFile(pathToFile, file) {
+  const pathToJsonDataFile = path.resolve(pathToFile, `${file}.json`);
   return fs.readJson(pathToJsonDataFile);
 }
 
 module.exports = (on, config) => {
-  switch (config.env.environment) {
-    case 'local-app':
-      return getConfigurationByFile('local-app');
-    case 'at21':
-      return getConfigurationByFile('at21');
-    case 'at22':
-      return getConfigurationByFile('at22');
-    case 'tt02':
-      return getConfigurationByFile('tt02');
+  console.log(config);
+  if (config.env.component === 'appfrontend') {
+    var pathToConfig = 'e2e/config/app';
+    switch (config.env.environment) {
+      case 'local':
+        return getConfigurationByFile(pathToConfig, 'local');
+      case 'at21':
+        return getConfigurationByFile(pathToConfig, 'at21');
+      case 'at22':
+        return getConfigurationByFile(pathToConfig, 'at22');
+      case 'tt02':
+        return getConfigurationByFile(pathToConfig, 'tt02');
+    }
+  }
+
+  if (config.env.component === 'studio') {
+    var pathToConfig = 'e2e/config/studio';
+    switch (config.env.environment) {
+      case 'local':
+        return getConfigurationByFile(pathToConfig, 'local');
+      case 'dev':
+        return getConfigurationByFile(pathToConfig, 'dev');
+      case 'staging':
+        return getConfigurationByFile(pathToConfig, 'staging');
+      case 'prod':
+        return getConfigurationByFile(pathToConfig, 'prod');
+    }
   }
 
   return config;
