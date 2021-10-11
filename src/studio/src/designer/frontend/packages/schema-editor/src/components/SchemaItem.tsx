0a277da3e21ca876e17d27a0ade5b892343defdb
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2021-10-11 15:45:02 +0200

    feature/7032-set-type-create-node (#7056)
    * Refactored to select node type on new item. Reused altinn menu across solutions.
    
    * Snapshots + fixed some tests
    
    * traced and fixed ts-errors causing tests to fail (#7087)
    
    * Code review
    
    * Code smells
    
    * Reuse type in editor.tsx
    
    * removed trailing commas
    
    * Types
    
    * shared alias
    
    * Renamed shared alias to shared-app to be consistent with rest of studio projects
    
    * Missing english translation
    
    * Reverted altinn-shared in root
    
    * package reference in shared aswell
    
    * correct path
    
    * fix tests
    
    * Added tests for new add button and context menu
    
    * Code smells
    
    Co-authored-by: EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
    Co-authored-by: Martin Joakim Ulstein <6736558+mjulstein@users.noreply.github.com>

--- src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaItem.tsx
+++ src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaItem.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TreeItem, { TreeItemProps } from '@material-ui/lab/TreeItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { addProperty, deleteProperty, navigateToType, promoteProperty, setSelectedId } from '../features/editor/schemaEditorSlice';
-import { ILanguage, ISchemaState, UiSchemaItem } from '../types';
+import { ILanguage, ISchemaState, PropertyType, UiSchemaItem } from '../types';
 import { SchemaItemLabel } from './SchemaItemLabel';
 import { getDomFriendlyID } from '../utils';
 
@@ -145,9 +145,11 @@ function SchemaItem(props: SchemaItemProps) {
     dispatch(deleteProperty({ path: item.path }));
   };
 
-  const handleAddProperty = () => {
+  const handleAddProperty = (type: PropertyType) => {
     dispatch(addProperty({
       path: itemToDisplay.path,
+      type: (type === 'field' ? '' : undefined),
+      $ref: (type === 'reference' ? '' : undefined),
     }));
   };
 
