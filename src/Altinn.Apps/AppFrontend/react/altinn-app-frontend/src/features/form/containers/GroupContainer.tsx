Altinn/altinn-studio

commit 71502f4678891ad55372e9334806a965821a67a1

Repeating group open by default (#6743)
* Repeating group open by default

* Added explicit checks

* Changed prop name to "openByDefault" and added it to the edit object

* More efficient check for openByDefault

* Removed reduntant syntax

* Repeating group open by default

* Added explicit checks

* Changed prop name to "openByDefault" and added it to the edit object

* More efficient check for openByDefault

* Removed reduntant syntax

* corrected the object-curly-spacing

* Keep group open when first item deleted and openByDefault is true

* Update package.json

Bumped version to 3.12.3

* update app frontend version

Co-authored-by: Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>

src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/containers/GroupContainer.tsx
@@ -85,8 +85,17 @@ export function GroupContainer({
   }, [formData, container]);
 
   React.useEffect(() => {
-    if (container.edit?.multiPage) {
+    const { edit } = container;
+    if (!edit) {
+      return;
+    }
+
+    if (edit.multiPage) {
       setMultiPageIndex(0);
+    }
+
+    if (edit.openByDefault && repeatingGroupIndex === -1) {
+      onClickAdd();
     }
   }, [container]);
 
@@ -104,12 +113,17 @@ export function GroupContainer({
   };
 
   const onClickRemove = (groupIndex: number) => {
-    dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: -1 }));
+    dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: -1 }));    
     dispatch(FormLayoutActions.updateRepeatingGroups({
       layoutElementId: id,
       remove: true,
       index: groupIndex,
     }));
+
+    if(container.edit?.openByDefault && groupIndex === 0 && repeatingGroups[id].count === 0) {
+      dispatch(FormLayoutActions.updateRepeatingGroups({ layoutElementId: id }));
+      dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: (groupIndex) }));
+    }
   };
 
   const onClickSave = () => {

src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/layout/index.ts
@@ -136,6 +136,7 @@ export interface IGroupEditProperties {
   saveButton?: boolean;
   deleteButton?: boolean;
   multiPage?: boolean;
+  openByDefault?: boolean;
 }
 
 export interface IGroupFilter {

commit 4306ac11ffadcdf21acb42f1a53c66cc49b9137f

Fix style errors
Introduced in https://github.com/Altinn/altinn-studio/pull/6743

src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/form/containers/GroupContainer.tsx
@@ -113,14 +113,14 @@ export function GroupContainer({
   };
 
   const onClickRemove = (groupIndex: number) => {
-    dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: -1 }));    
+    dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: -1 }));
     dispatch(FormLayoutActions.updateRepeatingGroups({
       layoutElementId: id,
       remove: true,
       index: groupIndex,
     }));
 
-    if(container.edit?.openByDefault && groupIndex === 0 && repeatingGroups[id].count === 0) {
+    if (container.edit?.openByDefault && groupIndex === 0 && repeatingGroups[id].count === 0) {
       dispatch(FormLayoutActions.updateRepeatingGroups({ layoutElementId: id }));
       dispatch(FormLayoutActions.updateRepeatingGroupsEditIndex({ group: id, index: (groupIndex) }));
     }