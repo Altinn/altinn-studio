b98f623aec3ac33ee4531fd6f0aa322b61dd270d
Rune T. Larsen <rune@hjemmekino.no>
2021-10-22 13:25:08 +0200

    Removed newtonsoft

--- src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Api/Models/InstansiationInstance.cs
+++ src/Altinn.Apps/AppTemplates/AspNet/Altinn.App.Api/Models/InstansiationInstance.cs
@@ -1,38 +1,32 @@
 using System;
 using System.Collections.Generic;
 using Altinn.Platform.Storage.Interface.Models;
-using Newtonsoft.Json;
 
 namespace Altinn.App.Api.Models
 {
     /// <summary>
     /// Specialized model for isntansiation of instances
     /// </summary>
-    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
     public class InstansiationInstance
     {
         /// <summary>
         /// Gets or sets the instance owner information. 
         /// </summary>
-        [JsonProperty(PropertyName = "instanceOwner")]
         public InstanceOwner InstanceOwner { get; set; }
 
         /// <summary>
         /// Gets or sets the due date to submit the instance to application owner.
         /// </summary>
-        [JsonProperty(PropertyName = "dueBefore")]
         public DateTime? DueBefore { get; set; }
 
         /// <summary>
         /// Gets or sets date and time for when the instance should first become visible for the instance owner.
         /// </summary>
-        [JsonProperty(PropertyName = "visibleAfter")]
         public DateTime? VisibleAfter { get; set; }
 
         /// <summary>
         /// Gets or sets the prefill values for the instance.        
         /// </summary>
-        [JsonProperty(PropertyName = "prefill")]
         public Dictionary<string, string> Prefill { get; set; }
 
         /// <inheritdoc/>
