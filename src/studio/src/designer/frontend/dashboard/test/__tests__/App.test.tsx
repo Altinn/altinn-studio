6260674a761bca1778b2f2ffb9ee7c238999cc7f
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2021-10-26 09:46:05 +0200

    refactor: rewrite App component to hooks for easier maintainability, remove dead code, and add proper tests

--- src/studio/src/designer/frontend/dashboard/test/__tests__/App.test.tsx
+++ src/studio/src/designer/frontend/dashboard/test/__tests__/App.test.tsx
@@ -1,6 +1,58 @@
-// Just a hard-coded test to test that jest runs
-describe('Dashboard - /dashboard/src/App.tsx', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import 'jest';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+
+import { App } from '../../App';
+
+describe('Dashboard > App', () => {
+  let store: any;
+  const dispatchMock = () => Promise.resolve({});
+  const initialState = {
+    dashboard: {
+      user: {
+        full_name: 'John Smith',
+        login: 'johnsmith',
+      },
+    },
+  };
+
+  const mountComponent = () =>
+    mount(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      { context: { store } },
+    );
+
+  beforeEach(() => {
+    store = configureStore()(initialState);
+    store.dispatch = jest.fn(dispatchMock);
+  });
+
+  it('should call dispatch on mount to setup initial data', () => {
+    act(() => {
+      mountComponent();
+    });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(4);
+    expect(store.dispatch).toHaveBeenNthCalledWith(1, {
+      payload: { url: 'http://localhost/designerapi/User/Current' },
+      type: 'dashboard/fetchCurrentUser',
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(2, {
+      payload: { languageCode: 'nb', url: 'http://localhost/designerapi/Language/GetLanguageAsJSON' },
+      type: 'language/fetchLanguage',
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(3, {
+      payload: { url: 'http://localhost/designerapi/Repository/UserRepos' },
+      type: 'dashboard/fetchServices',
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(4, {
+      payload: { url: 'http://localhost/designerapi/Repository/Organizations' },
+      type: 'dashboard/fetchOrganisations',
+    });
   });
 });
