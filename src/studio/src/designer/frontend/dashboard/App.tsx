dd6d329963f6169f88179aefc4063eab2cfddf5c
mjulstein <6736558+mjulstein@users.noreply.github.com>
2021-11-04 14:44:20 +0100

    added a spinner for when user object is not loaded correctly and a logout button to escape the condition

--- src/studio/src/designer/frontend/dashboard/App.tsx
+++ src/studio/src/designer/frontend/dashboard/App.tsx
@@ -1,11 +1,11 @@
 import { createTheme, MuiThemeProvider } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import * as React from 'react';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import AppBarComponent from 'app-shared/navigation/main-header/appBar';
 import altinnTheme from 'app-shared/theme/altinnStudioTheme';
+import AltinnSpinner from 'app-shared/components/AltinnSpinner';
 import { StandaloneDataModelling } from './features';
 import { CloneService } from './features/cloneService/cloneServices';
 import { KnownIssues } from './features/knownIssues/knownIssues';
@@ -14,6 +14,8 @@ import { DashboardActions } from './resources/fetchDashboardResources/dashboardS
 import { fetchLanguage } from './resources/fetchLanguage/languageSlice';
 
 import './App.css';
+import { AltinnButton } from 'app-shared/components';
+import { post } from 'app-shared/utils/networking';
 
 const theme = createTheme(altinnTheme);
 
@@ -21,7 +23,7 @@ export const App = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: any) => state.dashboard.user);
 
-  useEffect(() => {
+  React.useEffect(() => {
     dispatch(
       DashboardActions.fetchCurrentUser({
         url: `${window.location.origin}/designerapi/User/Current`,
@@ -48,10 +50,17 @@ export const App = () => {
     );
   }, [dispatch]);
 
+  const [showLogOutButton, setShowLogoutButton] = React.useState(false);
+  React.useEffect(()=> {
+    if (!user){
+      setTimeout(()=> setShowLogoutButton(true), 5000);
+    }
+  }, [user]);
+
   return (
     <MuiThemeProvider theme={theme}>
       <Router>
-        <div>
+        {user ? <div>
           <AppBarComponent
             org={user.full_name || user.login}
             app={null}
@@ -90,10 +99,25 @@ export const App = () => {
             exact={true}
             component={StandaloneDataModelling}
           />
-        </div>
+        </div> : <Grid justifyContent='center'>
+          <AltinnSpinner spinnerText='Venter på svar' />
+          {showLogOutButton && <AltinnButton
+            onClickFunction={handleLogout}
+            btnText={'Logg ut'}
+          />}
+        </Grid>
+        }
       </Router>
     </MuiThemeProvider>
   );
 };
 
 export default App;
+const handleLogout = () => {
+  const altinnWindow: Window = window;
+  const url = `${altinnWindow.location.origin}/repos/user/logout`;
+  post(url).then(() => {
+    window.location.assign(`${altinnWindow.location.origin}/Home/Logout`);
+  });
+  return true;
+}
