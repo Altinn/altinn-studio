b08f28a93a0278f34d19f75039771d463c2e8a63
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2021-11-08 08:38:11 +0100

    chore: remove lint warnings from gulpfile

--- src/studio/src/designer/backend/gulpfile.js
+++ src/studio/src/designer/backend/gulpfile.js
@@ -4,23 +4,36 @@ const chokidar = require('chokidar');
 const del = require('del');
 const fs = require('fs');
 
+// When specifying options, you need to add all options to avoid lint errors.
+// This can be removed if/when https://github.com/Klathmon/gulp-run-command/pull/11 is released
+const defaultGulpRunOptions = {
+  quiet: false,
+  ignoreErrors: false,
+  timeout: undefined,
+  env: {},
+};
+
 const cleanGlobs = [
-  "wwwroot/designer/css/lib/**/*.css",
-  "wwwroot/designer/js/lib/**/*.js",
-  "wwwroot/designer/css/font-awesome/*.css",
-  "wwwroot/designer/js/lib/**",
-  "wwwroot/designer/css/lib/**",
-  "wwwroot/designer/css/bootstrap*.css",
-  "wwwroot/designer/css/font/fontawesome*.*"
+  'wwwroot/designer/css/lib/**/*.css',
+  'wwwroot/designer/js/lib/**/*.js',
+  'wwwroot/designer/css/font-awesome/*.css',
+  'wwwroot/designer/js/lib/**',
+  'wwwroot/designer/css/lib/**',
+  'wwwroot/designer/css/bootstrap*.css',
+  'wwwroot/designer/css/font/fontawesome*.*',
 ];
 
-
 const jsServDevFile = '../frontend/dist/app-development/app-development.js';
-const jsServDevModuleFile0 = '../frontend/dist/app-development/1.app-development.js';
-const jsServDevModuleFile1 = '../frontend/dist/app-development/2.app-development.js';
-const jsServDevModuleFile2 = '../frontend/dist/app-development/3.app-development.js';
-const jsServDevModuleFile3 = '../frontend/dist/app-development/4.app-development.js';
-const jsServDevMonacoWorker1 = '../frontend/dist/app-development/editor.worker.js';
+const jsServDevModuleFile0 =
+  '../frontend/dist/app-development/1.app-development.js';
+const jsServDevModuleFile1 =
+  '../frontend/dist/app-development/2.app-development.js';
+const jsServDevModuleFile2 =
+  '../frontend/dist/app-development/3.app-development.js';
+const jsServDevModuleFile3 =
+  '../frontend/dist/app-development/4.app-development.js';
+const jsServDevMonacoWorker1 =
+  '../frontend/dist/app-development/editor.worker.js';
 const jsServDevMonacoWorker2 = '../frontend/dist/app-development/ts.worker.js';
 const jsDashboardFile = '../frontend/dist/dashboard/dashboard.js';
 const cssServDevFile = '../frontend/dist/app-development/app-development.css';
@@ -29,82 +42,85 @@ const cssDashboardFile = '../frontend/dist/dashboard/dashboard.css';
 let jsWatcher = null;
 let cssWatcher = null;
 
-const copyGlobs = [{
-    src: "node_modules/bootstrap/dist/css/bootstrap*.css",
-    dest: "wwwroot/designer/css/"
+const copyGlobs = [
+  {
+    src: 'node_modules/bootstrap/dist/css/bootstrap*.css',
+    dest: 'wwwroot/designer/css/',
   },
   {
-    src: "node_modules/jquery-ui-dist/*.js",
-    dest: "wwwroot/designer/js/lib/jquery-ui/"
+    src: 'node_modules/jquery-ui-dist/*.js',
+    dest: 'wwwroot/designer/js/lib/jquery-ui/',
   },
   {
-    src: "node_modules/bootstrap/dist/js/bootstrap*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/bootstrap/dist/js/bootstrap*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/json-editor/dist/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/json-editor/dist/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/select2/dist/js/select2.full.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/select2/dist/js/select2.full.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/select2/dist/css/select2.css",
-    dest: "wwwroot/designer/css/lib"
+    src: 'node_modules/select2/dist/css/select2.css',
+    dest: 'wwwroot/designer/css/lib',
   },
   {
-    src: "node_modules/jquery/dist/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/jquery/dist/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/requirejs/require.js",
-    dest: "wwwroot/designer/js/lib/"
+    src: 'node_modules/requirejs/require.js',
+    dest: 'wwwroot/designer/js/lib/',
   },
   {
-    src: "node_modules/underscore/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/underscore/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/requirejs-text/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/requirejs-text/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/js-beautify/js/lib/beautify*.js",
-    dest: "wwwroot/designer/js/lib/"
+    src: 'node_modules/js-beautify/js/lib/beautify*.js',
+    dest: 'wwwroot/designer/js/lib/',
   },
   {
-    src: "node_modules/sightglass/*.js",
-    dest: "wwwroot/designer/js/lib/"
+    src: 'node_modules/sightglass/*.js',
+    dest: 'wwwroot/designer/js/lib/',
   },
   {
-    src: "node_modules/rivets/dist/*.js",
-    dest: "wwwroot/designer/js/lib/"
+    src: 'node_modules/rivets/dist/*.js',
+    dest: 'wwwroot/designer/js/lib/',
   },
   {
-    src: "node_modules/jquery-validation-unobtrusive/dist/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/jquery-validation-unobtrusive/dist/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/jquery-validation/dist/*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/jquery-validation/dist/*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/popper.js/dist/umd/*.*.js",
-    dest: "wwwroot/designer/js/lib"
+    src: 'node_modules/popper.js/dist/umd/*.*.js',
+    dest: 'wwwroot/designer/js/lib',
   },
   {
-    src: "node_modules/monaco-editor/min/**/*.*",
-    dest: "wwwroot/designer/js/lib/monaco-editor"
+    src: 'node_modules/monaco-editor/min/**/*.*',
+    dest: 'wwwroot/designer/js/lib/monaco-editor',
   },
   {
-    src: "node_modules/bootstrap-list-filter/bootstrap-list-filter.min.js",
-    dest: "wwwroot/designer/js/lib"
-  }
+    src: 'node_modules/bootstrap-list-filter/bootstrap-list-filter.min.js',
+    dest: 'wwwroot/designer/js/lib',
+  },
 ];
 
 function copyNodeModulePackages(cb) {
-  copyGlobs.map(copyGlob => gulp.src(copyGlob.src).pipe(gulp.dest(copyGlob.dest)));
+  copyGlobs.map((copyGlob) =>
+    gulp.src(copyGlob.src).pipe(gulp.dest(copyGlob.dest)),
+  );
   cb();
 }
 
@@ -136,12 +152,24 @@ function copyDashboardJs() {
 function copyServDevJs() {
   setTimeout(function () {
     gulp.src(jsServDevFile).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevModuleFile0).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevModuleFile1).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevModuleFile2).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevModuleFile3).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevMonacoWorker1).pipe(gulp.dest('./wwwroot/designer/js/react'));
-    gulp.src(jsServDevMonacoWorker2).pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevModuleFile0)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevModuleFile1)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevModuleFile2)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevModuleFile3)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevMonacoWorker1)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
+    gulp
+      .src(jsServDevMonacoWorker2)
+      .pipe(gulp.dest('./wwwroot/designer/js/react'));
   }, 1000);
   return;
 }
@@ -216,75 +244,102 @@ function setupWatchers(cb) {
   cb();
 }
 
-gulp.task('build', gulp.series([
-  copyNodeModulePackages,
-]));
+gulp.task('build', gulp.series([copyNodeModulePackages]));
 
-gulp.task('copy-files', gulp.series(
-  copyNodeModulePackages,
-  copyReactJs,
-  copyReactCss
-));
+gulp.task(
+  'copy-files',
+  gulp.series(copyNodeModulePackages, copyReactJs, copyReactCss),
+);
 
-gulp.task('clean', gulp.series(
-  deleteServDevCss,
-  deleteDashboardCss,
-  deleteServDevJs,
-  deleteDashboardJs,
-  cleanNodeModulePackages,
-  run('npm run clean', {
-    cwd: '../frontend/dashboard',
-  }),
-  run('npm run clean', {
-    cwd: '../frontend/app-development',
-  })
-));
+gulp.task(
+  'clean',
+  gulp.series(
+    deleteServDevCss,
+    deleteDashboardCss,
+    deleteServDevJs,
+    deleteDashboardJs,
+    cleanNodeModulePackages,
+    run('npm run clean', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/dashboard',
+    }),
+    run('npm run clean', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/app-development',
+    }),
+  ),
+);
 
-gulp.task('develop', gulp.parallel(
-  copyNodeModulePackages,
-  setupWatchers,
-  run('dotnet run'),
-  run('npm run webpack-watch', {
-    cwd: '../frontend/app-development',
-  })
-));
+gulp.task(
+  'develop',
+  gulp.parallel(
+    copyNodeModulePackages,
+    setupWatchers,
+    run('dotnet run'),
+    run('npm run webpack-watch', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/app-development',
+    }),
+  ),
+);
 
-gulp.task('develop-designer-frontend', gulp.parallel(
-  copyNodeModulePackages,
-  setupWatchers,
-  run('npm run webpack-watch', {
-    cwd: '../frontend/app-development',
-  })
-));
+gulp.task(
+  'develop-designer-frontend',
+  gulp.parallel(
+    copyNodeModulePackages,
+    setupWatchers,
+    run('npm run webpack-watch', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/app-development',
+    }),
+  ),
+);
 
-gulp.task('develop-dashboard', gulp.parallel(
-  copyNodeModulePackages,
-  setupWatchers,
-  run('dotnet run'),
-  run('npm run webpack-watch', {
-    cwd: '../frontend/dashboard',
-  })
-));
+gulp.task(
+  'develop-dashboard',
+  gulp.parallel(
+    copyNodeModulePackages,
+    setupWatchers,
+    run('dotnet run'),
+    run('npm run webpack-watch', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/dashboard',
+    }),
+  ),
+);
 
-gulp.task('build-ux-editor', gulp.series(
-  run('npm run build', {
-    cwd: '../frontend/ux-editor',
-  }),
-  'copy-files'
-));
+gulp.task(
+  'build-ux-editor',
+  gulp.series(
+    run('npm run build', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/ux-editor',
+    }),
+    'copy-files',
+  ),
+);
 
-gulp.task('install-react-app-dependencies', gulp.series(
-  run('lerna bootstrap --hoist --ci', {
-    cwd: '../Frontend',
-  })
-));
+gulp.task(
+  'install-react-app-dependencies',
+  gulp.series(
+    run('lerna bootstrap --hoist --ci', {
+      ...defaultGulpRunOptions,
+      cwd: '../Frontend',
+    }),
+  ),
+);
 
-gulp.task('default', gulp.series([
-  run('npm run build', {
-    cwd: '../frontend/app-development',
-  }),
-  run('npm run build', {
-    cwd: '../frontend/dashboard',
-  }),
-  'copy-files'
-]));
+gulp.task(
+  'default',
+  gulp.series([
+    run('npm run build', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/app-development',
+    }),
+    run('npm run build', {
+      ...defaultGulpRunOptions,
+      cwd: '../frontend/dashboard',
+    }),
+    'copy-files',
+  ]),
+);
