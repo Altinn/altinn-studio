e89d2952aa2be23ab3ca73c6d3e2d90756359954
Rune T. Larsen <rune@hjemmekino.no>
2021-11-16 13:11:19 +0100

    Removed es lint disabling

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
@@ -2,7 +2,6 @@ import { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 import { watchGetApplicationSettingsSaga } from './fetch/fetchApplicationSettingsSaga';
 
-// eslint-disable-next-line func-names
 export default function* applicationSettingsSagas(): SagaIterator {
   yield fork(watchGetApplicationSettingsSaga);
 }
