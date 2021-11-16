52cba3a720e00a4f496accd72ffabf7f3c089414
Rune Tømmerås Larsen <rune@hjemmekino.no>
2021-11-16 13:10:40 +0100

    Update src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
@@ -3,6 +3,6 @@ import { fork } from 'redux-saga/effects';
 import { watchGetApplicationSettingsSaga } from './fetch/fetchApplicationSettingsSaga';
 
 // eslint-disable-next-line func-names
-export default function* (): SagaIterator {
+export default function* applicationSettingsSagas(): SagaIterator {
   yield fork(watchGetApplicationSettingsSaga);
 }
