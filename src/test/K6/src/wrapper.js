621fab582b81c4c0e4753a2ab78730f2af6a6f70
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-11-17 08:59:30 +0100

    print retry count to the console

--- src/test/K6/src/wrapper.js
+++ src/test/K6/src/wrapper.js
@@ -4,25 +4,29 @@ import { sleep } from 'k6';
 //wrapper functions around k6 http methods enabling retrying requests when response code is 0, 408 and > 500
 
 export function httpPost(url, body, params) {
-  var res;
+  var res,
+    retriedCount = 0;
   for (var retries = 3; retries > 0; retries--) {
     res = http.post(url, body, params);
     if (res.status != 0 && res.status != 408 && res.status < 500) {
       return res;
     }
     sleep(10);
+    console.log(`Retry number: ${++retriedCount}`);
   }
   return res;
 }
 
 export function httpGet(url, params) {
-  var res;
+  var res,
+    retriedCount = 0;
   for (var retries = 3; retries > 0; retries--) {
     res = http.get(url, params);
     if (res.status != 0 && res.status != 408 && res.status < 500) {
       return res;
     }
     sleep(10);
+    console.log(`Retry number: ${++retriedCount}`);
   }
   return res;
 }
