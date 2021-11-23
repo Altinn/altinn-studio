8d7bcda98a3c31dd20300f9171b1a10384684e4a
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-11-23 08:17:38 +0100

    chore: reverted removeal of auth

--- src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Controllers/ReceiptController.cs
+++ src/Altinn.Platform/Altinn.Platform.Receipt/Receipt/Controllers/ReceiptController.cs
@@ -20,6 +20,7 @@ namespace Altinn.Platform.Receipt
     /// <summary>
     /// Contains all actions for receipt
     /// </summary>
+    [Authorize]
     [ApiController]
     public class ReceiptController : Controller
     {
