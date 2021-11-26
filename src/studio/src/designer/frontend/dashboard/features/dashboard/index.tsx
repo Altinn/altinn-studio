45b4b8ae47906d82ac2057f4e48f1cd60bf1533f
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2021-11-26 13:47:18 +0100

    feat: add add repo button to new dashboard

--- src/studio/src/designer/frontend/dashboard/features/dashboard/index.tsx
+++ src/studio/src/designer/frontend/dashboard/features/dashboard/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 
+import { CreateNewService } from '../createService/createNewService';
+
 import { FavoriteReposList } from './FavoriteReposList';
 import { OrgReposList } from './OrgReposList';
 
 export const Dashboard = () => {
   return (
     <div style={{ marginTop: '50px' }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <CreateNewService />
+      </div>
+
       <FavoriteReposList />
 
       <div style={{ marginTop: '50px' }}>
