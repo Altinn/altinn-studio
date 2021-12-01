c0479401df0f7fdd0fa6dc1ff7e1a8773e5290d3
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2021-12-01 13:44:50 +0100

    chore: export util method used by receipt

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/sagas/index.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/sagas/index.ts
@@ -1,4 +1,4 @@
-import { SagaIterator } from 'redux-saga';
+import createSagaMiddleware, { SagaIterator, SagaMiddleware } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 
 import FormDataSagas from '../features/form/data/formDataSagas';
@@ -20,7 +20,8 @@ import TextResourcesSagas from '../shared/resources/textResources/textResourcesS
 import IsLoadingSagas from '../shared/resources/isLoading/isLoadingSagas';
 import QueueSagas from '../shared/resources/queue/queueSagas';
 import OptionSagas from '../shared/resources/options/optionsSagas';
-import { sagaMiddleware } from 'src/store';
+
+export const sagaMiddleware: SagaMiddleware = createSagaMiddleware();
 
 function* root(): SagaIterator {
   yield fork(FormDataSagas);
