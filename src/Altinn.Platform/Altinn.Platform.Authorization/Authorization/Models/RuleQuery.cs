b9eda19339f36492bc4e8961ef1ba43ebf9ebf36
Ivar Tryti <oit@brreg.no>
2021-12-08 13:36:45 +0100

    broke down a long summary line into three shorter lines

--- src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Models/RuleQuery.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Authorization/Models/RuleQuery.cs
@@ -4,7 +4,9 @@ using System.ComponentModel.DataAnnotations;
 namespace Altinn.Platform.Authorization.Models
 {
     /// <summary>
-    /// Queries for a list of already delegated rules from the supplied offeredby for the supplied app. If coveredby has any key roles, those party ids can be included in the query to have the 3.0 PIP lookup those as well. If offeredby is a sub unit, parenty party id can be supplied to include rules given from that party as well.
+    /// Queries for a list of already delegated rules from the supplied offeredby for the supplied app. If coveredby
+    /// has any key roles, those party ids can be included in the query to have the 3.0 PIP lookup those as well.
+    /// If offeredby is a sub unit, parenty party id can be supplied to include rules given from that party as well.
     /// </summary>
     public class RuleQuery
     {
