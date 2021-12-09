8a5e31d950b127d20f2b9a6eb08bac78fd4519c4
acn-sbuad <stephanie.buadu@avanade.com>
2021-12-09 15:40:10 +0100

    Added service for validating access token

---
+++ src/Altinn.Common/Altinn.Common.AccessToken/Altinn.Common.AccessToken/Services/AccessTokenValidator.cs
@@ -0,0 +1,65 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace Altinn.Common.AccessToken.Services
+{
+    public class AccessTokenValidator : IAccessTokenValidator
+    {
+        private readonly ISigningKeysResolver _signingKeysResolver;
+        private readonly ILogger<IAccessTokenValidator> _logger;
+
+        public AccessTokenValidator(
+            ISigningKeysResolver signingKeysResolver,
+            ILogger<IAccessTokenValidator> logger)
+        {
+            _signingKeysResolver = signingKeysResolver;
+            _logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> ValidateAccessToken(string token)
+        {
+            JwtSecurityTokenHandler validator = new JwtSecurityTokenHandler();
+
+            if (!validator.CanReadToken(token))
+            {
+                return false;
+            }
+
+            JwtSecurityToken jwt = validator.ReadJwtToken(token);
+            TokenValidationParameters validationParameters = await GetTokenValidationParameters(jwt.Issuer);
+
+            try
+            {
+                validator.ValidateToken(token, validationParameters, out _);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning("Failed to validate token.", ex);
+            }
+
+            return false;
+        }
+
+        private async Task<TokenValidationParameters> GetTokenValidationParameters(string issuer)
+        {
+            TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            tokenValidationParameters.IssuerSigningKeys = await _signingKeysResolver.GetSigningKeys(issuer);
+            return tokenValidationParameters;
+        }
+    }
+}
---
+++ src/Altinn.Common/Altinn.Common.AccessToken/Altinn.Common.AccessToken/Services/IAccessTokenValidator.cs
@@ -0,0 +1,19 @@
+using Microsoft.IdentityModel.Tokens;
+
+using System.Threading.Tasks;
+
+namespace Altinn.Common.AccessToken.Services
+{
+    /// <summary>
+    /// Interface to validate access token
+    /// </summary>
+    public interface IAccessTokenValidator
+    {
+        /// <summary>
+        /// Validates an access token
+        /// </summary>
+        /// <param name="token">The token to validate</param>
+        /// <returns>A boolean indicating the validity of the token</returns>
+        public Task<bool> ValidateAccessToken(string token);
+    }
+}
