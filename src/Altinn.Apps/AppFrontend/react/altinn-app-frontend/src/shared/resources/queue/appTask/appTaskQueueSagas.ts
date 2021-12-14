8b3d4372b73cbcd48f36f4f2a3ca3287d0cc6e10
Rune Tømmerås Larsen <rune@hjemmekino.no>
2021-12-14 17:38:39 +0100

    Feature/7173 OIDC frontend (#7468)
    * Added app settings frontend
    
    * debuging
    
    * slice
    
    * updated to toolkit implementation
    
    * Fixed reducers
    
    * Fixed redux
    
    * removed linting ignore
    
    * Update src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/utils/urlHelper.ts
    
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
    
    * Update src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/features/entrypoint/Entrypoint.tsx
    
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
    
    * Fixed compile error
    
    * Update src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/applicationSettings/applicationSettingsSagas.ts
    
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
    
    * Removed es lint disabling
    
    * added first test. Does it work?
    
    * more test updates
    
    * updated tests
    
    * Addded missing param
    
    * es lint diasble
    
    * Added 404 error handling
    
    * types
    
    * Use app selector
    
    * Updated version
    
    * mock data
    
    * Removed non used declaration
    
    * chore: fixed indent in app.tsx
    
    * test: tests for sagas + reducer
    
    * chore: code smell
    
    * chore: tests + fixed bug in fetch applicaiton settings saga
    
    * chore: fixed code smell + actual working test
    
    * chore: fixing them tests
    
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
    Co-authored-by: EKEBERG Steffen <steffen.ekeberg@soprasteria.com>

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/queue/appTask/appTaskQueueSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/queue/appTask/appTaskQueueSagas.ts
@@ -2,6 +2,8 @@ import { SagaIterator } from 'redux-saga';
 import { call, put, take } from 'redux-saga/effects';
 import { startInitialAppTaskQueue, startInitialAppTaskQueueFulfilled } from '../queueSlice';
 import ApplicationMetadataActions from '../../applicationMetadata/actions';
+import { ApplicationSettingsActions } from '../../applicationSettings/applicationSettingsSlice';
+
 import TextResourcesActions from '../../textResources/textResourcesActions';
 import ProfileActions from '../../profile/profileActions';
 import LanguageActions from '../../language/languageActions';
@@ -9,6 +11,7 @@ import PartyActions from '../../party/partyActions';
 import { profileApiUrl } from '../../../../utils/urlHelper';
 
 export function* startInitialAppTaskQueueSaga(): SagaIterator {
+  yield put(ApplicationSettingsActions.fetchApplicationSettings());
   yield call(ProfileActions.fetchProfile, profileApiUrl);
   yield call(TextResourcesActions.fetchTextResources);
   yield call(LanguageActions.fetchLanguage);
