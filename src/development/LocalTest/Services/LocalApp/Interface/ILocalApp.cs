f36a1b2390494470fb677250a21246f1762867f3
Ivar Nesje <ivar.nesje@finanstilsynet.no>
2021-12-14 09:37:12 +0100

    Changes required to run LocalTest in http mode
    Originaly proposed as part of #7525

---
+++ src/development/LocalTest/Services/LocalApp/Interface/ILocalApp.cs
@@ -0,0 +1,17 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Altinn.Platform.Storage.Interface.Models;
+
+namespace LocalTest.Services.LocalApp.Interface
+{
+    public interface ILocalApp
+    {
+        Task<string?> GetXACMLPolicy(string appId);
+
+        Task<Application?> GetApplicationMetadata(string appId);
+
+        Task<Dictionary<string, Application>> GetApplications();
+    }
+}
