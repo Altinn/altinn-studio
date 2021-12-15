8e9a5417b6537c6eca0939ac69161eb7f197ba65
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2021-12-15 12:59:19 +0100

    test: introduce msw to easily intercept api calls and mock responses, and testing-library to more easily write tests like a user would interact with the page

--- src/studio/src/designer/frontend/dashboard/test/setupTests.ts
+++ src/studio/src/designer/frontend/dashboard/test/setupTests.ts
@@ -2,6 +2,7 @@ import * as Enzyme from 'enzyme';
 import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import 'jest';
+import '@testing-library/jest-dom/extend-expect';
 import 'whatwg-fetch';
 
 Enzyme.configure({
---
+++ src/studio/src/designer/frontend/dashboard/test/testUtils.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import type { RenderOptions } from '@testing-library/react';
+import type { PreloadedState } from '@reduxjs/toolkit';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import { setupStore } from 'app/store';
+import type { AppStore, RootState } from 'app/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  component: any,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) => {
+  function Wrapper({ children }: React.PropsWithChildren<unknown>) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return {
+    store,
+    ...rtlRender(component, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+  };
+};
+
+export const handlers = [
+  rest.get('http://localhost/designer/api/v1/orgs', (req, res, ctx) => {
+    const mockApiResponse = [
+      {
+        avatar_url: 'avatar.png',
+        description: '',
+        full_name: 'test-org',
+        id: 1,
+        location: '',
+        username: 'org-username',
+        website: '',
+      },
+    ];
+    return res(ctx.json(mockApiResponse));
+  }),
+];
+
+export { setupServer, rest };
