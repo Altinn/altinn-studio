73e9e3769968839fe750df566845284aeeadc202
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-12-21 09:30:17 +0100

    tests for new app creation modal (#7715)
    * tests for new app creation modal
    
    * fixed flaky favourite test

--- src/test/cypress/e2e/pageobjects/header.js
+++ src/test/cypress/e2e/pageobjects/header.js
@@ -1,11 +1,13 @@
 //Selectors in header of studio common to all pages
 export const header = {
   profileIcon: '#profile-icon-button',
+  profileIconDesigner: "button[aria-label*='profilikon']",
   menu: {
     all: '#menu-all',
     self: '#menu-self',
     org: "[id='menu-org*']",
     gitea: '#menu-gitea',
     logOut: '#menu-logout',
+    openRepo: 'a[href*="repos"]',
   },
 };
