a6b0c0e93794ffb0118bb35fcc39a3d8edd819b3
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2021-12-23 10:55:49 +0100

    Test/new appfrontend tests (#7743)
    * new test for task validation and labelgrid
    
    * updated config
    
    * stateless to statefull

--- src/test/cypress/e2e/integration/app-frontend/validation.js
+++ src/test/cypress/e2e/integration/app-frontend/validation.js
@@ -9,11 +9,8 @@ const appFrontend = new AppFrontend();
 const mui = new Common();
 
 describe('Validation', () => {
-  beforeEach(() => {
-    cy.navigateToChangeName();
-  });
-
   it('Required field validation on blur', () => {
+    cy.navigateToChangeName();
     cy.get(appFrontend.changeOfName.newFirstName).should('be.visible').focus().blur();
     cy.get(appFrontend.fieldValidationError.replace('field', appFrontend.changeOfName.newFirstName.substr(1)))
       .should('exist')
@@ -24,6 +21,7 @@ describe('Validation', () => {
   });
 
   it('Custom field validation - error', () => {
+    cy.navigateToChangeName();
     cy.intercept('GET', '**/validate').as('validateData');
     cy.get(appFrontend.changeOfName.newFirstName).should('be.visible').type('test').blur();
     cy.wait('@validateData');
@@ -38,6 +36,7 @@ describe('Validation', () => {
   });
 
   it('Custom field validation - warning', () => {
+    cy.navigateToChangeName();
     cy.intercept('GET', '**/validate').as('validateData');
     cy.get(appFrontend.changeOfName.newMiddleName).should('be.visible').type('test').blur();
     cy.wait('@validateData');
@@ -49,6 +48,7 @@ describe('Validation', () => {
   });
 
   it('Page validation on clicking next', () => {
+    cy.navigateToChangeName();
     cy.get(appFrontend.changeOfName.newFirstName).should('be.visible').clear().type('test').blur();
     cy.get(appFrontend.changeOfName.confirmChangeName).should('be.visible').find('input').check();
     cy.intercept('GET', '**/validate').as('validateData');
@@ -62,6 +62,7 @@ describe('Validation', () => {
   });
 
   it('Validation on uploaded attachment type', () => {
+    cy.navigateToChangeName();
     cy.get(appFrontend.changeOfName.upload).attachFile('test.png');
     cy.get(appFrontend.fieldValidationError.replace('field', appFrontend.changeOfName.upload.substr(1)))
       .should('exist')
@@ -70,10 +71,30 @@ describe('Validation', () => {
   });
 
   it('Client side validation from json schema', () => {
+    cy.navigateToChangeName();
     cy.get(appFrontend.changeOfName.newLastName).should('be.visible').type('client').blur();
     cy.get(appFrontend.fieldValidationError.replace('field', appFrontend.changeOfName.newLastName.substr(1)))
       .should('exist')
       .should('be.visible')
       .should('have.text', texts.clientSide);
+  });
+
+  it('Task validation', () => {
+    cy.intercept('**/active', []).as('noActiveInstances');
+    cy.startAppInstance(Cypress.env('multiData2Stage'));
+    cy.get(appFrontend.closeButton).should('be.visible');
+    cy.intercept('GET', '**/validate', [
+      {
+        severity: 1,
+        code: 'error',
+        description: 'task validation',
+      },
+    ]);
+    cy.get(appFrontend.sendinButton).should('be.visible').click();
+    cy.get(appFrontend.errorReport)
+      .should('exist')
+      .should('be.visible')
+      .should('be.focused')
+      .should('contain.text', 'task validation');
   });
 });
