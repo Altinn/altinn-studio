d40a3b1d06d2a2194bd5c6b79703c441c4d346db
Jeevananthan Kesavan <49391725+jeevananthank@users.noreply.github.com>
2022-01-03 16:19:32 +0100

    new tests for add, get, delete rules

--- src/test/K6/src/api/platform/authorization/delegations.js
+++ src/test/K6/src/api/platform/authorization/delegations.js
@@ -26,3 +26,140 @@ export function getPolicies(appOwner, appName) {
   ];
   return http.post(endpoint, JSON.stringify(body), params);
 }
+
+/**
+ * POST api call to add a rule that delagates access to an user
+ * @param {*} altinnToken token for authorizing the request
+ * @param {*} policyMatchKeys keys to be populated in the request
+ * @param {*} delegatedByUserId user id of the user who delegates the rule
+ * @param {*} offeredByPartyId party id of the user who offers the rule
+ * @param {*} coveredById user id or party id of whom that receives the rule
+ * @param {*} appOwner
+ * @param {*} appName
+ * @param {*} altinnTask Task_1, EndEvent_1
+ * @param {*} altinnAction read,write,sign
+ * @returns response of the POST request
+ */
+export function addRules(altinnToken, policyMatchKeys, delegatedByUserId, offeredByPartyId, coveredById, appOwner, appName, altinnTask, altinnAction) {
+  var endpoint = config.platformAuthorization.addRules;
+  var params = header.buildHearderWithRuntimeandJson(altinnToken, 'platform');
+  var body = [];
+  body.push(generatePolicyMatch(policyMatchKeys, delegatedByUserId, offeredByPartyId, coveredById, appOwner, appName, altinnTask, altinnAction));
+  return http.post(endpoint, JSON.stringify(body), params);
+}
+
+/**
+ * POST call to get rules matching a criteria
+ * @param {*} altinnToken token for authorizing the request
+ * @param {*} policyMatchKeys keys to be populated in the request
+ * @param {*} offeredByPartyId party id of the user who offers the rule
+ * @param {*} coveredById user id or party id of whom that receives the rule
+ * @param {*} appOwner
+ * @param {*} appName
+ * @param {*} altinnTask Task_1, EndEvent_1
+ * @returns response of the POST request
+ */
+export function getRules(altinnToken, policyMatchKeys, offeredByPartyId, coveredById, appOwner, appName, altinnTask) {
+  var endpoint = config.platformAuthorization.getRules;
+  var params = header.buildHearderWithRuntimeandJson(altinnToken, 'platform');
+  var body = {};
+  body.policyMatches = [];
+  body.policyMatches.push(generatePolicyMatch(policyMatchKeys, null, offeredByPartyId, coveredById, appOwner, appName, altinnTask, null));
+  body.keyRolePartyIds = [];
+  return http.post(endpoint, JSON.stringify(body), params);
+}
+
+/**
+ * POSt call to delete a delegated rule based on rule id
+ * @param {*} altinnToken token for authorizing the request
+ * @param {*} policyMatchKeys keys to be populated in the request
+ * @param {Array} ruleIds an array of guids
+ * @param {*} deletedByUserId user who deletes the rule
+ * @param {*} offeredByPartyId party id of the user who offers the rule
+ * @param {*} coveredById user id or party id of whom that receives the rule
+ * @param {*} appOwner 
+ * @param {*} appName 
+ * @param {*} altinnTask Task_1, EndEvent_1
+ * @param {*} altinnAction read,write,sign
+ * @returns  response of the POST request
+ */
+export function deleteRules(
+  altinnToken,
+  policyMatchKeys,
+  ruleIds,
+  deletedByUserId,
+  offeredByPartyId,
+  coveredById,
+  appOwner,
+  appName,
+  altinnTask,
+  altinnAction,
+) {
+  var endpoint = config.platformAuthorization.deleteRules;
+  var params = header.buildHearderWithRuntimeandJson(altinnToken, 'platform');
+  var body = [{}];
+  body[0].policyMatch = generatePolicyMatch(policyMatchKeys, null, offeredByPartyId, coveredById, appOwner, appName, altinnTask, altinnAction);
+  body[0].ruleIds = ruleIds;
+  body[0].deletedByUserId = deletedByUserId;
+  return http.post(endpoint, JSON.stringify(body), params);
+}
+
+/**
+ * function to build a policy match with action, user and resource details
+ * @param {*} policyMatch Template
+ * @param {*} policyMatchKeys keys to be populated in policy match
+ * @param {*} delegatedByUserId user id of the user who delegates the rule
+ * @param {*} offeredByPartyId party id of the user who offers the rule
+ * @param {*} coveredById user id or party id of whom that receives the rule
+ * @param {*} appOwner
+ * @param {*} appName
+ * @param {*} altinnTask Task_1, EndEvent_1
+ * @param {*} altinnAction read,write,sign
+ * @returns json object of a completed policy match
+ */
+function generatePolicyMatch(policyMatchKeys, delegatedByUserId, offeredByPartyId, coveredById, appOwner, appName, altinnTask, altinnAction) {
+  var policyMatch = {
+    coveredBy: [],
+    resource: [],
+  };
+  var coveredBy = policyMatchKeys['coveredBy'];
+  var resources = policyMatchKeys['resource'];
+
+  if (coveredById && coveredBy) {
+    policyMatch.coveredBy.push({});
+    policyMatch.coveredBy[0].id = coveredBy;
+    policyMatch.coveredBy[0].value = coveredById.toString();
+  }
+
+  if (altinnAction) {
+    policyMatch.action = {};
+    policyMatch.action.id = 'urn:oasis:names:tc:xacml:1.0:action:action-id';
+    policyMatch.action.value = altinnAction;
+  }
+
+  if (delegatedByUserId) policyMatch.delegatedByUserId = delegatedByUserId;
+  if (offeredByPartyId) policyMatch.offeredByPartyId = offeredByPartyId;
+
+  for (var i = 0; i < resources.length; i++) {
+    var value = '';
+    policyMatch.resource.push({});
+    switch (resources[i]) {
+      case 'urn:altinn:org':
+        value = appOwner;
+        break;
+      case 'urn:altinn:app':
+        value = appName;
+        break;
+      case 'urn:altinn:task':
+        value = altinnTask;
+        break;
+      case 'urn:altinn:appresource':
+        value = 'events';
+        break;
+    }
+    policyMatch.resource[i].id = resources[i];
+    policyMatch.resource[i].value = value;
+  }
+
+  return policyMatch;
+}
--- src/test/K6/src/tests/platform/authorization/delegations.js
+++ src/test/K6/src/tests/platform/authorization/delegations.js
@@ -4,7 +4,7 @@
   -e env=*** -e org=*** -e app=***  -e tokengenuser=*** -e tokengenuserpwd=*** -e appsaccesskey=***
 */
 import { check } from 'k6';
-import { addErrorCount } from '../../../errorcounter.js';
+import { addErrorCount, stopIterationOnFail } from '../../../errorcounter.js';
 import * as delegation from '../../../api/platform/authorization/delegations.js';
 import { generateToken } from '../../../api/altinn-testtools/token-generator.js';
 import { generateJUnitXML, reportPath } from '../../../report.js';
@@ -34,11 +34,59 @@ export function setup() {
 //Tests for platform Authorization:Delegations
 export default function (data) {
   const altinnToken = data;
-  var res, success;
+  var res, success, policyMatchKeys, ruleId;
 
   res = delegation.getPolicies(appOwner, appName);
   success = check(res, {
     'GET app policy - status is 200': (r) => r.status === 200,
+  });
+  addErrorCount(success);
+
+  policyMatchKeys = {
+    coveredBy: 'urn:altinn:userid',
+    resource: ['urn:altinn:app', 'urn:altinn:org', 'urn:altinn:task'],
+  };
+  res = delegation.addRules(altinnToken, policyMatchKeys, 111, 123, 456, appOwner, appName, 'Task_1', 'read');
+  success = check(res, {
+    'Add delegation rule - status is 201': (r) => r.status === 201,
+    'Add delegation rule - rule id is not empty': (r) => r.json('0.ruleId') != null,
+    'Add delegation rule - createdSuccessfully is true': (r) => r.json('0.createdSuccessfully') === true,
+    'Add delegation rule - offeredByPartyId matches': (r) => r.json('0.offeredByPartyId') === 123,
+    'Add delegation rule - coveredBy matches': (r) => r.json('0.coveredBy.0.value') === '456',
+  });
+  addErrorCount(success);
+  stopIterationOnFail('Add delegation rule Failed', success, res);
+
+  ruleId = res.json('0.ruleId');
+
+  res = delegation.addRules(altinnToken, policyMatchKeys, 111, 123, 456, appOwner, 'test', 'Task_1', 'read');
+  success = check(res, {
+    'Add delegation rule for an invalid app - status is 400': (r) => r.status === 400,
+    'Add delegation rule for an invalid app - failed': (r) => r.body == 'Delegation could not be completed',
+  });
+  addErrorCount(success);
+
+  policyMatchKeys = {
+    coveredBy: 'urn:altinn:userid',
+    resource: ['urn:altinn:app', 'urn:altinn:org'],
+  };
+  res = delegation.getRules(altinnToken, policyMatchKeys, 123, 456, appOwner, appName, null);
+  success = check(res, {
+    'Get delegated rule - status is 200': (r) => r.status === 200,
+    'Get delegated rule - rule id matches': (r) => r.json('0.ruleId') === ruleId,
+    'Get delegated rule - createdSuccessfully is false': (r) => r.json('0.createdSuccessfully') === false,
+    'Get delegated rule - offeredByPartyId matches': (r) => r.json('0.offeredByPartyId') === 123,
+    'Get delegated rule - coveredBy matches': (r) => r.json('0.coveredBy.0.value') === '456',
+  });
+  addErrorCount(success);
+
+  policyMatchKeys = {
+    coveredBy: 'urn:altinn:userid',
+    resource: ['urn:altinn:app', 'urn:altinn:org', 'urn:altinn:task'],
+  };
+  res = delegation.deleteRules(altinnToken, policyMatchKeys, [ruleId], 111, 123, 456, appOwner, appName, 'Task_1', 'read');
+  success = check(res, {
+    'Delete delegated rule - status is 200': (r) => r.status === 200,
   });
   addErrorCount(success);
 }
