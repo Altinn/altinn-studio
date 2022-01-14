6e34a73bb5a58616c818d4c805ce39eddd232a48
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-01-14 09:00:04 +0100

    test: reduce duplication of test code to make sonarcloud happy

--- src/Altinn.Apps/AppFrontend/react/receipt/testUtils.ts
+++ src/Altinn.Apps/AppFrontend/react/receipt/testUtils.ts
@@ -22,6 +22,22 @@ const party: IParty = {
 
 const date = new Date();
 
+const applicationCommonFields: any = {
+  description: null,
+  allowedContentTypes: ['application/xml'],
+  allowedContributers: null,
+  appLogic: {
+    autoCreate: true,
+    classRef: 'Altinn.App.Models.MessageV1',
+    schemaRef: null,
+  },
+  taskId: 'Task_1',
+  maxSize: null,
+  maxCount: 1,
+  minCount: 1,
+  grouping: null,
+};
+
 export const handlers: any = [
   rest.get(
     'https://platform.at21.altinn.cloud/receipt/api/v1/instances/mockInstanceOwnerId/6697de17-18c7-4fb9-a428-d6a414a797ae',
@@ -145,88 +161,55 @@ export const handlers: any = [
         title: { nb: 'frontend-test' },
         dataTypes: [
           {
+            ...applicationCommonFields,
             id: 'message',
-            description: null,
             allowedContentTypes: ['application/xml'],
-            allowedContributers: null,
             appLogic: {
-              autoCreate: true,
+              ...applicationCommonFields.appLogic,
               classRef: 'Altinn.App.Models.MessageV1',
-              schemaRef: null,
             },
-            taskId: 'Task_1',
-            maxSize: null,
-            maxCount: 1,
-            minCount: 1,
-            grouping: null,
           },
           {
             id: 'fileUpload-message',
-            description: null,
             allowedContentTypes: null,
-            allowedContributers: null,
             appLogic: null,
-            taskId: 'Task_1',
             maxSize: 5,
             maxCount: 3,
             minCount: 0,
-            grouping: null,
           },
           {
             id: 'ServiceModel-test',
-            description: null,
-            allowedContentTypes: ['application/xml'],
-            allowedContributers: null,
             appLogic: {
-              autoCreate: true,
+              ...applicationCommonFields.appLogic,
+
               classRef: 'Altinn.App.Models.Skjema',
-              schemaRef: null,
             },
             taskId: 'Task_2',
-            maxSize: null,
-            maxCount: 1,
-            minCount: 1,
-            grouping: null,
           },
           {
             id: 'fileUpload-changename',
-            description: null,
             allowedContentTypes: null,
-            allowedContributers: null,
             appLogic: null,
             taskId: 'Task_2',
             maxSize: 5,
             maxCount: 3,
             minCount: 0,
-            grouping: null,
           },
           {
             id: 'nested-group',
-            description: null,
-            allowedContentTypes: ['application/xml'],
-            allowedContributers: null,
             appLogic: {
-              autoCreate: true,
+              ...applicationCommonFields.appLogic,
               classRef: 'Altinn.App.Models.NestedGroup',
-              schemaRef: null,
             },
             taskId: 'Task_3',
-            maxSize: null,
-            maxCount: 1,
-            minCount: 1,
-            grouping: null,
           },
           {
             id: 'ref-data-as-pdf',
-            description: null,
             allowedContentTypes: ['application/pdf'],
-            allowedContributers: null,
             appLogic: null,
             taskId: null,
-            maxSize: null,
             maxCount: 0,
             minCount: 0,
-            grouping: null,
           },
         ],
         partyTypesAllowed: {
@@ -257,12 +240,10 @@ export const handlers: any = [
             id: '37130.SkattemeldingEndringEtterFristPostdatadef37130.Label',
             value:
               'Post i RF-1030 Skattemelding for formues- og inntektsskatt som skal  endres',
-            variables: null,
           },
           {
             id: '37131.SkattemeldingEndringEtterFristOpprinneligBelopdatadef37131.Label',
             value: '1\\. **Endre fra**',
-            variables: null,
           },
           {
             id: '37132.SkattemeldingEndringEtterFristNyttBelopdatadef37132.Label',
@@ -277,118 +258,98 @@ export const handlers: any = [
           {
             id: '37133.SkattemeldingEndringEtterFristKommentardatadef37133.Label',
             value: 'Kommentarer',
-            variables: null,
           },
           {
             id: '68.OppgavegiverNavndatadef68.Label',
             value: 'Oppgave giver navn',
-            variables: null,
           },
           {
             id: 'BegrunnelseAnnet',
             value:
               'Forklar hvorfor du ønsker å ta navnet, og eventuelt hvilken tilknytning du har til navnet:',
-            variables: null,
           },
           {
             id: 'BegrunnelseGard1',
             value: 'Gårdsbruk du vil ta navnet fra',
-            variables: null,
           },
           {
             id: 'BegrunnelseGard2',
             value: 'Kommune gårdsbruket ligger i',
-            variables: null,
           },
-          { id: 'BegrunnelseGard3', value: 'Gårdsnummer', variables: null },
-          { id: 'BegrunnelseGard4', value: 'Bruksnummer', variables: null },
+          { id: 'BegrunnelseGard3', value: 'Gårdsnummer' },
+          { id: 'BegrunnelseGard4', value: 'Bruksnummer' },
           {
             id: 'BegrunnelseGard5',
             value: 'Forklar din tilknytning til gårdsbruket',
-            variables: null,
           },
           {
             id: 'BegrunnelseNyttNavn',
             value:
               'Du ønsker å ta et navn som du tror er nytt i Norge. Forklar hvorfor du ønsker dette navnet:',
-            variables: null,
           },
           {
             id: 'BegrunnelseSamboer1',
             value: 'Etternavn på samboer',
-            variables: null,
           },
           {
             id: 'BegrunnelseSamboer2',
             value: 'Fødselsnummer på samboer',
-            variables: null,
           },
           {
             id: 'BegrunnelseSlektskap',
             value:
               'Forklar hvem du tar navnet fra, og hvilken side slektskapet ligger',
-            variables: null,
           },
           {
             id: 'BegrunnelseSteforeldre',
             value:
               'Etternavn på ste- eller fosterforeldre du ønsker å ta navnet til',
-            variables: null,
           },
           {
             id: 'BegrunnelseValgNavn',
             value: 'Vennligst oppgi begrunnelse for endring av navn',
-            variables: null,
           },
           {
             id: 'BekreftNavnTittel',
             value: 'Bekreftelse av navn',
-            variables: null,
           },
-          { id: 'DineEndringer', value: 'Dine endringer', variables: null },
+          { id: 'DineEndringer', value: 'Dine endringer' },
           {
             id: 'EndreNavnFra',
             value: 'Du har valgt å endre:',
-            variables: null,
           },
-          { id: 'EndreNavnTil', value: 'Til:', variables: null },
-          { id: 'Epost', value: 'E-post', variables: null },
-          { id: 'Fodselsnummer', value: 'Fødselsnummer', variables: null },
+          { id: 'EndreNavnTil', value: 'Til:' },
+          { id: 'Epost', value: 'E-post' },
+          { id: 'Fodselsnummer', value: 'Fødselsnummer' },
           {
             id: 'HintEtternavn',
             value: 'Bindestrek må benyttes dersom du ønsker to etternavn.',
-            variables: null,
           },
           {
             id: 'HintMellomnavn',
             value:
               'Mellomnavn må være et navn som kan benyttes som etternavn. ',
-            variables: null,
           },
           {
             id: 'Kontaktinformasjon',
             value: 'Kontaktinformasjon',
-            variables: null,
           },
-          { id: 'NavarendeNavn', value: 'Nåværende navn', variables: null },
+          { id: 'NavarendeNavn', value: 'Nåværende navn' },
           {
             id: 'PersonNyttEtternavn',
             value: 'Nytt etternavn',
-            variables: null,
           },
-          { id: 'PersonNyttFornavn', value: 'Nytt fornavn', variables: null },
+          { id: 'PersonNyttFornavn', value: 'Nytt fornavn' },
           {
             id: 'PersonNyttFornavn.helptext',
             value:
               'Tenk deg godt om, det tar 10 år før du kan endre navn igjen',
-            variables: null,
           },
           {
             id: 'PersonNyttMellomnavn',
             value: 'Nytt mellomnavn',
-            variables: null,
           },
-          { id: 'ServiceName', value: 'frontend-test', variables: null },
+          { id: 'ServiceName', value: 'frontend-test' },
           {
             id: 'confirm.body',
             value: 'Vennligst vent, endrer navn til {1}.',
@@ -406,42 +367,35 @@ export const handlers: any = [
           {
             id: 'datofeltet',
             value: 'Når vil du at navnendringen skal skje?',
-            variables: null,
           },
           {
             id: 'error.testValue',
             value:
               "test er ikke en gyldig <a href='https://www.altinn.no/' target='_blank'>verdi</a>",
-            variables: null,
           },
           {
             id: 'kontaktinfoBeskrivelse',
             value:
               'Denne kontaktinformasjonen brukes til å kontakte deg hvis det er noen spørsmål rundt informasjonen du har sendt inn.',
-            variables: null,
           },
-          { id: 'messageAttachments', value: 'Vedlegg', variables: null },
+          { id: 'messageAttachments', value: 'Vedlegg' },
           {
             id: 'messageBody',
             value:
               'Bruke dette app for å teste app frontend <br/> 1. Melding <br/> 2. Endring-av-navn-v2 <br/> 3. Repeterende gruppe',
-            variables: null,
           },
           {
             id: 'messageTitle',
             value: '## Appen for test av app frontend',
-            variables: null,
           },
           {
             id: 'messageTitleHelp',
             value: 'Bruk dette appen for test av app frontend',
-            variables: null,
           },
-          { id: 'summaryTitle', value: '## Oppsummering', variables: null },
+          { id: 'summaryTitle', value: '## Oppsummering' },
           {
             id: 'vedlegg',
             value: 'Last opp eventuell dokumentasjon for ditt nye navn',
-            variables: null,
           },
         ],
       };
