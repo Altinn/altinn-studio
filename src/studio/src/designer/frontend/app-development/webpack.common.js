28520988dbd410b12ec58b790e9bbbdeb9587a0f
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-01-15 11:55:33 +0100

    refactor: extract common parts of webpack.config to avoid repetition

---
+++ src/studio/src/designer/frontend/app-development/webpack.common.js
@@ -0,0 +1,70 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MonacoPlugin = require('monaco-editor-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'core-js/modules/es.object.assign',
+    'core-js/modules/es.array.find-index',
+    'core-js/modules/es.array.find',
+    './index.tsx',
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist/app-development'),
+    filename: 'app-development.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss'],
+    alias: {
+      'app-shared': path.resolve(__dirname, '../shared/'),
+      '@altinn/schema-editor': path.resolve(
+        __dirname,
+        '../packages/schema-editor/src/',
+      ),
+      utils: path.resolve(__dirname, 'utils/'),
+      services: path.resolve(__dirname, 'src/services/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?/,
+        use: [{ loader: 'ts-loader', options: { transpileOnly: true } }],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app-development.css',
+    }),
+    new MonacoPlugin({
+      output: path.join('../app-development', 'js', 'react'),
+      languages: ['typescript', 'javascript', 'csharp'],
+    }),
+  ],
+};
--- src/studio/src/designer/frontend/app-development/webpack.config.production.js
+++ src/studio/src/designer/frontend/app-development/webpack.config.production.js
@@ -1,32 +1,13 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const MonacoPlugin = require('monaco-editor-webpack-plugin');
-const path = require('path');
+
+const commonConfig = require('./webpack.common');
 
 module.exports = {
+  ...commonConfig,
   mode: 'production',
   devtool: false,
-  entry: [
-    "core-js/modules/es.object.assign",
-    "core-js/modules/es.array.find-index",
-    "core-js/modules/es.array.find",
-    "./index.tsx"
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist/app-development'),
-    filename: "app-development.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".scss"],
-    alias: {
-      "app-shared": path.resolve(__dirname, "../shared/"),
-      "@altinn/schema-editor": path.resolve(__dirname, "../packages/schema-editor/src/"),
-      //"ux-editor": path.resolve(__dirname, "../ux-editor/")
-      "utils": path.resolve(__dirname, "utils/"),
-    }
-  },
   performance: {
     hints: false,
   },
@@ -39,42 +20,10 @@ module.exports = {
     ],
   },
   module: {
-    rules: [{
-      test: /\.jsx?/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-      }
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        "css-loader"
-      ]
-    },
-    {
-      test: /\.css$/,
-      use: [{
-        loader: MiniCssExtractPlugin.loader,
-      },
-      {
-        loader: "css-loader",
-        options: {
-          url: false
-        }
-      }
-      ]
-    },
-    {
-      test: /\.tsx?/,
-      use: [
-        { loader: "ts-loader", options: { transpileOnly: true } }
-      ]
-    }
-    ],
+    rules: [...commonConfig.module.rules],
   },
   plugins: [
+    ...commonConfig.plugins,
     new ForkTsCheckerWebpackPlugin({
       async: false,
     }),
@@ -94,12 +43,5 @@ module.exports = {
         minifyURLs: true,
       },
     }),
-    new MiniCssExtractPlugin({
-      filename: "app-development.css",
-    }),
-    new MonacoPlugin({
-      output: path.join('../app-development', 'js', 'react'),
-      languages: ['typescript', 'javascript', 'csharp'],
-    }),
   ],
-}
+};
