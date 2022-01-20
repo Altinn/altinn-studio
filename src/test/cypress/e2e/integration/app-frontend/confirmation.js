1cf68f38205282d47e427aebb8f111eede4c280e
EKEBERG Steffen <steffen.ekeberg@soprasteria.com>
2022-01-20 23:19:37 +0100

    chore: try fixing cypress tests

--- src/test/cypress/e2e/integration/app-frontend/confirmation.js
+++ src/test/cypress/e2e/integration/app-frontend/confirmation.js
@@ -10,9 +10,7 @@ describe('Confirm', () => {
   it('Confirm page displays texts and attachments', () => {
     cy.compelteTask3Form();
     cy.get(appFrontend.backButton).should('be.visible');
-    cy.intercept('GET', '**/orgs/altinn-orgs.json').as('getAltinnOrgs');
     cy.get(appFrontend.sendinButton).should('be.visible').click();
-    cy.wait('@getAltinnOrgs');
     cy.get(appFrontend.confirmContainer).should('be.visible');
     cy.get(appFrontend.confirmBody).should('contain.text', texts.confirmBody);
     cy.get(appFrontend.confirmSendInButton).should('be.visible');
