45b86d50b288b3e23ae51566b4e02309bcad8dab
acn-sbuad <stephanie.buadu@avanade.com>
2022-01-20 15:39:41 +0100

    current progress

---
+++ src/studio/src/designer/backend.Tests/TypedHttpClients/AltinnAuthenticationClientTest.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Altinn.Studio.Designer.Configuration;
+using Altinn.Studio.Designer.TypedHttpClients.AltinnAuthentication;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Moq;
+using Moq.Protected;
+
+using Xunit;
+
+namespace Designer.Tests.TypedHttpClients
+{
+    public class AltinnAuthenticationClientTest
+    {
+        private readonly Mock<IOptionsMonitor<PlatformSettings>> _platformSettingsOptions;
+        private readonly Mock<ILogger<AltinnAuthenticationClient>> _logger;
+        private readonly Mock<HttpMessageHandler> _handlerMock;
+
+        public AltinnAuthenticationClientTest()
+        {
+            _platformSettingsOptions = new();
+            _logger = new();
+            _handlerMock = new(MockBehavior.Strict);
+        }
+
+        [Fact]
+        public async Task TC01()
+        {
+            // Arrange
+            HttpRequestMessage actualRequest = null;
+
+            HttpResponseMessage httpResponseMessage = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("this is an exchanged token")
+            };
+
+            void SetRequest(HttpRequestMessage request) => actualRequest = request;
+            InitializeMocks(httpResponseMessage, SetRequest);
+            var sut = new AltinnAuthenticationClient(new HttpClient(_handlerMock.Object), _platformSettingsOptions.Object, _logger.Object);
+
+            // Act
+            await sut.ConvertTokenAsync("this is a random token", new Uri("https://platform.at22.altinn.cloud/storage/api/v1/instances"));
+
+            Assert.NotNull(actualRequest);
+            Assert.Equal(HttpMethod.Post, actualRequest.Method);
+            Assert.EndsWith("app", actualRequest.RequestUri.OriginalString);
+        }
+
+        private void InitializeMocks(HttpResponseMessage httpResponseMessage, Action<HttpRequestMessage> callback)
+        {
+            PlatformSettings platformSettings = new PlatformSettings
+            {
+                SubscriptionKeyHeaderName = "HeaderName",
+                ApiAuthenticationConvertUri = "authentication/api/v1/exchange/altinnstudio"
+            };
+
+            _platformSettingsOptions.Setup(s => s.CurrentValue).Returns(platformSettings);
+
+            GeneralSettings generalSettings = new GeneralSettings
+            {
+                HostName = "at22.altinn.cloud"
+            };
+
+            _handlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>((request, _) => callback(request))
+                .ReturnsAsync(httpResponseMessage)
+                .Verifiable();
+        }
+    }
+}
