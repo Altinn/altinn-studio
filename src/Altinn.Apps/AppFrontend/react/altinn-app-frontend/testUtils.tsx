22860dfeae4accb48b6d1e538c1f0109bac8b770
Håkon <2082481+haakemon@users.noreply.github.com>
2022-02-01 08:53:33 +0000

    fix: missing dependencies for useEffect hook in dropdown component (#7927)

---
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/testUtils.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import type { RenderOptions } from '@testing-library/react';
+import type { PreloadedState } from '@reduxjs/toolkit';
+import { RootState, AppStore, setupStore } from 'src/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  component: any,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) => {
+  function Wrapper({ children }: React.PropsWithChildren<unknown>) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return {
+    store,
+    ...rtlRender(component, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+  };
+};
