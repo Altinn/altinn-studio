6c1155b5d1590aee118f4759c9926676c19a2b02
Ronny Birkeli <ronny.birkeli@digdir.no>
2022-02-03 14:59:03 +0100

    chore: fix better asserts on tests

--- src/studio/src/designer/backend.Tests/Services/SourceControlLoggingDecoratorTests.cs
+++ src/studio/src/designer/backend.Tests/Services/SourceControlLoggingDecoratorTests.cs
@@ -1,48 +1,70 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Altinn.Studio.Designer;
 using Altinn.Studio.Designer.Services.Implementation;
 using Altinn.Studio.Designer.Services.Interfaces;
 using AltinnCore.Authentication.Constants;
 using FluentAssertions;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Designer.Tests.Services
 {
     public class SourceControlLoggingDecoratorTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private WebApplicationFactory<Startup> _webApplicationFactory;
+        private ITestOutputHelper _outputHelper;
 
-        public SourceControlLoggingDecoratorTests(WebApplicationFactory<Startup> webApplicationFactory)
+        public SourceControlLoggingDecoratorTests(WebApplicationFactory<Startup> webApplicationFactory, ITestOutputHelper outputHelper)
         {
             _webApplicationFactory = webApplicationFactory;
+            _outputHelper = outputHelper;
         }
 
         [Fact]
-        public void Container_AskForISourceControl_ReturnsSourceControLogingDecorator()
+        public void DecoratedISourceControlService_EnforceExceptionFromImplementation_LogsErrorWithAdditionalInfo()
+        {
+            // Since the system under test is a logging decorator class, we
+            // want to make sure it actually logs and that it doesn't crash
+            // while collecting additional information to put in the logs.
+            var loggerMock = new Mock<ILogger<SourceControlLoggingDecorator>>();
+            loggerMock
+                .Setup(l => l.Log(
+                    It.Is<LogLevel>(l => l == LogLevel.Error),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Failed executing method")),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)))
+                .Verifiable();
+                
+            var loggerFactoryMock = new Mock<ILoggerFactory>();
+            loggerFactoryMock.Setup(f => f.CreateLogger(It.IsAny<string>())).Returns(loggerMock.Object);
+            var serviceProvider = GetServiceProvider(loggerFactoryMock);
+            var service = serviceProvider.GetService<ISourceControl>();
+
+            Action action = () => service.Status("org_should_not_exists", "repo_should_not_exists");
+
+            action.Should().Throw<LibGit2Sharp.RepositoryNotFoundException>();
+            loggerMock.Verify();
+        }
+
+        [Fact]
+        public void Container_DecoratesISourceControlService_ReturnsDecoratorClass()
         {
             var loggerMock = new Mock<ILogger<SourceControlLoggingDecorator>>();
-            loggerMock.Setup(l => l.Log<object>(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<object>(), It.IsAny<Exception>(), It.IsAny<Func<object, Exception, string>>())).Verifiable();
             var loggerFactoryMock = new Mock<ILoggerFactory>();
             loggerFactoryMock.Setup(f => f.CreateLogger(It.IsAny<string>())).Returns(loggerMock.Object);
             var serviceProvider = GetServiceProvider(loggerFactoryMock);
 
             var service = serviceProvider.GetService<ISourceControl>();
-            
+
             service.Should().BeOfType<SourceControlLoggingDecorator>();
-            Assert.Throws<LibGit2Sharp.RepositoryNotFoundException>(() => service.Status("test", "repo"));
-            loggerMock.Verify();
         }
 
         public IServiceProvider GetServiceProvider(Mock<ILoggerFactory> loggerFactoryMock)
