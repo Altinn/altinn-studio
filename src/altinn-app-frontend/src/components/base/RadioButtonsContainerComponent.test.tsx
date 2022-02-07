7f663da772b533a14d727fec057a4e541b5836c3
Håkon <2082481+haakemon@users.noreply.github.com>
2022-02-07 08:42:13 +0000

    fix: add missing dependencies for useEffect hooks in radio button (#7974)
    Co-authored-by: Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>

---
+++ src/altinn-app-frontend/src/components/base/RadioButtonsContainerComponent.test.tsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen, fireEvent } from '@testing-library/react';
+
+import { renderWithProviders } from '../../../testUtils';
+
+import { RadioButtonContainerComponent } from './RadioButtonsContainerComponent';
+import type { IComponentProps } from 'src/components';
+import type { IRadioButtonsContainerProps } from './RadioButtonsContainerComponent';
+
+const render = (props: Partial<IRadioButtonsContainerProps> = {}) => {
+  const allProps: IRadioButtonsContainerProps = {
+    options: [],
+    optionsId: 'countries',
+    preselectedOptionIndex: undefined,
+    title: 'title',
+    legend: 'legend',
+    handleDataChange: jest.fn(),
+    handleFocusUpdate: jest.fn(),
+    getTextResource: (value) => value,
+    ...({} as IComponentProps),
+    ...props,
+  };
+
+  const countries = [
+    {
+      label: 'Norway',
+      value: 'norway',
+    },
+    {
+      label: 'Sweden',
+      value: 'sweden',
+    },
+    {
+      label: 'Denmark',
+      value: 'denmark',
+    },
+  ];
+
+  renderWithProviders(<RadioButtonContainerComponent {...allProps} />, {
+    preloadedState: {
+      optionState: {
+        options: {
+          countries,
+        },
+        error: {
+          name: '',
+          message: '',
+        },
+      },
+    },
+  });
+};
+
+const getRadio = ({ name, isChecked = false }) => {
+  return screen.getByRole('radio', {
+    name: name,
+    checked: isChecked,
+  });
+};
+
+describe('RadioButtonsContainerComponent', () => {
+  it('should call handleDataChange with value of preselectedOptionIndex when simpleBinding is not set', () => {
+    const handleChange = jest.fn();
+    render({
+      handleDataChange: handleChange,
+      preselectedOptionIndex: 1,
+      formData: {
+        simpleBinding: undefined,
+      },
+    });
+
+    expect(handleChange).toHaveBeenCalledWith('sweden');
+  });
+
+  it('should not call handleDataChange when simpleBinding is set and preselectedOptionIndex', () => {
+    const handleChange = jest.fn();
+    render({
+      handleDataChange: handleChange,
+      preselectedOptionIndex: 0,
+      formData: {
+        simpleBinding: 'denmark',
+      },
+    });
+
+    expect(getRadio({ name: 'Norway' })).toBeInTheDocument();
+    expect(getRadio({ name: 'Sweden' })).toBeInTheDocument();
+    expect(getRadio({ name: 'Denmark', isChecked: true })).toBeInTheDocument();
+
+    expect(handleChange).not.toHaveBeenCalled();
+  });
+
+  it('should not set any as selected when no binding and no preselectedOptionIndex is set', () => {
+    const handleChange = jest.fn();
+    render({ handleDataChange: handleChange });
+
+    expect(getRadio({ name: 'Norway' })).toBeInTheDocument();
+    expect(getRadio({ name: 'Sweden' })).toBeInTheDocument();
+    expect(getRadio({ name: 'Denmark' })).toBeInTheDocument();
+
+    expect(handleChange).not.toHaveBeenCalled();
+  });
+
+  it('should call handleDataChange with updated value when selection changes', () => {
+    const handleChange = jest.fn();
+    render({
+      handleDataChange: handleChange,
+      formData: {
+        simpleBinding: 'norway',
+      },
+    });
+
+    expect(getRadio({ name: 'Norway', isChecked: true })).toBeInTheDocument();
+    expect(getRadio({ name: 'Sweden' })).toBeInTheDocument();
+    expect(getRadio({ name: 'Denmark' })).toBeInTheDocument();
+
+    userEvent.click(getRadio({ name: 'Denmark' }));
+
+    expect(handleChange).toHaveBeenCalledWith('denmark');
+  });
+
+  it('should call handleDataChange on blur with already selected value', () => {
+    const handleChange = jest.fn();
+    render({
+      handleDataChange: handleChange,
+      formData: {
+        simpleBinding: 'norway',
+      },
+    });
+
+    expect(getRadio({ name: 'Denmark' })).toBeInTheDocument();
+
+    fireEvent.focus(getRadio({ name: 'Denmark' }));
+    fireEvent.blur(getRadio({ name: 'Denmark' }));
+
+    expect(handleChange).toHaveBeenCalledWith('norway');
+  });
+
+  it('should call handleDataChange on blur with empty value when no item is selected', () => {
+    const handleChange = jest.fn();
+    render({
+      handleDataChange: handleChange,
+    });
+
+    expect(getRadio({ name: 'Denmark' })).toBeInTheDocument();
+
+    fireEvent.focus(getRadio({ name: 'Denmark' }));
+    fireEvent.blur(getRadio({ name: 'Denmark' }));
+
+    expect(handleChange).toHaveBeenCalledWith('');
+  });
+});
