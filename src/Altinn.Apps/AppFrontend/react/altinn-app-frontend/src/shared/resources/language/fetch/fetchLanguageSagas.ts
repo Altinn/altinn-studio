fc683a20b4abb972703bda265f55f9bc538244ea
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-02-09 11:31:55 +0100

    chore: move tests next to implementation

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/language/fetch/fetchLanguageSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/language/fetch/fetchLanguageSagas.ts
@@ -1,20 +1,23 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, all, take, select } from 'redux-saga/effects';
-// import { get } from '../../../../utils/networking';
-import { IProfile } from 'altinn-shared/types';
+
+import type { IProfile } from 'altinn-shared/types';
+import type { IRuntimeState } from '../../../../types';
+
 import { getLanguageFromCode } from 'altinn-shared/language';
 import LanguageActions from '../languageActions';
 import * as LanguageActionTypes from './fetchLanguageActionTypes';
 import * as ProfileActionTypes from '../../profile/fetch/fetchProfileActionTypes';
 import { appTaskQueueError } from '../../queue/queueSlice';
-import { IRuntimeState } from '../../../../types';
 
 const profileState = (state: IRuntimeState): IProfile => state.profile.profile;
 
 export function* fetchLanguageSaga(): SagaIterator {
   try {
     const profile: IProfile = yield select(profileState);
-    const language = getLanguageFromCode(profile.profileSettingPreference.language);
+    const language = getLanguageFromCode(
+      profile.profileSettingPreference.language,
+    );
     yield call(LanguageActions.fetchLanguageFulfilled, language);
   } catch (error) {
     yield call(LanguageActions.fetchLanguageRecjeted, error);
---
+++ src/Altinn.Apps/AppFrontend/react/receipt/src/utils/receipt.test.tsx
@@ -0,0 +1,65 @@
+import 'jest';
+import {IAltinnOrg, IAltinnOrgs, IApplication, IData, IInstance, ILanguage, IParty, ITextResource} from '../../../shared/src/types/index';
+import { getInstanceMetaDataObject } from './receipt';
+
+describe('utils > receipt', () => {
+  const instance = {
+    data: [
+      {dataType: 'default', lastChanged: new Date(2018, 11, 24, 10, 33)} as IData,
+    ],
+    org: 'testOrg',
+  } as IInstance;
+  const partyPerson = {
+    name: 'Ola Nordmann',
+    ssn: '12345678',
+  } as IParty;
+  const partyOrg = {
+    name: 'FIRMA AS',
+    orgNumber: 12345,
+  } as IParty;
+  const language: ILanguage = {
+    receipt_platform: {
+      date_sent: 'Dato sendt',
+      sender: 'Avsender',
+      receiver: 'Mottaker',
+      reference_number: 'Referansenummer',
+    },
+  };
+  const organisations: IAltinnOrgs = {
+    testOrg: {
+      name: {
+        nb: 'TEST ORG',
+      },
+    } as unknown as IAltinnOrg,
+  };
+  const expectedResult = {
+    'Dato sendt': '24.12.2018 / 10:33',
+    'Avsender': '12345678-Ola Nordmann',
+    'Mottaker': 'TEST ORG',
+    'Referansenummer': 'd6a414a797ae',
+  };
+  const application = {
+    dataTypes: [{appLogic: true, id: 'default'}],
+  } as IApplication;
+
+  it('should return instance metadata object with correct values for person', () => {
+    const result = getInstanceMetaDataObject(instance, partyPerson, language, organisations, application, [], 'nb');
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return instance metadata object with correct values for org', () => {
+    const result = getInstanceMetaDataObject(instance, partyOrg, language, organisations, application, [], 'nb');
+    const expectedOrgResult = expectedResult;
+    expectedOrgResult.Avsender = '12345-FIRMA AS';
+    expect(result).toEqual(expectedOrgResult);
+  });
+
+  it('should display appOwner name from text resources if defined', () => {
+    const textResourceWithAppOwner: ITextResource[] = [{
+      id: 'appOwner',
+      value: 'Name from resources'
+    }];
+    const result = getInstanceMetaDataObject(instance, partyOrg, language, organisations, application, textResourceWithAppOwner, 'nb');
+    expect(result.Mottaker).toEqual('Name from resources');
+  });
+});
