0e5ee1b340234bcd268a0cfb3d44f4fc2de3b23c
Ivar Nesje <ivarne@users.noreply.github.com>
2022-02-10 08:55:57 +0100

    Auth level localtest (#8023)
    * Make Authentication level selectable in LocalTest
    
    * Get correct authLevel from app if LocalAppMode == "http"

--- src/development/LocalTest/Controllers/HomeController.cs
+++ src/development/LocalTest/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using System.Xml;
 
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -15,20 +16,16 @@ using Microsoft.Extensions.Options;
 
 using AltinnCore.Authentication.Constants;
 using Altinn.Platform.Profile.Models;
-using Altinn.Platform.Storage.Repository;
 using Altinn.Platform.Storage.Interface.Models;
 
 using LocalTest.Configuration;
 using LocalTest.Models;
 using LocalTest.Services.Authentication.Interface;
 using LocalTest.Services.Profile.Interface;
-using LocalTest.Services.Localtest.Interface;
 using LocalTest.Services.LocalApp.Interface;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using System.Text;
-using Newtonsoft.Json;
 
 namespace LocalTest.Controllers
 {
@@ -71,6 +68,8 @@ namespace LocalTest.Controllers
             model.AppPath = _localPlatformSettings.AppRepositoryBasePath;
             model.StaticTestDataPath = _localPlatformSettings.LocalTestingStaticTestDataPath;
             model.LocalAppUrl = _localPlatformSettings.LocalAppUrl;
+            var defaultAuthLevel = _localPlatformSettings.LocalAppMode == "http" ? await GetAppAuthLevel(model.TestApps.First().Value) : 2;
+            model.AuthenticationLevels = GetAuthenticationLevels(defaultAuthLevel);
 
             if (!model.TestApps?.Any() ?? true)
             {
@@ -112,7 +111,7 @@ namespace LocalTest.Controllers
             claims.Add(new Claim(AltinnCoreClaimTypes.UserId, profile.UserId.ToString(), ClaimValueTypes.String, issuer));
             claims.Add(new Claim(AltinnCoreClaimTypes.UserName, profile.UserName, ClaimValueTypes.String, issuer));
             claims.Add(new Claim(AltinnCoreClaimTypes.PartyID, profile.PartyId.ToString(), ClaimValueTypes.Integer32, issuer));
-            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticationLevel, "2", ClaimValueTypes.Integer32, issuer));
+            claims.Add(new Claim(AltinnCoreClaimTypes.AuthenticationLevel, startAppModel.AuthenticationLevel, ClaimValueTypes.Integer32, issuer));
 
             ClaimsIdentity identity = new ClaimsIdentity(_generalSettings.GetClaimsIdentity);
             identity.AddClaims(claims);
@@ -225,8 +224,62 @@ namespace LocalTest.Controllers
 
             return userItems;
         }
+        private async Task<int> GetAppAuthLevel(string appId)
+        {
+            try {
+                var policyString = await _localApp.GetXACMLPolicy(appId);
+                var document = new XmlDocument();
+                document.LoadXml(policyString);
+                var nsMngr = new XmlNamespaceManager(document.NameTable);
+                nsMngr.AddNamespace("xacml", "urn:oasis:names:tc:xacml:3.0:core:schema:wd-17");
+                var authLevelNode = document.SelectSingleNode("/xacml:Policy/xacml:ObligationExpressions/xacml:ObligationExpression[@ObligationId='urn:altinn:obligation:authenticationLevel1']/xacml:AttributeAssignmentExpression[@Category='urn:altinn:minimum-authenticationlevel']/xacml:AttributeValue", nsMngr);
+                return int.Parse(authLevelNode.InnerText);
+            }
+            catch(Exception)
+            {
+                // Return default auth level if app auth level can't be found.
+                return 2;
+            }
+        }
 
-        private async Task<IEnumerable<SelectListItem>> GetAppsList()
+        private List<SelectListItem> GetAuthenticationLevels(int defaultAuthLevel)
+        {
+            return new()
+            {
+                new()
+                {
+                    Value = "0",
+                    Text = "Nivå 0",
+                    Selected = defaultAuthLevel == 0
+                },
+                new()
+                {
+                    Value = "1",
+                    Text = "Nivå 1",
+                    Selected = defaultAuthLevel == 1
+                },
+                new()
+                {
+                    Value = "2",
+                    Text = "Nivå 2",
+                    Selected = defaultAuthLevel == 2
+                },
+                new()
+                {
+                    Value = "3",
+                    Text = "Nivå 3",
+                    Selected = defaultAuthLevel == 3
+                },
+                new()
+                {
+                    Value = "4",
+                    Text = "Nivå 4",
+                    Selected = defaultAuthLevel == 4
+                },
+            };
+        }
+
+        private async Task<List<SelectListItem>> GetAppsList()
         {
             var applications = await _localApp.GetApplications();
             return applications.Select((kv) => GetSelectItem(kv.Value, kv.Key)).ToList();
