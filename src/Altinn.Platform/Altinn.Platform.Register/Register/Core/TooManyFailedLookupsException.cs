508e7b8e82a58e55a6282b6b70096d1b5f8075cc
Terje Holene <terje.holene@gmail.com>
2022-02-11 12:49:09 +0100

    Make custom exception serializable

--- src/Altinn.Platform/Altinn.Platform.Register/Register/Core/TooManyFailedLookupsException.cs
+++ src/Altinn.Platform/Altinn.Platform.Register/Register/Core/TooManyFailedLookupsException.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Altinn.Platform.Register.Core
 {
     /// <summary>
     /// Represents a situation where a user has performed too many failed lookup requests.
     /// </summary>
+    [Serializable]
     public class TooManyFailedLookupsException : Exception
     {
         /// <summary>
@@ -18,7 +20,17 @@ namespace Altinn.Platform.Register.Core
         /// Initializes a new instance of the <see cref="TooManyFailedLookupsException"/> class.
         /// </summary>
         /// <param name="message">The message that descibes the error.</param>
-        public TooManyFailedLookupsException(string message) : base(message)
+        public TooManyFailedLookupsException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="TooManyFailedLookupsException"/> class using the
+        /// given <see cref="SerializationInfo"/>.
+        /// </summary>
+        protected TooManyFailedLookupsException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
