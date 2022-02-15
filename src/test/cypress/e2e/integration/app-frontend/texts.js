b19bc47b833286074881295a0660f32d93ebf8a9
lorang92 <steffen.ekeberg@gmail.com>
2022-02-15 18:32:56 +0100

    test: added cypress test for variables in text with backend calculation

---
+++ src/test/cypress/e2e/integration/app-frontend/texts.js
@@ -0,0 +1,23 @@
+/// <reference types='cypress' />
+/// <reference types="../../support" />
+
+import AppFrontend from '../../pageobjects/app-frontend';
+
+const appFrontend = new AppFrontend();
+
+describe('Texts', () => {
+
+  beforeEach(() => {
+    cy.navigateToChangeName();
+  });
+
+  it('Variable in texts work and are updated if the variable is updated with a calculation backend', () => {
+    cy.get(appFrontend.changeOfName.newMiddleName).should('be.visible').type('Steffen').blur();
+    cy.get(appFrontend.changeOfName.newMiddleNameDescription).should('contain.text', 'Steffen');
+
+    // We update newLastName which triggers a calculation backend that updates NewMiddleName
+    cy.get(appFrontend.changeOfName.newFirstName).should('be.visible').type('TriggerCalculation').blur();
+    cy.get(appFrontend.changeOfName.newMiddleNameDescription).should('contain.text', 'MiddleNameFromCalculation');
+  });
+});
+
