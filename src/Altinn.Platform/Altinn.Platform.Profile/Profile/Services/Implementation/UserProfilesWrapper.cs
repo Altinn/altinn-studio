61836e0aafe80617051516e8adf54fd54435b98b
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-02-18 16:40:07 +0100

    Caching user profile retrieved from bridge (#8055)

--- src/Altinn.Platform/Altinn.Platform.Profile/Profile/Services/Implementation/UserProfilesWrapper.cs
+++ src/Altinn.Platform/Altinn.Platform.Profile/Profile/Services/Implementation/UserProfilesWrapper.cs
@@ -2,12 +2,14 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 using Altinn.Platform.Profile.Configuration;
 using Altinn.Platform.Profile.Models;
 using Altinn.Platform.Profile.Services.Interfaces;
 
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -21,6 +23,7 @@ namespace Altinn.Platform.Profile.Services.Implementation
         private readonly ILogger _logger;
         private readonly GeneralSettings _generalSettings;
         private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _serializerOptions;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UserProfilesWrapper"/> class
@@ -28,30 +31,40 @@ namespace Altinn.Platform.Profile.Services.Implementation
         /// <param name="httpClient">HttpClient from default http client factory</param>
         /// <param name="logger">the logger</param>
         /// <param name="generalSettings">the general settings</param>
-        public UserProfilesWrapper(HttpClient httpClient, ILogger<UserProfilesWrapper> logger, IOptions<GeneralSettings> generalSettings)
+        public UserProfilesWrapper(
+            HttpClient httpClient,
+            ILogger<UserProfilesWrapper> logger,
+            IOptions<GeneralSettings> generalSettings)
         {
             _logger = logger;
             _generalSettings = generalSettings.Value;
             _client = httpClient;
+
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+            };
         }
 
         /// <inheritdoc />
         public async Task<UserProfile> GetUser(int userId)
         {
-            UserProfile user = null;
+            UserProfile user;
+
             Uri endpointUrl = new Uri($"{_generalSettings.BridgeApiEndpoint}users/{userId}");
 
             HttpResponseMessage response = await _client.GetAsync(endpointUrl);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                user = Newtonsoft.Json.JsonConvert.DeserializeObject<UserProfile>(content);
-                user.ProfileSettingPreference.Language = MapLanguageString(user.ProfileSettingPreference.Language);
-            }
-            else
+
+            if (!response.IsSuccessStatusCode)
             {
                 _logger.LogError("Getting user {userId} failed with {statusCode}", userId, response.StatusCode);
+                return null;
             }
+
+            string content = await response.Content.ReadAsStringAsync();
+            user = JsonSerializer.Deserialize<UserProfile>(content, _serializerOptions);
 
             return user;
         }
@@ -59,37 +72,22 @@ namespace Altinn.Platform.Profile.Services.Implementation
         /// <inheritdoc />
         public async Task<UserProfile> GetUser(string ssn)
         {
-            UserProfile user = null;
-
+            UserProfile user;
             Uri endpointUrl = new Uri($"{_generalSettings.BridgeApiEndpoint}users");
             StringContent requestBody = new StringContent(JsonSerializer.Serialize(ssn), Encoding.UTF8, "application/json");
 
             HttpResponseMessage response = await _client.PostAsync(endpointUrl, requestBody);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                user = Newtonsoft.Json.JsonConvert.DeserializeObject<UserProfile>(content);
-                user.ProfileSettingPreference.Language = MapLanguageString(user.ProfileSettingPreference.Language);
-            }
-            else
+
+            if (!response.IsSuccessStatusCode)
             {
                 _logger.LogError("Getting user by SSN failed with statuscode {statusCode}", response.StatusCode);
+                return null;
             }
 
-            return user;
-        }
+            string content = await response.Content.ReadAsStringAsync();
+            user = JsonSerializer.Deserialize<UserProfile>(content, _serializerOptions);
 
-        // Obsolete when TFS43729 is in production
-        private string MapLanguageString(string languageType)
-        {
-            return languageType switch
-            {
-                "1044" => "nb",
-                "1033" => "en",
-                "2068" => "nn",
-                "1083" => "se",
-                _ => languageType,
-            };
+            return user;
         }
     }
 }
