9c51f58822601b6cc38161d3134d03609e67b61d
lorang92 <steffen.ekeberg@gmail.com>
2022-02-22 15:16:09 +0100

    test: unit test for pdf

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/__mocks__/instanceDataStateMock.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/__mocks__/instanceDataStateMock.ts
@@ -27,6 +27,27 @@ export function getInstanceDataStateMock(customStates?: Partial<IInstanceDataSta
           lastChanged: new Date('2021-06-04T13:30:48.4307222Z'),
           lastChangedBy: '12345',
         },
+        {
+          id: '917e7e06-2665-4307-8c05-b0accd8964c6',
+          instanceGuid: 'b174e1f8-135c-4419-bd1c-fd171a0af1fe',
+          dataType: 'ref-data-as-pdf',
+          filename: 'mockApp.pdf',
+          contentType: 'application/pdf',
+          blobStoragePath: 'mockOrg/mockApp/b174e1f8-135c-4419-bd1c-fd171a0af1fe/data/917e7e06-2665-4307-8c05-b0accd8964c6',
+          selfLinks: {
+            apps: 'https://altinn3local.no/mockOrg/mockApp/instances/501337/b174e1f8-135c-4419-bd1c-fd171a0af1fe/data/917e7e06-2665-4307-8c05-b0accd8964c6',
+            platform: 'https://platform.altinn3local.no/storage/api/v1/instances/501337/b174e1f8-135c-4419-bd1c-fd171a0af1fe/data/917e7e06-2665-4307-8c05-b0accd8964c6'
+          },
+          size: 3537,
+          locked: false,
+          refs: [],
+          isRead: true,
+          tags: [],
+          created: new Date('2022-02-22T14:07:07.490511Z'),
+          createdBy: '12345',
+          lastChanged: new Date('2022-02-22T14:07:07.490511Z'),
+          lastChangedBy: '12345'
+        }
       ],
       id: '91cefc5e-c47b-40ff-a8a4-05971205f783',
       instanceState: null,
