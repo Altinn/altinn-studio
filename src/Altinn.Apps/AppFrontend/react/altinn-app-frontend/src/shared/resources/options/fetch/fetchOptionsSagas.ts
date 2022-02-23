e7eceb1fdf1f347bf0e256919b01bc85b62b19cc
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2022-02-23 18:19:35 +0100

    feature/7893-secure-options-frontend (#8092)
    * feat: enable secure options frontend
    
    * chore: bump package.json
    
    * test: bump coverage
    
    * feat: secure on refetch
    
    Co-authored-by: EKEBERG Steffen <steffen.ekeberg@soprasteria.com>

--- src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/options/fetch/fetchOptionsSagas.ts
+++ src/Altinn.Apps/AppFrontend/react/altinn-app-frontend/src/shared/resources/options/fetch/fetchOptionsSagas.ts
@@ -19,6 +19,7 @@ export const formDataSelector = (state: IRuntimeState) => state.formData.formDat
 export const userLanguageSelector = (state: IRuntimeState) =>
   state.profile.profile.profileSettingPreference.language;
 export const optionsSelector = (state: IRuntimeState): IOptions => state.optionState.options;
+export const instanceIdSelector = (state: IRuntimeState): string => state.instanceData.instance?.id;
 
 export function* fetchOptionsSaga(): SagaIterator {
   const layouts: ILayouts = yield select(formLayoutSelector);
@@ -33,7 +34,8 @@ export function* fetchOptionsSaga(): SagaIterator {
       ) {
         yield fork(fetchSpecificOptionSaga, {
           optionsId: component.optionsId,
-          dataMapping: component?.mapping,
+          dataMapping: component.mapping,
+          secure: component.secure,
         });
         fetchedOptions.push(component.optionsId);
       }
@@ -44,18 +46,21 @@ export function* fetchOptionsSaga(): SagaIterator {
 export function* fetchSpecificOptionSaga({
   optionsId,
   dataMapping,
+  secure
 }: IFetchSpecificOptionSaga): SagaIterator {
   const optionKey = getOptionLookupKey(optionsId, dataMapping);
+  const instanceId = yield select(instanceIdSelector);
   try {
     yield call(OptionsActions.fetchingOptions, optionKey);
     const formData: IFormData = yield select(formDataSelector);
     const language: string = yield select(userLanguageSelector);
-    const url = getOptionsUrl({ optionsId, formData, language, dataMapping });
+    const url = getOptionsUrl({ optionsId, formData, language, dataMapping, secure, instanceId });
     const options: IOption[] = yield call(get, url);
     const optionData: IOptionData = {
       id: optionsId,
       options,
       mapping: dataMapping,
+      secure,
     };
     yield call(OptionsActions.fetchOptionsFulfilled, optionKey, optionData);
   } catch (error) {
@@ -73,10 +78,12 @@ export function* checkIfOptionsShouldRefetchSaga({
   for (const optionsKey of Object.keys(options)) {
     const dataMapping = options[optionsKey].mapping;
     const optionsId = options[optionsKey].id;
+    const secure = options[optionsKey].secure;
     if (dataMapping && Object.keys(dataMapping).includes(field)) {
       yield fork(fetchSpecificOptionSaga, {
         optionsId,
         dataMapping,
+        secure,
       });
     }
   }
