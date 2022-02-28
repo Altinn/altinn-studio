1562525272d90f64dbfd04e665ed23157a2b0f87
lorang92 <steffen.ekeberg@gmail.com>
2022-02-28 09:42:08 +0100

    chore: smell

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/services/PDFGenerator.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/services/PDFGenerator.java
@@ -272,7 +272,7 @@ public class PDFGenerator {
   private void renderLayoutElement(FormLayoutElement element) throws IOException {
     String componentType = element.getType();
     String componentId = element.getId();
-    if (componentsIgnoredFromGeneration.contains(element.getType())
+    if (componentsIgnoredFromGeneration.contains(componentType)
       || !LayoutUtils.includeComponentInPdf(componentId, layoutSettings)) {
       return;
     }
