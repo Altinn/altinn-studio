c46099a41a1ffd38af318bebb1eed8baf10a948d
Steffen Lorang Ekeberg <steffen.ekeberg@gmail.com>
2022-03-01 09:54:15 +0100

    bug/8054-frontend-rules-calculation (#8134)
    * bug: check rules on calculation backend
    
    * chore: bump package.json
    
    * test: cypress test for case

--- src/altinn-app-frontend/src/features/form/rules/rulesActionTypes.ts
+++ src/altinn-app-frontend/src/features/form/rules/rulesActionTypes.ts
@@ -1,7 +1,5 @@
 const moduleName = 'RULE_CONNECTION';
 
-export const CHECK_IF_RULE_SHOULD_RUN = `${moduleName}.CHECK_IF_RULE_SHOULD_RUN`;
-
 // Fetch rule handler actions
 export const FETCH_RULE_MODEL = `${moduleName}.FETCH_RULE_MODEL`;
 export const FETCH_RULE_MODEL_FULFILLED = `${moduleName}.FETCH_RULE_MODEL_FULFILLED`;
