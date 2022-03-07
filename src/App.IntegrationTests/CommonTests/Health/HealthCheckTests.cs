43a56a437ffc528717ed0d3229fb43f357917bc8
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-03-07 10:05:59 +0100

    Added health check support (#49)

---
+++ src/App.IntegrationTests/CommonTests/Health/HealthCheckTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+using Xunit;
+
+namespace App.IntegrationTestsRef.CommonTests.Health
+{
+    /// <summary>
+    /// Health check 
+    /// </summary>
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Altinn.App.AppLogic.App>>
+    {
+        private readonly WebApplicationFactory<Altinn.App.AppLogic.App> _factory;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="factory">The web applicaiton factory</param>
+        public HealthCheckTests(WebApplicationFactory<Altinn.App.AppLogic.App> factory)
+        {
+            _factory = factory;
+        }
+
+        /// <summary>
+        /// Verify that component responds on health check
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task VerifyHealthCheck_OK()
+        {
+            HttpClient client = GetTestClient();
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "/health");
+
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        private HttpClient GetTestClient()
+        {
+            HttpClient client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                });
+            }).CreateClient();
+
+            return client;
+        }
+    }
+}
