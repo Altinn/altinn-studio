b101a9dbeb6e42fcddfc6267982fce29ebafd5bc
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-03-15 15:16:32 +0100

    Add unit tests, stylecop issues

--- src/Altinn.Platform/Altinn.Platform.Authorization/Functions/Configuration/KeyVaultSettings.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Functions/Configuration/KeyVaultSettings.cs
@@ -1,18 +1,17 @@
-namespace Altinn.Platform.Authorization.Functions.Configuration
+namespace Altinn.Platform.Authorization.Functions.Configuration;
+
+/// <summary>
+/// Configuration object used to hold settings for the KeyVault.
+/// </summary>
+public class KeyVaultSettings
 {
     /// <summary>
-    /// Configuration object used to hold settings for the KeyVault.
+    /// Uri to keyvault
     /// </summary>
-    public class KeyVaultSettings
-    {
-        /// <summary>
-        /// Uri to keyvault
-        /// </summary>
-        public string KeyVaultURI { get; set; }
+    public string KeyVaultUri { get; set; }
 
-        /// <summary>
-        /// Name of the certificate secret
-        /// </summary>
-        public string PlatformCertSecretId { get; set; } = "platform-access-token-private-cert";
-    }
+    /// <summary>
+    /// Name of the certificate secret
+    /// </summary>
+    public string PlatformCertSecretId { get; set; } = "platform-access-token-private-cert";
 }
--- src/Altinn.Platform/Altinn.Platform.Authorization/Functions/Services/AccessTokenProvider.cs
+++ src/Altinn.Platform/Altinn.Platform.Authorization/Functions/Services/AccessTokenProvider.cs
@@ -8,53 +8,65 @@ using Altinn.Platform.Authorization.Functions.Configuration;
 using Altinn.Platform.Authorization.Functions.Services.Interfaces;
 using Microsoft.Extensions.Options;
 
-namespace Altinn.Platform.Authorization.Functions.Services
+namespace Altinn.Platform.Authorization.Functions.Services;
+
+/// <inheritdoc />
+public class AccessTokenProvider : IAccessTokenProvider
 {
-    public class AccessTokenProvider : IAccessTokenProvider
+    private readonly IKeyVaultService _keyVaultService;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+    private readonly PlatformSettings _platformSettings;
+    private readonly AccessTokenSettings _accessTokenSettings;
+    private readonly KeyVaultSettings _keyVaultSettings;
+    private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(1, 1);
+    private static DateTime _cacheTokenUntil = DateTime.MinValue;
+    private string _accessToken;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AccessTokenProvider"/> class.
+    /// </summary>
+    /// <param name="keyVaultService">The key vault service.</param>
+    /// <param name="accessTokenGenerator">The access token generator.</param>
+    /// <param name="accessTokenSettings">The access token settings.</param>
+    /// <param name="keyVaultSettings">The key vault settings.</param>
+    /// <param name="platformSettings">The platform settings.</param>
+    public AccessTokenProvider(
+        IKeyVaultService keyVaultService,
+        IAccessTokenGenerator accessTokenGenerator,
+        IOptions<AccessTokenSettings> accessTokenSettings,
+        IOptions<KeyVaultSettings> keyVaultSettings,
+        IOptions<PlatformSettings> platformSettings)
     {
-        private readonly IKeyVaultService _keyVaultService;
-        private readonly IAccessTokenGenerator _accessTokenGenerator;
-        private readonly PlatformSettings _platformSettings;
-        private readonly AccessTokenSettings _accessTokenSettings;
-        private readonly KeyVaultSettings _keyVaultSettings;
-        private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(1, 1);
-        private static DateTime _cacheTokenUntil = DateTime.MinValue;
-        private string _accessToken;
+        _keyVaultService = keyVaultService;
+        _accessTokenGenerator = accessTokenGenerator;
+        _platformSettings = platformSettings.Value;
+        _accessTokenSettings = accessTokenSettings.Value;
+        _keyVaultSettings = keyVaultSettings.Value;
+    }
 
-        public AccessTokenProvider(IKeyVaultService keyVaultService, IAccessTokenGenerator accessTokenGenerator,
-            IOptions<AccessTokenSettings> accessTokenSettings, IOptions<KeyVaultSettings> keyVaultSettings, IOptions<PlatformSettings> platformSettings)
+    /// <inheritdoc />
+    public async Task<string> GetAccessToken()
+    {
+        await Semaphore.WaitAsync();
+
+        try
         {
-            _keyVaultService = keyVaultService;
-            _accessTokenGenerator = accessTokenGenerator;
-            _platformSettings = platformSettings.Value;
-            _accessTokenSettings = accessTokenSettings.Value;
-            _keyVaultSettings = keyVaultSettings.Value;
+            if (_accessToken == null || _cacheTokenUntil < DateTime.UtcNow)
+            {
+                string certBase64 = await _keyVaultService.GetCertificateAsync(_keyVaultSettings.KeyVaultUri, _keyVaultSettings.PlatformCertSecretId);
+                _accessToken = _accessTokenGenerator.GenerateAccessToken(
+                    _platformSettings.AccessTokenIssuer,
+                    "platform.authorization",
+                    new X509Certificate2(Convert.FromBase64String(certBase64), (string)null, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable));
+            }
+
+            _cacheTokenUntil = DateTime.UtcNow.AddSeconds(_accessTokenSettings.TokenLifetimeInSeconds - 2); // Add some slack to avoid tokens expiring in transit
+
+            return _accessToken;
         }
-
-        public async Task<string> GetAccessToken()
+        finally
         {
-            await Semaphore.WaitAsync();
-
-            try
-            {
-                if (_accessToken == null || _cacheTokenUntil < DateTime.UtcNow)
-                {
-                    string certBase64 = await _keyVaultService.GetCertificateAsync(_keyVaultSettings.KeyVaultURI, _keyVaultSettings.PlatformCertSecretId);
-                    _accessToken = _accessTokenGenerator.GenerateAccessToken(
-                        _platformSettings.AccessTokenIssuer,
-                        "platform.authorization",
-                        new X509Certificate2(Convert.FromBase64String(certBase64), (string)null, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable));
-                }
-
-                _cacheTokenUntil = DateTime.UtcNow.AddSeconds(_accessTokenSettings.TokenLifetimeInSeconds - 2); // Add some slack to avoid tokens expiring in transit
-
-                return _accessToken;
-            }
-            finally
-            {
-                Semaphore.Release();
-            }
+            Semaphore.Release();
         }
     }
 }
-
