4ba1649c32ecb25af1b7edd41292a9c2e531c3cc
simendsjo <simendsjo@gmail.com>
2022-03-15 13:18:06 +0100

    Expose prefill method to user (#42)

--- src/Altinn.App.PlatformServices/Interface/IPrefill.cs
+++ src/Altinn.App.PlatformServices/Interface/IPrefill.cs
@@ -9,6 +9,14 @@ namespace Altinn.App.Services.Interface
     public interface IPrefill
     {
         /// <summary>
+        /// Prefills the data model based on key/values in the dictionary.
+        /// </summary>
+        /// <param name="dataModel">The data model object</param>
+        /// <param name="externalPrefill">External given prefill</param>
+        /// <param name="continueOnError">Ignore errors when true, throw on errors when false</param>
+        void PrefillDataModel(object dataModel, Dictionary<string, string> externalPrefill, bool continueOnError = false);
+
+        /// <summary>
         /// Prefills the data model based on the prefill json configuration file
         /// </summary>
         /// <param name="partyId">The partyId of the instance owner</param>
