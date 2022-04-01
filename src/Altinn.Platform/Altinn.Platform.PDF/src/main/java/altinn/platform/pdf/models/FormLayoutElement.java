5d5ed99f6cce708a6a8444366f9703b6912af490
acn-sbuad <stephanie.buadu@avanade.com>
2022-04-01 10:56:10 +0200

    extended model, added tests

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/FormLayoutElement.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/FormLayoutElement.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.SerializationUtils;
 
 @Schema(description = "A form layout element")
 @Getter
 @Setter
-public class FormLayoutElement {
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormLayoutElement implements Cloneable{
   private String type;
   private String id;
   private HashMap<String, String> dataModelBindings;
@@ -22,4 +27,11 @@ public class FormLayoutElement {
   private int count;
   private int maxCount;
   private List<String> dataTypeIds;
+  private GroupEditProperties edit;
+
+  @Override
+  public Object clone()  {
+    return new FormLayoutElement(type, id, SerializationUtils.clone(dataModelBindings), textResourceBindings, optionsId, options, simplified, children, count, maxCount, dataTypeIds, edit);
+  }
 }
+
