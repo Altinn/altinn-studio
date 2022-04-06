0322d7acda642d915a1484d584ab06128ec29e63
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-04-06 11:19:48 +0200

    Nested repeating groups rendered correctly (#8345)

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/FormLayoutElement.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/FormLayoutElement.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.SerializationUtils;
 
 @Schema(description = "A form layout element")
 @Getter
 @Setter
-public class FormLayoutElement {
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormLayoutElement implements Cloneable{
   private String type;
   private String id;
   private HashMap<String, String> dataModelBindings;
@@ -22,4 +27,31 @@ public class FormLayoutElement {
   private int count;
   private int maxCount;
   private List<String> dataTypeIds;
+  private GroupEditProperties edit;
+
+  @Override
+  public Object clone() {
+  FormLayoutElement fle = null;
+    try {
+      fle = (FormLayoutElement) super.clone();
+    } catch (CloneNotSupportedException e) {
+      fle = new FormLayoutElement(
+        this.type,
+        this.id,
+        this.dataModelBindings,
+        this.textResourceBindings,
+        this.optionsId,
+        this.options,
+        this.simplified,
+        this.children,
+        this.count,
+        this.maxCount,
+        this.dataTypeIds,
+       this.edit);
+    }
+
+    fle.dataModelBindings = SerializationUtils.clone(dataModelBindings);
+    return fle;
 }
+}
+
