3dd188b2bf9ff31b7d65c2425b9dd864d5426c9c
Ivar Nesje <ivarne@users.noreply.github.com>
2022-05-04 08:00:18 +0200

    Log exceptions as data instead of string (#63)

--- src/Altinn.App.Api/Controllers/ProcessController.cs
+++ src/Altinn.App.Api/Controllers/ProcessController.cs
@@ -468,7 +468,7 @@ namespace Altinn.App.Api.Controllers
 
         private ActionResult ExceptionResponse(Exception exception, string message)
         {
-            _logger.LogError($"{message}: {exception}");
+            _logger.LogError(exception, message);
 
             if (exception is PlatformHttpException)
             {
