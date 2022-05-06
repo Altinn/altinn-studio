91e7d6726a208a2c7f412f1bb4c395ecb7fae0f0
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-05-06 09:36:58 +0200

    Chore/make test run on macos (#82)
    * first go at replacing windows path separators
    
    * fix path related issues
    
    * build and test action for windoes and macos
    
    * fix some warnings

--- test/Altinn.PlatformServices.Tests/Helpers/PathHelperTests.cs
+++ test/Altinn.PlatformServices.Tests/Helpers/PathHelperTests.cs
@@ -8,7 +8,6 @@ namespace Altinn.App.PlatformServices.Tests.Helpers
         [Theory]
         [InlineData("/this/is/legal/path", "/this/is/legal/path/test")]
         [InlineData("/this/is/legal/path", "/this/is/legal/path/test/test2/../../test2")]
-        [InlineData("c:\\this\\is\\legal\\path", "c:\\this\\is\\legal\\path\\test")]
         public void AbsoluteLegalPathsReturnsTrueForValidPaths(string legalPath, string filePath)
         {
             Assert.True(PathHelper.ValidateLegalFilePath(legalPath, filePath));
@@ -17,7 +16,6 @@ namespace Altinn.App.PlatformServices.Tests.Helpers
         [Theory]
         [InlineData("/this/is/legal/path", "/this/is/not/legal/path/test")]
         [InlineData("/this/is/legal/path", "/this/is/legal/path/test/../../../../test2")]
-        [InlineData("c:\\this\\is\\legal\\path", "c:\\this\\is\\not\\legal\\path\\test")]
         public void AbsoluteLegalPathsReturnsFalseForInvalidPaths(string legalPath, string filePath)
         {
             Assert.False(PathHelper.ValidateLegalFilePath(legalPath, filePath));
@@ -26,7 +24,6 @@ namespace Altinn.App.PlatformServices.Tests.Helpers
         [Theory]
         [InlineData("this/is/legal/path", "this/is/legal/path/test")]
         [InlineData("this/is/legal/path", "this/is/legal/path/test/test2/../../test2")]
-        [InlineData("this\\is\\legal\\path", "this\\is\\legal\\path\\test")]
         public void RelativeLegalPathsReturnsTrueForValidPaths(string legalPath, string filePath)
         {
             Assert.True(PathHelper.ValidateLegalFilePath(legalPath, filePath));
