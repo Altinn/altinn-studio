bd61b48f429d83d9b7042a224f9023f5b241ea6e
tba76 <thomabak@gmail.com>
2022-05-18 12:37:58 +0200

    Query model for request body

---
+++ src/Altinn.Platform/Altinn.Platform.Storage/Storage/Helpers/MessageBoxQueryModel.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Altinn.Platform.Storage.Helpers
+{
+    /// <summary>
+    /// Represents queryparams for search from messagebox.
+    /// </summary>
+    public class MessageBoxQueryModel
+    {
+        /// <summary>
+        /// List of instance owner party id(s)
+        /// </summary>
+        [JsonPropertyName("instanceOwnerPartyId")]
+        public List<int> InstanceOwnerPartyId { get; set; }
+
+        /// <summary>
+        /// The application id
+        /// </summary>
+        [JsonPropertyName("appId")]
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// Boolean indicating whether to include active instances.
+        /// </summary>
+        [JsonPropertyName("includeActive")]
+        public bool IncludeActive { get; set; }
+
+        /// <summary>
+        /// Boolean indicating whether to include archived instances.
+        /// </summary>
+        [JsonPropertyName("includeArchived")]
+        public bool IncludeArchived { get; set; }
+
+        /// <summary>
+        /// Boolean indicating whether to include deleted instances.
+        /// </summary>
+        [JsonPropertyName("includeDeleted")]
+        public bool IncludeDeleted { get; set; }
+
+        /// <summary>
+        /// Last changed date.
+        /// </summary>
+        [JsonPropertyName("lastChanged")]
+        public string LastChanged { get; set; }
+
+        /// <summary>
+        /// Created time.
+        /// </summary>
+        [JsonPropertyName("created")]
+        public string Created { get; set; }
+
+        /// <summary>
+        /// Search string.
+        /// </summary>
+        [JsonPropertyName("searchString")]
+        public string SearchString { get; set; }
+
+        /// <summary>
+        /// The archive reference.
+        /// </summary>
+        [JsonPropertyName("archiveReference")]
+        public string ArchiveReference { get; set; }
+
+        /// <summary>
+        /// Language nb, en, nn-NO
+        /// </summary>
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+    }
+}
