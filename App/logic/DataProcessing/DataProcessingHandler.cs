1030465c892810aebcac64b63d0963af89324dfd
Kylstad, Nina <nkylstad@gmail.com>
2022-05-18 11:03:16 +0200

    anonymous stateless

--- App/logic/DataProcessing/DataProcessingHandler.cs
+++ App/logic/DataProcessing/DataProcessingHandler.cs
@@ -2,49 +2,95 @@ using System;
 using System.Threading.Tasks;
 using Altinn.Platform.Storage.Interface.Models;
 
-//// using Altinn.App.Models; // <-- Uncomment this line to refer to app model(s)
+using Altinn.App.Models;
 
 namespace Altinn.App.AppLogic.DataProcessing
 {
+  /// <summary>
+  /// Represents a business logic class responsible for running calculations on an instance.
+  /// </summary>
+  public class DataProcessingHandler
+  {
     /// <summary>
-    /// Represents a business logic class responsible for running calculations on an instance.
+    /// Perform data processing on data read. When reading data from App API
     /// </summary>
-    public class DataProcessingHandler
+    /// <example>
+    /// if (data.GetType() == typeof(Skjema)
+    /// {
+    ///     Skjema model = (Skjema)data;
+    ///     // Perform calculations and manipulation of data model here
+    /// }
+    /// </example>
+    /// <param name="instance">The instance that data belongs to</param>
+    /// <param name="dataId">The dataId for data if available</param>
+    /// <param name="data">The data as object</param>
+    public async Task<bool> ProcessDataWrite(Instance instance, Guid? dataId, object data)
     {
-        /// <summary>
-        /// Perform data processing on data read. When reading data from App API
-        /// </summary>
-        /// <example>
-        /// if (data.GetType() == typeof(Skjema)
-        /// {
-        ///     Skjema model = (Skjema)data;
-        ///     // Perform calculations and manipulation of data model here
-        /// }
-        /// </example>
-        /// <param name="instance">The instance that data belongs to</param>
-        /// <param name="dataId">The dataId for data if available</param>
-        /// <param name="data">The data as object</param>
-        public async Task<bool> ProcessDataRead(Instance instance, Guid? dataId, object data)
-        {
-            return await Task.FromResult(false);
-        }
-
-        /// <summary>
-        /// Perform data processing on data write. When posting and putting data against app
-        /// </summary>
-        /// <example>
-        /// if (data.GetType() == typeof(Skjema)
-        /// {
-        ///     Skjema model = (Skjema)data;
-        ///     // Perform calculations and manipulation of data model here
-        /// }
-        /// </example>
-        /// <param name="instance">The instance that data belongs to</param>
-        /// <param name="dataId">The dataId for data if available</param>
-        /// <param name="data">The data as object</param>
-        public async Task<bool> ProcessDataWrite(Instance instance, Guid? dataId, object data)
-        {
-            return await Task.FromResult(false);
-        }
+      return await Task.FromResult(false);
     }
+
+    /// <summary>
+    /// Perform data processing on data write. When posting and putting data against app
+    /// </summary>
+    /// <example>
+    /// if (data.GetType() == typeof(Skjema)
+    /// {
+    ///     Skjema model = (Skjema)data;
+    ///     // Perform calculations and manipulation of data model here
+    /// }
+    /// </example>
+    /// <param name="instance">The instance that data belongs to</param>
+    /// <param name="dataId">The dataId for data if available</param>
+    /// <param name="data">The data as object</param>
+    public async Task<bool> ProcessDataRead(Instance instance, Guid? dataId, object data)
+    {
+      if (data.GetType() != typeof(Skjema))
+      {
+        return false;
+      }
+      Skjema skjema = (Skjema)data;
+      SetupModel(skjema);
+      SetIdNumber(skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317);
+      SetJournalNumber(skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.Journalnummerdatadef33316);
+
+      return await Task.FromResult(true);
+    }
+
+    private void CheckName(AnsattNavndatadef1223 name)
+    {
+      if (name?.value == "test")
+      {
+        name.value = "automation";
+      }
+    }
+
+    private void SetJournalNumber( Journalnummerdatadef33316 journalNumber)
+    {
+      if (journalNumber?.value == null)
+      {
+        journalNumber.value = 1234;
+      }
+    }
+
+    private void SetIdNumber(IdentifikasjonsnummerKravdatadef33317 idField)
+    {
+      if (idField?.value == null)
+      {
+        idField.value = "1234567890";
+      }
+      else if (idField?.value == "1337")
+      {
+        idField.value = "1705";
+      }
+    }
+
+    private void SetupModel(Skjema skjema)
+    {
+      skjema.OpplysningerOmArbeidstakerengrp8819 ??= new OpplysningerOmArbeidstakerengrp8819();
+      skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854 ??= new Skjemainstansgrp8854();
+      skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.Journalnummerdatadef33316 ??= new Journalnummerdatadef33316();
+      skjema.OpplysningerOmArbeidstakerengrp8819.Skjemainstansgrp8854.IdentifikasjonsnummerKravdatadef33317 ??= new IdentifikasjonsnummerKravdatadef33317();
+      skjema.OpplysningerOmArbeidstakerengrp8819.OpplysningerOmArbeidstakerengrp8855 ??= new OpplysningerOmArbeidstakerengrp8855();
+    }
+  }
 }
