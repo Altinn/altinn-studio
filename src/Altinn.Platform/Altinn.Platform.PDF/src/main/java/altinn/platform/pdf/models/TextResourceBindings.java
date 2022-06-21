b83452645a5345c667022daf99c348f74fa7093d
Håkon Carson Bogsrud <hakon.carson.bogsrud@digdir.no>
2022-06-21 12:09:42 +0200

    fix: handle Panel component in PDF generation

--- src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/TextResourceBindings.java
+++ src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/TextResourceBindings.java
@@ -3,12 +3,15 @@ package altinn.platform.pdf.models;
 public class TextResourceBindings {
   private String title;
   private String description;
+  private String body;
 
   public String getTitle() { return title; }
 
   public void setTitle(String title) { this.title = title; }
 
   public String getDescription() { return description; }
+
+  public String getBody() { return body; }
 
   public void setDescription(String description) { this.description = description; }
 }
