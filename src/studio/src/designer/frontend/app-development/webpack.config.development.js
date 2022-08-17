6fcc39d4ed045e44bfda87d3329d866cb710dde4
Nina Kylstad <nkylstad@gmail.com>
2022-08-17 12:40:10 +0200

    add CORS headers to enable hotreload in dev (#8811)

--- src/studio/src/designer/frontend/app-development/webpack.config.development.js
+++ src/studio/src/designer/frontend/app-development/webpack.config.development.js
@@ -47,5 +47,10 @@ module.exports = {
         warnings: false,
       },
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
 };
