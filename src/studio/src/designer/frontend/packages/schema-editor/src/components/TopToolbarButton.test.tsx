ebb250199ee421c6f35c8eadcad70bf2ecd6e835
Michael Johansen <michael.johansen@gmail.com>
2022-08-19 13:28:03 +0200

    suggestion/not scan images on unrelated changes

--- src/studio/src/designer/frontend/packages/schema-editor/src/components/TopToolbarButton.test.tsx
+++ src/studio/src/designer/frontend/packages/schema-editor/src/components/TopToolbarButton.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import TopToolbarButton from './TopToolbarButton';
-import {
-  fireEvent,
-  render,
-  RenderResult,
-  screen,
-} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const renderButton = (text: string, style = 'text', disabled = false) => {
   const handleClick = jest.fn();
+  const user = userEvent.setup();
   act(() => {
     render(
       <TopToolbarButton
@@ -23,56 +20,52 @@ const renderButton = (text: string, style = 'text', disabled = false) => {
       </TopToolbarButton>,
     );
   });
-  return handleClick;
+  return { handleClick, user };
 };
 
-test('renders a text button', async () => {
+test('renders a text button', () => {
   renderButton('delete');
-  const button = await screen.findByRole('button');
+  const button = screen.getByRole('button');
   expect(button).toBeDefined();
   expect(button.textContent).toBe('delete');
 });
 
-test('renders a icon only button with aria-label', async () => {
+test('renders a icon only button with aria-label', () => {
   renderButton('delete', 'icon');
-  const button = await screen.findByRole('button');
+  const button = screen.getByRole('button');
   expect(button).toBeDefined();
   expect(button.textContent).not.toBe('delete');
   expect(button.getAttribute('aria-label')).toBe('delete');
   expect(button.getAttribute('class')).toContain('makeStyles-iconButton');
 });
 
-test('renders a warning button', async () => {
+test('renders a warning button', () => {
   renderButton('delete', 'warning');
-  const button = await screen.findByRole('button');
+  const button = screen.getByRole('button');
   expect(button).toBeDefined();
   expect(button.getAttribute('class')).toContain('warn');
 });
 
 test('reacts to being clicked', async () => {
-  const handleClick = renderButton('delete', 'text');
-  const button = await screen.findByRole('button');
-  fireEvent.click(button);
+  const { handleClick, user } = renderButton('delete', 'text');
+  await user.click(screen.getByRole('button'));
   expect(handleClick).toBeCalledTimes(1);
 });
 
 test('rects to being clicked (icon button)', async () => {
-  const handleClick = renderButton('delete', 'icon');
-  const button = await screen.findByRole('button');
-  fireEvent.click(button);
+  const { handleClick, user } = renderButton('delete', 'icon');
+  await user.click(screen.getByRole('button'));
   expect(handleClick).toBeCalledTimes(1);
 });
 
-test('does nothing when disabled', async () => {
-  const handleClick = renderButton('delete', 'text', true);
-  const button = await screen.findByRole('button');
-  fireEvent.click(button);
+test('does nothing when disabled', () => {
+  const { handleClick } = renderButton('delete', 'text', true);
+  fireEvent.click(screen.getByRole('button'));
   expect(handleClick).toBeCalledTimes(0);
 });
 
-test('does nothing when disabled (icon button)', async () => {
-  const handleClick = renderButton('delete', 'icon', true);
-  const button = await screen.findByRole('button');
-  fireEvent.click(button);
+test('does nothing when disabled (icon button)', () => {
+  const { handleClick } = renderButton('delete', 'icon', true);
+  fireEvent.click(screen.getByRole('button'));
   expect(handleClick).toBeCalledTimes(0);
 });
