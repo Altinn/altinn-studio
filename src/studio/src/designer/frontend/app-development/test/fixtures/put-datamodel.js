b010ba5f924d54a84462459227343ab01c98c9b2
Michael Johansen <michael.johansen@gmail.com>
2022-09-06 15:50:57 +0200

    8895 datamodelling schema attribs as data

--- src/studio/src/designer/frontend/app-development/test/fixtures/put-datamodel.js
+++ src/studio/src/designer/frontend/app-development/test/fixtures/put-datamodel.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const fs = require('fs');
+const prettier = require('prettier');
 module.exports = (modelpath, jsonSchema) => {
   const filename = modelpath.split('/').pop();
   const filepath = path.resolve(__dirname, 'storage', filename);
-  fs.writeFileSync(filepath, JSON.stringify(jsonSchema), 'utf-8');
+  const content = JSON.stringify(jsonSchema);
+  const contentFormatted = prettier.format(content, {
+    parser: 'json',
+    printWidth: 120,
+  });
+  fs.writeFileSync(filepath, contentFormatted, 'utf-8');
   return jsonSchema;
 };
