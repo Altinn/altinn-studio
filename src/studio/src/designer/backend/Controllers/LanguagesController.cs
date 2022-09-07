37af402a4840d6c23945d82929d8bb0367b19969
Andrea Standeren <andn@Andreas-MacBook-Pro.local>
2022-09-07 13:21:05 +0200

    Add delete language endpoint to languagecontroller

--- src/studio/src/designer/backend/Controllers/LanguagesController.cs
+++ src/studio/src/designer/backend/Controllers/LanguagesController.cs
@@ -48,5 +48,24 @@ namespace Altinn.Studio.Designer.Controllers
 
             return Ok(languages);
         }
+
+        /// <summary>
+        /// Endpoint for deleting a specific language in the application.
+        /// </summary>
+        /// <param name="org">Unique identifier of the organisation responsible for the app.</param>
+        /// <param name="repo">Application identifier which is unique within an organisation.</param>
+        /// <param name="languageCode">Language identifier.</param>
+        /// <returns>List of languages as JSON</returns>
+        [HttpDelete]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<bool> DeleteLanguage(string org, string repo, string languageCode)
+        {
+            string developer = AuthenticationHelper.GetDeveloperUserName(HttpContext);
+
+            bool deleted = _languagesService.DeleteLanguage(org, repo , developer, languageCode);
+
+            return Ok(deleted);
+        }
     }
 }
