ea8a374114a191602a3203ef70c839261ec40933
Mirko Sekulic <misha.sekulic@gmail.com>
2022-09-13 13:13:59 +0200

    carry targetNamespace to jsonSchema and c# classes

---
+++ src/studio/src/designer/backend.Tests/FluentTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Designer.Tests;
+
+/// <summary>
+/// TODO: This is the same class as the one in the DataModeling.Tests project.
+/// TODO: Create new project or externalize class in some common nuget package.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[Obsolete("Move and merge with same class from Datamodeling.Tests")]
+public class FluentTestsBase<T>
+    where T : class
+{
+    protected T Given => this as T;
+
+    protected T That => this as T;
+
+    protected T And => this as T;
+
+    protected T When => this as T;
+
+    protected T Then => this as T;
+
+    protected T But => this as T;
+}
