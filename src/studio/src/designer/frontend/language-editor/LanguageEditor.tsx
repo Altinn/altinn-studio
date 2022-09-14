f34ba5a15c3fe74b209d799819dfef92df1f4555
rubyjane88 <rubyjane88@gmail.com>
2022-09-14 11:27:03 +0200

    update

--- src/studio/src/designer/frontend/language-editor/LanguageEditor.tsx
+++ src/studio/src/designer/frontend/language-editor/LanguageEditor.tsx
@@ -3,8 +3,8 @@
 
 import React from 'react';
 import Select from 'react-select';
-import ISO6391 from 'iso-639-1'
-import debounce from 'lodash.debounce';
+
+import ISO6391 from 'iso-639-1';
 
 import type { ILanguageEditor } from './utils';
 
@@ -12,16 +12,79 @@ import { Button, TextField } from '@altinn/altinn-design-system';
 import AltinnColumnLayout from 'app-shared/components/AltinnColumnLayout';
 import AltinnRadioGroup from 'app-shared/components/AltinnRadioGroup';
 import { getAllTranslationKeys, transformLanguages } from './utils';
-import { updateLanguage} from '../../frontend/app-development/features/languageEditor/utils'
 
 import { makeStyles, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: '#17C96B',
+    border: 'none',
+    boxShadow: 'none',
+    boxSizing: 'border-box',
+    height: '36px',
+    position: 'sticky',
+    marginBottom: '29px',
+    marginTop: '5rem',
+    top: '177px',
+    width: '100px',
+  },
+  btnSecondary: {
+    backgroundColor: 'transparent',
+    border: '2px dashed #ccc',
+    height: '36px',
+    width: '84px',
+    active: {
+      border: '2px dashed #000000',
+    },
+  },
+  leftColBodyContainer: {
+    display: 'grid',
+    backgroundColor: '#FFF',
+    height: '100%',
+    columnGap: '10rem',
+    gridTemplateColumns: '1fr 2fr',
+    gridAutoRows: 'minmax(100px, auto)',
+    paddingRight: '3rem',
+    marginTop: '2rem',
+    width: '100%',
+  },
+  lineBorder: {
+    border: '0.5px solid #BCC7CC',
+    position: 'sticky',
+    marginTop: '2rem',
+    top: '0px',
+    width: '100%',
+  },
+  radioGroup: {
+    width: '100%',
+    display: 'flex',
+    borderBottom: '1px solid #BCC7CC',
+    marginBottom: '5px',
+    padding: '1rem',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  rightColBodyContainer: {
+    backgroundColor: '#E5E5E5',
+    height: '100%',
+    padding: '7rem',
+    width: '100%',
+  },
+  stickyHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'sticky',
+    marginBottom: '8rem',
+    top: '0',
+  },
+});
+
 interface ILanguageEditorProps extends ILanguageEditor {
   onTranslationChange: ({
-                          translationKey,
-                          langCode,
-                          newValue,
-                        }: {
+    translationKey,
+    langCode,
+    newValue,
+  }: {
     translationKey: string;
     langCode: string;
     newValue: string;
@@ -30,18 +93,18 @@ interface ILanguageEditorProps extends ILanguageEditor {
 }
 
 export const LanguageEditor = ({
-                                 newSprakField,
-                                 setNewSprakField,
-                                 isNewTextInput,
-                                 sprak,
-                                 setSprak,
-                                 selectedSprak,
-                                 setSelectedSprak,
-                                 languages,
-                                 setIsNewTextInput,
-                               }: // onKeyChange,
+  newSprakField,
+  setNewSprakField,
+  isNewTextInput,
+  sprak,
+  setSprak,
+  selectedSprak,
+  setSelectedSprak,
+  languages,
+  setIsNewTextInput,
+}: // onKeyChange,
 // onTranslationChange,
-                                 ILanguageEditorProps) => {
+ILanguageEditorProps) => {
   const classes = useStyles();
   const allTranslationKeys = getAllTranslationKeys({ languages });
   const transformedLanguages = transformLanguages({
@@ -116,14 +179,14 @@ export const LanguageEditor = ({
                                   top: '95px',
                                   left: '36px',
                                   lineHeight: '24.32px',
-                                  width: '100%',
+                                  width: '323px',
                                 }}
                               >
               <span>
                 Vi anbefaler å legge til oversettelser for bokmål, nynorsk og
                 engelsk. Ved behov kan du også legge til andre språk.
               </span>
-                              </div>
+            </div>
 
                               <div
                                 style={{
@@ -195,11 +258,11 @@ export const LanguageEditor = ({
                               <div
                                 style={{
                                   display: 'flex',
-                                  width: '100%',
-                                  justifyContent: 'space-around',
+                                  width: '400px',
+                                  justifyContent: 'space-evenly',
                                 }}
                               >
-                                <div style={{border: '1px solid #000000', width: '240px' }}>
+                                <div style={{ width: '280px' }}>
                                   <Select
                                     onChange={handleSelectOnChange}
                                     options={modifiedLanguageCodes}
@@ -220,7 +283,7 @@ export const LanguageEditor = ({
                             </div>
                           }
       >
-        <div style={{marginBottom: '10rem', backgroundColor: "#fff"}}>
+        <div style={{marginBottom: '10rem'}}>
           <div className={classes.stickyHeader}>
             <Button
               id='nyTekstBtn'
@@ -291,13 +354,6 @@ export const LanguageEditor = ({
                               defaultValue={
                                 transformedLanguages[translationKey][language]
                               }
-                              onChange={debounce(async (e) => {
-                                await updateLanguage({
-                                  languages,
-                                  translationKey,
-                                  e,
-                                });
-                              }, 1000)}
                             />
                           </div>
                         );
@@ -313,67 +369,3 @@ export const LanguageEditor = ({
     </div>
   );
 };
-
-
-const useStyles = makeStyles({
-  btn: {
-    backgroundColor: '#17C96B',
-    border: 'none',
-    boxShadow: 'none',
-    boxSizing: 'border-box',
-    height: '36px',
-    position: 'sticky',
-    marginBottom: '29px',
-    marginTop: '5rem',
-    top: '177px',
-    width: '100px',
-  },
-  btnSecondary: {
-    backgroundColor: 'transparent',
-    border: '2px dashed #ccc',
-    height: '36px',
-    width: '84px',
-    active: {
-      border: '2px dashed #000000',
-    },
-  },
-  leftColBodyContainer: {
-    display: 'grid',
-    backgroundColor: '#FFF',
-    height: '100%',
-    columnGap: '10rem',
-    gridTemplateColumns: '1fr 2fr',
-    gridAutoRows: 'minmax(100px, auto)',
-    marginTop: '2rem',
-    width: '100%',
-  },
-  lineBorder: {
-    border: '1px solid #BCC7CC',
-    marginTop: '2rem',
-    top: '0px',
-    width: '100%',
-  },
-  radioGroup: {
-    width: '100%',
-    display: 'flex',
-    borderBottom: '1px solid #BCC7CC',
-    marginBottom: '5px',
-    padding: '1rem',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  rightColBodyContainer: {
-    height: '100%',
-    padding: '7rem',
-    width: '100%',
-  },
-  stickyHeader: {
-    borderBottom: '1px solid #BCC7CC',
-    display: 'flex',
-    justifyContent: 'space-between',
-    paddingBottom: '1rem',
-    position: 'sticky',
-    marginBottom: '5rem',
-    top: '0',
-  },
-});
