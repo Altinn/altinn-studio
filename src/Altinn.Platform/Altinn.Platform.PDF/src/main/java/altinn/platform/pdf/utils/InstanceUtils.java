Altinn/altinn-studio

commit 5d32064b3db40de7fbfa66b6ba41ee3e85b9c05b

Extended characters in PDF (#8556)
* extended character support

* Upgrade test to junit.jupiter and cleanup some of the code (#8557)

* Upgrade test to junit.jupiter and some code cleanup

* Fiks loading of font from main/resources

* minor code cleanup

* updated tests

* Sonarcloud code smells

* code readability

* code smell

* patch

* Changed testname to match the rest.

Co-authored-by: Vemund Gaukstad <tjololo@users.noreply.github.com>
Co-authored-by: tba76 <thomabak@gmail.com>

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/InstanceUtils.java
@@ -26,7 +26,7 @@ public class InstanceUtils {
 
     for (Data data: instance.getData()) {
       if (id.equals(data.getDataType())) {
-        list.add(TextUtils.removeIllegalChars(data.getFilename()));
+        list.add(data.getFilename());
       }
     }
     return list;
@@ -46,7 +46,7 @@ public class InstanceUtils {
 
     for (Data data: instance.getData()) {
       if (id.equals(data.getDataType())) {
-        map.put(TextUtils.removeIllegalChars(data.getFilename()), data.getTags());
+        map.put(data.getFilename(), data.getTags());
       }
     }
     return map;

src/Altinn.Platform/Altinn.Platform.PDF/pom.xml
@@ -8,7 +8,7 @@
   <properties>
     <build.source>17</build.source>
     <build.target>17</build.target>
-    <log4j2.version>2.17.1</log4j2.version>
+    <log4j2.version>2.17.2</log4j2.version>
   </properties>
   <build>
     <plugins>
@@ -36,12 +36,6 @@
     <version>2.7.0</version>
   </parent>
   <dependencies>
-    <dependency>
-      <groupId>junit</groupId>
-      <artifactId>junit</artifactId>
-      <version>4.13.2</version>
-      <scope>test</scope>
-    </dependency>
     <dependency>
       <groupId>org.apache.pdfbox</groupId>
       <artifactId>pdfbox</artifactId>

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/models/FormLayoutElement.java
@@ -15,7 +15,7 @@ import org.apache.commons.lang3.SerializationUtils;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class FormLayoutElement implements Cloneable{
+public class FormLayoutElement implements Cloneable {
   private String type;
   private String id;
   private HashMap<String, String> dataModelBindings;
@@ -54,6 +54,6 @@ public class FormLayoutElement implements Cloneable{
 
     fle.dataModelBindings = SerializationUtils.clone(dataModelBindings);
     return fle;
-}
+  }
 }
 

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/services/PDFGenerator.java
@@ -13,19 +13,21 @@ import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.*;
 import org.apache.pdfbox.pdmodel.documentinterchange.markedcontent.PDMarkedContent;
 import org.apache.pdfbox.pdmodel.documentinterchange.markedcontent.PDPropertyList;
 import org.apache.pdfbox.pdmodel.documentinterchange.taggedpdf.StandardStructureTypes;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitWidthDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.viewerpreferences.PDViewerPreferences;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.w3c.dom.Document;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -46,9 +48,10 @@ public class PDFGenerator {
   private float headerFontSize = 14;
   private float leading = 1.2f * fontSize;
   private float margin = 50;
-  private PDFont font;
-  private PDFont fontBold;
+  private PDType0Font font;
+  private PDType0Font fontBold;
   private TextResources textResources;
+  private String data;
   private Instance instance;
   private Document formData;
   private FormLayout originalFormLayout;
@@ -77,7 +80,6 @@ public class PDFGenerator {
     "Summary"
   );
 
-
   /**
    * Constructor for the AltinnPDFGenerator object
    */
@@ -89,6 +91,7 @@ public class PDFGenerator {
     this.originalFormLayout = pdfContext.getFormLayout();
     this.formLayouts = pdfContext.getFormLayouts();
     this.optionsDictionary = pdfContext.getOptionsDictionary();
+    this.data = pdfContext.getData();
     this.textResources = pdfContext.getTextResources();
     this.instance = pdfContext.getInstance();
     this.output = new ByteArrayOutputStream();
@@ -97,12 +100,6 @@ public class PDFGenerator {
     this.language = pdfContext.getLanguage();
     this.userProfile = pdfContext.getUserProfile();
     this.layoutSettings = pdfContext.getLayoutSettings();
-    try {
-      this.formData = FormUtils.parseXml(pdfContext.getData());
-      this.textResources.setResources(parseAndCleanTextResources(this.textResources.getResources(), this.formData));
-    } catch (Exception e) {
-      BasicLogger.log(Level.SEVERE, e.toString());
-    }
   }
 
   /**
@@ -122,11 +119,23 @@ public class PDFGenerator {
     PDDocumentCatalog catalog = document.getDocumentCatalog();
     catalog.setDocumentOutline((outline));
     PDResources resources = new PDResources();
-    font = PDType1Font.HELVETICA;
-    fontBold = PDType1Font.HELVETICA_BOLD;
-    resources.put(COSName.getPDFName("Helv"), font);
+
+    font = PDType0Font.load(document, new FileInputStream("./classes/font/inter/Inter-Medium.ttf"), true);
+    fontBold = PDType0Font.load(document, new FileInputStream("./classes/font/inter/Inter-Bold.ttf"), true);
+    COSName fontCOSName = resources.add(font);
+
+    data = new String(Base64.decodeBase64(data), StandardCharsets.UTF_8);
+    data = TextUtils.removeIllegalChars(data, font);
+
+    try {
+      formData = FormUtils.parseXml(data);
+      textResources.setResources(parseAndCleanTextResources(textResources.getResources(), formData, font));
+    } catch (Exception e) {
+      BasicLogger.log(Level.SEVERE, e.toString());
+    }
+
     form.setDefaultResources(resources);
-    String defaultAppearance = "/Helv 10 Tf 0 0 0 rg";
+    String defaultAppearance = "/" + fontCOSName.getName() + " 10 Tf 0 0 0 rg";
     form.setDefaultAppearance(defaultAppearance);
     catalog.setAcroForm(form);
     catalog.setLanguage(getLanguage());
@@ -384,8 +393,7 @@ public class PDFGenerator {
     currentContent.beginText();
     currentContent.newLineAtOffset(xPoint, yPoint);
     currentContent.setFont(fontBold, headerFontSize);
-    String unparsedHeader = TextUtils.getAppOwnerName(instance.getOrg(), getLanguage(), textResources) + " - " + TextUtils.getAppName(textResources);
-    String header = TextUtils.removeIllegalChars(unparsedHeader);
+    String header = TextUtils.getAppOwnerName(instance.getOrg(), getLanguage(), textResources) + " - " + TextUtils.getAppName(textResources);
     List<String> lines = TextUtils.splitTextToLines(header, fontBold, headerFontSize, width);
     for (String line : lines) {
       currentContent.showText(line);
@@ -445,7 +453,7 @@ public class PDFGenerator {
     currentContent = new PDPageContentStream(document, currentPage);
   }
 
-  private void renderText(String text, PDFont font, float fontSize, String type) throws IOException {
+  private void renderText(String text, PDType0Font font, float fontSize, String type) throws IOException {
     addSection(currentPart);
     beginMarkedContent(COSName.P);
     addContentToCurrentSection(COSName.P, type);
@@ -621,7 +629,7 @@ public class PDFGenerator {
     float indent = 10;
     currentContent.newLineAtOffset(xPoint + indent, yPoint);
     for (String file : files) {
-      currentContent.showText("- " + file);
+      currentContent.showText("- " + TextUtils.removeIllegalChars(file, font));
       currentContent.newLineAtOffset(0, -leading);
       yPoint -= leading;
     }
@@ -640,7 +648,7 @@ public class PDFGenerator {
     for (String name : files.keySet()) {
       List<String> tags = files.get(name);
 
-      currentContent.showText("- " + name + " - ");
+      currentContent.showText("- " + TextUtils.removeIllegalChars(name, font) + " - ");
       for (String tag : tags) {
         if (tag != tags.get(tags.size() - 1))
           currentContent.showText(tag + ", ");
@@ -707,11 +715,11 @@ public class PDFGenerator {
     currentContent.beginMarkedContent(name, PDPropertyList.create(currentMarkedContentDictionary));
   }
 
-  private List<TextResourceElement> parseAndCleanTextResources(List<TextResourceElement> resources, Document formData) {
+  private List<TextResourceElement> parseAndCleanTextResources(List<TextResourceElement> resources, Document formData, PDType0Font font) {
     List<String> replaceValues = new ArrayList<>();
 
     for (TextResourceElement res : resources) {
-      res.setValue(TextUtils.removeIllegalChars(res.getValue()));
+      res.setValue(TextUtils.removeIllegalChars(res.getValue(), font));
       replaceValues.clear();
       if (res.getVariables() != null) {
         for (TextResourceVariableElement variable : res.getVariables()) {

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/FormUtils.java
@@ -1,8 +1,7 @@
 package altinn.platform.pdf.utils;
 
-import altinn.platform.pdf.models.FormLayoutData;
 import altinn.platform.pdf.models.FormLayoutElement;
-import org.apache.tomcat.util.codec.binary.Base64;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -16,7 +15,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -44,8 +42,7 @@ public class FormUtils {
     }
     String[] keySplit = key.split(Pattern.quote("."));
     Element rootElement = formData.getDocumentElement();
-    String value = getValueOfEndNode(rootElement, keySplit, 0);
-    return TextUtils.removeIllegalChars(value);
+    return getValueOfEndNode(rootElement, keySplit, 0);
   }
 
   /**
@@ -264,22 +261,20 @@ public class FormUtils {
 
 
   /**
-   * Parses the base 64 encoded xml file and creates a Document wrapper
-   * @param xmlBaseEncoded the base 64 xml string
+   * Parses the data xml file and creates a Document wrapper
+   * @param xml the xml string containing data
    * @return a document wrapper for the xml file
    * @throws ParserConfigurationException
    * @throws IOException
    * @throws SAXException
    */
-  public static Document parseXml(String xmlBaseEncoded) throws ParserConfigurationException, IOException, SAXException {
-    byte[] xmlAsBytes = Base64.decodeBase64(xmlBaseEncoded);
-    String xmlAsString = new String(xmlAsBytes, StandardCharsets.UTF_8);
+  public static Document parseXml(String xml) throws ParserConfigurationException, IOException, SAXException {
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
     DocumentBuilder builder = factory.newDocumentBuilder();
-    return builder.parse(new InputSource(new StringReader(xmlAsString)));
+    return builder.parse(new InputSource(new StringReader(xml)));
   }
 }
 

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/LayoutUtils.java
@@ -2,7 +2,7 @@ package altinn.platform.pdf.utils;
 
 import altinn.platform.pdf.models.*;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.w3c.dom.Document;
 
 import java.io.IOException;
@@ -29,7 +29,7 @@ public class LayoutUtils {
    * @return
    * @throws IOException
    */
-  public static float getElementHeight(FormLayoutElement element, PDFont font, float fontSize, float width, float leading, float textMargin, TextResources textResources, Document formData, Instance instance) throws IOException {
+  public static float getElementHeight(FormLayoutElement element, PDType0Font font, float fontSize, float width, float leading, float textMargin, TextResources textResources, Document formData, Instance instance) throws IOException {
     float height = 0;
     TextResourceBindings textResourceBindings = element.getTextResourceBindings();
     if (textResourceBindings.getTitle() != null && !textResourceBindings.getTitle().isEmpty()) {

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/TextUtils.java
@@ -3,8 +3,7 @@ package altinn.platform.pdf.utils;
 import altinn.platform.pdf.models.TextResourceElement;
 import altinn.platform.pdf.models.TextResources;
 import com.google.gson.Gson;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.encoding.WinAnsiEncoding;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
@@ -25,6 +24,8 @@ public class TextUtils {
   private static String appOwnerKey = "appOwner";
   private static String appNameKey = "appName";
   private static String oldAppNameKey = "ServiceName";
+  private static final List<Byte> ILLEGAL_CHAR_TYPES = List.of(Character.CONTROL, Character.FORMAT, Character.PRIVATE_USE, Character.SURROGATE, Character.UNASSIGNED);
+
 
   private TextUtils() {}
 
@@ -79,19 +80,48 @@ public class TextUtils {
   /**
    * Removes illegal chars that pdf-generation does not handle
    * @param raw the unfiltered string
+   * @param font the font used for checking glyphs
    * @return the filtered string
    */
-  public static String removeIllegalChars(String raw) {
-    if (raw == null) {
+  public static String removeIllegalChars(String raw, PDType0Font font) {
+    if (raw == null || raw.length() == 0) {
       return "";
     }
-    StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < raw.length(); i++) {
-        if (WinAnsiEncoding.INSTANCE.contains(raw.charAt(i)) ) {
-            builder.append(raw.charAt(i));
+
+    var builder = new StringBuilder(raw.length());
+    int codePoint;
+    for (int offset = 0; offset < raw.length(); offset += Character.charCount(codePoint))
+    {
+        codePoint = raw.codePointAt(offset);
+        int characterType = Character.getType(codePoint);
+
+        if (ILLEGAL_CHAR_TYPES.contains((byte)characterType)) {
+          if (codePoint == 13 || codePoint == 10) { // newline characters are allowed
+            builder.append(Character.toChars(codePoint));
+          }
+        } else { // not illegal character, append if font can render glyph
+          builder.append(filterUsingFont(font, codePoint));
         }
     }
-    return builder.toString().replaceAll("\\p{Cntrl}", "");
+
+    return builder.toString();
+  }
+
+  /**
+   * Returns string for codePoint if supported by font, else returns empty string
+   * @param font the font used for checking glyphs
+   * @param codePoint the codePoint for the character to check
+   * @return the filtered string
+   */
+  private static String filterUsingFont(PDType0Font font, int codePoint) {
+    try {
+        String text = new String(Character.toChars(codePoint));
+        // Ugly hack for checking if text can be handled by font. Throws if glyph is missing.
+        float width = font.getStringWidth(text);
+        return (width > 0) ? text : "";
+    } catch (final Exception e) {
+        return "";
+    }
   }
 
   /**
@@ -99,7 +129,7 @@ public class TextUtils {
    * @param text the text
    * @return the the height needed to fit the text
    */
-  public static float getHeightNeededForText(String text, PDFont font, float fontSize, float width) throws IOException {
+  public static float getHeightNeededForText(String text, PDType0Font font, float fontSize, float width) throws IOException {
     float fontHeight = getFontHeight(font, fontSize);
     if (text == null || text.length() == 0) {
       return fontHeight;
@@ -112,7 +142,7 @@ public class TextUtils {
       heightNeeded += ((numberOfLines - 1) * fontHeight * 0.865);
     }
     return heightNeeded;
-}
+  }
 
   /**
    * Gets the height needed for a multi line text box
@@ -124,7 +154,7 @@ public class TextUtils {
    * @return the height need in pixels
    * @throws IOException
    */
-  public static float getHeightNeededForTextBox(String text, PDFont font, float fontSize, float width, float leading) throws IOException {
+  public static float getHeightNeededForTextBox(String text, PDType0Font font, float fontSize, float width, float leading) throws IOException {
     float textHeight = getHeightNeededForText(text, font, fontSize, width);
     float leadingDiff = (leading - fontSize);
     return textHeight + leadingDiff*2;
@@ -139,7 +169,7 @@ public class TextUtils {
    * @param width the width of the page
    * @return a list of lines
    */
-  public static List<String> splitTextToLines(String text, PDFont font, float fontSize, float width) throws IOException {
+  public static List<String> splitTextToLines(String text, PDType0Font font, float fontSize, float width) throws IOException {
     List<String> lines = new ArrayList<>();
     if (text == null || text.length() == 0) {
       return lines;
@@ -191,7 +221,7 @@ public class TextUtils {
    * @param fontSize the font size
    * @return the height
    */
-  public static float getFontHeight(PDFont font, float fontSize) {
+  public static float getFontHeight(PDType0Font font, float fontSize) {
     return (font.getFontDescriptor().getCapHeight() / 1000 * fontSize);
   }
 
@@ -203,8 +233,8 @@ public class TextUtils {
    * @param width the width
    * @return a list of lines needed to fit word
    */
-  public static List<String> splitWordToFitWidth(String word, PDFont font, float fontSize, float width) throws IOException {
-    List<String> lines = new ArrayList();
+  public static List<String> splitWordToFitWidth(String word, PDType0Font font, float fontSize, float width) throws IOException {
+    List<String> lines = new ArrayList<>();
     if (word == null || font == null) {
       return lines;
     }
@@ -238,7 +268,7 @@ public class TextUtils {
    * @return the width in pixels
    * @throws IOException
    */
-  public static float getStringWidth(String word, PDFont font, float fontSize) throws IOException {
+  public static float getStringWidth(String word, PDType0Font font, float fontSize) throws IOException {
     return fontSize * font.getStringWidth(word) / 1000;
   }
 

commit bcba4ae2d011a6cca4be809b85d1a85000a065d5

bug: filter illegal chars in file name on pdf
Reverting changes introduced in https://github.com/Altinn/altinn-studio/pull/8556/files#diff-e9fa0350ad73d2e3c298ca18eaaefa7507fa6c38ddd6dc947f4694530f08f174L29 as this caused issues for DAT

src/Altinn.Platform/Altinn.Platform.PDF/src/main/java/altinn/platform/pdf/utils/InstanceUtils.java
@@ -26,7 +26,7 @@ public class InstanceUtils {
 
     for (Data data: instance.getData()) {
       if (id.equals(data.getDataType())) {
-        list.add(data.getFilename());
+        list.add(TextUtils.removeIllegalChars(data.getFilename()));
       }
     }
     return list;
@@ -46,7 +46,7 @@ public class InstanceUtils {
 
     for (Data data: instance.getData()) {
       if (id.equals(data.getDataType())) {
-        map.put(data.getFilename(), data.getTags());
+        map.put(TextUtils.removeIllegalChars(data.getFilename()), data.getTags());
       }
     }
     return map;