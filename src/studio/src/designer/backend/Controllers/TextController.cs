b45322ffc71a99cd32324284754bf9c114d885c2
rubyjane88 <rubyjane88@gmail.com>
2022-09-16 17:46:23 +0200

    reverted additional uncommented code and remove TODO comment

--- src/studio/src/designer/backend/Controllers/TextController.cs
+++ src/studio/src/designer/backend/Controllers/TextController.cs
@@ -98,11 +98,9 @@ namespace Altinn.Studio.Designer.Controllers
         [HttpPost]
         public IActionResult SaveResource([FromBody] dynamic jsonData, string id, string org, string app)
         {
-            // TODO: Why is this method partially commented out?
-            // id = id.Split('-')[0];
-            JObject json = jsonData;
 
-            Console.WriteLine(org + app + id);
+            id = id.Split('-')[0];
+            JObject json = jsonData;
 
             JArray resources = json["resources"] as JArray;
             string[] duplicateKeys = resources.GroupBy(obj => obj["id"]).Where(grp => grp.Count() > 1).Select(grp => grp.Key.ToString()).ToArray();
