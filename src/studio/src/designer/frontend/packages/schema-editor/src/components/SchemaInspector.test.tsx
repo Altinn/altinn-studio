ab308f963183f8f50cd8788e3571b1bdd4fc6ddc
Nina Kylstad <nkylstad@gmail.com>
2022-09-20 12:56:41 +0200

    add workaround to failing test

--- src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaInspector.test.tsx
+++ src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaInspector.test.tsx
@@ -13,6 +13,21 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { FieldType, UiSchemaItem } from '../types';
 
+// workaround for https://jestjs.io/docs/26.x/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 const getMockSchemaByPath = (selectedId: string): UiSchemaItem => {
   const mockUiSchema = buildUISchema(dataMock.definitions, '#/definitions');
   return getUiSchemaItem(mockUiSchema, selectedId);
