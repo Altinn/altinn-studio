802df27c1ddd1024bc52746cae01fb54cf82461d
Andrea Standeren <andn@Andreas-MacBook-Pro.local>
2022-09-27 16:14:25 +0200

    Fix typo

--- src/studio/src/designer/backend/Infrastructure/GitRepository/AltinnGitRepository.cs
+++ src/studio/src/designer/backend/Infrastructure/GitRepository/AltinnGitRepository.cs
@@ -18,7 +18,7 @@ namespace Altinn.Studio.Designer.Infrastructure.GitRepository
     /// </summary>
     /// <remarks>This class knows that the repository is an Altinn Repository and hence knows
     /// about folders and file names and can map them to their respective models.
-    /// It shoud hovever only have methods that are shared between the different types of Altinn Repositories
+    /// It should however only have methods that are shared between the different types of Altinn Repositories
     /// and not any methods that are specific to App or Datamodels repositories.</remarks>
     public class AltinnGitRepository : GitRepository, IAltinnGitRepository
     {
