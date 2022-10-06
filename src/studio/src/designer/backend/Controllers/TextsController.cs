2798d207ec07c2cece73b76360075d12aec5c4ef
Andrea Standeren <andn@Andreas-MacBook-Pro.local>
2022-10-06 13:55:53 +0200

    Remove comment

--- src/studio/src/designer/backend/Controllers/TextsController.cs
+++ src/studio/src/designer/backend/Controllers/TextsController.cs
@@ -21,8 +21,7 @@ namespace Altinn.Studio.Designer.Controllers
     /// which interacts with the text files in the old format.
     /// </remarks>
     [Authorize]
-
-    //[AutoValidateAntiforgeryToken]
+    [AutoValidateAntiforgeryToken]
     [Route("designer/api/v2/{org}/{repo}/texts")]
     public class TextsController : ControllerBase
     {
