ab1be5c6322ef7efa9ac44d637beaf1546310b3b
Andrea Standeren <andn@Andreas-MacBook-Pro.local>
2022-10-06 07:37:06 +0200

    Change v1 texts file to include markdown

--- src/studio/src/designer/backend.Tests/Controllers/ConvertTextsControllerTests.cs
+++ src/studio/src/designer/backend.Tests/Controllers/ConvertTextsControllerTests.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Configuration;
 using Altinn.Studio.Designer.Controllers;
