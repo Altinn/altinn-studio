c25b2d119f47304f445271aa06cc990bbe62a731
Tomas <tomas.engebretsen@gmail.com>
2022-10-12 14:38:16 +0200

    Make enum fields only appear when relevant

--- src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaInspector/ItemRestrictions.tsx
+++ src/studio/src/designer/frontend/packages/schema-editor/src/components/SchemaInspector/ItemRestrictions.tsx
@@ -22,12 +22,12 @@ export interface RestrictionItemProps {
   language: ILanguage;
   onChangeRestrictionValue: (id: string, key: string, value: string) => void;
 }
-interface Props {
+export interface ItemRestrictionsProps {
   selectedNode: UiSchemaNode;
   itemsNode?: UiSchemaNode;
   language: ILanguage;
 }
-export const ItemRestrictions = ({ selectedNode, itemsNode, language }: Props) => {
+export const ItemRestrictions = ({ selectedNode, itemsNode, language }: ItemRestrictionsProps) => {
   const dispatch = useDispatch();
   const handleRequiredChanged = (e: any) => {
     const { checked } = e.target;
@@ -75,14 +75,8 @@ export const ItemRestrictions = ({ selectedNode, itemsNode, language }: Props) =
   };
 
   const t = (key: string) => getTranslation(key, language);
-  const selectedRestrictionProps: RestrictionItemProps = {
-    restrictions: selectedNode.restrictions ?? {},
-    readonly: selectedNode.ref !== undefined,
-    path: selectedNode.pointer,
-    onChangeRestrictionValue,
-    language,
-  };
-  const itemsRestrictionProps: RestrictionItemProps = {
+  const itemNode = selectedNode.objectKind === ObjectKind.Array ? itemsNode : selectedNode;
+  const restrictionProps: RestrictionItemProps = {
     restrictions: itemsNode?.restrictions ?? {},
     readonly: itemsNode?.ref !== undefined,
     path: itemsNode?.pointer ?? '',
@@ -90,8 +84,6 @@ export const ItemRestrictions = ({ selectedNode, itemsNode, language }: Props) =
     language,
   };
   const restrictionNode = itemsNode ?? selectedNode;
-  const restrictionProps =
-    selectedNode.objectKind === ObjectKind.Array ? itemsRestrictionProps : selectedRestrictionProps;
   return (
     <div className={classes.root}>
       <Checkbox
@@ -113,8 +105,8 @@ export const ItemRestrictions = ({ selectedNode, itemsNode, language }: Props) =
           [CombinationKind.AnyOf]: undefined,
           [CombinationKind.OneOf]: undefined,
         }[restrictionNode.fieldType]}
-      {selectedNode.objectKind === ObjectKind.Array && <ArrayRestrictions {...selectedRestrictionProps} />}
-      {selectedNode.fieldType !== FieldType.Object && (
+      {selectedNode.objectKind === ObjectKind.Array && <ArrayRestrictions {...restrictionProps} />}
+      {[FieldType.String, FieldType.Integer, FieldType.Number].includes(itemNode?.fieldType as FieldType) && (
         <>
           <Divider />
           <Fieldset legend={t('enum_legend')}>
