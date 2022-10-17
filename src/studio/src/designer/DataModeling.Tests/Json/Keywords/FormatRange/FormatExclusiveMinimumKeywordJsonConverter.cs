a93a9c989ddc0ddda720c9734e832792de43e962
Mirko Sekulic <misha.sekulic@gmail.com>
2022-10-17 16:35:33 +0200

    feat: format range tests

---
+++ src/studio/src/designer/DataModeling.Tests/Json/Keywords/FormatRange/FormatExclusiveMinimumKeywordJsonConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+using Altinn.Studio.DataModeling.Json.Keywords;
+using FluentAssertions;
+using Xunit;
+
+namespace DataModeling.Tests.Json.Keywords.FormatRange;
+
+public class FormatExclusiveMinimumKeywordJsonConverter: FormatRangeTestBase<FormatExclusiveMinimumKeywordJsonConverter, FormatExclusiveMinimumKeyword>
+{
+    private const string KeywordPlaceholder = "formatExclusiveMinimum";
+
+    protected override JsonConverter<FormatExclusiveMinimumKeyword> Converter
+    => new FormatExclusiveMinimumKeyword.FormatExclusiveMinimumKeywordJsonConverter();
+
+    protected override FormatExclusiveMinimumKeyword CreateKeywordWithValue(string value)
+    {
+        return new FormatExclusiveMinimumKeyword(value);
+    }
+
+    [Theory]
+    [InlineData("2022-10-17")]
+    public void Write_ValidStructure_ShouldWriteToJson(string value)
+    {
+        Given.That.KeywordCreatedWithValue(value)
+            .When.XsdTextKeywordWrittenToStream()
+            .Then.SerializedKeywordShouldBe($@"{{""{KeywordPlaceholder}"":""{value}""}}");
+    }
+
+    [Theory]
+    [InlineData("2022-10-17")]
+    public void Read_ValidJson_FromSchema(string value)
+    {
+        var jsonSchema = @$"{{
+                ""{KeywordPlaceholder}"": ""{value}""
+            }}";
+
+        Given.That.JsonSchemaLoaded(jsonSchema)
+            .When.XsdTextKeywordReadFromSchema()
+            .Then.Keyword.Should().NotBeNull();
+
+        And.Keyword.Value.Should().Be(value);
+    }
+}
---
+++ src/studio/src/designer/DataModeling/Json/Keywords/FormatMinimumKeyword.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Json.Schema;
+
+namespace Altinn.Studio.DataModeling.Json.Keywords;
+
+/// <summary>
+/// Used to represent minimum on the date types
+/// </summary>
+public sealed class FormatMinimumKeyword: IJsonSchemaKeyword, IEquatable<FormatMinimumKeyword>
+{
+    /// <summary>
+    /// The name of the keyword
+    /// </summary>
+    internal const string Name = "formatExclusiveMinimum";
+
+    /// <summary>
+    /// The value, format of minimum
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// Create a new instance with the specified value
+    /// </summary>
+    /// <param name="value">Minimum format</param>
+    public FormatMinimumKeyword(string value)
+    {
+        Value = value;
+    }
+
+    /// <inheritdoc />
+    public void Validate(ValidationContext context)
+    {
+    }
+
+    /// <inheritdoc />
+    public bool Equals(FormatMinimumKeyword other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return ReferenceEquals(this, other) || Equals(Value, other.Value);
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as FormatMinimumKeyword);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    /// <summary>
+    /// Serializer for the FormatMinimumKeyword keyword
+    /// </summary>
+    internal class FormatMinimumKeywordJsonConverter : JsonConverter<FormatMinimumKeyword>
+    {
+        /// <summary>
+        /// Read @xsdType keyword from json schema
+        /// </summary>
+        public override FormatMinimumKeyword Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException("Expected string");
+            }
+
+            return new FormatMinimumKeyword(reader.GetString());
+        }
+
+        /// <summary>
+        /// Write formatExclusiveMaximum keyword to json
+        /// </summary>
+        public override void Write(Utf8JsonWriter writer, FormatMinimumKeyword value, JsonSerializerOptions options)
+        {
+            writer.WriteString(Name, value.Value);
+        }
+    }
+}
