270cdb46c44ffede6da091734b627d79af9e334b
Nina Kylstad <nkylstad@gmail.com>
2022-10-17 20:07:11 +0200

    remove use of mui grid

--- src/studio/src/designer/frontend/app-development/features/dataModelling/containers/DataModellingContainer.tsx
+++ src/studio/src/designer/frontend/app-development/features/dataModelling/containers/DataModellingContainer.tsx
@@ -1,43 +1,28 @@
 import React from 'react';
 import { DataModelling } from 'app-shared/features';
 import VersionControlHeader from 'app-shared/version-control/versionControlHeader';
-import { makeStyles, createStyles, Grid } from '@material-ui/core';
 import { useAppSelector } from 'common/hooks';
+import classes from './DataModellingContainer.module.css'
 
 interface IDataModellingContainerProps {
   language: any;
 }
 
-const useStyles = makeStyles(
-  createStyles({
-    root: {
-      marginLeft: 80,
-      width: 'calc(100% - 80px)',
-      display: 'flex',
-    },
-    versionControlHeaderMargin: {
-      marginBottom: 12,
-    },
-  }),
-);
-
 const DataModellingContainer = ({ language }: IDataModellingContainerProps) => {
-  const classes = useStyles();
-
   const [org, repo] = useAppSelector((state) => {
     const id = state.applicationMetadataState.applicationMetadata.id;
     return id?.split('/') || [];
   })
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={12} className={classes.versionControlHeaderMargin}>
+    <div className={classes.root}>
+      <div className={classes.row}>
         <VersionControlHeader language={language}/>
-      </Grid>
-      <Grid item xs={12}>
+      </div>
+      <div className={classes.row}>
         <DataModelling language={language} org={org} repo={repo} />
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
 };
 
