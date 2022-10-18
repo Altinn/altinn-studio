f9f8ccb0fea353d275fab5d0c1052bbeeffa40ee
Nina Kylstad <nkylstad@gmail.com>
2022-10-18 15:19:45 +0200

    mock axios & console in test

--- src/studio/src/designer/frontend/packages/schema-editor/src/features/editor/schemaEditorSaga.test.ts
+++ src/studio/src/designer/frontend/packages/schema-editor/src/features/editor/schemaEditorSaga.test.ts
@@ -2,9 +2,21 @@ import { expectSaga } from 'redux-saga-test-plan';
 import { autosaveModelSaga, autoSavePropsSelector } from './schemaEditorSaga';
 import { select } from 'redux-saga/effects'
 import { put } from 'app-shared/utils/networking';
+import axios from 'axios';
 import { buildUiSchema } from '@altinn/schema-model';
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
 describe('schemaEditorSaga', () => {
+  let spy: any;
+  beforeAll(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    spy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  })
+  afterAll(() => {
+    spy.mockRestore();
+  })
   const jsonschema = {
     properties: {
       test: {
@@ -14,7 +26,8 @@ describe('schemaEditorSaga', () => {
   };
   const uiSchema = buildUiSchema(jsonschema);
   describe('AutosaveModeSaga', () => {
-    test('autosave', () => {
+    test('successful autosave', () => {
+      mockedAxios.put.mockResolvedValueOnce("test");
       return expectSaga(autosaveModelSaga)
       .provide([
         [select(autoSavePropsSelector), { uiSchema, saveUrl: 'test' }],
@@ -22,5 +35,19 @@ describe('schemaEditorSaga', () => {
       .call(put, `test&saveOnly=true`, {...jsonschema})
       .run();
     })
-  })
+  });
+  describe('AutosaveModeSaga', () => {
+    test('failing autosave', () => {
+      console.error = jest.fn();
+      mockedAxios.put.mockRejectedValueOnce("error");
+      return expectSaga(autosaveModelSaga)
+      .provide([
+        [select(autoSavePropsSelector), { uiSchema, saveUrl: 'test' }],
+      ])
+      .call(put, `test&saveOnly=true`, {...jsonschema})
+      .run();
+
+      expect(console.error).toHaveBeenCalledWith('Failed to save JSON Schema model. ', 'error');
+    })
+  });
 })
