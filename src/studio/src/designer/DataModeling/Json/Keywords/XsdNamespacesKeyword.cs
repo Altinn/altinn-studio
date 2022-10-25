8a80258bb45a9ba76150a2934fe0f019845b0fb3
Mirko Sekulic <misha.sekulic@gmail.com>
2022-10-25 13:45:16 +0200

    refactor: validation comment on keyword

--- src/studio/src/designer/DataModeling/Json/Keywords/XsdNamespacesKeyword.cs
+++ src/studio/src/designer/DataModeling/Json/Keywords/XsdNamespacesKeyword.cs
@@ -44,6 +44,7 @@ namespace Altinn.Studio.DataModeling.Json.Keywords
         /// <param name="context">Contextual details for the validation process.</param>
         public void Validate(ValidationContext context)
         {
+            // No validation for keyword.
         }
 
         /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
