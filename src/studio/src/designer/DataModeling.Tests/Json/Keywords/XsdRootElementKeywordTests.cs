55eb78ec9d57ab5c0087cc004ecc6a1cc4da5ad4
Mirko Sekulic <misha.sekulic@gmail.com>
2022-11-09 15:45:03 +0100

    feat: root element keyword tests

---
+++ src/studio/src/designer/DataModeling.Tests/Json/Keywords/XsdRootElementKeywordTests.cs
@@ -0,0 +1,24 @@
+﻿using Altinn.Studio.DataModeling.Json.Keywords;
+using DataModeling.Tests.Json.Keywords.BaseClasses;
+using FluentAssertions;
+using Xunit;
+
+namespace DataModeling.Tests.Json.Keywords;
+
+public class XsdRootElementKeywordTests: ValueKeywordTestsBase<XsdRootElementKeywordTests, XsdRootElementKeyword, string>
+{
+    protected override XsdRootElementKeyword CreateKeywordWithValue(string value) => new(value);
+
+    [Theory]
+    [InlineData("melding")]
+    [InlineData("root")]
+    public void SameKeywords_Should_BeEqual(string value)
+    {
+        var expectedKeyword = new XsdRootElementKeyword(value);
+        object expectedKeywordObject = new XsdRootElementKeyword(value);
+        Given.That.KeywordCreatedWithValue(value)
+            .Then.KeywordShouldEqual(expectedKeyword)
+            .And.KeywordShouldEqualObject(expectedKeywordObject)
+            .But.KeywordShouldNotEqual(null);
+    }
+}
