c1180d49072a191e840df14bf1f43be332bde852
Nina Kylstad <nkylstad@gmail.com>
2022-11-20 21:09:38 +0100

    menu & administration for datamodels repo

---
+++ src/studio/src/designer/frontend/app-development/features/administration/components/DatamodelsAdministration.tsx
@@ -0,0 +1,23 @@
+import { getLanguageFromKey } from 'app-shared/utils/language';
+import React from 'react';
+
+export interface DatamodelsAdministrationProps {
+  language: any;
+}
+
+export function DatamodelsAdministration({ language }: DatamodelsAdministrationProps) {
+  return (
+    <div>
+      <p>
+        {getLanguageFromKey('administration.datamodels_info1', language)}
+      </p>
+      <p>
+        {getLanguageFromKey('administration.datamodels_info2', language)}
+      </p>
+      <p>
+        {getLanguageFromKey('administration.datamodels_info3', language)}&nbsp;
+        <a href="https://docs.altinn.studio/app/development/data/data-model/">{getLanguageFromKey('administration.datamodels_info_link', language)}</a>
+      </p>
+    </div>
+  );
+}
