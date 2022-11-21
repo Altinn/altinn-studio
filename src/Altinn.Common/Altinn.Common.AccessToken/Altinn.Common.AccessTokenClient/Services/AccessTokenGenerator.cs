a28be32b8a09f42b5fefcb37ceb6f64258c3edb8
Benjamin D. Brodie <113984758+b-brodie-digdir@users.noreply.github.com>
2022-11-21 16:22:05 +0100

    Set accesstoken ValidFrom to be 5 seconds earlier (#9292)
    * Set accesstoken ValidFrom to be 5 seconds earlier
    * Move contant to configuration setting.
    * Avoid checking in yarn.lock in root folder

--- src/Altinn.Common/Altinn.Common.AccessToken/Altinn.Common.AccessTokenClient/Services/AccessTokenGenerator.cs
+++ src/Altinn.Common/Altinn.Common.AccessToken/Altinn.Common.AccessTokenClient/Services/AccessTokenGenerator.cs
@@ -85,6 +85,7 @@ namespace Altinn.Common.AccessTokenClient.Services
                 SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
                 {
                     Subject = new ClaimsIdentity(principal.Identity),
+                    NotBefore = DateTime.UtcNow.AddSeconds(_accessTokenSettings.ValidFromAdjustmentSeconds),
                     Expires = DateTime.UtcNow.AddSeconds(_accessTokenSettings.TokenLifetimeInSeconds),
                     SigningCredentials = signingCredentials,
                     Audience = "platform.altinn.no",
