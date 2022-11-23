b4770437ed8f502de5280fc5247395e23517151c
Nina Kylstad <nkylstad@gmail.com>
2022-11-23 12:49:14 +0100

    move css into css module

--- src/studio/src/designer/frontend/shared/features/dataModelling/components/SchemaSelect.tsx
+++ src/studio/src/designer/frontend/shared/features/dataModelling/components/SchemaSelect.tsx
@@ -1,9 +1,9 @@
-import React, { CSSProperties } from 'react';
+import React  from 'react';
 import { Grid } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import Select from 'react-select';
 import classNames from 'classnames';
 import type { IMetadataOption } from '../functions/types';
+import classes from './SchemaSelect.module.css';
 
 export interface ISchemaSelectProps {
   disabled: boolean;
@@ -17,54 +17,16 @@ export interface GroupedOption {
   readonly options: readonly IMetadataOption[];
 }
 
-const groupStyles = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-};
-
-const groupBadgeStyles: CSSProperties = {
-  backgroundColor: '#0062BA',
-  borderRadius: '2em',
-  color: 'white',
-  display: 'inline-block',
-  fontWeight: 'normal',
-  lineHeight: '1',
-  minWidth: 1,
-  padding: '0.2em 0.5em',
-  textAlign: 'center',
-}
-
 const formatGroupLabel = (data: GroupedOption) => (
-  <div style={groupStyles}>
+  <div className={classes.group}>
     <span>{data.label}</span>
-    <span style={groupBadgeStyles}>{data.options.length}</span>
+    <span className={classes.groupBadge}>{data.options.length}</span>
   </div>
 );
 
-const useStyles = makeStyles({
-  root: {
-    margin: 12,
-    width: '100%',
-    maxHeight: 36,
-  },
-  select: {
-    minWidth: 400,
-    zIndex: 1101,
-  },
-  chevron: {
-    margin: 12,
-    color: '#006BD8',
-    '&.fa.fa-nedtrekk': {
-      fontSize: 16,
-      fontWeight: 600,
-    },
-  },
-});
 export const SchemaSelect = (props: ISchemaSelectProps) => {
   const { onChange, disabled, selectedOption, options } = props;
 
-  const classes = useStyles();
   const customStyles = {
     control: (base: any, state: any) => {
       const opacity = state.isDisabled ? 0.5 : 1;
@@ -79,7 +41,7 @@ export const SchemaSelect = (props: ISchemaSelectProps) => {
     },
   };
   const IndicatorSeparator = () => <></>;
-  const DropdownIndicator = () => <i className={classNames(['fa fa-nedtrekk', classes.chevron])} aria-hidden />;
+  const DropdownIndicator = () => <i className={classNames([classes['fa.fa-nedtrekk'],'fa fa-nedtrekk', classes.chevron])} aria-hidden />;
   return (
     <Grid item xs={4}>
       <Select<IMetadataOption, false, GroupedOption>
