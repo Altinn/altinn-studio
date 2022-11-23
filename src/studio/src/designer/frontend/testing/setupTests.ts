daa910c5d396a08efaff392c5c225fef1c235de1
Nina Kylstad <nkylstad@gmail.com>
2022-11-23 13:05:08 +0100

    use route params instead of applicationmetadata

--- src/studio/src/designer/frontend/testing/setupTests.ts
+++ src/studio/src/designer/frontend/testing/setupTests.ts
@@ -7,3 +7,17 @@ import failOnConsole from 'jest-fail-on-console';
 failOnConsole( {
     shouldFailOnWarn: true,
 });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
