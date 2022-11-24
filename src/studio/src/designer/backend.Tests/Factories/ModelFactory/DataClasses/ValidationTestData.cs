964cb1b60d6be680ade6d556afc4296766d513c6
Mirko Sekulic <misha.sekulic@gmail.com>
2022-11-24 11:56:52 +0100

    feat: validate data against schemas (#9284)
    * feat: init datavalidation tests
    
    * feat: validating produced xml against xsd
    
    * feat: init random object generator
    
    * feat: init randomobject version
    
    * feat: init class
    
    * feat: Data validator
    
    * feat: random as private in tests
    
    * refactor: typo fix
    
    * refactor: pass random to Xeger
    
    * fix: failing tests

---
+++ src/studio/src/designer/backend.Tests/Factories/ModelFactory/DataClasses/ValidationTestData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Designer.Tests.Factories.ModelFactory.DataClasses;
+
+public class ValidationTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "Model/Xsd/Gitea/nsm-klareringsportalen.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/stami-mu-bestilling-2021.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/dat-aarligmelding-bemanning.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/dihe-redusert-foreldrebetaling-bhg.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/nbib-melding.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/udir-vfkl.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/Kursdomene_BliTjenesteeier_M_2020-05-25_5703_34553_SERES.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/Kursdomene_BekrefteBruksvilkår_M_2020-05-25_5704_34554_SERES.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/stami-atid-databehandler-2022.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/stami-mu-databehandler-2021.xsd" };
+        yield return new object[] { "Model/Xsd/Gitea/skd-formueinntekt-skattemelding-v2.xsd" };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
