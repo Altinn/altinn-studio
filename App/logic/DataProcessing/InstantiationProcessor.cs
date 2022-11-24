6a4ddcdcc7aed8186925c2bce0da7d7b9bb733d1
Caroline Stensland Selte <cs@itverket.no>
2022-11-24 11:26:10 +0100

    Upgrade to v7

--- App/logic/DataProcessing/InstantiationProcessor.cs
+++ App/logic/DataProcessing/InstantiationProcessor.cs
@@ -1,56 +1,23 @@
+﻿using Altinn.App.Core.Features;
+using Altinn.App.Core.Interface;
 using Altinn.App.Models;
-using Altinn.App.Services.Interface;
-using Altinn.App.Services.Models.Validation;
-using Altinn.Platform.Storage.Interface.Models;
-using System.Threading.Tasks;
 using Altinn.Platform.Register.Models;
+using Altinn.Platform.Storage.Interface.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Altinn.App.AppLogic
+namespace Altinn.App.logic.DataProcessing
 {
-    public class InstantiationHandler
+    public class InstantiationProcessor : IInstantiationProcessor
     {
-        private IProfile _profileService;
         private IRegister _registerService;
 
-        /// <summary>
-        /// Set up access to profile and register services
-        /// </summary>
-        /// <param name="profileService"></param>
-        /// <param name="registerService"></param>
-        public InstantiationHandler(IProfile profileService, IRegister registerService)
+        public InstantiationProcessor(IRegister registerService)
         {
-            _profileService = profileService;
             _registerService = registerService;
+
         }
 
-        /// <summary>
-        /// Run validations related to instantiation
-        /// </summary>
-        /// <example>
-        /// if ([some condition])
-        /// {
-        ///     return new ValidationResult("[error message]");
-        /// }
-        /// return null;
-        /// </example>
-        /// <param name="instance"></param>
-        /// <param name="validationResults"></param>
-        /// <returns>The validation result object (null if no errors) </returns>
-        public async Task<InstantiationValidationResult> RunInstantiationValidation(Instance instance)
-        {
-            return await Task.FromResult((InstantiationValidationResult)null);
-        }
-
-        /// <summary>
-        /// Run events related to instantiation
-        /// </summary>
-        /// <remarks>
-        /// For example custom prefill.
-        /// </remarks>
-        /// <param name="instance">Instance information</param>
-        /// <param name="data">The data object created</param>
-        /// <param name="prefill">External prefill available under instansiation if supplied</param>
         public async Task DataCreation(Instance instance, object data, Dictionary<string, string> prefill)
         {
             if (data.GetType() == typeof(Skjema))
--- App/services/options/ReferenceOptions.cs
+++ App/services/options/ReferenceOptions.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Altinn.App.Common.Models;
-using Altinn.App.PlatformServices.Options;
+using Altinn.App.Core.Features;
+using Altinn.App.Core.Models;
 
 namespace Altinn.App.services.options
 {
