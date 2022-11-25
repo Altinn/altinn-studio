1d7bbe3968c4aeb1070fcc5fc0314000d97a74f9
Michael <michael.johansen@digdir.no>
2022-11-25 14:08:29 +0100

    9318 overview of mui components to replace (#9319)

---
+++ src/studio/src/designer/frontend/stats.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const ts = require('ts-morph');
+const path = require('path');
+const glob = require('glob');
+
+const getFileImports = (file) => {
+  const project = new ts.Project();
+  const sourceFile = project.createSourceFile(
+    'dummy.ts',
+    fs.readFileSync(path.join(__dirname, file), 'utf8'),
+  );
+  const output = [];
+  sourceFile.getImportDeclarations().forEach((importDecl) => {
+    const module = importDecl.getModuleSpecifier().getLiteralText();
+
+    importDecl.getNamedImports().forEach((imp) => {
+      output.push({
+        file,
+        module,
+        named: imp.getName(),
+      });
+    });
+  });
+  return output;
+};
+
+//const data = getFileImports('dashboard/index.tsx');
+//console.log(data);
+const stream = fs.createWriteStream('stats.csv');
+glob(__dirname + '/**/*.ts*', {}, (err, files) => {
+  stream.write(`${["file","module","name"].join(';')}\r\n`);
+  files.forEach((file) => {
+    if (!file.includes('node_modules')) {
+      const short = file.substring(__dirname.length);
+      getFileImports(short).forEach((stat) => {
+        stream.write(`${Object.values(stat).join(';')}\r\n`);
+      });
+    }
+  });
+  stream.end();
+  console.log('Done collecting stats');
+});
