0260d5c6549f57b7d3f0a78be42005d3f1c098cc
Mirko Sekulic <misha.sekulic@gmail.com>
2022-12-05 15:55:21 +0100

    fix: restriction tests

--- src/studio/src/designer/backend.Tests/Factories/ModelFactory/DataClasses/CSharpE2ERestrictionsTestData.cs
+++ src/studio/src/designer/backend.Tests/Factories/ModelFactory/DataClasses/CSharpE2ERestrictionsTestData.cs
@@ -14,10 +14,10 @@ public class CSharpE2ERestrictionsTestData: IEnumerable<object[]>
         yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "t2", "string", "[MinLength(10)]" };
         yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "t2", "string", "[MaxLength(10)]" };
         yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "t4", "string", @"[RegularExpression(@""^\d\.\d\.\d$"")]" };
-        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "n1", "decimal", @"[RegularExpression(@""^(([0-9]){1}(\.)?){0,10}$"")]" };
+        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "n1", "decimal", @"[RegularExpression(@""^-?(([0-9]){1}(\.)?){0,10}$"")]" };
         yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "n1", "decimal", @"[Range(-100, 100)]" };
-        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "i1", "int", @"[RegularExpression(@""^[0-9]{0,10}$"")]" };
-        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "i2", "decimal", @"[RegularExpression(@""^[0-9]{0,10}$"")]" };
+        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "i1", "int", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
+        yield return new object[] { "Model/Xsd/SimpleTypeRestrictions.xsd", "i2", "decimal", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
