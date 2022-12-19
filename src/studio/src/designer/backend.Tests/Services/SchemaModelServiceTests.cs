d9a30ec8ed3a1f68701589ec9c4c533b6b0a1576
mjulstein <6736558+mjulstein@users.noreply.github.com>
2022-12-19 12:33:24 +0100

    change from altinn3.no to altinn.localhost as the local development url to reduce steps needed for local development

--- src/studio/src/designer/backend.Tests/Services/SchemaModelServiceTests.cs
+++ src/studio/src/designer/backend.Tests/Services/SchemaModelServiceTests.cs
@@ -255,8 +255,8 @@ namespace Designer.Tests.Services
         }
 
         [Theory]
-        [InlineData("ttd", "apprepo", "test", "", "http://altinn3.no/repos")]
-        [InlineData("ttd", "apprepo", "test", "/path/to/folder/", "http://altinn3.no/repos")]
+        [InlineData("ttd", "apprepo", "test", "", "http://local.altinn.studio/repos")]
+        [InlineData("ttd", "apprepo", "test", "/path/to/folder/", "http://local.altinn.studio/repos")]
         public void GetSchemaUri_ValidNameProvided_ShouldReturnUri(string org, string repository, string schemaName, string relativePath, string repositoryBaseUrl)
         {
             var altinnGitRepositoryFactory = new AltinnGitRepositoryFactory(TestDataHelper.GetTestDataRepositoriesRootDirectory());
