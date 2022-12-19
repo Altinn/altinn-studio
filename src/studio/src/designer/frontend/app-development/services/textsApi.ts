cb79febed34ca4c371ad748530e8f10de12a52bb
Michael Johansen <michael.johansen@gmail.com>
2022-12-19 14:29:46 +0100

    chore/demo devops team

--- src/studio/src/designer/frontend/app-development/services/textsApi.ts
+++ src/studio/src/designer/frontend/app-development/services/textsApi.ts
@@ -17,6 +17,9 @@ type OrgAppLangData = OrgAppLang & {
 const languageFileUrl = ({ org, app, langCode }) =>
   `/designer/${org}/${app}/Text/SaveResource/${langCode}`;
 
+const deleteLangPath = ({ org, app, langCode }) =>
+  `/designer/${org}/${app}/Text/DeleteLanguage/${langCode}`;
+
 export const textsApi = appDevelopmentApi.injectEndpoints({
   endpoints: (builder) => ({
     getAppTextsByLangCode: builder.query<TextResourceFile, OrgAppLang>({
@@ -56,7 +59,7 @@ export const textsApi = appDevelopmentApi.injectEndpoints({
     }),
     deleteByLangCode: builder.mutation<void, OrgAppLang>({
       query: (params) => ({
-        url: languageFileUrl(params),
+        url: deleteLangPath(params),
         method: 'DELETE',
       }),
       async onQueryStarted({ org, app, langCode: deletedLangCode }, { dispatch, queryFulfilled }) {
