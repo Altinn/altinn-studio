049648294ad9465cd3a55e20e09b3ce9650e80e4
Martin Joakim Ulstein <mjulstein@users.noreply.github.com>
2022-12-22 10:56:10 +0100

    help tslint understand module.css

--- src/studio/src/designer/frontend/declaration.d.ts
+++ src/studio/src/designer/frontend/declaration.d.ts
@@ -4,3 +4,11 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+
+interface Mapping {
+  [key: string]: string;
+}
+declare module '*.module.css' {
+  const mapping: Mapping;
+  export default mapping;
+}
