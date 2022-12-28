148fd85c97e0c8d988de521d09fe32a36f7575d8
Michael <michael.johansen@digdir.no>
2022-12-28 12:07:13 +0100

    9421 text editor search and filter texts (#9463)

--- src/studio/src/designer/frontend/packages/shared/src/pure/utils.ts
+++ src/studio/src/designer/frontend/packages/shared/src/pure/utils.ts
@@ -1,5 +1,7 @@
 export const isNumeric = (str: string) => parseInt(str).toString() === str;
 export const deepCopy = (value: any) => JSON.parse(JSON.stringify(value));
+export const updateKey = (obj: any, key: string, val: string) => ({ ...deepCopy(obj), [key]: val });
+
 export const removeKey = (obj: any, key: string) => {
   const copy = deepCopy(obj);
   delete copy[key];
