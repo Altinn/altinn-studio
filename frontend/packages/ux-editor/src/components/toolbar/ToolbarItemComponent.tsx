da0216bd08b300b300857f7a9357c28256d932e3
Michael <michael.johansen@digdir.no>
2022-12-30 11:07:51 +0100

    Chore/fixing toolbaritems (#9479)

--- frontend/packages/ux-editor/src/components/toolbar/ToolbarItemComponent.tsx
+++ frontend/packages/ux-editor/src/components/toolbar/ToolbarItemComponent.tsx
@@ -1,124 +1,38 @@
 import React from 'react';
-import { ListItem, ListItemIcon, ListItemText, Paper } from '@mui/material';
-import { createStyles, withStyles } from '@mui/styles';
 import classNames from 'classnames';
-import { connect } from 'react-redux';
-import { getComponentTitleByComponentType } from '../../utils/language';
+import classes from './ToolbarItemComponent.module.css';
 import type { IAppState } from '../../types/global';
+import { Button, ButtonVariant } from '@altinn/altinn-design-system';
+import { Helptext } from '@navikt/ds-icons';
+import { getComponentTitleByComponentType } from '../../utils/language';
+import { useSelector } from 'react-redux';
 
 export interface IToolbarItemProvidedProps {
-  classes: any;
   componentType: string;
   onClick: any;
   thirdPartyLabel?: string;
   icon: string;
 }
 
-export interface IToolbarItemProps extends IToolbarItemProvidedProps {
-  language: any;
-}
-
-// eslint-disable-next-line react/prefer-stateless-function
-class ToolbarItem extends React.Component<IToolbarItemProps> {
-  public render(): JSX.Element {
-    return (
-      <Paper square={true} classes={{ root: classNames(this.props.classes.paper) }}>
-        <ListItem classes={{ root: classNames(this.props.classes.listItem) }} component='div'>
-          <ListItemIcon classes={{ root: classNames(this.props.classes.listComponentIcon) }}>
-            <i className={this.props.icon} />
-          </ListItemIcon>
-          <ListItemText
-            classes={{
-              primary: classNames(this.props.classes.listItemText),
-              root: classNames(this.props.classes.listItemTextRoot),
-            }}
-          >
-            {this.props.thirdPartyLabel == null
-              ? getComponentTitleByComponentType(this.props.componentType, this.props.language)
-              : this.props.thirdPartyLabel}
-          </ListItemText>
-          <ListItemIcon
-            classes={{ root: classNames(this.props.classes.listItemIcon) }}
-            // eslint-disable-next-line react/jsx-no-bind
-            onClick={this.props.onClick.bind(this, this.props.componentType)}
-          >
-            <i className={`fa fa-help-circle ${this.props.classes.helpIcon}`} />
-          </ListItemIcon>
-        </ListItem>
-      </Paper>
-    );
-  }
-}
-
-const styles = () =>
-  createStyles({
-    searchBox: {
-      border: '1px solid #0062BA',
-      marginTop: '10px',
-      marginBottom: '24px',
-      background: 'none',
-    },
-    searchBoxInput: {
-      fontSize: '14px',
-      color: '#6A6A6A',
-      padding: '6px',
-    },
-    searchBoxIcon: {
-      color: '#000000',
-    },
-    listItemText: {
-      fontSize: '14px',
-      whiteSpace: 'nowrap',
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-    },
-    listItemTextRoot: {
-      paddingRight: '0px',
-    },
-    listItem: {
-      cursor: 'grab',
-      paddingLeft: '12px',
-      paddingRight: '8px',
-      paddingTop: '9px',
-      paddingBottom: '8px',
-    },
-    paper: {
-      marginBottom: '6px',
-      backgroundColor: '#FFFFFF',
-    },
-    helpOutline: {
-      width: '24px',
-      height: '24px',
-    },
-    listItemIcon: {
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-    listComponentIcon: {
-      minWidth: '40px',
-    },
-    helpIcon: {
-      color: '#0062BA',
-      cursor: 'pointer',
-      fontSize: '2em !important',
-      '&:hover': {
-        color: '#000000',
-      },
-    },
-  });
-
-const mapStateToProps: (state: IAppState, props: IToolbarItemProvidedProps) => IToolbarItemProps = (
-  state: IAppState,
-  props: IToolbarItemProvidedProps
-) => ({
-  language: state.appData.languageState.language,
-  componentType: props.componentType,
-  onClick: props.onClick,
-  classes: props.classes,
-  thirdPartyLabel: props.thirdPartyLabel,
-  icon: props.icon,
-});
-
-export const ToolbarItemComponent = withStyles(styles, { withTheme: true })(
-  connect(mapStateToProps)(ToolbarItem)
-);
+export const ToolbarItemComponent = (props: IToolbarItemProvidedProps) => {
+  const language = useSelector((state: IAppState) => state.appData.languageState.language);
+  return (
+    <div className={classes.toolbarItem}>
+      <div className={classes.componentIcon}>
+        <i className={classNames(classes.listComponentIcon, props.icon)} />
+      </div>
+      <div className={classes.componentLabel}>
+        {props.thirdPartyLabel == null
+          ? getComponentTitleByComponentType(props.componentType, language)
+          : props.thirdPartyLabel}
+      </div>
+      <div className={classes.componentHelpIcon}>
+        <Button
+          onClick={(e: any) => props.onClick(props.componentType, e)}
+          icon={<Helptext />}
+          variant={ButtonVariant.Quiet}
+        />
+      </div>
+    </div>
+  );
+};
