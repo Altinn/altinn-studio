faa9bbbb416e4e73a62919dca40b03a5c2c16093
LAPTOP-S0QU96IH\david <david@framit.no>
2023-01-10 09:09:32 +0100

    updated useValidation to return error from validate method

--- frontend/packages/ux-editor/src/hooks/useValidation/useValidation.ts
+++ frontend/packages/ux-editor/src/hooks/useValidation/useValidation.ts
@@ -3,15 +3,16 @@ import { Validation, validate as doValidate } from '../../utils/validationUtils'
 
 interface UseValidationResult {
   validationError: string | undefined;
-  validate: (valueToValidate: string) => void;
+  validate: (valueToValidate: string) => UseValidationResult['validationError'];
 }
 export const useValidation = (validation: Validation): UseValidationResult => {
   const [validationError, setValidationState] =
     useState<UseValidationResult['validationError']>(undefined);
 
-  const validate = (valueToValidate: string): void => {
+  const validate = (valueToValidate: string): UseValidationResult['validationError'] => {
     const { error } = doValidate(validation, valueToValidate);
     setValidationState(error);
+    return error;
   };
 
   return {
