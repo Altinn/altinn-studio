db6f5f8ca2d6ae0c0c82942b06f5d3243e838c79
Michael Johansen <michael.johansen@gmail.com>
2023-01-11 16:07:41 +0100

    9530 altinn studio deploy font sizes and layout adjustments

--- frontend/testing/mockend/src/routes/create-release.js
+++ frontend/testing/mockend/src/routes/create-release.js
@@ -2,7 +2,5 @@ module.exports = (req, res) => {
   const { tagName, name, body, targetCommitish } = req.body;
 
   res.status(201);
-  res.json(content);
+  res.json({ tagName, name, body, targetCommitish });
 };
-
-// {"tagName":"8999","name":"8999","body":"Dette er en thing","targetCommitish":"fff8d87281088bcdddfd8affa5e78e5b0d0b2503"}
