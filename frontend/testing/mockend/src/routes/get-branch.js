0b5cb1b530777ab63944ce85082b935ebb4f1f5f
Michael Johansen <michael.johansen@gmail.com>
2023-01-11 13:49:51 +0100

    9530 altinn studio deploy font sizes and layout adjustments

--- frontend/testing/mockend/src/routes/get-branch.js
+++ frontend/testing/mockend/src/routes/get-branch.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
     commit: {
       author: { email: '@jugglingnutcase', name: 'Nick Fury', username: 'nickf' },
       committer: { email: '@jugglingnutcase', name: 'Nick Fury', username: 'nickf' },
-      id: 'fff8d87281088bcdddfd8affa5e78e5b0d0b2503',
+      id: '940f119905cada186000ab31a8e3be88d73375ce',
       message: 'This would be a commit message',
       timestamp: '2023-01-11T08:44:59Z',
       url: `https://localhost:2004/repos/${org}/${app}/commit/fff8d87281088bcdddfd8affa5e78e5b0d0b2503`,
