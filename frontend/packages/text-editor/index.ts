21ad8c560e23c9d5dbb7478d9a2f6775631c690e
mjulstein <6736558+mjulstein@users.noreply.github.com>
2023-01-11 14:47:12 +0100

    Fix: get default language from text-editor package when initializing.

--- frontend/packages/text-editor/index.ts
+++ frontend/packages/text-editor/index.ts
@@ -1,2 +1,3 @@
 export { TextEditor } from './src/TextEditor';
+export { defaultLangCode } from './src/constants';
 export type { TextResourceFile, Language, LangCode, TextResourceEntry } from './src/types';
