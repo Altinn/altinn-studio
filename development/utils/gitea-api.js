8b3ee0251a95de7a10aea110f2e2c7df8fbf3394
Michael Johansen <michael.johansen@gmail.com>
2023-01-12 19:31:11 +0100

    feat/setup script now creates deploy teams

--- development/utils/gitea-api.js
+++ development/utils/gitea-api.js
@@ -1,4 +1,4 @@
-const { request } = require("http");
+const { request } = require('http');
 
 /**
  * @see http://studio.localhost/repos/api/swagger
@@ -10,19 +10,19 @@ module.exports = (options) =>
   new Promise(function (resolve, reject) {
     const req = request(
       {
-        host: "studio.localhost",
+        host: 'studio.localhost',
         path: options.path,
-        auth: [options.user, options.pass].join(":"),
+        auth: [options.user, options.pass].join(':'),
         method: options.method,
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       },
       (response) => {
         const data = [];
-        response.on("data", (chunk) => data.push(chunk));
-        response.on("end", () => {
-          console.log(options.method, options.path, response.statusCode);
+        response.on('data', (chunk) => data.push(chunk));
+        response.on('end', () => {
+          console.log(options.method, options.path, response.statusCode, response.statusMessage);
           if (data.length) {
             resolve(JSON.parse(data.join()));
           } else {
