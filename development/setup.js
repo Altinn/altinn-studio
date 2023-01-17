7708d1b5f49672201ca6079f394170f92017fbbc
Michael <michael.johansen@digdir.no>
2023-01-17 16:02:44 +0100

    bug/added dns check for setup script (#9590)
    Co-authored-by: Martin Joakim Ulstein <6736558+mjulstein@users.noreply.github.com>

--- development/setup.js
+++ development/setup.js
@@ -2,6 +2,7 @@ const giteaApi = require('./utils/gitea-api.js');
 const waitFor = require('./utils/wait-for.js');
 const runCommand = require('./utils/run-command.js');
 const ensureDotEnv = require('./utils/ensure-dot-env.js');
+const dnsIsOk = require('./utils/check-if-dns-is-correct.js');
 const path = require('path');
 const fs = require('fs');
 
@@ -196,16 +197,23 @@ const addReleaseAndDeployTestDataToDb = async () => {
 const script = async () => {
   const currentEnv = ensureDotEnv();
   await startingDockerCompose();
-  await waitFor('http://studio.localhost/repos/');
-  await createAdminUser(currentEnv);
-  await ensureAdminPassword(currentEnv);
-  await createTestDepOrg(currentEnv);
-  await createTestDepTeams(currentEnv);
-  await addUserToSomeTestDepTeams(currentEnv);
-  await createCypressEnvFile(currentEnv);
-  await addReleaseAndDeployTestDataToDb();
-  process.exit(0);
+  const dnsOk = await dnsIsOk('studio.localhost');
+  if (dnsOk) {
+    await waitFor('http://studio.localhost/repos/');
+    await createAdminUser(currentEnv);
+    await ensureAdminPassword(currentEnv);
+    await createTestDepOrg(currentEnv);
+    await createTestDepTeams(currentEnv);
+    await addUserToSomeTestDepTeams(currentEnv);
+    await createCypressEnvFile(currentEnv);
+    await addReleaseAndDeployTestDataToDb();
+    process.exit(0);
+  } else {
+    console.error(
+      "DNS entry for studio.localhost does not resolve to 127.0.0.1. Check that it is set in  /etc/hosts"
+    );
+    process.exit(1);
+  }
 };
 
 script().then();
-//addUserToSomeTestDepTeams(ensureDotEnv()).then(console.log('done'));
