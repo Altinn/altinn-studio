fc9d7d72d9f9ca27bfa5fce8145c23eab2f3d655
LAPTOP-S0QU96IH\david <david@framit.no>
2023-01-18 16:23:52 +0100

    fixed noOptionMessage is not a function and move useText to where it is used

--- frontend/packages/ux-editor/src/components/config/editModal/EditDataModelBindings.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditDataModelBindings.tsx
@@ -4,7 +4,6 @@ import type { IAppState } from '../../../types/global';
 import type { IGenericEditComponent } from '../componentConfig';
 import { getMinOccursFromDataModel, getXsdDataTypeFromDataModel } from '../../../utils/datamodel';
 import { ComponentTypes } from '../../index';
-import { useText } from '../../../hooks';
 import { textSelector } from '../../../selectors/textSelectors';
 
 export interface EditDataModelBindingsProps extends IGenericEditComponent {
@@ -19,24 +18,23 @@ export interface EditDataModelBindingsProps extends IGenericEditComponent {
 export const EditDataModelBindings = ({
   component,
   handleComponentChange,
-  renderOptions,
+  renderOptions
 }: EditDataModelBindingsProps) => {
   const language = useSelector(textSelector);
   const dataModel = useSelector((state: IAppState) => state.appData.dataModel.model);
-  const t = useText();
 
   const handleDataModelChange = (selectedDataModelElement: string, key = 'simpleBinding') => {
     handleComponentChange({
       ...component,
       dataModelBindings: {
         ...component.dataModelBindings,
-        [key]: selectedDataModelElement,
+        [key]: selectedDataModelElement
       },
       required: getMinOccursFromDataModel(selectedDataModelElement, dataModel) > 0,
       timeStamp:
         component.type === ComponentTypes.Datepicker
           ? getXsdDataTypeFromDataModel(selectedDataModelElement, dataModel) === 'DateTime'
-          : undefined,
+          : undefined
     });
   };
 
@@ -44,7 +42,6 @@ export const EditDataModelBindings = ({
     dataModelBinding: component.dataModelBindings,
     onDataModelChange: handleDataModelChange,
     language,
-    t,
-    ...renderOptions,
+    ...renderOptions
   });
 };
