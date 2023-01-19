b23731742cc661e0c53b88800f36ed6496a687fc
David Øvrelid <46874830+framitdavid@users.noreply.github.com>
2023-01-19 10:42:42 +0100

    Increase DX by typesafe useText-hook to ensure developers use only available translation keys. (#9607)
    * Typesafe t('key')

---
+++ frontend/packages/shared/src/types/language.ts
@@ -0,0 +1,1 @@
+export type { TranslationKey } from '../../../../language/type';
--- frontend/packages/ux-editor/src/hooks/useText.ts
+++ frontend/packages/ux-editor/src/hooks/useText.ts
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux';
 import { textSelector } from '../selectors/textSelectors';
 import { getLanguageFromKey } from 'app-shared/utils/language';
+import type { TranslationKey } from 'app-shared/types/language';
 
-export type UseText = (key: string) => string;
+export type UseText = (key: TranslationKey) => string;
+
 export const useText = (): UseText => {
   const texts = useSelector(textSelector);
-  return (key: string) => getLanguageFromKey(key, texts);
+  return (key: TranslationKey) => getLanguageFromKey(key, texts);
 };
