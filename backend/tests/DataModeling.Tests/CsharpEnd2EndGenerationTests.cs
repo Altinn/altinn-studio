bca3b30be4e3803f1016108e86c6d1b92d5c6817
Mirko Sekulic <misha.sekulic@gmail.com>
2023-01-19 15:19:48 +0100

    move restriction tests

--- backend/tests/DataModeling.Tests/CsharpEnd2EndGenerationTests.cs
+++ backend/tests/DataModeling.Tests/CsharpEnd2EndGenerationTests.cs
@@ -2,6 +2,7 @@
 using System.Xml.Serialization;
 using DataModeling.Tests.Assertions;
 using DataModeling.Tests.BaseClasses;
+using DataModeling.Tests.TestDataClasses;
 using Designer.Tests.Factories.ModelFactory.DataClasses;
 using FluentAssertions;
 using SharedResources.Tests;
@@ -26,6 +27,21 @@ namespace DataModeling.Tests
             And.GeneratedClassesShouldBeEquivalentToExpected(expectedCsharpClassPath);
         }
 
+        // enum, max/min exclusive, fractions are ignored in c# class.
+        [Theory]
+        [ClassData(typeof(CSharpE2ERestrictionsTestData))]
+        public void Convert_CSharpClass_ShouldContainRestriction(string xsdSchemaPath, string propertyName, string expectedPropertyType, string restrictionString)
+        {
+            Given.That.XsdSchemaLoaded(xsdSchemaPath)
+                .When.LoadedXsdSchemaConvertedToJsonSchema()
+                .And.ConvertedJsonSchemaConvertedToModelMetadata()
+                .And.ModelMetadataConvertedToCsharpClass()
+                .And.CSharpClassesCompiledToAssembly()
+                .Then.CompiledAssembly.Should().NotBeNull();
+
+            And.PropertyShouldHaveDefinedTypeAndContainAnnotation("Root", propertyName, expectedPropertyType, restrictionString);
+        }
+
         private void GeneratedClassesShouldBeEquivalentToExpected(string expectedCsharpClassPath)
         {
             string expectedClasses = SharedResourcesHelper.LoadTestDataAsString(expectedCsharpClassPath);
@@ -36,6 +52,12 @@ namespace DataModeling.Tests
             var oldType = expectedAssembly.GetType(newType.FullName);
             oldType.Should().NotBeNull();
             TypeAssertions.IsEquivalentTo(oldType, newType);
+        }
+
+        private void PropertyShouldHaveDefinedTypeAndContainAnnotation(string className, string propertyName, string propertyType, string annotationString)
+        {
+            var type = CompiledAssembly.Types().Single(type => type.Name == className);
+            TypeAssertions.PropertyShouldContainCustomAnnotationAndHaveTypeType(type, propertyName, propertyType, annotationString);
         }
     }
 }
