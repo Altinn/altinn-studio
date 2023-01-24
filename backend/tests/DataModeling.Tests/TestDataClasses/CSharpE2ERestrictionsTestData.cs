b6db58ca77e3292d835d6d36df26eba191edec88
Mirko Sekulic <misha.sekulic@gmail.com>
2023-01-24 10:15:47 +0100

    restriction tests fixed

--- backend/tests/DataModeling.Tests/TestDataClasses/CSharpE2ERestrictionsTestData.cs
+++ backend/tests/DataModeling.Tests/TestDataClasses/CSharpE2ERestrictionsTestData.cs
@@ -9,21 +9,21 @@ public class CSharpE2ERestrictionsTestData : IEnumerable<object[]>
 {
     public IEnumerator<object[]> GetEnumerator()
     {
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "t1", "string", "[MinLength(5)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "t1", "string", "[MaxLength(20)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "t2", "string", "[MinLength(10)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "t2", "string", "[MaxLength(10)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "t4", "string", @"[RegularExpression(@""^\d\.\d\.\d$"")]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "n1", "decimal?", @"[RegularExpression(@""^-?(([0-9]){1}(\.)?){0,10}$"")]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "n1", "decimal?", @"[Range(-100, 100)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "n1", "decimal?", "[Required]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "i1", "int?", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "i1", "int?", "[Required]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "i2", "decimal?", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "i2", "decimal?", "[Required]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "nonPrimitive", "string", @"[RegularExpression(@""[0-9]+"")]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "nonPrimitive", "string", "[MinLength(5)]" };
-        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictions.xsd", "nonPrimitive", "string", "[MaxLength(20)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "t1", "string", "[MinLength(5)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "t1", "string", "[MaxLength(20)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "t2", "string", "[MinLength(10)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "t2", "string", "[MaxLength(10)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "t4", "string", @"[RegularExpression(@""^\d\.\d\.\d$"")]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "n1", "decimal?", @"[RegularExpression(@""^-?(([0-9]){1}(\.)?){0,10}$"")]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "n1", "decimal?", @"[Range(-100, 100)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "n1", "decimal?", "[Required]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "i1", "int?", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "i1", "int?", "[Required]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "i2", "decimal?", @"[RegularExpression(@""^-?[0-9]{0,10}$"")]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "i2", "decimal?", "[Required]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "nonPrimitive", "string", @"[RegularExpression(@""[0-9]+"")]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "nonPrimitive", "string", "[MinLength(5)]" };
+        yield return new object[] { "Model/XmlSchema/General/SimpleTypeRestrictionsExtended.xsd", "nonPrimitive", "string", "[MaxLength(20)]" };
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
