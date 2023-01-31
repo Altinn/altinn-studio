e17b2603999cbff83759129544f35b925d452deb
Michael <michael.johansen@digdir.no>
2023-01-31 11:31:15 +0100

    9583 as an app developer i need to be able to add an auto complete attribute to our inputs so our app is accessible (#9680)

---
+++ frontend/packages/ux-editor/src/components/config/editModal/EditAutoComplete.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import type { IGenericEditComponent } from '../componentConfig';
+import { TextField, Popover, Button } from '@digdir/design-system-react';
+
+const getLastWord = (value: string) => value.split(' ').pop();
+const stdAutocompleteOpts = [
+  'on',
+  'off',
+  'name',
+  'honorific-prefix',
+  'given-name',
+  'additional-name',
+  'family-name',
+  'honorific-suffix',
+  'nickname',
+  'email',
+  'username',
+  'new-password',
+  'current-password',
+  'one-time-code',
+  'organization-title',
+  'organization',
+  'street-address',
+  'address-line1',
+  'address-line2',
+  'address-line3',
+  'address-level4',
+  'address-level3',
+  'address-level2',
+  'address-level1',
+  'country',
+  'country-name',
+  'postal-code',
+  'cc-name',
+  'cc-given-name',
+  'cc-additional-name',
+  'cc-family-name',
+  'cc-number',
+  'cc-exp',
+  'cc-exp-month',
+  'cc-exp-year',
+  'cc-csc',
+  'cc-type',
+  'transaction-currency',
+  'transaction-amount',
+  'language',
+  'bday',
+  'bday-day',
+  'bday-month',
+  'bday-year',
+  'sex',
+  'tel',
+  'tel-country-code',
+  'tel-national',
+  'tel-area-code',
+  'tel-local',
+  'tel-extension',
+  'url',
+  'photo',
+];
+
+export const getAutocompleteOptions = (phrase: string) => {
+  const lastWord = getLastWord(phrase);
+  return stdAutocompleteOpts.includes(lastWord) || lastWord === ''
+    ? []
+    : stdAutocompleteOpts.filter((alternative) => alternative.includes(lastWord)).slice(0, 6);
+};
+
+export const EditAutoComplete = ({ component, handleComponentChange }: IGenericEditComponent) => {
+  const [value, setValue] = useState<string>(component?.autocomplete || '');
+
+  const handleWordClick = (word: string) => {
+    const parts = value.split(' ');
+    parts[parts.length - 1] = word;
+    const newValue = parts.join(' ');
+    setValue(newValue);
+    handleComponentChange({
+      ...component,
+      autocomplete: newValue,
+    });
+  };
+
+  const persistChange = () =>
+    handleComponentChange({
+      ...component,
+      autocomplete: value,
+    });
+
+  const handleChange = (event: any) => setValue(event.target.value);
+  const options = getAutocompleteOptions(value);
+  return (
+    <div>
+      <TextField
+        id={`component-id-input${component.id}`}
+        label='Autocomplete (WCAG)'
+        onBlur={persistChange}
+        onChange={handleChange}
+        value={value}
+      />
+      <Popover
+        variant='default'
+        open={options.length > 0}
+        placement='bottom-start'
+        arrow={false}
+        trigger={<div />}
+      >
+        {options.map((word) => (
+          <Button
+            size='small'
+            color='inverted'
+            variant='quiet'
+            key={word}
+            style={{ float: 'left' }}
+            onClick={() => handleWordClick(word)}
+          >
+            {word}
+          </Button>
+        ))}
+      </Popover>
+    </div>
+  );
+};
