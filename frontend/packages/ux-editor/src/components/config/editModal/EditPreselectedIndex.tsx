6942aca355865040fa6858ec2ea19d19b4bf48ac
andreastanderen <71079896+andrealouisestanderen@users.noreply.github.com>
2023-01-31 15:57:59 +0100

    Update frontend/packages/ux-editor/src/components/config/editModal/EditPreselectedIndex.tsx
    Co-authored-by: Tomas Engebretsen <tomas.engebretsen@digdir.no>

--- frontend/packages/ux-editor/src/components/config/editModal/EditPreselectedIndex.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditPreselectedIndex.tsx
@@ -3,6 +3,7 @@ import type { IFormGenericOptionsComponent } from '../../../types/global';
 import { TextField } from '@digdir/design-system-react';
 import type { IGenericEditComponent } from '../componentConfig';
 import { useText } from '../../../hooks';
+import { ComponentTypes } from '../../';
 
 export function EditPreselectedIndex({ component, handleComponentChange }: IGenericEditComponent) {
   const t = useText();
@@ -14,16 +15,16 @@ export function EditPreselectedIndex({ component, handleComponentChange }: IGene
     });
   };
 
-  function textSwitch(param) {
-    switch (param) {
+  const mapComponentTypeToText = (componentType: ComponentTypes) => {
+    switch (componentType) {
       case ComponentTypes.Checkboxes:
         return t('ux_editor.modal_check_box_set_preselected');
-      case ComponentTypes.RadioButtons:
-        return t('ux_editor.modal_radio_button_set_preselected');
-      case ComponentTypes.Dropdown:
-        return t('ux_editor.component_dropdown_set_preselected');
-      default:
-        return 'Unknown component';
+        case ComponentTypes.RadioButtons:
+          return t('ux_editor.modal_radio_button_set_preselected');
+          case ComponentTypes.Dropdown:
+            return t('ux_editor.component_dropdown_set_preselected');
+            default:
+              return 'Unknown component';
     }
   }
 
@@ -32,7 +33,7 @@ export function EditPreselectedIndex({ component, handleComponentChange }: IGene
       <TextField
         defaultValue={(component as IFormGenericOptionsComponent).preselectedOptionIndex}
         formatting={{ number: {} }}
-        label={textSwitch(component.type)}
+        label={mapComponentTypeToText(component.type)}
         onChange={handlePreselectedOptionChange}
         placeholder={t('ux_editor.modal_selection_set_preselected_placeholder')}
       />
