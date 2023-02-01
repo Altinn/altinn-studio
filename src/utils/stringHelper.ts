03bf46a7c493202ee986c6883f6d2b4b0e450545
Ole Martin Handeland <github@olemartin.org>
2023-02-01 15:22:35 +0100

    Preventing crash on Party selection for users with names that contains abnormal spaces (#885)
    Co-authored-by: Ole Martin Handeland <git@olemartin.org>

--- src/utils/stringHelper.ts
+++ src/utils/stringHelper.ts
@@ -1,8 +1,19 @@
-export const capitalizeName: (name: string) => string = (name: string) => {
+export const capitalizeName = (name: string) => {
   return name
     .toLowerCase()
     .split(' ')
-    .map((str: string) => [str.split('')[0].toUpperCase(), str.substring(1)].join(''))
+    .map((word) => word.trim())
+    .filter((word) => !!word)
+    .map((word) => {
+      const firstLetter = word[0];
+      const rest = word.substring(1);
+
+      if (firstLetter && rest) {
+        return firstLetter.toUpperCase() + rest;
+      }
+
+      return word;
+    })
     .join(' ')
     .trim();
 };
