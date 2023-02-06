27f3a3bfe0bf60deb2351d9d7d779978d7ef0261
Ivar Nesje <ivarne@users.noreply.github.com>
2023-02-06 10:16:40 +0100

    Localtest: Add service owner orgs as possible lookups in register api. (#9684)
    * Localtest: Use CDN orgs in register api.
    
    * Get cdn orgs url from config.

--- src/development/LocalTest/Clients/CdnAltinnOrgs/AltinnOrgsClient.cs
+++ src/development/LocalTest/Clients/CdnAltinnOrgs/AltinnOrgsClient.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Altinn.Platform.Authentication.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace LocalTest.Clients.CdnAltinnOrgs;
 
@@ -12,15 +14,17 @@ public class AltinnOrgsClient
 {
     private static JsonSerializerOptions JSON_OPTIONS = new JsonSerializerOptions(JsonSerializerDefaults.Web);
     private readonly HttpClient _client;
+    private readonly GeneralSettings _authOptions;
 
-    public AltinnOrgsClient(HttpClient client)
+    public AltinnOrgsClient(HttpClient client, IOptions<GeneralSettings> authOptions)
     {
         _client = client;
+        _authOptions = authOptions.Value;
     }
 
     public async Task<CdnOrgs> GetCdnOrgs()
     {
-        var orgsJson = await _client.GetByteArrayAsync("https://altinncdn.no/orgs/altinn-orgs.json");
+        var orgsJson = await _client.GetByteArrayAsync(_authOptions.GetOrganisationRepositoryLocation);
         return JsonSerializer.Deserialize<CdnOrgs>(orgsJson, JSON_OPTIONS) ?? throw new JsonException("altinn-orgs respones was \"null\"");
     }
 }
--- src/development/LocalTest/Services/Register/Implementation/OrganizationsWrapper.cs
+++ src/development/LocalTest/Services/Register/Implementation/OrganizationsWrapper.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using Altinn.Platform.Register.Models;
+using LocalTest.Clients.CdnAltinnOrgs;
 using LocalTest.Services.Register.Interface;
 using LocalTest.Services.TestData;
 
@@ -11,17 +12,36 @@ namespace LocalTest.Services.Register.Implementation
     public class OrganizationsWrapper : IOrganizations
     {
         private readonly TestDataService _testDataService;
+        private readonly AltinnOrgsClient _orgsClient;
 
-        public OrganizationsWrapper(TestDataService testDataService)
+        public OrganizationsWrapper(TestDataService testDataService, AltinnOrgsClient orgsClient)
         {
             _testDataService = testDataService;
+            _orgsClient = orgsClient;
         }
 
         /// <inheritdoc />
         public async Task<Organization?> GetOrganization(string orgNr)
         {
             var data = await _testDataService.GetTestData();
-            return data.Register.Org.TryGetValue(orgNr, out var value) ? value : null;
+
+            if (data.Register.Org.TryGetValue(orgNr, out var value))
+            {
+                return value;
+            }
+
+            // Make lookup work for all the orgs that has apps in altinn.
+            var cdnOrgs = await _orgsClient.GetCdnOrgs();
+            var cdnOrg = cdnOrgs?.Orgs?.Values.FirstOrDefault(org=>org.Orgnr == orgNr);
+            if(cdnOrg is not null)
+            {
+                return new Organization
+                {
+                    Name = cdnOrg.Name?.Nb,
+                    OrgNumber = cdnOrg.Orgnr,
+                };
+            }
+            return null;
         }
     }
 }
