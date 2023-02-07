256c1fbb02c2b24eb57cf5fa86519d15b469924f
LAPTOP-S0QU96IH\david <david@framit.no>
2023-02-07 23:36:50 +0100

    bugfixed method findPreferredMetadataOption

--- frontend/packages/shared/src/features/dataModelling/functions/findPreferredMetadataOption.ts
+++ frontend/packages/shared/src/features/dataModelling/functions/findPreferredMetadataOption.ts
@@ -1,11 +1,20 @@
+import { GroupedOption } from '../components/SchemaSelect';
 import type { IMetadataOption } from './types';
 
-export function findPreferredMetadataOption(
-  metadataOptions: IMetadataOption[],
+export const findPreferredMetadataOption = (
+  groupedMetaData: GroupedOption[],
   preferred?: string
-) {
-  if (!metadataOptions?.length || !preferred) {
-    return undefined;
-  }
-  return metadataOptions.find(({ label }) => label === preferred);
-}
+): IMetadataOption => {
+  if (!groupedMetaData.length || !preferred) return;
+
+  const metadataOptions = getOptionsFromGroupedMetaData(groupedMetaData);
+
+  if (!metadataOptions.length) return;
+
+  const preferredMetadataOption = metadataOptions.find(({ label }) => label === preferred);
+  return preferredMetadataOption;
+};
+
+const getOptionsFromGroupedMetaData = (groupedMetaData: GroupedOption[]): IMetadataOption[] => {
+  return groupedMetaData.map(({ options }) => options).flat();
+};
