7989b426f3e577a335bbe7e38528b43ef4d9acd9
LAPTOP-S0QU96IH\david <david@framit.no>
2023-02-08 22:03:32 +0100

    removed unused useEffect

--- frontend/packages/text-editor/src/TextList.tsx
+++ frontend/packages/text-editor/src/TextList.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import classes from './TextEditor.module.css';
 import { TextRow } from './TextRow';
-import React, { useEffect } from 'react';
 import type {
   TextResourceEntry,
   TextResourceEntryDeletion,
