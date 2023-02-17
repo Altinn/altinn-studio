5bd875174e8c96b23fe73e08e7df6a46a89cc7b4
Bjørnar Østtveit <47412359+bjosttveit@users.noreply.github.com>
2023-02-17 10:28:46 +0100

    tweaked the pdf config (#191)

--- src/Altinn.App.Core/Models/Pdf/PdfGeneratorRequestOptions.cs
+++ src/Altinn.App.Core/Models/Pdf/PdfGeneratorRequestOptions.cs
@@ -8,12 +8,12 @@ internal class PdfGeneratorRequestOptions
     /// <summary>
     /// Indicate whether header and footer should be included.
     /// </summary>
-    public bool DisplayHeaderFooter { get; set; } = true;
+    public bool DisplayHeaderFooter { get; set; } = false;
 
     /// <summary>
     /// Indicate wheter the background should be included.
     /// </summary>
-    public bool PrintBackground { get; set; } = false;
+    public bool PrintBackground { get; set; } = true;
 
     /// <summary>
     /// Defines the page size. Default is A4.
@@ -34,20 +34,20 @@ internal class MarginOptions
     /// <summary>
     /// Top margin, accepts values labeled with units.
     /// </summary>
-    public string Top { get; set; } = "0.4in";
+    public string Top { get; set; } = "0.75in";
 
     /// <summary>
     /// Left margin, accepts values labeled with units
     /// </summary>
-    public string Left { get; set; } = "0.4in";
+    public string Left { get; set; } = "0.75in";
 
     /// <summary>
     /// Bottom margin, accepts values labeled with units
     /// </summary>
-    public string Bottom { get; set; } = "0.4in";
+    public string Bottom { get; set; } = "0.75in";
 
     /// <summary>
     /// Right margin, accepts values labeled with units
     /// </summary>
-    public string Right { get; set; } = "0.4in";
+    public string Right { get; set; } = "0.75in";
 }
