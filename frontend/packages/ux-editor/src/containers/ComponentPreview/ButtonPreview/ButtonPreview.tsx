bc76a03c85b43f9e4512ca41e5ed44ae546a9d51
LAPTOP-S0QU96IH\david <david@framit.no>
2023-02-17 14:37:42 +0100

    First draft of unit-tests

--- frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.tsx
+++ frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.tsx
@@ -5,25 +5,26 @@ import { getTextResource } from '../../../utils/language';
 import { useSelector } from 'react-redux';
 import { ComponentTypes } from '../../../components';
 
-
-interface ButtonPreviewProps {
+export interface ButtonPreviewProps {
   component: IFormButtonComponent;
 }
 export const ButtonPreview = ({ component }: ButtonPreviewProps): JSX.Element => {
-  const language = useSelector(
-    (state: IAppState) => state.appData.textResources.resources?.['nb']
-  );
-    
+  const language = useSelector((state: IAppState) => state.appData.textResources.resources?.['nb']);
+
   const isNavigationButton = component.type === ComponentTypes.NavigationButtons;
   const buttonColor = isNavigationButton ? ButtonColor.Success : ButtonColor.Primary;
-  const navigationButtonText = component.showBackButton ?  component.textResourceBindings?.next :  component.textResourceBindings?.previous
-  const buttonText = isNavigationButton ?  navigationButtonText : component.textResourceBindings?.title;
+
+  const navigationButtonText = component.showBackButton
+    ? component.textResourceBindings?.back
+    : component.textResourceBindings?.next;
+
+  const buttonText = isNavigationButton
+    ? navigationButtonText
+    : component.textResourceBindings?.title;
+
   return (
     <Button variant={ButtonVariant.Filled} color={buttonColor}>
       {getTextResource(buttonText, language)}
     </Button>
   );
 };
-
-
-
