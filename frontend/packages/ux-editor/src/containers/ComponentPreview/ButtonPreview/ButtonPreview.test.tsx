4ad1fd0c1bf1be0d4f73a24271455eece56a5ec5
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-02-20 09:07:34 +0100

    Add (Next button), css and test for all buttons

--- frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.test.tsx
+++ frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.test.tsx
@@ -5,6 +5,7 @@ import { ButtonPreview } from './ButtonPreview';
 import { ComponentTypes } from '../../../components';
 import { appDataMock, renderWithMockStore } from '../../../testing/mocks';
 
+// Test 1, tests if a button "Send inn" renders.
 describe('ButtonPreview', () => {
   test('should render "Send inn" button', () => {
     renderWithMock({
@@ -18,6 +19,7 @@ describe('ButtonPreview', () => {
     expect(screen.getByRole('button', { name: 'Send inn' }));
   });
 
+// Test 2, tests if a button "next" renders.
   test('should render next navigation button', () => {
     renderWithMock({
       id: 'PreviewNavigationButton',
@@ -29,10 +31,10 @@ describe('ButtonPreview', () => {
       type: ComponentTypes.NavigationButtons,
       onClickAction: () => {},
     });
-
     expect(screen.getByRole('button', { name: 'next' }));
   });
 
+// Test 3, tests if a button "Back" renders.
   test('should render back navigation button', () => {
     renderWithMock({
       id: 'PreviewNavigationButton',
@@ -44,14 +46,33 @@ describe('ButtonPreview', () => {
       type: ComponentTypes.NavigationButtons,
       onClickAction: () => {},
     });
-
     expect(screen.getByRole('button', { name: 'back' }));
   });
+
+  // Test 4, tests if the tow buttons "Back" and "Next" render together.
+  test("Should render back and next buttons", () => {
+    renderWithMock({
+      id:"PreviewNavigationButton",
+      textResourceBindings: {
+        next: "next",
+        back: "back",
+      },
+      showBackButton: true,
+      showNextButton: true,
+      type: ComponentTypes.NavigationButtons,
+      onClickAction: () => {},
+    });
+    expect(screen.getByRole("button", { name: "back" }));
+    expect(screen.getByRole("button", { name: "next" }));
+  })
 });
 
+//  Returns a rendered component with mock data from the appDataMock object.
 const renderWithMock = (component: IFormButtonComponent) => {
   const appData = {
     ...appDataMock,
   };
+//  Render the component with a mock store.
   return renderWithMockStore({ appData })(<ButtonPreview component={component} />);
 };
+
--- frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.tsx
+++ frontend/packages/ux-editor/src/containers/ComponentPreview/ButtonPreview/ButtonPreview.tsx
@@ -4,27 +4,36 @@ import { IAppState, IFormButtonComponent } from '../../../types/global';
 import { getTextResource } from '../../../utils/language';
 import { useSelector } from 'react-redux';
 import { ComponentTypes } from '../../../components';
+import clasess from "./ButtonPreview.module.css";
 
 export interface ButtonPreviewProps {
   component: IFormButtonComponent;
 }
-export const ButtonPreview = ({ component }: ButtonPreviewProps): JSX.Element => {
-  const language = useSelector((state: IAppState) => state.appData.textResources.resources?.['nb']);
 
+export const ButtonPreview = ({ component }: ButtonPreviewProps): JSX.Element => { 
+
+  // Extract resource for norwegian language and assign it to language const.
+  const language = useSelector((state: IAppState) => state.appData.textResources.resources?.['nb']);
+ 
+  // Determine the type of the button, and then chose its color, text, and back/next button.
   const isNavigationButton = component.type === ComponentTypes.NavigationButtons;
   const buttonColor = isNavigationButton ? ButtonColor.Primary : ButtonColor.Success;
-
   const navigationButtonText = component.showBackButton
     ? component.textResourceBindings?.back
     : component.textResourceBindings?.next;
+  const buttonText = isNavigationButton? navigationButtonText : component.textResourceBindings?.title; 
 
-  const buttonText = isNavigationButton
-    ? navigationButtonText
-    : component.textResourceBindings?.title;
-
+  // Render the button based on its type; either Send or navigation(Back/Next). 
   return (
-    <Button variant={ButtonVariant.Filled} color={buttonColor}>
-      {getTextResource(buttonText, language)}
-    </Button>
+    <div className={clasess.root}>
+      <Button variant={ButtonVariant.Filled} color={buttonColor}>
+        {getTextResource(buttonText, language)}
+      </Button>
+      {isNavigationButton && component.showBackButton && (
+        <Button className={clasess.showNextButton} variant={ButtonVariant.Filled} color={buttonColor}>
+          {getTextResource(component.textResourceBindings?.next, language,)}
+        </Button>
+      )}
+    </div>
   );
 };
