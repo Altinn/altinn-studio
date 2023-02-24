63f3e5ce54d68c512e2e12eaae4877a2342b427d
Mirko Sekulic <misha.sekulic@gmail.com>
2023-02-24 12:10:44 +0100

    chore: assembly exploring (#9919)
    * Fix assembly exploring
    
    * pipeline run. log lib info
    
    * only loadable assemblies
    
    * Extract and test Assembly explorer
    
    * fix wrong assembly load
    
    * additional check for not scanning provided marker type

--- backend/src/Designer/Configuration/Extensions/ServiceCollectionExtensions.cs
+++ backend/src/Designer/Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using Altinn.Studio.Designer.Helpers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Options;
 
 namespace Altinn.Studio.Designer.Configuration.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        private const string AltinnAssemblyIdentifier = "Altinn.";
         public static IServiceCollection RegisterSettingsByBaseType<TMarker>(this IServiceCollection services, IConfiguration configuration)
         {
-            var typesToRegister = GetTypesAssignedFrom<TMarker>()
+            var typesToRegister = AltinnAssembliesScanner.GetTypesAssignedFrom<TMarker>()
                 .Where(type => !type.IsInterface && !type.IsAbstract);
 
             foreach (var configType in typesToRegister)
@@ -50,63 +47,6 @@ namespace Altinn.Studio.Designer.Configuration.Extensions
             services.Configure<TOption>(configuration.GetSection(sectionName));
             services.TryAddScoped(typeof(TOption), svc => ((IOptionsSnapshot<object>)svc.GetService(typeof(IOptionsSnapshot<TOption>))).Value);
         }
-
-        private static IEnumerable<Type> GetTypesAssignedFrom<TAssignedFrom>()
-        {
-            if (DependencyContext.Default == null)
-            {
-                throw new InvalidOperationException("PreserveCompilationContext should be set to true.");
-            }
-
-            return DependencyContext.Default
-                .RuntimeLibraries
-                .Where(IsAltinnLibrary)
-                .Where(library => library.RuntimeAssemblyGroups.Any())
-                .Select(LoadAssembly)
-                .Where(assembly => assembly != null) // remove nulls
-                .GetTypesAssignedFrom<TAssignedFrom>();
-        }
-
-        private static IEnumerable<Type> GetTypesAssignedFrom<TAssignedFrom>(this IEnumerable<Assembly> assemblies)
-        {
-            return assemblies.SelectMany(a => a.GetTypesAssignedFrom<TAssignedFrom>());
-        }
-
-        private static IEnumerable<Type> GetTypesAssignedFrom<TAssignedFrom>(this Assembly assembly)
-        {
-            return assembly.GetLoadableTypes().Where(type => typeof(TAssignedFrom).IsAssignableFrom(type) && type != typeof(TAssignedFrom));
-        }
-
-        private static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
-        {
-            try
-            {
-                return assembly.GetTypes();
-            }
-            catch (ReflectionTypeLoadException e)
-            {
-                return e.Types.Where(t => t != null);
-            }
-        }
-
-        private static Assembly LoadAssembly(RuntimeLibrary library)
-        {
-            try
-            {
-                return Assembly.Load(new AssemblyName(library.Name));
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        private static bool IsAltinnLibrary(RuntimeLibrary library)
-        {
-            return library.Name.StartsWith(AltinnAssemblyIdentifier, StringComparison.OrdinalIgnoreCase)
-                || library.Dependencies.Any(d => d.Name.StartsWith(AltinnAssemblyIdentifier, StringComparison.OrdinalIgnoreCase));
-        }
-
 
     }
 }
