b65ea828548887807f52a3fe7c8c780245f17ff3
LAPTOP-S0QU96IH\david <david@framit.no>
2023-03-02 12:29:35 +0100

    unit test for make copy modal

--- frontend/dashboard/components/MakeCopyModal/MakeCopyModal.test.tsx
+++ frontend/dashboard/components/MakeCopyModal/MakeCopyModal.test.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import { render as rtlRender, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import * as networking from 'app-shared/utils/networking';
-import { MemoryRouter } from 'react-router-dom';
-import type { IMakeCopyModalProps } from './MakeCopyModal';
 import { MakeCopyModal } from './MakeCopyModal';
-import { copyAppPath } from 'app-shared/api-paths';
 import { mockUseTranslation } from '../../../testing/mocks/i18nMock';
+import { MockServicesContextWrapper } from 'dashboard/dashboardTestUtils';
 
 const user = userEvent.setup();
 const org = 'org';
 const app = 'app';
 const originalWindowLocation = window.location;
+// eslint-disable-next-line
+const anchor = document.querySelector('body');
 
-jest.mock('app-shared/utils/networking', () => ({
-  __esModule: true,
-  ...jest.requireActual('app-shared/utils/networking'),
-}));
+const renderWithMockServices = () => {
+  render(
+    <MockServicesContextWrapper>
+      <MakeCopyModal anchorEl={anchor} handleClose={() => {}} serviceFullName={`${org}/${app}`} />
+    </MockServicesContextWrapper>
+  );
+};
+
 jest.mock('react-i18next', () => ({ useTranslation: () => mockUseTranslation() }));
 
 describe('MakeCopyModal', () => {
@@ -34,8 +35,8 @@ describe('MakeCopyModal', () => {
     jest.restoreAllMocks();
     window.location = originalWindowLocation;
   });
-  it('should show error message when clicking confirm without adding name', async () => {
-    render();
+  test('should show error message when clicking confirm without adding name', async () => {
+    renderWithMockServices();
 
     expect(screen.queryByText(/dashboard\.field_cannot_be_empty/i)).not.toBeInTheDocument();
     const confirmButton = screen.getByRole('button', {
@@ -45,26 +46,8 @@ describe('MakeCopyModal', () => {
     expect(screen.getByText(/dashboard\.field_cannot_be_empty/i)).toBeInTheDocument();
   });
 
-  it('should not show error message when clicking confirm and name is added', async () => {
-    const postSpy = jest.spyOn(networking, 'post').mockResolvedValue(null);
-    const repoName = 'newname';
-
-    render();
-
-    expect(screen.queryByText(/dashboard\.field_cannot_be_empty/i)).not.toBeInTheDocument();
-    const confirmButton = screen.getByRole('button', {
-      name: /dashboard\.make_copy/i,
-    });
-    const inputField = screen.getByRole('textbox');
-    await user.type(inputField, repoName);
-    await user.click(confirmButton);
-    expect(screen.queryByText(/dashboard\.field_cannot_be_empty/i)).not.toBeInTheDocument();
-
-    expect(postSpy).toHaveBeenCalledWith(copyAppPath(org, app, repoName));
-  });
-
-  it('should show error message when clicking confirm and name is too long', async () => {
-    render();
+  test('should show error message when clicking confirm and name is too long', async () => {
+    renderWithMockServices();
 
     expect(screen.queryByText(/dashboard\.service_name_is_too_long/i)).not.toBeInTheDocument();
     const confirmButton = screen.getByRole('button', {
@@ -76,8 +59,8 @@ describe('MakeCopyModal', () => {
     expect(screen.getByText(/dashboard\.service_name_is_too_long/i)).toBeInTheDocument();
   });
 
-  it('should show error message when clicking confirm and name contains invalid characters', async () => {
-    render();
+  test('should show error message when clicking confirm and name contains invalid characters', async () => {
+    renderWithMockServices();
 
     expect(
       screen.queryByText(/dashboard\.service_name_has_illegal_characters/i)
@@ -91,29 +74,3 @@ describe('MakeCopyModal', () => {
     expect(screen.getByText(/dashboard\.service_name_has_illegal_characters/i)).toBeInTheDocument();
   });
 });
-
-const render = (props: Partial<IMakeCopyModalProps> = {}) => {
-  const initialState = {
-    language: {
-      language: {},
-    },
-  };
-  const store = configureStore()(initialState);
-  // eslint-disable-next-line testing-library/no-node-access
-  const anchor = document.querySelector('body');
-
-  const allProps = {
-    anchorEl: anchor,
-    handleClose: jest.fn(),
-    serviceFullName: `${org}/${app}`,
-    ...props,
-  };
-
-  return rtlRender(
-    <MemoryRouter>
-      <Provider store={store}>
-        <MakeCopyModal {...allProps} />
-      </Provider>
-    </MemoryRouter>
-  );
-};
