149cc53ee2d47d8d008fcb48664c28495a967aa1
johlie <48760352+johlie@users.noreply.github.com>
2023-03-02 10:07:10 +0100

    Add getTextResourceAsString to headers in listcomponent (#956)
    * Add getTextResourceAsString to headers in table
    
    * Added new object to store translated tableHeaders
    
    * Update src/layout/List/ListComponent.tsx
    
    * Update src/layout/List/ListComponent.tsx
    
    ---------
    
    Co-authored-by: Johanne Lie <joli@patentstyret.no>
    Co-authored-by: Ole Martin Handeland <github@olemartin.org>

--- src/layout/List/ListComponent.tsx
+++ src/layout/List/ListComponent.tsx
@@ -26,6 +26,7 @@ export const ListComponent = ({
   pagination,
   formData,
   handleDataChange,
+  getTextResourceAsString,
   sortableColumns,
   tableHeadersMobile,
   language,
@@ -52,6 +53,11 @@ export const ListComponent = ({
       handleDataChange(selectedValue[key], { key: key });
     }
   };
+
+  const tableHeadersValues = { ...tableHeaders };
+  for (const key in tableHeaders) {
+    tableHeadersValues[key] = getTextResourceAsString(tableHeaders[key]);
+  }
 
   const selectedRow: Record<string, string> = React.useMemo(() => {
     let matchRow: boolean[] = [];
@@ -119,7 +125,7 @@ export const ListComponent = ({
 
   const config: ResponsiveTableConfig<Record<string, string>> = {
     rows: calculatedDataList,
-    headers: tableHeaders,
+    headers: tableHeadersValues,
     showColumnsMobile: tableHeadersMobile,
     columnSort: {
       onSortChange: ({ column, next, previous }) => {
