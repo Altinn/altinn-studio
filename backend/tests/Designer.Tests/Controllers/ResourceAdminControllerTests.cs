b7a45470cb361c0d2833dbccf6a3f565f46f8b2a
Sondre Bråten <sondre.braten@avanade.com>
2023-03-07 12:43:43 +0100

    Fixed imports ordering in ResourceAdminControllerTests.cs.

--- backend/tests/Designer.Tests/Controllers/ResourceAdminControllerTests.cs
+++ backend/tests/Designer.Tests/Controllers/ResourceAdminControllerTests.cs
@@ -1,5 +1,5 @@
-﻿using System.Net.Http;
-using System.Net;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Controllers;
 using Altinn.Studio.Designer.Services.Interfaces;
