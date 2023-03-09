caa738c4649be1961979845779cc0945499e3a77
Rune T. Larsen <rune@hjemmekino.no>
2023-03-09 08:27:02 +0100

    More test samples

--- backend/tests/Designer.Tests/Helpers/PolicyHelperTests.cs
+++ backend/tests/Designer.Tests/Helpers/PolicyHelperTests.cs
@@ -19,7 +19,7 @@ namespace Designer.Tests.Helpers
     {
 
         [Fact]
-        public void TestXacmlToJson_RRH()
+        public void TestXacmlToJson_brg_rrh_innrapportering()
         {
             XacmlPolicy policy = AuthorizationUtil.ParsePolicy("brg_rrh-innrapportering.xml");
             ResourcePolicy convertedPolicy = PolicyConverter.ConvertPolicy(policy);
@@ -29,5 +29,42 @@ namespace Designer.Tests.Helpers
             AuthorizationUtil.WritePolicy("brg_rrh-innrapportering_converted.xml", convertedBackPolicy);
             AssertionUtil.AssertXacmlPolicy(policy, convertedBackPolicy);
         }
+
+        [Fact]
+        public void TestXacmlToJson_ssb_ra1000_01()
+        {
+            XacmlPolicy policy = AuthorizationUtil.ParsePolicy("ssb_ra1000-01.xml");
+            ResourcePolicy convertedPolicy = PolicyConverter.ConvertPolicy(policy);
+            Assert.NotNull(convertedPolicy);
+            XacmlPolicy convertedBackPolicy = PolicyConverter.ConvertPolicy(convertedPolicy);
+            AuthorizationUtil.WriteJsonPolicy("ssb_ra1000-01.json", convertedPolicy);
+            AuthorizationUtil.WritePolicy("ssb_ra1000-01_converted.xml", convertedBackPolicy);
+            AssertionUtil.AssertXacmlPolicy(policy, convertedBackPolicy);
+        }
+
+        [Fact]
+        public void TestXacmlToJson_krt_krt_1012a_1()
+        {
+            XacmlPolicy policy = AuthorizationUtil.ParsePolicy("krt_krt-1012a-1.xml");
+            ResourcePolicy convertedPolicy = PolicyConverter.ConvertPolicy(policy);
+            Assert.NotNull(convertedPolicy);
+            XacmlPolicy convertedBackPolicy = PolicyConverter.ConvertPolicy(convertedPolicy);
+            AuthorizationUtil.WriteJsonPolicy("krt_krt-1012a-1.json", convertedPolicy);
+            AuthorizationUtil.WritePolicy("krt_krt-1012a-1_converted.xml", convertedBackPolicy);
+            AssertionUtil.AssertXacmlPolicy(policy, convertedBackPolicy);
+        }
+
+        [Fact]
+        public void TestXacmlToJson_hmrhf_newsamhandlingsavik()
+        {
+            XacmlPolicy policy = AuthorizationUtil.ParsePolicy("hmrhf_newsamhandlingsavik.xml");
+            ResourcePolicy convertedPolicy = PolicyConverter.ConvertPolicy(policy);
+            Assert.NotNull(convertedPolicy);
+            XacmlPolicy convertedBackPolicy = PolicyConverter.ConvertPolicy(convertedPolicy);
+            AuthorizationUtil.WriteJsonPolicy("hmrhf_newsamhandlingsavik.json", convertedPolicy);
+            AuthorizationUtil.WritePolicy("hmrhf_newsamhandlingsavik_converted.xml", convertedBackPolicy);
+            AssertionUtil.AssertXacmlPolicy(policy, convertedBackPolicy);
+        }
+
     }
 }
