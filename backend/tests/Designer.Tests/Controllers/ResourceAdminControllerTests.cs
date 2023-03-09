022cf9de3821a2cbed2f87c4e9b0f994301139ac
Sondre-B <75827794+Sondre-B@users.noreply.github.com>
2023-03-09 14:04:40 +0100

    Created new controller ResourceAdminController (#9972)
    * Created new controller ResourceAdminController with endpoint for retrieving the resource repository for a given org.
    
    * Adjusted whitespace-formatting in the IGiteaMock class.
    
    * Fixed some code smells reported by SonarCloud.
    
    * Fixed imports ordering.
    
    * Fixed imports ordering in ResourceAdminControllerTests.cs.
    
    * Corrected bug in GetRepository-method.
    
    * Rewritten IGiteaMock.GetOrgRepos to use ternary operator.
    
    ---------
    
    Co-authored-by: Sondre Bråten <sondre.braten@avanade.com>

---
+++ backend/tests/Designer.Tests/Controllers/ResourceAdminControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Controllers;
+using Altinn.Studio.Designer.Services.Interfaces;
+using Designer.Tests.Controllers.ApiTests;
+using Designer.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Designer.Tests.Controllers
+{
+    public class ResourceAdminControllerTests : ApiTestsBase<RepositoryController, RepositoryControllerTests>
+    {
+        private readonly string _versionPrefix = "/designer/api";
+        private readonly Mock<IGitea> _giteaMock;
+
+        public ResourceAdminControllerTests(WebApplicationFactory<RepositoryController> factory) : base(factory)
+        {
+            _giteaMock = new Mock<IGitea>();
+        }
+
+        protected override void ConfigureTestServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGitea, IGiteaMock>();
+        }
+
+        [Fact]
+        public async Task GetResourceRepository_OK()
+        {
+            // Arrange
+            string uri = $"{_versionPrefix}/ttd/resources/repository";
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            // Act
+            HttpResponseMessage res = await HttpClient.Value.SendAsync(httpRequestMessage).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, res.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetResourceRepository_NotFound()
+        {
+            // Arrange
+            string uri = $"{_versionPrefix}/orgwithoutrepo/resources/repository";
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            // Act
+            HttpResponseMessage res = await HttpClient.Value.SendAsync(httpRequestMessage).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, res.StatusCode);
+        }
+    }
+}
