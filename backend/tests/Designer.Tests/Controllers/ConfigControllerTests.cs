73ac17f6efc6f26c8adc51c1734b2b8e927b37dc
andrealouisestanderen <andreastanderen@gmail.com>
2023-03-28 09:44:51 +0200

    Add test for ConfigController

--- backend/tests/Designer.Tests/Controllers/ConfigControllerTests.cs
+++ backend/tests/Designer.Tests/Controllers/ConfigControllerTests.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Configuration;
@@ -6,6 +9,7 @@ using Altinn.Studio.Designer.Controllers;
 using Altinn.Studio.Designer.Services.Interfaces;
 using Designer.Tests.Controllers.ApiTests;
 using Designer.Tests.Mocks;
+using Designer.Tests.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,12 +17,23 @@ using Xunit;
 
 namespace Designer.Tests.Controllers;
 
-public class ConfigControllerTests : ApiTestsBase<ConfigController, ConfigControllerTests>
+public class ConfigControllerTests : ApiTestsBase<ConfigController, ConfigControllerTests>, IDisposable
 {
     private readonly string _versionPrefix = "designer/api";
+    private readonly JsonSerializerOptions _options;
+    private string CreatedFolderPath { get; set; }
+
+    public void Dispose()
+    {
+        if (!string.IsNullOrWhiteSpace(CreatedFolderPath))
+        {
+            TestDataHelper.DeleteDirectory(CreatedFolderPath);
+        }
+    }
 
     public ConfigControllerTests(WebApplicationFactory<ConfigController> factory) : base(factory)
     {
+        _options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
     }
 
     protected override void ConfigureTestServices(IServiceCollection services)
@@ -36,13 +51,11 @@ public class ConfigControllerTests : ApiTestsBase<ConfigController, ConfigContro
 
         HttpResponseMessage response = await HttpClient.Value.SendAsync(httpRequestMessage);
         response.EnsureSuccessStatusCode();
-        string responseBody = await response.Content.ReadAsStringAsync();
-        JsonDocument responseDocument = JsonDocument.Parse(responseBody);
-        ServiceConfiguration serviceConfigResponse = JsonSerializer.Deserialize<ServiceConfiguration>(response.Content.ToString());
-        ServiceConfiguration serviceConfiguration = new () { RepositoryName = "apps-test" };
+        ServiceConfiguration serviceConfigResponse = await response.Content.ReadAsAsync<ServiceConfiguration>();
+        ServiceConfiguration serviceConfiguration = new () { RepositoryName = "apps-test", ServiceDescription = null, ServiceId = null, ServiceName = null};
 
         Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
-        Assert.Equal(serviceConfiguration, serviceConfigResponse);
+        Assert.Equal(serviceConfiguration.RepositoryName, serviceConfigResponse.RepositoryName);
     }
 
     [Fact]
@@ -53,21 +66,38 @@ public class ConfigControllerTests : ApiTestsBase<ConfigController, ConfigContro
 
         HttpResponseMessage response = await HttpClient.Value.SendAsync(httpRequestMessage);
         response.EnsureSuccessStatusCode();
+        ServiceConfiguration serviceConfigResponse = await response.Content.ReadAsAsync<ServiceConfiguration>();
+        ServiceConfiguration serviceConfiguration = GetServiceConfiguration("ttd", "hvem-er-hvem");
 
         Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
-        Assert.Equal(true, true);
+        Assert.Equal(serviceConfiguration.RepositoryName, serviceConfigResponse.RepositoryName);
+        Assert.Equal(serviceConfiguration.ServiceDescription, serviceConfigResponse.ServiceDescription);
+        Assert.Equal(serviceConfiguration.ServiceId, serviceConfigResponse.ServiceId);
+        Assert.Equal(serviceConfiguration.ServiceName, serviceConfigResponse.ServiceName);
     }
 
     [Fact]
     public async Task SetServiceConfig_OK()
     {
-        string dataPathWithData = $"{_versionPrefix}/ttd/hvem-er-hvem/config";
+        string targetRepository = TestDataHelper.GenerateTestRepoName();
+        CreatedFolderPath = await TestDataHelper.CopyRepositoryForTest("ttd", "hvem-er-hvem", "testUser", targetRepository);
+
+        string dataPathWithData = $"{_versionPrefix}/ttd/{targetRepository}/config";
         HttpRequestMessage httpRequestMessage = new (HttpMethod.Post, dataPathWithData);
+        httpRequestMessage.Content = JsonContent.Create( new { serviceName = "Alternative-form-name", serviceDescription = "", serviceId = "" });
 
         HttpResponseMessage response = await HttpClient.Value.SendAsync(httpRequestMessage);
         response.EnsureSuccessStatusCode();
+        ServiceConfiguration serviceConfiguration = GetServiceConfiguration("ttd",targetRepository);
 
         Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
-        Assert.Equal(true, true);
+        Assert.Equal("Alternative-form-name", serviceConfiguration.ServiceName);
+    }
+
+    private ServiceConfiguration GetServiceConfiguration(string org, string app)
+    {
+        string path = Path.Combine(TestRepositoriesLocation, "testUser", org, app, "config.json");
+        string config = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<ServiceConfiguration>(config, _options);
     }
 }
