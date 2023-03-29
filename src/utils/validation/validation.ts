42d66ffec79d772a8ebdd4fe42b8c91f8947193c
Ivar Nesje <ivarne@users.noreply.github.com>
2023-03-29 09:53:11 +0200

    Add a simple null check to validation code (#1059)

--- src/utils/validation/validation.ts
+++ src/utils/validation/validation.ts
@@ -845,6 +845,7 @@ export function findComponentFromValidationIssue(
         if (
           validation.field &&
           componentCandidate.dataModelBindings &&
+          componentCandidate.dataModelBindings[dataModelBindingKey] &&
           componentCandidate.dataModelBindings[dataModelBindingKey].toLowerCase() === fieldToCheck
         ) {
           found = true;
