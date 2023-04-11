d2aab0e12286c522dcd4c3622a795355b0ef7a62
andrealouisestanderen <andreastanderen@gmail.com>
2023-04-11 10:08:46 +0200

    Generalize endpoints for getting datamodel

--- frontend/packages/shared/src/constants.js
+++ frontend/packages/shared/src/constants.js
@@ -1,5 +1,4 @@
 export const APP_DEVELOPMENT_BASENAME = '/editor';
 export const DASHBOARD_BASENAME = '/dashboard';
-//export const PREVIEW_BASENAME = '/preview';
 export const DEFAULT_LANGUAGE = 'nb';
 export const BASE_CONTAINER_ID = '__base__';
