5751f44f9fc9ae1c570df8c8f05d6deedb26771c
Mirko Sekulic <misha.sekulic@gmail.com>
2023-04-17 15:23:37 +0200

    splitting config controller tests (#10186)

---
+++ backend/tests/Designer.Tests/Controllers/ConfigController/SetServiceConfigTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Configuration;
+using Designer.Tests.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Designer.Tests.Controllers.ConfigController
+{
+    public class SetServiceConfigTests : ConfigControllerTestsBase<SetServiceConfigTests>
+    {
+
+        public SetServiceConfigTests(WebApplicationFactory<Altinn.Studio.Designer.Controllers.ConfigController> factory) : base(factory)
+        {
+        }
+
+        [Theory]
+        [InlineData("ttd", "hvem-er-hvem", "testUser")]
+        public async Task SetServiceConfig_OK(string org, string app, string developer)
+        {
+            string targetRepository = TestDataHelper.GenerateTestRepoName();
+            CreatedFolderPath = await TestDataHelper.CopyRepositoryForTest(org, app, developer, targetRepository);
+
+            string dataPathWithData = VersionPrefix(org, targetRepository);
+            using HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, dataPathWithData);
+            httpRequestMessage.Content = JsonContent.Create(new { serviceName = "Alternative-form-name", serviceDescription = "", serviceId = "" });
+
+            using HttpResponseMessage response = await HttpClient.Value.SendAsync(httpRequestMessage);
+            response.EnsureSuccessStatusCode();
+            ServiceConfiguration serviceConfiguration = GetServiceConfiguration(org, targetRepository);
+
+            Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+            Assert.Equal("Alternative-form-name", serviceConfiguration.ServiceName);
+        }
+    }
+}
