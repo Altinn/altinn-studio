d1973b11e7127f5d2d903f470ca32e96423d6af4
andrealouisestanderen <andreastanderen@gmail.com>
2023-04-21 12:59:58 +0200

    Add test for redirect on preview button click

--- frontend/app-preview/src/views/LandingPage.tsx
+++ frontend/app-preview/src/views/LandingPage.tsx
@@ -3,9 +3,11 @@ import classes from './LandingPage.module.css';
 import { PreviewContext } from '../PreviewContext';
 import { useParams } from 'react-router-dom';
 import { stringify } from 'qs';
+import { useTranslation } from 'react-i18next';
 
 export const LandingPage = () => {
   const { org, app } = useParams();
+  const { t } = useTranslation();
 
   return (
     <PreviewContext>
@@ -13,7 +15,7 @@ export const LandingPage = () => {
         <h1>Altinn Studio - App Preview</h1>
       </div>
       <iframe
-        title='App in preview'
+        title={t('preview.iframe_title')}
         id='app-frontend-react-iframe'
         src={`/designer/html/preview.html?${stringify({ org, app })}`}
         className={classes.iframe}
