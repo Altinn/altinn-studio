356b17c565565fa3ab765b94fab109d002c58415
Mirko Sekulic <misha.sekulic@gmail.com>
2023-04-25 10:35:40 +0200

    base test class

---
+++ backend/tests/Designer.Tests/Controllers/ApplicationMetadataController/ApplicationMetadataControllerTestsBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Altinn.Studio.Designer.Configuration;
+using Altinn.Studio.Designer.Services.Interfaces;
+using Designer.Tests.Controllers.ApiTests;
+using Designer.Tests.Mocks;
+using Designer.Tests.Utils;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Designer.Tests.Controllers.ApplicationMetadataController
+{
+    public class ApplicationMetadataControllerTestsBase<TControllerTestType> : ApiTestsBase<Altinn.Studio.Designer.Controllers.ApplicationMetadataController, TControllerTestType>, IDisposable
+        where TControllerTestType : class
+    {
+        protected static string VersionPrefix(string org, string repository) => $"/designer/api/{org}/{repository}/metadata";
+        protected string CreatedFolderPath { get; set; }
+
+        public void Dispose()
+        {
+            if (!string.IsNullOrWhiteSpace(CreatedFolderPath))
+            {
+                TestDataHelper.DeleteDirectory(CreatedFolderPath);
+            }
+        }
+
+        public ApplicationMetadataControllerTestsBase(WebApplicationFactory<Altinn.Studio.Designer.Controllers.ApplicationMetadataController> factory) : base(factory)
+        {
+        }
+
+        protected override void ConfigureTestServices(IServiceCollection services)
+        {
+            services.Configure<ServiceRepositorySettings>(c =>
+                c.RepositoryLocation = TestRepositoriesLocation);
+            services.AddSingleton<IGitea, IGiteaMock>();
+        }
+    }
+}
