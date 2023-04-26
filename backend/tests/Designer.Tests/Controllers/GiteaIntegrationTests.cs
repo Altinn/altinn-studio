57150477cea8b1c429a40655b33f75f3998bf344
Mirko Sekulic <misha.sekulic@gmail.com>
2023-04-26 14:15:32 +0200

    Init Gitea Fixture

---
+++ backend/tests/Designer.Tests/Controllers/GiteaIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Configuration;
+using Altinn.Studio.Designer.Controllers;
+using Altinn.Studio.Designer.Services.Interfaces;
+using Designer.Tests.Controllers.ApiTests;
+using Designer.Tests.Fixtures;
+using Designer.Tests.Mocks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Designer.Tests.Controllers
+{
+    [Collection(nameof(GiteaCollection))]
+    public class GiteaIntegrationTests : ApiTestsBase<UserController, GiteaIntegrationTests>
+    {
+        private readonly string _versionPrefix = "designer/api/user";
+        private GiteaFixture _giteaFixture;
+
+        public GiteaIntegrationTests(GiteaFixture giteaFixture, WebApplicationFactory<UserController> factory) : base(factory)
+        {
+            _giteaFixture = giteaFixture;
+        }
+
+        protected override void ConfigureTestServices(IServiceCollection services)
+        {
+            services.Configure<ServiceRepositorySettings>(c =>
+                c.RepositoryLocation = TestRepositoriesLocation);
+            services.AddSingleton<IGitea, IGiteaMock>();
+        }
+
+        [Fact]
+        public async Task GetCurrentUser_ShouldReturnOk()
+        {
+            string requestUrl = $"{_versionPrefix}/current";
+            using HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+
+            using HttpResponseMessage response = await HttpClient.Value.SendAsync(httpRequestMessage);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Headers.First(h => h.Key == "Set-Cookie").Value.Should().Satisfy(e => e.Contains("XSRF-TOKEN"));
+        }
+    }
+}
---
+++ backend/tests/Designer.Tests/Fixtures/GiteaFixture.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
+using Testcontainers.PostgreSql;
+using Xunit;
+using static System.IO.Path;
+
+namespace Designer.Tests.Fixtures
+{
+    public class GiteaFixture : IAsyncLifetime
+    {
+        private INetwork _giteaNetwork;
+
+        public PostgreSqlContainer _postgreSqlContainer;
+
+        public IContainer _giteaContainer;
+
+        private async Task BuildAndCreateGiteaNetworkAsync()
+        {
+            _giteaNetwork = new NetworkBuilder()
+                .WithName(Guid.NewGuid().ToString("D"))
+                .Build();
+            await _giteaNetwork.CreateAsync();
+        }
+
+        private async Task BuildAndStartPostgreSqlContainerAsync()
+        {
+            _postgreSqlContainer = new PostgreSqlBuilder()
+                .WithNetwork(_giteaNetwork)
+                .WithNetworkAliases("db")
+                .WithUsername("gitea")
+                .WithPassword("gitea")
+                .WithDatabase("gitea")
+                .Build();
+            await _postgreSqlContainer.StartAsync();
+        }
+
+        private async Task BuildAndStartGiteaContainerAsync()
+        {
+            _giteaContainer = new ContainerBuilder()
+                .WithImage("gitea/gitea:1.19.1-rootless")
+                .WithNetwork(_giteaNetwork)
+                .WithPortBinding(3000, 3000)
+                .WithPortBinding(222, 22)
+                .WithEnvironment(new Dictionary<string, string>
+                {
+                    {"GITEA____RUN_MODE", "prod"},
+                    {"GITEA__database__DB_TYPE", "postgres"},
+                    {"GITEA__database__HOST", "db:5432"},
+                    {"GITEA__database__NAME", "gitea"},
+                    {"GITEA__database__USER", "gitea"},
+                    {"GITEA__database__PASSWD", "gitea"},
+                    {"USER_GID", "1000"},
+                    {"USER_UID", "1000"}
+                })
+                .Build();
+            await _giteaContainer.StartAsync();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await BuildAndCreateGiteaNetworkAsync();
+            await BuildAndStartPostgreSqlContainerAsync();
+            await BuildAndStartAltinnGiteaAsync();
+        }
+        public async Task DisposeAsync()
+        {
+            await _postgreSqlContainer.DisposeAsync();
+            await _giteaContainer.DisposeAsync();
+            await _giteaNetwork.DeleteAsync();
+        }
+
+
+
+        private async Task BuildAndStartAltinnGiteaAsync()
+        {
+            string giteaDockerFilePath = Combine(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath, "..", "gitea");
+            var altinnGiteaImage = new ImageFromDockerfileBuilder()
+                .WithDockerfileDirectory(giteaDockerFilePath)
+                .WithDockerfile("Dockerfile")
+                .WithName("repositories:latest")
+                .Build();
+
+            await altinnGiteaImage.CreateAsync();
+
+            _giteaContainer = new ContainerBuilder().WithImage("repositories:latest")
+                .WithNetwork(_giteaNetwork)
+                .WithPortBinding(3000, 3000)
+                .WithPortBinding(222, 22)
+                .WithEnvironment(new Dictionary<string, string>
+                {
+                    {"GITEA____RUN_MODE", "prod"},
+                    {"GITEA__database__DB_TYPE", "postgres"},
+                    {"GITEA__database__HOST", "db:5432"},
+                    {"GITEA__database__NAME", "gitea"},
+                    {"GITEA__database__USER", "gitea"},
+                    {"GITEA__database__PASSWD", "gitea"},
+                    {"USER_GID", "1000"},
+                    {"USER_UID", "1000"}
+                })
+                .Build();
+            await _giteaContainer.StartAsync();
+        }
+    }
+
+}
