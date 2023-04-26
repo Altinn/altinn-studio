009b0dacf323c57d1ce55b7e01bfaec05e2687f9
Mirko Sekulic <misha.sekulic@gmail.com>
2023-04-26 15:44:00 +0200

    Create gitea admin user

--- backend/tests/Designer.Tests/Fixtures/GiteaFixture.cs
+++ backend/tests/Designer.Tests/Fixtures/GiteaFixture.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Networks;
+using Polly;
 using Testcontainers.PostgreSql;
 using Xunit;
 using static System.IO.Path;
@@ -36,28 +39,6 @@ namespace Designer.Tests.Fixtures
                 .WithDatabase("gitea")
                 .Build();
             await _postgreSqlContainer.StartAsync();
-        }
-
-        private async Task BuildAndStartGiteaContainerAsync()
-        {
-            _giteaContainer = new ContainerBuilder()
-                .WithImage("gitea/gitea:1.19.1-rootless")
-                .WithNetwork(_giteaNetwork)
-                .WithPortBinding(3000, 3000)
-                .WithPortBinding(222, 22)
-                .WithEnvironment(new Dictionary<string, string>
-                {
-                    {"GITEA____RUN_MODE", "prod"},
-                    {"GITEA__database__DB_TYPE", "postgres"},
-                    {"GITEA__database__HOST", "db:5432"},
-                    {"GITEA__database__NAME", "gitea"},
-                    {"GITEA__database__USER", "gitea"},
-                    {"GITEA__database__PASSWD", "gitea"},
-                    {"USER_GID", "1000"},
-                    {"USER_UID", "1000"}
-                })
-                .Build();
-            await _giteaContainer.StartAsync();
         }
 
         public async Task InitializeAsync()
@@ -103,6 +84,20 @@ namespace Designer.Tests.Fixtures
                 })
                 .Build();
             await _giteaContainer.StartAsync();
+
+            // Make sure that gitea is up and running before we try to create the admin user
+            var policy = Policy.HandleResult<ExecResult>(x => x.ExitCode != 0)
+                .WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromSeconds(1));
+
+            await policy.ExecuteAsync(_ =>
+            {
+                return _giteaContainer.ExecAsync(new[]
+                {
+                    "/bin/sh", "-c", "gitea admin user create --username testadmin --password TestAdmin1234$ --email testadmin@digdir.no --must-change-password=false"
+                }, _);
+
+            }, CancellationToken.None);
+
         }
     }
 
