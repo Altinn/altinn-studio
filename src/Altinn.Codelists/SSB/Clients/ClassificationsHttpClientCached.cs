ad86b1f84d53cedf15f159d88b91336e2f46c0a7
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-05-02 21:35:36 +0200

    Add cache decorator implementation for SSB classifications client.

--- src/Altinn.Codelists/SSB/Clients/ClassificationsHttpClientCached.cs
+++ src/Altinn.Codelists/SSB/Clients/ClassificationsHttpClientCached.cs
@@ -1,21 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Caching.Memory;
 
 namespace Altinn.Codelists.SSB.Clients
 {
     /// <summary>
     /// Http client to get classification codes from SSB.
-    /// This class caches the information for performance reasons to avoid costly http calls.
+    /// This is a decorator class for <see cref="IClassificationsClient"/> 
+    /// that caches the information for performance reasons.
     /// </summary>
     public class ClassificationsHttpClientCached : IClassificationsClient
     {
-        /// <inheritdoc/>
-        public Task<ClassificationCodes> GetClassificationCodes(int classificationId, string language = "nb", DateOnly? atDate = null, string level = "", string variant = "")
+        private readonly IClassificationsClient _classificationsClient;
+        private readonly IMemoryCache _memoryCache;
+        private readonly Func<MemoryCacheEntryOptions> _getCacheEntryOptions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassificationsHttpClientCached"/> class.
+        /// </summary>
+        public ClassificationsHttpClientCached(IClassificationsClient classificationsClient, IMemoryCache memoryCache) : this(classificationsClient, memoryCache, DefaultCacheEntryOptions)
         {
-            throw new NotImplementedException();
+            _classificationsClient = classificationsClient;
+            _memoryCache = memoryCache;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClassificationsHttpClientCached"/> class.
+        /// </summary>
+        public ClassificationsHttpClientCached(IClassificationsClient classificationsClient, IMemoryCache memoryCache, Func<MemoryCacheEntryOptions> getCacheEntryOptionsFunc)
+        {
+            _classificationsClient = classificationsClient;
+            _memoryCache = memoryCache;
+            _getCacheEntryOptions = getCacheEntryOptionsFunc;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ClassificationCodes> GetClassificationCodes(int classificationId, string language = "nb", DateOnly? atDate = null, string level = "", string variant = "")
+        {
+            var cacheKey = GetCacheKey(classificationId, language, atDate, level, variant);
+
+            var codes = await _memoryCache.GetOrCreateAsync(cacheKey, async cacheEntry =>
+            {
+                var cacheEntryOptions = _getCacheEntryOptions.Invoke();
+                cacheEntry.SetOptions(cacheEntryOptions);
+                var data = await _classificationsClient.GetClassificationCodes(classificationId, language, atDate, level, variant);
+
+                if (data is null)
+                {
+                    cacheEntry.Dispose();
+                    return null;
+                }
+
+                return data;
+            });
+
+            return codes ?? new ClassificationCodes() { Codes = new List<ClassificationCode>() };
+        }
+
+        private static string GetCacheKey(int classificationId, string language, DateOnly? atDate, string level, string variant)
+        {
+            return $"{classificationId}_{language}_{atDate?.ToString("yyyy-MM-dd")}_{level}_{variant}";
+        }
+
+        // Expires the cache entry at midnight, to get potential new or removed entries.
+        private static MemoryCacheEntryOptions DefaultCacheEntryOptions()
+        {
+            DateTime expirationTime = DateTime.Today.AddDays(1);
+
+            return new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpiration = expirationTime,
+                Priority = CacheItemPriority.Normal
+            };
         }
     }
 }
