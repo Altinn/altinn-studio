725e93390f804c3f1a3a88550576c6dd2b16548d
Mirko Sekulic <misha.sekulic@gmail.com>
2023-05-09 11:50:22 +0200

    Assign random ports for test gitea

--- backend/tests/Designer.Tests/Fixtures/GiteaFixture.cs
+++ backend/tests/Designer.Tests/Fixtures/GiteaFixture.cs
@@ -100,8 +100,8 @@ namespace Designer.Tests.Fixtures
 
             _giteaContainer = new ContainerBuilder().WithImage("repositories:latest")
                 .WithNetwork(_giteaNetwork)
-                .WithPortBinding(3000)
-                .WithPortBinding(222, 22)
+                .WithPortBinding(3000, true)
+                .WithPortBinding( 22, true)
                 .WithEnvironment(new Dictionary<string, string>
                 {
                     {"GITEA____RUN_MODE", "prod"},
