c1a9b5f28bceb1d16ddb737007d1c51e70d8b5d3
Mirko Sekulic <misha.sekulic@gmail.com>
2023-05-12 07:58:44 +0200

    remoteUrl fix (#10349)

---
+++ backend/src/Designer/Helpers/Extensions/UriExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Altinn.Studio.Designer.Helpers.Extensions
+{
+    public static class UriExtensions
+    {
+        public static Uri Append(this Uri uri, params string[] paths)
+        {
+            return new Uri(paths.Aggregate(uri.AbsoluteUri, (current, path) => $"{current.TrimEnd('/')}/{path.TrimStart('/')}"));
+        }
+    }
+}
---
+++ backend/tests/Designer.Tests/Helpers/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Altinn.Studio.Designer.Helpers.Extensions;
+using Xunit;
+
+namespace Designer.Tests.Helpers
+{
+    public class Extensions
+    {
+        [Theory]
+        [InlineData("http://studio.localhost/repos", "http://studio.localhost/repos/ttd/testrepo.git", "ttd/testrepo.git")]
+        [InlineData("http://studio.localhost/repos", "http://studio.localhost/repos/first/second/third", "first", "second", "third")]
+        [InlineData("http://altinn-repositories:3000", "http://altinn-repositories:3000/ttd/testrepo.git", "ttd/testrepo.git")]
+        public void AppendToUri_ShouldReturnExpected(string baseUrl, string expectedOutput, params string[] paths)
+        {
+            var uri = new System.Uri(baseUrl);
+            var result = uri.Append(paths);
+            Assert.Equal(expectedOutput, result.ToString());
+        }
+    }
+}
