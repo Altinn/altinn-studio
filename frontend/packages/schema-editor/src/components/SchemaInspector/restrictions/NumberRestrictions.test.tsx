e07a2665d95ea0d040f5e45eafaaeff27315eb62
Nina Kylstad <nkylstad@gmail.com>
2023-05-16 09:04:21 +0200

    allow value 0 (#10356)
    * allow value 0
    
    * added tests
    
    * linting
    
    * remove act-calls

--- frontend/packages/schema-editor/src/components/SchemaInspector/restrictions/NumberRestrictions.test.tsx
+++ frontend/packages/schema-editor/src/components/SchemaInspector/restrictions/NumberRestrictions.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import { NumberRestrictions } from './NumberRestrictions';
 import { textMock } from '../../../../../../testing/mocks/i18nMock';
 import userEvent from '@testing-library/user-event';
+import { IntRestrictionKeys } from '@altinn/schema-model';
 
 const user = userEvent.setup();
 
@@ -85,5 +86,61 @@ describe('NumberRestrictions component', () => {
     const checkbox = screen.getAllByLabelText(textMock('schema_editor.format_date_inclusive'))[0];
     await act(() => user.click(checkbox));
     expect(onChangeRestrictions).toHaveBeenCalled();
+  });
+
+  it('Should call onChangeRestrictions with correct values when value is changed', async () => {
+    const onChangeRestrictions = jest.fn();
+    const props = {
+      restrictions: {},
+      path: '',
+      onChangeRestrictions,
+      onChangeRestrictionValue: jest.fn(),
+      isInteger: false,
+    };
+    render(<NumberRestrictions readonly={false} {...props} />);
+    const textBoxMinimum = screen.getByRole('textbox', {
+      name: textMock('schema_editor.minimum_inclusive'),
+    });
+    userEvent.type(textBoxMinimum, '1');
+    const expectedRestrictions = {
+      [IntRestrictionKeys.minimum]: 1,
+      [IntRestrictionKeys.exclusiveMinimum]: undefined,
+      [IntRestrictionKeys.maximum]: undefined,
+      [IntRestrictionKeys.exclusiveMaximum]: undefined,
+      [IntRestrictionKeys.multipleOf]: undefined,
+      [IntRestrictionKeys.integer]: undefined,
+    };
+
+    await waitFor(() =>
+      expect(onChangeRestrictions).toHaveBeenCalledWith('', expectedRestrictions)
+    );
+  });
+
+  it('Should call onChangeRestrictions with correct values when value is changed to 0', async () => {
+    const onChangeRestrictions = jest.fn();
+    const props = {
+      restrictions: {},
+      path: '',
+      onChangeRestrictions,
+      onChangeRestrictionValue: jest.fn(),
+      isInteger: false,
+    };
+    render(<NumberRestrictions readonly={false} {...props} />);
+    const textBoxMinimum = screen.getByRole('textbox', {
+      name: textMock('schema_editor.minimum_inclusive'),
+    });
+    userEvent.type(textBoxMinimum, '0');
+    const expectedRestrictions = {
+      [IntRestrictionKeys.minimum]: 0,
+      [IntRestrictionKeys.exclusiveMinimum]: undefined,
+      [IntRestrictionKeys.maximum]: undefined,
+      [IntRestrictionKeys.exclusiveMaximum]: undefined,
+      [IntRestrictionKeys.multipleOf]: undefined,
+      [IntRestrictionKeys.integer]: undefined,
+    };
+
+    await waitFor(() =>
+      expect(onChangeRestrictions).toHaveBeenCalledWith('', expectedRestrictions)
+    );
   });
 });
