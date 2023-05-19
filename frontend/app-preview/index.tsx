348f473cb9ebe64898a9255295c9d94637a92c87
andrealouisestanderen <andreastanderen@gmail.com>
2023-05-19 09:45:31 +0200

    Add imports

--- frontend/app-preview/index.tsx
+++ frontend/app-preview/index.tsx
@@ -6,6 +6,7 @@ import { PREVIEW_BASENAME } from 'app-shared/constants';
 import { PreviewConnectionContextProvider } from "app-shared/providers/PreviewConnectionContext";
 import { ServicesContextProvider } from './common/ServiceContext';
 import * as queries from "./queries/queries";
+import '@altinn/figma-design-tokens/dist/tokens.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
--- frontend/app-preview/src/views/LandingPage.tsx
+++ frontend/app-preview/src/views/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './LandingPage.module.css';
 import { PreviewContext } from '../PreviewContext';
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { usePreviewConnection } from "app-shared/providers/PreviewConnectionContext";
 import { useInstanceIdQuery } from '../../hooks/queries/useInstanceIdQuery';
 import AltinnStudioLogo from "app-shared/navigation/main-header/AltinnStudioLogo";
-import { Button, ButtonVariant } from '@digdir/design-system-react';
+import { Button, ButtonColor, ButtonVariant } from '@digdir/design-system-react';
 
 export const LandingPage = () => {
   const { org, app } = useParams();
@@ -15,6 +15,8 @@ export const LandingPage = () => {
   const previewConnection = usePreviewConnection();
   const { data: instanceId } = useInstanceIdQuery(org, app);
   const selectedLayoutInEditor = localStorage.getItem(instanceId);
+  const selectedViewSize = localStorage.getItem('viewSize');
+  const [viewSize, setViewSize] = useState<string>(selectedViewSize ?? 'desktop');
 
   const isIFrame = (input: HTMLElement | null): input is HTMLIFrameElement =>
     input !== null && input.tagName === 'IFRAME';
@@ -33,6 +35,13 @@ export const LandingPage = () => {
   }
 
   const handleChangeViewSizeClick = () => {
+    if (viewSize === 'desktop') {
+      localStorage.setItem('viewSize', 'mobile');
+      setViewSize('mobile');
+    } else {
+      localStorage.setItem('viewSize', 'desktop');
+      setViewSize('desktop');
+    }
   };
 
   return (
@@ -46,19 +55,22 @@ export const LandingPage = () => {
           </div>
         </div>
         <div className={classes.subHeader}>
-          <Button className={classes.viewSizeButtons} variant={ButtonVariant.Quiet} onClick={handleChangeViewSizeClick}>
+          <Button className={classes.desktopViewButton} style={viewSize === 'desktop' ? { color: 'black' } : {}} variant={viewSize === 'desktop' ? ButtonVariant.Filled : ButtonVariant.Outline} color={ButtonColor.Inverted} onClick={handleChangeViewSizeClick}>
             {t('preview.view_size_desktop')}
           </Button>
-          <Button className={classes.viewSizeButtons} variant={ButtonVariant.Quiet} onClick={handleChangeViewSizeClick}>
+          <Button className={classes.mobileViewButton} style={viewSize === 'mobile' ? { color: 'black' } : {}} variant={viewSize === 'mobile' ? ButtonVariant.Filled : ButtonVariant.Outline} color={ButtonColor.Inverted} onClick={handleChangeViewSizeClick}>
             {t('preview.view_size_mobile')}
           </Button>
         </div>
-        <iframe
-          title={t('preview.iframe_title')}
-          id='app-frontend-react-iframe'
-          src={`/designer/html/preview.html?${stringify({ org, app })}`}
-          className={classes.iframe}
-        ></iframe>
+        <div className={classes.iframeMobileViewContainer}>
+          <iframe
+            title={t('preview.iframe_title')}
+            id='app-frontend-react-iframe'
+            src={`/designer/html/preview.html?${stringify({ org, app })}`}
+            className={viewSize === 'desktop' ? classes.iframeDesktop : classes.iframeMobile}
+          ></iframe>
+          {viewSize === 'mobile' && <div className={classes.iframeMobileViewOverlay}></div>}
+        </div>
       </PreviewContext>
   );
 };
