96f912546c50045924e4423b77ad9cdc75f6ea89
andreastanderen <71079896+standeren@users.noreply.github.com>
2023-05-19 14:17:04 +0200

    10222 add multiple view sizes in preview (#10379)
    * Add subheader in preview with buttons for viewsizes

--- frontend/app-preview/src/PreviewApp.tsx
+++ frontend/app-preview/src/PreviewApp.tsx
@@ -3,6 +3,24 @@ import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './views/LandingPage';
 import { NoMatch } from './views/NoMatch';
 import classes from './PreviewApp.module.css';
+import { DEFAULT_LANGUAGE } from "app-shared/constants";
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import nb from '../../language/src/nb.json';
+import en from '../../language/src/en.json';
+
+i18next.use(initReactI18next).init({
+  lng: DEFAULT_LANGUAGE,
+  resources: {
+    nb: { translation: nb },
+    en: { translation: en },
+  },
+  fallbackLng: 'nb',
+  react: {
+    transSupportBasicHtmlNodes: true,
+    transKeepBasicHtmlNodesFor: ['em'],
+  },
+});
 
 export const PreviewApp = () => {
   return (
--- frontend/app-preview/src/views/LandingPage.tsx
+++ frontend/app-preview/src/views/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './LandingPage.module.css';
 import { PreviewContext } from '../PreviewContext';
 import { useParams } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { usePreviewConnection } from "app-shared/providers/PreviewConnectionContext";
 import { useInstanceIdQuery } from '../../hooks/queries/useInstanceIdQuery';
 import AltinnStudioLogo from "app-shared/navigation/main-header/AltinnStudioLogo";
+import { Button, ButtonColor, ButtonVariant } from '@digdir/design-system-react';
 
 export const LandingPage = () => {
   const { org, app } = useParams();
@@ -14,6 +15,8 @@ export const LandingPage = () => {
   const previewConnection = usePreviewConnection();
   const { data: instanceId } = useInstanceIdQuery(org, app);
   const selectedLayoutInEditor = localStorage.getItem(instanceId);
+  const selectedViewSize = localStorage.getItem('viewSize');
+  const [viewSize, setViewSize] = useState<string>(selectedViewSize ?? 'desktop');
 
   const isIFrame = (input: HTMLElement | null): input is HTMLIFrameElement =>
     input !== null && input.tagName === 'IFRAME';
@@ -31,6 +34,16 @@ export const LandingPage = () => {
     })
   }
 
+  const handleChangeViewSizeClick = () => {
+    if (viewSize === 'desktop') {
+      localStorage.setItem('viewSize', 'mobile');
+      setViewSize('mobile');
+    } else {
+      localStorage.setItem('viewSize', 'desktop');
+      setViewSize('desktop');
+    }
+  };
+
   return (
       <PreviewContext>
         <div className={classes.header}>
@@ -41,12 +54,23 @@ export const LandingPage = () => {
             {'BETA'}
           </div>
         </div>
-        <iframe
-          title={t('preview.iframe_title')}
-          id='app-frontend-react-iframe'
-          src={`/designer/html/preview.html?${stringify({ org, app })}`}
-          className={classes.iframe}
-        ></iframe>
+        <div className={classes.subHeader}>
+          <Button className={classes.desktopViewButton} style={viewSize === 'desktop' ? { color: 'black' } : {}} variant={viewSize === 'desktop' ? ButtonVariant.Filled : ButtonVariant.Outline} color={ButtonColor.Inverted} onClick={handleChangeViewSizeClick}>
+            {t('preview.view_size_desktop')}
+          </Button>
+          <Button className={classes.mobileViewButton} style={viewSize === 'mobile' ? { color: 'black' } : {}} variant={viewSize === 'mobile' ? ButtonVariant.Filled : ButtonVariant.Outline} color={ButtonColor.Inverted} onClick={handleChangeViewSizeClick}>
+            {t('preview.view_size_mobile')}
+          </Button>
+        </div>
+        <div className={classes.iframeMobileViewContainer}>
+          <iframe
+            title={t('preview.iframe_title')}
+            id='app-frontend-react-iframe'
+            src={`/designer/html/preview.html?${stringify({ org, app })}`}
+            className={viewSize === 'desktop' ? classes.iframeDesktop : classes.iframeMobile}
+          ></iframe>
+          {viewSize === 'mobile' && <div className={classes.iframeMobileViewOverlay}></div>}
+        </div>
       </PreviewContext>
   );
 };
