9d54363afc63ed1c6d08d08bfd4acd83a34a33e3
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-05-24 09:37:06 +0200

    used select-react

--- frontend/packages/ux-editor/src/components/config/editModal/EditCodeList.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditCodeList.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { TextField } from '@digdir/design-system-react';
 import { IGenericEditComponent } from '../componentConfig';
 import { useText } from '../../../hooks';
-import type { FormCheckboxesComponent, FormRadioButtonsComponent } from '../../../types/FormComponent';
+import type {
+  FormCheckboxesComponent,
+  FormRadioButtonsComponent,
+} from '../../../types/FormComponent';
+import Select from 'react-select';
 
 export enum SelectedOptionsType {
   Codelist = 'codelist',
@@ -12,7 +16,21 @@ export enum SelectedOptionsType {
 
 export function EditCodeList({ component, handleComponentChange }: IGenericEditComponent) {
   const t = useText();
+  const [selectedOption, setSelectedOption] = React.useState(null);
+  const [previousOptions, setPreviousOptions] = React.useState([]);
 
+  /* 
+  const handleOptionsIdChange = (selectedOption) => {
+  const optionsId = selectedOption.value;
+  setSelectedOption(selectedOption);
+  setPreviousOptions([...previousOptions, optionsId]);
+
+  handleComponentChange({
+    ...component,
+    optionsId: optionsId,
+  });
+};
+ */
   const handleOptionsIdChange = (e: any) => {
     handleComponentChange({
       ...component,
@@ -22,12 +40,22 @@ export function EditCodeList({ component, handleComponentChange }: IGenericEditC
 
   return (
     <div>
+      <div>
+        {t('ux_editor.modal_properties_code_list_id')}
+        <Select
+          id='modal-properties-code-list-id'
+          onChange={handleOptionsIdChange}
+          /*value={selectedOption}
+       options={previousOptions.map((option) => ({ value: option, label: option }))} */
+        />
+      </div>
+      {/* 
       <TextField
         id='modal-properties-code-list-id'
         label={t('ux_editor.modal_properties_code_list_id')}
         onChange={handleOptionsIdChange}
         value={(component as FormCheckboxesComponent | FormRadioButtonsComponent).optionsId || ''}
-      />
+      /> */}
       <p>
         <a
           target='_blank'
