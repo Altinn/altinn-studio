7d00923ce85f2aebbdcfd7643024939aeddb413d
andrealouisestanderen <andreastanderen@gmail.com>
2023-06-01 14:31:30 +0200

    Use groups concept in previewHub to only send to clients with same developer name

--- backend/src/Designer/Hubs/PreviewHub.cs
+++ backend/src/Designer/Hubs/PreviewHub.cs
@@ -1,6 +1,7 @@
-using System;
 using System.Threading.Tasks;
+using Altinn.Studio.Designer.Helpers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 
@@ -10,15 +11,26 @@ namespace Altinn.Studio.Designer.Hubs;
 public class PreviewHub : Hub
 {
     private readonly ILogger _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public PreviewHub(ILogger<PreviewHub> logger)
+    public PreviewHub(ILogger<PreviewHub> logger, IHttpContextAccessor httpContextAccessor)
     {
         _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        string developer = AuthenticationHelper.GetDeveloperUserName(_httpContextAccessor.HttpContext);
+        string connectionId = Context.ConnectionId;
+        Groups.AddToGroupAsync(connectionId, developer);
+        return base.OnConnectedAsync();
     }
 
     public async Task SendMessage(string message)
     {
+        string developer = AuthenticationHelper.GetDeveloperUserName(_httpContextAccessor.HttpContext);
         _logger.LogInformation("Message received from client: {MessageFromClient}", message);
-        await Clients.Others.SendAsync("ReceiveMessage", message);
+        await Clients.Group(developer).SendAsync("ReceiveMessage", message);
     }
 }
