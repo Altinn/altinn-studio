5b77060c183841ba934f04448aed5d3854b457d3
andrealouisestanderen <andreastanderen@gmail.com>
2023-06-02 09:03:33 +0200

    Add test

--- backend/tests/Designer.Tests/Controllers/TextController/UpdateTextsForKeys.cs
+++ backend/tests/Designer.Tests/Controllers/TextController/UpdateTextsForKeys.cs
@@ -38,9 +38,9 @@ namespace Designer.Tests.Controllers.TextController
             CreatedFolderPath = await TestDataHelper.CopyRepositoryForTest(org, app, developer, targetRepository);
 
             string file = TestDataHelper.GetFileFromRepo(org, targetRepository, developer, $"App/config/texts/resource.{lang}.json");
-            var expectedResource = JsonSerializer.Deserialize<Altinn.Studio.Designer.Models.TextResource>(file, _jsonOptions);
+            TextResource expectedResource = JsonSerializer.Deserialize<TextResource>(file, _jsonOptions);
 
-            PrepareExpectedResource(expectedResource, updateDictionary);
+            PrepareExpectedResourceWithoutVariables(expectedResource, updateDictionary);
 
             string url = $"{VersionPrefix(org, targetRepository)}/language/{lang}";
 
@@ -53,9 +53,30 @@ namespace Designer.Tests.Controllers.TextController
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             string actualContent = TestDataHelper.GetFileFromRepo(org, targetRepository, developer, $"App/config/texts/resource.{lang}.json");
             JsonUtils.DeepEquals(JsonSerializer.Serialize(expectedResource, _jsonOptions), actualContent).Should().BeTrue();
-
         }
-        private static void PrepareExpectedResource(TextResource resource, Dictionary<string, string> updateDictionary)
+
+        [Theory]
+        [MemberData(nameof(DataForTextWithVariables))]
+        public async Task UpdateTextsForKeys_ForTextsThatHaveVariables_MaintainsVariablesAndReturnsOk(string org, string app, string developer, string lang, Dictionary<string, string> updateDictionary)
+        {
+            string targetRepository = TestDataHelper.GenerateTestRepoName();
+            CreatedFolderPath = await TestDataHelper.CopyRepositoryForTest(org, app, developer, targetRepository);
+
+            string url = $"{VersionPrefix(org, targetRepository)}/language/{lang}";
+
+            using var httpContent = new StringContent(JsonSerializer.Serialize(updateDictionary), Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            // Act
+            using HttpResponseMessage response = await HttpClient.Value.PutAsync(url, httpContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            string actualContent = TestDataHelper.GetFileFromRepo(org, targetRepository, developer, $"App/config/texts/resource.{lang}.json");
+            TextResource actualResource = JsonSerializer.Deserialize<TextResource>(actualContent, _jsonOptions);
+            actualResource.Resources.Find(el => el.Id == "TextUsingVariables").Variables.Should().NotBeNull();
+        }
+
+        private static void PrepareExpectedResourceWithoutVariables(TextResource resource, Dictionary<string, string> updateDictionary)
         {
             foreach ((string key, string value) in updateDictionary)
             {
@@ -79,6 +100,15 @@ namespace Designer.Tests.Controllers.TextController
                 {
                     {"Epost", "new"},
                     {"nonExistingKey", "new value"}
+                }}
+            };
+
+        public static IEnumerable<object[]> DataForTextWithVariables =>
+            new List<object[]>
+            {
+                new object[] { "ttd", "hvem-er-hvem", "testUser", "nb", new Dictionary<string,string>()
+                {
+                    {"TextUsingVariables", "Dette er den nye teksten og variablene {0} og {1} har ikke blitt borte eller endret"},
                 }}
             };
     }
