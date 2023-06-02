410eec1e4d05cf7aef7e4bbfb727cd2437124827
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-06-02 13:31:46 +0200

    Add tests covering mime type validator.

---
+++ test/Altinn.FileAnalyzers.Tests/MimeType/MimeTypeAnalyserTests.cs
@@ -0,0 +1,38 @@
+﻿using Altinn.App.Core.Features.FileAnalysis;
+using Altinn.Codelists.Tests;
+using Altinn.FileAnalyzers.MimeType;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Altinn.FileAnalyzers.Tests.MimeType
+{
+    public class MimeTypeAnalyserTests
+    {
+        [Fact]
+        public async Task Analyse_ValidPdf_ShouldReturnCorrectMimeType()
+        {
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            MimeTypeAnalyser mimeTypeAnalyser = new MimeTypeAnalyser(httpContextAccessorMock.Object);
+            var stream = EmbeddedResource.LoadDataAsStream("Altinn.FileAnalyzers.Tests.MimeType.example.pdf");
+
+            FileAnalysisResult analysisResult = await mimeTypeAnalyser.Analyse(stream);
+
+            analysisResult.MimeType.Should().Be("application/pdf");
+        }
+
+        // Test that the analyser fails on a file that is not a PDF but a jpg
+        [Fact]
+        public async Task Analyse_InvalidPdf_ShouldReturnCorrectMimeType()
+        {
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            MimeTypeAnalyser mimeTypeAnalyser = new MimeTypeAnalyser(httpContextAccessorMock.Object);
+            var stream = EmbeddedResource.LoadDataAsStream("Altinn.FileAnalyzers.Tests.MimeType.example.jpg.pdf");
+            FileAnalysisResult analysisResult = await mimeTypeAnalyser.Analyse(stream);
+            analysisResult.MimeType.Should().Be("image/jpeg");
+        }
+
+
+
+    }
+}
--- test/Altinn.FileAnalyzers.Tests/MimeType/MimeTypeValidatorTests.cs
+++ test/Altinn.FileAnalyzers.Tests/MimeType/MimeTypeValidatorTests.cs
@@ -4,62 +4,61 @@ using Altinn.FileAnalyzers.MimeType;
 using Altinn.Platform.Storage.Interface.Models;
 using FluentAssertions;
 
-namespace Altinn.FileAnalyzers.Tests.MimeType
+namespace Altinn.FileAnalyzers.Tests.MimeType;
+
+public class MimeTypeValidatorTests
 {
-    public class MimeTypeValidatorTests
-    {
-        [Fact]
-        public async Task Validate_MimeTypeCorrect_ShouldValidateOk()
-        {   // Simulate config in applicationMetadata.json
-            var dataType = new DataType()
+    [Fact]
+    public async Task Validate_MimeTypeCorrect_ShouldValidateOk()
+    {   // Simulate config in applicationMetadata.json
+        var dataType = new DataType()
+        {
+            EnabledFileValidators = new List<string>() { "mimeTypeValidator" },
+            AllowedContentTypes = new List<string>() { "application/pdf" }
+        };
+
+        // Simulate file analysis result
+        IEnumerable<FileAnalysisResult> fileAnalysisResults = new List<FileAnalysisResult>()
+        {
+            new FileAnalysisResult("mimeTypeValidator")
             {
-                EnabledFileValidators = new List<string>() { "mimeTypeValidator" },
-                AllowedContentTypes = new List<string>() { "application/pdf" }
-            };
+                MimeType = "application/pdf",
+                Filename = "test.pdf",
+                Extensions = new List<string>() { "pdf" }
+            }
+        };
 
-            // Simulate file analysis result
-            IEnumerable<FileAnalysisResult> fileAnalysisResults = new List<FileAnalysisResult>()
+        var validator = new MimeTypeValidator();
+        (bool success, IEnumerable<ValidationIssue> errors) = await validator.Validate(dataType, fileAnalysisResults);
+
+        success.Should().BeTrue();
+        errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Validate_MimeTypeCorrect_ShouldReturnWithError()
+    {   // Simulate config in applicationMetadata.json
+        var dataType = new DataType()
+        {
+            EnabledFileValidators = new List<string>() { "mimeTypeValidator" },
+            AllowedContentTypes = new List<string>() { "application/pdf" }
+        };
+
+        // Simulate file analysis result
+        IEnumerable<FileAnalysisResult> fileAnalysisResults = new List<FileAnalysisResult>()
+        {
+            new FileAnalysisResult("mimeTypeValidator")
             {
-                new FileAnalysisResult("mimeTypeValidator")
-                {
-                    MimeType = "application/pdf",
-                    Filename = "test.pdf",
-                    Extensions = new List<string>() { "pdf" }
-                }
-            };
+                MimeType = "application/pdfx",
+                Filename = "test.png",
+                Extensions = new List<string>() { "png" }
+            }
+        };
 
-            var validator = new MimeTypeValidator();
-            (bool success, IEnumerable<ValidationIssue> errors) = await validator.Validate(dataType, fileAnalysisResults);
+        var validator = new MimeTypeValidator();
+        (bool success, IEnumerable<ValidationIssue> errors) = await validator.Validate(dataType, fileAnalysisResults);
 
-            success.Should().BeTrue();
-            errors.Should().BeEmpty();
-        }
-
-        [Fact]
-        public async Task Validate_MimeTypeCorrect_ShouldReturnWithError()
-        {   // Simulate config in applicationMetadata.json
-            var dataType = new DataType()
-            {
-                EnabledFileValidators = new List<string>() { "mimeTypeValidator" },
-                AllowedContentTypes = new List<string>() { "application/pdf" }
-            };
-
-            // Simulate file analysis result
-            IEnumerable<FileAnalysisResult> fileAnalysisResults = new List<FileAnalysisResult>()
-            {
-                new FileAnalysisResult("mimeTypeValidator")
-                {
-                    MimeType = "application/pdfx",
-                    Filename = "test.png",
-                    Extensions = new List<string>() { "png" }
-                }
-            };
-
-            var validator = new MimeTypeValidator();
-            (bool success, IEnumerable<ValidationIssue> errors) = await validator.Validate(dataType, fileAnalysisResults);
-
-            success.Should().BeFalse();
-            errors.FirstOrDefault()?.Code.Should().Be(ValidationIssueCodes.DataElementCodes.ContentTypeNotAllowed);
-        }
+        success.Should().BeFalse();
+        errors.FirstOrDefault()?.Code.Should().Be(ValidationIssueCodes.DataElementCodes.ContentTypeNotAllowed);
     }
 }
