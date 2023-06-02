1345db518ae3aeb4cc73bda3bd16a5c3738f1642
andreastanderen <71079896+standeren@users.noreply.github.com>
2023-06-02 12:48:59 +0200

    Use groups concept in previewHub to only send to clients with same developer name (#10446)
    * Use groups concept in previewHub to only send to clients with same developer name
    
    * Fix tests

--- backend/tests/Designer.Tests/Hubs/PreviewHubTests.cs
+++ backend/tests/Designer.Tests/Hubs/PreviewHubTests.cs
@@ -1,6 +1,9 @@
-using System.Threading;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Hubs;
+using AltinnCore.Authentication.Constants;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -11,10 +14,14 @@ namespace Designer.Tests.Hubs;
 public class PreviewHubTests
 {
     private readonly Mock<ILogger<PreviewHub>> _logger;
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessor;
+    private const string TestUser = "testUser";
 
     public PreviewHubTests()
     {
         _logger = new Mock<ILogger<PreviewHub>>();
+        _httpContextAccessor = new Mock<IHttpContextAccessor>();
+        _httpContextAccessor.Setup(req => req.HttpContext).Returns(GetHttpContextForTestUser(TestUser));
     }
 
     [Fact]
@@ -24,14 +31,50 @@ public class PreviewHubTests
         Mock<IHubCallerClients> mockClients = new();
         Mock<IClientProxy> mockClientProxy = new();
 
-        mockClients.Setup(clients => clients.Others).Returns(mockClientProxy.Object);
+        mockClients.Setup(clients => clients.Group(TestUser)).Returns(mockClientProxy.Object);
 
-        PreviewHub previewHubMock = new(_logger.Object) { Clients = mockClients.Object };
+        PreviewHub previewHubMock = new(_logger.Object, _httpContextAccessor.Object) { Clients = mockClients.Object };
 
         // act
         await previewHubMock.SendMessage("testMessage");
 
         // assert
-        mockClients.Verify(clients => clients.Others, Times.Once);
+        mockClients.Verify(clients => clients.Group(TestUser), Times.Once);
+    }
+
+    [Fact]
+    public async Task SignalR_ClientWithDifferentDeveloperName_ShouldNotReceiveMessage()
+    {
+        // arrange
+        Mock<IHubCallerClients> mockClients = new();
+        Mock<IClientProxy> mockClientProxy = new();
+
+        mockClients.Setup(clients => clients.Group(TestUser)).Returns(mockClientProxy.Object);
+
+        PreviewHub previewHubMock = new(_logger.Object, _httpContextAccessor.Object) { Clients = mockClients.Object };
+
+        // act
+        await previewHubMock.SendMessage("testMessage");
+
+        // assert
+        mockClients.Verify(clients => clients.Group("AnotherDeveloperName"), Times.Never);
+    }
+
+    private static HttpContext GetHttpContextForTestUser(string userName)
+    {
+        List<Claim> claims = new()
+        {
+            new Claim(AltinnCoreClaimTypes.Developer, userName, ClaimValueTypes.String, "altinn.no")
+        };
+        ClaimsIdentity identity = new("TestUserLogin");
+        identity.AddClaims(claims);
+
+        ClaimsPrincipal principal = new(identity);
+        HttpContext c = new DefaultHttpContext();
+        c.Request.HttpContext.User = principal;
+        c.Request.RouteValues.Add("org", "ttd");
+        c.Request.RouteValues.Add("app", "apps-test-tba");
+
+        return c;
     }
 }
