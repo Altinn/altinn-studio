dd1665513b7d5692fbecb5168a5f65c66889be0b
Mirko Sekulic <misha.sekulic@gmail.com>
2023-06-07 09:11:38 +0200

    add test for reset endpoint

---
+++ backend/tests/Designer.Tests/Controllers/RepositoryController/ResetLocalRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Configuration;
+using Altinn.Studio.Designer.Services.Interfaces;
+using Designer.Tests.Mocks;
+using Designer.Tests.Utils;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Designer.Tests.Controllers.RepositoryController
+{
+    public class ResetLocalRepositoryTests : RepositoryControllerTestsBase<ResetLocalRepositoryTests>
+    {
+        public ResetLocalRepositoryTests(WebApplicationFactory<Altinn.Studio.Designer.Controllers.RepositoryController> factory) : base(factory)
+        {
+        }
+
+        // Do not use mocked repository
+        protected override void ConfigureTestServices(IServiceCollection services)
+        {
+            services.Configure<ServiceRepositorySettings>(c =>
+                c.RepositoryLocation = TestRepositoriesLocation);
+            services.AddSingleton<IGitea, IGiteaMock>();
+            services.AddTransient<ISourceControl, ISourceControlMock>();
+        }
+
+        [Theory]
+        [InlineData("ttd", "apps-test", "testUser")]
+        public async Task ResetRepo_Returns200(string org, string repo, string developer)
+        {
+            string targetRepository = TestDataHelper.GenerateTestRepoName();
+            CreatedFolderPath =  await TestDataHelper.CopyRepositoryForTest(org, repo, developer, targetRepository);
+            CreatedSecondaryFolderPath = await TestDataHelper.CopyRemoteRepositoryForTest(org, repo, targetRepository);
+
+            // Arrange
+            string uri = $"{VersionPrefix}/repo/{org}/{targetRepository}/reset";
+
+            // Act
+            using HttpResponseMessage res = await HttpClient.Value.GetAsync(uri);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, res.StatusCode);
+        }
+    }
+}
