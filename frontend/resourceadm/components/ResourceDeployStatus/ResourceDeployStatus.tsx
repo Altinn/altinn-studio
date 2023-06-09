19eacd68eff6621efaf0a35400ad8958e4b4407e
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-06-09 13:13:21 +0200

    Adding alert cards

--- frontend/resourceadm/components/ResourceDeployStatus/ResourceDeployStatus.tsx
+++ frontend/resourceadm/components/ResourceDeployStatus/ResourceDeployStatus.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './ResourceDeployStatus.module.css';
+import { XMarkOctagonFillIcon, CheckmarkCircleFillIcon } from '@navikt/aksel-icons';
 // import { Alert } from '@digdir/design-system-react';
 
 interface Props {
@@ -8,5 +9,21 @@ interface Props {
 }
 
 export const ResourceDeployStatus = ({ type, message }: Props) => {
-  return <div></div>; // <Alert></Alert>;
+  const displayIcon = () => {
+    if (type === 'success') {
+      return (
+        <CheckmarkCircleFillIcon title='Suksess, ingen feil' className={classes.successIcon} />
+      );
+    }
+    if (type === 'alert') {
+      return <XMarkOctagonFillIcon title='Error, valideringsfeil' className={classes.alertIcon} />;
+    }
+    return null;
+  };
+  return (
+    <div className={`${classes[type]} ${classes.card}`}>
+      {displayIcon()}
+      <p>{message}</p>
+    </div>
+  );
 };
--- frontend/resourceadm/pages/DeployResourcePage/DeployResourcePage.tsx
+++ frontend/resourceadm/pages/DeployResourcePage/DeployResourcePage.tsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import classes from './DeployResourcePage.module.css';
+import { ResourceDeployStatus } from 'resourceadm/components/ResourceDeployStatus';
+import { useOnce } from 'resourceadm/hooks/useOnce';
 
 export const DeployResourcePage = () => {
   // To manage state for when the page is loading if it is valid
   const [isValidatingResource, setIsValidatingResource] = useState(false);
 
   // To manage state for if the resource is valid or not
-  const [resourceIsValid, setResourceIsValid] = useState(false);
+  const [resourceIsValid, setResourceIsValid] = useState(true);
+
+  useOnce(() => {
+    // TODO - API call to backend to get validation
+  });
 
   return (
     <div className={classes.deployPageWrapper}>
       <h1 className={classes.pageHeader}>Publiser ressurs</h1>
-      <div>TODO - Valideringsstatus</div>
+      <ResourceDeployStatus
+        type={resourceIsValid ? 'success' : 'alert'}
+        message={
+          resourceIsValid
+            ? 'Ingen feil i ressursen. Du kan publisere den.'
+            : 'Det er feil i ressursen - TODO - LIST ALL'
+        }
+      />
       <div>
         <h2 className={classes.subHeader}>Velg miljø å publisere i</h2>
         <div>TODO - Test miljø</div>
