f97589feca3f36cfa38a3b3fa0c816d1c2e891d2
Mirko Sekulic <misha.sekulic@gmail.com>
2023-06-12 09:15:01 +0200

    disable parallelization on repository persistance endpoints

--- backend/src/Designer/Controllers/RepositoryController.cs
+++ backend/src/Designer/Controllers/RepositoryController.cs
@@ -264,7 +264,18 @@ namespace Altinn.Studio.Designer.Controllers
         [Route("repo/{org}/{repository:regex(^(?!datamodels$)[[a-z]][[a-z0-9-]]{{1,28}}[[a-z0-9]]$)}/commit-and-push")]
         public void CommitAndPushRepo([FromBody] CommitInfo commitInfo)
         {
-            _sourceControl.PushChangesForRepository(commitInfo);
+            string developer = AuthenticationHelper.GetDeveloperUserName(HttpContext);
+            SemaphoreSlim semaphore = _userRequestsSynchronizationService.GetRequestsSemaphore(commitInfo.Org, commitInfo.Repository, developer);
+            semaphore.Wait();
+            try
+            {
+                _sourceControl.PushChangesForRepository(commitInfo);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
         }
 
         /// <summary>
@@ -276,6 +287,9 @@ namespace Altinn.Studio.Designer.Controllers
         [Route("repo/{org}/{repository:regex(^(?!datamodels$)[[a-z]][[a-z0-9-]]{{1,28}}[[a-z0-9]]$)}/commit")]
         public ActionResult Commit([FromBody] CommitInfo commitInfo)
         {
+            string developer = AuthenticationHelper.GetDeveloperUserName(HttpContext);
+            SemaphoreSlim semaphore = _userRequestsSynchronizationService.GetRequestsSemaphore(commitInfo.Org, commitInfo.Repository, developer);
+            semaphore.Wait();
             try
             {
                 _sourceControl.Commit(commitInfo);
@@ -284,6 +298,10 @@ namespace Altinn.Studio.Designer.Controllers
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            finally
+            {
+                semaphore.Release();
             }
         }
 
@@ -296,14 +314,17 @@ namespace Altinn.Studio.Designer.Controllers
         [Route("repo/{org}/{repository:regex(^(?!datamodels$)[[a-z]][[a-z0-9-]]{{1,28}}[[a-z0-9]]$)}/push")]
         public async Task<ActionResult> Push(string org, string repository)
         {
-            bool pushSuccess = await _sourceControl.Push(org, repository);
-            if (pushSuccess)
+            string developer = AuthenticationHelper.GetDeveloperUserName(HttpContext);
+            SemaphoreSlim semaphore = _userRequestsSynchronizationService.GetRequestsSemaphore(org, repository, developer);
+            await semaphore.WaitAsync();
+            try
             {
-                return Ok();
+                bool pushSuccess = await _sourceControl.Push(org, repository);
+                return pushSuccess ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
             }
-            else
+            finally
             {
-                return StatusCode(StatusCodes.Status500InternalServerError);
+                semaphore.Release();
             }
         }
 
