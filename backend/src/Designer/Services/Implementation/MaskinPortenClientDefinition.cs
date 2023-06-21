02ed18356826090ee4fa5ed383cbef4f3a0b203f
Sondre Bråten <sondre.braten@avanade.com>
2023-06-21 11:17:38 +0200

    Removed some uncommented lines and unused usings.

--- backend/src/Designer/Services/Implementation/MaskinPortenClientDefinition.cs
+++ backend/src/Designer/Services/Implementation/MaskinPortenClientDefinition.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text;
 using System;
 using System.Threading.Tasks;
-using Altinn.ApiClients.Maskinporten.Config;
 using Altinn.ApiClients.Maskinporten.Interfaces;
 using Altinn.ApiClients.Maskinporten.Models;
 using Microsoft.Extensions.Options;
@@ -16,21 +15,16 @@ namespace Altinn.Studio.Designer.Services.Implementation
         private SecretClient _secretClient;
 
         public IMaskinportenSettings ClientSettings { get; set; }
-        //public MaskinportenClientSettings ClientSettings { get; set; }
 
         public MaskinPortenClientDefinition(IOptions<MaskinportenClientSettings> clientSettings)
         {
             ClientSettings = clientSettings.Value;
-            //_secrets = secrets;
         }
 
         public async Task<ClientSecrets> GetClientSecrets()
         {
             ClientSecrets clientSecrets = new ClientSecrets();
 
-            // Depending on runtime mode the secrets service will get data from keyvault or secrets.json when running locally
-            // https://docs.altinn.studio/app/development/configuration/secrets/
-            //string base64encodedJWK = await _secrets.GetSecretAsync("maskinportenJwk");
             byte[] base64EncodedBytes = Convert.FromBase64String(ClientSettings.EncodedJwk);
             string jwkjson = Encoding.UTF8.GetString(base64EncodedBytes);
             clientSecrets.ClientKey = new Microsoft.IdentityModel.Tokens.JsonWebKey(jwkjson);
