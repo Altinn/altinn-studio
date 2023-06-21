39c5050bcb4f2333ee20cae72138768b5cff8fb9
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-06-21 16:48:23 +0200

    Fixing hack so that master does not break

--- frontend/resourceadm/app/App.tsx
+++ frontend/resourceadm/app/App.tsx
@@ -12,6 +12,7 @@ import { ResourcePage } from 'resourceadm/pages/ResourcePage';
 import { ResourceDashboardPage } from 'resourceadm/pages/ResourceDashboardPage';
 import { ErrorPage } from '../pages/ErrorPage';
 import { RedirectPage } from '../pages/RedirectPage';
+import { SearchField } from '@altinn/altinn-design-system';
 
 export const App = (): JSX.Element => {
   const { t } = useTranslation();
@@ -54,28 +55,21 @@ export const App = (): JSX.Element => {
   if (componentIsReady) {
     return (
       <div className={classes.root}>
+        {/*
+            This is a "hack" to make sure that the resourceadm doesnt break. We do not
+            use any other dependencies to the old altin-design-system (which for some reason
+            is needed). By hiding the component it can not be seen by the user in the browser.
+        */}
+        <div style={{ display: 'none' }}>
+          <SearchField id='hack' label='hack' />
+        </div>
         <Routes>
           <Route element={<PageLayout />}>
-            <Route
-              path={basePath}
-              element={<ResourceDashboardPage />}
-            />
-            <Route
-              path={`${basePath}/resource/:resourceId/:pageType`}
-              element={<ResourcePage />}
-            />
-            <Route
-              path='/'
-              element={<ErrorPage />}
-            />
-            <Route
-              path='/:selectedContext'
-              element={<RedirectPage />}
-            />
-            <Route
-              path='/:selectedContext/:repo/*'
-              element={<ErrorPage />}
-            />
+            <Route path={basePath} element={<ResourceDashboardPage />} />
+            <Route path={`${basePath}/resource/:resourceId/:pageType`} element={<ResourcePage />} />
+            <Route path='/' element={<ErrorPage />} />
+            <Route path='/:selectedContext' element={<RedirectPage />} />
+            <Route path='/:selectedContext/:repo/*' element={<ErrorPage />} />
           </Route>
         </Routes>
       </div>
