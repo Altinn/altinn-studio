61e51ee36462fec516fc6f68bfd21da670c93824
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-06-27 14:38:20 +0200

    Updating style on the deploy page

--- frontend/resourceadm/pages/DeployResourcePage/DeployResourcePage.tsx
+++ frontend/resourceadm/pages/DeployResourcePage/DeployResourcePage.tsx
@@ -3,7 +3,7 @@ import classes from './DeployResourcePage.module.css';
 import { ResourceDeployStatus } from 'resourceadm/components/ResourceDeployStatus';
 import { ResourceDeployEnvCard } from 'resourceadm/components/ResourceDeployEnvCard';
 import { useOnce } from 'resourceadm/hooks/useOnce';
-import { TextField, Button } from '@digdir/design-system-react';
+import { TextField, Button, Spinner } from '@digdir/design-system-react';
 import { get } from 'app-shared/utils/networking';
 import { getValidatePolicyUrlBySelectedContextRepoAndId } from 'resourceadm/utils/backendUrlUtils';
 import { useParams } from 'react-router-dom';
@@ -11,6 +11,7 @@ import { NavigationBarPageType } from 'resourceadm/types/global';
 import { useRepoStatusQuery } from 'resourceadm/hooks/queries';
 import { Link } from 'resourceadm/components/Link';
 import { UploadIcon } from '@navikt/aksel-icons';
+import { ScreenReaderSpan } from 'resourceadm/components/ScreenReaderSpan';
 
 interface Props {
   navigateToPage: (page: NavigationBarPageType) => void;
@@ -25,13 +26,9 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
   const { selectedContext, resourceId } = useParams();
   const repo = `${selectedContext}-resources`;
 
-  // To manage state for if the resource is valid or not
-  const [resourceIsValid, setResourceIsValid] = useState(false);
-
-  const [currentEnvVersionTest, setCurrentEnvVersionTest] = useState('1');
-  const [currentEnvVersionProd, setCurrentEnvVersionProd] = useState('1');
-
   const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
   const [isLocalRepoInSync, setIsLocalRepoInSync] = useState(false);
 
   // TODO - Find out how the error response looks like
@@ -44,11 +41,11 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
   // from backend is different than what is in test/prod, then it is valid.
   // Do not compare it with the text directly, it must first be saved.
   // TODO - ADD information box.
-  const [newVersionText, setNewVersionText] = useState('1');
+  const [newVersionText, setNewVersionText] = useState('');
 
-  const versionInTest = 2;
-  const versionInProd = 1;
-  const versionInGitea = 2;
+  const [versionInTest, setVersionInTest] = useState<number>();
+  const [versionInProd, setVersionInProd] = useState<number>();
+  const [versionInGitea, setVersionInGitea] = useState<number>();
 
   useOnce(() => {
     get(getValidatePolicyUrlBySelectedContextRepoAndId(selectedContext, repo, resourceId))
@@ -56,7 +53,13 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
         // Remove error if status is 200
         res.status === '200' && setHasPolicyError(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.error(err);
+      });
+
+    setIsLoading(false);
+    setHasError(false);
+    setHasResourceError(true);
 
     /*get(getValidateResourceUrlBySelectedContextRepoAndId(selectedContext, repo, resourceId))
       .then((res) => {
@@ -65,14 +68,13 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
       })
       .catch((err) => console.log(err));
       */
-    // setHasResourceError(false);
-
-    // TODO - Get errors in resource
-
     // TODO - replace with API call
-    setResourceIsValid(true);
-    setCurrentEnvVersionTest('2');
-    setCurrentEnvVersionProd('1');
+    setVersionInGitea(2);
+    setVersionInTest(2);
+    setVersionInProd(1);
+
+    // TODO - Replace with API call - If version exists, set it
+    setNewVersionText('2');
   });
 
   useEffect(() => {
@@ -144,18 +146,6 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
   };
 
   /**
-   * Checks that the version number entered contains only numbers
-   *
-   * @param value the string to check
-   *
-   * @returns a boolean for if it has only numbers
-   */
-  const hasOnlyNumbers = (value: string): boolean => {
-    const regex = /^[0-9]+$/;
-    return regex.test(value);
-  };
-
-  /**
    * Checks if deploy is possible for the given type
    *
    * @param type the environment, test or prod
@@ -184,53 +174,70 @@ export const DeployResourcePage = ({ navigateToPage }: Props) => {
     return false;
   };
 
-  return (
-    <div className={classes.deployPageWrapper}>
-      <h1 className={classes.pageHeader}>Publiser ressurs</h1>
-      <h2 className={classes.subHeader}>Status</h2>
-      {displayStatusCard()}
-      <div>
-        <div className={classes.newVersionWrapper}>
-          <h2 className={classes.subHeader}>Nytt versjonsnummer</h2>
-          <p className={classes.text}>Sett et versjonsnummer for endringene du har gjort</p>
-          <div className={classes.textAndButton}>
-            <div className={classes.textfield}>
-              <TextField
-                placeholder='1' // TODO
-                value={newVersionText}
-                onChange={(e) => setNewVersionText(e.target.value)}
-                isValid={hasOnlyNumbers(newVersionText)} // TODO - only numbers??
-              />
+  const displayContent = () => {
+    if (isLoading) {
+      return (
+        <div className={classes.spinnerWrapper}>
+          <Spinner size='3xLarge' variant='interaction' title='Laster inn policy' />
+        </div>
+      );
+    }
+    // TODO error handling
+    if (hasError) {
+      return <p>Beklager, det skjedde en feil under innhenting av innholdet</p>;
+    }
+    return (
+      <>
+        <h1 className={classes.pageHeader}>Publiser ressurs</h1>
+        <div className={classes.contentWrapper}>
+          <h2 className={classes.subHeader}>Status</h2>
+          {displayStatusCard()}
+          <div className={classes.newVersionWrapper}>
+            <h2 className={classes.subHeader}>Nytt versjonsnummer</h2>
+            <p className={classes.text}>Sett et versjonsnummer for endringene du har gjort</p>
+            <div className={classes.textAndButton}>
+              <div className={classes.textfield}>
+                <TextField
+                  placeholder='' // TODO
+                  value={newVersionText}
+                  onChange={(e) => setNewVersionText(e.target.value)}
+                  // isValid={hasOnlyNumbers(newVersionText)} // TODO - only numbers??
+                  aria-labelledby='versionnumber-field'
+                />
+              </div>
+              <ScreenReaderSpan id='versionnumber-field' label='Nytt versjonssnummer' />
+              <Button
+                color='primary'
+                onClick={() => {
+                  // TODO - Save new version number
+                  alert('todo - Save new version number');
+                }}
+                iconPlacement='left'
+                icon={<UploadIcon title='Lagre versjonsnummer' />}
+              >
+                Lagre versjonsnummer
+              </Button>
             </div>
-            <Button
-              color='primary'
-              onClick={() => {
-                // TODO - Push to git
-                alert('todo - push to git');
-              }}
-              iconPlacement='left'
-              icon={<UploadIcon title='Last opp dine endringer' />}
-            >
-              Last opp dine endringer
-            </Button>
+          </div>
+          <h2 className={classes.subHeader}>Velg miljø å publisere i</h2>
+          <div className={classes.deployCardsWrapper}>
+            <ResourceDeployEnvCard
+              isDeployPossible={isDeployPossible('test')}
+              envName='Testmiljø TT-02' //'tt02-miljøet'
+              currentEnvVersion={versionInTest}
+              newEnvVersion={versionInGitea !== versionInTest ? versionInGitea : undefined}
+            />
+            <ResourceDeployEnvCard
+              isDeployPossible={isDeployPossible('prod')}
+              envName='Produksjonsmiljø' //'production-miljøet'
+              currentEnvVersion={versionInProd}
+              newEnvVersion={versionInGitea !== versionInProd ? versionInGitea : undefined}
+            />
           </div>
         </div>
-        <h2 className={classes.subHeader}>Velg miljø å publisere i</h2>
-        <div className={classes.deployCardsWrapper}>
-          <ResourceDeployEnvCard
-            isDeployPossible={isDeployPossible('test')}
-            envName='Testmiljø TT-02' //'tt02-miljøet'
-            currentEnvVersion={versionInTest}
-            newEnvVersion={versionInGitea !== versionInTest ? versionInGitea : undefined}
-          />
-          <ResourceDeployEnvCard
-            isDeployPossible={isDeployPossible('prod')}
-            envName='Produksjonsmiljø' //'production-miljøet'
-            currentEnvVersion={versionInProd}
-            newEnvVersion={versionInGitea !== versionInProd ? versionInGitea : undefined}
-          />
-        </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  };
+
+  return <div className={classes.deployPageWrapper}>{displayContent()}</div>;
 };
