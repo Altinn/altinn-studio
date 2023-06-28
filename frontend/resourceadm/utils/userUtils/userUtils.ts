ec81e5a66b8dbf7c0e6704a85f7a087e4e18c462
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-06-28 13:29:46 +0200

    Adding userUtils again

---
+++ frontend/resourceadm/utils/userUtils/userUtils.ts
@@ -0,0 +1,16 @@
+import { SelectedContextType } from 'app-shared/navigation/main-header/Header';
+import { Organization } from 'app-shared/types/Organization';
+
+export const userHasAccessToSelectedContext = ({
+  selectedContext,
+  orgs,
+}: {
+  selectedContext: string | SelectedContextType;
+  orgs: Organization[];
+}): boolean => {
+  if (selectedContext == SelectedContextType.Self || selectedContext == SelectedContextType.All) {
+    return true;
+  }
+
+  return Boolean(orgs.find((org) => org.username === selectedContext));
+};
