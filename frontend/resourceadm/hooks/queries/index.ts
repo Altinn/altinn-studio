f95751e7b3e4a8a64d96c8478518a8570d2625e4
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-06-28 13:27:18 +0200

    Deleting more unused code

--- frontend/resourceadm/hooks/queries/index.ts
+++ frontend/resourceadm/hooks/queries/index.ts
@@ -1,6 +1,4 @@
 export { useOrganizationsQuery } from './useOrganizationsQuery';
-export { useSearchReposQuery } from './useSearchReposQuery';
-export { useStarredReposQuery } from './useStarredReposQuery';
 export { useRepoMetadataQuery } from './useRepoMetadataQuery';
 export { useRepoPullQuery } from './useRepoPullQuery';
 export { useRepoStatusQuery } from './useRepoStatusQuery';
--- frontend/resourceadm/utils/userUtils/index.ts
+++
@@ -1,1 +0,0 @@
-export { userHasAccessToSelectedContext } from './userUtils';
