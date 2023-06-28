e5b084cac8e662c7654bb440987f593617918b1e
Nina Kylstad <nkylstad@gmail.com>
2023-06-28 10:47:28 +0200

    add unit test for useRepoCommitAndPushMutation

--- frontend/app-development/hooks/mutations/useRepoCommitAndPushMutation.ts
+++ frontend/app-development/hooks/mutations/useRepoCommitAndPushMutation.ts
@@ -16,9 +16,5 @@ export const useRepoCommitAndPushMutation = (owner, app) => {
       q.invalidateQueries({ queryKey: [QueryKey.RepoStatus, owner, app] }).then();
       q.invalidateQueries({ queryKey: [QueryKey.BranchStatus, owner, app, 'master'] }).then();
     },
-    onError: (error) => {
-      q.invalidateQueries({ queryKey: [QueryKey.RepoStatus, owner, app] }).then();
-      q.invalidateQueries({ queryKey: [QueryKey.BranchStatus, owner, app, 'master'] }).then();
-    },
   });
 };
