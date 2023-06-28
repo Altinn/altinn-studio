c8dafad44bd67f8d3098593bc3dfdfbcc2c67dc6
Sondre Bråten <sondre.braten@avanade.com>
2023-06-28 13:36:19 +0200

    Removed logic that is no longer in use after being moved out to the controller.

--- backend/src/Designer/Helpers/ResourceAdminHelper.cs
+++ backend/src/Designer/Helpers/ResourceAdminHelper.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Altinn.Studio.Designer.Models;
-using Altinn.Studio.PolicyAdmin.Models;
-using IdentityModel.OidcClient;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
 namespace Altinn.Studio.Designer.Helpers
 {
@@ -17,53 +9,6 @@ namespace Altinn.Studio.Designer.Helpers
         {
             ListviewServiceResource simplifiedResource = new ListviewServiceResource { Identifier = resource.Identifier, Title = resource.Title };
             return simplifiedResource;
-        }
-
-        public static string ValidateServiceResource(ServiceResource resourceToValidate, bool strictMode = false)
-        {
-            List<string> missingResourceAttributes = new List<string>();
-
-            if (!ValidDictionaryAttribute(resourceToValidate.Title))
-            {
-                missingResourceAttributes.Add("Title");
-            }
-
-            if (!ValidDictionaryAttribute(resourceToValidate.Description))
-            {
-                missingResourceAttributes.Add("Description");
-            }
-
-            if (resourceToValidate.ResourceType == null)
-            {
-                missingResourceAttributes.Add("ResourceType");
-            }
-
-            if (strictMode && (resourceToValidate.ThematicArea == null || string.IsNullOrEmpty(resourceToValidate.ThematicArea)))
-            {
-                missingResourceAttributes.Add("ThematicArea");
-            }
-
-            if (missingResourceAttributes.Count > 0)
-            {
-                return $"Validation of resource failed because of missing attribute(s)";
-            }
-            else
-            {
-                return $"Validation of resource completed. Resource is valid";
-            }
-        }
-
-        public static bool ValidationProblemDetailsListHasNoErrors(List<ValidationProblemDetails> validationProblemDetailsList)
-        {
-            foreach (ValidationProblemDetails vdp in validationProblemDetailsList)
-            {
-                if (vdp.Errors.Count != 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
 
         public static bool ValidDictionaryAttribute(Dictionary<string, string> titleToValidate)
