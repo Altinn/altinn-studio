3e91c6384dfb5b9005baac8da21c1cf9890bd296
Rune T. Larsen <rune@hjemmekino.no>
2023-07-13 12:14:10 +0200

    first version

---
+++ backend/src/Designer/TypedHttpClients/Altinn2Metadata/Altinn2MetadataClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System;
+using Altinn.Studio.Designer.Models;
+using PolicyAdmin.Models;
+using System.Threading.Tasks;
+
+namespace Altinn.Studio.Designer.TypedHttpClients.Altinn2Metadata
+{
+    public class Altinn2MetadataClient : IAltinn2MetadataClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public Altinn2MetadataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ServiceResource> GetServiceResourceFromService(string serviceCode, int serviceEditionCode)
+        {
+            // Temp location. Will be moved to CDN
+            string url = $"https://at21.altinn.cloud/sblbridge/metadata/api/resourceregisterresource/?serviceCode={serviceCode}&serviceEdition={serviceEditionCode}";
+
+            ServiceResource serviceResource;
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                string contentString = await response.Content.ReadAsStringAsync();
+                serviceResource = System.Text.Json.JsonSerializer.Deserialize<ServiceResource>(contentString);
+                return serviceResource;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Something went wrong when retrieving service resource", ex);
+            }
+        }
+    }
+}
---
+++ backend/src/Designer/TypedHttpClients/Altinn2Metadata/IAltinn2MetadataClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+
+namespace Altinn.Studio.Designer.TypedHttpClients.Altinn2Metadata
+{
+    public interface IAltinn2MetadataClient
+    {
+        Task<ServiceResource> GetServiceResourceFromService(string serviceCode, int serviceEditionCode);
+    }
+}
