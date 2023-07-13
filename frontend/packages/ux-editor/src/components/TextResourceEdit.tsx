6d43c82bd3cedef3e87800365cdce2e731891a8e
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-07-13 12:43:49 +0200

    Add a check to avoid adding empty text resources

--- frontend/packages/ux-editor/src/components/TextResourceEdit.tsx
+++ frontend/packages/ux-editor/src/components/TextResourceEdit.tsx
@@ -70,10 +70,12 @@ const TextBox = ({ language, t, textResource, textResourceId }: TextBoxProps) =>
   const { mutate } = useUpsertTextResourcesMutation(org, app);
 
   const updateTextResource = (text: string) => {
-      mutate({
-        language,
-        textResources: [{ id: textResourceId, value: text, variables:textResource?.variables }],
-      });
+    if (!textResource && !text) return;
+
+    mutate({
+      language,
+      textResources: [{ id: textResourceId, value: text, variables:textResource?.variables }],
+    });
   };
 
   const [value, setValue] = useState<string>(textResource?.value || '');
