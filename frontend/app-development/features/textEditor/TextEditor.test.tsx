9981d4dc696ff9c1f47a258dd2627646cef6d64a
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-07-18 17:51:26 +0200

    Fix unit tests

--- frontend/app-development/features/textEditor/TextEditor.test.tsx
+++ frontend/app-development/features/textEditor/TextEditor.test.tsx
@@ -1,29 +1,15 @@
 import { renderWithProviders } from '../../test/testUtils';
 import { APP_DEVELOPMENT_BASENAME } from 'app-shared/constants';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import { TextEditor } from './TextEditor';
 import { textMock } from '../../../testing/mocks/i18nMock';
 
-const mockServiceInformation = {
-  error: null,
-};
-
 describe('TextEditor', () => {
-/*   it('should render the spinner', async () => {
+  it('should render the spinner', () => {
     renderWithProviders(<TextEditor />, {
       startUrl: `${APP_DEVELOPMENT_BASENAME}/test-org/test-app`,
     });
     expect(screen.getByText(textMock('general.loading'))).toBeInTheDocument();
-  }); */
-
-  it('should render the component', async () => {
-    renderWithProviders(<TextEditor />, {
-      startUrl: `${APP_DEVELOPMENT_BASENAME}/test-org/test-app`,
-      preloadedState: {
-        serviceInformation: mockServiceInformation,
-      },
-    });
-    await waitFor(() => expect(screen.queryByText(textMock('general.loading'))).not.toBeInTheDocument());
   });
 });
