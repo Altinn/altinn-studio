d8f4d27752dc40ba94f4d09de465eb070c28b5c4
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-07-25 09:36:10 +0200

    Updating link component

--- frontend/resourceadm/components/Link/Link.tsx
+++ frontend/resourceadm/components/Link/Link.tsx
@@ -3,29 +3,20 @@ import classes from './Link.module.css';
 
 interface Props {
   text: string;
-  href?: string;
-  onClick?: () => void;
+  href: string;
   icon?: ReactNode;
 }
 
 /**
- * 'a' element button.
+ * 'a' element Link.
  *
  * @param props.text text to display on the element
  * @param props.href the href of the 'a' element
  * @param props.icon icon to be displayed
- * @returns
+ *
+ * TODO - Solve issue with visited.
  */
-export const Link = ({ text, href, onClick, icon }: Props) => {
-  // TODO - Move the button to its own component to separate the button from the link
-  if (onClick) {
-    return (
-      <button className={classes.link} onClick={onClick}>
-        {text}
-        {icon}
-      </button>
-    );
-  }
+export const Link = ({ text, href, icon }: Props) => {
   return (
     <a className={classes.link} href={href}>
       {text}
