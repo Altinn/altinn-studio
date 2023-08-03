fc3badd14a703a16090dc017d2887d3c5b23781b
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-08-03 11:37:50 +0200

    Add rightMenu tests

--- frontend/packages/ux-editor/src/components/rightMenu/RightMenu.test.tsx
+++ frontend/packages/ux-editor/src/components/rightMenu/RightMenu.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { IAppDataState } from '../../features/appData/appDataReducers';
-import { RightMenu } from './RightMenu';
+import { RightMenu, RightMenuProps } from './RightMenu';
 import { appDataMock, renderWithMockStore } from '../../testing/mocks';
-import { screen } from '@testing-library/react';
+import { render as rtlRender, act, screen, waitFor } from '@testing-library/react';
 import { mockUseTranslation } from '../../../../../testing/mocks/i18nMock';
+import { FormContext } from '../../containers/FormContext';
+import userEvent from '@testing-library/user-event';
+
+const user = userEvent.setup();
 
 // Test data:
 const contentText = 'Innhold';
@@ -14,6 +18,16 @@ const texts = {
   'right_menu.dynamics': conditionalRenderingText,
   'right_menu.calculations': calculationsText,
 };
+
+const FormContextProviderMock = {
+  formId: null,
+  form: null,
+  handleDiscard: jest.fn(),
+  handleEdit: jest.fn(),
+  handleUpdate: jest.fn(),
+  handleContainerSave: jest.fn().mockImplementation(() => Promise.resolve()),
+  handleComponentSave: jest.fn().mockImplementation(() => Promise.resolve()),
+}
 
 const contentTabTestId = 'content-tab';
 const conditionalRenderingTabTestId = 'conditional-rendering-tab';
@@ -32,7 +46,65 @@ jest.mock('./CalculationsTab', () => ({
 jest.mock('react-i18next', () => ({ useTranslation: () => mockUseTranslation(texts) }));
 
 describe('RightMenu', () => {
-  it('Renders all tabs', () => {
+  describe('Content', () => {
+    it('Closes content on load', () => {
+      render();
+      const button = screen.queryByRole("button", { name: contentText });
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+
+    it('Toggles content when clicked', async () => {
+      render();
+      const button = screen.queryByRole("button", { name: contentText });
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "true");
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+
+    it('Opens content when a component is selected', async () => {
+      const { rerender } = render();
+      rerender(getComponent({ formId: 'test2' }));
+      const button = screen.queryByRole("button", { name: contentText });
+      await waitFor(() => expect(button).toHaveAttribute('aria-expanded', "true"));
+    });
+  });
+
+  describe('Conditional rendering', () => {
+    it('Closes conditional rendering on load', () => {
+      render();
+      const button = screen.queryByRole("button", { name: conditionalRenderingText });
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+
+    it('Toggles conditional rendering when clicked', async () => {
+      render();
+      const button = screen.queryByRole("button", { name: conditionalRenderingText });
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "true");
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+  });
+
+  describe('Calculations', () => {
+    it('Closes calculations on load', () => {
+      render();
+      const button = screen.queryByRole("button", { name: calculationsText });
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+
+    it('Toggles calculations when clicked', async () => {
+      render();
+      const button = screen.queryByRole("button", { name: calculationsText });
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "true");
+      await act(() => user.click(button));
+      expect(button).toHaveAttribute('aria-expanded', "false");
+    });
+  });
+
+  it('Renders accordion', () => {
     render();
     expect(screen.getByText(contentText)).toBeInTheDocument();
     expect(screen.getByText(conditionalRenderingText)).toBeInTheDocument();
@@ -43,7 +115,15 @@ describe('RightMenu', () => {
   });
 });
 
-const render = (props = {}) => {
-  const appData: IAppDataState = { ...appDataMock };
-  return renderWithMockStore({ appData })(<RightMenu {...props} />);
+const getComponent = (formContextProps: Partial<FormContext> = {}, rightMenuProps: Partial<RightMenuProps> = {}) => (
+  <FormContext.Provider value={{
+    ...FormContextProviderMock,
+    ...formContextProps
+  }}>
+    <RightMenu {...rightMenuProps} />
+  </FormContext.Provider>
+);
+
+const render = (formContextProps: Partial<FormContext> = {}, rightMenuProps: Partial<RightMenuProps> = {}) => {
+  return rtlRender(getComponent(formContextProps, rightMenuProps));
 };
