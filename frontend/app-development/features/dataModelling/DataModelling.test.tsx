1f00f984eb8edf1a2f373e6276702a49a422d4b8
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-08-14 12:35:12 +0200

    Remove info dialogs

--- frontend/app-development/features/dataModelling/DataModelling.test.tsx
+++ frontend/app-development/features/dataModelling/DataModelling.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { DataModelling } from './DataModelling';
 import { render as rtlRender, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { LOCAL_STORAGE_KEY, setLocalStorageItem } from '@altinn/schema-editor/utils/localStorage';
 import { textMock } from '../../../testing/mocks/i18nMock';
 import { ServicesContextProps, ServicesContextProvider } from 'app-shared/contexts/ServicesContext';
 import { queriesMock } from 'app-shared/mocks/queriesMock';
@@ -59,36 +58,7 @@ describe('DataModelling', () => {
     expect(getDatamodelsXsd).toHaveBeenCalledTimes(1);
   });
 
-  it('Should show info dialog by default when loading the page', () => {
-    // make sure setting to turn off info dialog is cleared
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-    const queryClient = createQueryClientMock();
-    queryClient.setQueryData([QueryKey.DatamodelsMetadata, org, app], []);
-    render(queryClient);
-    const dialogHeader = screen.getByRole('heading', { name: textMock('schema_editor.info_dialog_title') });
-    expect(dialogHeader).toBeInTheDocument();
-  });
-
-  it('should display no data-models message when schema is undefined and loadState is loaded', async () => {
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-    const queryClient = createQueryClientMock();
-    queryClient.setQueryData([QueryKey.DatamodelsMetadata, org, app], [jsonMetadata1Mock]);
-    render(queryClient);
-    const dialogHeader = screen.getByRole('heading', { name: textMock('schema_editor.info_dialog_title') });
-    expect(dialogHeader).toBeInTheDocument();
-  });
-
-  it('Should not show info dialog when loading the page if user has asked to not show it again', () => {
-    // make sure setting to turn off info dialog is set
-    setLocalStorageItem('hideIntroPage', true);
-    render();
-    const dialogHeader = screen.queryByRole('heading', { name: textMock('schema_editor.info_dialog_title') });
-    expect(dialogHeader).not.toBeInTheDocument();
-  });
-
   it('Should show start dialog when no models are present and intro page is closed', () => {
-    // make sure setting to turn off info dialog is set
-    setLocalStorageItem('hideIntroPage', true);
     const queryClient = createQueryClientMock();
     queryClient.setQueryData([QueryKey.DatamodelsMetadata, org, app], []);
     render(queryClient);
@@ -97,16 +67,12 @@ describe('DataModelling', () => {
   });
 
   it('Should not show start dialog when the models have not been loaded yet', () => {
-    // make sure setting to turn off info dialog is set
-    setLocalStorageItem('hideIntroPage', true);
     render();
     expect(screen.getByTitle(textMock('general.loading'))).toBeInTheDocument();
     expect(screen.queryByRole('heading', { name: textMock('app_data_modelling.landing_dialog_header') })).not.toBeInTheDocument();
   });
 
   it('Should not show start dialog when there are models present', async () => {
-    // make sure setting to turn off info dialog is set
-    setLocalStorageItem('hideIntroPage', true);
     getDatamodels.mockImplementation(() => Promise.resolve([jsonMetadata1Mock]));
     render();
     await waitForElementToBeRemoved(() => screen.queryByTitle(textMock('general.loading')));
--- frontend/app-development/features/textEditor/TextEditor.tsx
+++ frontend/app-development/features/textEditor/TextEditor.tsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import type { LangCode } from '@altinn/text-editor';
 import { TextEditor as TextEditorImpl, defaultLangCode } from '@altinn/text-editor';
-import { PanelVariant, PopoverPanel } from '@altinn/altinn-design-system';
-import { Button, ButtonColor, ButtonVariant } from '@digdir/design-system-react';
 import { PageSpinner } from 'app-shared/components';
-import { Link, useParams, useSearchParams } from 'react-router-dom';
-import classes from './TextEditor.module.css';
-import { getLocalStorage, setLocalStorage } from 'app-shared/utils/localStorage';
+import { useParams, useSearchParams } from 'react-router-dom';
 import { TextResourceIdMutation } from '@altinn/text-editor/src/types';
-import { useTranslation } from 'react-i18next';
 import { useLanguagesQuery, useTextResourcesQuery } from '../../hooks/queries';
 import {
   useAddLanguageMutation,
@@ -16,8 +11,6 @@ import {
   useTextIdMutation,
   useUpsertTextResourceMutation,
 } from '../../hooks/mutations';
-
-const storageGroupName = 'textEditorStorage';
 
 export const TextEditor = () => {
   const [searchParams, setSearchParams] = useSearchParams({ lang: '', search: '' });
@@ -52,12 +45,6 @@ useEffect(() => {
     }
     setSearchParams(params);
   };
-  
-  const { t } = useTranslation();
-
-  const [hideIntroPage, setHideIntroPage] = useState(
-    () => getLocalStorage(storageGroupName, 'hideTextsIntroPage') ?? false
-  );
 
   const { mutate: addLanguageMutation } = useAddLanguageMutation(org, app);
   const handleAddLanguage = (language: LangCode) =>
@@ -72,9 +59,6 @@ useEffect(() => {
   const { mutate: deleteLanguageMutation } = useDeleteLanguageMutation(org, app);
   const { mutate: textIdMutation } = useTextIdMutation(org, app);
 
-  const handleHideIntroPageButtonClick = () =>
-    setHideIntroPage(setLocalStorage(storageGroupName, 'hideTextsIntroPage', true));
-
   const { mutate: upsertTextResource } = useUpsertTextResourceMutation(org, app);
 
   if (isInitialLoadingLang || isFetchingTranslations || !textResources) {
@@ -82,53 +66,17 @@ useEffect(() => {
   }
 
   return (
-    <>
-      <PopoverPanel
-        forceMobileLayout={true}
-        variant={PanelVariant.Info}
-        open={!hideIntroPage}
-        side={'bottom'}
-        title={t('text_editor.info_dialog_title')}
-        trigger={<span className={'sr-only'} />}
-        onOpenChange={() => setHideIntroPage(!hideIntroPage)}
-      >
-        <p>{t('text_editor.info_dialog_1')}</p>
-        <p>
-          <Link target={'_blank'} to={`/../designer/${org}/${app}/Text`} relative={'path'}>
-            {t('text_editor.info_dialog_nav_to_old')}
-          </Link>
-        </p>
-        <span className={classes.buttons}>
-          <Button
-            color={ButtonColor.Primary}
-            onClick={() => setHideIntroPage(true)}
-            variant={ButtonVariant.Outline}
-            size='small'
-          >
-            {t('general.close')}
-          </Button>
-          <Button
-            color={ButtonColor.Secondary}
-            onClick={handleHideIntroPageButtonClick}
-            variant={ButtonVariant.Outline}
-            size='small'
-          >
-            {t('general.do_not_show_anymore')}
-          </Button>
-        </span>
-      </PopoverPanel>
-      <TextEditorImpl
-        addLanguage={handleAddLanguage}
-        availableLanguages={appLangCodes}
-        deleteLanguage={(langCode: LangCode) => deleteLanguageMutation({ langCode })}
-        searchQuery={getSearchQuery()}
-        selectedLangCodes={selectedLangCodes}
-        setSearchQuery={setSearchQuery}
-        setSelectedLangCodes={handleSelectedLangCodes}
-        textResourceFiles={textResources || {}}
-        updateTextId={(data: TextResourceIdMutation) => textIdMutation([data])}
-        upsertTextResource={upsertTextResource}
-      />
-    </>
+    <TextEditorImpl
+      addLanguage={handleAddLanguage}
+      availableLanguages={appLangCodes}
+      deleteLanguage={(langCode: LangCode) => deleteLanguageMutation({ langCode })}
+      searchQuery={getSearchQuery()}
+      selectedLangCodes={selectedLangCodes}
+      setSearchQuery={setSearchQuery}
+      setSelectedLangCodes={handleSelectedLangCodes}
+      textResourceFiles={textResources || {}}
+      updateTextId={(data: TextResourceIdMutation) => textIdMutation([data])}
+      upsertTextResource={upsertTextResource}
+    />
   );
 };
