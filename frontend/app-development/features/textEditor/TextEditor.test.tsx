ec9241218639aa80aee1aff0b0449379f1c679f9
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-08-14 16:20:34 +0200

    Add missing tests that generates code coverage warnings

--- frontend/app-development/features/textEditor/TextEditor.test.tsx
+++ frontend/app-development/features/textEditor/TextEditor.test.tsx
@@ -1,11 +1,10 @@
 import { renderWithProviders } from '../../test/testUtils';
 import { APP_DEVELOPMENT_BASENAME } from 'app-shared/constants';
-import { act, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { act, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import React from 'react';
 import { TextEditor } from './TextEditor';
 import { textMock } from '../../../testing/mocks/i18nMock';
 import { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
-import { queriesMock } from 'app-shared/mocks/queriesMock';
 import userEvent from '@testing-library/user-event';
 
 const user = userEvent.setup();
@@ -16,8 +15,9 @@ const app = 'test-app';
 const testTextResourceKey = 'test-key';
 const testTextResourceValue = 'test-value';
 
-const mockQueries: ServicesContextProps = {
-  ...queriesMock,
+const queries: Partial<ServicesContextProps> = {
+  deleteLanguageCode: jest.fn().mockImplementation(() => Promise.resolve()),
+  upsertTextResources: jest.fn().mockImplementation(() => Promise.resolve()),
   getTextResources: jest.fn().mockImplementation(() => Promise.resolve({
     resources:[
       {
@@ -26,7 +26,7 @@ const mockQueries: ServicesContextProps = {
       }
     ]
   })),
-  getTextLanguages: jest.fn().mockImplementation(() => Promise.resolve(['nb'])),
+  getTextLanguages: jest.fn().mockImplementation(() => Promise.resolve(['nb', 'en'])),
 };
 
 const mockSetSearchParams = jest.fn();
@@ -60,25 +60,49 @@ Object.defineProperty(window, 'localStorage', { value: mockLocalStorage });
 
 describe('TextEditor', () => {
   it('renders the component', async () => {
-    render(mockQueries);
-    await waitForElementToBeRemoved(() => screen.queryByText(textMock('general.loading')));
+    await render();
 
     expect(screen.getByText(testTextResourceKey)).toBeInTheDocument();
     expect(screen.getByText(testTextResourceValue)).toBeInTheDocument();
   });
 
   it('updates search query when searching text', async () => {
-    render(mockQueries);
-    await waitForElementToBeRemoved(() => screen.queryByText(textMock('general.loading')));
+    await render();
 
     const search = '1';
     const searchInput = screen.getByTestId('text-editor-search-default');
     await act(() => user.type(searchInput, search));
+
     expect(mockSetSearchParams).toHaveBeenCalledWith({ search });
   });
 
+  it('adds a text resource when clicking "New text" button', async () => {
+    await render();
+
+    const addButton = screen.getByRole('button', { name: 'Ny tekst' });
+    await act(() => user.click(addButton));
+
+    expect(queries.upsertTextResources).toBeCalledTimes(2);
+  });
+
+  it('deletes a text resource when clicking delete button', async () => {
+    await render();
+
+    const deleteButton = screen.getByTestId('delete-en');
+    await act(() => user.click(deleteButton));
+
+    const confirmButton = await screen.findByRole('button', { name: textMock('schema_editor.language_confirm_deletion') });
+    await act(() => user.click(confirmButton));
+
+    expect(queries.deleteLanguageCode).toBeCalledWith(org, app, 'en');
+
+    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
+  });
+
   it('renders the spinner', () => {
-    render();
+    renderWithProviders(<TextEditor />, {
+      startUrl: `${APP_DEVELOPMENT_BASENAME}/${org}/${app}`,
+    });
     expect(screen.getByText(textMock('general.loading'))).toBeInTheDocument();
   });
 
@@ -91,9 +115,11 @@ describe('TextEditor', () => {
   });
 });
 
-const render = (queries: Partial<ServicesContextProps> = {}) => {
+const render = async () => {
   renderWithProviders(<TextEditor />, {
     queries,
     startUrl: `${APP_DEVELOPMENT_BASENAME}/${org}/${app}`,
   });
+
+  await waitForElementToBeRemoved(() => screen.queryByText(textMock('general.loading')));
 };
