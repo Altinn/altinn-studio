739790c520bf6e8ffd2caff756a05b703d536c4a
LAPTOP-S0QU96IH\david <david@framit.no>
2023-08-16 12:19:54 +0200

    created utils to make it easy to interacting with localstorage

---
+++ frontend/packages/shared/src/hooks/useWebStorage.ts
@@ -0,0 +1,22 @@
+import { TypedStorage, typedLocalStorage } from 'app-shared/utils/webStorage';
+import { useCallback, useState } from 'react';
+
+const useWebStorage = <T>(
+  typedStorage: TypedStorage,
+  key: string,
+  initialValue?: T
+): [T, (newValue: T) => void] => {
+  const [value, setValue] = useState<T>(typedStorage.getItem(key) || initialValue);
+
+  const setStorageValue = useCallback(
+    (newValue: T) => {
+      typedStorage.setItem(key, newValue);
+      setValue(newValue);
+    },
+    [key, typedStorage]
+  );
+  return [value, setStorageValue];
+};
+
+export const useLocalStorage = <T>(key: string, initialValue?: T) =>
+  useWebStorage<T>(typedLocalStorage, key, initialValue);
