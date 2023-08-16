febacff9b19853937dd662bc452a76411200f3f0
Mirko Sekulic <misha.sekulic@gmail.com>
2023-08-16 09:03:15 +0200

    SkipOnPlatforms attributes

---
+++ backend/tests/Designer.Tests/TestAttributes/OsPlatformConsts.cs
@@ -0,0 +1,10 @@
+﻿namespace Designer.Tests.TestAttributes
+{
+    public static class OsPlatformConsts
+    {
+        public const string FreeBSD = "FREEBSD";
+        public const string Linux = "LINUX";
+        public const string OSX = "OSX";
+        public const string Windows = "WINDOWS";
+    }
+}
