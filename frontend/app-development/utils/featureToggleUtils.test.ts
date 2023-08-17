793fbae019ef51ddef03844bbb3212d4abfffba9
LAPTOP-S0QU96IH\david <david@framit.no>
2023-08-17 12:23:10 +0200

    updated the unit-test

--- frontend/app-development/utils/featureToggleUtils.test.ts
+++ frontend/app-development/utils/featureToggleUtils.test.ts
@@ -3,43 +3,43 @@ import { shouldDisplayFeature } from './featureToggleUtils';
 
 describe('featureToggle localStorage', () => {
   it('should return true if feature is enabled in the localStorage', () => {
-    typedLocalStorage.setItem<string[]>('featureFlags', ['process']);
-    expect(shouldDisplayFeature('process')).toBe(true);
+    typedLocalStorage.setItem<string[]>('featureFlags', ['processEditor']);
+    expect(shouldDisplayFeature('processEditor')).toBe(true);
   });
 
   it('should return true if featureFlag includes in feature params', () => {
-    typedLocalStorage.setItem<string[]>('featureFlags', ['demo', 'process']);
-    expect(shouldDisplayFeature('process')).toBe(true);
+    typedLocalStorage.setItem<string[]>('featureFlags', ['demo', 'processEditor']);
+    expect(shouldDisplayFeature('processEditor')).toBe(true);
   });
 
   it('should return false if feature is not enabled in the localStorage', () => {
     typedLocalStorage.setItem<string[]>('featureFlags', ['demo']);
-    expect(shouldDisplayFeature('process')).toBe(false);
+    expect(shouldDisplayFeature('processEditor')).toBe(false);
   });
 
   it('should return false if feature is not enabled in the localStorage', () => {
-    expect(shouldDisplayFeature('process')).toBe(false);
+    expect(shouldDisplayFeature('processEditor')).toBe(false);
   });
 });
 
 describe('featureToggle url', () => {
   it('should return true if feature is enabled in the url', () => {
-    window.history.pushState({}, 'PageUrl', '/?featureFlags=process');
-    expect(shouldDisplayFeature('process')).toBe(true);
+    window.history.pushState({}, 'PageUrl', '/?featureFlags=processEditor');
+    expect(shouldDisplayFeature('processEditor')).toBe(true);
   });
 
   it('should return true if featureFlag includes in feature params', () => {
-    window.history.pushState({}, 'PageUrl', '/?featureFlags=demo,process');
-    expect(shouldDisplayFeature('process')).toBe(true);
+    window.history.pushState({}, 'PageUrl', '/?featureFlags=demo,processEditor');
+    expect(shouldDisplayFeature('processEditor')).toBe(true);
   });
 
   it('should return false if feature is not included in the url', () => {
     window.history.pushState({}, 'PageUrl', '/?featureFlags=demo');
-    expect(shouldDisplayFeature('process')).toBe(false);
+    expect(shouldDisplayFeature('processEditor')).toBe(false);
   });
 
   it('should return false if feature is not included in the url', () => {
     window.history.pushState({}, 'PageUrl', '/');
-    expect(shouldDisplayFeature('process')).toBe(false);
+    expect(shouldDisplayFeature('processEditor')).toBe(false);
   });
 });
