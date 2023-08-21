913f7dc8b6ac466d8e1cfa9b4546f854b5d6a0c6
LAPTOP-S0QU96IH\david <david@framit.no>
2023-08-21 20:42:10 +0200

    revert formatting of code

--- frontend/packages/shared/src/api/queries.ts
+++ frontend/packages/shared/src/api/queries.ts
@@ -1,6 +1,51 @@
 import { get, put } from 'app-shared/utils/networking';
-import { branchStatusPath, datamodelMetadataPath, datamodelPath, datamodelsPath, datamodelsXsdPath, deployPermissionsPath, deploymentsPath, envConfigPath, formLayoutsPath, instanceIdForPreviewPath, layoutSetsPath, layoutSettingsPath, optionListIdsPath, orgsListPath, processEditorPath, releasesPath, repoMetaPath, repoPullPath, repoSearchPath, repoStatusPath, resourceActionsPath, resourceListPath, resourcePolicyPath, resourcePublishStatusPath, resourceSectorsPath, resourceSinglePath, resourceSubjectsPath, resourceThematicEurovocPath, resourceThematicLosPath, resourceValidatePolicyPath, resourceValidateResourcePath, ruleConfigPath, ruleHandlerPath, textLanguagesPath, textResourcesPath, userCurrentPath, userStarredListPath, widgetSettingsPath } from './paths';
-import { AppDeploymentsResponse, AppReleasesResponse, DatamodelMetadataResponse, SearchRepoFilterParams, SearchRepositoryResponse } from 'app-shared/types/api';
+import {
+  branchStatusPath,
+  datamodelMetadataPath,
+  datamodelPath,
+  datamodelsPath,
+  datamodelsXsdPath,
+  deployPermissionsPath,
+  deploymentsPath,
+  envConfigPath,
+  formLayoutsPath,
+  instanceIdForPreviewPath,
+  layoutSetsPath,
+  layoutSettingsPath,
+  optionListIdsPath,
+  orgsListPath,
+  processEditorPath,
+  releasesPath,
+  repoMetaPath,
+  repoPullPath,
+  repoSearchPath,
+  repoStatusPath,
+  resourceActionsPath,
+  resourceListPath,
+  resourcePolicyPath,
+  resourcePublishStatusPath,
+  resourceSectorsPath,
+  resourceSinglePath,
+  resourceSubjectsPath,
+  resourceThematicEurovocPath,
+  resourceThematicLosPath,
+  resourceValidatePolicyPath,
+  resourceValidateResourcePath,
+  ruleConfigPath,
+  ruleHandlerPath,
+  textLanguagesPath,
+  textResourcesPath,
+  userCurrentPath,
+  userStarredListPath,
+  widgetSettingsPath,
+} from './paths';
+import {
+  AppDeploymentsResponse,
+  AppReleasesResponse,
+  DatamodelMetadataResponse,
+  SearchRepoFilterParams,
+  SearchRepositoryResponse,
+} from 'app-shared/types/api';
 import { BranchStatus } from 'app-shared/types/BranchStatus';
 import { DatamodelMetadataJson, DatamodelMetadataXsd } from 'app-shared/types/DatamodelMetadata';
 import { DeployEnvironment } from 'app-shared/types/DeployEnvironment';
