61d71f85cf4793c67fb145c95014624e31a747a6
Nina Kylstad <nkylstad@gmail.com>
2023-08-22 16:09:51 +0200

    Add some more tests

--- frontend/packages/ux-editor/src/components/config/FormComponentConfig.test.tsx
+++ frontend/packages/ux-editor/src/components/config/FormComponentConfig.test.tsx
@@ -42,6 +42,70 @@ describe('FormComponentConfig', () => {
     });
   });
 
+  it('should render list of unsupported properties', async () => {
+    render({
+      props: {
+        hideUnsupported: false,
+        schema: {
+          ...InputSchema,
+          properties: {
+            ...InputSchema.properties,
+            unsupportedProperty: {
+              type: 'array',
+              items: {
+                type: 'object',
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(
+      screen.getByText(textMock('ux_editor.edit_component.unsupported_properties_message'))
+    ).toBeInTheDocument();
+    expect(screen.getByText('unsupportedProperty')).toBeInTheDocument();
+  });
+
+  it('should not render list of unsupported properties if hideUnsupported is true', async () => {
+    render({
+      props: {
+        hideUnsupported: true,
+        schema: {
+          ...InputSchema,
+          properties: {
+            ...InputSchema.properties,
+            unsupportedProperty: {
+              type: 'array',
+              items: {
+                type: 'object',
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(
+      screen.queryByText(textMock('ux_editor.edit_component.unsupported_properties_message'))
+    ).not.toBeInTheDocument();
+    expect(screen.queryByText('unsupportedProperty')).not.toBeInTheDocument();
+  });
+
+  it('should not render property if it is null', async () => {
+    render({
+      props: {
+        hideUnsupported: true,
+        schema: {
+          ...InputSchema,
+          properties: {
+            ...InputSchema.properties,
+            nullProperty: null,
+          },
+        },
+      },
+    });
+    expect(screen.queryByText('nullProperty')).not.toBeInTheDocument();
+  });
+
   const render = ({
     props = {},
     queries = {},
