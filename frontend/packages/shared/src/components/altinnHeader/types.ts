2c685b0062f5888eb992ff063e7f41eb5b5a8ff8
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-08-22 10:23:18 +0200

    Fixing feedback on PR

--- frontend/packages/shared/src/components/altinnHeader/types.ts
+++ frontend/packages/shared/src/components/altinnHeader/types.ts
@@ -1,11 +1,13 @@
+import { ButtonProps } from '@digdir/design-system-react';
+
 export type AltinnHeaderVariant = 'regular' | 'preview';
 
 export interface AltinnButtonActionItem {
   title: string;
   menuKey: string;
   path?: (org: string, app: string) => string;
-  buttonVariant: 'filled' | 'outline' | 'quiet';
-  buttonColor?: 'inverted';
+  buttonVariant: ButtonProps['variant'];
+  buttonColor?: ButtonProps['color'];
   headerButtonsClasses: any;
   handleClick: () => void;
   inBeta?: boolean;
