bf579d4651108dfbbf7aebdb0bd0c53dfa8faa79
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-08-22 19:17:55 +0200

    Fixing build issue

--- frontend/resourceadm/data-mocks/policies/index.ts
+++ frontend/resourceadm/data-mocks/policies/index.ts
@@ -4,7 +4,7 @@ import {
   PolicyRuleBackendType,
   PolicyRuleResourceType,
   PolicySubjectType,
-} from '@altinn/policy-editor/src/PolicyEditorTypes';
+} from '@altinn/policy-editor';
 
 // RESOURCE ID
 export const resourceIdMock1: string = 'resource_id_1';
