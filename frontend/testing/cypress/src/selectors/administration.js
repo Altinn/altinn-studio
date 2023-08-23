cb216cfe94e094bdef3725a24132243b1d3b2f44
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2023-08-23 10:51:24 +0200

    Cypress improvements 3 (#10864)

---
+++ frontend/testing/cypress/src/selectors/administration.js
@@ -0,0 +1,6 @@
+import * as texts from "@altinn-studio/language/src/nb.json";
+
+export const administration = {
+  getAppNameField: () => cy.findByRole('textbox', { name: texts['general.service_name'] }),
+  getDescriptionField: () => cy.findByRole('textbox', { name: texts['administration.service_comment'] }),
+};
