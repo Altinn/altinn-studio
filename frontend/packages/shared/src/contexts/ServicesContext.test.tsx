0d12e915d5534af67d9d114f1b01d242506bcaad
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-08-23 15:02:42 +0200

    Add unit tests for react-query callbacks and error boundaries

---
+++ frontend/packages/shared/src/contexts/ServicesContext.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, renderHook, screen, waitFor } from '@testing-library/react';
+import { ServicesContextProvider } from './ServicesContext';
+import { queriesMock } from 'app-shared/mocks/queriesMock';
+import { useQuery } from '@tanstack/react-query';
+
+const wrapper = ({ children }) => (
+  <ServicesContextProvider {...queriesMock}>
+    {children}
+  </ServicesContextProvider>
+);
+
+describe('ServicesContext', () => {
+  it('displays a default error message if an API call fails', async () => {
+    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+    const { result } = renderHook(() => useQuery(['fetchData'],() => Promise.reject(), { retry: false }), { wrapper });
+
+    await waitFor(() => result.current.isError);
+
+    expect(await screen.findByText(/general.error_message/)).toBeInTheDocument();
+    expect(mockConsoleError).toHaveBeenCalled();
+  });
+
+  it('displays a default error message if a component throws an error while rendering', () => {
+    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+    const data = null;
+
+    const ErrorComponent = () => <div>{data.length}</div>;
+    render(<ErrorComponent />, { wrapper });
+
+    expect(screen.getByText(/general.error_message/)).toBeInTheDocument();
+    expect(screen.getByText(/general.try_again/)).toBeInTheDocument();
+    expect(mockConsoleError).toHaveBeenCalled();
+  });
+});
