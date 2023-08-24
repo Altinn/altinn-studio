f29daf0249a4dd14da6f0ca9c313242f7d0720d7
LAPTOP-S0QU96IH\david <david@framit.no>
2023-08-24 09:39:09 +0200

    format

--- frontend/packages/shared/src/api/queries.ts
+++ frontend/packages/shared/src/api/queries.ts
@@ -38,13 +38,7 @@ import {
   userStarredListPath,
   widgetSettingsPath,
 } from './paths';
-import {
-  AppDeploymentsResponse,
-  AppReleasesResponse,
-  DatamodelMetadataResponse,
-  SearchRepoFilterParams,
-  SearchRepositoryResponse,
-} from 'app-shared/types/api';
+import { AppDeploymentsResponse, AppReleasesResponse, DatamodelMetadataResponse, SearchRepoFilterParams, SearchRepositoryResponse } from 'app-shared/types/api';
 import { BranchStatus } from 'app-shared/types/BranchStatus';
 import { DatamodelMetadataJson, DatamodelMetadataXsd } from 'app-shared/types/DatamodelMetadata';
 import { DeployEnvironment } from 'app-shared/types/DeployEnvironment';
@@ -62,94 +56,51 @@ import { buildQueryParams } from 'app-shared/utils/urlUtils';
 import { componentSchemaUrl, orgsListUrl } from '../cdn-paths';
 import type { JsonSchema } from 'app-shared/types/JsonSchema';
 import { expressionSchemaUrl, layoutSchemaUrl, numberFormatSchemaUrl } from '../cdn-paths';
-import {
-  PolicyActionType,
-  PolicyBackendType,
-  PolicySubjectType,
-  ResourceBackendType,
-  ResourceSectorType,
-  ResourceThematicType,
-  ResourceType,
-  ResourceVersionStatusType,
-  ValidationType,
-} from 'resourceadm/types/global';
+import { PolicyActionType, PolicyBackendType, PolicySubjectType, ResourceBackendType, ResourceSectorType, ResourceThematicType, ResourceType, ResourceVersionStatusType, ValidationType } from 'resourceadm/types/global';
 
-export const getAppReleases = (owner: string, app: string) =>
-  get<AppReleasesResponse>(releasesPath(owner, app, 'Descending'));
-export const getBranchStatus = (owner: string, app: string, branch: string) =>
-  get<BranchStatus>(branchStatusPath(owner, app, branch));
-export const getDatamodel = (owner: string, app: string, modelPath: string) =>
-  get<JsonSchema>(datamodelPath(owner, app, modelPath));
-export const getDatamodelMetadata = (owner: string, app: string) =>
-  get<DatamodelMetadataResponse>(datamodelMetadataPath(owner, app));
-export const getDatamodels = (owner: string, app: string) =>
-  get<DatamodelMetadataJson[]>(datamodelsPath(owner, app));
-export const getDatamodelsXsd = (owner: string, app: string) =>
-  get<DatamodelMetadataXsd[]>(datamodelsXsdPath(owner, app));
-export const getDeployPermissions = (owner: string, app: string) =>
-  get<string[]>(deployPermissionsPath(owner, app));
-export const getDeployments = (owner: string, app: string) =>
-  get<AppDeploymentsResponse>(deploymentsPath(owner, app, 'Descending'));
+export const getAppReleases = (owner: string, app: string) => get<AppReleasesResponse>(releasesPath(owner, app, 'Descending'));
+export const getBranchStatus = (owner: string, app: string, branch: string) => get<BranchStatus>(branchStatusPath(owner, app, branch));
+export const getDatamodel = (owner: string, app: string, modelPath: string) => get<JsonSchema>(datamodelPath(owner, app, modelPath));
+export const getDatamodelMetadata = (owner: string, app: string) => get<DatamodelMetadataResponse>(datamodelMetadataPath(owner, app));
+export const getDatamodels = (owner: string, app: string) => get<DatamodelMetadataJson[]>(datamodelsPath(owner, app));
+export const getDatamodelsXsd = (owner: string, app: string) => get<DatamodelMetadataXsd[]>(datamodelsXsdPath(owner, app));
+export const getDeployPermissions = (owner: string, app: string) => get<string[]>(deployPermissionsPath(owner, app));
+export const getDeployments = (owner: string, app: string) => get<AppDeploymentsResponse>(deploymentsPath(owner, app, 'Descending'));
 export const getEnvironments = () => get<DeployEnvironment[]>(envConfigPath());
-export const getFormLayoutSettings = (owner: string, app: string, layoutSetName: string) =>
-  get<ILayoutSettings>(layoutSettingsPath(owner, app, layoutSetName));
-export const getFormLayouts = (owner: string, app: string, layoutSetName: string) =>
-  get<FormLayoutsResponse>(formLayoutsPath(owner, app, layoutSetName));
-export const getLayoutSets = (owner: string, app: string) =>
-  get<LayoutSets>(layoutSetsPath(owner, app));
-export const getInstanceIdForPreview = (owner: string, app: string) =>
-  get<string>(instanceIdForPreviewPath(owner, app));
+export const getFormLayoutSettings = (owner: string, app: string, layoutSetName: string) => get<ILayoutSettings>(layoutSettingsPath(owner, app, layoutSetName));
+export const getFormLayouts = (owner: string, app: string, layoutSetName: string) => get<FormLayoutsResponse>(formLayoutsPath(owner, app, layoutSetName));
+export const getLayoutSets = (owner: string, app: string) => get<LayoutSets>(layoutSetsPath(owner, app));
+export const getInstanceIdForPreview = (owner: string, app: string) => get<string>(instanceIdForPreviewPath(owner, app));
 export const getOrgList = () => get<OrgsState>(orgsListUrl());
 export const getOrganizations = () => get<Organization[]>(orgsListPath());
-export const getRepoMetadata = (owner: string, app: string) =>
-  get<Repository>(repoMetaPath(owner, app));
-export const getRepoPull = (owner: string, app: string) =>
-  get<RepoStatus>(repoPullPath(owner, app));
-export const getRepoStatus = (owner: string, app: string) =>
-  get<RepoStatus>(repoStatusPath(owner, app));
-export const getRuleConfig = (owner: string, app: string, layoutSetName: string) =>
-  get<RuleConfig>(ruleConfigPath(owner, app, layoutSetName));
-export const getRuleModel = (owner: string, app: string, layoutSetName: string) =>
-  get(ruleHandlerPath(owner, app, layoutSetName));
+export const getRepoMetadata = (owner: string, app: string) => get<Repository>(repoMetaPath(owner, app));
+export const getRepoPull = (owner: string, app: string) => get<RepoStatus>(repoPullPath(owner, app));
+export const getRepoStatus = (owner: string, app: string) => get<RepoStatus>(repoStatusPath(owner, app));
+export const getRuleConfig = (owner: string, app: string, layoutSetName: string) => get<RuleConfig>(ruleConfigPath(owner, app, layoutSetName));
+export const getRuleModel = (owner: string, app: string, layoutSetName: string) => get(ruleHandlerPath(owner, app, layoutSetName));
 export const getStarredRepos = () => get<IRepository[]>(userStarredListPath());
-export const getTextLanguages = (owner: string, app: string): Promise<string[]> =>
-  get(textLanguagesPath(owner, app));
-export const getTextResources = (owner: string, app: string, lang: string) =>
-  get<ITextResourcesWithLanguage>(textResourcesPath(owner, app, lang));
+export const getTextLanguages = (owner: string, app: string): Promise<string[]> => get(textLanguagesPath(owner, app));
+export const getTextResources = (owner: string, app: string, lang: string) => get<ITextResourcesWithLanguage>(textResourcesPath(owner, app, lang));
 export const getUser = () => get<User>(userCurrentPath());
-export const getWidgetSettings = (owner: string, app: string) =>
-  get<WidgetSettingsResponse | null>(widgetSettingsPath(owner, app));
-export const searchRepos = (filter: SearchRepoFilterParams) =>
-  get<SearchRepositoryResponse>(`${repoSearchPath()}${buildQueryParams(filter)}`);
-export const getOptionListIds = (owner: string, app: string) =>
-  get<string[]>(optionListIdsPath(owner, app));
+export const getWidgetSettings = (owner: string, app: string) => get<WidgetSettingsResponse | null>(widgetSettingsPath(owner, app));
+export const searchRepos = (filter: SearchRepoFilterParams) => get<SearchRepositoryResponse>(`${repoSearchPath()}${buildQueryParams(filter)}`);
+export const getOptionListIds = (owner: string, app: string) => get<string[]>(optionListIdsPath(owner, app));
 
 export const getExpressionSchema = () => get<string[]>(expressionSchemaUrl());
 export const getLayoutSchema = () => get<string[]>(layoutSchemaUrl());
 export const getNumberFormatSchema = () => get<string[]>(numberFormatSchemaUrl());
-export const getComponentSchema = (component: string) =>
-  get<string[]>(componentSchemaUrl(component));
+export const getComponentSchema = (component: string) => get<string[]>(componentSchemaUrl(component));
 export const getComponentsCommonDefsSchema = () => get<string[]>(componentSchemaUrl('common-defs'));
 
 // Resourceadm
-export const getPolicy = (org: string, repo: string, id: string) =>
-  get<PolicyBackendType>(resourcePolicyPath(org, repo, id));
-export const getPolicyActions = (org: string, repo: string) =>
-  get<PolicyActionType[]>(resourceActionsPath(org, repo));
-export const getPolicySubjects = (org: string, repo: string) =>
-  get<PolicySubjectType[]>(resourceSubjectsPath(org, repo));
-export const getResourcePublishStatus = (org: string, repo: string, id: string) =>
-  get<ResourceVersionStatusType>(resourcePublishStatusPath(org, repo, id));
+export const getPolicy = (org: string, repo: string, id: string) => get<PolicyBackendType>(resourcePolicyPath(org, repo, id));
+export const getPolicyActions = (org: string, repo: string) => get<PolicyActionType[]>(resourceActionsPath(org, repo));
+export const getPolicySubjects = (org: string, repo: string) => get<PolicySubjectType[]>(resourceSubjectsPath(org, repo));
+export const getResourcePublishStatus = (org: string, repo: string, id: string) => get<ResourceVersionStatusType>(resourcePublishStatusPath(org, repo, id));
 export const getResourceList = (org: string) => get<ResourceType[]>(resourceListPath(org));
-export const getResource = (org: string, repo: string, id: string) =>
-  get<ResourceBackendType>(resourceSinglePath(org, repo, id));
-export const getValidatePolicy = (org: string, repo: string, id: string) =>
-  get<ValidationType>(resourceValidatePolicyPath(org, repo, id));
-export const getValidateResource = (org: string, repo: string, id: string) =>
-  get<ValidationType>(resourceValidateResourcePath(org, repo, id));
-export const getResourceSectors = (org: string) =>
-  get<ResourceSectorType[]>(resourceSectorsPath(org));
-export const getResourceThematicLos = (org: string) =>
-  get<ResourceThematicType[]>(resourceThematicLosPath(org));
-export const getResourceThematicEurovoc = (org: string) =>
-  get<ResourceThematicType[]>(resourceThematicEurovocPath(org));
+export const getResource = (org: string, repo: string, id: string) => get<ResourceBackendType>(resourceSinglePath(org, repo, id));
+export const getValidatePolicy = (org: string, repo: string, id: string) => get<ValidationType>(resourceValidatePolicyPath(org, repo, id));
+export const getValidateResource = (org: string, repo: string, id: string) => get<ValidationType>(resourceValidateResourcePath(org, repo, id));
+export const getResourceSectors = (org: string) => get<ResourceSectorType[]>(resourceSectorsPath(org));
+export const getResourceThematicLos = (org: string) => get<ResourceThematicType[]>(resourceThematicLosPath(org));
+export const getResourceThematicEurovoc = (org: string) => get<ResourceThematicType[]>(resourceThematicEurovocPath(org));
