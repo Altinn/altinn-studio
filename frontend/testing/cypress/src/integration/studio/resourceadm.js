25423b83a5097b3a2f9c939e7a09100e1e9872c0
Torgeir Holen <torgeir333@gmail.com>
2023-08-25 11:47:36 +0200

    initial commit of resourceadm cypress tests

---
+++ frontend/testing/cypress/src/integration/studio/resourceadm.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+/// <reference types="../../support" />
+
+context("Resourceadm", () => {
+  before(() => {
+    cy.studiologin(Cypress.env("autoTestUser"), Cypress.env("autoTestUserPwd"));
+    // the login session will last throughout the tests
+  });
+
+  beforeEach(() => {
+    cy.visit("/resourceadm/ttd/ttd-resources/"); // each test starts the same place
+  });
+
+  it("is possible to visit Resourceadm main page", () => {
+    cy.url().should("include", "/ttd/ttd-resources"); // URL assertion of main page
+  });
+
+  // The next 3 tests navigates through resourceadm error handling: work in progress
+  it("is possible to switch to all, and go to Dashboard via Error page", () => {
+    cy.switchSelectedContext("all"); // triggers Error page as Resourceadm requires
+    // both a valid, single organization and a complimentary org-resources repo, as in
+    // /ttd/ttd-resources/
+    cy.url().should("include", "/resourceadm/all");
+    cy.findAllByRole("link").contains("Gå tilbake til dashboard").click();
+    cy.url().should("include", "/dashboard");
+  });
+
+  it("is possible to switch to self, and go to Dashboard via Error page", () => {
+    cy.switchSelectedContext("self"); // triggers Error page as Resourceadm requires
+    // both a valid, single organization and a complimentary org-resources repo, as in
+    // /ttd/ttd-resources/
+    cy.findAllByRole("link").contains("Gå tilbake til dashboard").click();
+    cy.url().should("include", "/dashboard");
+  });
+
+  it("is possible to switch to all, and return via Redirect page", () => {
+    cy.switchSelectedContext("all"); // sets up for redirect test
+    cy.url().should("include", "/resourceadm/all");
+    cy.visit("/resourceadm/ttd/"); // tests Router redirect in case of correct org,
+    // but missing /org-resources/ segment in URL
+    cy.url().should("include", "/ttd/ttd-resources");
+  });
+});
