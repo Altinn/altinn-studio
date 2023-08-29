09cc13dfabd6ccdd250aed150310bbfb10404585
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-08-29 15:06:39 +0200

    Handle error codes returned by the API

--- frontend/packages/shared/src/contexts/ServicesContext.test.tsx
+++ frontend/packages/shared/src/contexts/ServicesContext.test.tsx
@@ -3,7 +3,24 @@ import { render, renderHook, screen, waitFor } from '@testing-library/react';
 import { ServicesContextProvider } from './ServicesContext';
 import { queriesMock } from 'app-shared/mocks/queriesMock';
 import { useQuery } from '@tanstack/react-query';
-import { textMock } from '../../../../testing/mocks/i18nMock';
+import { mockUseTranslation } from '../../../../testing/mocks/i18nMock';
+
+const texts = {
+  'api_errors.DM_01': 'DM_01 error message',
+  'general.error_message': 'Something went wrong',
+  'general.try_again': 'Try again',
+};
+
+// Mocks:
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    ...mockUseTranslation(texts),
+    i18n: {
+      exists: (key: string) => texts[key] !== undefined,
+    },
+  }),
+  Trans: ({ i18nKey }: { i18nKey: any }) => texts[i18nKey],
+}));
 
 const wrapper = ({ children }) => (
   <ServicesContextProvider {...queriesMock}>
@@ -12,13 +29,45 @@ const wrapper = ({ children }) => (
 );
 
 describe('ServicesContext', () => {
+  it('displays a specific error message if API returns an error code and the error messages does exist', async () => {
+    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+    const { result } = renderHook(() => useQuery(['fetchData'],() => Promise.reject({
+      response: {
+        data: {
+          errorCode: 'DM_01',
+        }
+      }
+    }), { retry: false }), { wrapper });
+
+    await waitFor(() => result.current.isError);
+
+    expect(await screen.findByText(texts['api_errors.DM_01'])).toBeInTheDocument();
+    expect(mockConsoleError).toHaveBeenCalled();
+  });
+
+  it('displays a default error message if API returns an error code but the error message does not exist', async () => {
+    const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
+    const { result } = renderHook(() => useQuery(['fetchData'],() => Promise.reject({
+      response: {
+        data: {
+          errorCode: 'DM_02',
+        }
+      }
+    }), { retry: false }), { wrapper });
+
+    await waitFor(() => result.current.isError);
+
+    expect(await screen.findByText(texts['general.error_message'])).toBeInTheDocument();
+    expect(mockConsoleError).toHaveBeenCalled();
+  });
+
   it('displays a default error message if an API call fails', async () => {
     const mockConsoleError = jest.spyOn(console, 'error').mockImplementation();
     const { result } = renderHook(() => useQuery(['fetchData'],() => Promise.reject(), { retry: false }), { wrapper });
 
     await waitFor(() => result.current.isError);
 
-    expect(await screen.findByText(textMock('general.error_message'))).toBeInTheDocument();
+    expect(await screen.findByText(texts['general.error_message'])).toBeInTheDocument();
     expect(mockConsoleError).toHaveBeenCalled();
   });
 
@@ -28,8 +77,8 @@ describe('ServicesContext', () => {
     const ErrorComponent = () => { throw new Error('Intentional render error'); };
     render(<ErrorComponent />, { wrapper });
 
-    expect(screen.getByText(textMock('general.error_message'))).toBeInTheDocument();
-    expect(screen.getByText(textMock('general.try_again'))).toBeInTheDocument();
+    expect(screen.getByText(texts['general.error_message'])).toBeInTheDocument();
+    expect(screen.getByText(texts['general.try_again'])).toBeInTheDocument();
     expect(mockConsoleError).toHaveBeenCalled();
   });
 });
--- frontend/packages/shared/src/contexts/ServicesContext.tsx
+++ frontend/packages/shared/src/contexts/ServicesContext.tsx
@@ -14,7 +14,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer, Slide, toast } from 'react-toastify';
 import { ErrorBoundary } from 'react-error-boundary';
 import { AxiosError } from 'axios';
-import {  useTranslation } from 'react-i18next';
+import type { i18n } from 'i18next';
+import { useTranslation } from 'react-i18next';
 import { ErrorBoundaryFallback } from '../components/ErrorBoundaryFallback';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -29,13 +30,17 @@ export type ServicesContextProviderProps = ServicesContextProps & {
 
 const ServicesContext = createContext<ServicesContextProps>(null);
 
+interface ApiError {
+  errorCode: string,
+}
+
 const handleError = (
-  error: AxiosError,
+  error: AxiosError<ApiError>,
   t: (key: string) => string,
-  meta: QueryMeta | MutationMeta
+  i18n: i18n,
+  meta: QueryMeta | MutationMeta,
 ): void => {
   // TODO : log axios errors
-  // TODO : handle messages from API
   // TODO : logout user when session is expired
 
   if (
@@ -43,6 +48,15 @@ const handleError = (
     (meta?.hideDefaultError instanceof Function && meta?.hideDefaultError?.(error))
   )
     return;
+
+  if (error?.response?.data?.errorCode) {
+    const errorMessageKey = `api_errors.${error?.response?.data?.errorCode}`;
+
+    if (i18n.exists(errorMessageKey)) {
+      toast.error(t(errorMessageKey), { toastId: errorMessageKey });
+      return;
+    }
+  }
 
   toast.error(
     () => t('general.error_message'),
@@ -56,7 +70,7 @@ export const ServicesContextProvider = ({
   clientConfig,
   ...queries
 }: ServicesContextProviderProps) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const [queryClient] = useState(
     () =>
@@ -64,11 +78,11 @@ export const ServicesContextProvider = ({
       new QueryClient({
         ...clientConfig,
         queryCache: new QueryCache({
-          onError: (error: AxiosError, query) => handleError(error, t, query.options?.meta),
+          onError: (error: AxiosError<ApiError>, query) => handleError(error, t, i18n, query.options?.meta),
         }),
         mutationCache: new MutationCache({
-          onError: (error: AxiosError, variables, context, mutation) =>
-            handleError(error, t, mutation.options?.meta),
+          onError: (error: AxiosError<ApiError>, variables, context, mutation) =>
+            handleError(error, t, i18n, mutation.options?.meta),
         }),
       })
   );
