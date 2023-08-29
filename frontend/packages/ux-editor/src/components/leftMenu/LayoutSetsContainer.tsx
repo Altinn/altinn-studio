7e12b298f1a2d0392cf1749ace7fcd0ae7cf35dc
andrealouisestanderen <andreastanderen@gmail.com>
2023-08-29 14:30:54 +0200

    Update selected layout to first in set when changing selected layout set

--- frontend/packages/ux-editor/src/components/leftMenu/LayoutSetsContainer.tsx
+++ frontend/packages/ux-editor/src/components/leftMenu/LayoutSetsContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLayoutSetsQuery } from '../../hooks/queries/useLayoutSetsQuery';
 import { selectedLayoutSetSelector } from '../../selectors/formLayoutSelectors';
@@ -6,6 +6,7 @@ import { FormLayoutActions } from '../../features/formDesigner/formLayout/formLa
 import { Button } from '@digdir/design-system-react';
 import { typedLocalStorage } from 'app-shared/utils/webStorage';
 import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
+import { useFormLayoutSettingsQuery } from '../../hooks/queries/useFormLayoutSettingsQuery';
 
 export function LayoutSetsContainer() {
   const { org, app } = useStudioUrlParams();
@@ -13,13 +14,19 @@ export function LayoutSetsContainer() {
   const layoutSetsQuery = useLayoutSetsQuery(org, app);
   const layoutSetNames = layoutSetsQuery.data?.sets?.map((set) => set.id);
   const selectedLayoutSet: string = useSelector(selectedLayoutSetSelector);
+  const { data: formLayoutSettings } = useFormLayoutSettingsQuery(org, app, selectedLayoutSet);
+
+  useEffect(() => {
+    if (formLayoutSettings) {
+      const newSelectedLayout = formLayoutSettings.pages.order[0];
+      debugger;
+      dispatch(FormLayoutActions.updateSelectedLayout(newSelectedLayout));
+    }
+  }, [dispatch, formLayoutSettings, selectedLayoutSet]);
 
   const onLayoutSetClick = (set: string) => {
     dispatch(FormLayoutActions.updateSelectedLayoutSet(set));
-    dispatch(FormLayoutActions.updateSelectedLayout(set)); // to edit configs for particular set
     typedLocalStorage.setItem<string>('layoutSet' + app, set);
-    // add field in state saying if selectedEditorLevel is 'layout' or 'layoutset'
-    // check above state in ux-editor and render layoutset-config if level === 'layoutset'
   };
 
   return (
