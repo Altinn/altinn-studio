0b3231c5bfd7a4b4d7ff943693ff9a6e87935138
David Øvrelid <46874830+framitdavid@users.noreply.github.com>
2023-08-31 10:55:53 +0200

    feat(logging): install and start logging exceptions (#10938)
    Logging to Application Insights
    
    ---------
    
    Co-authored-by: Mirko Sekulic <misha.sekulic@gmail.com>

---
+++ backend/src/Designer/Configuration/ApplicationInsightsSettings.cs
@@ -0,0 +1,9 @@
+﻿using Altinn.Studio.Designer.Configuration.Marker;
+
+namespace Altinn.Studio.Designer.Configuration
+{
+    public class ApplicationInsightsSettings
+    {
+        public string InstrumentationKey { get; set; }
+    }
+}
