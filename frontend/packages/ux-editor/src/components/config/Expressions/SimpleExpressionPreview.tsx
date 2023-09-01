379f998ab16f1d9bc644d56cc20641d4f0a830a0
andreastanderen <71079896+standeren@users.noreply.github.com>
2023-09-01 12:19:48 +0200

    Dynamics improvements (#10976)
    * General improvements on dynamics
    
    * Clean up expression handling
    
    * Add divider between subexpressions
    
    * Adapt texts
    
    ---------
    
    Co-authored-by: Tomas <tomas.engebretsen@gmail.com>

---
+++ frontend/packages/ux-editor/src/components/config/Expressions/SimpleExpressionPreview.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Expression,
+  expressionDataSourceTexts,
+  expressionFunctionTexts,
+  Operator,
+  SubExpression
+} from '../../../types/Expressions';
+import classes from './SimpleExpressionPreview.module.css';
+import { ArrowRightIcon } from '@navikt/aksel-icons';
+import { useText } from '../../../hooks';
+
+export type SimpleExpressionPreviewProps = {
+  expression: Expression;
+};
+
+export const SimpleExpressionPreview = ({ expression }: SimpleExpressionPreviewProps) => {
+  const t = useText();
+  return (
+    <>
+      {expression.subExpressions.map((subExp: SubExpression, index: number) => (
+        <div key={subExp.id}>
+          <p>
+            <ArrowRightIcon fontSize='1.5rem'/>
+            {expressionDataSourceTexts(t)[subExp.dataSource]}
+            <span>{subExp.value}</span>
+          </p>
+          <p className={classes.bold}>{expressionFunctionTexts(t)[subExp.function]}</p>
+          <p>
+            <ArrowRightIcon fontSize='1.5rem'/>
+            {expressionDataSourceTexts(t)[subExp.comparableDataSource]}
+            <span>{subExp.comparableValue}</span>
+          </p>
+          {index !== expression.subExpressions.length - 1 && (
+            <center className={classes.bold}>{expression.operator === Operator.And ? 'Og' : 'Eller'}</center>
+          )}
+        </div>
+        )
+      )}
+    </>
+  );
+}
