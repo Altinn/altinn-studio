f52ba96ce3e567ce574fb359b13eb48857e589f2
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-09-08 13:26:31 +0200

    fixed sizeand title for icons

--- frontend/dashboard/components/RepoList/RepoList.tsx
+++ frontend/dashboard/components/RepoList/RepoList.tsx
@@ -188,7 +188,7 @@ export const RepoList = ({
             />,
             <GridActionsCellItem
               className={cn(classes.actionLink, classes.editLink)}
-              icon={<PencilIcon  title="pencile_Icon" className={cn(classes.linkIcon, classes.editLink)} />}
+              icon={<PencilIcon  title={t("dashboard.edit_app_icon")} className={cn(classes.linkIcon, classes.editLink)} />}
               key={`dashboard.edit_app${params.row.id}`}
               label={t('dashboard.edit_app')}
               onClick={() => (window.location.href = editUrl)}
