333b3d80fb7649b215207c08d7a018d5123dc2f3
WilliamThorenfeldt <133344438+WilliamThorenfeldt@users.noreply.github.com>
2023-09-11 15:58:47 +0200

    Feature/10964 refactor resource page (#10990)
    * Adding tests for left navigation
    
    * Implementing tests
    
    * Implementing more tests
    
    * Fixing broken test
    
    * Adding tests for resource dashboardpage
    
    * Adding resource page tests
    
    * Removing thematic area
    
    * Adding comments about new page
    
    * Finalising resource page test
    
    * Implementing about resource tests
    
    * Implementing policy page tests
    
    * Fixing toast on validate policy
    
    * Removing sectors
    
    * replacing isPublicService with visible
    
    * Fixing resourcetype
    
    * Adding status to resource
    
    * Implementing self identified users
    
    * Adding enterpriseUserEnabled
    
    * Implementing availableFortype
    
    * Refactoring fields
    
    * Refactoring
    
    * Adding switch and text field components
    
    * Addding resource switch
    
    * Refactoring dropdown and checkbox
    
    * Fixing input fields
    
    * Adding error handling to right description if delegable
    
    * Implementing contact point
    
    * Adding final error validation
    
    * Fixing backend validation
    
    * Fixing ref issues
    
    * Renaming to ResourcePageInputs
    
    * Fixing broken tests
    
    * Final fixes
    
    * Fixing queriesmock issue
    
    * Fixing resource validation
    
    * Fixing remove error on click checkbox
    
    * removing whitespace

---
+++ frontend/resourceadm/components/ResourcePageInputs/ResourceLanguageTextField.tsx
@@ -0,0 +1,120 @@
+import React, { forwardRef } from 'react';
+import classes from './ResourcePageInputs.module.css';
+import { TextField, Paragraph, Label } from '@digdir/design-system-react';
+import { InputFieldErrorMessage } from './InputFieldErrorMessage';
+
+type ResourceLanguageTextFieldProps = {
+  /**
+   * The label of the text field
+   */
+  label: string;
+  /**
+   * The description of the text field
+   */
+  description: string;
+  /**
+   * The value in the field
+   */
+  value: string;
+  /**
+   * Function that updates the value in the field
+   * @param value the new value
+   * @returns void
+   */
+  onChangeValue: (value: string) => void;
+  /**
+   * Function to be executed when the field is focused
+   * @returns void
+   */
+  onFocus: () => void;
+  /**
+   * The id of the field
+   */
+  id: string;
+  /**
+   * Flag for if the value is valid
+   */
+  isValid: boolean;
+  /**
+   * Function to be executed on key down
+   */
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  /**
+   * Function to be executed on blur
+   * @returns void
+   */
+  onBlur: () => void;
+  /**
+   * Flag for if the error message should be shown
+   */
+  showErrorMessage?: boolean;
+  /**
+   * The error text to be shown
+   */
+  errorText?: string;
+};
+
+/**
+ * @component
+ *    Displays an input textfield for a resource variable that has language support.
+ *
+ * @property {string}[label] - The label of the text field
+ * @property {string}[description] - The description of the text field
+ * @property {string}[value] - The value in the field
+ * @property {function}[onChangeValue] - Function that updates the value in the field
+ * @property {function}[onFocus] - unction to be executed when the field is focused
+ * @property {string}[id] - The id of the field
+ * @property {boolean}[isValid] - Flag for if the value is valid
+ * @property {React.KeyboardEventHandler<HTMLInputElement>}[onKeyDown] - Function to be executed on key down
+ * @property {function}[onBlur] - Function to be executed on blur
+ * @property {boolean}[showErrorMessage] - Flag for if the error message should be shown
+ * @property {string}[errorText] - The error text to be shown
+ *
+ * @returns {React.ReactNode} - The rendered component
+ */
+export const ResourceLanguageTextField = forwardRef<
+  HTMLInputElement,
+  ResourceLanguageTextFieldProps
+>(
+  (
+    {
+      label,
+      description,
+      value,
+      onChangeValue,
+      onFocus,
+      id,
+      isValid,
+      onKeyDown,
+      onBlur,
+      showErrorMessage = false,
+      errorText,
+    },
+    ref
+  ): React.ReactNode => {
+    return (
+      <>
+        <div className={classes.divider} />
+        <Label size='medium' spacing htmlFor={id}>
+          {label}
+        </Label>
+        <Paragraph size='small'>{description}</Paragraph>
+        <div className={classes.inputWrapper}>
+          <TextField
+            value={value}
+            onChange={(e) => onChangeValue(e.target.value)}
+            onFocus={onFocus}
+            id={id}
+            isValid={isValid}
+            ref={ref}
+            onKeyDown={onKeyDown}
+            onBlur={onBlur}
+          />
+          {showErrorMessage && <InputFieldErrorMessage message={errorText} />}
+        </div>
+      </>
+    );
+  }
+);
+
+ResourceLanguageTextField.displayName = 'ResourceLanguageTextField';
---
+++ frontend/resourceadm/components/ResourcePageInputs/index.ts
@@ -0,0 +1,6 @@
+export { ResourceLanguageTextField } from './ResourceLanguageTextField';
+export { ResourceLanguageTextArea } from './ResourceLanguageTextArea';
+export { ResourceTextField } from './ResourceTextField';
+export { ResourceSwitchInput } from './ResourceSwitchInput';
+export { ResourceCheckboxGroup } from './ResourceCheckboxGroup';
+export { ResourceDropdown } from './ResourceDropdown';
