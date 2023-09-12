d9c22ea9fdebb8cc6b126fce607dc1ec300dfd94
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-09-12 13:11:41 +0200

    Adding test and documentation for GoBackButton, deleting old LeftNavBar

---
+++ frontend/packages/shared/src/components/LeftNavigationBar/GoBackButton/GoBackButton.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { GoBackButton, GoBackButtonProps } from './GoBackButton';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+
+const mockBackButtonText: string = 'Go back';
+
+describe('GoBackButton', () => {
+  afterEach(jest.clearAllMocks);
+
+  const mockOnClickBackButton = jest.fn();
+
+  const defaultProps: GoBackButtonProps = {
+    navElementClassName: '.navigationElement',
+    onClickBackButton: mockOnClickBackButton,
+    backButtonText: mockBackButtonText,
+  };
+
+  it('calls the "onClickBackButton" function when the button is clicked', async () => {
+    const user = userEvent.setup();
+    render(<GoBackButton {...defaultProps} />);
+
+    const backButton = screen.getByRole('button', { name: mockBackButtonText });
+    await act(() => user.click(backButton));
+    expect(mockOnClickBackButton).toHaveBeenCalledTimes(1);
+  });
+});
--- frontend/packages/shared/src/components/LeftNavigationBar/GoBackButton/GoBackButton.tsx
+++ frontend/packages/shared/src/components/LeftNavigationBar/GoBackButton/GoBackButton.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import classes from './GoBackButton.module.css';
 import cn from 'classnames';
 import { ArrowLeftIcon } from '@navikt/aksel-icons';
 import { Paragraph } from '@digdir/design-system-react';
 
 export type GoBackButtonProps = {
+  /**
+   * Classname for navigation element
+   */
   navElementClassName: string;
+  /**
+   * Function to be executed when clicking the back button
+   * @returns void
+   */
   onClickBackButton: () => void;
+  /**
+   * Text on the back button
+   */
   backButtonText: string;
 };
 
+/**
+ * @component
+ *    Displays the back button on top of the LeftNavigationBar
+ *
+ * @example
+ *      <GoBackButton
+ *        navElementClassName={classes.navigationElement}
+ *        onClickBackButton={onClickUpperTabBackButton}
+ *        backButtonText={backButtonText}
+ *      />
+ *
+ * @property {string}[navElementClassName] - Classname for navigation element
+ * @property {function}[onClickBackButton] - Function to be executed when clicking the back button
+ * @property {string}[backButtonText] - Text on the back button
+ *
+ * @returns {ReactNode} - The rendered component
+ */
 export const GoBackButton = ({
   navElementClassName,
   onClickBackButton,
   backButtonText,
-}: GoBackButtonProps) => {
+}: GoBackButtonProps): ReactNode => {
   return (
     <button
       className={cn(navElementClassName, classes.backButton)}
