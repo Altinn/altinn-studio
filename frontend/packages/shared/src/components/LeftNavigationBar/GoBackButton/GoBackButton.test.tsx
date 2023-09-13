155e9fe07816701a6402c5294564dce25c9a6134
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-09-13 12:53:24 +0200

    Fixing gobackbutton test

--- frontend/packages/shared/src/components/LeftNavigationBar/GoBackButton/GoBackButton.test.tsx
+++ frontend/packages/shared/src/components/LeftNavigationBar/GoBackButton/GoBackButton.test.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { GoBackButton, GoBackButtonProps } from './GoBackButton';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router-dom';
 
 const mockBackButtonText: string = 'Go back';
 
 describe('GoBackButton', () => {
   afterEach(jest.clearAllMocks);
 
-  const mockOnClickBackButton = jest.fn();
-
   const defaultProps: GoBackButtonProps = {
     className: '.navigationElement',
-    onClick: mockOnClickBackButton,
+    to: '/back',
     text: mockBackButtonText,
   };
 
-  it('calls the "onClickBackButton" function when the button is clicked', async () => {
-    const user = userEvent.setup();
-    render(<GoBackButton {...defaultProps} />);
+  it('calls the "onClickBackButton" function when the button is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <GoBackButton {...defaultProps} />
+      </MemoryRouter>
+    );
 
-    const backButton = screen.getByRole('button', { name: mockBackButtonText });
-    await act(() => user.click(backButton));
-    expect(mockOnClickBackButton).toHaveBeenCalledTimes(1);
+    const backButton = screen.getByRole('link', { name: mockBackButtonText });
+    expect(backButton).toBeInTheDocument();
   });
 });
