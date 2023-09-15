d7a43d5a39dca4b84d4a0104dc970df65f6e56df
Rune T. Larsen <rune@hjemmekino.no>
2023-09-15 08:44:02 +0200

    Fix cloning of repo

--- backend/src/Designer/Controllers/ResourceAdmController.cs
+++ backend/src/Designer/Controllers/ResourceAdmController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Altinn.Studio.Designer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Altinn.Studio.Designer.Controllers
 {
@@ -7,9 +8,18 @@ namespace Altinn.Studio.Designer.Controllers
     /// </summary>
     public class ResourceAdmController : Controller
     {
+        private readonly ISourceControl _sourceControl;
+
+        public ResourceAdmController(ISourceControl sourceControl)
+        {
+            _sourceControl = sourceControl;
+        }
+
+
         [Route("/resourceadm/{*AllValues}")]
         public IActionResult Index(string org, string repo)
         {
+            _sourceControl.VerifyCloneExists(org, repo);
             return View();
         }
     }
