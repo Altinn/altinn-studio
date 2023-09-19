3dfd76c1fd3e54f7ae4bb7f50dffe5cc3bf08a1a
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-09-19 16:13:01 +0200

    Adding basic CSS for the task

--- frontend/app-development/features/SettingsModalButton/SettingsModal/components/Tabs/AbouTab/AboutTab.tsx
+++ frontend/app-development/features/SettingsModalButton/SettingsModal/components/Tabs/AbouTab/AboutTab.tsx
@@ -1,8 +1,11 @@
 import React, { ReactNode } from 'react';
+import classes from './AboutTab.module.css';
 import { useTranslation } from 'react-i18next';
 import { TabHeader } from '../../TabHeader';
 import type { AppConfig } from 'app-shared/types/AppConfig';
-import { Paragraph } from '@digdir/design-system-react';
+import { Label, Paragraph } from '@digdir/design-system-react';
+import { Divider } from 'app-shared/primitives';
+import { Buldings3Icon, PersonCircleIcon } from '@navikt/aksel-icons';
 
 export type AboutTabProps = {
   /**
@@ -38,6 +41,24 @@ export const AboutTab = ({ appConfig, org, app }: AboutTabProps): ReactNode => {
       <Paragraph>{JSON.stringify(appConfig)}</Paragraph>
       <Paragraph>{org}</Paragraph>
       <Paragraph>{app}</Paragraph>
+      <Divider marginless />
+      <div className={classes.contentWrapper}>
+        <Label as='p' spacing className={classes.label}>
+          {t('settings_modal.about_tab_created_for')}
+        </Label>
+        <div className={classes.createdFor}>
+          <Buldings3Icon className={classes.createdForIcon} />
+          <Paragraph className={classes.paragraph}>TODO-Created for</Paragraph>
+        </div>
+        <Label as='p' spacing className={classes.label}>
+          {t('settings_modal.about_tab_created_by')}
+        </Label>
+        <div className={classes.createdBy}>
+          <PersonCircleIcon className={classes.createdByIcon} />
+          <Paragraph className={classes.paragraph}>TODO-Created by</Paragraph>
+        </div>
+        <Paragraph>Dato: TODO-date</Paragraph>
+      </div>
     </div>
   );
 };
