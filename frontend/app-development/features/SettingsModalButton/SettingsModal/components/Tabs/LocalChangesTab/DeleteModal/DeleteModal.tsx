e16c075edaec9021d60585215808bed35824dc10
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-09-20 16:04:02 +0200

    Implementing working delete modal

--- frontend/app-development/features/SettingsModalButton/SettingsModal/components/Tabs/LocalChangesTab/DeleteModal/DeleteModal.tsx
+++ frontend/app-development/features/SettingsModalButton/SettingsModal/components/Tabs/LocalChangesTab/DeleteModal/DeleteModal.tsx
@@ -3,7 +3,7 @@ import classes from './DeleteModal.module.css';
 import { useTranslation } from 'react-i18next';
 import { Modal } from 'app-shared/components/Modal';
 import { TrashIcon } from '@navikt/aksel-icons';
-import { Button, Heading, TextField } from '@digdir/design-system-react';
+import { Button, Heading, Paragraph, TextField } from '@digdir/design-system-react';
 
 export type DeleteModalProps = {
   isOpen: boolean;
@@ -35,14 +35,19 @@ export const DeleteModal = ({
         <div className={classes.titleWrapper}>
           <TrashIcon className={classes.modalIcon} />
           <Heading level={1} size='xsmall'>
-            {t('settings_modal.local_changes_tab_verification_modal_title')}
+            {t('settings_modal.local_changes_tab_delete_modal_title')}
           </Heading>
         </div>
       }
     >
       <div className={classes.contentWrapper}>
+        <Paragraph size='small' className={classes.paragraph}>
+          {t('settings_modal.local_changes_tab_delete_modal_text', {
+            appName,
+          })}
+        </Paragraph>
         <TextField
-          label={t('settings_modal.local_changes_tab_verification_modal_textfield_label')}
+          label={t('settings_modal.local_changes_tab_delete_modal_textfield_label')}
           value={nameToDelete}
           onChange={(e) => setNameToDelete(e.target.value)}
         />
@@ -52,10 +57,16 @@ export const DeleteModal = ({
             color='danger'
             onClick={onDelete}
             disabled={appName !== nameToDelete}
+            size='small'
           >
-            {t('settings_modal.local_changes_tab_verification_modal_delete_button')}
+            {t('settings_modal.local_changes_tab_delete_modal_delete_button')}
           </Button>
-          <Button variant='outline' color='danger' onClick={handleClose}>
+          <Button
+            variant='outline'
+            onClick={handleClose}
+            className={classes.cancelButton}
+            size='small'
+          >
             {t('general.cancel')}
           </Button>
         </div>
