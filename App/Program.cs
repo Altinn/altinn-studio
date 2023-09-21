93e2e1ba961ff4f07b8718206ce67c8d3759c9c5
= <bjosttveit@gmail.com>
2023-09-21 11:07:53 +0200

    update to v8 and fix validation trigger

--- App/Program.cs
+++ App/Program.cs
@@ -55,12 +55,7 @@ void ConfigureWebHostBuilder(IWebHostBuilder builder)
 }
 
 void Configure()
-{
-    if (app.Environment.IsDevelopment())
     {
-        app.UseDeveloperExceptionPage();
-    }
-
     string applicationId = StartupHelper.GetApplicationId();
     if (!string.IsNullOrEmpty(applicationId))
     {
@@ -72,16 +67,5 @@ void Configure()
             c.RoutePrefix = applicationId + "/swagger";
         });
     }
-
-    app.UseDefaultSecurityHeaders();
-    app.UseRouting();
-    app.UseStaticFiles('/' + applicationId);
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllers();
-    });
-    app.UseHealthChecks("/health");
+    app.UseAltinnAppCommonConfiguration();
 }
