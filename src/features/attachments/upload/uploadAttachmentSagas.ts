da14dcbdabee79a2f7dfb88bdb8f8b746066f426
Bjørnar Østtveit <47412359+bjosttveit@users.noreply.github.com>
2023-09-28 11:11:44 +0200

    fix attachment validation (#1527)

--- src/features/attachments/upload/uploadAttachmentSagas.ts
+++ src/features/attachments/upload/uploadAttachmentSagas.ts
@@ -91,8 +91,8 @@ export function* uploadAttachmentSaga({
   } catch (err) {
     let validations: IComponentValidations;
 
-    if (backendFeatures?.jsonObjectInDataResponse && isAxiosError(err) && err.response?.data?.result) {
-      const validationIssues: BackendValidationIssue[] = err.response.data.result;
+    if (backendFeatures?.jsonObjectInDataResponse && isAxiosError(err) && err.response?.data) {
+      const validationIssues: BackendValidationIssue[] = err.response.data;
 
       validations = {
         simpleBinding: {
