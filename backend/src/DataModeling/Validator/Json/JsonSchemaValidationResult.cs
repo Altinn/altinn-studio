32125a8b5621ba4b9da3aa919059778aa563b521
Mirko Sekulic <misha.sekulic@gmail.com>
2023-09-29 09:12:54 +0200

    feat: json schema validation (#11249)
    * init json schema validation
    
    * init validation service
    
    * Added validation in update jsonschema endpoint
    
    * fix test file path
    
    * Use 422 instead of 400
    
    * fix validation and add invalid reference test case

---
+++ backend/src/DataModeling/Validator/Json/JsonSchemaValidationResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Altinn.Studio.DataModeling.Validator.Json
+{
+    public class JsonSchemaValidationResult
+    {
+        public IReadOnlyList<JsonSchemaValidationIssue> ValidationIssues { get; }
+
+        public JsonSchemaValidationResult(List<JsonSchemaValidationIssue> validationIssues)
+        {
+            ValidationIssues = validationIssues;
+        }
+
+        public bool IsValid => ValidationIssues.Count == 0;
+
+    }
+}
