e280fbd0d01bd40f849f8fb9b2f85fb2b4584608
LAPTOP-S0QU96IH\david <david@framit.no>
2023-10-02 19:40:49 +0200

    added more expect to ensure the icon is formed correctly

--- frontend/libs/altinn-icons/src/icons/Icons.test.tsx
+++ frontend/libs/altinn-icons/src/icons/Icons.test.tsx
@@ -6,11 +6,14 @@ import React from 'react';
 // Get all icon names from the AltinnIcons module
 const altinnIcons = Object.keys(AltinnIcons);
 
-it.each(altinnIcons)('should render %s icon with fill currentColor', (iconName) => {
+it.each(altinnIcons)('should render %s icon with correct attributes', (iconName) => {
   const IconComponent = AltinnIcons[iconName];
   render(<IconComponent role='img' />);
 
   const iconElement = screen.getByRole('img');
 
   expect(iconElement).toHaveAttribute('fill', 'currentColor');
+  expect(iconElement).toHaveAttribute('width', '24');
+  expect(iconElement).toHaveAttribute('height', '24');
+  expect(iconElement).toHaveAttribute('viewBox', '0 0 24 24');
 });
