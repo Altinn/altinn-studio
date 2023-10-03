075a262f81cbea43d5f3b8b06a6607c7c25dce2b
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-10-03 16:17:34 +0200

    added test

--- frontend/packages/ux-editor/src/components/config/componentSpecificContent/FileUpload/FileUploadComponent.test.tsx
+++ frontend/packages/ux-editor/src/components/config/componentSpecificContent/FileUpload/FileUploadComponent.test.tsx
@@ -5,7 +5,8 @@ import { useLayoutSchemaQuery } from '../../../../hooks/queries/useLayoutSchemaQ
 import { FileUploadComponent } from './FileUploadComponent';
 import { ComponentType } from 'app-shared/types/ComponentType';
 import type { FormFileUploaderComponent } from '../../../../types/FormComponent';
-import { waitFor } from '@testing-library/react';
+import { waitFor, screen } from '@testing-library/react';
+import { textMock } from '../../../../../../../testing/mocks/i18nMock';
 
 // Test data:
 const component: FormFileUploaderComponent = {
@@ -30,6 +31,42 @@ const defaultProps: IGenericEditComponent = {
 describe('FileUploadComponent', () => {
   it('Renders without errors', async () => {
     await render();
+  });
+
+  it('render radio button for all fil types', async () => {
+    await render();
+    <FileUploadComponent {...defaultProps} />;
+    const radioButtonForAllFile = screen.getByRole('radio', {
+      name: textMock('ux_editor.modal_properties_valid_file_endings_all'),
+    });
+    expect(radioButtonForAllFile).toBeInTheDocument();
+  });
+
+  it('render radio button for custom file types', async () => {
+    await render();
+    <FileUploadComponent {...defaultProps} />;
+    const radioButtonForCustomFile = screen.getByRole('radio', {
+      name: textMock('ux_editor.modal_properties_valid_file_endings_custom'),
+    });
+    expect(radioButtonForCustomFile).toBeInTheDocument();
+  });
+
+  it('render radio button when a simple file is uploaded', async () => {
+    await render();
+    <FileUploadComponent {...defaultProps} />;
+    const radioButtonForSimpleFile = screen.getByRole('radio', {
+      name: textMock('ux_editor.modal_properties_file_upload_simple'),
+    });
+    expect(radioButtonForSimpleFile).toBeInTheDocument();
+  });
+
+  it('render radio button when a list of files is uploaded', async () => {
+    await render();
+    <FileUploadComponent {...defaultProps} />;
+    const radioButtonForList = screen.getByRole('radio', {
+      name: textMock('ux_editor.modal_properties_file_upload_list'),
+    });
+    expect(radioButtonForList).toBeInTheDocument();
   });
 });
 
