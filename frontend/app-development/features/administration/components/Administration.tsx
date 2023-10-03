cbd16b57ebc5713ca90501c63733637c97a37aea
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-10-03 11:47:27 +0200

    Replace metadata with appConfig

--- frontend/app-development/features/administration/components/Administration.tsx
+++ frontend/app-development/features/administration/components/Administration.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import classes from './Administration.module.css';
-import { useAppMetadataQuery } from 'app-development/hooks/queries';
+import { useAppConfigQuery } from 'app-development/hooks/queries';
 import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
 import { PageSpinner } from 'app-shared/components/PageSpinner';
 
 export const Administration = () => {
   const { org, app } = useStudioUrlParams();
-  const { data: metadata, isLoading } = useAppMetadataQuery(org, app);
+  const { data: appConfigData, isLoading } = useAppConfigQuery(org, app);
 
   if (isLoading) return <PageSpinner />;
 
   return (
     <div className={classes.administration}>
-      <div className={classes.overlay}></div>
       <div className={classes.container}>
-        <h1 className={classes.header}>{metadata?.title?.nb}</h1>
+        <h1 className={classes.header}>{appConfigData.serviceName}</h1>
         <div className={classes.content}>
           <main className={classes.main}>
             <div className={classes.placeholder}>{/* APP STATUS PLACEHOLDER */}</div>
