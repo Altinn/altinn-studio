86da8402734716ac9c4c68ab6734a6e0afa88b4e
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2023-10-05 13:38:00 +0200

    Remove preview layout set from local storage (#11286)

--- frontend/packages/shared/src/hooks/useLocalStorage.ts
+++ frontend/packages/shared/src/hooks/useLocalStorage.ts
@@ -4,8 +4,8 @@ import { TypedStorage, typedLocalStorage } from 'app-shared/utils/webStorage';
 const useWebStorage = <T>(
   typedStorage: TypedStorage,
   key: string,
-  initialValue?: T
-): [T, (newValue: T) => void] => {
+  initialValue?: T,
+): [T, (newValue: T) => void, () => void] => {
   const [value, setValue] = useState<T>(typedStorage.getItem(key) || initialValue);
 
   const setStorageValue = useCallback(
@@ -13,15 +13,21 @@ const useWebStorage = <T>(
       typedStorage.setItem(key, newValue);
       setValue(newValue);
     },
-    [key, typedStorage]
+    [key, typedStorage],
   );
-  return [value, setStorageValue];
+
+  const removeStorageValue = useCallback(() => {
+    typedStorage.removeItem(key);
+    setValue(undefined);
+  }, [key, typedStorage]);
+
+  return [value, setStorageValue, removeStorageValue];
 };
 
 /**
  * @param key - the key to use in local storage
  * @param initialValue - the initial value to use if there is no value in local storage
- * @returns [value, setValue] - the value and a function to set the value
+ * @returns [value, setValue, removeValue] - the value, a function to set the value and a function to remove it
  * @description
  * useLocalStorage is a hook that allows you to use local storage the same way you would with useState
  */
