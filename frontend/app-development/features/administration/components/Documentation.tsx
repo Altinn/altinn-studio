022503d82ebfdbd1d215986ef90dcfb650c562a3
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-10-10 12:52:23 +0200

    Move texts to i18n files

--- frontend/app-development/features/administration/components/Documentation.tsx
+++ frontend/app-development/features/administration/components/Documentation.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import classes from './Documentation.module.css';
 import { Heading, Link, Paragraph } from '@digdir/design-system-react';
 import { ExternalLinkIcon } from '@navikt/aksel-icons';
+import { useTranslation } from 'react-i18next';
 
 export const Documentation = () => {
+  const { t } = useTranslation();
   return (
     <div className={classes.documentation}>
       <Heading level={2} size='xxsmall' className={classes.heading}>
-        Kom i gang
+        {t('administration.documentation.title')}
       </Heading>
       <Paragraph size='small' className={classes.content}>
-        Som ny bruker av Altinn Studio kan det være vanskelig å komme i gang. Her har du en guide
-        som skal hjelpe deg
+        {t('administration.documentation.content')}
       </Paragraph>
-      <Link href='https://docs.altinn.studio/altinn-studio/' className={classes.guideLink}>
-        <span>Les vår guide</span>
-        <ExternalLinkIcon className={classes.guideLinkIcon} />
+      <Link href='https://docs.altinn.studio/altinn-studio/' className={classes.link}>
+        <span>{t('administration.documentation.link')}</span>
+        <ExternalLinkIcon className={classes.linkIcon} />
       </Link>
     </div>
   );
