e882a6bc390f2205526056171be9d277b75b0f4b
WilliamThorenfeldt <133344438+WilliamThorenfeldt@users.noreply.github.com>
2023-10-12 14:20:08 +0200

    Fixing style policy editor, adding error handling, and connecting API for action and subject (#11325)
    * Implementing working SettingsModalButton
    
    * Refactoring code, and adding more tests based on feedback
    
    * adding feature flag
    
    * Fixing bug on GiteaHeader
    
    * Fixing styles
    
    * Implements API for subject and action
    
    * addign error handling
    
    * Fixing comments from PR
    
    * almost fixed tests
    
    * Fixing the tests
    
    * Cleanup code
    
    * adding feature flag
    
    * feature flag fix
    
    * remmoving comments
    
    * testing
    
    * testing
    
    * testing
    
    * removing console.log
    
    * removing husky things
    
    * fixing
    
    * Fixing feedback from PR
    
    * fixing subjects bug
    
    * fixing broken test
    
    * fixing broken tests

---
+++ frontend/packages/shared/src/hooks/queries/useResourcePolicySubjectsQuery.ts
@@ -0,0 +1,44 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { useServicesContext } from 'app-shared/contexts/ServicesContext';
+import type { PolicySubject } from '@altinn/policy-editor';
+import { QueryKey } from 'app-shared/types/QueryKey';
+import { AxiosError } from 'axios';
+
+const policySubjectOrg: PolicySubject = {
+  subjectDescription: '[org]',
+  subjectId: '[org]',
+  subjectSource: '[org]',
+  subjectTitle: '[org]',
+};
+
+/**
+ * Query to get the list of subjects for a policy.
+ *
+ * @param org the organisation of the user
+ * @param repo the repo the user is in
+ * @param addOrgToList if the value for org should be added to the list of subjects
+ *
+ * @returns UseQueryResult with a list of subjects of PolicySubject
+ */
+export const useResourcePolicySubjectsQuery = (
+  org: string,
+  repo: string,
+  addOrgToList?: boolean,
+): UseQueryResult<PolicySubject[], AxiosError> => {
+  const { getPolicySubjects } = useServicesContext();
+
+  return useQuery<PolicySubject[], AxiosError>(
+    [QueryKey.ResourcePolicySubjects, org, repo],
+    () => getPolicySubjects(org, repo),
+    {
+      select: (policySubjects) => {
+        if (
+          addOrgToList &&
+          !(policySubjects || []).some((d) => d.subjectId === policySubjectOrg.subjectId)
+        )
+          policySubjects.push(policySubjectOrg);
+        return policySubjects;
+      },
+    },
+  );
+};
