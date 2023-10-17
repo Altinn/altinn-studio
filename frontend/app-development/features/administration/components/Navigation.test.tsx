23cc7a5e011f6d3e0135bd4a221bb75eed39156f
Nina Kylstad <nkylstad@gmail.com>
2023-10-17 15:48:43 +0200

    use renderWithProviders for test

--- frontend/app-development/features/administration/components/Navigation.test.tsx
+++ frontend/app-development/features/administration/components/Navigation.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { Navigation } from './Navigation';
 import { TopBarMenu, menu } from 'app-development/layout/AppBar/appBarConfig';
 import { textMock } from '../../../../testing/mocks/i18nMock';
+import { renderWithProviders } from 'app-development/test/testUtils';
+import { APP_DEVELOPMENT_BASENAME } from 'app-shared/constants';
 
 describe('Navigation', () => {
   it('renders component', async () => {
-    render(<Navigation />);
+    renderWithProviders(<Navigation />, {
+      startUrl: `${APP_DEVELOPMENT_BASENAME}/my-org/my-app`,
+    });
 
     menu
       .filter((item) => item.key !== TopBarMenu.About)
