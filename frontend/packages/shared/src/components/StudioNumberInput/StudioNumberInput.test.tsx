061475a069e41a2f34a1e40fd6889aba065550cd
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-11-02 09:08:09 +0100

    updated test

--- frontend/packages/shared/src/components/StudioNumberInput/StudioNumberInput.test.tsx
+++ frontend/packages/shared/src/components/StudioNumberInput/StudioNumberInput.test.tsx
@@ -4,42 +4,39 @@ import { StudioNumberInput, StudioNumberInputProps } from './StudioNumberInput';
 import userEvent from '@testing-library/user-event';
 
 const user = userEvent.setup();
-
+const description = 'description';
 describe('StudioNumberInput', () => {
   it('should render description and input field', () => {
-    render({ description: 'description' });
+    render({ description });
     expect(screen.getByText('description')).toBeInTheDocument();
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   it('should update input value on change', async () => {
-    render({ description: 'description' });
+    render({ description });
     const inputElement = screen.getByRole('textbox');
     await act(() => user.type(inputElement, '123'));
-
     expect(inputElement).toHaveValue('123');
   });
 
   it('should not allow non-numeric input', async () => {
-    render({ description: 'description' });
+    render({ description });
     const inputElement = screen.getByRole('textbox');
     await act(() => user.type(inputElement, 'abc'));
-
     expect(inputElement).toHaveValue('');
   });
 
   it('should not allow non-numeric input after numeric input', async () => {
-    render({ description: 'description' });
+    render({ description });
     const inputElement = screen.getByRole('textbox');
     await act(() => user.type(inputElement, '123abc'));
-
     expect(inputElement).toHaveValue('123');
   });
 });
 
 const render = (props: Partial<StudioNumberInputProps> = {}) => {
   const allProps: StudioNumberInputProps = {
-    description: 'description',
+    description,
     ...props,
   } as StudioNumberInputProps;
   rtlRender(<StudioNumberInput {...allProps} />);
