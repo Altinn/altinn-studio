1fa1b77b03141dceeb6855858ab24f2604219077
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-11-07 08:00:10 +0100

    fixing comment from PR

--- frontend/packages/shared/src/components/AltinnHeaderProfile/hooks/useUserNameAndOrg.ts
+++ frontend/packages/shared/src/components/AltinnHeaderProfile/hooks/useUserNameAndOrg.ts
@@ -1,6 +1,6 @@
 import { Repository } from 'app-shared/types/Repository';
 import { User } from 'app-shared/types/User';
-import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 /**
  * Hook that returns the username and org of a logged in user.
@@ -8,27 +8,19 @@ import i18next from 'i18next';
  * @param user the User that is logged in
  * @param org the organisatio of the user
  * @param repository the reposiroty the application is in
- * @param t translation function
  *
  * @returns {string} - The username and org
  */
-export const useUserNameAndOrg = (
-  user: User,
-  org: string,
-  repository: Repository,
-  t: typeof i18next.t,
-): string => {
-  const getUserNameAndOrg = () => {
-    const userName: string = user?.full_name || user?.login;
-    if (!repository) return userName;
-    if (org && user.login !== org) {
-      return t('shared.header_user_for_org', {
-        user: user.full_name || user.login,
-        org: repository.owner.full_name || repository.owner.login,
-      });
-    }
-    return userName;
-  };
+export const useUserNameAndOrg = (user: User, org: string, repository: Repository): string => {
+  const { t } = useTranslation();
 
-  return getUserNameAndOrg();
+  if (repository && org && user.login !== org) {
+    return t('shared.header_user_for_org', {
+      user: getUsername(user),
+      org: getUsername(repository.owner),
+    });
+  }
+  return getUsername(user);
 };
+
+const getUsername = (user: User | Repository['owner']) => user?.full_name || user?.login;
