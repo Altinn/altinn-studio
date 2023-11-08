0441bdcc1c78c3d2a0e2803dd9f7f40f57948b6c
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2023-11-08 12:53:40 +0100

    Schema editor fields cleanup (#11519)

--- frontend/packages/shared/src/components/AltinnTextField/AltinnTextField.tsx
+++ frontend/packages/shared/src/components/AltinnTextField/AltinnTextField.tsx
@@ -1,17 +1,14 @@
-import {
-  LegacyTextField,
-  LegacyTextFieldProps as TextFieldProps,
-} from '@digdir/design-system-react';
+import { Textfield, TextfieldProps } from '@digdir/design-system-react';
 import React from 'react';
 import cn from 'classnames';
 import classes from './AltinnTextField.module.css';
 
-export type AltinnTextFieldProps = TextFieldProps & {
+export type AltinnTextFieldProps = TextfieldProps & {
   withAsterisk?: boolean;
 };
 
 export const AltinnTextField = ({ withAsterisk, ...rest }: AltinnTextFieldProps) => (
   <span className={cn(withAsterisk && classes.withAsterisk)}>
-    <LegacyTextField {...rest} />
+    <Textfield {...rest} />
   </span>
 );
--- frontend/packages/ux-editor/src/components/config/Expressions/DataSourceValue.tsx
+++ frontend/packages/ux-editor/src/components/config/Expressions/DataSourceValue.tsx
@@ -12,7 +12,7 @@ import { useFormLayoutsQuery } from '../../../hooks/queries/useFormLayoutsQuery'
 import { getComponentIds, getDataModelElementNames } from '../../../utils/expressionsUtils';
 import { useText } from '../../../hooks';
 import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
-import { useAppContext } from "../../../hooks/useAppContext";
+import { useAppContext } from '../../../hooks/useAppContext';
 
 export interface DataSourceValueProps {
   subExpression: SubExpression;
