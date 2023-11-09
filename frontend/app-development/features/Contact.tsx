f743345b3cac0c19b34319afb26aebb0e21485b7
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-11-09 12:47:24 +0100

    Add breadcrumb & clean css

--- frontend/app-development/features/Contact.tsx
+++ frontend/app-development/features/Contact.tsx
@@ -1,56 +1,90 @@
 import React from 'react';
 import classes from './Contact.module.css';
-import { Heading, Paragraph } from '@digdir/design-system-react';
+import { useAppConfigQuery } from 'app-development/hooks/queries';
+import { Heading, Link, Paragraph } from '@digdir/design-system-react';
+import { toast } from 'react-toastify';
 import { Trans, useTranslation } from 'react-i18next';
-import { EnvelopeClosedIcon } from '@navikt/aksel-icons';
+import { EnvelopeClosedIcon, ChevronRightIcon } from '@navikt/aksel-icons';
 import Slack from 'app-shared/icons/Slack.svg';
+import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
+import { AltinnSpinner } from 'app-shared/components';
+import { Center } from 'app-shared/components/Center';
+import { RoutePaths } from 'app-development/enums/RoutePaths';
 
 export const Contact = () => {
+  const { org, app } = useStudioUrlParams();
+
+  const {
+    data: appConfigData,
+    isError: isAppConfigError,
+    isLoading: isLoadingAppConfig,
+  } = useAppConfigQuery(org, app, { hideDefaultError: true });
   const { t } = useTranslation();
+
+  if (isAppConfigError) {
+    toast.error(t('contact.fetch_app_error_message'));
+  }
+
+  if (isLoadingAppConfig) {
+    return (
+      <Center>
+        <AltinnSpinner spinnerText={t('general.loading')} className={classes.spinner} />
+      </Center>
+    );
+  }
+
+  const appName = appConfigData?.serviceName || app;
 
   return (
     <div className={classes.pageContainer}>
       <div className={classes.container}>
-        <div className={classes.header}>
-          <Heading size='xlarge'>{t('contact.heading')}</Heading>
+        <div className={classes.breadcrumb}>
+          <Link href={RoutePaths.Overview}>{appName}</Link>
+          <ChevronRightIcon />
+          {t('contact.heading')}
         </div>
-        <div className={classes.block}>
-          <div className={classes.iconContainer}>
-            <EnvelopeClosedIcon className={classes.icon} />
+        <div className={classes.content}>
+          <div className={classes.heading}>
+            <Heading size='xlarge'>{t('contact.heading')}</Heading>
           </div>
-          <div className={classes.content}>
-            <Heading level={2} size='xsmall'>
-              {t('contact.email.heading')}
-            </Heading>
-            <Paragraph>{t('contact.email.content')}</Paragraph>
-            <Paragraph className={classes.link}>
-              <Trans i18nKey='contact.email.link'>
-                <a />
-              </Trans>
-            </Paragraph>
+          <div className={classes.block}>
+            <div className={classes.iconContainer}>
+              <EnvelopeClosedIcon className={classes.icon} />
+            </div>
+            <div className={classes.textContainer}>
+              <Heading level={2} size='xsmall'>
+                {t('contact.email.heading')}
+              </Heading>
+              <Paragraph>{t('contact.email.content')}</Paragraph>
+              <Paragraph className={classes.link}>
+                <Trans i18nKey='contact.email.link'>
+                  <Link> </Link>
+                </Trans>
+              </Paragraph>
+            </div>
           </div>
-        </div>
-        <div className={classes.block}>
-          <div className={classes.iconContainer}>
-            <Slack />
-          </div>
-          <div className={classes.content}>
-            <Heading level={2} size='xsmall'>
-              {t('contact.slack.heading')}
-            </Heading>
-            <Paragraph>{t('contact.slack.content')}</Paragraph>
-            <Paragraph>
-              <ul>
-                {t('contact.slack.content_list', { returnObjects: true }).map((item) => (
-                  <li key={item}>{item}</li>
-                ))}
-              </ul>
-            </Paragraph>
-            <Paragraph className={classes.link}>
-              <Trans i18nKey='contact.slack.link'>
-                <a />
-              </Trans>
-            </Paragraph>
+          <div className={classes.block}>
+            <div className={classes.iconContainer}>
+              <Slack />
+            </div>
+            <div className={classes.textContainer}>
+              <Heading level={2} size='xsmall'>
+                {t('contact.slack.heading')}
+              </Heading>
+              <Paragraph>{t('contact.slack.content')}</Paragraph>
+              <Paragraph>
+                <ul>
+                  {t('contact.slack.content_list', { returnObjects: true }).map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </Paragraph>
+              <Paragraph className={classes.link}>
+                <Trans i18nKey='contact.slack.link'>
+                  <Link> </Link>
+                </Trans>
+              </Paragraph>
+            </div>
           </div>
         </div>
       </div>
