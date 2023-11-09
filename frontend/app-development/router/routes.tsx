957464f3792cb0e2ff2498e58117284224bda31e
Michael Queyrichon <michael.queyrichon@digdir.no>
2023-11-09 16:54:32 +0100

    Clean up RouterRoute properties

--- frontend/app-development/router/routes.tsx
+++ frontend/app-development/router/routes.tsx
@@ -8,7 +8,6 @@ import { DeployPage } from '../features/appPublish/pages/deployPage';
 import { ProcessEditor } from 'app-development/features/processEditor';
 import { shouldDisplayFeature } from 'app-shared/utils/featureToggleUtils';
 import { RoutePaths } from 'app-development/enums/RoutePaths';
-import { TopBarMenu } from 'app-shared/enums/TopBarMenu';
 
 interface IRouteProps {
   headerTextKey?: string;
@@ -25,61 +24,37 @@ interface IRouteProps {
 
 interface RouterRoute {
   path: RoutePaths;
-  exact: boolean;
-  activeSubHeaderSelection: TopBarMenu;
   subapp: any;
-  activeLeftMenuSelection?: string;
   props?: IRouteProps;
 }
 
 export const routerRoutes: RouterRoute[] = [
   {
     path: RoutePaths.UIEditor,
-    exact: true,
-    activeSubHeaderSelection: TopBarMenu.Create,
-    activeLeftMenuSelection: 'UI-Editor',
     subapp: SubApp,
   },
   {
     path: RoutePaths.Overview,
-    exact: true,
-    activeSubHeaderSelection: TopBarMenu.About,
-    activeLeftMenuSelection: 'Om appen',
     subapp: shouldDisplayFeature('newAdministration') ? Administration : LegacyAdministration,
   },
   {
     path: RoutePaths.Contact,
-    exact: true,
-    activeSubHeaderSelection: TopBarMenu.About,
-    activeLeftMenuSelection: 'Om appen',
     subapp: Contact,
   },
   {
     path: RoutePaths.Datamodel,
-    exact: true,
-    activeSubHeaderSelection: TopBarMenu.Datamodel,
-    activeLeftMenuSelection: '',
     subapp: DataModellingContainer,
   },
   {
     path: RoutePaths.Deploy,
-    exact: true,
-    activeSubHeaderSelection: TopBarMenu.Deploy,
-    activeLeftMenuSelection: '',
     subapp: DeployPage,
   },
   {
-    activeSubHeaderSelection: TopBarMenu.Text,
-    activeLeftMenuSelection: 'Tekster',
     path: RoutePaths.Text,
-    exact: true,
     subapp: TextEditor,
   },
   {
-    activeSubHeaderSelection: TopBarMenu.ProcessEditor,
-    activeLeftMenuSelection: '',
     path: RoutePaths.ProcessEditor,
-    exact: true,
     subapp: ProcessEditor,
   },
 ];
