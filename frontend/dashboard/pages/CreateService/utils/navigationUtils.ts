0c57752b9722cef151bac9ad8f6a8d2e211afb7f
LAPTOP-S0QU96IH\david <david@framit.no>
2023-11-15 14:00:25 +0100

    fixed lint issue

--- frontend/dashboard/pages/CreateService/utils/navigationUtils.ts
+++ frontend/dashboard/pages/CreateService/utils/navigationUtils.ts
@@ -1,6 +1,6 @@
 ﻿import { getAppDevelopmentRootRoute } from '../../../utils/urlUtils';
 
 export const navigateToAppDevelopment = (org: string, repo: string): void => {
-    const appDevelopmentRootRoute: string = getAppDevelopmentRootRoute({org, repo});
-    window.location.assign(appDevelopmentRootRoute);
+  const appDevelopmentRootRoute: string = getAppDevelopmentRootRoute({ org, repo });
+  window.location.assign(appDevelopmentRootRoute);
 };
