bf55c5ac1cba4076f5ce028a9cfef81b6453b06d
JamalAlabdullah <90609090+JamalAlabdullah@users.noreply.github.com>
2023-11-21 13:59:23 +0100

    11559 deploy regression test w46 (#11596)
    * Added new tests to Cypress tests, including adding objects to the data model and generating the data model.

--- frontend/testing/cypress/src/selectors/dashboard.js
+++ frontend/testing/cypress/src/selectors/dashboard.js
@@ -4,17 +4,21 @@ import * as testids from '../../../testids';
 const getAllAppsHeader = () => cy.findByRole('heading', { name: texts['dashboard.all_apps'] });
 const getUserAppsListHeader = () => cy.findByRole('heading', { name: texts['dashboard.my_apps'] });
 const getFavouritesHeader = () => cy.findByRole('heading', { name: texts['dashboard.favourites'] });
-const getSearchResultsHeader = () => cy.findByRole('heading', { name: texts['dashboard.search_result'] });
-const getOrgAppsHeader = (org) => cy.findByRole('heading', { name: texts['dashboard.org_apps'].replace('{{orgName}}', org) });
+const getSearchResultsHeader = () =>
+  cy.findByRole('heading', { name: texts['dashboard.search_result'] });
+const getOrgAppsHeader = (org) =>
+  cy.findByRole('heading', { name: texts['dashboard.org_apps'].replace('{{orgName}}', org) });
 const getUserAppsList = () => getUserAppsListHeader().next();
 const getSearchResults = () => getSearchResultsHeader().next();
-const getLinksCellForApp = (table, name) => table.findByRole('cell', { name }).siblings('div[data-field=\'links\']');
+const getLinksCellForApp = (table, name) =>
+  table.findByRole('cell', { name }).siblings("div[data-field='links']");
 
 export const dashboard = {
   getAllAppsHeader,
   getAppOwnerField: () => cy.findByRole('combobox', { name: texts['general.service_owner'] }),
   getCancelButton: () => cy.findByRole('button', { name: texts['general.cancel'] }),
-  getCreateAppButton: () => cy.findByRole('button', { name: texts['dashboard.create_service_btn'] }),
+  getCreateAppButton: () =>
+    cy.findByRole('button', { name: texts['dashboard.create_service_btn'] }),
   getFavourites: () => getFavouritesHeader().next(),
   getFavouritesHeader,
   getLinksCellForSearchResultApp: (name) => getLinksCellForApp(getSearchResults(), name),
