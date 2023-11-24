aad037a829a54a106d431dfbb1e8db43666e625c
Nina Kylstad <nkylstad@gmail.com>
2023-11-24 11:04:06 +0100

    added some tests

---
+++ frontend/testing/cypress/src/selectors/administrationTab.js
@@ -0,0 +1,7 @@
+import * as texts from '@altinn-studio/language/src/nb.json';
+
+export const administrationTab = {
+  getAppNameField: () =>
+    cy.findByRole('textbox', { name: texts['settings_modal.about_tab_name_label'] }),
+  getHeader: () => cy.findByRole('heading', { name: texts['administration.administration'] }),
+};
