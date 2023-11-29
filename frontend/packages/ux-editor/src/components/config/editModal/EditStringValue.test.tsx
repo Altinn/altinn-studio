a507b684714a914ec4d22291335ac5fea5907b98
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2023-11-29 16:12:16 +0100

    remove unused fireEvenet

--- frontend/packages/ux-editor/src/components/config/editModal/EditStringValue.test.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditStringValue.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 
 import { EditStringValue } from './EditStringValue';
 import { renderWithMockStore, renderHookWithMockStore } from '../../../testing/mocks';
