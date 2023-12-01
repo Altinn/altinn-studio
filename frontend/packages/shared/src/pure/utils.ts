36b5167473b9508d931ba39a0905f3c302d8f54b
WilliamThorenfeldt <133344438+WilliamThorenfeldt@users.noreply.github.com>
2023-12-01 07:03:14 +0100

    Add deprecated (#11722)

--- frontend/packages/shared/src/pure/utils.ts
+++ frontend/packages/shared/src/pure/utils.ts
@@ -1,9 +1,1 @@
-export const isNumeric = (str: string) => parseInt(str).toString() === str;
 export const deepCopy = <T>(value: T) => JSON.parse(JSON.stringify(value)) as T;
-export const updateKey = (obj: any, key: string, val: string) => ({ ...deepCopy(obj), [key]: val });
-
-export const removeKey = (obj: any, key: string) => {
-  const copy = deepCopy(obj);
-  delete copy[key];
-  return copy;
-};
