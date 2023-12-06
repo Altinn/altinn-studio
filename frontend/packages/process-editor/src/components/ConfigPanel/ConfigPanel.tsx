e1e867ac82ff8dc54e9160048a5742580bf4296d
Nina Kylstad <nkylstad@gmail.com>
2023-12-06 08:29:39 +0100

    Created ConfigPanel component (#11788)
    * Created ConfigPanel component
    
    * remove unused import
    
    * PR comments
    
    * use heading from designsystem
    
    * update test

---
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigPanel.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import classes from './ConfigPanel.module.css';
+import { VersionAlert } from './VersionAlert';
+import { useTranslation } from 'react-i18next';
+import { Alert, Heading, Paragraph } from '@digdir/design-system-react';
+import { useBpmnContext } from '../../contexts/BpmnContext';
+
+/**
+ * @component
+ *  Displays the configuration panel area of the ProcessEditor
+ *
+ * @returns {JSX.Element} - The rendered component
+ */
+export const ConfigPanel = (): JSX.Element => {
+  const { t } = useTranslation();
+  const { isEditAllowed } = useBpmnContext();
+  return (
+    <div className={classes.configPanel}>
+      {!isEditAllowed && <VersionAlert />}
+      <div className={classes.content}>
+        <Heading level={2} size='xsmall'>
+          {t('process_editor.configuration_panel_heading')}
+        </Heading>
+        <Alert severity='info'>
+          <Heading level={3} size='xxsmall'>
+            {t('process_editor.configuration_panel.under_development_title')}
+          </Heading>
+          <Paragraph size='small'>
+            {t('process_editor.configuration_panel.under_development_body')}
+          </Paragraph>
+        </Alert>
+      </div>
+    </div>
+  );
+};
