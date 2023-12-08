78e90f7d6b34e3e776826820b1cfd579d6972d5d
LAPTOP-S0QU96IH\david <david@framit.no>
2023-12-08 11:45:09 +0100

    work in progress

---
+++ playwright.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, devices } from '@playwright/test';
+import { config } from 'dotenv';
+
+config();
+
+type SupportedEnvironment = 'local' | 'dev' | 'prod';
+const environmentMap: Record<SupportedEnvironment, string> = {
+  local: 'studio.localhost',
+  dev: 'https://dev.altinn.studio',
+  prod: 'https://altinn.studio',
+};
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './frontend/testing/playwright',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], baseURL: 'studio.localhost' },
+    },
+  ],
+});
