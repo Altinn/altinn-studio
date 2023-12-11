619f1d846a6091fadadff6dff4bb328ab7a8adbb
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-12-11 11:26:04 +0100

    renaming

--- frontend/libs/studio-pure-functions/src/arrayUtils/arrayUtils.ts
+++ frontend/libs/studio-pure-functions/src/arrayUtils/arrayUtils.ts
@@ -1,8 +1,8 @@
 export class ArrayUtils {
   /**
-   * Removes duplicates from an array.
-   * @param array The array of interest.
-   * @returns The array without duplicates.
+   * Removes duplicates from an array
+   * @param array The array of interest
+   * @returns The array without duplicates
    */
   public static removeDuplicates<T>(array: T[]): T[] {
     return [...new Set(array)];
