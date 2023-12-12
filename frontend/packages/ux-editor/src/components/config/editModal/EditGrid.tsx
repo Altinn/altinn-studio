34e01c6d815ba092f0ee41d957cc303a27dc970c
andrealouisestanderen <andreastanderen@gmail.com>
2023-12-12 11:03:05 +0100

    Split EditGrid into a separate comp EditGridForGivenViewSize

--- frontend/packages/ux-editor/src/components/config/editModal/EditGrid.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditGrid.tsx
@@ -1,88 +1,64 @@
 import React, { useState } from 'react';
 import type { IGenericEditComponent } from '../componentConfig';
 import { useText } from '../../../hooks';
-import { StudioSlider } from '@studio/components';
-import { Paragraph, Switch, ToggleGroup } from '@digdir/design-system-react';
+import { ToggleGroup } from '@digdir/design-system-react';
 import { FormField } from '../../FormField';
 import classes from './EditGrid.module.css';
+import { EditGridForGivenViewSize } from './EditGridForGivenViewSize';
 
-enum ViewSizeForGridProp {
+export enum ViewSizeForGridProp {
   S = 'xs',
   M = 'md',
 }
 
 export const EditGrid = ({ handleComponentChange, component }: IGenericEditComponent) => {
-  const defaultViewSize = ViewSizeForGridProp.M;
-  const [viewSizeForGridProp, setViewSizeForGridProp] =
-    useState<ViewSizeForGridProp>(defaultViewSize);
-  const [useDefaultGridSize, setUseDefaultGridSize] = useState<boolean>(
-    (viewSizeForGridProp === ViewSizeForGridProp.S && !component.grid?.xs) ||
-      (viewSizeForGridProp === ViewSizeForGridProp.M && !component.grid?.md),
+  const getDefaultViewSize = (): ViewSizeForGridProp => {
+    if (component.grid?.md) return ViewSizeForGridProp.M;
+    else if (component.grid?.xs) return ViewSizeForGridProp.S;
+    else return ViewSizeForGridProp.M;
+  };
+  const [viewSizeForGridProp, setViewSizeForGridProp] = useState<ViewSizeForGridProp>(
+    getDefaultViewSize(),
   );
+
   const t = useText();
 
-  const handleSliderChange = (newValue: string) => {
-    const newGridObject =
-      viewSizeForGridProp === ViewSizeForGridProp.S ? { xs: newValue } : { md: newValue };
-    handleComponentChange({
-      ...component,
-      grid: {
-        ...component.grid,
-        ...newGridObject,
-      },
-    });
-  };
-
-  const handleSwitchChange = () => {
-    setUseDefaultGridSize(!useDefaultGridSize);
-    // call function to mutate component on new ref
-    if (viewSizeForGridProp === ViewSizeForGridProp.S) delete component.grid?.xs;
-    if (viewSizeForGridProp === ViewSizeForGridProp.M) delete component.grid?.md;
-    handleComponentChange(component);
-  };
+  const gridIsSetForViewSize = (viewSize: ViewSizeForGridProp): boolean =>
+    viewSize === ViewSizeForGridProp.S ? !!component.grid?.xs : !!component.grid?.md;
 
   return (
     <FormField
       id={component.id}
       value={component.grid}
-      onChange={handleSliderChange}
       propertyPath={component.propertyPath}
       componentType={component.type}
       helpText={t('ux_editor.modal_properties_grid_help')}
-      renderField={({ fieldProps }) => {
-        return (
-          <>
-            <div className={classes.toggleGroupContainer}>
-              <ToggleGroup
-                defaultValue={viewSizeForGridProp}
-                name='toggle-group-grid-size'
-                onChange={(viewSize: ViewSizeForGridProp) => setViewSizeForGridProp(viewSize)}
-                size='small'
-              >
-                <ToggleGroup.Item value={ViewSizeForGridProp.S}>
-                  {t('ux_editor.modal_properties_grid_size_xs')}
-                </ToggleGroup.Item>
-                <ToggleGroup.Item value={ViewSizeForGridProp.M}>
-                  {t('ux_editor.modal_properties_grid_size_md')}
-                </ToggleGroup.Item>
-              </ToggleGroup>
-            </div>
-            <Paragraph size='small'>{t('ux_editor.modal_properties_grid')}</Paragraph>
-            <StudioSlider
-              disabled={useDefaultGridSize}
-              sliderValue={
-                viewSizeForGridProp === ViewSizeForGridProp.S
-                  ? component.grid?.xs
-                  : component.grid?.md
-              }
-              handleSliderChange={(newValue) => handleSliderChange(newValue)}
-            />
-            <Switch checked={useDefaultGridSize} onChange={handleSwitchChange} size='small'>
-              {t('ux_editor.modal_properties_grid_use_default')}
-            </Switch>
-          </>
-        );
-      }}
+      renderField={({ fieldProps }) => (
+        <>
+          <div className={classes.toggleGroupContainer}>
+            <ToggleGroup
+              defaultValue={viewSizeForGridProp}
+              name='toggle-group-grid-size'
+              onChange={(viewSize: ViewSizeForGridProp) => setViewSizeForGridProp(viewSize)}
+              size='small'
+            >
+              <ToggleGroup.Item value={ViewSizeForGridProp.S}>
+                {t('ux_editor.modal_properties_grid_size_xs')}
+              </ToggleGroup.Item>
+              <ToggleGroup.Item value={ViewSizeForGridProp.M}>
+                {t('ux_editor.modal_properties_grid_size_md')}
+              </ToggleGroup.Item>
+            </ToggleGroup>
+          </div>
+          <EditGridForGivenViewSize
+            {...fieldProps}
+            viewSize={viewSizeForGridProp}
+            useDefaultGridSize={!gridIsSetForViewSize(viewSizeForGridProp)}
+            handleComponentChange={handleComponentChange}
+            component={component}
+          />
+        </>
+      )}
     />
   );
 };
---
+++ frontend/packages/ux-editor/src/components/config/editModal/EditGridForGivenViewSize.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import type { IGenericEditComponent } from '../componentConfig';
+import { useText } from '../../../hooks';
+import { StudioSlider } from '@studio/components';
+import { Paragraph, Switch } from '@digdir/design-system-react';
+import { ViewSizeForGridProp } from './EditGrid';
+
+export interface EditGridForGivenViewSizeProps extends IGenericEditComponent {
+  viewSize: ViewSizeForGridProp;
+  useDefaultGridSize: boolean;
+}
+
+export const EditGridForGivenViewSize = ({
+  viewSize,
+  useDefaultGridSize,
+  handleComponentChange,
+  component,
+}: EditGridForGivenViewSizeProps) => {
+  const t = useText();
+
+  const DEFAULT_GRID_VALUE = '12';
+
+  const handleSliderChange = (newValue: string) => {
+    const newGridObject = viewSize === ViewSizeForGridProp.S ? { xs: newValue } : { md: newValue };
+    handleComponentChange({
+      ...component,
+      grid: {
+        ...component.grid,
+        ...newGridObject,
+      },
+    });
+  };
+
+  const handleSwitchChange = () => {
+    // call function to mutate component on new ref
+    if (useDefaultGridSize) {
+      handleSliderChange(DEFAULT_GRID_VALUE);
+    } else {
+      if (viewSize === ViewSizeForGridProp.S) delete component.grid?.xs;
+      if (viewSize === ViewSizeForGridProp.M) delete component.grid?.md;
+      if (component.grid?.keys?.length === 0) delete component.grid;
+      handleComponentChange(component);
+    }
+  };
+
+  debugger;
+  return (
+    <>
+      <Paragraph size='small'>{t('ux_editor.modal_properties_grid')}</Paragraph>
+      <StudioSlider
+        disabled={useDefaultGridSize}
+        sliderValue={viewSize === ViewSizeForGridProp.S ? component.grid?.xs : component.grid?.md}
+        handleSliderChange={(newValue) => handleSliderChange(newValue)}
+      />
+      <Switch checked={useDefaultGridSize} onChange={handleSwitchChange} size='small'>
+        {t('ux_editor.modal_properties_grid_use_default')}
+      </Switch>
+    </>
+  );
+};
