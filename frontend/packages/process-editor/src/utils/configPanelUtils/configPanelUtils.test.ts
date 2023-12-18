38aad78db4e676069613f3097f607ffc7eee716d
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2023-12-18 20:32:52 +0100

    adding tests

--- frontend/packages/process-editor/src/utils/configPanelUtils/configPanelUtils.test.ts
+++ frontend/packages/process-editor/src/utils/configPanelUtils/configPanelUtils.test.ts
@@ -1,4 +1,26 @@
-import { getConfigTitleHelpTextKey, getConfigTitleKey } from './configPanelUtils';
+import { ApplicationMetadata, DataTypeElement } from 'app-shared/types/ApplicationMetadata';
+import {
+  filterOutRefDataAsPdf,
+  getApplicationMetadataWithUpdatedDataTypes,
+  getConfigTitleHelpTextKey,
+  getConfigTitleKey,
+  getSelectedDataTypes,
+  getValidDataTypeIdsAndTaskIds,
+  mapDataTypesToDataTypeIdAndTaskIds,
+} from './configPanelUtils';
+import { DataTypeIdAndTaskId } from '../../types/DataTypeIdAndTaskId';
+
+const mockId: string = 'mockId';
+const mockOrg: string = 'org';
+const mockDataTypeId1: string = 'type1';
+const mockDataTypeTaskId1: string = 'oldTask';
+const mockDataType1: DataTypeElement = { id: mockDataTypeId1, taskId: mockDataTypeTaskId1 };
+const mockDataTypes: DataTypeElement[] = [mockDataType1];
+const mockApplicationMetadata: ApplicationMetadata = {
+  id: mockId,
+  org: mockOrg,
+  dataTypes: mockDataTypes,
+};
 
 describe('configPanelUtils', () => {
   describe('getConfigTitleKey', () => {
@@ -42,6 +64,68 @@ describe('configPanelUtils', () => {
     it('returns signing helptext key when taskType is "signing"', () => {
       const key = getConfigTitleHelpTextKey('signing');
       expect(key).toEqual('process_editor.configuration_panel_header_help_text_signing');
+    });
+  });
+
+  describe('getValidDataTypeIdsAndTaskIds', () => {
+    it('returns empty array if applicationMetadata.dataTypes is falsy', () => {
+      const result = getValidDataTypeIdsAndTaskIds({
+        ...mockApplicationMetadata,
+        dataTypes: null,
+      });
+      expect(result).toEqual([]);
+    });
+
+    it('returns empty array if filtered data types list is empty', () => {
+      const result = getValidDataTypeIdsAndTaskIds({
+        ...mockApplicationMetadata,
+        dataTypes: [],
+      });
+      expect(result).toEqual([]);
+    });
+
+    it('returns mapped data types if dataTypes list is valid', () => {
+      const result = getValidDataTypeIdsAndTaskIds(mockApplicationMetadata);
+      expect(result).toEqual([{ dataTypeId: mockDataTypeId1, taskId: mockDataTypeTaskId1 }]);
+    });
+  });
+
+  describe('filterOutRefDataAsPdf', () => {
+    it('filters out "ref-data-as-pdf" from data types list', () => {
+      const mockDataTypes: DataTypeElement[] = [{ id: mockDataTypeId1 }, { id: 'ref-data-as-pdf' }];
+      const result = filterOutRefDataAsPdf(mockDataTypes);
+      expect(result).toEqual([{ id: mockDataTypeId1 }]);
+      expect(result).toHaveLength(1);
+    });
+  });
+
+  describe('mapDataTypesToDataTypeIdAndTaskIds', () => {
+    it('maps data types to DataTypeIdAndTaskId format', () => {
+      const result = mapDataTypesToDataTypeIdAndTaskIds(mockDataTypes);
+      expect(result).toEqual([{ dataTypeId: mockDataTypeId1, taskId: mockDataTypeTaskId1 }]);
+    });
+  });
+
+  describe('getSelectedDataTypes', () => {
+    it('returns selected data types for a given bpmnTaskId', () => {
+      const mockDataTypeIdAndTaskId: DataTypeIdAndTaskId[] = [
+        { dataTypeId: 'type1', taskId: 'task1' },
+        { dataTypeId: 'type2', taskId: 'task2' },
+      ];
+      const result = getSelectedDataTypes('task2', mockDataTypeIdAndTaskId);
+      expect(result).toEqual(['type2']);
+    });
+  });
+
+  describe('getApplicationMetadataWithUpdatedDataTypes', () => {
+    it('updates taskIds in the copied applicationMetadata', () => {
+      const newTaskId: string = 'newTask';
+      const result = getApplicationMetadataWithUpdatedDataTypes(
+        mockApplicationMetadata,
+        newTaskId,
+        [mockDataTypeId1],
+      );
+      expect(result.dataTypes[0].taskId).toEqual(newTaskId);
     });
   });
 });
