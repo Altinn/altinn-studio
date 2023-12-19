295b5c4ed0c79a211cf32bc27d92d455cdcb8a09
David Ovrelid <david@framit.no>
2023-12-19 13:57:28 +0100

    added a comment to explain why we extends the test

--- frontend/testing/playwright/playwright.config.ts
+++ frontend/testing/playwright/playwright.config.ts
@@ -5,6 +5,7 @@ type ExtendedTestOptions = {
   testAppName: string;
 };
 
+// Extends the default test to support custom parameters such as appName for our test app
 export const test = base.extend<ExtendedTestOptions>({
   testAppName: [process.env.PLAYWRIGHT_DESIGNER_APP_NAME, { option: true }],
 });
