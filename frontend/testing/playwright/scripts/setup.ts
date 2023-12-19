277252e4ec74cc6d075dd675bf09eb33caa21aba
David Ovrelid <david@framit.no>
2023-12-19 23:03:11 +0100

    create script to make access-token to gitea

--- frontend/testing/playwright/scripts/setup.ts
+++ frontend/testing/playwright/scripts/setup.ts
@@ -2,6 +2,8 @@
 import fs from 'fs';
 import os from 'os';
 
+const giteaApi = require('../../../../development/utils/gitea-api');
+
 // Configure the dotenv to read form the .env file on root of monorepo.
 require('dotenv').config({ path: path.resolve(__dirname, '../../../../.env') });
 
@@ -11,6 +13,31 @@ const environment: Record<string, string> = {
 
   PLAYWRIGHT_USER: process.env.GITEA_CYPRESS_USER,
   PLAYWRIGHT_PASS: process.env.GITEA_CYPRESS_PASS,
+  GITEA_ACCESS_TOKEN: null,
+};
+
+const createGiteaAccessToken = async (): Promise<void> => {
+  const result = await giteaApi({
+    path: `/repos/api/v1/users/${process.env.GITEA_ADMIN_USER}/tokens`,
+    method: 'POST',
+    user: process.env.GITEA_ADMIN_USER,
+    pass: process.env.GITEA_ADMIN_PASS,
+    body: {
+      name: 'setup.ts' + ' ' + Date.now(),
+      scopes: [
+        'write:activitypub',
+        'write:admin',
+        'write:issue',
+        'write:misc',
+        'write:notification',
+        'write:organization',
+        'write:package',
+        'write:repository',
+        'write:user',
+      ],
+    },
+  });
+  environment.GITEA_ACCESS_TOKEN = result.sha1;
 };
 
 const getEnvFilePath = (): string => {
@@ -23,7 +50,8 @@ const mapEnvironment = () => {
     .join(os.EOL);
 };
 
-const updateEnvironmentVars = (): void => {
+const updateEnvironmentVars = async (): Promise<void> => {
+  await createGiteaAccessToken();
   const filePath: string = getEnvFilePath();
   console.table(environment);
   fs.writeFileSync(filePath, mapEnvironment(), { encoding: 'utf8', flag: 'w' });
