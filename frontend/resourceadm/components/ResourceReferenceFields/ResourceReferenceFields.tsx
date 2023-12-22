660aee045f4b91ae616a922d61c9933902cf41a5
Martin Gunnerud <mgunnerud@gmail.com>
2023-12-22 08:49:30 +0100

    add fields for resource references

---
+++ frontend/resourceadm/components/ResourceReferenceFields/ResourceReferenceFields.tsx
@@ -0,0 +1,149 @@
+import React, { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, Modal } from '@digdir/design-system-react';
+import classes from './ResourceReferenceFields.module.css';
+import type { ResourceReference } from 'app-shared/types/ResourceAdm';
+import { ResourceReferenceFieldset } from './ResourceReferenceFieldset';
+
+const DELETE_ID_NOT_SET = -1;
+
+// Empty value for when adding a new field
+const emptyResrouceReference: ResourceReference = {
+  referenceSource: 'Default',
+  reference: '',
+  referenceType: 'Default',
+};
+
+export type ResourceReferenceFieldsProps = {
+  /**
+   * The current resource references list
+   */
+  resourceReferenceList: ResourceReference[];
+  /**
+   * Function to be executed when resource references are changed
+   * @param resourceReference the updated list of resource references
+   * @returns void
+   */
+  onResourceReferenceFieldChanged: (resourceReference: ResourceReference[]) => void;
+  /**
+   * If the error should be shown
+   */
+  showErrors: boolean;
+};
+
+/**
+ * @component
+ *    Renders the list of resource referencess as groups with the input and select fields
+ *
+ * @property {ResourceReference[]}[resourceReferenceList] - The current resource references list
+ * @property {function}[onResourceReferenceFieldChanged] - Function to be executed when resource references are changed
+ * @property {boolean}[showErrors] - If the error should be shown
+ *
+ * @returns {React.ReactNode} - The rendered component
+ */
+export const ResourceReferenceFields = ({
+  resourceReferenceList,
+  onResourceReferenceFieldChanged,
+  showErrors,
+}: ResourceReferenceFieldsProps): React.ReactNode => {
+  const { t } = useTranslation();
+  const deleteModalRef = useRef<HTMLDialogElement>(null);
+
+  const [deleteId, setDeleteId] = useState<number>(DELETE_ID_NOT_SET);
+  const [resourceReferences, setResourceReferences] = useState<ResourceReference[]>(
+    resourceReferenceList ?? [emptyResrouceReference],
+  );
+
+  /**
+   * Adds the resource reference to the list
+   */
+  const handleClickAddButton = () => {
+    const updatedList = [...resourceReferences, emptyResrouceReference];
+    setResourceReferences(updatedList);
+    onResourceReferenceFieldChanged(updatedList);
+  };
+
+  /**
+   * Removes the resource reference from the list
+   */
+  const handleClickRemoveButton = () => {
+    const updatedList = resourceReferences.filter((_rr, index) => index !== deleteId);
+    onCloseDeleteModal();
+    setResourceReferences(updatedList);
+    onResourceReferenceFieldChanged(updatedList);
+  };
+
+  /**
+   * Updates the resource reference when leaving a field
+   * @param resourceReference
+   * @param pos
+   */
+  const onChangeResourceReferenceField = (resourceReference: ResourceReference, pos: number) => {
+    const updatedList = [...resourceReferences];
+    updatedList[pos] = resourceReference;
+    setResourceReferences(updatedList);
+    onResourceReferenceFieldChanged(updatedList);
+  };
+
+  /**
+   * Closes the delete resource reference modal
+   */
+  const onCloseDeleteModal = (): void => {
+    deleteModalRef.current?.close();
+    setDeleteId(DELETE_ID_NOT_SET);
+  };
+  /**
+   * Displays each resource reference as a group of 3 elements
+   */
+  const displayReferenceFields = resourceReferences.map(
+    (resourceReference: ResourceReference, pos: number) => (
+      <div key={`${JSON.stringify(resourceReference)}-${pos}`}>
+        <ResourceReferenceFieldset
+          resourceReference={resourceReference}
+          onChangeResourceReferenceField={(rr: ResourceReference) =>
+            onChangeResourceReferenceField(rr, pos)
+          }
+          showErrors={showErrors}
+        />
+        <Button
+          size='small'
+          color='danger'
+          aria-disabled={resourceReferences.length < 2}
+          onClick={() => {
+            if (resourceReferences.length > 1) {
+              // TODO: can the last resourceReference be deleted??
+              deleteModalRef.current?.showModal();
+              setDeleteId(pos);
+            }
+          }}
+        >
+          {t('resourceadm.about_resource_reference_delete')}
+        </Button>
+      </div>
+    ),
+  );
+
+  return (
+    <>
+      <Modal ref={deleteModalRef} onClose={onCloseDeleteModal}>
+        <Modal.Header>{t('resourceadm.about_resource_reference_delete')}</Modal.Header>
+        <Modal.Content>{t('resourceadm.about_resource_reference_confirm_delete')}</Modal.Content>
+        <Modal.Footer>
+          <Button color='danger' size='small' onClick={handleClickRemoveButton}>
+            {t('resourceadm.about_resource_reference_confirm_delete_button')}
+          </Button>
+          <Button size='small' variant='tertiary' onClick={onCloseDeleteModal}>
+            {t('general.cancel')}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <div className={classes.divider} />
+      {displayReferenceFields}
+      <div className={classes.buttonWrapper}>
+        <Button size='small' onClick={handleClickAddButton}>
+          {t('resourceadm.about_resource_reference_add_reference')}
+        </Button>
+      </div>
+    </>
+  );
+};
---
+++ frontend/resourceadm/components/ResourceReferenceFields/ResourceReferenceFieldset.tsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Fieldset, Select, Textfield } from '@digdir/design-system-react';
+import classes from './ResourceReferenceFields.module.css';
+import type {
+  ResourceReference,
+  ResourceReferenceSource,
+  ResourceReferenceType,
+} from 'app-shared/types/ResourceAdm';
+import { InputFieldErrorMessage } from '../ResourcePageInputs/InputFieldErrorMessage';
+
+type ResourceReferenceFieldsetProps = {
+  /**
+   * The resourceReference to display in the fieldset
+   */
+  resourceReference: ResourceReference;
+  /**
+   * Function to be executed when leaving a text field
+   * @param resourceReference the resourceReference
+   * @returns void
+   */
+  onChangeResourceReferenceField: (resourceReference: ResourceReference) => void;
+  /**
+   * If the error should be shown
+   */
+  showErrors: boolean;
+};
+
+/**
+ * @component
+ *    Displays the fieldset with the 3 fields in a ResourceReference
+ *
+ * @property {ResourceReference}[resourceReference] - The resourceReference to display in the fieldset
+ * @property {function}[onChangeResourceReferenceField] - Function to be executed when resourceReference is changed
+ * @property {boolean}[showErrors] - If errors should be shown or not
+ *
+ * @returns {React.ReactNode} - The rendered component
+ */
+export const ResourceReferenceFieldset = ({
+  resourceReference,
+  onChangeResourceReferenceField,
+  showErrors,
+}: ResourceReferenceFieldsetProps): React.ReactNode => {
+  const { t } = useTranslation();
+
+  const [referenceSource, setReferenceSource] = useState<ResourceReferenceSource>(
+    resourceReference.referenceSource,
+  );
+  const [referenceType, setReferenceType] = useState<ResourceReferenceType>(
+    resourceReference.referenceType,
+  );
+  const [reference, setReference] = useState(resourceReference.reference);
+
+  const isValid = referenceSource && referenceType && reference;
+  const hasError = !isValid && showErrors;
+
+  return (
+    <>
+      <div className={classes.divider} />
+      <Fieldset
+        legend={t('resourceadm.about_resource_references')}
+        description={t('resourceadm.about_resource_references_description')}
+        size='small'
+        className={classes.fieldset}
+      >
+        <Select
+          label={t('resourceadm.about_resource_reference_source')}
+          value={referenceSource}
+          options={[
+            {
+              value: 'Default',
+              label: 'Default',
+            },
+            {
+              value: 'Altinn1',
+              label: 'Altinn1',
+            },
+            {
+              value: 'Altinn2',
+              label: 'Altinn2',
+            },
+            {
+              value: 'Altinn3',
+              label: 'Altinn3',
+            },
+            {
+              value: 'ExternalPlatform',
+              label: 'ExternalPlatform',
+            },
+          ]}
+          onChange={(value: ResourceReferenceSource) => setReferenceSource(value)}
+          onBlur={() => {
+            onChangeResourceReferenceField({ ...resourceReference, referenceSource });
+          }}
+          error={hasError}
+        />
+        <Select
+          label={t('resourceadm.about_resource_reference_type')}
+          value={referenceType}
+          options={[
+            {
+              value: 'Default',
+              label: 'Default',
+            },
+            {
+              value: 'Uri',
+              label: 'Uri',
+            },
+            {
+              value: 'DelegationSchemeId',
+              label: 'DelegationSchemeId',
+            },
+            {
+              value: 'MaskinportenScope',
+              label: 'MaskinportenScope',
+            },
+            {
+              value: 'ServiceCode',
+              label: 'ServiceCode',
+            },
+            {
+              value: 'ServiceEditionCode',
+              label: 'ServiceEditionCode',
+            },
+          ]}
+          onChange={(value: ResourceReferenceType) => setReferenceType(value)}
+          onBlur={() => {
+            onChangeResourceReferenceField({ ...resourceReference, referenceType });
+          }}
+          error={hasError}
+        />
+        <Textfield
+          label={t('resourceadm.about_resource_reference')}
+          size='small'
+          value={reference}
+          onChange={(e) => setReference(e.target.value)}
+          error={hasError}
+          onBlur={() => {
+            onChangeResourceReferenceField({ ...resourceReference, reference });
+          }}
+        />
+      </Fieldset>
+      {hasError && (
+        <InputFieldErrorMessage message={t('resourceadm.about_resource_reference_error')} />
+      )}
+    </>
+  );
+};
