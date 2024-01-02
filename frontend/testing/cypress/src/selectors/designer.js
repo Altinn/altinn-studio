58f908935a57a7a4e798b7d43a9d26ca97a1fcef
andreastanderen <71079896+standeren@users.noreply.github.com>
2024-01-02 13:44:32 +0100

    Add initial page to app from create service in backend (#11921)
    * Add initial page to app from create service in backend
    
    * Fix cypress test
    
    * Change name of initial page to Side1

--- frontend/testing/cypress/src/selectors/designer.js
+++ frontend/testing/cypress/src/selectors/designer.js
@@ -6,6 +6,7 @@ const getToolbarItems = () => cy.findAllByTestId(testids.draggableToolbarItem);
 export const designer = {
   getAddPageButton: () => cy.findByRole('button', { name: texts['ux_editor.pages_add'] }),
   getDroppableList: () => cy.findByTestId(testids.droppableList),
+  getPageHeaderButton: (page) => cy.findByRole('button', { name: `${page}` }),
   getPageAccordionByName: (page) => cy.findByTestId(testids.pageAccordionContent(page)),
   getToolbarItemByText: (text) => getToolbarItems().findByText(text),
   getToolbarItems,
