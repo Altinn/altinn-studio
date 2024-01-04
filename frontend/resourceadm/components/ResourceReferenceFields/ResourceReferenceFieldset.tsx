367deecd64ca86b7ffc693bfd419eea344247095
Martin Gunnerud <mgunnerud@gmail.com>
2024-01-04 11:24:14 +0100

    replace Select component with radio buttons

--- frontend/resourceadm/components/ResourceReferenceFields/ResourceReferenceFieldset.tsx
+++ frontend/resourceadm/components/ResourceReferenceFields/ResourceReferenceFieldset.tsx
@@ -1,12 +1,62 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Fieldset, Select, Textfield } from '@digdir/design-system-react';
+import { Fieldset, Radio, Textfield } from '@digdir/design-system-react';
 import type {
   ResourceReference,
   ResourceReferenceSource,
   ResourceReferenceType,
 } from 'app-shared/types/ResourceAdm';
 import { InputFieldErrorMessage } from '../ResourcePageInputs/InputFieldErrorMessage';
+
+const referenceSourceOptions = [
+  {
+    value: 'Default',
+    label: 'Default',
+  },
+  {
+    value: 'Altinn1',
+    label: 'Altinn1',
+  },
+  {
+    value: 'Altinn2',
+    label: 'Altinn2',
+  },
+  {
+    value: 'Altinn3',
+    label: 'Altinn3',
+  },
+  {
+    value: 'ExternalPlatform',
+    label: 'ExternalPlatform',
+  },
+];
+
+const referenceTypeOptions = [
+  {
+    value: 'Default',
+    label: 'Default',
+  },
+  {
+    value: 'Uri',
+    label: 'Uri',
+  },
+  {
+    value: 'DelegationSchemeId',
+    label: 'DelegationSchemeId',
+  },
+  {
+    value: 'MaskinportenScope',
+    label: 'MaskinportenScope',
+  },
+  {
+    value: 'ServiceCode',
+    label: 'ServiceCode',
+  },
+  {
+    value: 'ServiceEditionCode',
+    label: 'ServiceEditionCode',
+  },
+];
 
 type ResourceReferenceFieldsetProps = {
   /**
@@ -60,72 +110,42 @@ export const ResourceReferenceFieldset = ({
         description={t('resourceadm.about_resource_references_description')}
         size='small'
       >
-        <Select
-          label={t('resourceadm.about_resource_reference_source')}
-          value={referenceSource}
-          options={[
-            {
-              value: 'Default',
-              label: 'Default',
-            },
-            {
-              value: 'Altinn1',
-              label: 'Altinn1',
-            },
-            {
-              value: 'Altinn2',
-              label: 'Altinn2',
-            },
-            {
-              value: 'Altinn3',
-              label: 'Altinn3',
-            },
-            {
-              value: 'ExternalPlatform',
-              label: 'ExternalPlatform',
-            },
-          ]}
+        <Radio.Group
+          size='small'
           onChange={(value: ResourceReferenceSource) => setReferenceSource(value)}
+          value={referenceSource}
+          legend={t('resourceadm.about_resource_reference_source')}
+          error={hasError}
           onBlur={() => {
             onChangeResourceReferenceField({ ...resourceReference, referenceSource });
           }}
-          error={hasError}
-        />
-        <Select
-          label={t('resourceadm.about_resource_reference_type')}
-          value={referenceType}
-          options={[
-            {
-              value: 'Default',
-              label: 'Default',
-            },
-            {
-              value: 'Uri',
-              label: 'Uri',
-            },
-            {
-              value: 'DelegationSchemeId',
-              label: 'DelegationSchemeId',
-            },
-            {
-              value: 'MaskinportenScope',
-              label: 'MaskinportenScope',
-            },
-            {
-              value: 'ServiceCode',
-              label: 'ServiceCode',
-            },
-            {
-              value: 'ServiceEditionCode',
-              label: 'ServiceEditionCode',
-            },
-          ]}
+        >
+          {referenceSourceOptions.map((opt) => {
+            return (
+              <Radio key={opt.value} value={opt.value}>
+                {opt.label}
+              </Radio>
+            );
+          })}
+        </Radio.Group>
+        <Radio.Group
+          size='small'
           onChange={(value: ResourceReferenceType) => setReferenceType(value)}
+          value={referenceType}
+          legend={t('resourceadm.about_resource_reference_type')}
+          error={hasError}
           onBlur={() => {
             onChangeResourceReferenceField({ ...resourceReference, referenceType });
           }}
-          error={hasError}
-        />
+        >
+          {referenceTypeOptions.map((opt) => {
+            return (
+              <Radio key={opt.value} value={opt.value}>
+                {opt.label}
+              </Radio>
+            );
+          })}
+        </Radio.Group>
         <Textfield
           label={t('resourceadm.about_resource_reference')}
           size='small'
