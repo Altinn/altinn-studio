4895d0ccef890028d86157d21a1c7b8b3b2c0a5a
Nina Kylstad <nkylstad@gmail.com>
2024-01-08 10:48:10 +0100

    suggestion for new help text

--- frontend/packages/process-editor/src/components/Canvas/VersionHelpText/VersionHelpText.tsx
+++ frontend/packages/process-editor/src/components/Canvas/VersionHelpText/VersionHelpText.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './VersionHelpText.module.css';
-import { HelpText, Link, Paragraph } from '@digdir/design-system-react';
-import { useTranslation, Trans } from 'react-i18next';
+import { HelpText, Paragraph } from '@digdir/design-system-react';
+import { useTranslation } from 'react-i18next';
 import { useBpmnContext } from '../../../contexts/BpmnContext';
 
 /**
@@ -25,7 +25,12 @@ export const VersionHelpText = (): JSX.Element => {
         <Paragraph spacing size='small' className={classes.helpTextContent}>
           {t('process_editor.too_old_version_helptext_content', { version: appLibVersion })}
         </Paragraph>
-        <Paragraph size='small' spacing>
+        {/*
+          Temporarily hidden until v8 becomes available.
+          TODO: Add back the below text/links when v8 is available.
+          Github: https://github.com/Altinn/altinn-studio/issues/11968
+        */}
+        {/* <Paragraph size='small' spacing>
           <Trans i18nKey={t('process_editor.help_text_and_links')}>
             Trenger du hjelp til å oppgradere, kan du{' '}
             <Link href='https://docs.altinn.studio/nb/app/maintainance/dependencies/'>
@@ -33,7 +38,7 @@ export const VersionHelpText = (): JSX.Element => {
             </Link>{' '}
             eller <Link href='/contact'>ta kontakt med oss</Link>.
           </Trans>
-        </Paragraph>
+        </Paragraph> */}
       </HelpText>
     </div>
   );
