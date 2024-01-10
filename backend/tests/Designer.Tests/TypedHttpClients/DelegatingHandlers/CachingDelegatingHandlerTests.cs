7afae816096eb2d1927e0d270c3b45f52cb108ed
Mirko Sekulic <misha.sekulic@gmail.com>
2024-01-10 10:47:25 +0100

    Added more tests

--- backend/tests/Designer.Tests/TypedHttpClients/DelegatingHandlers/CachingDelegatingHandlerTests.cs
+++ backend/tests/Designer.Tests/TypedHttpClients/DelegatingHandlers/CachingDelegatingHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.TypedHttpClients.DelegatingHandlers;
 using FluentAssertions;
@@ -9,55 +10,76 @@ namespace Designer.Tests.TypedHttpClients.DelegatingHandlers
 {
     public class CachingDelegatingHandlerTests
     {
+        readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+        private CachingDelegatingHandler GenerateCachingDelegatingHandler() => new(_memoryCache, 10)
+        {
+            InnerHandler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = true
+            }
+        };
+
         [Theory]
         [InlineData(new byte[] { 1, 2, 3 })]
         public async Task Get_ShouldUseCache(byte[] expectedResponse)
         {
-            IMemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
-            using CachingDelegatingHandler handler = new CachingDelegatingHandler(memoryCache, 10)
-            {
-                InnerHandler = new HttpClientHandler()
-                {
-                    AllowAutoRedirect = true
-                }
-            };
+            using CachingDelegatingHandler handler = GenerateCachingDelegatingHandler();
 
-            using HttpClient client = new HttpClient(handler);
+            using HttpClient client = new(handler);
 
-            CachingDelegatingHandler.CacheResponseDataEntry cacheResponseDataEntry = new CachingDelegatingHandler.CacheResponseDataEntry()
+            CachingDelegatingHandler.CacheResponseDataEntry cacheResponseDataEntry = new()
             {
                 Data = expectedResponse,
                 StatusCode = System.Net.HttpStatusCode.OK
             };
 
-            memoryCache.Set($"{HttpMethod.Get}_http://nonexistingurl1234.no/", cacheResponseDataEntry);
+            _memoryCache.Set($"{HttpMethod.Get}_http://nonexistingurl1234.no/", cacheResponseDataEntry);
 
             var response = await client.GetAsync("http://nonexistingurl1234.no/");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
 
             byte[] responseArray = await response.Content.ReadAsByteArrayAsync();
             responseArray.Should().BeEquivalentTo(expectedResponse);
         }
 
         [Fact]
+        public async Task NonGetRequest_Keep_StatusCode()
+        {
+            using CachingDelegatingHandler handler = GenerateCachingDelegatingHandler();
+
+            using HttpClient client = new(handler);
+
+            var response = await client.PostAsync("https://docs.altinn.studio/", null);
+
+            response.StatusCode.Should().Be(HttpStatusCode.MethodNotAllowed);
+        }
+
+        [Fact]
+        public async Task NonSuccessGetRequest_Keep_StatusCode()
+        {
+            using CachingDelegatingHandler handler = GenerateCachingDelegatingHandler();
+
+            using HttpClient client = new(handler);
+
+            var response = await client.GetAsync("https://docs.altinn.studio/nonexisting");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
         public async Task Get_ShouldBeCached()
         {
-            IMemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
-            using CachingDelegatingHandler handler = new CachingDelegatingHandler(memoryCache, 120)
-            {
-                InnerHandler = new HttpClientHandler()
-                {
-                    AllowAutoRedirect = true
-                }
-            };
+            using CachingDelegatingHandler handler = GenerateCachingDelegatingHandler();
 
             using HttpClient client = new HttpClient(handler);
 
-            var response = await client.GetAsync("https://info.altinn.no/");
+            var response = await client.GetAsync("https://docs.altinn.studio/");
 
             response.IsSuccessStatusCode.Should().BeTrue();
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
 
-            memoryCache.TryGetValue($"{HttpMethod.Get}_https://info.altinn.no/", out CachingDelegatingHandler.CacheResponseDataEntry _).Should().BeTrue();
+            _memoryCache.TryGetValue($"{HttpMethod.Get}_https://docs.altinn.studio/", out CachingDelegatingHandler.CacheResponseDataEntry _).Should().BeTrue();
         }
     }
 }
