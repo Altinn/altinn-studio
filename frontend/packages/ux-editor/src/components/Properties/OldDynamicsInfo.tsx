62cb3109174f26bec312e81949c92a63fa507cad
andreastanderen <71079896+standeren@users.noreply.github.com>
2024-01-10 15:12:41 +0100

    Add selected layoutset to dynamics link if it exists (#11985)
    * Add selected layoutset to dynamics link if it exists
    
    * Add tests
    
    * Use Link from DS instead of a

--- frontend/packages/ux-editor/src/components/Properties/OldDynamicsInfo.tsx
+++ frontend/packages/ux-editor/src/components/Properties/OldDynamicsInfo.tsx
@@ -4,11 +4,16 @@ import { ExternalLinkIcon } from '@navikt/aksel-icons';
 import { useTranslation } from 'react-i18next';
 import { giteaEditLink, altinnDocsUrl } from 'app-shared/ext-urls';
 import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
+import { useAppContext } from '../../hooks/useAppContext';
+import { Link } from '@digdir/design-system-react';
 
 export const OldDynamicsInfo = () => {
   const { t } = useTranslation();
+  const { selectedLayoutSet } = useAppContext();
   const { app, org } = useStudioUrlParams();
-  const dynamicLocation = `App/ui/RuleHandler.js`;
+  const dynamicLocation = selectedLayoutSet
+    ? `App/ui/${selectedLayoutSet}/RuleHandler.js`
+    : 'App/ui/RuleHandler.js';
   return (
     <div>
       <div className={classes.header}>{t('right_menu.dynamics')}</div>
@@ -16,7 +21,7 @@ export const OldDynamicsInfo = () => {
         <p>
           {t('right_menu.dynamics_description')}
           <br />
-          <a
+          <Link
             className={classes.externalLink}
             href={altinnDocsUrl('/nb/app/development/logic/dynamic/')}
             rel='noopener noreferrer'
@@ -26,12 +31,17 @@ export const OldDynamicsInfo = () => {
             <span className={classes.externalLinkIcon}>
               <ExternalLinkIcon />
             </span>
-          </a>
+          </Link>
         </p>
-        <a className={classes.textLink} href={giteaEditLink(org, app, dynamicLocation)} target='_blank' rel='noreferrer'>
+        <Link
+          className={classes.textLink}
+          href={giteaEditLink(org, app, dynamicLocation)}
+          target='_blank'
+          rel='noreferrer'
+        >
           {t('right_menu.dynamics_edit')}
-        </a>{' '}
-        ({t('right_menu.dynamics_edit_comment')})
+        </Link>{' '}
+        {t('right_menu.dynamics_edit_comment')}
       </div>
     </div>
   );
