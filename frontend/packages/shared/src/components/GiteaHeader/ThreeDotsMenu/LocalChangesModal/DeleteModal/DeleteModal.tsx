cc9f8a293ced46c48cd59bed0f483ab87b9e532e
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-01-16 21:17:38 +0100

    fixing feedback from PR

--- frontend/packages/shared/src/components/GiteaHeader/ThreeDotsMenu/LocalChangesModal/DeleteModal/DeleteModal.tsx
+++ frontend/packages/shared/src/components/GiteaHeader/ThreeDotsMenu/LocalChangesModal/DeleteModal/DeleteModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 import classes from './DeleteModal.module.css';
 import { useTranslation } from 'react-i18next';
 import { StudioModal, StudioSpinner } from '@studio/components';
@@ -14,45 +14,25 @@ export type DeleteModalProps = {
   app: string;
 };
 
-/**
- * @component
- *    Displays a Warning modal to the user to ensure they really want to
- *    do an action.
- *
- * @property {boolean}[isOpen] - If the modal is open or not
- * @property {function}[onClose] - Function to execute on close
- * @property {string}[app] - The app
- * @property {string}[org] - The org
- *
- * @returns {ReactNode} - The rendered component
- */
-export const DeleteModal = ({ isOpen, onClose, app, org }: DeleteModalProps): ReactNode => {
+export const DeleteModal = ({ isOpen, onClose, app, org }: DeleteModalProps): JSX.Element => {
   const { t } = useTranslation();
 
-  const { mutate: deleteLocalChanges } = useResetRepositoryMutation(org, app);
+  const { mutate: deleteLocalChanges, isPending: isPendingDeleteLocalChanges } =
+    useResetRepositoryMutation(org, app);
 
   const [nameToDelete, setNameToDelete] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleClose = () => {
     setNameToDelete('');
     onClose();
   };
 
-  const reset = () => {
-    setIsLoading(false);
-    setNameToDelete('');
-    onClose();
-  };
-
   const handleDelete = () => {
-    setIsLoading(true);
     deleteLocalChanges(undefined, {
       onSuccess: () => {
-        reset();
+        handleClose();
         toast.success(t('local_changes.modal_deleted_success'));
       },
-      onError: () => setIsLoading(false),
     });
   };
 
@@ -70,7 +50,7 @@ export const DeleteModal = ({ isOpen, onClose, app, org }: DeleteModalProps): Re
       }
     >
       <div className={classes.contentWrapper}>
-        {isLoading ? (
+        {isPendingDeleteLocalChanges ? (
           <StudioSpinner />
         ) : (
           <>
@@ -78,7 +58,7 @@ export const DeleteModal = ({ isOpen, onClose, app, org }: DeleteModalProps): Re
               {t('local_changes.modal_delete_modal_text')}
             </Paragraph>
             <Textfield
-              label={t('local_changes.modal_delete_modal_textfield_label')}
+              label={t('local_changes.modal_delete_modal_textfield_label', { appName: app })}
               size='small'
               value={nameToDelete}
               onChange={(e) => setNameToDelete(e.target.value)}
