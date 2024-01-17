f0a5a26fc41a44ea0287fc415b80b59f22cba394
David Ovrelid <david@framit.no>
2024-01-18 00:00:17 +0100

    fixed imports that Rider did not fix

--- frontend/packages/ux-editor/src/containers/DesignView/FormTree/UnknownReferencedItem/UnknownReferencedItem.test.tsx
+++ frontend/packages/ux-editor/src/containers/DesignView/FormTree/UnknownReferencedItem/UnknownReferencedItem.test.tsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
-import { UnknownReferencedItem, UnknownReferencedItemProps } from './UnknownItem';
+import { UnknownReferencedItem, UnknownReferencedItemProps } from './UnknownReferencedItem';
 import { layoutMock } from '../../../../testing/layoutMock';
 import { textMock } from '../../../../../../../testing/mocks/i18nMock';
 import { useDeleteUnknownComponentReference } from './useDeleteUnknownComponentReference';
