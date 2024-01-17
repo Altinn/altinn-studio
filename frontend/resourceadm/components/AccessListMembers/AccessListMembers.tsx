29b85d2459dfed4b9b8196640d1342594c548e86
Martin Gunnerud <mgunnerud@gmail.com>
2024-01-17 15:14:38 +0100

    replace LegacyTable with new table component

--- frontend/resourceadm/components/AccessListMembers/AccessListMembers.tsx
+++ frontend/resourceadm/components/AccessListMembers/AccessListMembers.tsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import {
-  Alert,
-  LegacyTable,
-  LegacyTableRow,
-  LegacyTableCell,
-  LegacyTableHeader,
-  LegacyTableBody,
-  Textfield,
-  Radio,
-} from '@digdir/design-system-react';
+import { Alert, Textfield, Radio } from '@digdir/design-system-react';
 import classes from './AccessListMembers.module.css';
 import { AccessList, AccessListMember } from 'app-shared/types/ResourceAdm';
 import { FieldWrapper } from '../FieldWrapper';
 import { useRemoveAccessListMemberMutation } from '../../hooks/mutations/useRemoveAccessListMemberMutation';
 import { useAddAccessListMemberMutation } from '../../hooks/mutations/useAddAccessListMemberMutation';
-import { AccessListMemberRow } from './AccessListMemberRow';
 import { useDebounce } from 'react-use';
 import { usePartiesRegistryQuery } from '../../hooks/queries/usePartiesRegistryQuery';
 import { useSubPartiesRegistryQuery } from '../../hooks/queries/useSubPartiesRegistryQuery';
@@ -24,8 +14,8 @@ import { StudioSpinner } from '@studio/components';
 import { PlusIcon, PlusCircleIcon, MinusCircleIcon } from '@studio/icons';
 import { AccessListMembersPaging } from './AccessListMembersPaging';
 import { StudioButton } from '@studio/components';
+import { AccessListMembersTable } from './AccessListMembersTable';
 
-const COLUMN_SPAN = 100;
 const PARTY_SEARCH_TYPE = 'PARTY';
 const SUBPARTY_SEARCH_TYPE = 'SUBPARTY';
 
@@ -75,110 +65,71 @@ export const AccessListMembers = ({ org, env, list }: AccessListMembersProps): R
       label={t('resourceadm.listadmin_list_organizations')}
       description={t('resourceadm.listadmin_list_organizations_description')}
     >
-      <LegacyTable>
-        <LegacyTableHeader>
-          <LegacyTableRow>
-            <LegacyTableCell width='15%'>{t('resourceadm.listadmin_orgnr')}</LegacyTableCell>
-            <LegacyTableCell width='45%'>{t('resourceadm.listadmin_navn')}</LegacyTableCell>
-            <LegacyTableCell width='15%'>{t('resourceadm.listadmin_type')}</LegacyTableCell>
-            <LegacyTableCell width='25%' />
-          </LegacyTableRow>
-        </LegacyTableHeader>
-        <LegacyTableBody>
-          {listItems.length === 0 && (
-            <LegacyTableRow>
-              <LegacyTableCell colSpan={COLUMN_SPAN}>
-                <Alert severity='info'>{t('resourceadm.listadmin_empty_list')}</Alert>
-              </LegacyTableCell>
-            </LegacyTableRow>
-          )}
-          {listItems.map((item) => {
-            return (
-              <AccessListMemberRow
-                key={item.orgNr}
-                item={item}
-                actionButton={
-                  <StudioButton
-                    onClick={() => handleRemoveMember(item.orgNr)}
-                    variant='tertiary'
-                    size='small'
-                  >
-                    {t('resourceadm.listadmin_remove_from_list')}
-                    <MinusCircleIcon className={classes.buttonIcon} />
-                  </StudioButton>
-                }
+      <AccessListMembersTable
+        listItems={listItems}
+        buttonNode={
+          <>
+            {t('resourceadm.listadmin_remove_from_list')}
+            <MinusCircleIcon className={classes.buttonIcon} />
+          </>
+        }
+        onButtonClick={(item: AccessListMember) => handleRemoveMember(item.orgNr)}
+      />
+      {listItems.length === 0 && (
+        <Alert severity='info'>{t('resourceadm.listadmin_empty_list')}</Alert>
+      )}
+      {isAddMode && (
+        <>
+          <div className={classes.searchWrapper}>
+            <FieldWrapper label={t('resourceadm.listadmin_search')} fieldId='party-search'>
+              <Textfield
+                id='party-search'
+                value={searchText}
+                onChange={(event) => setSearchText(event.target.value)}
               />
-            );
-          })}
-          {isAddMode && (
-            <>
-              <LegacyTableRow>
-                <LegacyTableCell colSpan={COLUMN_SPAN}>
-                  <div className={classes.searchWrapper}>
-                    <FieldWrapper label={t('resourceadm.listadmin_search')} fieldId='party-search'>
-                      <Textfield
-                        id='party-search'
-                        value={searchText}
-                        onChange={(event) => setSearchText(event.target.value)}
-                      />
-                      <div className={classes.noSearchResults} aria-live='polite'>
-                        {resultData?.parties?.length === 0 && (
-                          <div>
-                            {isSubPartySearch
-                              ? t('resourceadm.listadmin_search_no_sub_parties')
-                              : t('resourceadm.listadmin_search_no_parties')}
-                          </div>
-                        )}
-                      </div>
-                    </FieldWrapper>
-                    <Radio.Group
-                      hideLegend
-                      onChange={() => setIsSubPartySearch((old) => !old)}
-                      value={isSubPartySearch ? SUBPARTY_SEARCH_TYPE : PARTY_SEARCH_TYPE}
-                      inline
-                      legend={t('resourceadm.listadmin_search_party_type')}
-                    >
-                      <Radio value={PARTY_SEARCH_TYPE}>{t('resourceadm.listadmin_parties')}</Radio>
-                      <Radio value={SUBPARTY_SEARCH_TYPE}>
-                        {t('resourceadm.listadmin_sub_parties')}
-                      </Radio>
-                    </Radio.Group>
+              <div className={classes.noSearchResults} aria-live='polite'>
+                {resultData?.parties?.length === 0 && (
+                  <div>
+                    {isSubPartySearch
+                      ? t('resourceadm.listadmin_search_no_sub_parties')
+                      : t('resourceadm.listadmin_search_no_parties')}
                   </div>
-                  {(isLoadingParties || isLoadingSubParties) && (
-                    <div className={classes.spinnerContainer}>
-                      <StudioSpinner />
-                    </div>
-                  )}
-                </LegacyTableCell>
-              </LegacyTableRow>
-              {resultData?.parties.map((party) => {
-                return (
-                  <AccessListMemberRow
-                    key={party.orgNr}
-                    item={party}
-                    actionButton={
-                      <StudioButton
-                        onClick={() => handleAddMember(party)}
-                        disabled={!!listItems.find((item) => item.orgNr === party.orgNr)}
-                        variant='tertiary'
-                        size='small'
-                      >
-                        {t('resourceadm.listadmin_add_to_list')}
-                        <PlusCircleIcon className={classes.buttonIcon} />
-                      </StudioButton>
-                    }
-                  />
-                );
-              })}
-              <LegacyTableRow>
-                <LegacyTableCell colSpan={COLUMN_SPAN}>
-                  <AccessListMembersPaging resultData={resultData} setSearchUrl={setSearchUrl} />
-                </LegacyTableCell>
-              </LegacyTableRow>
-            </>
+                )}
+              </div>
+            </FieldWrapper>
+            <Radio.Group
+              hideLegend
+              onChange={() => setIsSubPartySearch((old) => !old)}
+              value={isSubPartySearch ? SUBPARTY_SEARCH_TYPE : PARTY_SEARCH_TYPE}
+              inline
+              legend={t('resourceadm.listadmin_search_party_type')}
+            >
+              <Radio value={PARTY_SEARCH_TYPE}>{t('resourceadm.listadmin_parties')}</Radio>
+              <Radio value={SUBPARTY_SEARCH_TYPE}>{t('resourceadm.listadmin_sub_parties')}</Radio>
+            </Radio.Group>
+          </div>
+          <AccessListMembersTable
+            isHeaderHidden
+            listItems={resultData?.parties ?? []}
+            disableButtonFn={(disableItem: AccessListMember) =>
+              !!listItems.find((listItem) => disableItem.orgNr === listItem.orgNr)
+            }
+            buttonNode={
+              <>
+                {t('resourceadm.listadmin_add_to_list')}
+                <PlusCircleIcon className={classes.buttonIcon} />
+              </>
+            }
+            onButtonClick={handleAddMember}
+          />
+          {(isLoadingParties || isLoadingSubParties) && (
+            <div className={classes.spinnerContainer}>
+              <StudioSpinner />
+            </div>
           )}
-        </LegacyTableBody>
-      </LegacyTable>
+          <AccessListMembersPaging resultData={resultData} setSearchUrl={setSearchUrl} />
+        </>
+      )}
       {!isAddMode && (
         <div className={classes.addMoreWrapper}>
           <StudioButton variant='secondary' onClick={() => setIsAddMode(true)}>
