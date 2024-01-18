2d2a1846949bcd59f5883481f93789c1401297df
David Ovrelid <david@framit.no>
2024-01-18 22:22:21 +0100

    typo

--- frontend/packages/ux-editor/src/components/UnknownComponentAlert/UnknownComponentAlert.test.tsx
+++ frontend/packages/ux-editor/src/components/UnknownComponentAlert/UnknownComponentAlert.test.tsx
@@ -5,7 +5,7 @@ import { textMock } from '../../../../../testing/mocks/i18nMock';
 
 describe('UnknownComponentAlert', () => {
   it('should render information about unknown component', () => {
-    render(<UnknownComponentAlert componentName='UnkwnonComponentName' />);
+    render(<UnknownComponentAlert componentName='UnknownComponentName' />);
     expect(
       screen.getByText(
         textMock('ux_editor.edit_component.unknown_component', {
