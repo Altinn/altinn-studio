9637e758e3318641e94143254259fa6767dddf06
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-01-23 14:22:07 +0100

    trying to fix

--- frontend/testing/playwright/tests/data-model/data-model.teardown.ts
+++ frontend/testing/playwright/tests/data-model/data-model.teardown.ts
@@ -5,6 +5,5 @@ import { test } from '../../extenders/testExtend';
 test('Delete the test app', async ({ request, testAppName }) => {
   const gitea = new Gitea();
   const response = await request.delete(gitea.getDeleteAppEndpoint({ app: testAppName }));
-  console.log('-- RESPONSE: ', response);
   expect(response.ok()).toBeTruthy();
 });
