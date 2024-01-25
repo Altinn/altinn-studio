d834bdd45439c1b6dbc02900cfb037964cddafc0
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-01-25 18:16:03 +0100

    Replacing Login Cypress tests with Playwright

--- frontend/testing/playwright/tests/logout-and-invalid-login-only/logout-and-invalid-login-only.spec.ts
+++ frontend/testing/playwright/tests/logout-and-invalid-login-only/logout-and-invalid-login-only.spec.ts
@@ -4,6 +4,7 @@ import { DashboardPage } from '../../pages/DashboardPage';
 
 // This line must be there to ensure that the tests do not run in parallell, and
 // that the before all call is being executed before we start the tests
+// Test nr. 2 is dependent on test 1, and therefore is "serial" important
 test.describe.configure({ mode: 'serial' });
 
 test('That it is possible to login with valid user credentials, and then log out again', async ({
@@ -11,6 +12,8 @@ test('That it is possible to login with valid user credentials, and then log out
 }): Promise<void> => {
   const loginPage = new LoginPage(page);
   const dashboardPage = new DashboardPage(page);
+
+  // As we are logged in already, we need to start with login out
   await dashboardPage.loadDashboardPage();
 
   await dashboardPage.verifyDashboardPage();
