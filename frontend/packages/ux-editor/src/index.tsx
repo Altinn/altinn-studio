1f87dffab55c1a70e351af721f7587cc8931011c
Nina Kylstad <nkylstad@gmail.com>
2024-01-25 22:00:42 +0100

    test + some refactoring

--- frontend/packages/ux-editor/src/index.tsx
+++ frontend/packages/ux-editor/src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from './App';
-import { store } from './store';
+import { setupStore } from './store';
 import './styles/index.css';
 
+const store = setupStore();
 const container = document.getElementById('root');
 const root = createRoot(container);
 
