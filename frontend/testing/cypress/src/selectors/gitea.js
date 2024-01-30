e8862307a2ea0c30f55bab150d3ea497f0231bd0
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-01-30 15:37:41 +0100

    merge master and delete unused cypress

--- frontend/testing/cypress/src/selectors/gitea.js
+++ frontend/testing/cypress/src/selectors/gitea.js
@@ -1,9 +1,4 @@
 export const gitea = {
-  getAltinnLogo: () => cy.findByAltText(/altinn(|-| )logo/i),
-  getLanguageMenu: () => cy.findByRole('menu'),
-  getLanguageMenuItem: (language) => cy.findByRole('menuitem', { name: language }),
-  getLoginButton: () => cy.findByRole('button', { name: /logg inn/i }),
-  getLoginErrorMessage: () => cy.findByText(/ugyldig brukernavn eller passord./i),
   getPasswordField: () => cy.findByLabelText(/passord/i),
   getRepositoryHeader: () => cy.findByRole('heading', { level: 1 }),
   getUsernameField: () => cy.findByLabelText(/brukernavn eller epost/i),
