fd000212d72b2d99eeabe685a7a11e479d98e974
Ivar Nesje <ivarne@users.noreply.github.com>
2024-01-31 07:45:28 +0100

    Support number, boolean and null as AppOption values (#417)

--- test/Altinn.App.Api.Tests/Controllers/OptionsControllerTests.cs
+++ test/Altinn.App.Api.Tests/Controllers/OptionsControllerTests.cs
@@ -58,6 +58,28 @@ namespace Altinn.App.Api.Tests.Controllers
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             headerValue.Should().NotContain("nb");
         }
+
+        [Fact]
+        public async Task Get_ShouldSerializeToCorrectTypes()
+        {
+            OverrideServicesForThisTest = (services) =>
+            {
+                services.AddTransient<IAppOptionsProvider, DummyProvider>();
+            };
+
+            string org = "tdd";
+            string app = "contributer-restriction";
+            HttpClient client = GetRootedClient(org, app);
+
+            string url = $"/{org}/{app}/api/options/test";
+            HttpResponseMessage response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            response.StatusCode.Should().Be(HttpStatusCode.OK, content);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var options = await response.Content.ReadAsStringAsync();
+            options.Should().Be("[{\"value\":null,\"label\":\"\"},{\"value\":\"SomeString\",\"label\":\"False\"},{\"value\":true,\"label\":\"True\"},{\"value\":0,\"label\":\"Zero\"},{\"value\":1,\"label\":\"One\",\"description\":\"This is a description\",\"helpText\":\"This is a help text\"}]");
+        }
     }
 
     public class DummyProvider : IAppOptionsProvider
@@ -71,6 +93,39 @@ namespace Altinn.App.Api.Tests.Controllers
                 Parameters = new()
                 {
                     { "lang", language }
+                },
+                Options = new List<AppOption>()
+                {
+                    new()
+                    {
+                        Value  = null,
+                        Label = "",
+                    },
+                    new ()
+                    {
+                        Value = "SomeString",
+                        Label = "False",
+                    },
+                    new ()
+                    {
+                        Value = "true",
+                        ValueType = AppOptionValueType.Boolean,
+                        Label = "True",
+                    },
+                    new ()
+                    {
+                        Value = "0",
+                        ValueType = AppOptionValueType.Number,
+                        Label = "Zero",
+                    },
+                    new ()
+                    {
+                        Value = "1",
+                        ValueType = AppOptionValueType.Number,
+                        Label = "One",
+                        Description = "This is a description",
+                        HelpText = "This is a help text"
+                    },
                 }
             };
 
