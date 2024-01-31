a158d2385d1c3276e577c768a75f528b7f199fb2
WilliamThorenfeldt <133344438+WilliamThorenfeldt@users.noreply.github.com>
2024-01-31 15:32:09 +0100

    Replace teardown wiht afterAll() (#12224)

--- frontend/testing/playwright/tests/create-app-only/create-app-only.spec.ts
+++ frontend/testing/playwright/tests/create-app-only/create-app-only.spec.ts
@@ -1,6 +1,16 @@
-﻿import { test } from '../../extenders/testExtend';
+﻿import { expect } from '@playwright/test';
+import { Gitea } from '../../helpers/Gitea';
+import { test } from '../../extenders/testExtend';
 import { CreateServicePage } from '../../pages/CreateServicePage';
 import { DashboardPage } from '../../pages/DashboardPage';
+
+test.describe.configure({ mode: 'serial' });
+
+test.afterAll(async ({ request, testAppName }) => {
+  const gitea = new Gitea();
+  const response = await request.delete(gitea.getDeleteAppEndpoint({ app: testAppName }));
+  expect(response.ok()).toBeTruthy();
+});
 
 test('should load dashboard and the user should navigate to create app form', async ({
   page,
