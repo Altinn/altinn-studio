baae0f06ef8bc0e463f285e525fcbb15596117fe
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-02-01 07:36:31 +0100

    addign back functions for cypress

--- frontend/testing/cypress/src/selectors/gitea.js
+++ frontend/testing/cypress/src/selectors/gitea.js
@@ -1,4 +1,7 @@
 export const gitea = {
+  getLoginButton: () => cy.findByRole('button', { name: /logg inn/i }),
+  getLanguageMenu: () => cy.findByRole('menu'),
+  getLanguageMenuItem: (language) => cy.findByRole('menuitem', { name: language }),
   getPasswordField: () => cy.findByLabelText(/passord/i),
   getRepositoryHeader: () => cy.findByRole('heading', { level: 1 }),
   getUsernameField: () => cy.findByLabelText(/brukernavn eller epost/i),
