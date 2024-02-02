35abc675fea55bff8ea302150f05f2c1697397af
Ole Martin Handeland <github@olemartin.org>
2024-02-02 09:43:59 +0100

    Showing the current git branch in LocalTest when picking a frontend version (#1769)
    Co-authored-by: Ole Martin Handeland <git@olemartin.org>

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const env = require('dotenv').config().parsed ?? {};
 const path = require('path');
+const fs = require('fs');
 
 const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
@@ -28,6 +29,16 @@ console.log('WEBPACK_SILENT =', !enableNotifier);
 console.log('WEBPACK_SOURCE_MAPS =', enableSourceMaps);
 console.log('WEBPACK_MINIFY =', enableMinify);
 console.log('====================================');
+
+// Find the git current branch name from .git/HEAD. This is used in LocalTest to show you the current branch name.
+// We can't just read this when starting, as you may want to switch branch while the dev server is running. This
+// will be called every time you refresh/reload the dev server.
+const branchName = {
+  toString() {
+    const hasGitFolder = fs.existsSync('.git');
+    return hasGitFolder ? fs.readFileSync('.git/HEAD', 'utf-8').trim().split('/').pop() : 'unknown-branch';
+  },
+};
 
 module.exports = {
   ...common,
@@ -71,7 +82,10 @@ module.exports = {
     historyApiFallback: true,
     allowedHosts: 'all',
     hot: true,
-    headers: { 'Access-Control-Allow-Origin': '*' },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'X-Altinn-Frontend-Branch': branchName,
+    },
     client: {
       overlay: {
         errors: true,
