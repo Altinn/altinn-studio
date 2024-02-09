6bf96effd8636127a8c425a13fb1175c31776102
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-02-09 17:14:06 +0100

    Fix wait-for script

--- development/utils/wait-for.js
+++ development/utils/wait-for.js
@@ -1,22 +1,36 @@
 const { get } = require('http');
 module.exports = (url) =>
   new Promise(function (resolve, reject) {
-    let attemts = 0;
+    let attempts = 0;
+
+    const checkAttempts = () => {
+      attempts++;
+      if (attempts > 10) {
+        clearInterval(intervalId);
+        console.log('Giving up: ', url);
+        reject('Giving up this');
+      } else {
+        console.log('Waiting for:', url);
+      }
+    };
+
     const intervalId = setInterval(function () {
-      get(url, (res) => {
+      const req = get(url, (res) => {
         if (res.statusCode === 200) {
           console.log(url, ' is up!');
           clearInterval(intervalId);
           resolve();
         } else {
-          console.log('Waiting for:', url);
+          checkAttempts();
         }
-        if (attemts > 10) {
-          clearInterval(intervalId);
-          console.log('Giving up: ', url);
-          reject('Giving up this');
+      });
+
+      req.on('error', (err) => {
+        if (err.code !== 'ECONNREFUSED') {
+          console.error(err);
         }
-        attemts++;
-      }).end();
+
+        checkAttempts();
+      });
     }, 1000);
   });
