97fef7614a689d3aecb2c6a65b2ad99e0888fd63
JamalAlabdullah <90609090+JamalAlabdullah@users.noreply.github.com>
2024-02-09 09:33:27 +0100

    Bug/12035 not possible to delete a data model in studio (#12115)
    * fixed removing  datamodell and old datamodell

--- frontend/testing/testUtils.ts
+++ frontend/testing/testUtils.ts
@@ -1,3 +1,4 @@
+import { waitFor } from '@testing-library/react';
 import { APP_DEVELOPMENT_BASENAME } from 'app-shared/constants';
 
 export const TEST_DOMAIN = 'http://localhost';
@@ -8,3 +9,5 @@ export const setWindowLocationForTests = (org: string, app: string) => {
     `${TEST_DOMAIN}${APP_DEVELOPMENT_BASENAME}/${org}/${app}`,
   ) as unknown as Location;
 };
+
+export const verifyNeverOccurs = (fn: () => void) => expect(waitFor(fn)).rejects.toThrow();
