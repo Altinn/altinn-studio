0db6f9dc8ca54d0d58af94692144191d2ee2d3ef
Nina Kylstad <nkylstad@gmail.com>
2024-02-13 15:51:14 +0100

    update script

--- frontend/scripts/flattenJson/run.ts
+++ frontend/scripts/flattenJson/run.ts
@@ -1,31 +1,52 @@
 import axios from 'axios';
 import jsonpointer from 'jsonpointer';
 
-const getLayoutSchema = async () => {
-  const response = await axios.get(
-    'https://altinncdn.no/schemas/json/layout/layout.schema.v1.json',
-  );
-  return response.data;
+const versionSettings = {
+  v3: {
+    layoutSchemaUrl: 'https://altinncdn.no/schemas/json/layout/layout.schema.v1.json',
+    componentSchemaPath: '../../packages/ux-editor-v3/src/testing/schemas/json/component',
+  },
+  v4: {
+    layoutSchemaUrl:
+      'https://altinncdn.no/toolkits/altinn-app-frontend/4.0.0-rc2/schemas/json/layout/layout.schema.v1.json',
+    componentSchemaPath: '../../packages/ux-editor/src/testing/schemas/json/component',
+  },
+};
+const validVersions = ['v3', 'v4'] as const;
+type AppFrontendVersion = (typeof validVersions)[number];
+
+const isValidVersion = (version: string): boolean => {
+  return validVersions.includes(version as AppFrontendVersion);
 };
 
-const writeToFile = (name: string, data: any) => {
-  const fileName = `schemas/${name}.schema.v1.json`;
+const getLayoutSchema = async (version?: AppFrontendVersion) => {
+  const response = await axios.get(versionSettings[version || 'v4'].layoutSchemaUrl);
+  return response?.data;
+};
+
+const writeToFile = (name: string, data: any, version: AppFrontendVersion) => {
+  const path = require('path');
   const fs = require('fs');
-  // check if directory exists
-  if (!fs.existsSync('schemas')) {
-    fs.mkdirSync('schemas');
-  }
+
+  const dirPath = path.resolve(__dirname, versionSettings[version].componentSchemaPath);
+  const fileName = `${dirPath}/${name}.schema.v1.json`;
+
   fs.writeFile(fileName, JSON.stringify(data), function (err: any) {
     if (err) return console.log(err);
     console.log(`Wrote ${fileName}`);
   });
 };
 
-const expandAllOf = (schema: any, layoutSchema: any) => {
+const expandAllOf = (schema: any, layoutSchema: any, componentNode: boolean = true) => {
   const expandedSchema: any = {};
-  schema.allOf?.slice(0, -1).forEach((item: any) => {
+  const allOfList = componentNode ? schema.allOf?.slice(0, -1) : schema.allOf;
+  allOfList.forEach((item: any) => {
     if (item.$ref) {
-      const ref = jsonpointer.get(layoutSchema, item.$ref.replace('#/', '/'));
+      let ref = jsonpointer.get(layoutSchema, item.$ref.replace('#/', '/'));
+
+      if (ref.allOf) {
+        ref = expandAllOf(ref, layoutSchema, false);
+      }
       expandedSchema.properties = { ...expandedSchema.properties, ...ref.properties };
     } else if (item.properties) {
       expandedSchema.properties = { ...expandedSchema.properties, ...item.properties };
@@ -41,8 +62,7 @@ const expandAnyOf = (schema: any, layoutSchema: any) => {
     if (item.$ref) {
       let ref = jsonpointer.get(layoutSchema, item.$ref.replace('#/', '/'));
       if (ref.allOf) {
-        console.log('ref: ', ref);
-        ref = expandAllOf(ref, layoutSchema);
+        ref = expandAllOf(ref, layoutSchema, false);
       }
       anyOf.push(ref);
     } else if (item.properties) {
@@ -85,6 +105,7 @@ const generateComponentSchema = (name: string, layoutSchema: any) => {
 
   if (componentSchema.allOf) {
     schema = { ...schema, ...expandAllOf(componentSchema, layoutSchema) };
+
     if (
       !verifySchema(
         schema,
@@ -98,27 +119,42 @@ const generateComponentSchema = (name: string, layoutSchema: any) => {
   }
 
   // Expand all refs in properties
-  for (const property in schema.properties) {
-    if (
-      schema.properties[property].$ref &&
-      !schema.properties[property].$ref.startsWith('expression.schema')
-    ) {
-      schema.properties[property] = expandRef(schema.properties[property].$ref, layoutSchema);
-    }
-    if (schema.properties[property].items?.$ref) {
-      schema.properties[property].items = expandRef(
-        schema.properties[property].items.$ref,
-        layoutSchema,
-      );
-    }
-    ensureStringTypeWithEnums(schema.properties[property]);
-  }
+  schema.properties = expandRefsInProperties(schema.properties, layoutSchema);
   schema.title = `${name} component schema`;
   return schema;
 };
 
+const expandRefsInProperties = (properties: any, layoutSchema: any) => {
+  const expandedProperties = { ...properties };
+  for (const property in properties) {
+    if (
+      expandedProperties[property].$ref &&
+      !expandedProperties[property].$ref.startsWith('expression.schema')
+    ) {
+      expandedProperties[property] = expandRef(expandedProperties[property].$ref, layoutSchema);
+    }
+    if (expandedProperties[property].items?.$ref) {
+      expandedProperties[property].items = expandRef(
+        expandedProperties[property].items.$ref,
+        layoutSchema,
+      );
+    }
+    if (expandedProperties[property].allOf) {
+      expandedProperties[property] = expandAllOf(expandedProperties[property], layoutSchema);
+    }
+    ensureStringTypeWithEnums(expandedProperties[property]);
+  }
+
+  return expandedProperties;
+};
+
 const run = async () => {
-  const layoutSchema: any = await getLayoutSchema();
+  let version: string = process.argv[2] || '';
+  if (!isValidVersion(version)) {
+    version = 'v4';
+    console.warn('Invalid version. Please provide a valid version: v3 or v4. Defaulting to v4.');
+  }
+  const layoutSchema: any = await getLayoutSchema(version as AppFrontendVersion);
   const allComponents = layoutSchema.definitions.AnyComponent.properties.type.enum;
 
   allComponents.forEach((componentName: string) => {
@@ -126,7 +162,7 @@ const run = async () => {
       componentName === 'AddressComponent' ? 'Address' : componentName,
       layoutSchema,
     );
-    writeToFile(componentName, schema);
+    writeToFile(componentName, schema, version as AppFrontendVersion);
   });
 };
 
