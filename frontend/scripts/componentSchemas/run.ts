1624e42e1a3258b64ecd0d4b980b1bac43f0536e
Nina Kylstad <nkylstad@gmail.com>
2024-02-14 12:11:07 +0100

    order of textResourceBindings

--- frontend/scripts/componentSchemas/run.ts
+++ frontend/scripts/componentSchemas/run.ts
@@ -2,6 +2,10 @@ import { expandAllOf, expandAnyOf, expandRefsInProperties, verifySchema } from '
 import type { AppFrontendVersion } from './version';
 import { isValidVersion, versionSettings } from './version';
 import { getLayoutSchema } from './api';
+import { generateComponentPropertyLabels, generateTextResourceLabels } from './languageUtils';
+
+const allTextResourceBindingKeys = [];
+const allPropertyKeys = [];
 
 const writeToFile = (name: string, data: any, version: AppFrontendVersion) => {
   const path = require('path');
@@ -16,6 +20,17 @@ const writeToFile = (name: string, data: any, version: AppFrontendVersion) => {
   });
 };
 
+const addTextResourceBindingKeys = (schema: any) => {
+  if (schema.properties?.textResourceBindings) {
+    const textResourceBindingKeys = Object.keys(schema.properties.textResourceBindings.properties);
+    allTextResourceBindingKeys.push(...textResourceBindingKeys);
+  }
+};
+
+const addProperties = (propertyKeys: string[]) => {
+  allPropertyKeys.push(...propertyKeys);
+};
+
 const generateComponentSchema = (name: string, layoutSchema: any) => {
   const definitionName = `Comp${name}`;
   console.log('definitionName: ', definitionName);
@@ -27,6 +42,10 @@ const generateComponentSchema = (name: string, layoutSchema: any) => {
 
   if (componentSchema.allOf) {
     schema = { ...schema, ...expandAllOf(componentSchema, layoutSchema) };
+    const expectedProperties = Object.keys(
+      componentSchema.allOf[componentSchema.allOf.length - 1].properties,
+    );
+    addProperties(expectedProperties);
 
     if (
       !verifySchema(
@@ -42,8 +61,30 @@ const generateComponentSchema = (name: string, layoutSchema: any) => {
 
   // Expand all refs in properties
   schema.properties = expandRefsInProperties(schema.properties, layoutSchema);
+
+  // Sort text resource binding keys
+  if (schema.properties?.textResourceBindings) {
+    schema.properties.textResourceBindings.properties = sortTextResourceBindings(
+      schema.properties.textResourceBindings.properties,
+    );
+  }
   schema.title = `${name} component schema`;
   return schema;
+};
+
+const sortTextResourceBindings = (textResourceBindings: any) => {
+  const { title, description, help, ...rest } = textResourceBindings;
+  const sorted: any = {};
+  if (title) {
+    sorted.title = title;
+  }
+  if (description) {
+    sorted.description = description;
+  }
+  if (help) {
+    sorted.help = help;
+  }
+  return { ...sorted, ...rest };
 };
 
 const run = async () => {
@@ -62,12 +103,18 @@ const run = async () => {
       componentName === 'AddressComponent' ? 'Address' : componentName,
       layoutSchema,
     );
+    addTextResourceBindingKeys(schema);
     writeToFile(
       componentName === 'AddressComponent' ? 'Address' : componentName,
       schema,
       version as AppFrontendVersion,
     );
   });
+
+  const uniqueTextResourceBindingKeys = [...new Set(allTextResourceBindingKeys)];
+  generateTextResourceLabels(uniqueTextResourceBindingKeys);
+  console.log('--------------------------------------------------------');
+  generateComponentPropertyLabels([...new Set(allPropertyKeys)]);
 };
 
 run();
