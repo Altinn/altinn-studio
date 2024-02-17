6f518a992f1d3f6f62d8f7f0c9c73441fa520a5a
Lars <74791975+lassopicasso@users.noreply.github.com>
2024-02-17 23:42:22 +0100

    fix 2

--- frontend/packages/ux-editor/src/components/config/editModal/EditGrid/EditGrid.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditGrid/EditGrid.tsx
@@ -63,7 +63,7 @@ export const EditGrid = <T extends ComponentType>({
         {Object.values(ViewSize).map((viewSize: ViewSize) => {
           return (
             <Tabs.Tab key={viewSize} value={viewSize} className={classes.tab}>
-              {iconMapping[viewSize]}
+              {iconMapping[viewSize] || null }
               {t(`ux_editor.modal_properties_grid_size_${viewSize}`)}
             </Tabs.Tab>
           );
