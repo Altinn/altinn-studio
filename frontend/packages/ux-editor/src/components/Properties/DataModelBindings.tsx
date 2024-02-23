72a4042d9d138d6c42428ffc1026137af93a270d
andreastanderen <andreastanderen@gmail.com>
2024-02-23 16:02:34 +0100

    Add visual restrictions when component is attachment

--- frontend/packages/ux-editor/src/components/Properties/DataModelBindings.tsx
+++ frontend/packages/ux-editor/src/components/Properties/DataModelBindings.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { EditDataModelBindings } from '../config/editModal/EditDataModelBindings';
 import { StudioSpinner } from '@studio/components';
-import { Alert } from '@digdir/design-system-react';
+import { Alert, Switch } from '@digdir/design-system-react';
 import { useComponentSchemaQuery } from '../../hooks/queries/useComponentSchemaQuery';
 import { useFormItemContext } from '../../containers/FormItemContext';
 import { useText } from '../../hooks';
 import classes from './DataModelBindings.module.css';
+import { useFormLayout } from '../../hooks/useFormLayoutsSelector';
+import { ComponentType } from 'app-shared/types/ComponentType';
+import { isItemChildOfContainer } from '../../utils/formLayoutUtils';
+import { selectedLayoutNameSelector } from '../../selectors/formLayoutSelectors';
 
 export const DataModelBindings = (): React.JSX.Element => {
+  const selectedLayout = useSelector(selectedLayoutNameSelector);
+  const layout = useFormLayout(selectedLayout);
   const { formItemId, formItem, handleUpdate, debounceSave } = useFormItemContext();
   const { data: schema } = useComponentSchemaQuery(formItem.type);
+  const [multipleAttachments, setMultipleAttachments] = useState<boolean>(false);
   const t = useText();
 
   if (!schema) {
@@ -22,20 +30,41 @@ export const DataModelBindings = (): React.JSX.Element => {
     return <Alert>{t('ux_editor.modal_properties_data_model_binding_not_present')}</Alert>;
   }
 
+  let dataModelBindingsProperties = dataModelBindings?.properties;
+
   if (dataModelBindings.anyOf) {
-    dataModelBindings.anyOf.forEach((dataModelPropDescription) => {
-      const { properties } = dataModelPropDescription;
-      dataModelBindings.properties = {
-        ...dataModelBindings.properties,
-        ...properties,
-      };
-    });
+    const { properties } = Object.values(dataModelBindings.anyOf).find((dataModelProp: any) =>
+      (dataModelProp.required as string[]).includes(multipleAttachments ? 'list' : 'simpleBinding'),
+    ) as any;
+    dataModelBindingsProperties = properties;
   }
 
+  const handleMultipleAttachmentsSwitch = () => {
+    setMultipleAttachments(!multipleAttachments);
+    const updatedComponent = {
+      ...formItem,
+      dataModelBindings: {},
+    };
+    handleUpdate(updatedComponent);
+    debounceSave(formItemId, updatedComponent);
+  };
+
   return (
-    dataModelBindings?.properties && (
+    dataModelBindingsProperties && (
       <div className={classes.container}>
-        {Object.keys(dataModelBindings?.properties).map((propertyKey: string) => {
+        {(formItem.type === ComponentType.FileUploadWithTag ||
+          formItem.type === ComponentType.FileUpload) &&
+          isItemChildOfContainer(layout, formItem, ComponentType.RepeatingGroup) && (
+            <Alert severity='warning'>
+              {t('ux_editor.modal_properties_data_model_restrictions_attachment_components')}
+            </Alert>
+          )}
+        {dataModelBindings.anyOf && (
+          <Switch checked={multipleAttachments} onChange={handleMultipleAttachmentsSwitch}>
+            {t('schema_editor.multiple_answers')}
+          </Switch>
+        )}
+        {Object.keys(dataModelBindingsProperties).map((propertyKey: string) => {
           return (
             <div
               className={classes.dataModelBindings}
@@ -48,7 +77,7 @@ export const DataModelBindings = (): React.JSX.Element => {
                   debounceSave(formItemId, updatedComponent);
                 }}
                 editFormId={formItemId}
-                helpText={dataModelBindings?.properties[propertyKey]?.description}
+                helpText={dataModelBindingsProperties[propertyKey]?.description}
                 renderOptions={{
                   key: propertyKey,
                   label: propertyKey !== 'simpleBinding' ? propertyKey : undefined,
