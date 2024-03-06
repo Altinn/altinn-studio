0770cebd3b4bb71168fcb1d00e60e9acfee9ddb2
Ole Martin Handeland <github@olemartin.org>
2024-03-06 12:48:06 +0100

    Marking 8.0.0 final as our dependency (#1889)
    Co-authored-by: Ole Martin Handeland <git@olemartin.org>

--- src/features/applicationMetadata/ApplicationMetadataProvider.tsx
+++ src/features/applicationMetadata/ApplicationMetadataProvider.tsx
@@ -11,8 +11,8 @@ import { isAtLeastVersion } from 'src/utils/versionCompare';
 import type { HttpClientError } from 'src/utils/network/sharedNetworking';
 
 export const MINIMUM_APPLICATION_VERSION = {
-  build: '8.0.0.106',
-  name: 'v8.0.0-rc1',
+  build: '8.0.0.108',
+  name: 'v8.0.0',
 };
 
 const useApplicationMetadataQuery = () => {
