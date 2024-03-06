8076a6cb1aab62be9d8050e76fe838fc3c577eca
davidovrelid.com <david@framit.no>
2024-03-06 11:38:18 +0100

    dotnet format

--- backend/src/Designer/Services/Implementation/RepositorySI.cs
+++ backend/src/Designer/Services/Implementation/RepositorySI.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using Altinn.Authorization.ABAC.Utils;
