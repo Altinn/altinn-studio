0e89b8af94953b962f5d04ae8501790b4c298410
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-03-07 15:38:57 +0100

    Sort environment by priority

--- frontend/app-development/hooks/queries/useEnvironmentsQuery.ts
+++ frontend/app-development/hooks/queries/useEnvironmentsQuery.ts
@@ -4,11 +4,21 @@ import { useServicesContext } from 'app-shared/contexts/ServicesContext';
 import type { DeployEnvironment } from 'app-shared/types/DeployEnvironment';
 import { QueryKey } from 'app-shared/types/QueryKey';
 
+const sortedEnvironmentNames = ['production', 'tt02', 'yt01', 'at21', 'at22', 'at23', 'at24'];
+const compare = (a: DeployEnvironment, b: DeployEnvironment) => {
+  const index1 = sortedEnvironmentNames.indexOf(a.name.toLowerCase());
+  const index2 = sortedEnvironmentNames.indexOf(b.name.toLowerCase());
+  return index1 === -1 || index2 === -1 ? -1 : index1 - index2;
+};
+
 export const useEnvironmentsQuery = (meta?: QueryMeta): UseQueryResult<DeployEnvironment[]> => {
   const { getEnvironments } = useServicesContext();
   return useQuery<DeployEnvironment[]>({
     queryKey: [QueryKey.Environments],
-    queryFn: () => getEnvironments(),
+    queryFn: async () => {
+      const environments = await getEnvironments();
+      return [...environments].sort(compare);
+    },
     meta,
   });
 };
