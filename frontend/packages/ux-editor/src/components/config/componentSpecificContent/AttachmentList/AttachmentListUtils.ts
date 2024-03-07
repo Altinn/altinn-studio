2907c5c82e4e892c82bc219de806472a32b0a694
lassopicasso <lars.walderhaug@digdir.no>
2024-03-07 16:23:40 +0100

    Updated attachmentListUtils

--- frontend/packages/ux-editor/src/components/config/componentSpecificContent/AttachmentList/AttachmentListUtils.ts
+++ frontend/packages/ux-editor/src/components/config/componentSpecificContent/AttachmentList/AttachmentListUtils.ts
@@ -1,24 +1,33 @@
+import type { ApplicationMetadata } from 'app-shared/types/ApplicationMetadata';
+import type { LayoutSets } from 'app-shared/types/api/LayoutSetsResponse';
+
 export const reservedDataTypes = {
   includeAll: 'include-all',
   currentTask: 'current-task',
   refDataAsPdf: 'ref-data-as-pdf',
 };
 
-type convertAttachmentsToBackendArgs = {
-  includeAllAttachments: boolean;
-  includePdf: boolean;
-  onlyCurrentTask: boolean;
-  selectedAttachments: string[];
+export const dataExternalFormat = (
+  selectedDataTypes: string[],
+  availableAttachments: string[],
+): string[] => {
+  const selectedAttachments = selectedDataTypes.filter(
+    (dataType) => !Object.values(reservedDataTypes).includes(dataType),
+  );
+
+  const includeAllAttachments = selectedAttachments.length === availableAttachments.length;
+
+  if (!includeAllAttachments) {
+    return selectedDataTypes;
+  }
+
+  const includeCurrentTask = selectedDataTypes.includes(reservedDataTypes.currentTask);
+  const includePdf = selectedDataTypes.includes(reservedDataTypes.refDataAsPdf);
+
+  return allDataExternalFormat(includePdf, includeCurrentTask);
 };
 
-export const convertAttachmentsToBackend = (args: convertAttachmentsToBackendArgs) => {
-  const { includeAllAttachments, includePdf, onlyCurrentTask, selectedAttachments } = args;
-  return includeAllAttachments
-    ? convertAllToBackend(includePdf, onlyCurrentTask)
-    : convertSomeToBackend(includePdf, onlyCurrentTask, selectedAttachments);
-};
-
-const convertAllToBackend = (includePdf: boolean, onlyCurrentTask: boolean) => {
+const allDataExternalFormat = (includePdf: boolean, onlyCurrentTask: boolean): string[] => {
   const allAttachments: string[] = includePdf ? [reservedDataTypes.includeAll] : [];
 
   if (onlyCurrentTask) {
@@ -27,16 +36,93 @@ const convertAllToBackend = (includePdf: boolean, onlyCurrentTask: boolean) => {
   return allAttachments;
 };
 
-const convertSomeToBackend = (
-  includePdf: boolean,
-  onlyCurrentTask: boolean,
-  selectedAttachments: string[],
+export const dataInternalFormat = (
+  tasks: string[],
+  availableDataTypes: Partial<ApplicationMetadata['dataTypes']>,
+  dataTypeIds: string[],
 ) => {
+  const availableAttachments = getAvailableAttachments(tasks, availableDataTypes);
+  const selectedDataTypes = getSelectedDataTypes(dataTypeIds, availableAttachments);
+
+  return {
+    availableAttachments,
+    selectedDataTypes,
+  };
+};
+
+const getAvailableAttachments = (
+  tasks: string[],
+  availableDataTypes: Partial<ApplicationMetadata['dataTypes']>,
+): string[] => {
+  const filteredAttachments = availableDataTypes.filter(
+    (dataType) =>
+      !Object.values(reservedDataTypes).includes(dataType.id) &&
+      !dataType.appLogic &&
+      tasks.some((task) => dataType.taskId === task),
+  );
+  const mappedAttachments = filteredAttachments?.map((dataType) => dataType.id);
+  const sortedAttachments = mappedAttachments.sort((a, b) => a.localeCompare(b));
+
+  return sortedAttachments;
+};
+
+const getSelectedDataTypes = (dataTypeIds: string[], attachments: string[]): string[] => {
+  const dataTypeIdsWithoutReserved = dataTypeIds.filter(
+    (dataType) => !Object.values(reservedDataTypes).includes(dataType),
+  );
+  const includeCurrentTask = dataTypeIds.includes(reservedDataTypes.currentTask);
+  const includeAllAttachments =
+    dataTypeIds.includes(reservedDataTypes.includeAll) ||
+    dataTypeIdsWithoutReserved.length === attachments.length ||
+    dataTypeIds.length === 0 ||
+    (includeCurrentTask && dataTypeIds.length === 1);
+
+  if (!includeAllAttachments) {
+    return dataTypeIds;
+  }
+
+  const includePdf =
+    dataTypeIds.includes(reservedDataTypes.refDataAsPdf) ||
+    dataTypeIds.includes(reservedDataTypes.includeAll);
+
+  const selectedDataTypes = attachments.slice();
   if (includePdf) {
-    selectedAttachments.push(reservedDataTypes.refDataAsPdf);
+    selectedDataTypes.push(reservedDataTypes.refDataAsPdf);
   }
-  if (onlyCurrentTask) {
-    selectedAttachments.push(reservedDataTypes.currentTask);
+  if (includeCurrentTask) {
+    selectedDataTypes.push(reservedDataTypes.currentTask);
   }
-  return selectedAttachments;
+
+  return selectedDataTypes;
+};
+
+export const getTasks = (
+  layoutSets: LayoutSets,
+  selectedLayoutSet: string,
+  onlyCurrentTask: boolean,
+): string[] => {
+  return onlyCurrentTask
+    ? currentTasks(layoutSets, selectedLayoutSet)
+    : sampleTasks(layoutSets, selectedLayoutSet);
+};
+
+const currentTasks = (layoutSets: LayoutSets, selectedLayoutSet: string): string[] =>
+  layoutSets.sets.find((layoutSet) => layoutSet.id === selectedLayoutSet).tasks;
+
+const sampleTasks = (layoutSets: LayoutSets, selectedLayoutSet: string): string[] => {
+  const tasks = [];
+  for (const layoutSet of layoutSets.sets) {
+    tasks.push(...layoutSet.tasks);
+    if (layoutSet.id === selectedLayoutSet) {
+      break;
+    }
+  }
+  return tasks;
+};
+
+export const validateSelection = (selectedDataTypes: string[]): boolean => {
+  return (
+    selectedDataTypes.length > 0 ||
+    (selectedDataTypes.length === 1 && !selectedDataTypes.includes(reservedDataTypes.currentTask))
+  );
 };
