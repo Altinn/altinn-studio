ad494b5481091d9a6b9e41f35c92ad7fb9c410e4
lassopicasso <lars.walderhaug@digdir.no>
2024-03-08 14:19:17 +0100

    temperoray status on attachmentListComponent tests before removing the once that are  unecessary

--- frontend/packages/ux-editor/src/components/config/componentSpecificContent/AttachmentList/AttachmentListComponent.test.tsx
+++ frontend/packages/ux-editor/src/components/config/componentSpecificContent/AttachmentList/AttachmentListComponent.test.tsx
@@ -11,6 +11,7 @@ import { textMock } from '../../../../../../../testing/mocks/i18nMock';
 import type { LayoutSets } from 'app-shared/types/api/LayoutSetsResponse';
 import { QueryKey } from 'app-shared/types/QueryKey';
 import type { DataTypeElement } from 'app-shared/types/ApplicationMetadata';
+import { reservedDataTypes } from './AttachmentListUtils';
 
 const user = userEvent.setup();
 const org = 'org';
@@ -41,7 +42,8 @@ const defaultDataTypes: DataTypeElement[] = [
   { id: 'test2', taskId: 'Task_1', appLogic: {} },
   { id: 'test3', taskId: 'Task_2' },
   { id: 'test4', taskId: 'Task_3' },
-  { id: 'ref-data-as-pdf' },
+  { id: 'test5', taskId: 'Task_3' },
+  { id: reservedDataTypes.refDataAsPdf },
 ];
 
 const defaultComponent: FormAttachmentListComponent = {
@@ -82,11 +84,35 @@ describe('AttachmentListComponent', () => {
     ).toBeInTheDocument();
   });
 
-  it('should display "Alle vedlegg" as selected by default if there are no dataTypeIds selected', async () => {
-    await render();
-    expect(
-      screen.getByText(textMock('ux_editor.component_properties.select_all_attachments')),
-    ).toBeInTheDocument();
+  [
+    undefined,
+    [],
+    [reservedDataTypes.includeAll],
+    [reservedDataTypes.currentTask],
+    ['test1', 'test3', 'test4', 'test5'],
+    ['test1', 'test3', 'test4', 'test5', reservedDataTypes.refDataAsPdf],
+  ].forEach((dataTypeIds) => {
+    it(`checks "Select All Attachments" checkbox when selected dataTypeIds are "${dataTypeIds}"`, async () => {
+      await render(
+        {
+          component: {
+            ...defaultComponent,
+            dataTypeIds,
+          },
+        },
+        'layoutSetId3',
+      );
+
+      const checkbox = screen.getByRole('checkbox', {
+        name: textMock('ux_editor.component_properties.select_all_attachments'),
+      });
+      expect(checkbox).toBeChecked();
+      if (dataTypeIds === undefined || !dataTypeIds.includes(reservedDataTypes.currentTask)) {
+        expect(screen.getByText('test1')).toBeInTheDocument();
+        expect(screen.getByText('test3')).toBeInTheDocument();
+      }
+      expect(screen.getByText('test4')).toBeInTheDocument();
+    });
   });
 
   it('should display some specified datatypes if selected', async () => {
@@ -94,118 +120,77 @@ describe('AttachmentListComponent', () => {
       {
         component: {
           ...defaultComponent,
-          dataTypeIds: ['test1', 'test3', 'test4'],
+          dataTypeIds: ['test1', 'test3'],
         },
       },
-      'layoutSetId3',
+      'layoutSetId2',
     );
 
     expect(screen.getByText('test1')).toBeInTheDocument();
     expect(screen.getByText('test3')).toBeInTheDocument();
+  });
+
+  it('should remove selected attachments from other tasks if "only current task" is set to true', async () => {
+    handleComponentChange.mockClear();
+    await render(
+      {
+        component: {
+          ...defaultComponent,
+          dataTypeIds: ['test3', 'test4'],
+        },
+      },
+      'layoutSetId3',
+    );
+
+    // TODO: there is an issue where the combobox triggers onChangeValue when rendered and the initial selected value is not [].
+    // This is a workaround, I have created an issue in the designsystem: https://github.com/digdir/designsystemet/issues/1640
+    expect(handleComponentChange).toHaveBeenCalledTimes(1);
+    handleComponentChange.mockClear();
+
+    expect(screen.getByText('test3')).toBeInTheDocument();
     expect(screen.getByText('test4')).toBeInTheDocument();
-  });
-
-  it('should unselect other data types when "Alle vedlegg" is selected', async () => {
-    await render(
-      {
-        component: {
-          ...defaultComponent,
-          dataTypeIds: ['test1', 'test3', 'test4'],
-        },
-      },
-      'layoutSetId3',
-    );
-
-    const dropdown = screen.getByRole('combobox');
-    await act(() => user.click(dropdown));
-    const option = screen.getByRole('option', {
-      name: textMock('ux_editor.component_properties.select_all_attachments'),
-    });
-    await act(() => user.click(option));
-
-    expect(handleComponentChange).toHaveBeenCalledWith({
-      ...defaultComponent,
-      dataTypeIds: ['include-all'],
-    });
-  });
-
-  it('should unselect "Alle vedlegg" when other data types is selected', async () => {
-    await render(
-      {
-        component: {
-          ...defaultComponent,
-          dataTypeIds: ['include-all'],
-        },
-      },
-      'layoutSetId3',
-    );
-
-    const dropdown = screen.getByRole('combobox');
-    await act(() => user.click(dropdown));
-    const option = screen.getByRole('option', { name: 'test1' });
-    await act(() => user.click(option));
-
-    expect(handleComponentChange).toHaveBeenCalledWith({
-      ...defaultComponent,
-      dataTypeIds: ['test1'],
-    });
-  });
-
-  it('should remove selected attachments from other tasks if only current task is set to true', async () => {
-    await render(
-      {
-        component: {
-          ...defaultComponent,
-          dataTypeIds: ['test1', 'test4'],
-        },
-      },
-      'layoutSetId3',
-    );
 
     const currentTaskCheckbox = screen.getByRole('checkbox', {
       name: textMock('ux_editor.component_properties.current_task'),
     });
-    expect(screen.getByText('test1')).toBeInTheDocument();
-    expect(screen.getByText('test4')).toBeInTheDocument();
-    await act(() => user.click(currentTaskCheckbox));
-
-    expect(screen.queryByText('test1')).not.toBeInTheDocument();
-    expect(screen.getByText('test4')).toBeInTheDocument();
-  });
-
-  it('should display only data types from current task when switch is checked', async () => {
-    await render({}, 'layoutSetId3');
-
-    const dropdown = screen.getByRole('combobox');
-    const currentTaskCheckbox = screen.getByRole('checkbox', {
-      name: textMock('ux_editor.component_properties.current_task'),
-    });
-
-    await act(() => user.click(dropdown));
-
-    expect(screen.getByRole('option', { name: 'test1' })).toBeInTheDocument();
-    expect(screen.getByRole('option', { name: 'test4' })).toBeInTheDocument();
 
     await act(() => user.click(currentTaskCheckbox));
     expect(currentTaskCheckbox).toBeChecked();
-    await act(() => user.click(dropdown));
 
-    expect(screen.queryByRole('option', { name: 'test1' })).not.toBeInTheDocument();
-    expect(screen.getByRole('option', { name: 'test4' })).toBeInTheDocument();
+    expect(handleComponentChange).toHaveBeenNthCalledWith(1, {
+      ...defaultComponent,
+      dataTypeIds: ['test4', 'current-task'],
+    });
+    expect(handleComponentChange).toHaveBeenCalledTimes(2);
+    expect(handleComponentChange).toHaveBeenNthCalledWith(2, {
+      ...defaultComponent,
+      dataTypeIds: ['test4', 'current-task'],
+    });
+    expect(screen.queryByText('test3')).not.toBeInTheDocument();
+    expect(screen.getByText('test4')).toBeInTheDocument();
   });
 
   it('should include pdf if the switch is set to true', async () => {
-    await render({}, 'layoutSetId3');
-
+    handleComponentChange.mockClear();
+    await render(
+      {
+        component: {
+          ...defaultComponent,
+          dataTypeIds: ['test3', 'test4'],
+        },
+      },
+      'layoutSetId3',
+    );
+    expect(handleComponentChange).toHaveBeenCalledTimes(1);
+    handleComponentChange.mockClear();
     const includePdfCheckbox = screen.getByRole('checkbox', {
       name: textMock('ux_editor.component_properties.select_pdf'),
     });
 
     await act(() => user.click(includePdfCheckbox));
-
     expect(handleComponentChange).toHaveBeenCalledWith({
       ...defaultComponent,
-      dataTypeIds: ['ref-data-as-pdf'],
+      dataTypeIds: ['test3', 'test4', reservedDataTypes.refDataAsPdf],
     });
   });
 
@@ -215,14 +200,10 @@ describe('AttachmentListComponent', () => {
     const dropdown = screen.getByRole('combobox');
     await act(() => user.click(dropdown));
 
-    expect(
-      screen.getByRole('option', {
-        name: textMock('ux_editor.component_properties.select_all_attachments'),
-      }),
-    ).toBeInTheDocument();
     expect(screen.getByRole('option', { name: 'test1' })).toBeInTheDocument();
     expect(screen.getByRole('option', { name: 'test3' })).toBeInTheDocument();
     expect(screen.getByRole('option', { name: 'test4' })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: 'test5' })).toBeInTheDocument();
     expect(screen.queryByRole('option', { name: 'test2' })).not.toBeInTheDocument();
   });
 
@@ -241,7 +222,7 @@ describe('AttachmentListComponent', () => {
     await act(() => user.click(dropdown));
     const option = screen.getByRole('option', { name: 'test3' });
     await act(() => user.click(option));
-
+    expect(handleComponentChange).toHaveBeenCalledTimes(1);
     expect(handleComponentChange).toHaveBeenCalledWith({
       ...defaultComponent,
       dataTypeIds: ['test1', 'test4', 'test3'],
@@ -269,7 +250,7 @@ describe('AttachmentListComponent', () => {
       {
         component: {
           ...defaultComponent,
-          dataTypeIds: ['ref-data-as-pdf'],
+          dataTypeIds: [reservedDataTypes.refDataAsPdf],
         },
       },
       'layoutSetId3',
@@ -285,7 +266,7 @@ describe('AttachmentListComponent', () => {
       {
         component: {
           ...defaultComponent,
-          dataTypeIds: ['ref-data-as-pdf', 'test1'],
+          dataTypeIds: [reservedDataTypes.refDataAsPdf, 'test1'],
         },
       },
       'layoutSetId3',
