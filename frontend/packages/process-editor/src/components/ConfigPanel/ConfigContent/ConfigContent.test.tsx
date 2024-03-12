b67478ca5448f9d672afcdd49508e3982242517f
davidovrelid.com <david@framit.no>
2024-03-12 11:14:41 +0100

    code clean up wrote tests for ConfigContent

--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/ConfigContent.test.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/ConfigContent.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ConfigContent } from './ConfigContent';
-import { render as rtlRender, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { textMock } from '../../../../../../testing/mocks/i18nMock';
 import type { BpmnContextProps } from '../../../contexts/BpmnContext';
 import { BpmnContext } from '../../../contexts/BpmnContext';
 import type { BpmnDetails } from '../../../types/BpmnDetails';
 import { BpmnTypeEnum } from '../../../enum/BpmnTypeEnum';
+import userEvent from '@testing-library/user-event';
 
 const mockBPMNXML: string = `<?xml version="1.0" encoding="UTF-8"?></xml>`;
 const mockAppLibVersion8: string = '8.0.3';
@@ -32,81 +33,80 @@ const mockBpmnContextValue: BpmnContextProps = {
 };
 
 describe('ConfigContent', () => {
-  afterEach(jest.clearAllMocks);
+  it('should render heading for selected task', () => {
+    renderConfigContent({ bpmnDetails: { ...mockBpmnDetails, taskType: 'data' } });
 
-  it('should display the details about the selected task when a "data" task is selected', () => {
-    render();
+    const heading = screen.getByRole('heading', {
+      name: textMock('process_editor.configuration_panel_data_task'),
+      level: 2,
+    });
 
-    expect(
-      screen.getByRole('heading', {
-        name: textMock('process_editor.configuration_panel_data_task'),
-        level: 2,
-      }),
-    ).toBeInTheDocument();
-
-    expect(screen.getByText(mockBpmnDetails.id)).toBeInTheDocument();
-    expect(screen.getByText(mockBpmnDetails.name)).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
   });
 
-  it('should display the details about the selected task when a "confirmation" task is selected', () => {
-    render({ bpmnDetails: { ...mockBpmnDetails, taskType: 'confirmation' } });
+  it('should render helpText for selected task', async () => {
+    const user = userEvent.setup();
+    renderConfigContent({ bpmnDetails: { ...mockBpmnDetails, taskType: 'data' } });
 
-    expect(
-      screen.getByRole('heading', {
-        name: textMock('process_editor.configuration_panel_confirmation_task'),
-        level: 2,
-      }),
-    ).toBeInTheDocument();
+    const helpTextButton = screen.getByRole('button', {
+      name: textMock('process_editor.configuration_panel_header_help_text_title'),
+    });
+    await act(() => user.click(helpTextButton));
 
-    expect(screen.getByText(mockBpmnDetails.id)).toBeInTheDocument();
-    expect(screen.getByText(mockBpmnDetails.name)).toBeInTheDocument();
+    const helpText = screen.getByText(
+      textMock('process_editor.configuration_panel_header_help_text_data'),
+    );
+
+    expect(helpText).toBeInTheDocument();
   });
 
-  it('should display the details about the selected task when a "feedback" task is selected', () => {
-    render({ bpmnDetails: { ...mockBpmnDetails, taskType: 'feedback' } });
+  it('should render EditTaskId component', () => {
+    renderConfigContent({ bpmnDetails: { ...mockBpmnDetails, taskType: 'data' } });
 
-    expect(
-      screen.getByRole('heading', {
-        name: textMock('process_editor.configuration_panel_feedback_task'),
-        level: 2,
-      }),
-    ).toBeInTheDocument();
+    const editTaskIdButton = screen.getByRole('button', {
+      name: textMock('process_editor.configuration_panel_change_task_id'),
+    });
 
-    expect(screen.getByText(mockBpmnDetails.id)).toBeInTheDocument();
-    expect(screen.getByText(mockBpmnDetails.name)).toBeInTheDocument();
+    expect(editTaskIdButton).toBeInTheDocument();
   });
 
-  it('should display the details about the selected task when a "signing" task is selected', () => {
-    render({ bpmnDetails: { ...mockBpmnDetails, taskType: 'signing' } });
+  it('should be able to change task id', async () => {
+    const user = userEvent.setup();
+    renderConfigContent({ bpmnDetails: { ...mockBpmnDetails, taskType: 'data' } });
 
-    expect(
-      screen.getByRole('heading', {
-        name: textMock('process_editor.configuration_panel_signing_task'),
-        level: 2,
-      }),
-    ).toBeInTheDocument();
+    const editTaskIdButton = screen.getByRole('button', {
+      name: textMock('process_editor.configuration_panel_change_task_id'),
+    });
 
-    expect(screen.getByText(mockBpmnDetails.id)).toBeInTheDocument();
-    expect(screen.getByText(mockBpmnDetails.name)).toBeInTheDocument();
+    await act(() => user.click(editTaskIdButton));
+    const input = screen.getByLabelText(
+      textMock('process_editor.configuration_panel_change_task_id'),
+    );
+
+    await act(() => user.clear(input));
+    await act(() => user.type(input, 'newId'));
+
+    // TODO implement test for saving the new id
+    expect(false).toBeTruthy();
   });
 
-  it('should display the details about the selected task when a task not of type "BpmnTaskType" is selected', () => {
-    render({ bpmnDetails: { ...mockBpmnDetails, taskType: undefined } });
+  it.each(['data', 'confirmation', 'feedback', 'signing'])(
+    'should render correct header config for each taskType',
+    (taskType) => {
+      renderConfigContent({ bpmnDetails: { ...mockBpmnDetails, taskType } });
 
-    expect(
-      screen.getByRole('heading', {
-        name: textMock('process_editor.configuration_panel_missing_task'),
+      const heading = screen.getByRole('heading', {
+        name: textMock(`process_editor.configuration_panel_${taskType}_task`),
         level: 2,
-      }),
-    ).toBeInTheDocument();
+      });
 
-    expect(screen.getByText(mockBpmnDetails.id)).toBeInTheDocument();
-    expect(screen.getByText(mockBpmnDetails.name)).toBeInTheDocument();
-  });
+      expect(heading).toBeInTheDocument();
+    },
+  );
 });
 
-const render = (rootContextProps: Partial<BpmnContextProps> = {}) => {
-  return rtlRender(
+const renderConfigContent = (rootContextProps: Partial<BpmnContextProps> = {}) => {
+  return render(
     <BpmnContext.Provider value={{ ...mockBpmnContextValue, ...rootContextProps }}>
       <ConfigContent />
     </BpmnContext.Provider>,
--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/ConfigContent.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/ConfigContent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { StudioSectionHeader, StudioToggleableTextfield } from '@studio/components';
-import { KeyVerticalIcon } from '@studio/icons';
+import { StudioSectionHeader } from '@studio/components';
 import { ConfigIcon } from './ConfigIcon';
 import { getConfigTitleKey, getConfigTitleHelpTextKey } from '../../../utils/configPanelUtils';
 import { useBpmnContext } from '../../../contexts/BpmnContext';
+import { EditTaskId } from './EditTaskId/EditTaskId';
 
 export const ConfigContent = (): React.ReactElement => {
   const { t } = useTranslation();
@@ -15,13 +15,8 @@ export const ConfigContent = (): React.ReactElement => {
   const configHeaderHelpText =
     bpmnDetails?.taskType && t(getConfigTitleHelpTextKey(bpmnDetails?.taskType));
 
-  const handleTaskIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newId = event.target.value;
-    // TODO: Use BPMN to update properties within the XML.
-    console.log({
-      oldId: bpmnDetails.id,
-      newId,
-    });
+  const handleTaskIdChange = () => {
+    // TODO implement the handler
   };
 
   return (
@@ -37,25 +32,7 @@ export const ConfigContent = (): React.ReactElement => {
           title: t('process_editor.configuration_panel_header_help_text_title'),
         }}
       />
-      <StudioToggleableTextfield
-        inputProps={{
-          icon: <KeyVerticalIcon />,
-          label: t('process_editor.configuration_panel_change_task_id'),
-          value: bpmnDetails.id,
-          onBlur: (event) => handleTaskIdChange(event),
-          size: 'small',
-        }}
-        viewProps={{
-          children: (
-            <span>
-              <b>ID:</b> {bpmnDetails.id}
-            </span>
-          ),
-          value: bpmnDetails.id,
-          variant: 'tertiary',
-          'aria-label': t('process_editor.configuration_panel_change_task_id'),
-        }}
-      />
+      <EditTaskId onChange={handleTaskIdChange} />
     </>
   );
 };
