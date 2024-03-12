9759f8e210dc6604bab34a815fa0325a1a7db0a5
davidovrelid.com <david@framit.no>
2024-03-12 10:58:27 +0100

    clean up component and write test for this component only

--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigPanel.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigPanel.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
-import classes from './ConfigPanel.module.css';
 import { useTranslation } from 'react-i18next';
 import { Paragraph, Alert, Heading } from '@digdir/design-system-react';
 import { useBpmnContext } from '../../contexts/BpmnContext';
 import { BpmnTypeEnum } from '../../enum/BpmnTypeEnum';
 import { ConfigContent } from './ConfigContent';
+import { ConfigSurface } from '../ConfigSurface/ConfigSurface';
 
 export const ConfigPanel = (): React.ReactElement => {
-  return <div className={classes.configPanel}>{<ConfigPanelContent />}</div>;
+  return (
+    <ConfigSurface>
+      <ConfigPanelContent />
+    </ConfigSurface>
+  );
 };
 
 const ConfigPanelContent = (): React.ReactElement => {
@@ -19,8 +22,8 @@ const ConfigPanelContent = (): React.ReactElement => {
   if (noTaskSelected) {
     return (
       <BpmnAlert
-        title='Ingen oppgave er valgt.'
-        message={t('process_editor.configuration_panel_no_task')}
+        title={t('process_editor.configuration_panel_no_task_title')}
+        message={t('process_editor.configuration_panel_no_task_message')}
       />
     );
   }
@@ -32,7 +35,7 @@ const ConfigPanelContent = (): React.ReactElement => {
 
   return (
     <BpmnAlert
-      title='Elementet er ikke støttet'
+      title={t('process_editor.configuration_panel_element_not_supported_title')}
       message={t('process_editor.configuration_panel_element_not_supported')}
     />
   );
