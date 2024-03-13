0f088c5e6aab4a8fc17fc55396dddc1b851676bc
Nina Kylstad <nkylstad@gmail.com>
2024-03-13 14:40:07 +0100

    ensure added and removed lists are synced

--- frontend/packages/process-editor/src/utils/processEditorUtils/processEditorUtils.test.ts
+++ frontend/packages/process-editor/src/utils/processEditorUtils/processEditorUtils.test.ts
@@ -1,4 +1,6 @@
-import { supportsProcessEditor } from './processEditorUtils';
+import { BpmnTypeEnum } from '../../enum/BpmnTypeEnum';
+import type { BpmnDetails } from '../../types/BpmnDetails';
+import { supportsProcessEditor, updateDataTaskTrackingLists } from './processEditorUtils';
 
 describe('processEditorUtils', () => {
   describe('supportsProcessEditor', () => {
@@ -10,6 +12,46 @@ describe('processEditorUtils', () => {
     it('returns false if version is older than 8', () => {
       const result = supportsProcessEditor('7.1.2');
       expect(result).toBeFalsy();
+    });
+  });
+
+  describe('updateDataTaskTrackingLists', () => {
+    it('calls the methods to add item to primary list and remove from secondary list', () => {
+      const updatePrimaryList = jest.fn();
+      const updateSecondaryList = jest.fn();
+      const itemToAdd: BpmnDetails = {
+        id: '1',
+        name: 'test',
+        taskType: 'data',
+        type: BpmnTypeEnum.Task,
+      };
+      const secondaryList: BpmnDetails[] = [
+        { id: '1', name: 'test', taskType: 'data', type: BpmnTypeEnum.Task },
+      ];
+
+      updateDataTaskTrackingLists(updatePrimaryList, updateSecondaryList, itemToAdd, secondaryList);
+
+      expect(updatePrimaryList).toHaveBeenCalledTimes(1);
+      expect(updateSecondaryList).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls the methods to add item to primary list only when item is not in secondary list', () => {
+      const updatePrimaryList = jest.fn();
+      const updateSecondaryList = jest.fn();
+      const itemToAdd: BpmnDetails = {
+        id: '1',
+        name: 'test',
+        taskType: 'data',
+        type: BpmnTypeEnum.Task,
+      };
+      const secondaryList: BpmnDetails[] = [
+        { id: '2', name: 'test2', taskType: 'data', type: BpmnTypeEnum.Task },
+      ];
+
+      updateDataTaskTrackingLists(updatePrimaryList, updateSecondaryList, itemToAdd, secondaryList);
+
+      expect(updatePrimaryList).toHaveBeenCalledTimes(1);
+      expect(updateSecondaryList).not.toHaveBeenCalled();
     });
   });
 });
