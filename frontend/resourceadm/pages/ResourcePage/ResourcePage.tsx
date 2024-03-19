e6b4144b736dd24e3de0f8626b11c780bdead7c4
Martin Gunnerud <mgunnerud@gmail.com>
2024-03-19 14:46:02 +0100

    add missing import after merge conflict

--- frontend/resourceadm/pages/ResourcePage/ResourcePage.tsx
+++ frontend/resourceadm/pages/ResourcePage/ResourcePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { NavigationBarPage } from '../../types/NavigationBarPage';
 import classes from './ResourcePage.module.css';
