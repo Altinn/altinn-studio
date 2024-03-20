88a1fea39a7787dd08a80b003edb1ee385523dbd
andreastanderen <71079896+standeren@users.noreply.github.com>
2024-03-20 12:34:30 +0100

    Refactor

--- backend/src/Designer/Services/Interfaces/IAppDevelopmentService.cs
+++ backend/src/Designer/Services/Interfaces/IAppDevelopmentService.cs
@@ -95,7 +95,7 @@ namespace Altinn.Studio.Designer.Services.Interfaces
         public Task<LayoutSets> GetLayoutSets(AltinnRepoEditingContext altinnRepoEditingContext, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Adds a config for an additional layout set to the layout-set.json
+        /// Adds a config for an additional layout set to the layout-sets.json
         /// </summary>
         /// <param name="altinnRepoEditingContext">An <see cref="AltinnRepoEditingContext"/>.</param>
         /// <param name="newLayoutSet">Config for the new layout set</param>
@@ -107,7 +107,7 @@ namespace Altinn.Studio.Designer.Services.Interfaces
         /// </summary>
         /// <param name="altinnRepoEditingContext">An <see cref="AltinnRepoEditingContext"/>.</param>
         /// <param name="layoutSetToUpdateId">The id of the layout set to replace</param>
-        /// <param name="newLayoutSet">Config for the new layout set</param>
+        /// <param name="newLayoutSet">Config for the updated layout set</param>
         /// <param name="cancellationToken">An <see cref="CancellationToken"/> that observes if operation is cancelled.</param>
         public Task<LayoutSets> UpdateLayoutSet(AltinnRepoEditingContext altinnRepoEditingContext, string layoutSetToUpdateId, LayoutSetConfig newLayoutSet, CancellationToken cancellationToken = default);
 
