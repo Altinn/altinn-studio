dc7113a4f69d53c1ece2d397f9458840b7912be5
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-03-21 14:12:20 +0100

    Change OrderDetailsCalculator implementation to calculate dynamic order lines based on inventory.

--- App/custom/OrderDetailsCalculator.cs
+++ App/custom/OrderDetailsCalculator.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Altinn.App.Core.Features.Payment;
 using Altinn.App.Core.Features.Payment.Models;
+using Altinn.App.Core.Internal.Data;
+using Altinn.App.Core.Models;
+using Altinn.App.Models;
 using Altinn.Platform.Storage.Interface.Models;
 
 namespace Altinn.App.Custom.Payment;
@@ -12,30 +16,44 @@ namespace Altinn.App.Custom.Payment;
 /// </summary>
 public class OrderDetailsCalculator : IOrderDetailsCalculator
 {
-    public Task<OrderDetails> CalculateOrderDetails(Instance instance)
-    {
-        List<Thing> things =
-        [
-            new Thing { Description = "A thing", Price = 50 },
-            new Thing { Description = "Another thing", Price = 100 }
-        ];
+    private readonly IDataClient _dataClient;
 
-        List<PaymentOrderLine> paymentOrderLines = things
+    public OrderDetailsCalculator(IDataClient dataClient)
+    {
+        _dataClient = dataClient;
+    }
+    
+    public async Task<OrderDetails> CalculateOrderDetails(Instance instance)
+    {
+        DataElement modelData = instance.Data.Single(x => x.DataType == "model");
+        InstanceIdentifier instanceIdentifier = new(instance);
+        
+        var formData = (Form) await _dataClient.GetFormData(instanceIdentifier.InstanceGuid, typeof(Form), instance.Org, instance.AppId,
+            instanceIdentifier.InstanceOwnerPartyId, new Guid(modelData.Id));
+        
+        List<PaymentOrderLine> paymentOrderLines = formData.GoodsAndServicesProperties.Inventory.InventoryProperties
             .Select((x, index) =>
                 new PaymentOrderLine
                 {
-                    Id = index.ToString(), Name = x.Description, PriceExVat = x.Price, Quantity = 1, VatPercent = 25.00M
+                    Id = index.ToString(), Name = $"{x.NiceClassification} - {x.GoodsAndServices}", PriceExVat = GetPriceForNiceClassification(x), Quantity = 1, VatPercent = 25.00M
                 })
             .ToList();
 
         var orderDetails = new OrderDetails { Currency = "NOK", OrderLines = paymentOrderLines };
 
-        return Task.FromResult(orderDetails);
+        return orderDetails;
     }
 
-    private class Thing
+    private decimal GetPriceForNiceClassification(InventoryProperties inventoryProperties)
     {
-        public required string Description { get; init; }
-        public required decimal Price { get; init; }
+        switch (inventoryProperties.NiceClassification)
+        {
+            case "1":
+                return 1000.00M;
+            case "2":
+                return 2000.00M;
+            default:
+                return 500.00M;
+        }
     }
 }
