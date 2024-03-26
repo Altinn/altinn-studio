71142e06953f158b884c3ab3fc3a5027ede6a12c
andreastanderen <71079896+standeren@users.noreply.github.com>
2024-03-26 15:40:11 +0100

    Add $schema and uiSettings to LayoutSets model in backend (#12579)
    * Add  and uiSettings to LayoutSets model in backend
    
    * Update tests

--- backend/src/Designer/Models/LayoutSets.cs
+++ backend/src/Designer/Models/LayoutSets.cs
@@ -1,13 +1,20 @@
+#nullable enable
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using JetBrains.Annotations;
 
 namespace Altinn.Studio.Designer.Models;
 
-public class LayoutSets
+public class LayoutSets : Altinn.App.Core.Models.LayoutSets
 {
+    [JsonPropertyName("$schema")]
+    public string Schema { get; set; }
+
     [JsonPropertyName("sets")]
     public List<LayoutSetConfig> Sets { get; set; }
+
+    [JsonExtensionData]
+    public IDictionary<string, object?> UnknownProperties { get; set; }
 }
 
 public class LayoutSetConfig
@@ -20,4 +27,7 @@ public class LayoutSetConfig
 
     [JsonPropertyName("tasks")]
     public List<string> Tasks { get; set; }
+
+    [JsonExtensionData]
+    public IDictionary<string, object?> UnknownProperties { get; set; }
 }
