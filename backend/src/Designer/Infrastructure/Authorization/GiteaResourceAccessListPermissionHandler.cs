7b0a287dfd1068aa4fec36a11b0ca2bac27f854e
Martin Gunnerud <mgunnerud@gmail.com>
2024-04-04 15:19:26 +0200

    Resourceadm: add Gitea teams for publish resources and access lists write permission (#12615)
    * add Authorize attribute for publish resource and resource access list services
    
    * add localGiteaAdmin and cypress test user to new Resources-Publish and AccessLists Gitea teams when running setup.js

---
+++ backend/src/Designer/Infrastructure/Authorization/GiteaResourceAccessListPermissionHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.RepositoryClient.Model;
+using Altinn.Studio.Designer.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Altinn.Studio.Designer.Infrastructure.Authorization
+{
+    /// <summary>
+    /// Authorization Handler for GiteaResourceAccessListPermissionRequirement
+    /// </summary>
+    public class GiteaResourceAccessListPermissionHandler : AuthorizationHandler<GiteaResourceAccessListPermissionRequirement>
+    {
+        private readonly IGitea _giteaApiWrapper;
+        private readonly HttpContext _httpContext;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="giteaApiWrapper">IGitea</param>
+        /// <param name="httpContextAccessor">IHttpContextAccessor</param>
+        public GiteaResourceAccessListPermissionHandler(
+            IGitea giteaApiWrapper,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContext = httpContextAccessor.HttpContext;
+            _giteaApiWrapper = giteaApiWrapper;
+        }
+
+        /// <inheritdoc/>
+        protected override async Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            GiteaResourceAccessListPermissionRequirement requirement)
+        {
+            if (_httpContext == null)
+            {
+                return;
+            }
+
+            string org = _httpContext.GetRouteValue("org")?.ToString();
+            string environment = _httpContext.Request.Query["env"];
+            if (string.IsNullOrWhiteSpace(org) || string.IsNullOrWhiteSpace(environment))
+            {
+                _httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
+            string matchTeam = $"Accesslists-{environment}";
+            List<Team> teams = await _giteaApiWrapper.GetTeams();
+
+            bool isTeamMember = teams.Any(t =>
+                t.Organization.Username.Equals(org, System.StringComparison.OrdinalIgnoreCase) &&
+                t.Name.Equals(matchTeam, System.StringComparison.OrdinalIgnoreCase)
+            );
+
+            if (isTeamMember)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                _httpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            }
+        }
+    }
+}
