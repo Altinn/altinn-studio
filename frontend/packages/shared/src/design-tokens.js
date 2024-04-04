03b0d539f6c9d76046af4efd98d374a1a4f77d0b
davidovrelid.com <david@framit.no>
2024-04-04 14:22:25 +0200

    expriment with next version

--- frontend/packages/shared/src/design-tokens.js
+++ frontend/packages/shared/src/design-tokens.js
@@ -1,2 +1,4 @@
 import '@altinn/figma-design-tokens/dist/tokens.css';
-import '@digdir/design-system-tokens/brand/altinn/tokens.css';
+import '@digdir/designsystemet-theme/brand/altinn/tokens.css';
+import '@digdir/designsystemet-css';
+// import './designsystemet-css-layer.css';
