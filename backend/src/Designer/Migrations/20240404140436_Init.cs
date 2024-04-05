8c75da9d9d664c6bb593bc7170f599e2e1c19eba
Mirko Sekulic <misha.sekulic@gmail.com>
2024-04-05 11:57:29 +0200

    use idempotent slq script as init ef-core script

--- backend/src/Designer/Migrations/20240404140436_Init.cs
+++ backend/src/Designer/Migrations/20240404140436_Init.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Altinn.Studio.Designer.Migrations.InitialSqlScripts;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,73 +14,18 @@ namespace Altinn.Studio.Designer.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.EnsureSchema(
-                name: "designer");
-
-            migrationBuilder.CreateTable(
-                name: "deployments",
-                schema: "designer",
-                columns: table => new
-                {
-                    sequenceno = table.Column<long>(type: "BIGSERIAL", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    buildid = table.Column<string>(type: "character varying", nullable: true),
-                    tagname = table.Column<string>(type: "character varying", nullable: true),
-                    org = table.Column<string>(type: "character varying", nullable: true),
-                    app = table.Column<string>(type: "character varying", nullable: true),
-                    buildresult = table.Column<string>(type: "character varying", nullable: true),
-                    created = table.Column<DateTime>(type: "timestamptz", nullable: false),
-                    entity = table.Column<string>(type: "text", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("deployments_pkey", x => x.sequenceno);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "releases",
-                schema: "designer",
-                columns: table => new
-                {
-                    sequenceno = table.Column<long>(type: "BIGSERIAL", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    buildid = table.Column<string>(type: "character varying", nullable: true),
-                    tagname = table.Column<string>(type: "character varying", nullable: true),
-                    org = table.Column<string>(type: "character varying", nullable: true),
-                    app = table.Column<string>(type: "character varying", nullable: true),
-                    buildstatus = table.Column<string>(type: "character varying", nullable: true),
-                    buildresult = table.Column<string>(type: "character varying", nullable: true),
-                    created = table.Column<DateTime>(type: "timestamptz", nullable: false),
-                    entity = table.Column<string>(type: "text", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("releases_pkey", x => x.sequenceno);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "idx_deployments_org_app",
-                schema: "designer",
-                table: "deployments",
-                columns: new[] { "org", "app" });
-
-            migrationBuilder.CreateIndex(
-                name: "idx_releases_org_app",
-                schema: "designer",
-                table: "releases",
-                columns: new[] { "org", "app" });
+            // initial sql used in yuniql migration are idempotent so it is safe to execute them as initial ef-core migration
+            migrationBuilder.Sql(InitialScriptsReadHelper.ReadInitialSqlScript("01-setup-tables.sql"));
+            migrationBuilder.Sql(InitialScriptsReadHelper.ReadInitialSqlScript("02-setup-deployments.sql"));
+            migrationBuilder.Sql(InitialScriptsReadHelper.ReadInitialSqlScript("03-setup-releases.sql"));
+            migrationBuilder.Sql(InitialScriptsReadHelper.ReadInitialSqlScript("04-setup-grants.sql"));
+            migrationBuilder.Sql(InitialScriptsReadHelper.ReadInitialSqlScript("05-setup-index.sql"));
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "deployments",
-                schema: "designer");
-
-            migrationBuilder.DropTable(
-                name: "releases",
-                schema: "designer");
+            // Disable down migration
         }
     }
 }
