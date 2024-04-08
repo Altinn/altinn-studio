e01b90776cf4e707e42644964728e327ded81e70
Rune T. Larsen <rune@hjemmekino.no>
2024-04-08 11:57:54 +0200

    dotnet format

--- backend/src/Designer/Enums/ResourceType.cs
+++ backend/src/Designer/Enums/ResourceType.cs
@@ -1,4 +1,4 @@
-﻿using NpgsqlTypes;
+﻿
 
 namespace Altinn.Studio.Designer.Enums
 {
@@ -7,25 +7,23 @@ namespace Altinn.Studio.Designer.Enums
     /// </summary>
     public enum ResourceType
     {
-        [PgName("default")]
+
         Default = 0,
 
-        [PgName("systemresource")]
         Systemresource = 1,
 
-        [PgName("maskinportenschema")]
         MaskinportenSchema = 2,
 
-        [PgName("altinn2service")]
+
         Altinn2Service = 4,
 
-        [PgName("altinnapp")]
+
         AltinnApp = 8,
 
-        [PgName("genericaccessresource")]
+
         GenericAccessResource = 16,
 
-        [PgName("brokerservice")]
+
         BrokerService = 32,
     }
 }
