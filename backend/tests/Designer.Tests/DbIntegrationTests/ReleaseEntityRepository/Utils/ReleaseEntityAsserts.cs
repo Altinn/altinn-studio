89a73033eab44eeeb0ae3f8e52aa734e81191b4f
Mirko Sekulic <misha.sekulic@gmail.com>
2024-04-09 23:19:16 +0200

    allow 100 ms percision loss

--- backend/tests/Designer.Tests/DbIntegrationTests/ReleaseEntityRepository/Utils/ReleaseEntityAsserts.cs
+++ backend/tests/Designer.Tests/DbIntegrationTests/ReleaseEntityRepository/Utils/ReleaseEntityAsserts.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using Altinn.Studio.Designer.Repository.Models;
 using FluentAssertions;
@@ -14,8 +15,13 @@ public static partial class EntityAssertions
         dbRecord.Buildid.Should().BeEquivalentTo(releaseEntity.Build.Id);
         dbRecord.Buildresult.Should().BeEquivalentTo(releaseEntity.Build.Result.ToString());
         dbRecord.Tagname.Should().BeEquivalentTo(releaseEntity.TagName);
-        dbRecord.Created.Should().Be(releaseEntity.Created);
         var entityFromColumn = JsonSerializer.Deserialize<ReleaseEntity>(dbRecord.Entity, JsonOptions);
         entityFromColumn.Should().BeEquivalentTo(releaseEntity);
+
+        // Allow precision loss up to 100 milliseconds
+        TimeSpan tolerance = TimeSpan.FromMilliseconds(100);
+        TimeSpan difference = (releaseEntity.Created - dbRecord.Created).Duration();
+        bool isWithinTolerance = difference <= tolerance;
+        isWithinTolerance.Should().BeTrue();
     }
 }
