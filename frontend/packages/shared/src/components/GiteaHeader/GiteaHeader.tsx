ee462aa52d6aa07e89d72efc25b9f5e6bce3e25c
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-04-09 13:06:47 +0200

    Display gitea menu bar when 500 error in repo status

--- frontend/packages/shared/src/components/GiteaHeader/GiteaHeader.tsx
+++ frontend/packages/shared/src/components/GiteaHeader/GiteaHeader.tsx
@@ -5,30 +5,13 @@ import { VersionControlButtons } from './VersionControlButtons';
 import { ThreeDotsMenu } from './ThreeDotsMenu';
 
 type GiteaHeaderProps = {
-  /**
-   * The name of the organisation
-   */
   org: string;
-  /**
-   * The name of the app / repository
-   */
   app: string;
-  /**
-   * Flag for if the three dots menu only should show the repository option. This is relevant for resourceadm
-   */
   menuOnlyHasRepository?: boolean;
-  /**
-   * Flag for if the component has a clone modal. This is relevant for app-development
-   */
   hasCloneModal?: boolean;
-  /**
-   * Classname for some extra styling for the right content
-   */
   rightContentClassName?: string;
-  /**
-   * Component to be shown on the left
-   */
   leftComponent?: ReactNode;
+  hasRepoError?: boolean;
 };
 
 /**
@@ -48,6 +31,7 @@ type GiteaHeaderProps = {
  * @property {boolean}[hasCloneModal] - Flag for if the component has a clone modal. This is relevant for app-development
  * @property {string}[rightContentClassName] - Classname for some extra styling for the right content
  * @property {ReactNode}[leftComponent] - Component to be shown on the left
+ * @property {boolean}[hasRepoError] - If the repository has an error
  *
  * @returns {React.ReactNode} - The rendered Gitea header component
  */
@@ -58,12 +42,13 @@ export const GiteaHeader = ({
   hasCloneModal = false,
   rightContentClassName,
   leftComponent,
+  hasRepoError,
 }: GiteaHeaderProps): React.ReactNode => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.leftContentWrapper}>{leftComponent}</div>
       <div className={`${classes.rightContentWrapper} ${rightContentClassName}`}>
-        <VersionControlButtons org={org} app={app} />
+        {!hasRepoError && <VersionControlButtons org={org} app={app} />}
         <ThreeDotsMenu
           onlyShowRepository={menuOnlyHasRepository}
           hasCloneModal={hasCloneModal}
