737b8e2f3c8a2946ca06482f093827bf27a9af22
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-04-11 16:13:27 +0200

    replacing NativeSelect with Combobox

--- frontend/dashboard/components/ServiceOwnerSelector/ServiceOwnerSelector.tsx
+++ frontend/dashboard/components/ServiceOwnerSelector/ServiceOwnerSelector.tsx
@@ -1,5 +1,5 @@
-import React, { useId } from 'react';
-import { Label, NativeSelect } from '@digdir/design-system-react';
+import React, { useId, useState } from 'react';
+import { Combobox } from '@digdir/design-system-react';
 import { useTranslation } from 'react-i18next';
 import type { Organization } from 'app-shared/types/Organization';
 import type { User } from 'app-shared/types/Repository';
@@ -30,26 +30,25 @@ export const ServiceOwnerSelector = ({
     selectableOptions.find((item) => item.value === selectedOrgOrUser)?.value ??
     selectableUser.value;
 
-  // MAKE COMBOBOX
+  const [selectedValue, setSelectedValue] = useState<string[]>([defaultValue]);
+
   return (
-    <div>
-      <Label spacing htmlFor={serviceOwnerId}>
-        {t('general.service_owner')}
-      </Label>
-      <NativeSelect
-        hideLabel
-        error={errorMessage}
-        id={serviceOwnerId}
-        name={name}
-        defaultValue={defaultValue}
-      >
-        {selectableOptions.map(({ value, label }) => (
-          <option key={value} value={value}>
-            {label}
-          </option>
-        ))}
-      </NativeSelect>
-    </div>
+    <Combobox
+      label={t('general.service_owner')}
+      error={selectedValue.length === 0 && errorMessage}
+      size='small'
+      name={name}
+      id={serviceOwnerId}
+      // Below is temporary until we get the 'initialValue' prop from Designsystemet
+      value={selectedValue}
+      onValueChange={(newValue: string[]) => setSelectedValue(newValue)}
+    >
+      {selectableOptions.map(({ value, label }) => (
+        <Combobox.Option key={value} value={value}>
+          {label}
+        </Combobox.Option>
+      ))}
+    </Combobox>
   );
 };
 
