e2618800b54babef91a4cc1a059da84d04b1ba9b
Ronny Birkeli <ronny.birkeli@gmail.com>
2024-04-12 13:57:37 +0200

    Log the exception before returning the error to the event system. (#588)
    Co-authored-by: Ronny Birkeli <ronny.birkeli@digdir.no>

--- src/Altinn.App.Api/Controllers/EventsReceiverController.cs
+++ src/Altinn.App.Api/Controllers/EventsReceiverController.cs
@@ -73,7 +73,7 @@ namespace Altinn.App.Api.Controllers
             }
             catch (Exception ex)
             {
-                _logger.LogError("Unable to process event {eventType}. An exception was raised while processing message {messageid}. Exception thrown {exceptionMessage}", cloudEvent.Type, cloudEvent.Id, ex.Message);
+                _logger.LogError(ex, "Unable to process event {eventType}. An exception was raised while processing message {messageid}. Exception thrown {exceptionMessage}", cloudEvent.Type, cloudEvent.Id, ex.Message);
                 return new StatusCodeResult(500);
             }
         }
