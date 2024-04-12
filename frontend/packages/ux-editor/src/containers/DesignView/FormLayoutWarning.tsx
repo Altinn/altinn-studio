6f817fb57a36e313a283b9a01c201ae3259f5658
lassopicasso <lars.walderhaug@digdir.no>
2024-04-12 10:33:08 +0200

    Add warning content in accordion when duplicated ids

--- frontend/packages/ux-editor/src/containers/DesignView/FormLayoutWarning.tsx
+++ frontend/packages/ux-editor/src/containers/DesignView/FormLayoutWarning.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import type { IInternalLayout } from '../../types/global';
 import { getDuplicatedIds } from '../../utils/formLayoutUtils';
-
+import { Paragraph } from '@digdir/design-system-react';
+import classes from './FormLayoutWarning.module.css';
 interface FormLayoutWarningProps {
   layout: IInternalLayout;
 }
 
 export const FormLayoutWarning = ({ layout }: FormLayoutWarningProps) => {
   const duplicatedIds = getDuplicatedIds(layout);
+  const severalDuplicatedIds = duplicatedIds.length > 1;
+
   return (
-    <div>
-      {duplicatedIds.map((id) => (
-        <div key={id}>{id}</div>
-      ))}
+    <div className={classes.warningWrapper}>
+      <Paragraph>
+        Denne IDen er brukt i flere komponenter:
+        {duplicatedIds.map((id) => (
+          <span key={id} className={classes.duplicatedId}>
+            {` ${id}${severalDuplicatedIds && ', '}`}
+          </span>
+        ))}
+      </Paragraph>
+      <Paragraph>
+        Du kan ikke publisere appen eller konfigurere komponentene før du har rettet opp feilen.
+      </Paragraph>
     </div>
   );
 };
