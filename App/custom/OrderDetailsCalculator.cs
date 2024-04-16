2212db59d54576efb3c06d7b2e7fbd37577ea8f4
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2024-04-16 08:22:30 +0200

    Don't return a order line for rows that don't have a value in field NiceClassification. Change is purely for less confusing testing of the app.

--- App/custom/OrderDetailsCalculator.cs
+++ App/custom/OrderDetailsCalculator.cs
@@ -32,6 +32,7 @@ public class OrderDetailsCalculator : IOrderDetailsCalculator
             instanceIdentifier.InstanceOwnerPartyId, new Guid(modelData.Id));
         
         List<PaymentOrderLine> paymentOrderLines = formData.GoodsAndServicesProperties.Inventory.InventoryProperties
+            .Where(x => !string.IsNullOrEmpty(x.NiceClassification))
             .Select((x, index) =>
                 new PaymentOrderLine
                 {
