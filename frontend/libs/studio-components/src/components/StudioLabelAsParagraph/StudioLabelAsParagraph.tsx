9d21f3d7f7c7df9a73033efe6708179a6408b7fc
WilliamThorenfeldt <133344438+WilliamThorenfeldt@users.noreply.github.com>
2024-04-23 09:47:59 +0200

    Adding new @studio/component - StudioLabelAsParagraph (#12684)
    * Adding new @studio/component - StudioLabelAsParagraph
    
    * removing unused import

---
+++ frontend/libs/studio-components/src/components/StudioLabelAsParagraph/StudioLabelAsParagraph.tsx
@@ -0,0 +1,16 @@
+import React, { forwardRef } from 'react';
+import { Label, type LabelProps } from '@digdir/design-system-react';
+
+type StudioLabelAsParagraphProps = Omit<LabelProps, 'asChild'>;
+
+export const StudioLabelAsParagraph = forwardRef<HTMLLabelElement, StudioLabelAsParagraphProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <Label asChild {...rest} ref={ref}>
+        <p>{children}</p>
+      </Label>
+    );
+  },
+);
+
+StudioLabelAsParagraph.displayName = 'StudioLabelAsParagraph';
