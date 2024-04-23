6daf810723be7e338f2b12917d57cf4f00ef267e
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-04-23 17:51:14 +0200

    Revert changes in scripts

--- frontend/scripts/componentSchemas/version.ts
+++ frontend/scripts/componentSchemas/version.ts
@@ -2,14 +2,14 @@ export const versionSettings = {
   v3: {
     layoutSchemaUrl: 'https://altinncdn.no/schemas/json/layout/layout.schema.v1.json',
     expressionSchemaUrl: 'https://altinncdn.no/schemas/json/layout/expression.schema.v1.json',
-    componentSchemaPath: '@altinn/ux-editor-v3/testing/schemas/json/component',
+    componentSchemaPath: '../../packages/ux-editor-v3/src/testing/schemas/json/component',
   },
   v4: {
     layoutSchemaUrl:
       'https://altinncdn.no/toolkits/altinn-app-frontend/4.0.0-rc2/schemas/json/layout/layout.schema.v1.json',
     expressionSchemaUrl:
       'https://altinncdn.no/toolkits/altinn-app-frontend/4.0.0-rc2/schemas/json/layout/expression.schema.v1.json',
-    componentSchemaPath: 'altinn/ux-editor/testing/schemas/json/component',
+    componentSchemaPath: '../../packages/ux-editor/src/testing/schemas/json/component',
   },
 };
 export const isValidVersion = (version: string) =>
