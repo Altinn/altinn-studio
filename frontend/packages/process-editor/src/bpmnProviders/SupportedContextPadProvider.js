c57c05ddd0414c00bc735f8b409082546c6fe1bb
davidovrelid.com <david@framit.no>
2024-04-23 13:50:18 +0200

    feat(Bpmnjs): Override default action when deleting a task

--- frontend/packages/process-editor/src/bpmnProviders/SupportedContextPadProvider.js
+++ frontend/packages/process-editor/src/bpmnProviders/SupportedContextPadProvider.js
@@ -4,9 +4,33 @@ class SupportedContextPadProvider {
   }
 
   getContextPadEntries() {
+    const overrideDeleteEntry = (entries) => {
+      const deleteEntry = entries['delete'];
+      entries['delete'] = {
+        ...deleteEntry,
+        action: {
+          click: function (event, element) {
+            if (element.type !== 'bpmn:Task') {
+              deleteEntry.action.click(event, element);
+              return;
+            }
+
+            const isConfirmed = confirm(
+              'Prosessteget du ønsker å slette er knyttet til en sidegruppe, som kan inneholde konfigureringer du selv har lagt til. Hvis du sletter dette prosessteget, sletter du også tilhørende sidegruppe.',
+            );
+
+            if (isConfirmed) {
+              deleteEntry.action.click(event, element);
+            }
+          },
+        },
+      };
+    };
+
     return function (entries) {
       // Should not be able to replace the entry
       delete entries['replace'];
+      overrideDeleteEntry(entries);
       return entries;
     };
   }
