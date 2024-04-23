c486a9f6a23f2666f836de5c9a2d7fb51e950c2e
Erling Hauan <erling.hauan@digdir.no>
2024-04-23 12:40:45 +0200

    Make pagination work

--- frontend/libs/studio-components/src/components/StudioTableWithPagination/StudioTableWithPagination.tsx
+++ frontend/libs/studio-components/src/components/StudioTableWithPagination/StudioTableWithPagination.tsx
@@ -1,34 +1,65 @@
-import { Table } from '@digdir/design-system-react';
-import React, { forwardRef } from 'react';
+import { Pagination, Table } from '@digdir/design-system-react';
+import React, { forwardRef, useEffect, useState } from 'react';
+import classes from './StudioTableWithPagination.module.css';
 
 type StudioTableWithPaginationProps = {
   columns: string[];
   rows: React.ReactNode[][];
   size: 'small' | 'medium' | 'large';
+  rowsPerPage: number;
 };
 
 export const StudioTableWithPagination = forwardRef<
   HTMLTableElement,
   StudioTableWithPaginationProps
->(({ columns, rows, size = 'medium' }, ref) => {
+>(({ columns, rows, size = 'medium', rowsPerPage = 3 }, ref) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentRows, setCurrentRows] = useState(rows);
+
+  let totalPages = 1;
+  if (rowsPerPage > 0) {
+    totalPages = Math.ceil(rows.length / rowsPerPage);
+  }
+
+  useEffect(() => {
+    if (rowsPerPage > 0) {
+      const startIndex = (currentPage - 1) * rowsPerPage;
+      const endIndex = startIndex + rowsPerPage;
+      setCurrentRows(rows.slice(startIndex, endIndex));
+    }
+  }, [currentPage, rowsPerPage, rows]);
+
   return (
-    <Table size={size}>
-      <Table.Head>
-        <Table.Row>
-          {columns.map((cell, i) => (
-            <Table.HeaderCell key={i}>{cell}</Table.HeaderCell>
-          ))}
-        </Table.Row>
-      </Table.Head>
-      <Table.Body>
-        {rows.map((row, i) => (
-          <Table.Row key={i}>
-            {row.map((cell, i) => (
-              <Table.Cell key={i}>{cell}</Table.Cell>
+    <div className={classes.component}>
+      <Table size={size}>
+        <Table.Head>
+          <Table.Row>
+            {columns.map((cell, i) => (
+              <Table.HeaderCell key={i}>{cell}</Table.HeaderCell>
             ))}
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+        </Table.Head>
+        <Table.Body>
+          {currentRows.map((row, i) => (
+            <Table.Row key={i}>
+              {row.map((cell, i) => (
+                <Table.Cell key={i}>{cell}</Table.Cell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      {rowsPerPage > 0 && (
+        <Pagination
+          size={size}
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onChange={setCurrentPage}
+          nextLabel='Neste'
+          previousLabel='Forrige'
+          itemLabel={(num) => `Side ${num}`}
+        />
+      )}
+    </div>
   );
 });
