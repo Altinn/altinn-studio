8f3eea87227e7fd4a180f5b8011ec6623ffc712c
David Ovrelid <46874830+framitdavid@users.noreply.github.com>
2024-04-24 12:49:03 +0200

    feat(Bpmnjs): Ensure that the user is fully informed about the consequences when deleting a task (#12702)
    * feat(Bpmnjs): Override default action when deleting a task

--- frontend/packages/process-editor/src/bpmnProviders/SupportedContextPadProvider.js
+++ frontend/packages/process-editor/src/bpmnProviders/SupportedContextPadProvider.js
@@ -4,9 +4,33 @@ class SupportedContextPadProvider {
   }
 
   getContextPadEntries() {
+    const overrideDeleteEntry = (entries) => {
+      const deleteEntry = entries['delete'];
+      entries['delete'] = {
+        ...deleteEntry,
+        action: {
+          click: function (event, element) {
+            if (element.type !== 'bpmn:Task') {
+              deleteEntry.action.click(event, element);
+              return;
+            }
+
+            const isConfirmed = confirm(
+              'Prosess-steget du vil slette kan være knyttet til en sidegruppe. Den kan inneholde visningsoppsett eller skjema du har satt opp. Hvis du sletter steget, sletter du også hele sidegruppen og alt som hører til.',
+            );
+
+            if (isConfirmed) {
+              deleteEntry.action.click(event, element);
+            }
+          },
+        },
+      };
+    };
+
     return function (entries) {
       // Should not be able to replace the entry
       delete entries['replace'];
+      overrideDeleteEntry(entries);
       return entries;
     };
   }
