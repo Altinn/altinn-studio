ab44e575b5a5d787981e8f6d604d356f3907aa0a
davidovrelid.com <david@framit.no>
2024-04-26 10:40:48 +0200

    small tips and tricks

--- frontend/libs/studio-components/src/components/StudioTableWithPagination/StudioTableWithPagination.tsx
+++ frontend/libs/studio-components/src/components/StudioTableWithPagination/StudioTableWithPagination.tsx
@@ -1,84 +1,89 @@
 import { Pagination, Table } from '@digdir/design-system-react';
 import React, { forwardRef, useState } from 'react';
 import classes from './StudioTableWithPagination.module.css';
-import { calcRowsToRender } from './utils';
+import { getRowsToRender } from './utils';
 import { useSortedRows } from '../../hooks/useSortedRows';
 import { SelectRowsPerPage } from './SelectRowsPerPage';
 
+export type Rows = Record<string, React.ReactNode>[];
+
+type PaginationTranslation = {
+  nextButtonText: string;
+  previousButtonText: string;
+  itemLabel: (num: number) => string;
+};
+
 type StudioTableWithPaginationProps = {
   columns: Record<'accessor' | 'value', string>[];
-  rows: Record<string, React.ReactNode>[];
+  rows: Rows;
   isSortable?: boolean;
   size?: 'small' | 'medium' | 'large';
-  initialRowsPerPage?: number;
+  pagination: {
+    initialRowsPerPage?: number;
+    paginationTranslation: PaginationTranslation;
+  };
 };
 
 export const StudioTableWithPagination = forwardRef<
   HTMLTableElement,
   StudioTableWithPaginationProps
->(
-  (
-    { columns, rows, isSortable = true, size = 'medium', initialRowsPerPage = 5 },
-    ref,
-  ): React.ReactElement => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [rowsPerPage, setRowsPerPage] = useState(initialRowsPerPage);
-    const { sortedRows, handleSorting } = useSortedRows(rows);
+>(({ columns, rows, isSortable = true, size = 'medium', pagination }, ref): React.ReactElement => {
+  const { initialRowsPerPage = 5 } = pagination;
+  const { nextButtonText, previousButtonText, itemLabel } = pagination.paginationTranslation;
 
-    const totalPages = Math.ceil(sortedRows.length / rowsPerPage);
-    const rowsToRender = calcRowsToRender(currentPage, rowsPerPage, sortedRows);
-    if (rowsToRender.length === 0) setCurrentPage(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(initialRowsPerPage);
+  const { sortedRows, handleSorting } = useSortedRows(rows);
 
-    const columnHasValue = (value) => {
-      return Boolean(value);
-    };
+  const totalPages = Math.ceil(sortedRows.length / rowsPerPage);
+  const rowsToRender = getRowsToRender(currentPage, rowsPerPage, sortedRows);
+  if (rowsToRender.length === 0) setCurrentPage(1);
 
-    return (
-      <>
-        <Table size={size} className={classes.table} ref={ref}>
-          <Table.Head>
-            <Table.Row>
-              {columns.map(({ accessor, value }) => (
-                <Table.HeaderCell
-                  key={accessor}
-                  sortable={isSortable && columnHasValue(value)}
-                  onSortClick={() => handleSorting(accessor)}
-                >
-                  {value}
-                </Table.HeaderCell>
+  return (
+    <>
+      <Table size={size} className={classes.table} ref={ref}>
+        <Table.Head>
+          <Table.Row>
+            {columns.map(({ accessor, value }) => (
+              <Table.HeaderCell
+                key={accessor}
+                sortable={isSortable && !!value}
+                onSortClick={() => handleSorting(accessor)}
+              >
+                {value}
+              </Table.HeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {rowsToRender.map((row) => (
+            <Table.Row key={String(row.id)}>
+              {columns.map(({ accessor }) => (
+                <Table.Cell key={accessor}>{row[accessor]}</Table.Cell>
               ))}
             </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            {rowsToRender.map((row) => (
-              <Table.Row key={String(row.id)}>
-                {columns.map(({ accessor }) => (
-                  <Table.Cell key={accessor}>{row[accessor]}</Table.Cell>
-                ))}
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-        {initialRowsPerPage > 0 && (
-          <div className={classes.paginationContainer}>
-            <SelectRowsPerPage setRowPerPage={setRowsPerPage} size={size} />
-            {totalPages > 1 && (
-              <Pagination
-                className={classes.pagination}
-                size={size}
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onChange={setCurrentPage}
-                nextLabel='Neste'
-                previousLabel='Forrige'
-                itemLabel={(num) => `Side ${num}`}
-                hideLabels
-                compact
-              />
-            )}
-          </div>
-        )}
-      </>
-    );
-  },
-);
+          ))}
+        </Table.Body>
+      </Table>
+      {initialRowsPerPage > 0 && (
+        <div className={classes.paginationContainer}>
+          <SelectRowsPerPage setRowPerPage={setRowsPerPage} size={size} />
+          {totalPages > 1 && (
+            <Pagination
+              className={classes.pagination}
+              size={size}
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onChange={setCurrentPage}
+              nextLabel={nextButtonText}
+              previousLabel={previousButtonText}
+              itemLabel={itemLabel}
+              hideLabels
+              compact
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+});
