57b4926cf91e09a19104f65289406cea25c04f9b
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-05-08 19:50:24 +0200

    Refactor ConfigEndEvent

--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigEndEvent/ConfigEndEvent.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigEndEvent/ConfigEndEvent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StudioLabelAsParagraph, StudioSectionHeader } from '@studio/components';
-import { Link, Paragraph } from '@digdir/design-system-react';
+import { Link, Paragraph, Accordion } from '@digdir/design-system-react';
 import { useTranslation } from 'react-i18next';
 import classes from './ConfigEndEvent.module.css';
 import { ConfigIcon } from '../ConfigContent/ConfigIcon';
@@ -18,45 +18,45 @@ export const ConfigEndEvent = () => {
           level: 2,
         }}
         helpText={{
-          // TODO - SKAL HJELPETEXT VÆRE DER?
           text: t('process_editor.configuration_panel_header_help_text_custom_receipt'),
           title: t('process_editor.configuration_panel_header_help_text_title'),
         }}
       />
-      {/*<Accordion color='subtle'>
+      <Accordion color='subtle'>
         <Accordion.Item>
-          <Accordion.Header>Kvittering</Accordion.Header>
-      <Accordion.Content>*/}
-      <div className={classes.container}>
-        <div className={classes.section}>
-          <StudioLabelAsParagraph size='small' spacing>
-            Standardkvittering
-          </StudioLabelAsParagraph>
-          <Paragraph size='small' className={classes.paragraph}>
-            Det er automatisk satt opp en standardkvittering i appen.
-          </Paragraph>
-          <Link
-            href='https://docs.altinn.studio/app/development/configuration/process/customize/#receipt'
-            rel='noopener noreferrer'
-            size='small'
-          >
-            Les mer om standardkvittering i dokumentasjonen
-          </Link>
-        </div>
-        <div className={classes.section}>
-          <StudioLabelAsParagraph size='small' spacing>
-            Opprett din egen kvittering
-          </StudioLabelAsParagraph>
-          <Paragraph size='small'>
-            Hvis du heller vil lage din egen kvittering, kan du opprette den her. Kvitteringen du
-            lager selv vil overstyre standardkvitteringen.
-          </Paragraph>
-          <CustomReceiptContent />
-        </div>
-      </div>
-      {/*</Accordion.Content>
+          <Accordion.Header>
+            {t('process_editor.configuration_panel_custom_receipt_accordion_header')}
+          </Accordion.Header>
+          <Accordion.Content>
+            <div className={classes.container}>
+              <div>
+                <StudioLabelAsParagraph size='small' spacing>
+                  {t('process_editor.configuration_panel_custom_receipt_default_receipt_heading')}
+                </StudioLabelAsParagraph>
+                <Paragraph size='small' className={classes.paragraph}>
+                  {t('process_editor.configuration_panel_custom_receipt_default_receipt_info')}
+                </Paragraph>
+                <Link
+                  href='https://docs.altinn.studio/app/development/configuration/process/customize/#receipt'
+                  rel='noopener noreferrer'
+                  size='small'
+                >
+                  {t('process_editor.configuration_panel_custom_receipt_default_receipt_link')}
+                </Link>
+              </div>
+              <div>
+                <StudioLabelAsParagraph size='small' spacing>
+                  {t('process_editor.configuration_panel_custom_receipt_heading')}
+                </StudioLabelAsParagraph>
+                <Paragraph size='small'>
+                  {t('process_editor.configuration_panel_custom_receipt_info')}
+                </Paragraph>
+                <CustomReceiptContent />
+              </div>
+            </div>
+          </Accordion.Content>
         </Accordion.Item>
-              </Accordion>*/}
+      </Accordion>
     </>
   );
 };
