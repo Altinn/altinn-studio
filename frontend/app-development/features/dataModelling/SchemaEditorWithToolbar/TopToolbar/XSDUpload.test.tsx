b51a598481b93b10ef56ab9da05735b827e47895
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-05-14 18:39:47 +0200

    Fix unit tests for XSDUpload and useUploadDatamodelMutation

--- frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/XSDUpload.test.tsx
+++ frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/XSDUpload.test.tsx
@@ -7,15 +7,10 @@ import type { QueryClient } from '@tanstack/react-query';
 import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
 import * as testids from '../../../../../testing/testids';
 import { renderWithMockStore } from '../../../../test/mocks';
-import { QueryKey } from 'app-shared/types/QueryKey';
 import type { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
 import { createApiErrorMock } from 'app-shared/mocks/apiErrorMock';
 
 const user = userEvent.setup();
-
-// Test data:
-const org = 'org';
-const app = 'app';
 
 jest.mock('../../../../hooks/mutations/useUploadDatamodelMutation', () => ({
   __esModule: true,
@@ -43,7 +38,7 @@ const render = ({
 describe('XSDUpload', () => {
   afterEach(jest.restoreAllMocks);
 
-  it('should show a spinner when uploading', async () => {
+  it('shows a spinner when uploading', async () => {
     useUploadDatamodelMutationSpy.mockReturnValue({ isPending: true });
 
     render();
@@ -51,7 +46,7 @@ describe('XSDUpload', () => {
     expect(screen.getByText(textMock('app_data_modelling.uploading_xsd'))).toBeInTheDocument();
   });
 
-  it('should show file picker button', () => {
+  it('shows file picker button', () => {
     render();
 
     const button = screen.getByRole('button', { name: textMock('app_data_modelling.upload_xsd') });
@@ -61,7 +56,7 @@ describe('XSDUpload', () => {
     expect(fileInput).toBeInTheDocument();
   });
 
-  it('should show error text when file upload results in error', async () => {
+  it('uploads a file', async () => {
     const errorCode = 'ModelWithTheSameTypeNameExists';
     const file = new File(['hello'], 'hello.xsd', { type: 'text/xml' });
     render({
@@ -82,12 +77,17 @@ describe('XSDUpload', () => {
     expect(await screen.findByRole('alert')).toHaveTextContent(textMock(`api_errors.${errorCode}`));
   });
 
-  it('Invalidates metadata queries when upload is successful', async () => {
-    const filename = 'hello';
-    const file = new File([filename], `${filename}.xsd`, { type: 'text/xml' });
-    const queryClient = createQueryClientMock();
-    const invalidateQueriesSpy = jest.spyOn(queryClient, 'invalidateQueries');
-    render({ queryClient });
+  it('shows error text when file upload results in error', async () => {
+    const errorCode = 'ModelWithTheSameTypeNameExists';
+    const file = new File(['hello'], 'hello.xsd', { type: 'text/xml' });
+    render({
+      queries: {
+        uploadDatamodel: jest
+          .fn()
+          .mockImplementation(() => Promise.reject(createApiErrorMock(400, errorCode))),
+      },
+      queryClient: null,
+    });
 
     await clickUploadButton();
 
@@ -95,12 +95,6 @@ describe('XSDUpload', () => {
 
     await user.upload(fileInput, file);
 
-    expect(invalidateQueriesSpy).toHaveBeenCalledTimes(2);
-    expect(invalidateQueriesSpy).toHaveBeenCalledWith({
-      queryKey: [QueryKey.DatamodelsJson, org, app],
-    });
-    expect(invalidateQueriesSpy).toHaveBeenCalledWith({
-      queryKey: [QueryKey.DatamodelsXsd, org, app],
-    });
+    expect(await screen.findByRole('alert')).toHaveTextContent(textMock(`api_errors.${errorCode}`));
   });
 });
