ecdcc5018f443ba2f804e7f2dcd5e71be4f60176
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-05-23 13:43:20 +0200

    Fixing navigation to lage page

--- frontend/app-development/hooks/mutations/useAddLayoutSetMutation.ts
+++ frontend/app-development/hooks/mutations/useAddLayoutSetMutation.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useServicesContext } from 'app-shared/contexts/ServicesContext';
 import { QueryKey } from 'app-shared/types/QueryKey';
 import type { LayoutSetConfig, LayoutSets } from 'app-shared/types/api/LayoutSetsResponse';
+import { useLocalStorage } from 'app-shared/hooks/useLocalStorage';
 import type { LayoutSetsResponse } from 'app-shared/types/api/AddLayoutSetResponse';
 
 const isLayoutSets = (obj: LayoutSetsResponse): obj is LayoutSets => {
@@ -12,6 +13,7 @@ const isLayoutSets = (obj: LayoutSetsResponse): obj is LayoutSets => {
 export const useAddLayoutSetMutation = (org: string, app: string) => {
   const { addLayoutSet } = useServicesContext();
   const queryClient = useQueryClient();
+  const [_, setSelectedLayoutSet] = useLocalStorage<string>('layoutSet/' + app, null);
 
   return useMutation({
     mutationFn: ({
@@ -26,6 +28,7 @@ export const useAddLayoutSetMutation = (org: string, app: string) => {
         layoutSetConfig,
       })),
     onSuccess: ({ layoutSets, layoutSetConfig }) => {
+      setSelectedLayoutSet(layoutSetConfig.id);
       // Need this check since endpoint might return 200 OK, but with info details
       // when process-editor renders the tasks and 'adds' them on first mount, when they already exists.
       if (isLayoutSets(layoutSets)) {
--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigEndEvent/CustomReceiptContent/RedirectToCreatePageButton/RedirectToCreatePageButton.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigEndEvent/CustomReceiptContent/RedirectToCreatePageButton/RedirectToCreatePageButton.tsx
@@ -4,12 +4,21 @@ import { useStudioUrlParams } from 'app-shared/hooks/useStudioUrlParams';
 import { PackagesRouter } from 'app-shared/navigation/PackagesRouter';
 import { PencilWritingIcon } from '@studio/icons';
 import { StudioButton, StudioLabelAsParagraph } from '@studio/components';
+import { useLocalStorage } from 'app-shared/hooks/useLocalStorage';
 import { useTranslation } from 'react-i18next';
+import { useBpmnApiContext } from '@altinn/process-editor/contexts/BpmnApiContext';
 
 export const RedirectToCreatePageButton = (): React.ReactElement => {
   const { t } = useTranslation();
   const { org, app } = useStudioUrlParams();
   const packagesRouter = new PackagesRouter({ org, app });
+  const { existingCustomReceiptLayoutSetId } = useBpmnApiContext();
+
+  const [, setSelectedLayoutSet] = useLocalStorage<string>('layoutSet/' + app, null);
+
+  const handleClick = () => {
+    setSelectedLayoutSet(existingCustomReceiptLayoutSetId);
+  };
 
   return (
     <div className={classes.goToCreatePageWrapper}>
@@ -24,6 +33,7 @@ export const RedirectToCreatePageButton = (): React.ReactElement => {
           color='second'
           icon={<PencilWritingIcon />}
           href={packagesRouter.getPackageNavigationUrl('editorUiEditor')}
+          onClick={handleClick}
           className={classes.goToCreateButton}
         >
           {t('process_editor.configuration_panel_custom_receipt_navigate_to_lage_button')}
