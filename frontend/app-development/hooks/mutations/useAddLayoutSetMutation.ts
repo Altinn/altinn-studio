171fe72b7bb038ee9a6b5c6484142ef2d7bf6dbd
WilliamThorenfeldt <william.thorenfeldt@accenture.com>
2024-05-23 13:35:51 +0200

    removing local storage layout set

--- frontend/app-development/hooks/mutations/useAddLayoutSetMutation.ts
+++ frontend/app-development/hooks/mutations/useAddLayoutSetMutation.ts
@@ -2,7 +2,6 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useServicesContext } from 'app-shared/contexts/ServicesContext';
 import { QueryKey } from 'app-shared/types/QueryKey';
 import type { LayoutSetConfig, LayoutSets } from 'app-shared/types/api/LayoutSetsResponse';
-import { useLocalStorage } from 'app-shared/hooks/useLocalStorage';
 import type { LayoutSetsResponse } from 'app-shared/types/api/AddLayoutSetResponse';
 
 const isLayoutSets = (obj: LayoutSetsResponse): obj is LayoutSets => {
@@ -13,7 +12,6 @@ const isLayoutSets = (obj: LayoutSetsResponse): obj is LayoutSets => {
 export const useAddLayoutSetMutation = (org: string, app: string) => {
   const { addLayoutSet } = useServicesContext();
   const queryClient = useQueryClient();
-  const [_, setSelectedLayoutSet] = useLocalStorage<string>('layoutSet/' + app, null);
 
   return useMutation({
     mutationFn: ({
@@ -28,7 +26,6 @@ export const useAddLayoutSetMutation = (org: string, app: string) => {
         layoutSetConfig,
       })),
     onSuccess: ({ layoutSets, layoutSetConfig }) => {
-      setSelectedLayoutSet(layoutSetConfig.id);
       // Need this check since endpoint might return 200 OK, but with info details
       // when process-editor renders the tasks and 'adds' them on first mount, when they already exists.
       if (isLayoutSets(layoutSets)) {
