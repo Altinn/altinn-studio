3f012407f4da812adccd1f3e529ea714cbe918df
JamalAlabdullah <90609090+JamalAlabdullah@users.noreply.github.com>
2024-05-28 11:18:55 +0200

    Feat/12496 make it possible to open two different layout sets in two different windows (#12857)
    * make it possible to open two different layout sets in two different windows

--- frontend/packages/ux-editor/src/hooks/useSelectedFormLayoutSetName.test.tsx
+++ frontend/packages/ux-editor/src/hooks/useSelectedFormLayoutSetName.test.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 import React from 'react';
 import { useSelectedFormLayoutSetName } from './';
 import { layoutSet1NameMock, layoutSetsMock } from '../testing/layoutMock';
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import type { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
 import { ServicesContextProvider } from 'app-shared/contexts/ServicesContext';
 import { queriesMock } from 'app-shared/mocks/queriesMock';
@@ -11,6 +11,7 @@ import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryKey } from 'app-shared/types/QueryKey';
 import { app, org } from '@studio/testing/testids';
+import { typedLocalStorage } from 'app-shared/utils/webStorage';
 
 // Test data:
 const selectedLayoutSet = layoutSet1NameMock;
@@ -56,5 +57,41 @@ describe('useSelectedFormLayoutSetName', () => {
     });
 
     expect(result.current.selectedFormLayoutSetName).toEqual(selectedLayoutSet);
+  });
+
+  it('Should initialize state with local storage value', async () => {
+    const storageKey = 'selectedFormLayoutSetName';
+    typedLocalStorage.setItem(storageKey, selectedLayoutSet);
+
+    const client = createQueryClientMock();
+    client.setQueryData([QueryKey.LayoutSets, org, app], layoutSetsMock);
+
+    const { result } = renderHook(() => useSelectedFormLayoutSetName(), {
+      wrapper: ({ children }) => {
+        return wrapper({ children, client });
+      },
+    });
+
+    expect(result.current.selectedFormLayoutSetName).toEqual(selectedLayoutSet);
+  });
+
+  it('Should update local storage based on the state', async () => {
+    const storageKey = 'selectedFormLayoutSetName';
+    const newLayoutSetName = 'newLayoutSet';
+
+    const client = createQueryClientMock();
+    client.setQueryData([QueryKey.LayoutSets, org, app], layoutSetsMock);
+
+    const { result } = renderHook(() => useSelectedFormLayoutSetName(), {
+      wrapper: ({ children }) => {
+        return wrapper({ children, client });
+      },
+    });
+
+    await waitFor(() => {
+      result.current.setSelectedFormLayoutSetName(newLayoutSetName);
+    });
+
+    expect(typedLocalStorage.getItem(storageKey)).toEqual(newLayoutSetName);
   });
 });
