0ac8e6faade6ee4fff53593756d76930002e5f07
Mirko Sekulic <misha.sekulic@gmail.com>
2024-06-11 11:33:22 +0200

    optimize imports

--- eidlogger/src/main/java/no/altinn/eidlogger/controller/EidController.java
+++ eidlogger/src/main/java/no/altinn/eidlogger/controller/EidController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 public class EidController {
   private final EventLogger eventLogger;
