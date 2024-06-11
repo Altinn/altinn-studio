d9fc4685f532b1ba9b78d72dee8f6455f44e4320
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-06-11 12:11:44 +0200

    Move redirection logic from PageLayout component to guard hook
    Reducing clutter in PageLayout component and splitting up redirection
    logic flow to be easier to read. Ref. comment by David on #12943
    
    Co-authored-by: David Ovrelid <46874830+framitdavid@users.noreply.github.com>

--- frontend/dashboard/pages/PageLayout/PageLayout.tsx
+++ frontend/dashboard/pages/PageLayout/PageLayout.tsx
@@ -1,38 +1,19 @@
-import AppHeader, {
-  HeaderContext,
-  SelectedContextType,
-} from 'app-shared/navigation/main-header/Header';
-import { Outlet, useNavigate } from 'react-router-dom';
-import type { NavigateFunction } from 'react-router-dom';
+import AppHeader, { HeaderContext } from 'app-shared/navigation/main-header/Header';
+import { Outlet } from 'react-router-dom';
 import { useOrganizationsQuery } from 'dashboard/hooks/queries';
 import { useUserQuery } from 'app-shared/hooks/queries';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import type { IHeaderContext } from 'app-shared/navigation/main-header/Header';
 import { useTranslation } from 'react-i18next';
 
-import { useSelectedContext } from 'dashboard/hooks/useSelectedContext';
-import { typedSessionStorage } from 'app-shared/utils/webStorage';
 import { StudioPageSpinner } from '@studio/components';
-import type { Organization } from 'app-shared/types/Organization';
-import { userHasAccessToSelectedContext } from 'dashboard/utils/userUtils';
+import { useContextRedirectionGuard } from 'dashboard/hooks/guards/useContextRedirectionGuard';
 
 export const PageLayout = () => {
   const { t } = useTranslation();
   const { data: user } = useUserQuery();
   const { data: organizations } = useOrganizationsQuery();
-
-  const selectedContext = useSelectedContext();
-  const navigate = useNavigate();
-  const [isReady, setIsReady] = useState<boolean>(false);
-
-  if (selectedContext !== SelectedContextType.None) {
-    typedSessionStorage.setItem('dashboard::selectedContext', selectedContext);
-  }
-
-  useEffect(() => {
-    handleRedirection(selectedContext, organizations, navigate);
-    setIsReady(true);
-  }, [selectedContext, organizations, navigate]);
+  const { isRedirectionComplete } = useContextRedirectionGuard(organizations);
 
   const headerContextValue: IHeaderContext = useMemo(
     () => ({
@@ -42,7 +23,7 @@ export const PageLayout = () => {
     [organizations, user],
   );
 
-  if (!isReady) return <StudioPageSpinner spinnerTitle={t('dashboard.loading')} />;
+  if (!isRedirectionComplete) return <StudioPageSpinner spinnerTitle={t('dashboard.loading')} />;
   return (
     <>
       <HeaderContext.Provider value={headerContextValue}>
@@ -52,27 +33,3 @@ export const PageLayout = () => {
     </>
   );
 };
-
-function handleRedirection(
-  selectedContext: string,
-  organizations: Organization[],
-  navigate: NavigateFunction,
-) {
-  let navigateToContext = selectedContext;
-
-  if (selectedContext === SelectedContextType.None) {
-    navigateToContext =
-      typedSessionStorage.getItem('dashboard::selectedContext') || SelectedContextType.Self;
-  }
-
-  if (
-    organizations &&
-    userHasAccessToSelectedContext({ selectedContext: navigateToContext, orgs: organizations })
-  ) {
-    if (navigateToContext !== selectedContext) {
-      navigate(navigateToContext + location.search, { replace: true });
-    }
-  } else {
-    navigate(SelectedContextType.Self, { replace: true });
-  }
-}
