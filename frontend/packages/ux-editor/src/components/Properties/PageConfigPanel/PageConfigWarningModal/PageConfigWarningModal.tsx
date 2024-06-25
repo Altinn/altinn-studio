946754a4e5b0af02f5e91978bbba170e230a467b
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2024-06-25 15:30:26 +0200

    updated design for warning modal --WIP

--- frontend/packages/ux-editor/src/components/Properties/PageConfigPanel/PageConfigWarningModal/PageConfigWarningModal.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PageConfigPanel/PageConfigWarningModal/PageConfigWarningModal.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import React from 'react';
 import classes from './PageConfigWarningModal.module.css';
-import { Button, Modal } from '@digdir/design-system-react';
+import { Modal } from '@digdir/design-system-react';
 import { useTranslation } from 'react-i18next';
 
 export interface PageConfigWarningModalProps {
@@ -11,21 +11,16 @@ export const PageConfigWarningModal = ({ modalRef }: PageConfigWarningModalProps
   const { t } = useTranslation();
   return (
     <Modal ref={modalRef} role='dialog'>
-      <Modal.Header closeButton={false}>
+      <Modal.Header closeButton={true}>
         {t('ux_editor.modal_properties_warning_modal_title')}
       </Modal.Header>
       <Modal.Content>
-        {t('ux_editor.modal_properties_warning_modal_sub_title')}
-        <div className={classes.instructiveTitle}>
-          {t('ux_editor.modal_properties_warning_modal_instructive_text_title')}
+        <div className={classes.subTitle}>
+          {t('ux_editor.modal_properties_warning_modal_sub_title')}
         </div>
         {t('ux_editor.modal_properties_warning_modal_instructive_text_body')}
       </Modal.Content>
-      <Modal.Footer>
-        <Button variant='primary' onClick={() => modalRef.current?.close()}>
-          {t('ux_editor.modal_properties_warning_modal_close_button')}
-        </Button>
-      </Modal.Footer>
+      <Modal.Footer />
     </Modal>
   );
 };
