c4fdb52ef1890313b8175d684a4b345f73037ef3
davidovrelid.com <david@framit.no>
2024-07-02 09:42:59 +0200

    use smaller min-height on small buttons

--- frontend/libs/studio-components/src/components/StudioButton/StudioButton.tsx
+++ frontend/libs/studio-components/src/components/StudioButton/StudioButton.tsx
@@ -27,6 +27,7 @@ const StudioButton: OverridableComponent<StudioButtonProps, HTMLButtonElement> =
   // Information can be found here: https://www.designsystemet.no/bloggen/2024/v1rc1#fargemodus
   const classNames = cn(givenClassName, classes.studioButton, {
     [classes.inverted]: color === 'inverted',
+    [classes.small]: rest.size === 'small',
   });
   const selectedColor = color === 'inverted' ? undefined : color;
 
