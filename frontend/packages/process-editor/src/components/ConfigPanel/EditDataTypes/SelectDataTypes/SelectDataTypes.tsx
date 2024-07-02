ed013e52e6fa62d865adcbe2e54e962a1aa949dd
andreastanderen <andreastanderen@gmail.com>
2024-07-02 13:44:27 +0200

    Add new text key for data model select label

--- frontend/packages/process-editor/src/components/ConfigPanel/EditDataTypes/SelectDataTypes/SelectDataTypes.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/EditDataTypes/SelectDataTypes/SelectDataTypes.tsx
@@ -45,7 +45,7 @@ export const SelectDataTypes = ({
   return (
     <div className={classes.dataTypeSelectAndButtons}>
       <Combobox
-        label={t('process_editor.configuration_panel_set_data_model')}
+        label={t('process_editor.configuration_panel_set_data_model_label')}
         value={currentValue && dataModelIds.includes(existingDataType) ? currentValue : undefined}
         description={t('process_editor.configuration_panel_data_model_selection_description')}
         size='small'
