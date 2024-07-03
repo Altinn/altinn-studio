f0532fcc6cb212d7f69b832892a0c73d30a2848d
Magnusrm <Magnusrm@noreply.altinn.studio>
2024-07-03 12:50:43 +0000

    add repeating group datafield

--- App/models/model.cs
+++ App/models/model.cs
@@ -82,6 +82,11 @@ namespace Altinn.App.Models.model
     [JsonPropertyName("multipleSelect")]
     public string multipleSelect { get; set; }
 
+    [XmlElement("repeatingGroup", Order = 15)]
+    [JsonProperty("repeatingGroup")]
+    [JsonPropertyName("repeatingGroup")]
+    public List<RepeatingGroupExample> repeatingGroup { get; set; }
+
   }
 
   public class GridExample
@@ -151,6 +156,37 @@ namespace Altinn.App.Models.model
     [JsonProperty("ExampleCheckboxThree")]
     [JsonPropertyName("ExampleCheckboxThree")]
     public string ExampleCheckboxThree { get; set; }
+
+  }
+
+  public class RepeatingGroupExample
+  {
+    [XmlAttribute("altinnRowId")]
+    [JsonPropertyName("altinnRowId")]
+    [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    [Newtonsoft.Json.JsonIgnore]
+    public Guid AltinnRowId { get; set; }
+
+    public bool ShouldSerializeAltinnRowId() => AltinnRowId != default;
+
+    [XmlElement("name", Order = 1)]
+    [JsonProperty("name")]
+    [JsonPropertyName("name")]
+    public string name { get; set; }
+
+    [Range(Double.MinValue,Double.MaxValue)]
+    [XmlElement("points", Order = 2)]
+    [JsonProperty("points")]
+    [JsonPropertyName("points")]
+    public decimal? points { get; set; }
+
+    public bool ShouldSerializepoints() => points.HasValue;
+
+    [RegularExpression(@"^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")]
+    [XmlElement("date", Order = 3)]
+    [JsonProperty("date")]
+    [JsonPropertyName("date")]
+    public string date { get; set; }
 
   }
 }
