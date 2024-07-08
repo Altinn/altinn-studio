bd3d243e04aeca730935f0933f2cf838bd6311fb
Mirko Sekulic <misha.sekulic@gmail.com>
2024-07-08 13:45:40 +0200

    disable calls to session controller from frontend

--- frontend/app-development/sharedResources/user/userSagas.ts
+++ frontend/app-development/sharedResources/user/userSagas.ts
@@ -2,14 +2,14 @@ import type { SagaIterator } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 import { watchFetchDeployPermissionsSaga } from './permissions/permissionsSaga';
 import {
-  watchFetchRemainingSessionSaga,
-  watchKeepAliveSaga,
+  // watchFetchRemainingSessionSaga,
+  // watchKeepAliveSaga,
   watchSignOutUserSaga,
 } from './session/sessionSagas';
 
 export default function* userSagas(): SagaIterator {
   yield fork(watchFetchDeployPermissionsSaga);
-  yield fork(watchFetchRemainingSessionSaga);
-  yield fork(watchKeepAliveSaga);
+  // yield fork(watchFetchRemainingSessionSaga);
+  // yield fork(watchKeepAliveSaga);
   yield fork(watchSignOutUserSaga);
 }
