9b4b03913d3b91adc99e6825615c688c3c417bda
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-07-09 11:31:51 +0200

    Change `TextResourceEditor` LegacySelect to StudioNativeSelect

--- frontend/packages/ux-editor/src/components/TextResource/TextResourceEditor/TextResourcePicker/TextResourcePicker.tsx
+++ frontend/packages/ux-editor/src/components/TextResource/TextResourceEditor/TextResourcePicker/TextResourcePicker.tsx
@@ -1,16 +1,10 @@
-import type { LegacySingleSelectOption } from '@digdir/design-system-react';
-import { LegacySelect } from '@digdir/design-system-react';
 import React from 'react';
-import { prepend } from 'app-shared/utils/arrayUtils';
 import type { ITextResource } from 'app-shared/types/global';
 import { useTextResourcesSelector } from '../../../../hooks';
-import {
-  allTextResourceIdsWithTextSelector,
-  textResourceByLanguageAndIdSelector,
-} from '../../../../selectors/textResourceSelectors';
+import { allTextResourceIdsWithTextSelector } from '../../../../selectors/textResourceSelectors';
 import { DEFAULT_LANGUAGE } from 'app-shared/constants';
 import { useTranslation } from 'react-i18next';
-import { TextResourceOption } from './TextResourceOption';
+import { StudioNativeSelect } from '@studio/components';
 
 export type TextResourcePickerProps = {
   textResourceId?: string;
@@ -22,30 +16,26 @@ export const TextResourcePicker = ({
   onReferenceChange,
 }: TextResourcePickerProps) => {
   const { t } = useTranslation();
-  const textResource: ITextResource = useTextResourcesSelector<ITextResource>(
-    textResourceByLanguageAndIdSelector(DEFAULT_LANGUAGE, textResourceId),
-  );
   const textResources: ITextResource[] = useTextResourcesSelector<ITextResource[]>(
     allTextResourceIdsWithTextSelector(DEFAULT_LANGUAGE),
   );
 
-  const searchOptions: LegacySingleSelectOption[] = prepend<LegacySingleSelectOption>(
-    textResources.map((tr) => ({
-      label: tr.id,
-      value: tr.id,
-      formattedLabel: <TextResourceOption textResource={tr} />,
-      keywords: [tr.id, tr.value],
-    })),
-    { label: t('ux_editor.search_text_resources_none'), value: '' },
-  );
-
   return (
-    <LegacySelect
-      hideLabel={true}
+    <StudioNativeSelect
       label={t('ux_editor.search_text_resources_label')}
-      onChange={(id) => onReferenceChange(id === '' ? undefined : id)}
-      options={searchOptions}
-      value={textResource?.id ?? ''}
-    />
+      onChange={(event) => {
+        console.log('event.target.value', event.target.value);
+        onReferenceChange(event.target.value === '' ? undefined : event.target.value);
+      }}
+      value={textResourceId}
+      size='sm'
+    >
+      <option value=''>{t('ux_editor.search_text_resources_none')}</option>
+      {textResources.map((option) => (
+        <option title={option.value} key={option.id} value={option.id}>
+          {option.id}
+        </option>
+      ))}
+    </StudioNativeSelect>
   );
 };
