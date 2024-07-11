540ebb6d7eb5207a10fbed40a2595744d65b7a0d
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-07-11 14:05:25 +0200

    Add tests for combobox enum with multiple selection
    Remove unused code
    
    Fix accidental deletion of test value

--- frontend/packages/ux-editor/src/components/config/editModal/EditStringValue.test.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditStringValue.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 
 import { EditStringValue } from './EditStringValue';
 import { renderWithProviders } from '../../../testing/mocks';
@@ -9,11 +9,18 @@ import userEvent from '@testing-library/user-event';
 
 const user = userEvent.setup();
 
-const renderEditStringValue = ({ maxLength = undefined, handleComponentChange = jest.fn() } = {}) =>
+const renderEditStringValue = ({
+  multiple = false,
+  enumValues = null,
+  maxLength = undefined,
+  handleComponentChange = jest.fn(),
+} = {}) =>
   renderWithProviders(
     <EditStringValue
       handleComponentChange={handleComponentChange}
       propertyKey='maxLength'
+      multiple={multiple}
+      enumValues={enumValues}
       component={{
         id: 'c24d0812-0c34-4582-8f31-ff4ce9795e96',
         type: ComponentType.Input,
@@ -43,12 +50,73 @@ describe('EditStringValue', () => {
     expect(handleComponentChange).toHaveBeenCalledWith({
       id: 'c24d0812-0c34-4582-8f31-ff4ce9795e96',
       type: ComponentType.Input,
+      maxLength: 'new value',
       textResourceBindings: {
         title: 'ServiceName',
       },
-      maxLength: 'new value',
       itemType: 'COMPONENT',
       dataModelBindings: { simpleBinding: 'some-path' },
+    });
+  });
+
+  it('should call onChange for enum values', async () => {
+    const handleComponentChange = jest.fn();
+    renderEditStringValue({ handleComponentChange, enumValues: ['one', 'two', 'three'] });
+
+    await user.selectOptions(screen.getByRole('combobox'), 'one');
+    expect(handleComponentChange).toHaveBeenCalledWith({
+      id: 'c24d0812-0c34-4582-8f31-ff4ce9795e96',
+      type: ComponentType.Input,
+      textResourceBindings: {
+        title: 'ServiceName',
+      },
+      maxLength: 'one',
+      itemType: 'COMPONENT',
+      dataModelBindings: { simpleBinding: 'some-path' },
+    });
+  });
+
+  it('should call onChange for multiple enum values', async () => {
+    const handleComponentChange = jest.fn();
+    renderEditStringValue({
+      handleComponentChange,
+      enumValues: ['one', 'two', 'three'],
+      multiple: true,
+    });
+
+    await user.click(screen.getByRole('combobox'));
+    await user.click(screen.getByRole('option', { name: 'one' }));
+
+    await waitFor(() => {
+      //await user.selectOptions(screen.getByRole('listbox'), screen.getByRole('option', { name: "one" }));
+      expect(handleComponentChange).toHaveBeenCalledWith({
+        id: 'c24d0812-0c34-4582-8f31-ff4ce9795e96',
+        type: ComponentType.Input,
+        textResourceBindings: {
+          title: 'ServiceName',
+        },
+        maxLength: ['one'],
+        itemType: 'COMPONENT',
+        dataModelBindings: { simpleBinding: 'some-path' },
+      });
+    });
+
+    await user.click(screen.getByRole('option', { name: 'two' }));
+    await waitFor(() => {
+      expect(handleComponentChange).toHaveBeenCalledWith(
+        expect.objectContaining({
+          maxLength: ['one', 'two'],
+        }),
+      );
+    });
+
+    await user.click(screen.getByRole('option', { name: 'one' }));
+    await waitFor(() => {
+      expect(handleComponentChange).toHaveBeenCalledWith(
+        expect.objectContaining({
+          maxLength: ['two'],
+        }),
+      );
     });
   });
 });
