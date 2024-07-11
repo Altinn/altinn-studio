ba0cb0f0062884e97a8a06b5986a3144ac50d7d1
David Ovrelid <david@framit.no>
2024-07-11 21:33:20 +0200

    POC featue-request button in Studio

---
+++ frontend/app-development/utils/feedback.ts
@@ -0,0 +1,34 @@
+﻿type FeedbackTypes = 'featureRequest';
+
+type FeedbackConfig = {
+  labels: Array<string>;
+  template: string;
+};
+
+const feedbackTypeMap: Record<FeedbackTypes, FeedbackConfig> = {
+  featureRequest: {
+    labels: ['kind/feature-request', 'status/triage'],
+    template: 'feature_request.yml',
+  },
+};
+
+export class Feedback {
+  private readonly gitUrl: string = 'https://github.com';
+  private readonly org: string = 'Altinn';
+  private readonly repo: string = 'altinn-studio';
+
+  constructor() {}
+
+  public goToFeedbackUrl(feedbackType: FeedbackTypes = 'featureRequest'): string {
+    return this.getGitIssueUrl + this.optionToUrlParams(feedbackTypeMap[feedbackType]);
+  }
+
+  private get getGitIssueUrl(): string {
+    return `${this.gitUrl}/${this.org}/${this.repo}/issues/new`;
+  }
+
+  private optionToUrlParams(feedbackType): string {
+    const labels = feedbackType.labels.join(', ');
+    return `assignees=&labels=${labels}&projects=&template=${feedbackType.template}`;
+  }
+}
