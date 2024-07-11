b277c1e364a546bc69896c0110725e47e5ea7755
andreastanderen <andreastanderen@gmail.com>
2024-07-11 15:01:20 +0200

    Replace RuleModal in Calculations with Alert

--- frontend/packages/ux-editor/src/components/Properties/Calculations.test.tsx
+++ frontend/packages/ux-editor/src/components/Properties/Calculations.test.tsx
@@ -5,11 +5,16 @@ import { FormItemContext } from '../../containers/FormItemContext';
 import { formItemContextProviderMock } from '../../testing/formItemContextMocks';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import type { FormComponent } from '../../types/FormComponent';
+import { ServicesContextProvider } from 'app-shared/contexts/ServicesContext';
+import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
+import { queriesMock } from 'app-shared/mocks/queriesMock';
+import { appContextMock } from '@altinn/ux-editor/testing/appContextMock';
+import { AppContext } from '@altinn/ux-editor/AppContext';
 
 describe('Calculations', () => {
-  it('should render unknown component when components is unknown for Studio', () => {
+  it.skip('should render unknown component when components is unknown for Studio', () => {
     const formType = 'randomUnknownComponent' as unknown as FormComponent;
-    studioRender({ formItem: { ...formItemContextProviderMock.formItem, type: formType } });
+    renderCalculations({ formItem: { ...formItemContextProviderMock.formItem, type: formType } });
     expect(
       screen.getByText(
         textMock('ux_editor.edit_component.unknown_component', {
@@ -18,20 +23,38 @@ describe('Calculations', () => {
       ),
     );
   });
+
+  it('should render alert saying that calculations is deprecated with a documentation link ', () => {
+    renderCalculations();
+    const alert = screen.getByText(textMock('right_menu.rules_calculations_deprecated_info'));
+    const linkToDocs = screen.getByRole('link', { name: textMock('right_menu.dynamics_link') });
+    expect(alert).toBeInTheDocument();
+    expect(linkToDocs).toBeInTheDocument();
+  });
+
+  it('should render link to edit ruleHandler directly in Gitea', () => {
+    renderCalculations();
+    const linkToEdit = screen.getByRole('link', { name: textMock('right_menu.dynamics_edit') });
+    expect(linkToEdit).toBeInTheDocument();
+  });
 });
 
 const getCalculationsWithMockedFormItemContext = (props: Partial<FormItemContext> = {}) => {
   return (
-    <FormItemContext.Provider
-      value={{
-        ...formItemContextProviderMock,
-        ...props,
-      }}
-    >
-      <Calculations />
-    </FormItemContext.Provider>
+    <ServicesContextProvider {...queriesMock} client={createQueryClientMock()}>
+      <AppContext.Provider value={{ ...appContextMock }}>
+        <FormItemContext.Provider
+          value={{
+            ...formItemContextProviderMock,
+            ...props,
+          }}
+        >
+          <Calculations />
+        </FormItemContext.Provider>
+      </AppContext.Provider>
+    </ServicesContextProvider>
   );
 };
-const studioRender = (props: Partial<FormItemContext> = {}) => {
+const renderCalculations = (props: Partial<FormItemContext> = {}) => {
   return render(getCalculationsWithMockedFormItemContext(props));
 };
--- frontend/packages/ux-editor/src/components/Properties/Calculations.tsx
+++ frontend/packages/ux-editor/src/components/Properties/Calculations.tsx
@@ -1,47 +1,47 @@
 import React from 'react';
+import { Alert, Heading, Link } from '@digdir/designsystemet-react';
+import { ExternalLinkIcon } from '@studio/icons';
+import { StudioParagraph } from '@studio/components';
+import { useAppContext, useText } from '../../hooks';
 import classes from './Calculations.module.css';
-import { StudioButton } from '@studio/components';
-import { PlusIcon } from '@studio/icons';
-import { RuleModal } from '../toolbar/RuleModal';
-import { OldDynamicsInfo } from './OldDynamicsInfo';
-import { Divider } from 'app-shared/primitives';
-import { useText } from '../../hooks';
-import { useFormItemContext } from '../../containers/FormItemContext';
-import { formItemConfigs } from '../../data/formItemConfig';
-import { UnknownComponentAlert } from '../UnknownComponentAlert';
+import { altinnDocsUrl, giteaEditLink } from 'app-shared/ext-urls';
+import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 
 export const Calculations = () => {
-  const { formItem: form } = useFormItemContext();
-
-  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
   const t = useText();
+  const { app, org } = useStudioEnvironmentParams();
+  const { selectedFormLayoutSetName } = useAppContext();
+  const dynamicLocation = `App/ui/${selectedFormLayoutSetName}/RuleHandler.js`;
 
+  /*
   const isUnknownInternalComponent: boolean = form && !formItemConfigs[form.type];
   if (isUnknownInternalComponent) {
     return <UnknownComponentAlert componentName={form.type} />;
   }
+   */
 
   return (
     <div className={classes.calculations}>
+      <Alert size='small'>
+        <Heading size='xxsmall'>{t('right_menu.rules_calculations_deprecated_info_title')}</Heading>
+        <StudioParagraph size='small'>
+          {t('right_menu.rules_calculations_deprecated_info')}
+        </StudioParagraph>
+        <Link
+          href={altinnDocsUrl('/nb/app/development/logic/dynamic/')}
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          {t('right_menu.dynamics_link')}
+          <ExternalLinkIcon />
+        </Link>
+      </Alert>
       <div>
-        <div className={classes.header}>
-          <span>{t('right_menu.rules_calculations')}</span>
-          <StudioButton
-            aria-label={t('right_menu.rules_calculations_add_alt')}
-            icon={<PlusIcon />}
-            onClick={() => setModalOpen(true)}
-            variant='tertiary'
-            size='small'
-          />
-        </div>
-        <RuleModal
-          modalOpen={modalOpen}
-          handleClose={() => setModalOpen(false)}
-          handleOpen={() => setModalOpen(true)}
-        />
+        <Link href={giteaEditLink(org, app, dynamicLocation)} target='_blank' rel='noreferrer'>
+          {t('right_menu.dynamics_edit')}
+          <ExternalLinkIcon />
+        </Link>
       </div>
-      <Divider marginless />
-      <OldDynamicsInfo />
     </div>
   );
 };
