26ae88e5116420d0a50925f78fca3f74917b7522
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-07-12 10:37:25 +0200

    smaller code changes to CodeGeneratorContext (#2200)
    * smaller code changes to CodeGeneratorContext
    
    * fixes errors

--- src/codegen/CodeGeneratorContext.ts
+++ src/codegen/CodeGeneratorContext.ts
@@ -4,23 +4,25 @@ import type { JSONSchema7 } from 'json-schema';
 import type { MaybeSymbolizedCodeGenerator } from 'src/codegen/CodeGenerator';
 import type { SchemaFile } from 'src/codegen/SchemaFile';
 
+type JsonSchema7WithDefinitions = Required<Pick<JSONSchema7, 'definitions'>> & JSONSchema7;
+
 /**
  * This code relies on the fact that the code generator will generate one file at a time, and reset the context
  * between each file. This means that we can use a global variable to store the context, and use this context
  * to store information about the current file being generated (such as imports, etc).
  */
 export class CodeGeneratorContext {
-  private static fileInstance: CodeGeneratorFileContext<any> | undefined;
+  private static fileContext: CodeGeneratorFileContext<any> | undefined;
 
   public static curFile(): CodeGeneratorFileContext<any> {
-    if (!this.fileInstance) {
+    if (!this.fileContext) {
       throw new Error(
         'CodeGeneratorFileContext has not been initialized, run this in code that is called ' +
           'within CodeGeneratorContext.generateFile(), such as a CodeGenerator.toTypeScript() method',
       );
     }
 
-    return this.fileInstance;
+    return this.fileContext;
   }
 
   public static async generateTypeScript(
@@ -28,7 +30,7 @@ export class CodeGeneratorContext {
     fn: () => string | Promise<string>,
   ): Promise<{ result: string }> {
     const instance = new CodeGeneratorFileContext(targetFile, 'typeScript');
-    CodeGeneratorContext.fileInstance = instance;
+    CodeGeneratorContext.fileContext = instance;
     const functionOutput = await fn();
     const parts: string[] = [];
     const symbols: SymbolTable<'typeScript'> = {};
@@ -38,7 +40,7 @@ export class CodeGeneratorContext {
     }
 
     // Sort symbols and add them in sorted order to the file
-    const sortedSymbols = Object.keys(symbols).sort();
+    const sortedSymbols = Object.keys(symbols).sort((a, b) => a.localeCompare(b));
     for (const symbol of sortedSymbols) {
       parts.push(symbols[symbol]);
     }
@@ -51,68 +53,84 @@ export class CodeGeneratorContext {
       parts.push(functionOutput);
     }
 
-    CodeGeneratorContext.fileInstance = undefined;
+    CodeGeneratorContext.fileContext = undefined;
 
     return { result: parts.join('\n\n') };
   }
 
+  private static findRefs(jsonSchema: JsonSchema7WithDefinitions): Set<string> {
+    const { definitions, ...jsonSchemaWithoutDefs } = jsonSchema;
+    const refRegex = /"\$ref":\s*"([^"]+)"/g;
+    const foundRefs = new Set<string>();
+
+    for (const definition of [...Object.values(definitions), jsonSchemaWithoutDefs]) {
+      const jsonDefinition = JSON.stringify(definition);
+      const refMatches = jsonDefinition.match(refRegex);
+      if (refMatches) {
+        for (const ref of refMatches) {
+          const result = ref.replace('"$ref":', '').replace(/"/g, '').trim().replace('#/definitions/', '');
+          foundRefs.add(result);
+        }
+      } else if (jsonDefinition.includes('$ref')) {
+        throw new Error(`Could not find ref in ${jsonDefinition}`);
+      }
+    }
+
+    return foundRefs;
+  }
+
+  private static cleanDefinitions(jsonSchema: JsonSchema7WithDefinitions): NonNullable<JSONSchema7['definitions']> {
+    let removedLastRound: number | undefined = undefined;
+    const { definitions: originalDefinitions, ...rest } = jsonSchema;
+    let cleanedDefinitions = { ...originalDefinitions };
+
+    while (removedLastRound === undefined || removedLastRound > 0) {
+      const foundRefs = this.findRefs({ ...rest, definitions: cleanedDefinitions });
+      const allRefs = new Set<string>(Object.keys(cleanedDefinitions));
+      const notFoundRefs = [...allRefs].filter((ref) => !foundRefs.has(ref));
+
+      const finalSchemaDefs = structuredClone(cleanedDefinitions);
+      removedLastRound = 0;
+      for (const key of notFoundRefs) {
+        delete finalSchemaDefs[key];
+        removedLastRound++;
+      }
+
+      cleanedDefinitions = finalSchemaDefs;
+    }
+    return cleanedDefinitions;
+  }
+
   public static async generateJsonSchema(baseFilePath: string, file: SchemaFile): Promise<{ result: JSONSchema7 }> {
     const targetFile = baseFilePath + file.getFileName();
-    const instance = new CodeGeneratorFileContext(targetFile, 'jsonSchema');
-    CodeGeneratorContext.fileInstance = instance;
-    const functionOutput = await file.getSchema();
+    const jsonSchemaFileContext = new CodeGeneratorFileContext(targetFile, 'jsonSchema');
+    CodeGeneratorContext.fileContext = jsonSchemaFileContext;
+    const jsonSchema = await file.getSchema();
+
     const symbols: SymbolTable<'jsonSchema'> = {};
-    while (Object.keys(instance.symbols).length) {
-      const newSymbols = instance.getSymbols(symbols);
+    while (Object.keys(jsonSchemaFileContext.symbols).length) {
+      const newSymbols = jsonSchemaFileContext.getSymbols(symbols);
       Object.assign(symbols, newSymbols);
     }
 
     // Sort symbols and add them in sorted order to the file
-    const sortedSymbols = Object.keys(symbols).sort();
+    const sortedSymbols = Object.keys(symbols).sort((a, b) => a.localeCompare(b));
     for (const symbol of sortedSymbols) {
-      functionOutput.definitions = functionOutput.definitions || {};
-      functionOutput.definitions[symbol] = symbols[symbol];
+      jsonSchema.definitions = jsonSchema.definitions ?? {};
+      jsonSchema.definitions[symbol] = symbols[symbol];
     }
 
-    if (file.shouldCleanDefinitions() && functionOutput.definitions) {
-      let removedLastRound: number | undefined = undefined;
-      while (removedLastRound === undefined || removedLastRound > 0) {
-        const { definitions, ...rest } = functionOutput;
-        const foundRefs = new Set<string>();
-        const allRefs = new Set<string>(Object.keys(definitions));
-        const defs = [...Object.values(definitions), rest];
-        for (const value of defs) {
-          const asJson = JSON.stringify(value);
-          const refRegex = /"\$ref":\s*"([^"]+)"/g;
-          const refMatches = asJson.match(refRegex);
-          if (refMatches) {
-            for (const ref of refMatches) {
-              const result = ref.replace('"$ref":', '').replace(/"/g, '').trim().replace('#/definitions/', '');
-              foundRefs.add(result);
-            }
-          } else if (asJson.includes('$ref')) {
-            throw new Error(`Could not find ref in ${asJson}`);
-          }
-        }
-
-        const notFoundRefs = [...allRefs].filter((ref) => !foundRefs.has(ref));
-        const finalSchemaDefs = structuredClone(definitions);
-        removedLastRound = 0;
-        for (const key of notFoundRefs) {
-          delete finalSchemaDefs[key];
-          removedLastRound++;
-        }
-        functionOutput.definitions = finalSchemaDefs;
-      }
+    if (file.shouldCleanDefinitions() && jsonSchema.definitions) {
+      jsonSchema.definitions = this.cleanDefinitions({ ...jsonSchema, definitions: jsonSchema.definitions });
     }
 
-    CodeGeneratorContext.fileInstance = undefined;
+    CodeGeneratorContext.fileContext = undefined;
 
     return {
       result: {
         $schema: 'http://json-schema.org/draft-07/schema#',
         $id: `https://altinncdn.no/${targetFile}`,
-        ...functionOutput,
+        ...jsonSchema,
       },
     };
   }
@@ -146,7 +164,7 @@ export class CodeGeneratorFileContext<T extends FileType> {
       this.imports[from] = new Set();
     }
 
-    const set = this.imports[from] as Set<string>;
+    const set: Set<string> = this.imports[from];
     set.add(symbol);
   }
 
@@ -175,7 +193,7 @@ export class CodeGeneratorFileContext<T extends FileType> {
 
   getImportsAsTypeScript(): string {
     const importLines = Object.keys(this.imports).map((fileName) => {
-      const symbols = Array.from(this.imports[fileName]).sort();
+      const symbols = Array.from(this.imports[fileName]).sort((a, b) => a.localeCompare(b));
       return `import { ${symbols.join(', ')} } from '${fileName}';`;
     });
 
