4d12e09a6f08b3597957ad02e21f58523fb1b751
andreastanderen <andreastanderen@gmail.com>
2024-07-16 10:45:30 +0200

    Fix PR comments

--- frontend/packages/ux-editor/src/hooks/useComponentPropertyEnumValue.test.ts
+++ frontend/packages/ux-editor/src/hooks/useComponentPropertyEnumValue.test.ts
@@ -3,13 +3,13 @@ import { textMock } from '@studio/testing/mocks/i18nMock';
 import { useComponentPropertyEnumValue } from '@altinn/ux-editor/hooks/useComponentPropertyEnumValue';
 import type { KeyValuePairs } from 'app-shared/types/KeyValuePairs';
 
-const someEnumValue = 'someEnumValue';
+const enumValueWithoutText = 'enumValueWithoutText';
 const customTextMockToHandleUndefined = (
   keys: string | string[],
   variables?: KeyValuePairs<string>,
 ) => {
   const key = Array.isArray(keys) ? keys[0] : keys;
-  if (key === `ux_editor.component_properties.enum_${someEnumValue}`) return key;
+  if (key === `ux_editor.component_properties.enum_${enumValueWithoutText}`) return key;
   return variables
     ? '[mockedText(' + key + ', ' + JSON.stringify(variables) + ')]'
     : '[mockedText(' + key + ')]';
@@ -31,7 +31,7 @@ describe('useComponentPropertyEnumValue', () => {
   });
   it('Returns a function that returns the enum value if there was no text key for the description', () => {
     const result = renderHook(() => useComponentPropertyEnumValue()).result.current;
-    const propertyEnumValue = result(someEnumValue);
-    expect(propertyEnumValue).toEqual(someEnumValue);
+    const propertyEnumValue = result(enumValueWithoutText);
+    expect(propertyEnumValue).toEqual(enumValueWithoutText);
   });
 });
