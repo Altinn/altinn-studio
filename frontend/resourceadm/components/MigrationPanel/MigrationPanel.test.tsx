db330bdde1af2a788d8353fec3233550cf9d684c
Martin Gunnerud <mgunnerud@gmail.com>
2024-07-23 09:08:52 +0200

    remove unused import

--- frontend/resourceadm/components/MigrationPanel/MigrationPanel.test.tsx
+++ frontend/resourceadm/components/MigrationPanel/MigrationPanel.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import { MemoryRouter } from 'react-router-dom';
