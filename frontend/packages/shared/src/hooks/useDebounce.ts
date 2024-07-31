1b3e2ff52c069dfc099720775b85ea81110dd513
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-07-31 10:20:04 +0200

    Add useCallback to cache debounce function between renders
    Explicitly type debounceRef to avoid NodeJS.Timeout being used as type

--- frontend/packages/shared/src/hooks/useDebounce.ts
+++ frontend/packages/shared/src/hooks/useDebounce.ts
@@ -1,15 +1,19 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 
 type UseDebounceOptions = {
   debounceTimeInMs: number;
 };
 export const useDebounce = ({ debounceTimeInMs }: UseDebounceOptions) => {
-  const debounceRef = useRef(undefined);
-  const debounce = (callback: Function): void => {
-    clearTimeout(debounceRef.current);
-    debounceRef.current = setTimeout(() => {
-      callback();
-    }, debounceTimeInMs);
-  };
+  const debounceRef = useRef<ReturnType<typeof setTimeout>>(undefined);
+  const debounce = useCallback(
+    (callback: Function): void => {
+      clearTimeout(debounceRef.current);
+      debounceRef.current = setTimeout(() => {
+        callback();
+      }, debounceTimeInMs);
+    },
+    [debounceTimeInMs],
+  );
+
   return { debounce };
 };
