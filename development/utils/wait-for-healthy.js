6a0997cd14e6c67d2953b023b942d3313d6e7220
davidovrelid.com <david@framit.no>
2024-08-08 14:56:29 +0200

    This should be the final commit

--- development/utils/wait-for-healthy.js
+++ development/utils/wait-for-healthy.js
@@ -3,20 +3,20 @@ const { execSync } = require('child_process');
 module.exports = (name, timeout = 60000) =>
   new Promise(function (resolve, reject) {
     const intervalId = setInterval(function () {
-      var buffer = execSync(`docker inspect --format='{{json .State.Health.Status}}' ${name} `);
+      const buffer = execSync(`docker inspect --format='{{json .State.Health.Status}}' ${name} `);
 
       const status = JSON.parse(buffer.toString());
 
-      if (status == `healthy`) {
+      if (status === `healthy`) {
         console.log(name, ' is healthy!');
         clearInterval(intervalId);
         resolve();
+      } else {
+        setTimeout(() => {
+          clearInterval(intervalId);
+          console.log('Giving up waiting for healthy of: ', name);
+          reject('Giving up this');
+        }, timeout);
       }
     }, 1000);
-
-    setTimeout(() => {
-      clearInterval(intervalId);
-      console.log('Giving up: ', name);
-      reject('Giving up this');
-    }, timeout);
   });
