c28fa1a39b36ac95bc14f00bbc8a58356506f527
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-08-29 14:43:30 +0200

    feat: New Studio Component `RecommendedNextAction`

---
+++ frontend/libs/studio-components/src/components/StudioRecommendedNextAction/index.ts
@@ -0,0 +1,4 @@
+export { StudioRecommendedNextAction } from './StudioRecommendedNextAction';
+export type { StudioRecommendedNextActionProps } from './StudioRecommendedNextAction';
+export { StudioRecommendedNextActionContextProvider } from './context/StudioRecommendedNextActionContext';
+export { useStudioRecommendedNextActionContext } from './context/useStudioRecommendedNextActionContext';
