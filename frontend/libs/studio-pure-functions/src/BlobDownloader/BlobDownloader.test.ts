534d26048af157154eb9d9c669559da92e1a540a
Nina Kylstad <nkylstad@gmail.com>
2024-08-30 09:40:13 +0200

    some more tests

--- frontend/libs/studio-pure-functions/src/BlobDownloader/BlobDownloader.test.ts
+++ frontend/libs/studio-pure-functions/src/BlobDownloader/BlobDownloader.test.ts
@@ -2,17 +2,25 @@ import { BlobDownloader } from './BlobDownloader';
 
 describe('BlobDownloader', () => {
   const data = { test: 'test' };
-  const blobDownloader = new BlobDownloader(JSON.stringify(data));
-  global.URL.createObjectURL = jest.fn();
-  global.URL.revokeObjectURL = jest.fn();
+
+  beforeEach(() => {
+    global.URL.createObjectURL = jest.fn();
+    global.URL.revokeObjectURL = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('should generate a download url', () => {
+    const blobDownloader = new BlobDownloader(JSON.stringify(data));
     blobDownloader.getDownloadURL();
     const testBlob = new Blob([JSON.stringify(data)], { type: 'application/json' });
     expect(global.URL.createObjectURL).toHaveBeenCalledWith(testBlob);
   });
 
   it('should revoke a download url', () => {
+    const blobDownloader = new BlobDownloader(JSON.stringify(data));
     const downloadUrl = blobDownloader.getDownloadURL();
     blobDownloader.revokeDownloadURL(downloadUrl);
     expect(global.URL.revokeObjectURL).toHaveBeenCalledWith(downloadUrl);
@@ -24,5 +32,16 @@ describe('BlobDownloader', () => {
     blobDownloaderWithFileType.getDownloadURL();
     const testBlob = new Blob([JSON.stringify(data)], { type: fileType });
     expect(global.URL.createObjectURL).toHaveBeenCalledWith(testBlob);
+  });
+
+  it('should handle clicking the download link', () => {
+    const blobDownloader = new BlobDownloader(JSON.stringify(data));
+    const mockGetDownloadURL = jest.fn();
+    const mockGetRevokeDownloadURL = jest.fn();
+    jest.spyOn(blobDownloader, 'getDownloadURL').mockImplementation(mockGetDownloadURL);
+    jest.spyOn(blobDownloader, 'revokeDownloadURL').mockImplementation(mockGetRevokeDownloadURL);
+    blobDownloader.handleDownloadClick();
+    expect(mockGetDownloadURL).toHaveBeenCalled();
+    expect(mockGetRevokeDownloadURL).toHaveBeenCalled();
   });
 });
