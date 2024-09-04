99d2961e76943da45e111dde61a0b26b2749ba12
Ole Martin Handeland <git@olemartin.org>
2024-09-04 16:45:31 +0200

    Resolving merge conflicts?

--- App/models/model.cs
+++ App/models/model.cs
@@ -7,47 +7,6 @@ using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
-<<<<<<< HEAD
-namespace Altinn.App.Models.model
-{
-  [XmlRoot(ElementName="model")]
-  public class model
-  {
-    [XmlElement("biler", Order = 1)]
-    [JsonProperty("biler")]
-    [JsonPropertyName("biler")]
-    public biler biler { get; set; }
-
-  }
-
-  public class biler
-  {
-    [XmlElement("bil", Order = 1)]
-    [JsonProperty("bil")]
-    [JsonPropertyName("bil")]
-    public bil bil { get; set; }
-
-  }
-
-  public class bil
-  {
-    [XmlElement("registreringsnummer", Order = 1)]
-    [JsonProperty("registreringsnummer")]
-    [JsonPropertyName("registreringsnummer")]
-    public string registreringsnummer { get; set; }
-
-    [XmlElement("merke", Order = 2)]
-    [JsonProperty("merke")]
-    [JsonPropertyName("merke")]
-    public string merke { get; set; }
-
-    [XmlElement("modell", Order = 3)]
-    [JsonProperty("modell")]
-    [JsonPropertyName("modell")]
-    public string modell { get; set; }
-
-  }
-=======
 
 namespace Altinn.App.Models.model
 {
@@ -72,5 +31,4 @@ namespace Altinn.App.Models.model
 
         public bool ShouldSerializeAlder() => Alder.HasValue;
     }
->>>>>>> master
 }
