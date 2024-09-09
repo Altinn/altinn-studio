acccb49c45427e3fefd4b6148aac0d6faa1ff69e
Magnus Revheim Martinsen <mrmartinsen.96@gmail.com>
2024-09-09 10:51:03 +0200

    remove header summary2 (#2415)

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -4,7 +4,6 @@ import type { JSX } from 'react';
 import { HeaderDef } from 'src/layout/Header/config.def.generated';
 import { HeaderComponent } from 'src/layout/Header/HeaderComponent';
 import type { PropsFromGenericComponent } from 'src/layout';
-import type { Summary2Props } from 'src/layout/Summary2/SummaryComponent2/types';
 
 export type IHeaderProps = PropsFromGenericComponent<'Header'>;
 
@@ -14,14 +13,4 @@ export class Header extends HeaderDef {
       return <HeaderComponent {...props} />;
     },
   );
-
-  // TODO: Temporary solution to show the summary for Headers
-  renderSummary2(props: Summary2Props<'Header'>): JSX.Element | null {
-    return (
-      <HeaderComponent
-        node={props.target}
-        containerDivRef={React.createRef()}
-      />
-    );
-  }
 }
