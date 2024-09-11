eed9a61dea3a412bcf7ba3c3e86d33685937a669
lassopicasso <lars.walderhaug@digdir.no>
2024-09-11 12:05:43 +0200

    refactor tests a bit

--- frontend/libs/studio-components/src/components/StudioCombobox/StudioCombobox.test.tsx
+++ frontend/libs/studio-components/src/components/StudioCombobox/StudioCombobox.test.tsx
@@ -3,19 +3,36 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { StudioCombobox, type StudioComboboxProps } from './index';
 import userEvent from '@testing-library/user-event';
 
+const options = {
+  ole: 'Ole',
+  dole: 'Dole',
+  doffen: 'Doffen',
+  invalid: 'Kinkliane Koff',
+};
+const noResults = 'No results';
+
 describe('StudioCombobox', () => {
+  const verifyOnValueChange = async (
+    onValueChange: jest.Mock,
+    expectedCalls: number,
+    expectedValue: string[],
+  ) => {
+    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(expectedCalls));
+    expect(onValueChange).toHaveBeenCalledWith(expectedValue);
+  };
+
   it('should render the component', () => {
     renderTestCombobox();
     const studioCombobox = screen.getByRole('combobox');
     expect(studioCombobox).toBeInTheDocument();
   });
 
-  it('should render the empty state', async () => {
+  it('should render the empty state when no results are found', async () => {
     const user = userEvent.setup();
     renderTestCombobox();
     const studioCombobox = screen.getByRole('combobox');
-    await user.type(studioCombobox, 'Kinkliane Koff');
-    const emptyState = screen.getByText('No results');
+    await user.type(studioCombobox, options.invalid);
+    const emptyState = screen.getByText(noResults);
     expect(emptyState).toBeInTheDocument();
   });
 
@@ -27,19 +44,17 @@ describe('StudioCombobox', () => {
     expect(studioCombobox).toHaveValue('');
 
     await user.click(studioCombobox);
-    await user.click(screen.getByRole('option', { name: 'Ole' }));
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(1));
-    expect(onValueChange).toHaveBeenCalledWith(['Ole']);
-    expect(studioCombobox).toHaveValue('Ole');
+    await user.click(screen.getByRole('option', { name: options.ole }));
+    await verifyOnValueChange(onValueChange, 1, [options.ole]);
+    expect(studioCombobox).toHaveValue(options.ole);
 
-    const comboboxIsClosed = screen.queryByRole('option', { name: 'Ole' });
+    const comboboxIsClosed = screen.queryByRole('option', { name: options.ole });
     expect(comboboxIsClosed).toBeNull();
 
     await user.click(studioCombobox);
-    await user.click(screen.getByRole('option', { name: 'Dole' }));
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(2));
-    expect(onValueChange).toHaveBeenCalledWith(['Dole']);
-    expect(studioCombobox).toHaveValue('Dole');
+    await user.click(screen.getByRole('option', { name: options.dole }));
+    await verifyOnValueChange(onValueChange, 2, [options.dole]);
+    expect(studioCombobox).toHaveValue(options.dole);
   });
 
   it('should be possible to select multiple options', async () => {
@@ -49,44 +64,38 @@ describe('StudioCombobox', () => {
     const studioCombobox = screen.getByRole('combobox');
     await user.click(studioCombobox);
 
-    await user.click(screen.getByRole('option', { name: 'Ole' }));
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(1));
-    expect(onValueChange).toHaveBeenCalledWith(['Ole']);
+    await user.click(screen.getByRole('option', { name: options.ole }));
+    await verifyOnValueChange(onValueChange, 1, [options.ole]);
 
-    await user.click(screen.getByRole('option', { name: 'Dole' }));
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(2));
-    expect(onValueChange).toHaveBeenCalledWith(['Ole', 'Dole']);
+    await user.click(screen.getByRole('option', { name: options.dole }));
+    await verifyOnValueChange(onValueChange, 2, [options.ole, options.dole]);
 
-    await user.click(screen.getByRole('option', { name: 'Doffen' }));
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(3));
-    expect(onValueChange).toHaveBeenCalledWith(['Ole', 'Dole', 'Doffen']);
+    await user.click(screen.getByRole('option', { name: options.doffen }));
+    await verifyOnValueChange(onValueChange, 3, [options.ole, options.dole, options.doffen]);
   });
 
   it('should be possible to clear the selected options', async () => {
     const user = userEvent.setup();
     const onValueChange = jest.fn();
-    renderTestCombobox({ multiple: true, onValueChange, value: ['Ole', 'Dole', 'Doffen'] });
+    renderTestCombobox({
+      multiple: true,
+      onValueChange,
+      value: [options.ole, options.dole, options.doffen],
+    });
 
     const clearButton = screen.getByRole('button', { name: 'Fjern alt' });
     await user.click(clearButton);
-    await waitFor(() => expect(onValueChange).toHaveBeenCalledTimes(1));
-    expect(onValueChange).toHaveBeenCalledWith([]);
-  });
-
-  it('should display preselected option as selected', async () => {
-    renderTestCombobox({ value: ['Ole'] });
-    const combobox = screen.getByRole('combobox');
-    expect(combobox).toHaveValue('Ole');
+    await verifyOnValueChange(onValueChange, 1, []);
   });
 });
 
 const renderTestCombobox = (props?: StudioComboboxProps) => {
   render(
     <StudioCombobox {...props}>
-      <StudioCombobox.Empty>No results</StudioCombobox.Empty>
-      <StudioCombobox.Option value='Ole'>Ole</StudioCombobox.Option>
-      <StudioCombobox.Option value='Dole'>Dole</StudioCombobox.Option>
-      <StudioCombobox.Option value='Doffen'>Doffen</StudioCombobox.Option>
+      <StudioCombobox.Empty>{noResults}</StudioCombobox.Empty>
+      <StudioCombobox.Option value={options.ole}>{options.ole}</StudioCombobox.Option>
+      <StudioCombobox.Option value={options.dole}>{options.dole}</StudioCombobox.Option>
+      <StudioCombobox.Option value={options.doffen}>{options.doffen}</StudioCombobox.Option>
     </StudioCombobox>,
   );
 };
