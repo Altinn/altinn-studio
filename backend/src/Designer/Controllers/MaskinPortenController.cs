176a34a3d014a05af67787ca57860df6b2047ef4
Mirko Sekulic <misha.sekulic@gmail.com>
2024-09-16 15:23:30 +0200

    Add todo note

--- backend/src/Designer/Controllers/MaskinPortenController.cs
+++ backend/src/Designer/Controllers/MaskinPortenController.cs
@@ -13,6 +13,9 @@ namespace Altinn.Studio.Designer.Controllers;
 [Route("designer/api/[controller]")]
 public class MaskinPortenController(IMaskinPortenHttpClient maskinPortenHttpClient) : ControllerBase
 {
+
+    // TODO: Cleanup model and create separation between presentation dto, domain model and external api model
+    // Will be done under: https://github.com/Altinn/altinn-studio/issues/12767 and https://github.com/Altinn/altinn-studio/issues/12766
     [Authorize(AnsattPortenConstants.AnsattportenAuthorizationPolicy)]
     [HttpGet("scopes")]
     public async Task<IActionResult> Get(CancellationToken cancellationToken)
