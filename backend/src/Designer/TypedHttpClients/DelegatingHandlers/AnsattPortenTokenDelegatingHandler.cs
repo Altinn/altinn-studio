f69d1793ad4965c1b9c34fd9e1c26524a4775870
Mirko Sekulic <misha.sekulic@gmail.com>
2024-09-17 09:08:14 +0200

    feat: ansattporten integration (#13537)

---
+++ backend/src/Designer/TypedHttpClients/DelegatingHandlers/AnsattPortenTokenDelegatingHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Altinn.Studio.Designer.TypedHttpclients.DelegatingHandlers;
+
+public class AnsattPortenTokenDelegatingHandler(IHttpContextAccessor httpContextAccessor) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        string token = await httpContextAccessor.HttpContext!.GetTokenAsync(AnsattPortenConstants.AnsattpotenCookiesAuthenticationScheme, "access_token");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
+
