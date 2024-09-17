eb06761e8300dfac37dbfae087006403dbc09744
wrt95 <wrt_95@hotmail.com>
2024-09-17 14:49:00 +0200

    Adding onClick test for StudioPAgeHeaderButton

--- frontend/libs/studio-components/src/components/StudioPageHeader/StudioPageHeaderButton/StudioPageHeaderButton.test.tsx
+++ frontend/libs/studio-components/src/components/StudioPageHeader/StudioPageHeaderButton/StudioPageHeaderButton.test.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { StudioPageHeaderButton, type StudioPageHeaderButtonProps } from './StudioPageHeaderButton';
+import userEvent from '@testing-library/user-event';
 
 describe('StudioPageHeaderButton', () => {
   it('should apply the correct class based on variant and color - regular dark', () => {
     renderStudioPageHeaderButton({ variant: 'regular', color: 'dark' });
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: buttonText });
     expect(button).toHaveClass('regularDark');
   });
 
   it('should apply the correct class based on variant and color - regular light', () => {
     renderStudioPageHeaderButton({ variant: 'regular', color: 'light' });
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: buttonText });
     expect(button).toHaveClass('regularLight');
   });
 
   it('should apply the correct class based on variant and color - preview dark', () => {
     renderStudioPageHeaderButton({ variant: 'preview', color: 'dark' });
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: buttonText });
     expect(button).toHaveClass('previewDark');
   });
 
   it('should apply the correct class based on variant and color - preview light', () => {
     renderStudioPageHeaderButton({ variant: 'preview', color: 'light' });
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: buttonText });
     expect(button).toHaveClass('previewLight');
   });
 
@@ -37,7 +38,21 @@ describe('StudioPageHeaderButton', () => {
 
     expect(ref.current).toBeInstanceOf(HTMLButtonElement);
   });
+
+  it('should call the onClick function when the button is clicked', async () => {
+    const user = userEvent.setup();
+
+    const mockOnClick = jest.fn();
+    renderStudioPageHeaderButton({ onClick: mockOnClick });
+
+    const button = screen.getByRole('button', { name: buttonText });
+    await user.click(button);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
 });
+
+const buttonText: string = 'Button';
 
 const defaultProps: StudioPageHeaderButtonProps = {
   color: 'dark',
@@ -50,7 +65,7 @@ const renderStudioPageHeaderButton = (
 ) => {
   return render(
     <StudioPageHeaderButton ref={ref} {...defaultProps} {...props}>
-      Button
+      {buttonText}
     </StudioPageHeaderButton>,
   );
 };
