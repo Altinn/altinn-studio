66126d0c0a2ce2fcedee0e72f36d97de2db73360
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-09-17 12:44:31 +0200

    Add validation to xsd file upload for duplicate datatypes

--- frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/XSDUpload.tsx
+++ frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/XSDUpload.tsx
@@ -8,6 +8,8 @@ import type { ApiError } from 'app-shared/types/api/ApiError';
 import { toast } from 'react-toastify';
 import type { MetadataOption } from '../../../../types/MetadataOption';
 import { fileSelectorInputId } from '@studio/testing/testids';
+import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
+import { useAppMetadataQuery } from 'app-shared/hooks/queries';
 
 export interface XSDUploadProps {
   selectedOption?: MetadataOption;
@@ -21,6 +23,8 @@ export const XSDUpload = ({
   uploaderButtonVariant,
 }: XSDUploadProps) => {
   const { t } = useTranslation();
+  const { org, app } = useStudioEnvironmentParams();
+  const { data: appMetadata } = useAppMetadataQuery(org, app);
   const { mutate: uploadDataModel, isPending: uploading } = useUploadDataModelMutation(
     selectedOption?.value?.repositoryRelativeUrl,
     {
@@ -40,7 +44,27 @@ export const XSDUpload = ({
     });
   };
 
-  const handleInvalidFileName = () => toast.error(t('app_data_modelling.upload_xsd_invalid_error'));
+  const fileNameRegEx: RegExp = /^[a-zA-Z][a-zA-Z0-9_.\-æÆøØåÅ ]*$/;
+
+  const validateFileName = (fileName: string): boolean => {
+    const nameFollowsRegexRules = Boolean(fileName.match(fileNameRegEx));
+    if (!nameFollowsRegexRules) {
+      toast.error(t('app_data_modelling.upload_xsd_invalid_error'));
+      return false;
+    }
+
+    const fileNameWithoutExtension = fileName.split('.').slice(0, -1).join('.');
+    const duplicateDataType = Boolean(
+      appMetadata.dataTypes?.find(
+        (dataType) => dataType.id.toLowerCase() === fileNameWithoutExtension.toLowerCase(),
+      ),
+    );
+    if (duplicateDataType) {
+      toast.error(t('schema_editor.error_data_type_name_exists'));
+      return false;
+    }
+    return true;
+  };
 
   return (
     <span ref={uploadButton}>
@@ -54,8 +78,8 @@ export const XSDUpload = ({
           ref={fileInputRef}
           uploaderButtonText={uploadButtonText}
           customFileNameValidation={{
-            validateFileName: isFileNameMatchingRegEx,
-            onInvalidFileName: handleInvalidFileName,
+            validateFileName,
+            onInvalidFileName: () => {},
           }}
           dataTestId={fileSelectorInputId}
         />
@@ -63,6 +87,3 @@ export const XSDUpload = ({
     </span>
   );
 };
-
-const fileNameRegEx: RegExp = /^[a-zA-Z][a-zA-Z0-9_.\-æÆøØåÅ ]*$/;
-const isFileNameMatchingRegEx = (fileName: string): boolean => !!fileName.match(fileNameRegEx);
