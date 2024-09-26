67f57ad7fb913d95b58bb51d4905408cb0c9de84
wrt95 <wrt_95@hotmail.com>
2024-09-26 08:45:53 +0200

    fixing text

--- frontend/studio-root/pages/PageLayout/PageLayout.tsx
+++ frontend/studio-root/pages/PageLayout/PageLayout.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { StudioPageHeader } from '@studio/components/';
+import { useTranslation } from 'react-i18next';
 
 export const PageLayout = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <StudioPageHeader>
         <StudioPageHeader.Main>
-          <StudioPageHeader.Left title='Tilbake' showTitle={false} />
+          <StudioPageHeader.Left title={t('general.back')} showTitle={false} />
         </StudioPageHeader.Main>
       </StudioPageHeader>
       <Outlet />
