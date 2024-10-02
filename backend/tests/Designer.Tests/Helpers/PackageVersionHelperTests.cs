a560df48142301abe994b461bf555b25e0261888
Michael Queyrichon <michael.queyrichon@digdir.no>
2024-10-02 12:55:05 +0200

    Fix tests

--- backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
+++ backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
@@ -9,7 +9,7 @@ namespace Designer.Tests.Helpers
     public class PackageVersionHelperTests
     {
         [Theory]
-        [InlineData("Altinn.App.Api", true, "8.0.0")]
+        [InlineData("Altinn.App.Api", true, "8.3.5")]
         [InlineData("NonExistingNuget", false)]
         public void TryGetPackageVersionFromCsprojFile_GivenValidCsprojFile_ReturnsTrue(string packageName, bool expectedResult, string expectedVersion = "")
         {
