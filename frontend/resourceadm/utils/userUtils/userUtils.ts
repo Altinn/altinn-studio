321c38f0fc37d92cb95e750b032a2e38200240f0
wrt95 <wrt_95@hotmail.com>
2024-10-04 15:51:27 +0200

    refactor(resourceadm): Moving headercontext and gitea header

--- frontend/resourceadm/utils/userUtils/userUtils.ts
+++ frontend/resourceadm/utils/userUtils/userUtils.ts
@@ -1,4 +1,4 @@
-import { SelectedContextType } from 'app-shared/navigation/main-header/Header';
+import { SelectedContextType } from 'resourceadm/context/HeaderContext';
 import type { Organization } from 'app-shared/types/Organization';
 
 export const userHasAccessToOrganization = ({
