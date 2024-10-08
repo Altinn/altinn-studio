25e00117febf081b82d7710bec25527214ef5419
Tomas <tomas.engebretsen@gmail.com>
2024-10-08 14:50:39 +0200

    feat: Add keyboard navigation to input table

--- frontend/libs/studio-components/src/components/StudioInputTable/HeaderCell/HeaderCellCheckbox.tsx
+++ frontend/libs/studio-components/src/components/StudioInputTable/HeaderCell/HeaderCellCheckbox.tsx
@@ -2,6 +2,8 @@ import { StudioTable } from '../../StudioTable';
 import React, { forwardRef, useId } from 'react';
 import type { StudioCheckboxProps } from '../../StudioCheckbox';
 import { StudioCheckbox } from '../../StudioCheckbox';
+import type { HTMLCellInputElement } from '../types/HTMLCellInputElement';
+import { getNextInputElement } from '../dom-utils/getNextInputElement';
 
 export type HeaderCellCheckboxProps = Omit<StudioCheckboxProps, 'value'> & { value?: string };
 
@@ -11,10 +13,38 @@ export const HeaderCellCheckbox = forwardRef<HTMLInputElement, HeaderCellCheckbo
     const value = givenValue ?? defaultValue;
     return (
       <StudioTable.HeaderCell className={className}>
-        <StudioCheckbox ref={ref} value={value} {...rest} />
+        <StudioCheckbox
+          onKeyDown={handleKeyDown}
+          ref={ref}
+          size='small'
+          tabIndex={-1}
+          value={value}
+          {...rest}
+        />
       </StudioTable.HeaderCell>
     );
   },
 );
 
 HeaderCellCheckbox.displayName = 'HeaderCell.Checkbox';
+
+function handleKeyDown(event: React.KeyboardEvent<HTMLCellInputElement>): void {
+  moveFocus(event);
+}
+
+function moveFocus(event: React.KeyboardEvent<HTMLCellInputElement>) {
+  const nextElement = getNextElement(event);
+  if (nextElement) {
+    event.preventDefault();
+    nextElement.tabIndex = 0;
+    nextElement.focus();
+    event.currentTarget.tabIndex = -1;
+  }
+}
+
+function getNextElement({
+  key,
+  currentTarget,
+}: React.KeyboardEvent<HTMLCellInputElement>): HTMLCellInputElement | null {
+  return getNextInputElement(currentTarget, key);
+}
