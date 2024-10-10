da0bb76a94b249e8bf2ae30e3015e641353a7e81
Konrad-Simso <konrad.simso@digdir.no>
2024-10-10 15:10:19 +0200

    Render hook correctly in test file.

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/Utils/useValidateFileName.test.ts
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/Utils/useValidateFileName.test.ts
@@ -1,26 +1,27 @@
 import { useValidateFileName } from './useValidateFileName';
+import { renderHook } from '@testing-library/react';
 
 const optionIdOne = 'one';
 const optionIdTwo = 'two';
 const options: string[] = [optionIdOne];
+const result = renderHook(() => useValidateFileName(options)).result.current;
 
-const { validateFileName, getDuplicatedOptionIds } = useValidateFileName(options);
 describe('validateFileNameUtils', () => {
   it('should return true for valid filename', () => {
-    expect(validateFileName(optionIdTwo + '.json')).toBe(true);
+    expect(result.validateFileName(optionIdTwo + '.json')).toBe(true);
   });
 
   it('should return false for invalid filename', () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
-    expect(validateFileName('_test.json')).toBe(false);
+    expect(result.validateFileName('_test.json')).toBe(false);
     expect(window.alert).toHaveBeenCalled();
   });
 
   it('should return true for duplicate found', () => {
-    expect(getDuplicatedOptionIds(options, optionIdOne)).toBe(true);
+    expect(result.getDuplicatedOptionIds(options, optionIdOne)).toBe(true);
   });
 
   it('should return false for new value', () => {
-    expect(getDuplicatedOptionIds(options, optionIdTwo)).toBe(false);
+    expect(result.getDuplicatedOptionIds(options, optionIdTwo)).toBe(false);
   });
 });
