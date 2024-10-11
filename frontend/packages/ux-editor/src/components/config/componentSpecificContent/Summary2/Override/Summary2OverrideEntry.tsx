4f0c666ba94eaa42b3be9fb5b5101b6ce4a0dc80
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2024-10-11 14:36:46 +0200

    test

--- frontend/packages/ux-editor/src/components/config/componentSpecificContent/Summary2/Override/Summary2OverrideEntry.tsx
+++ frontend/packages/ux-editor/src/components/config/componentSpecificContent/Summary2/Override/Summary2OverrideEntry.tsx
@@ -33,7 +33,6 @@ export const Summary2OverrideEntry = ({
   const components = Object.values(formLayoutsData).flatMap((layout) =>
     getAllLayoutComponents(layout),
   );
-
   const componentsInGroup = getComponentsInGroup(formLayoutsData);
   const isComponentInGroup = componentsInGroup.includes(override.componentId);
 
