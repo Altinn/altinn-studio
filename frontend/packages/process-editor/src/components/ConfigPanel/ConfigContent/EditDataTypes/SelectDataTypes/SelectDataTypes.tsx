7c2ae8a59fd2f077ef4d6c5537039260e850648e
Nina Kylstad <nkylstad@gmail.com>
2024-10-15 11:48:57 +0200

    chore(process): Inform user of consequences of switching layoutset default datamodel

--- frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/EditDataTypes/SelectDataTypes/SelectDataTypes.tsx
+++ frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/EditDataTypes/SelectDataTypes/SelectDataTypes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Combobox } from '@digdir/designsystemet-react';
-import { StudioButton, StudioDeleteButton } from '@studio/components';
+import { StudioButton, StudioCombobox, StudioDeleteButton } from '@studio/components';
 import { useBpmnApiContext } from '../../../../../contexts/BpmnApiContext';
 import { useTranslation } from 'react-i18next';
 import { XMarkIcon } from '@studio/icons';
@@ -44,26 +43,36 @@ export const SelectDataTypes = ({
 
   return (
     <div className={classes.dataTypeSelectAndButtons}>
-      <Combobox
+      <StudioCombobox
         label={t('process_editor.configuration_panel_set_data_model_label')}
-        value={currentValue && dataModelIds.includes(existingDataType) ? currentValue : undefined}
-        description={t('process_editor.configuration_panel_data_model_selection_description')}
+        value={
+          existingDataType && dataModelOptionsToDisplay.includes(existingDataType)
+            ? currentValue
+            : undefined
+        }
+        description={
+          existingDataType
+            ? t(
+                'process_editor.configuration_panel_data_model_selection_description_existing_model',
+              )
+            : t('process_editor.configuration_panel_data_model_selection_description')
+        }
         size='small'
         className={classes.dataTypeSelect}
       >
-        <Combobox.Empty>
+        <StudioCombobox.Empty>
           {t('process_editor.configuration_panel_no_data_model_to_select')}
-        </Combobox.Empty>
+        </StudioCombobox.Empty>
         {dataModelOptionsToDisplay.map((option) => (
-          <Combobox.Option
+          <StudioCombobox.Option
             value={option}
             key={option}
             onClick={() => handleChangeDataModel([option])}
           >
             {option}
-          </Combobox.Option>
+          </StudioCombobox.Option>
         ))}
-      </Combobox>
+      </StudioCombobox>
       <div className={classes.buttons}>
         <StudioButton
           icon={<XMarkIcon />}
