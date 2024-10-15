c50b0b9819410a041dba4b7c89590a4e3c972859
davidovrelid.com <david@framit.no>
2024-10-15 11:12:20 +0200

    fix eslint

--- frontend/packages/shared/src/contexts/LoggerContext.test.tsx
+++ frontend/packages/shared/src/contexts/LoggerContext.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { LoggerContextProvider, LoggerContextProviderProps } from './LoggerContext';
+import { LoggerContextProvider, type LoggerContextProviderProps } from './LoggerContext';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
 jest.mock('@microsoft/applicationinsights-web', () => {
