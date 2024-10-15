fe614f4ba5ff145c2bd43872b65f2abb3c53be26
davidovrelid.com <david@framit.no>
2024-10-15 10:58:03 +0200

    bug(workspace): fixed bug due to logging application insights from frontend + added unit-tests for it

---
+++ frontend/packages/shared/src/contexts/LoggerContext.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { LoggerContextProvider, LoggerContextProviderProps } from './LoggerContext';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+
+jest.mock('@microsoft/applicationinsights-web', () => {
+  const mockTrackException = jest.fn();
+  return {
+    ApplicationInsights: jest.fn().mockImplementation(() => ({
+      loadAppInsights: jest.fn(),
+      trackException: mockTrackException,
+    })),
+    mockTrackException,
+  };
+});
+
+describe('LoggerContextProvider', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('does not initialize ApplicationInsights without connectionString', () => {
+    const emptyConfig = {};
+    renderLoggerContext({ config: emptyConfig });
+
+    expect(ApplicationInsights).not.toHaveBeenCalled();
+  });
+
+  test('does initialize ApplicationInsights when connectionString is provided', () => {
+    renderLoggerContext({
+      config: {
+        connectionString: 'my-unit-test-connection-string',
+      },
+    });
+
+    expect(ApplicationInsights).toHaveBeenCalled();
+  });
+});
+
+const renderLoggerContext = (props?: Pick<LoggerContextProviderProps, 'config'>): void => {
+  render(
+    <LoggerContextProvider config={{ ...props.config }}>
+      <div>child</div>
+    </LoggerContextProvider>,
+  );
+};
