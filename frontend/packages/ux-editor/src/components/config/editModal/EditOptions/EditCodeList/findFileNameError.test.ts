a594d40715ea1accb255e1f6c2a615888253e19f
Konrad-Simso <konrad.simso@digdir.no>
2024-10-16 16:13:22 +0200

    Update tests

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/findFileNameError.test.ts
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/findFileNameError.test.ts
@@ -8,14 +8,14 @@ const invalidFilename = '_InvalidFileName.json';
 
 describe('validateFileName', () => {
   it('should return true for valid filename', () => {
-    expect(findFileNameError(optionLists, validFilename)).toBe(true);
+    expect(findFileNameError(optionLists, validFilename)).toBe(null);
   });
 
   it('should return false for invalid filename', () => {
-    expect(findFileNameError(optionLists, invalidFilename)).toBe(false);
+    expect(findFileNameError(optionLists, invalidFilename)).toBe('invalidFileName');
   });
 
   it('should return false for duplicate filename', () => {
-    expect(findFileNameError(optionLists, optionListIdOne + '.json')).toBe(false);
+    expect(findFileNameError(optionLists, optionListIdOne + '.json')).toBe('fileExists');
   });
 });
