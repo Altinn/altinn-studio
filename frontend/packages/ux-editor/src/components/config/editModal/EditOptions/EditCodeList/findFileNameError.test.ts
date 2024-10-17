388d011f9eb261916096e73cfcbf142391d5fa4d
Konrad-Simso <konrad.simso@digdir.no>
2024-10-17 08:47:59 +0200

    Update nb.json and edit texts for tests.

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/findFileNameError.test.ts
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditCodeList/findFileNameError.test.ts
@@ -6,16 +6,16 @@ const optionLists: string[] = [optionListIdOne];
 const validFilename = 'two.json';
 const invalidFilename = '_InvalidFileName.json';
 
-describe('validateFileName', () => {
-  it('should return true for valid filename', () => {
+describe('findFileNameError', () => {
+  it('should return null for valid filename', () => {
     expect(findFileNameError(optionLists, validFilename)).toBe(null);
   });
 
-  it('should return false for invalid filename', () => {
+  it('should return "invalidFileName" for invalid filename', () => {
     expect(findFileNameError(optionLists, invalidFilename)).toBe('invalidFileName');
   });
 
-  it('should return false for duplicate filename', () => {
+  it('should return "fileExists" for duplicate filename', () => {
     expect(findFileNameError(optionLists, optionListIdOne + '.json')).toBe('fileExists');
   });
 });
