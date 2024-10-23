671e0e6461e30a9c39845f33f91255f8e9f988cc
Mirko Sekulic <misha.sekulic@gmail.com>
2024-10-23 15:20:09 +0200

    appscopes repository integration tests

---
+++ backend/tests/Designer.Tests/DbIntegrationTests/AppScopesRepository/GetAppScopesAsyncTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+using Altinn.Studio.Designer.Repository.Models.AppScope;
+using Designer.Tests.DbIntegrationTests.AppScopesRepository.Base;
+using Designer.Tests.Fixtures;
+using FluentAssertions;
+using Xunit;
+
+namespace Designer.Tests.DbIntegrationTests.AppScopesRepository;
+
+public class GetAppScopesAsyncTests : AppScopesIntegrationTestsBase
+{
+    public GetAppScopesAsyncTests(DesignerDbFixture dbFixture) : base(dbFixture)
+    {
+    }
+
+    [Theory]
+    [InlineData("org", "nonexistingapp")]
+    public async Task GetAppScopesAsync_NoScopesInDb_ReturnsNull(string org, string app)
+    {
+        var repository = new Altinn.Studio.Designer.Repository.ORMImplementation.AppScopesRepository(DbFixture.DbContext);
+        var result = await repository.GetAppScopesAsync(AltinnRepoContext.FromOrgRepo(org, app));
+
+        result.Should().BeNull();
+    }
+
+
+    [Theory]
+    [MemberData(nameof(GetAsyncTestData))]
+    public async Task GetAppScopesAsync_ReturnExpected(string org, string app, int numberOfScopes)
+    {
+        var entity = EntityGenerationUtils.GenerateAppScopesEntity(org, app, numberOfScopes);
+        await PrepareEntityInDatabaseAsync(entity);
+        var repository = new Altinn.Studio.Designer.Repository.ORMImplementation.AppScopesRepository(DbFixture.DbContext);
+        AppScopesEntity result = await repository.GetAppScopesAsync(AltinnRepoContext.FromOrgRepo(org, app));
+        result.Should().BeEquivalentTo(entity);
+    }
+
+    public static IEnumerable<object[]> GetAsyncTestData()
+    {
+        yield return ["ttd", Guid.NewGuid().ToString(), 3];
+    }
+}
---
+++ backend/tests/Designer.Tests/DbIntegrationTests/AppScopesRepository/SaveAppScopesAsyncIntegrationTests.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Designer.Tests.DbIntegrationTests.AppScopesRepository.Base;
+using Designer.Tests.Fixtures;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Designer.Tests.DbIntegrationTests.AppScopesRepository;
+
+public class SaveAppScopesAsyncIntegrationTests : AppScopesIntegrationTestsBase
+{
+    public SaveAppScopesAsyncIntegrationTests(DesignerDbFixture dbFixture) : base(dbFixture)
+    {
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task SaveAppScopesAsync_ShouldCreateAppScopes_IfNotExists(string org, string app, int numberOfScopes)
+    {
+        var entity = EntityGenerationUtils.GenerateAppScopesEntity(org, app, numberOfScopes);
+        var repository = new Altinn.Studio.Designer.Repository.ORMImplementation.AppScopesRepository(DbFixture.DbContext);
+        await repository.SaveAppScopesAsync(entity);
+
+        var dbRecord = await DbFixture.DbContext.AppScopes.AsNoTracking().FirstOrDefaultAsync(d =>
+            d.Org == org &&
+            d.App == app);
+
+        EntityAssertions.AssertEqual(entity, dbRecord);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task SaveAppScopesAsync_ShouldUpdateAppScopes_IfAlreadyExists(string org, string app, int numberOfScopes)
+    {
+        var entity = EntityGenerationUtils.GenerateAppScopesEntity(org, app, numberOfScopes);
+        await PrepareEntityInDatabaseAsync(entity);
+
+        var dbRecord = await DbFixture.DbContext.AppScopes.AsNoTracking().FirstOrDefaultAsync(d =>
+            d.Org == org &&
+            d.App == app);
+
+        EntityAssertions.AssertEqual(entity, dbRecord);
+
+        entity.Scopes = EntityGenerationUtils.GenerateMaskinPortenScopeEntities(4);
+        var repository = new Altinn.Studio.Designer.Repository.ORMImplementation.AppScopesRepository(DbFixture.DbContext);
+
+        var result = await repository.SaveAppScopesAsync(entity);
+        result.Should().BeEquivalentTo(entity);
+
+        var updatedDbRecord = await DbFixture.DbContext.AppScopes.AsNoTracking().FirstOrDefaultAsync(d =>
+            d.Org == org &&
+            d.App == app);
+
+        updatedDbRecord.Should().NotBeEquivalentTo(dbRecord);
+
+        EntityAssertions.AssertEqual(entity, updatedDbRecord);
+    }
+
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return ["ttd", "repo-" + Guid.NewGuid(), 3];
+    }
+}
