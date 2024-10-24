5b2c8e13dc7c70d6cad1560cd7d803a3f384cc12
JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>
2024-10-24 22:25:47 +0200

    added UI for created a new subform

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/EditLayoutSet.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/EditLayoutSet.tsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DefinedLayoutSet } from './DefinedLayoutSet/DefinedLayoutSet';
 import { SelectLayoutSet } from './SelectLayoutSet/SelectLayoutSet';
-import { StudioRecommendedNextAction } from '@studio/components';
+import {
+  StudioButton,
+  StudioIconTextfield,
+  StudioNativeSelect,
+  StudioProperty,
+  StudioRecommendedNextAction,
+} from '@studio/components';
+import { Paragraph } from '@digdir/designsystemet-react';
+import { CheckmarkIcon, ClipboardIcon, PlusIcon } from '@studio/icons';
+import classes from './EditLayoutSet.module.css';
 
 type EditLayoutSetProps = {
   existingLayoutSetForSubform: string;
@@ -30,19 +39,64 @@ export const EditLayoutSet = ({
   const layoutSetIsUndefined = !existingLayoutSetForSubform;
   if (layoutSetIsUndefined) {
     return (
-      <StudioRecommendedNextAction
-        title={t('ux_editor.component_properties.subform.choose_layout_set_header')}
-        description={t('ux_editor.component_properties.subform.choose_layout_set_description')}
-        hideSaveButton={true}
-        hideSkipButton={true}
-      >
-        <SelectLayoutSet
-          existingLayoutSetForSubForm={existingLayoutSetForSubform}
-          onUpdateLayoutSet={onUpdateLayoutSet}
-          onSetLayoutSetSelectorVisible={setIsLayoutSetSelectorVisible}
-          showButtons={false}
-        />
-      </StudioRecommendedNextAction>
+      <>
+        <StudioRecommendedNextAction
+          title={t('ux_editor.component_properties.subform.choose_layout_set_header')}
+          description={t('ux_editor.component_properties.subform.choose_layout_set_description')}
+          hideSaveButton={true}
+          hideSkipButton={true}
+        >
+          <Paragraph size='sm'>
+            {t('ux_editor.component_properties.subform.create_layout_set_description')}
+          </Paragraph>
+          <SelectLayoutSet
+            existingLayoutSetForSubForm={existingLayoutSetForSubform}
+            onUpdateLayoutSet={onUpdateLayoutSet}
+            onSetLayoutSetSelectorVisible={setIsLayoutSetSelectorVisible}
+            showButtons={false}
+          />
+          {/* TODO: add functionalty */}
+          <StudioProperty.Button
+            className={classes.button}
+            property={t('ux_editor.component_properties.subform.create_layout_set_button')}
+            icon={<PlusIcon />}
+            onClick={undefined}
+          />
+        </StudioRecommendedNextAction>
+
+        {/* TODO:  - Fix align for all  */}
+        <StudioRecommendedNextAction hideSaveButton={true} hideSkipButton={true}>
+          <ClipboardIcon />
+          {/* TODO:  - extract text   -  add functionalty */}
+          <StudioIconTextfield
+            error={undefined}
+            icon={undefined}
+            size='sm'
+            label={t('ux_editor.component_properties.subform.created_layout_set_name')}
+            onChange={undefined}
+            value={undefined}
+          />
+          {/* TODO:  - Add seelctor for datmodell*/}
+
+          <StudioNativeSelect
+            id={undefined}
+            label={t('ux_editor.component_properties.subform.datamodell_binding_layout_set_name')}
+            onChange={undefined}
+            value={undefined}
+            size='sm'
+          />
+
+          {/* TODO:  - extract text   -  add functionalty */}
+          <StudioButton
+            className={classes.savelayoutSetButton}
+            icon={<CheckmarkIcon />}
+            onClick={undefined}
+            title={'Lagreeeeeeeee'}
+            variant='tertiary'
+            color='success'
+          />
+        </StudioRecommendedNextAction>
+      </>
     );
   }
 
