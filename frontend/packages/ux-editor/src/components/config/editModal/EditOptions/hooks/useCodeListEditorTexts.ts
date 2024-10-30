ce873ce6895b414d76d2b22eccd3dce63407e7f6
Erling Hauan <erling.hauan@digdir.no>
2024-10-30 09:09:17 +0100

    Reuse established text keys

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/hooks/useCodeListEditorTexts.ts
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/hooks/useCodeListEditorTexts.ts
@@ -7,16 +7,18 @@ export const useCodeListEditorTexts = () => {
     add: t('ux_editor.modal_new_option'),
     codeList: t('ux_editor.modal_add_options_codelist'),
     delete: t('general.delete'),
-    deleteItem: (number) => t('ux_editor.modal_properties_code_list_delete_item', { number }),
+    deleteItem: (number: number) =>
+      t('ux_editor.modal_properties_code_list_delete_item', { number }),
     description: t('general.description'),
     emptyCodeList: t('ux_editor.modal_properties_code_list_empty'),
-    helpText: t('ux_editor.options_text_help_text'),
-    itemDescription: (number) =>
+    helpText: t('ux_editor.modal_properties_textResourceBindings_help'),
+    itemDescription: (number: number) =>
       t('ux_editor.modal_properties_code_list_item_description', { number }),
-    itemHelpText: (number) => t('ux_editor.modal_properties_code_list_item_helpText', { number }),
-    itemLabel: (number) => t('ux_editor.modal_properties_code_list_item_label', { number }),
-    itemValue: (number) => t('ux_editor.modal_properties_code_list_item_value', { number }),
-    label: t('ux_editor.options_text_label'),
+    itemHelpText: (number: number) =>
+      t('ux_editor.modal_properties_code_list_item_helpText', { number }),
+    itemLabel: (number: number) => t('ux_editor.modal_properties_code_list_item_label', { number }),
+    itemValue: (number: number) => t('ux_editor.modal_properties_code_list_item_value', { number }),
+    label: t('ux_editor.modal_properties_textResourceBindings_title'),
     value: t('general.value'),
   };
 };
