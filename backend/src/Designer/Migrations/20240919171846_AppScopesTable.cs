4adfb65143d1271eb88bd2b156af19024be27d4d
Mirko Sekulic <misha.sekulic@gmail.com>
2024-10-30 09:34:25 +0100

    feat: store maskinporten scopes in db (#13930)

---
+++ backend/src/Designer/Migrations/20240919171846_AppScopesTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Altinn.Studio.Designer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppScopesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "app_scopes",
+                schema: "designer",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    app = table.Column<string>(type: "character varying", nullable: false),
+                    org = table.Column<string>(type: "character varying", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamptz", nullable: false),
+                    scopes = table.Column<string>(type: "jsonb", nullable: false),
+                    created_by = table.Column<string>(type: "character varying", nullable: true),
+                    last_modified_by = table.Column<string>(type: "character varying", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("app_scopes_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_app_scopes_org_app",
+                schema: "designer",
+                table: "app_scopes",
+                columns: new[] { "org", "app" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "app_scopes",
+                schema: "designer");
+        }
+    }
+}
