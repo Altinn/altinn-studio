1ad70d0c1255d8576ab992b84c567bf53244eaae
Erling Hauan <erling.hauan@digdir.no>
2024-10-31 15:53:08 +0100

    Fix PR comments in EditManualOptionsWithEditor

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditManualOptionsWithEditor/EditManualOptionsWithEditor.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/EditManualOptionsWithEditor/EditManualOptionsWithEditor.tsx
@@ -1,11 +1,15 @@
 import React, { useRef } from 'react';
-import { Alert, ErrorMessage } from '@digdir/designsystemet-react';
 import classes from '../EditOptions.module.css';
-import { StudioCodeListEditor, StudioModal, StudioProperty } from '@studio/components';
+import {
+  StudioCodeListEditor,
+  StudioModal,
+  StudioProperty,
+  StudioAlert,
+  StudioErrorMessage,
+} from '@studio/components';
 import type { Option } from 'app-shared/types/Option';
 import { useTranslation } from 'react-i18next';
 import { useCodeListButtonValue, useCodeListEditorTexts } from '../hooks';
-import { useDebounce } from '@studio/hooks';
 import type { EditManualOptionsProps } from '../EditManualOptions';
 import { useComponentErrorMessage } from '@altinn/ux-editor/hooks';
 
@@ -19,23 +23,20 @@ export function EditManualOptionsWithEditor({
   const errorMessage = useComponentErrorMessage(component);
   const buttonValue = useCodeListButtonValue(component.options);
   const editorTexts = useCodeListEditorTexts();
-  const { debounce } = useDebounce({ debounceTimeInMs: 500 });
 
   const handleOptionsChange = (options: Option[]) => {
     if (component.optionsId) {
       delete component.optionsId;
     }
 
-    debounce(() => {
-      handleComponentChange({
-        ...component,
-        options,
-      });
+    handleComponentChange({
+      ...component,
+      options,
     });
   };
 
   if (isLayoutOptionsUnsupported) {
-    return <Alert severity='info'>{t('ux_editor.options.codelist_only')}</Alert>;
+    return <StudioAlert severity='info'>{t('ux_editor.options.codelist_only')}</StudioAlert>;
   }
 
   return (
@@ -45,24 +46,22 @@ export function EditManualOptionsWithEditor({
         property={t('ux_editor.modal_properties_code_list_custom_list')}
         value={buttonValue}
       />
-      <StudioModal.Root>
-        <StudioModal.Dialog
-          ref={manualOptionsModalRef}
-          className={classes.manualTabDialog}
-          closeButtonTitle={t('general.close')}
-          heading={t('ux_editor.modal_add_options_codelist')}
-        >
-          <StudioCodeListEditor
-            codeList={component.options ?? []}
-            onChange={(codeList) => handleOptionsChange(codeList)}
-            texts={editorTexts}
-          />
-        </StudioModal.Dialog>
-      </StudioModal.Root>
+      <StudioModal.Dialog
+        ref={manualOptionsModalRef}
+        className={classes.manualTabDialog}
+        closeButtonTitle={t('general.close')}
+        heading={t('ux_editor.modal_add_options_codelist')}
+      >
+        <StudioCodeListEditor
+          codeList={component.options ?? []}
+          onChange={(codeList) => handleOptionsChange(codeList)}
+          texts={editorTexts}
+        />
+      </StudioModal.Dialog>
       {errorMessage && (
-        <ErrorMessage className={classes.errorMessage} size='small'>
+        <StudioErrorMessage className={classes.errorMessage} size='small'>
           {errorMessage}
-        </ErrorMessage>
+        </StudioErrorMessage>
       )}
     </>
   );
