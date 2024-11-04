f23e1178933bdc6ac2f46a035751a75f59743ed7
andreastanderen <andreastanderen@gmail.com>
2024-11-04 14:36:22 +0100

    Fix PR comments

--- frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/SettingsModal.tsx
+++ frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/SettingsModal.tsx
@@ -3,13 +3,13 @@ import React, { forwardRef, useCallback, useImperativeHandle, useRef, useState }
 import classes from './SettingsModal.module.css';
 import { CogIcon } from '@studio/icons';
 import { StudioModal, StudioContentMenu } from '@studio/components';
-import type { SettingsModalTabId } from 'app-development/types/SettingsModalTabId';
+import type { SettingsModalTabId } from '../../../../../types/SettingsModalTabId';
 import { useTranslation } from 'react-i18next';
 import { PolicyTab } from './components/Tabs/PolicyTab';
 import { AboutTab } from './components/Tabs/AboutTab';
 import { AccessControlTab } from './components/Tabs/AccessControlTab';
 import { SetupTab } from './components/Tabs/SetupTab';
-import { type SettingsModalHandle } from 'app-development/types/SettingsModalHandle';
+import { type SettingsModalHandle } from '../../../../../types/SettingsModalHandle';
 import { useSettingsModalMenuTabConfigs } from './hooks/useSettingsModalMenuTabConfigs';
 
 export const SettingsModal = forwardRef<SettingsModalHandle, {}>(({}, ref): ReactElement => {
