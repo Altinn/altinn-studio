b00afec8678630e92dfe2f4a21f3f435a587c7d7
Tomas <tomas.engebretsen@gmail.com>
2024-11-05 13:47:45 +0100

    Changes based on review

--- frontend/libs/studio-components/src/components/StudioProperty/StudioProperty.stories.tsx
+++ frontend/libs/studio-components/src/components/StudioProperty/StudioProperty.stories.tsx
@@ -1,52 +1,24 @@
-import type { ChangeEvent } from 'react';
-import React, { useEffect } from 'react';
+import React from 'react';
 import type { Meta, StoryFn } from '@storybook/react';
-import { StudioProperty } from './index';
-import { ObjectUtils } from '@studio/pure-functions';
-import { StudioTextfield } from '../StudioTextfield';
-import { StudioButton } from '../StudioButton';
-import { XMarkIcon } from '@studio/icons';
+import { ComposedComponent } from './test-data/ComposedComponent';
+import { buttons } from './test-data/buttons';
+import { Decorator } from './storybook-utils/Decorator';
 
 const meta: Meta = {
   title: 'Components/StudioProperty',
-  component: ComposedPreviewComponent,
+  component: ComposedComponent,
   decorators: [
     (Story) => (
-      <div style={{ width: 500, border: '1px solid #000', padding: 'var(--fds-spacing-5) 0' }}>
+      <Decorator>
         <Story />
-      </div>
+      </Decorator>
     ),
   ],
 };
 
-export const Preview: PreviewStory = (args): React.ReactElement => (
-  <ComposedPreviewComponent {...args} />
-);
+export const Preview: PreviewStory = (args): React.ReactElement => <ComposedComponent {...args} />;
 
-Preview.args = {
-  buttons: [
-    {
-      property: 'Home',
-      value: 'Sweet Home 41, 0000 No Where',
-    },
-    {
-      property: 'Cabin',
-      value: 'Mountain Street, 99999 Snow Place',
-    },
-    {
-      property: 'Work',
-      value: 'Workstation 1, 12345 Office Town',
-    },
-    {
-      property: 'School',
-      value: '',
-    },
-    {
-      property: 'Kindergarten',
-      value: '',
-    },
-  ],
-};
+Preview.args = { buttons };
 
 type PreviewProps = {
   buttons: PreviewButtonProps[];
@@ -58,101 +30,5 @@ type PreviewButtonProps = {
 };
 
 type PreviewStory = StoryFn<PreviewProps>;
-
-function ComposedPreviewComponent({ buttons: givenButtons }: PreviewProps) {
-  const [buttons, setButtons] = React.useState(givenButtons);
-
-  useEffect(() => {
-    setButtons(givenButtons);
-  }, [givenButtons]);
-
-  const handleButtonChange = (index: number) => (value: string) => {
-    setButtons(updateButtonValue(buttons, index, value));
-  };
-
-  return (
-    <StudioProperty.Group>
-      {buttons.map((buttonProps, index) => (
-        <PreviewProperty
-          key={buttonProps.property}
-          onChange={handleButtonChange(index)}
-          property={buttonProps.property}
-          value={buttonProps.value}
-        />
-      ))}
-    </StudioProperty.Group>
-  );
-}
-
-function updateButtonValue(
-  buttons: PreviewButtonProps[],
-  index: number,
-  value: string,
-): PreviewButtonProps[] {
-  const newButtons = ObjectUtils.deepCopy<PreviewButtonProps[]>(buttons);
-  newButtons[index].value = value;
-  return newButtons;
-}
-
-type PreviewPropertyProps = PreviewButtonProps & {
-  onChange: (value: string) => void;
-};
-
-function PreviewProperty({ property, value, onChange }: PreviewPropertyProps): React.ReactElement {
-  const [isEditMode, setIsEditMode] = React.useState(false);
-
-  if (isEditMode) {
-    return (
-      <PreviewFieldset
-        property={property}
-        value={value}
-        onChange={onChange}
-        onClose={() => setIsEditMode(false)}
-      />
-    );
-  } else {
-    return (
-      <StudioProperty.Button
-        property={property}
-        value={value}
-        onClick={() => setIsEditMode(true)}
-      />
-    );
-  }
-}
-
-type PreviewFieldsetProps = PreviewPropertyProps & {
-  onClose: () => void;
-};
-
-function PreviewFieldset({
-  property,
-  value,
-  onChange,
-  onClose,
-}: PreviewFieldsetProps): React.ReactElement {
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value);
-  };
-
-  return (
-    <StudioProperty.Fieldset
-      legend={property}
-      menubar={<StudioButton icon={<XMarkIcon />} onClick={onClose} variant='secondary' />}
-    >
-      <StudioTextfield
-        autoFocus
-        hideLabel
-        label={property}
-        onChange={handleChange}
-        style={{
-          margin:
-            '0 var(--studio-property-fieldset-spacing) var(--studio-property-fieldset-spacing)',
-        }}
-        value={value}
-      />
-    </StudioProperty.Fieldset>
-  );
-}
 
 export default meta;
--- frontend/libs/studio-components/src/components/StudioProperty/StudioPropertyFieldset/StudioPropertyFieldset.stories.tsx
+++ frontend/libs/studio-components/src/components/StudioProperty/StudioPropertyFieldset/StudioPropertyFieldset.stories.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
 import type { Meta, StoryFn } from '@storybook/react';
 import { StudioProperty } from '../index';
-import { StudioTextarea } from '../../StudioTextarea';
 import { StudioButton } from '../../StudioButton';
 import { TrashIcon, FloppydiskIcon, XMarkIcon } from '@studio/icons';
-import { StudioTextfield } from '../../StudioTextfield';
+import { FieldsetContent } from './test-data/FieldsetContent';
 
 type Story = StoryFn<typeof StudioProperty.Fieldset>;
 
 const meta: Meta = {
   title: 'Components/StudioProperty/Fieldset',
   component: StudioProperty.Fieldset,
+  argTypes: {
+    children: {
+      table: { disable: true },
+    },
+    menubar: {
+      table: { disable: true },
+    },
+  },
 };
 export const Preview: Story = (args): React.ReactElement => <StudioProperty.Fieldset {...args} />;
 
 Preview.args = {
   legend: 'Contact details',
-  children: (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 'var(--studio-property-fieldset-spacing)',
-        margin: '0 var(--studio-property-fieldset-spacing) var(--studio-property-fieldset-spacing)',
-      }}
-    >
-      <StudioTextfield label='Name' />
-      <StudioTextarea label='Address' />
-    </div>
-  ),
+  children: <FieldsetContent />,
   menubar: (
     <>
       <StudioButton variant='secondary' color='success' title='Save' icon={<FloppydiskIcon />} />
