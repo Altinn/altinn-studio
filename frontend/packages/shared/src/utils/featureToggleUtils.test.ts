d3ef941106c4838db3cf70a48fa78a5859b4d72b
davidovrelid.com <david@framit.no>
2024-11-05 11:44:25 +0100

    moved webStorage to pure-functions, its not a hook

--- frontend/packages/shared/src/utils/featureToggleUtils.test.ts
+++ frontend/packages/shared/src/utils/featureToggleUtils.test.ts
@@ -1,4 +1,4 @@
-import { typedLocalStorage, typedSessionStorage } from '@studio/components/src/hooks/webStorage';
+import { typedLocalStorage, typedSessionStorage } from '@studio-pure-functions';
 import {
   addFeatureFlagToLocalStorage,
   removeFeatureFlagFromLocalStorage,
--- frontend/packages/ux-editor/src/utils/localStorageUtils.ts
+++ frontend/packages/ux-editor/src/utils/localStorageUtils.ts
@@ -1,4 +1,4 @@
-import { typedLocalStorage } from '@studio/components/src/hooks/webStorage';
+import { typedLocalStorage } from '@studio-pure-functions';
 
 export const cleanupStaleLocalStorageKeys = (): void => {
   // Storage keys that are not supported anymore and to be removed should be added in this array
