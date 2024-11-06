5ab5a7aa7cdae3b37cb0feaef44eeaf4996b0bec
davidovrelid.com <david@framit.no>
2024-11-06 15:20:24 +0100

    fixes

--- frontend/app-development/hooks/useOpenSettingsModalBasedQueryParam.test.ts
+++ frontend/app-development/hooks/useOpenSettingsModalBasedQueryParam.test.ts
@@ -13,11 +13,7 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('useOpenSettingsModalBasedQueryParam', () => {
-<<<<<<< HEAD
   it('should open "settingsModal" if query params has valid tab id', async () => {
-=======
-  it('should open dialog if query params has valid tab id', async () => {
->>>>>>> 41381f181 (feat(settings): make it possible to open settings modal based on query-params)
     const searchParams = buildSearchParams('about');
     setupSearchParamMock(searchParams);
 
@@ -26,16 +22,10 @@ describe('useOpenSettingsModalBasedQueryParam', () => {
 
     renderHookWithProviders()(() => useOpenSettingsModalBasedQueryParam());
     await waitFor(() => expect(openSettingsMock).toHaveBeenCalledWith('about'));
-<<<<<<< HEAD
     expect(openSettingsMock).toHaveBeenCalledTimes(1);
   });
 
   it('should not open "settingsModal" if query params has an invalid tab id', async () => {
-=======
-  });
-
-  it('should not open dialog of query params has invalid tab id', async () => {
->>>>>>> 41381f181 (feat(settings): make it possible to open settings modal based on query-params)
     const searchParams = buildSearchParams('doestNotExistTab');
     setupSearchParamMock(searchParams);
 
@@ -44,10 +34,7 @@ describe('useOpenSettingsModalBasedQueryParam', () => {
 
     renderHookWithProviders()(() => useOpenSettingsModalBasedQueryParam());
     await waitFor(() => expect(openSettingsMock).not.toHaveBeenCalledWith('doestNotExistTab'));
-<<<<<<< HEAD
     expect(openSettingsMock).toHaveBeenCalledTimes(0);
-=======
->>>>>>> 41381f181 (feat(settings): make it possible to open settings modal based on query-params)
   });
 });
 
@@ -61,11 +48,7 @@ function buildSearchParams(queryParamValue: string): URLSearchParams {
   return searchParams;
 }
 
-<<<<<<< HEAD
 function setupUseSettingsModalContextMock(openSettingsMock: typeof jest.fn): jest.Mock {
-=======
-function setupUseSettingsModalContextMock(openSettingsMock: jest.fn): jest.Mock {
->>>>>>> 41381f181 (feat(settings): make it possible to open settings modal based on query-params)
   return (useSettingsModalContext as jest.Mock).mockReturnValue({
     settingsRef: {
       current: {
