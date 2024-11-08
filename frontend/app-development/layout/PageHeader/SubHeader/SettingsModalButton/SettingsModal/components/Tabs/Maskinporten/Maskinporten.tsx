5096c7c1686ea66c4c77fa0ec69706d11c2043bc
davidovrelid.com <david@framit.no>
2024-11-08 11:57:59 +0100

    feat: context based login with ansattporten

--- frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/components/Tabs/Maskinporten/Maskinporten.tsx
+++ frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/components/Tabs/Maskinporten/Maskinporten.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import { useTranslation } from 'react-i18next';
 import { TabContent } from '../../TabContent';
 import { StudioButton, StudioHeading, StudioParagraph, StudioSpinner } from '@studio/components';
@@ -22,6 +23,20 @@ export const Maskinporten = (): React.ReactElement => {
     return <div>View when logged in comes here</div>;
   }
 
+=======
+import { TabContent } from '../../TabContent';
+import { ansattportenLoginPath } from 'app-shared/api/paths';
+import { StudioButton, StudioHeading, StudioParagraph } from '@studio/components';
+import { useTranslation } from 'react-i18next';
+
+export const Maskinporten = (): React.ReactElement => {
+  const { t } = useTranslation();
+
+  const handleLoginWithAnsattporten = (): void => {
+    window.location.href = ansattportenLoginPath();
+  };
+
+>>>>>>> 1f588e11a (feat: context based login with ansattporten)
   return (
     <TabContent>
       <StudioHeading level={2} size='sm' spacing>
--- frontend/packages/shared/src/api/paths.js
+++ frontend/packages/shared/src/api/paths.js
@@ -4,6 +4,9 @@ import { PREVIEW_MOCK_PARTY_ID, PREVIEW_MOCK_INSTANCE_GUID } from '../constants'
 // Base path
 const basePath = '/designer/api';
 
+// Ansattporten
+export const ansattportenLoginPath = () => `${basePath}/ansattporten/login`;
+
 // ApplicationMetadata
 export const appMetadataPath = (org, app) => `${basePath}/${org}/${app}/metadata`; // Get, Put, Post
 export const appMetadataAttachmentPath = (org, app) => `${basePath}/${org}/${app}/metadata/attachment-component`; // Post, Put, Delete
