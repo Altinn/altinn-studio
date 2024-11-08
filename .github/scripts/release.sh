e89ed17b232e8a246f7d44fed6eeeefc71f8238c
Nina Kylstad <nkylstad@gmail.com>
2024-11-08 15:00:01 +0100

    change time, revert test changes

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -54,11 +54,11 @@ echo "New git tag:        $NEW_VERSION"
 echo "Draft:              $DRAFT"
 
 # Create the release
-# curl -L \
-  # -X POST \
-  # -H "Accept: application/vnd.github+json" \
-  # -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  # -H "X-GitHub-Api-Version: 2022-11-28" \
-  # https://api.github.com/repos/altinn/altinn-studio/releases \
-  # -d "{\"tag_name\":\"$NEW_VERSION\",\"name\":\"$NEW_VERSION\",\"draft\":$DRAFT,\"prerelease\":false,\"generate_release_notes\":true}"
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/altinn/altinn-studio/releases \
+  -d "{\"tag_name\":\"$NEW_VERSION\",\"name\":\"$NEW_VERSION\",\"draft\":$DRAFT,\"prerelease\":false,\"generate_release_notes\":true}"
 
