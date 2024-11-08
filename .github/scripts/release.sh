178858d94bf13f5b2bc21ba5e56b61684dc2b8fe
Nina Kylstad <nkylstad@gmail.com>
2024-11-08 11:14:38 +0100

    final changes after test

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -2,12 +2,18 @@
 
 set -e
 set -u
-set -v
+
+DRAFT=true
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     --github-token)
       GITHUB_TOKEN="$2"
+      shift # pop option
+      shift # pop value
+      ;;
+     --draft)
+      DRAFT="$2"
       shift # pop option
       shift # pop value
       ;;
@@ -26,16 +32,12 @@ CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 FIRST_PART=${CURRENT_VERSION_PARTS[0]:1}
 SECOND_PART=${CURRENT_VERSION_PARTS[1]}
-COMMIT_ID=$(git rev-parse HEAD~0)
 YEAR="$(date +"%Y")"
 
-echo "Git tag:       $CURRENT_VERSION"
-echo "First part:    $FIRST_PART"
-echo "Second part:   $SECOND_PART"
-echo "Second part+1: $(($SECOND_PART+1))"
-echo "Commit ID:     $COMMIT_ID"
-echo "-------------------------------------"
-echo "Year:          $YEAR"
+echo "Current git tag:    $CURRENT_VERSION"
+echo "First part:         $FIRST_PART"
+echo "Second part:        $SECOND_PART"
+echo "Current year:       $YEAR"
 echo "-------------------------------------"
 
 
@@ -48,17 +50,15 @@ else
   NEW_VERSION="v${YEAR}.0"
 fi
 
-echo "New version:   $NEW_VERSION"
+echo "New git tag:        $NEW_VERSION"
+echo "Draft:              $DRAFT"
 
-echo "{\"tag_name\":\"$NEW_VERSION\",\"name\":\"${NEW_VERSION}\",\"draft\":true,\"prerelease\":false,\"generate_release_notes\":true}"
-
-
-# TODO: Flip from draft to release when ready and tested.
+# Create the release
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/altinn/altinn-studio/releases \
-  -d "{\"tag_name\":\"$NEW_VERSION\",\"name\":\"${NEW_VERSION}\",\"draft\":true,\"prerelease\":false,\"generate_release_notes\":true}"
+  -d "{\"tag_name\":\"$NEW_VERSION\",\"name\":\"$NEW_VERSION\",\"draft\":$DRAFT,\"prerelease\":false,\"generate_release_notes\":true}"
 
