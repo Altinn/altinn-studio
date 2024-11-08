1fcb0bb84bb7620e934886c4419fa1ae625c3c1e
davidovrelid.com <david@framit.no>
2024-11-08 11:57:59 +0100

    feat: context based login with ansattporten

---
+++ frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/components/Tabs/Maskinporten/Maskinporten.test.tsx
@@ -0,0 +1,5 @@
+describe('Maskinporten', () => {
+  it('should check and verify if the user is logged in', () => {});
+  it('should display information about login, if user is not logged in', () => {});
+  it('should display login with ansattporten if user is not logged in', () => {});
+});
