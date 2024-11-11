e3ff20476c8e47fc1b28d90dbbdb5918fd6588f8
Martin Gunnerud <mgunnerud@gmail.com>
2024-11-11 09:23:37 +0100

    cleanup merge

--- frontend/libs/studio-components/src/components/StudioContentMenu/StudioContentMenu.tsx
+++ frontend/libs/studio-components/src/components/StudioContentMenu/StudioContentMenu.tsx
@@ -1,4 +1,4 @@
-import React, { Children, forwardRef, useEffect, useState } from 'react';
+import React, { Children, forwardRef, useState } from 'react';
 import type { ReactElement, ReactNode } from 'react';
 import { StudioContentMenuBase } from './StudioContentMenuBase';
 
@@ -6,27 +6,18 @@ export type StudioContentMenuProps<TabId extends string> = {
   children: ReactNode;
   selectedTabId: TabId;
   onChangeTab: (tabId: TabId) => void;
-  isControlled?: boolean;
 };
 
 function StudioContentMenuForwarded<TabId extends string>(
-  { children, selectedTabId, onChangeTab, isControlled }: StudioContentMenuProps<TabId>,
+  { children, selectedTabId, onChangeTab }: StudioContentMenuProps<TabId>,
   ref: React.Ref<HTMLDivElement>,
 ): ReactElement {
   const firstTabId = getFirstTabId(children);
   const [selectedTab, setSelectedTab] = useState<TabId>(selectedTabId ?? firstTabId);
 
-  useEffect(() => {
-    if (isControlled) {
-      setSelectedTab(selectedTabId);
-    }
-  }, [selectedTabId, isControlled]);
-
   const handleChangeTab = (tabId: TabId) => {
     onChangeTab(tabId);
-    if (!isControlled) {
-      setSelectedTab(tabId);
-    }
+    setSelectedTab(tabId);
   };
 
   return (
