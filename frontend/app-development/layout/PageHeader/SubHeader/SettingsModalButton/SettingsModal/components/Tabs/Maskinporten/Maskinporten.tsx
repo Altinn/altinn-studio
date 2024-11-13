b8640ab6577a5c34027db05045960f744ca01a6b
wrt95 <wrt_95@hotmail.com>
2024-11-13 15:16:17 +0100

    feat: Message when no scope is available

--- frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/components/Tabs/Maskinporten/Maskinporten.tsx
+++ frontend/app-development/layout/PageHeader/SubHeader/SettingsModalButton/SettingsModal/components/Tabs/Maskinporten/Maskinporten.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './Maskinporten.module.css';
 import { useTranslation } from 'react-i18next';
 import { TabContent } from '../../TabContent';
-import { StudioButton, StudioHeading, StudioParagraph, StudioSpinner } from '@studio/components';
+import {
+  StudioAlert,
+  StudioButton,
+  StudioHeading,
+  StudioParagraph,
+  StudioSpinner,
+} from '@studio/components';
 import { useIsLoggedInWithAnsattportenQuery } from '../../../../../../../../hooks/queries/useIsLoggedInWithAnsattportenQuery';
 
 export const Maskinporten = (): React.ReactElement => {
+  const [scopes, setScopes] = useState(undefined);
   const { data: isLoggedInWithAnsattporten, isPending: isPendingAuthStatus } =
     useIsLoggedInWithAnsattportenQuery();
 
@@ -12,6 +20,7 @@ export const Maskinporten = (): React.ReactElement => {
 
   const handleLoginWithAnsattporten = (): void => {
     console.log('Will be implemented in next iteration when backend is ready');
+    setScopes([]);
   };
 
   if (isPendingAuthStatus) {
@@ -31,6 +40,11 @@ export const Maskinporten = (): React.ReactElement => {
       <StudioButton onClick={handleLoginWithAnsattporten}>
         {t('settings_modal.maskinporten_tab_login_with_ansattporten')}
       </StudioButton>
+      {scopes && scopes.length === 0 && (
+        <StudioAlert severity='info' className={classes.noScopeAlert}>
+          Du har ingen scopes lagt til i Maskinporten, så du kan ikke velge scope her.
+        </StudioAlert>
+      )}
     </TabContent>
   );
 };
