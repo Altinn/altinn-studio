fce3b3add847d697ac20169ef1bbcc565fccd972
lassopicasso <lars.walderhaug@digdir.no>
2024-11-13 15:51:22 +0100

    tests for createNewSubformLayoutset

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/CreateNewSubformLayoutSet/CreateNewSubformLayoutSet.test.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/CreateNewSubformLayoutSet/CreateNewSubformLayoutSet.test.tsx
@@ -3,14 +3,8 @@ import { renderWithProviders } from '../../../../../../testing/mocks';
 import { CreateNewSubformLayoutSet } from './CreateNewSubformLayoutSet';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import { screen } from '@testing-library/react';
-import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
-import { app, org } from '@studio/testing/testids';
-import { QueryKey } from 'app-shared/types/QueryKey';
 import { layoutSets } from 'app-shared/mocks/mocks';
 import userEvent from '@testing-library/user-event';
-import { AppContext } from '../../../../../../AppContext';
-import { appContextMock } from '../../../../../../testing/appContextMock';
-import { layoutSetsMock } from '@altinn/ux-editor/testing/layoutSetsMock';
 
 const onUpdateLayoutSetMock = jest.fn();
 const setShowCreateSubformCardMock = jest.fn();
@@ -33,37 +27,28 @@ jest.mock('./SubformDataModelSelect', () => ({
 describe('CreateNewSubformLayoutSet ', () => {
   afterEach(jest.clearAllMocks);
 
-  it('displays the card with label and input field', () => {
+  it('displays the card with label, input field and data model select', () => {
     renderCreateNewSubformLayoutSet();
-    const card = screen.getByRole('textbox', {
-      name: textMock('ux_editor.component_properties.subform.created_layout_set_name'),
-    });
-
-    expect(card).toBeInTheDocument();
-  });
-
-  it('displays the data model select', async () => {
-    renderCreateNewSubformLayoutSet();
+    const subformNameInput = screen.getByRole('textbox');
     const dataModelSelect = screen.getByRole('combobox');
+
+    expect(subformNameInput).toBeInTheDocument();
     expect(dataModelSelect).toBeInTheDocument();
   });
 
-  it('displays the save button', () => {
+  it('displays the save button and close button', () => {
     renderCreateNewSubformLayoutSet();
-    const saveButton = screen.getByRole('button', { name: textMock('general.close') });
+    const saveButton = screen.getByRole('button', { name: textMock('general.save') });
+    const closeButton = screen.getByRole('button', { name: textMock('general.close') });
+
     expect(saveButton).toBeInTheDocument();
+    expect(closeButton).toBeInTheDocument();
   });
 
-  it('displays the cancel button', () => {
-    renderCreateNewSubformLayoutSet();
-    const cancelButton = screen.getByRole('button', { name: textMock('general.close') });
-    expect(cancelButton).toBeInTheDocument();
-  });
-
-  it('displays not the cancel button when hasSubforms is false', () => {
+  it('displays not the close button when hasSubforms is false', () => {
     renderCreateNewSubformLayoutSet({ hasSubforms: false });
-    const cancelButton = screen.queryByRole('button', { name: textMock('general.close') });
-    expect(cancelButton).not.toBeInTheDocument();
+    const closeButton = screen.queryByRole('button', { name: textMock('general.close') });
+    expect(closeButton).not.toBeInTheDocument();
   });
 
   it('calls onSubformCreated when save button is clicked', async () => {
@@ -79,31 +64,26 @@ describe('CreateNewSubformLayoutSet ', () => {
     expect(onUpdateLayoutSetMock).toHaveBeenCalledWith('NewSubform');
   });
 
-  it('disables the save button when input is invalid', async () => {
+  it('disables the save button when input is invalid and data model is valid', async () => {
     const user = userEvent.setup();
     renderCreateNewSubformLayoutSet();
-
-    const saveButton = screen.getByRole('button', { name: textMock('general.save') });
-    expect(saveButton).toBeDisabled();
-
-    const input = screen.getByRole('textbox');
-
-    await user.type(input, 'æøå');
-    expect(saveButton).toBeDisabled();
-
-    await user.clear(input);
-    await user.type(input, 'e re a');
-    expect(saveButton).toBeDisabled();
-
-    await user.clear(input);
-    await user.type(input, 'NewSubform');
 
     const dataModelSelect = screen.getByRole('combobox');
     await user.selectOptions(dataModelSelect, ['moped']);
+
+    const saveButton = screen.getByRole('button', { name: textMock('general.save') });
+    expect(saveButton).toBeDisabled();
+
+    const input = screen.getByRole('textbox');
+    await user.type(input, 'æøå');
+    expect(saveButton).toBeDisabled();
+    await user.clear(input);
+    await user.type(input, 'NewSubform');
+
     expect(saveButton).not.toBeDisabled();
   });
 
-  it('disables the save button when the input is valid and no data model is selected', async () => {
+  it('disables the save button when the input is valid and data model is invalid', async () => {
     const user = userEvent.setup();
     renderCreateNewSubformLayoutSet();
 
@@ -114,7 +94,7 @@ describe('CreateNewSubformLayoutSet ', () => {
     expect(saveButton).toBeDisabled();
   });
 
-  it('does not disable the save button when the input is valid and a data model is selected', async () => {
+  it('enables save button when both input and data model is valid', async () => {
     const user = userEvent.setup();
     renderCreateNewSubformLayoutSet();
 
@@ -132,17 +112,12 @@ describe('CreateNewSubformLayoutSet ', () => {
 const renderCreateNewSubformLayoutSet = ({
   hasSubforms = true,
 }: { hasSubforms?: boolean } = {}) => {
-  const queryClient = createQueryClientMock();
-  queryClient.setQueryData([QueryKey.LayoutSets, org, app], layoutSetsMock);
   return renderWithProviders(
-    <AppContext.Provider value={{ ...appContextMock }}>
-      <CreateNewSubformLayoutSet
-        onUpdateLayoutSet={onUpdateLayoutSetMock}
-        layoutSets={layoutSets}
-        setShowCreateSubformCard={setShowCreateSubformCardMock}
-        hasSubforms={hasSubforms}
-      />
-    </AppContext.Provider>,
-    { queryClient },
+    <CreateNewSubformLayoutSet
+      onUpdateLayoutSet={onUpdateLayoutSetMock}
+      layoutSets={layoutSets}
+      setShowCreateSubformCard={setShowCreateSubformCardMock}
+      hasSubforms={hasSubforms}
+    />,
   );
 };
