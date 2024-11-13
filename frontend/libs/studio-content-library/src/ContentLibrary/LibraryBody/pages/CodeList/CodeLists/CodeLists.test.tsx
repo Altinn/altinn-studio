d57dd30a7546d7064ff5d7da5d53ea70c46c5b86
andreastanderen <andreastanderen@gmail.com>
2024-11-13 15:51:23 +0100

    Render StudioCodeListEditor inside each code list accordion

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeList/CodeLists/CodeLists.test.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeList/CodeLists/CodeLists.test.tsx
@@ -4,6 +4,7 @@ import type { CodeListsProps } from './CodeLists';
 import { CodeLists } from './CodeLists';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import type { CodeListWithMetadata } from '../CodeList';
+import type { UserEvent } from '@testing-library/user-event';
 import userEvent from '@testing-library/user-event';
 
 const codeListName = 'codeList';
@@ -11,6 +12,7 @@ const codeListMock: CodeListWithMetadata = {
   title: codeListName,
   codeList: [{ value: 'value', label: 'label' }],
 };
+const onUpdateCodeListMock = jest.fn();
 
 describe('CodeLists', () => {
   it('renders the code list', () => {
@@ -19,20 +21,37 @@ describe('CodeLists', () => {
     expect(codeListAccordion).toBeInTheDocument();
   });
 
-  it('renders a placeholder information alert when opening the accordion', async () => {
+  it('renders the code list editor when opening the accordion', async () => {
     const user = userEvent.setup();
     renderCodeLists();
-    const codeListAccordion = screen.getByRole('button', { name: codeListName });
-    await user.click(codeListAccordion);
-    const placeholderAlert = screen.getByText(
-      textMock('app_content_library.code_lists.edit_code_list_placeholder_text'),
+    await openCodeList(user);
+    const codeListEditor = screen.getByText(textMock('code_list_editor.legend'));
+    expect(codeListEditor).toBeVisible();
+  });
+
+  it('calls onUpdateCodeList when changing a code list', async () => {
+    const user = userEvent.setup();
+    renderCodeLists();
+    await openCodeList(user);
+    const codeListFirstItemValue = screen.getByLabelText(
+      textMock('code_list_editor.value_item', { number: 1 }),
     );
-    expect(placeholderAlert).toBeVisible();
+    await user.type(codeListFirstItemValue, 'Test');
+    expect(onUpdateCodeListMock).toHaveBeenCalledTimes(4);
+    expect(onUpdateCodeListMock).toHaveBeenLastCalledWith({
+      codeList: [expect.objectContaining({ value: 'Test' })],
+      title: codeListName,
+    });
   });
 });
+
+const openCodeList = async (user: UserEvent) => {
+  const codeListAccordion = screen.getByRole('button', { name: codeListName });
+  await user.click(codeListAccordion);
+};
 
 const renderCodeLists = (
   { codeLists }: Partial<CodeListsProps> = { codeLists: [codeListMock] },
 ) => {
-  render(<CodeLists codeLists={codeLists} onUpdateCodeList={jest.fn()} />);
+  render(<CodeLists codeLists={codeLists} onUpdateCodeList={onUpdateCodeListMock} />);
 };
