6d8e0d5502383d1a7f16277a248b271c1e084d78
Tomas <tomas.engebretsen@gmail.com>
2024-11-18 14:25:38 +0100

    fix: Support tabbing inside input table

--- frontend/libs/studio-components/src/components/StudioInputTable/dom-utils/activateTabbingOnFirstInputElement.ts
+++
@@ -1,9 +0,0 @@
-import type { HTMLCellInputElement } from '../types/HTMLCellInputElement';
-
-export function activateTabbingOnFirstInputElement(table: HTMLTableElement) {
-  getFirstInputElement(table)?.setAttribute('tabindex', '0');
-}
-
-function getFirstInputElement(table: HTMLTableElement): HTMLCellInputElement | null {
-  return table?.querySelector('input, textarea, button');
-}
