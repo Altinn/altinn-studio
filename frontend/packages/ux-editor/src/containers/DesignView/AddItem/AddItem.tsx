fbee706e06acca139677714522fa1f14689df988
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2024-11-18 08:09:35 +0100

    test searchbox

--- frontend/packages/ux-editor/src/containers/DesignView/AddItem/AddItem.tsx
+++ frontend/packages/ux-editor/src/containers/DesignView/AddItem/AddItem.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {
   addItemOfType,
+  getAvailableChildComponentsForContainer,
   getDefaultChildComponentsForContainer,
   getItem,
 } from '../../../utils/formLayoutUtils';
 import { useAddItemToLayoutMutation } from '../../../hooks/mutations/useAddItemToLayoutMutation';
 import { useFormItemContext } from '../../FormItemContext';
-import { useAppContext } from '../../../hooks';
+import { useAppContext, useFormLayouts } from '../../../hooks';
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 import type { IInternalLayout } from '../../../types/global';
 import type { ComponentType, CustomComponentType } from 'app-shared/types/ComponentType';
-import { StudioButton } from '@studio/components';
+import { StudioButton, StudioTextfield } from '@studio/components';
 import type { AddedItem } from './types';
 import { PlusIcon } from '@studio/icons';
 import { usePreviewContext } from 'app-development/contexts/PreviewContext';
@@ -18,6 +19,7 @@ import { DefaultItems } from './DefaultItems';
 import { AddItemModal } from './AddItemModal';
 import { BASE_CONTAINER_ID } from 'app-shared/constants';
 import classes from './AddItem.module.css';
+import { generateComponentId } from '../../../utils/generateId';
 
 export type AddItemProps = {
   containerId: string;
@@ -27,6 +29,11 @@ export type AddItemProps = {
 export const AddItem = ({ containerId, layout }: AddItemProps) => {
   const [selectedItem, setSelectedItem] = React.useState<AddedItem | null>(null);
   const [showDefaultComponents, setShowDefaultComponents] = React.useState(false);
+  const [defaultComponents, setDefaultComponents] = React.useState(
+    getDefaultChildComponentsForContainer(layout, containerId),
+  );
+  const shouldShowAllComponentsButton = defaultComponents.length > 8;
+  const layouts = useFormLayouts();
 
   const { doReloadPreview } = usePreviewContext();
   const { handleEdit } = useFormItemContext();
@@ -76,8 +83,22 @@ export const AddItem = ({ containerId, layout }: AddItemProps) => {
     setShowDefaultComponents(false);
   };
 
-  const defaultComponents = getDefaultChildComponentsForContainer(layout, containerId);
-  const shouldShowAllComponentsButton = defaultComponents.length > 8;
+  function updateComponentSearch(search: React.ChangeEvent<HTMLInputElement>) {
+    if (!search.target.value) {
+      setDefaultComponents(getDefaultChildComponentsForContainer(layout, containerId));
+      return;
+    }
+    const allComponets = Object.values(
+      getAvailableChildComponentsForContainer(layout, containerId),
+    ).flat();
+    setDefaultComponents(
+      allComponets
+        .filter((component) => {
+          return component.label.toLowerCase().includes(search.target.value.toLowerCase());
+        })
+        .slice(0, 8),
+    );
+  }
 
   console.log('Show default components', showDefaultComponents);
 
@@ -102,24 +123,41 @@ export const AddItem = ({ containerId, layout }: AddItemProps) => {
         </StudioButton>
       )}
       {showDefaultComponents && (
-        <div className={classes.addItemButtons}>
-          <DefaultItems
-            item={selectedItem}
-            setItem={setSelectedItem}
-            onAddItem={onAddComponent}
-            onCancel={handleHideDefaultComponents}
-            availableComponents={defaultComponents}
-            showAllButton={
-              shouldShowAllComponentsButton && (
-                <AddItemModal
-                  containerId={containerId}
-                  layout={layout}
-                  onAddComponent={onAddComponent}
-                />
-              )
-            }
+        <>
+          <StudioTextfield
+            autoFocus={true}
+            onChange={updateComponentSearch}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                const firstComponent = defaultComponents[0];
+                const addedItem: AddedItem = {
+                  componentType: firstComponent.type,
+                  componentId: generateComponentId(firstComponent.type as ComponentType, layouts),
+                };
+                onAddComponent(addedItem);
+              }
+            }}
+            placeholder='Søk etter komponent'
           />
-        </div>
+          <div className={classes.addItemButtons}>
+            <DefaultItems
+              item={selectedItem}
+              setItem={setSelectedItem}
+              onAddItem={onAddComponent}
+              onCancel={handleHideDefaultComponents}
+              availableComponents={defaultComponents}
+              showAllButton={
+                shouldShowAllComponentsButton && (
+                  <AddItemModal
+                    containerId={containerId}
+                    layout={layout}
+                    onAddComponent={onAddComponent}
+                  />
+                )
+              }
+            />
+          </div>
+        </>
       )}
     </div>
   );
