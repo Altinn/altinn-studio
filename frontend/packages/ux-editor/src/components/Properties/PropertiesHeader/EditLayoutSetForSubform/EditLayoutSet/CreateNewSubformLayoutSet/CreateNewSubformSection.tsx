a8112970f6d02269fed8e00374406879007693c1
lassopicasso <lars.walderhaug@digdir.no>
2024-11-21 07:44:42 +0100

    refactor createNewSubformSection

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/CreateNewSubformLayoutSet/CreateNewSubformSection.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/CreateNewSubformLayoutSet/CreateNewSubformSection.tsx
@@ -1,124 +1,115 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StudioButton, StudioCard, StudioSpinner, StudioTextfield } from '@studio/components';
-import { TrashIcon, CheckmarkIcon } from '@studio/icons';
-import classes from './CreateNewSubformLayoutSet.module.css';
-import { SubformDataModelSelect } from './SubformDataModelSelect';
+import {
+  StudioCard,
+  StudioParagraph,
+  StudioRecommendedNextAction,
+  StudioTextfield,
+} from '@studio/components';
 import { useValidateLayoutSetName } from 'app-shared/hooks/useValidateLayoutSetName';
-import { useCreateSubform } from '@altinn/ux-editor/hooks/useCreateSubform';
 import type { LayoutSets } from 'app-shared/types/api/LayoutSetsResponse';
-import { CreateNewDataModel } from './CreateNewDataModel';
+import { SubformDataModel } from './SubformDataModel';
+import { CreateNewSubformButtons } from './CreateNewSubformButtons';
+import { SubformInstructions } from '../SubformInstructions';
+import { useCreateSubform } from '@altinn/ux-editor/hooks/useCreateSubform';
 
-type CreateNewSubformLayoutSetProps = {
-  onUpdateLayoutSet: (layoutSetName: string) => void;
+type CreateNewSubformSectionProps = {
   layoutSets: LayoutSets;
   setShowCreateSubformCard: (showCreateSubform: boolean) => void;
   hasSubforms: boolean;
+  recommendedNextActionText: {
+    title: string;
+    description: string;
+  };
+  onComponentUpdate: (subform: string) => void;
 };
 
-export const CreateNewSubformLayoutSet = ({
-  onUpdateLayoutSet,
+export const CreateNewSubformSection = ({
   layoutSets,
   setShowCreateSubformCard,
   hasSubforms,
-}: CreateNewSubformLayoutSetProps): React.ReactElement => {
+  recommendedNextActionText: { title, description },
+  onComponentUpdate,
+}: CreateNewSubformSectionProps): React.ReactElement => {
   const { t } = useTranslation();
-  const [newSubform, setNewSubform] = useState('');
-  const [selectedDataType, setSelectedDataType] = useState<string>();
   const { validateLayoutSetName } = useValidateLayoutSetName();
+  const [nameError, setNameError] = useState<string>('');
+  const [dataModel, setDataModel] = useState<string>('');
+  const [displayDataModelInput, setDisplayDataModelInput] = useState(false);
   const { createSubform, isPendingLayoutSetMutation } = useCreateSubform();
-  const [nameError, setNameError] = useState('');
 
-  const [selectDisabled, setSelectDisabled] = useState(false);
-  const [showNewDataModelInput, setShowNewDataModelInput] = useState(false);
-  const [newDataModelName, setNewDataModelName] = useState('');
-
-  function handleChange(subformName: string) {
+  const handleSubformName = (subformName: string) => {
     const subformNameValidation = validateLayoutSetName(subformName, layoutSets);
     setNameError(subformNameValidation);
-    setNewSubform(subformName);
-  }
+  };
 
-  function handleCreateSubform() {
-    createSubform({
-      layoutSetName: newSubform,
-      onSubformCreated: onUpdateLayoutSet,
-      dataType: selectedDataType,
-    });
-  }
+  const handleDataModel = (dataModelId: string) => {
+    // Add data model validation here when this PR is merged: https://github.com/Altinn/altinn-studio/issues/13364
+    setDataModel(dataModelId);
+  };
 
-  function handleNewDataModelNameChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setNewDataModelName(e.target.value);
-  }
-
-  const handleClose = () => {
-    if (selectDisabled) {
-      setShowNewDataModelInput(false);
-      setSelectDisabled(false);
-      setNewDataModelName('');
+  const handleCloseButton = () => {
+    if (displayDataModelInput) {
+      setDataModel('');
+      setDisplayDataModelInput(false);
     } else {
       setShowCreateSubformCard(false);
     }
   };
 
-  const saveIcon = isPendingLayoutSetMutation ? (
-    <StudioSpinner size='sm' spinnerTitle={t('general.loading')} />
-  ) : (
-    <CheckmarkIcon />
-  );
+  const handleCreateSubformSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const formData: FormData = new FormData(e.currentTarget);
+    const newSubformName = formData.get('subform') as string;
+    const subformDataType = formData.get('subformDataType') as string;
 
-  function handleNewDataModelClick() {
-    setShowNewDataModelInput(true);
-    setSelectDisabled(true);
-  }
-
-  function isSaveButtonDisabled(): boolean {
-    if (showNewDataModelInput) return !newDataModelName;
-    return !newSubform || !!nameError || !selectedDataType;
-  }
+    createSubform({
+      layoutSetName: newSubformName,
+      onSubformCreated: onComponentUpdate,
+      dataType: subformDataType,
+    });
+  };
 
   return (
-    <StudioCard>
-      <StudioCard.Content>
-        <StudioTextfield
-          label={t('ux_editor.component_properties.subform.created_layout_set_name')}
-          value={newSubform}
-          size='sm'
-          disabled={isPendingLayoutSetMutation}
-          onChange={(e) => handleChange(e.target.value)}
-          error={nameError}
-        />
-        <SubformDataModelSelect
-          selectedDataType={selectedDataType}
-          setSelectedDataType={setSelectedDataType}
-          disabled={selectDisabled}
-        />
-        <CreateNewDataModel
-          showNewDataModelInput={showNewDataModelInput}
-          newDataModelName={newDataModelName}
-          onNewDataModelNameChange={handleNewDataModelNameChange}
-          onNewDataModelClick={handleNewDataModelClick}
-        />
-        <div className={classes.buttonGroup}>
-          <StudioButton
-            icon={saveIcon}
-            onClick={handleCreateSubform}
-            title={t('general.save')}
-            disabled={isSaveButtonDisabled()}
-            variant='secondary'
-            color='success'
+    <StudioRecommendedNextAction
+      title={t(title)}
+      description={t(description)}
+      hideSaveButton={true}
+      hideSkipButton={true}
+      onSave={handleCreateSubformSubmit}
+    >
+      {!hasSubforms && (
+        <>
+          <StudioParagraph size='sm'>
+            {t('ux_editor.component_properties.subform.no_existing_layout_set_empty_subform')}
+          </StudioParagraph>
+          <SubformInstructions />
+        </>
+      )}
+      <StudioCard>
+        <StudioCard.Content>
+          <StudioTextfield
+            name='subform'
+            label={t('ux_editor.component_properties.subform.created_layout_set_name')}
+            size='sm'
+            disabled={isPendingLayoutSetMutation}
+            onChange={(e) => handleSubformName(e.target.value)}
+            error={nameError}
           />
-          {(hasSubforms || selectDisabled) && (
-            <StudioButton
-              onClick={handleClose}
-              title={t('general.close')}
-              icon={<TrashIcon />}
-              variant='secondary'
-              color='danger'
-            />
-          )}
-        </div>
-      </StudioCard.Content>
-    </StudioCard>
+          <SubformDataModel
+            handleDataModel={handleDataModel}
+            setDisplayDataModelInput={setDisplayDataModelInput}
+            setDataModel={setDataModel}
+            displayDataModelInput={displayDataModelInput}
+          />
+          <CreateNewSubformButtons
+            isPendingLayoutSetMutation={isPendingLayoutSetMutation}
+            disableSaveButton={!!nameError || !dataModel}
+            displayCloseButton={hasSubforms || displayDataModelInput}
+            handleCloseButton={handleCloseButton}
+          />
+        </StudioCard.Content>
+      </StudioCard>
+    </StudioRecommendedNextAction>
   );
 };
