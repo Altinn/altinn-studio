ad39290455a8473eaa6ba914de8696a40c84bb51
lassopicasso <lars.walderhaug@digdir.no>
2024-11-21 07:43:43 +0100

    move create subform buttons to own file

---
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/EditLayoutSetForSubform/EditLayoutSet/CreateNewSubformLayoutSet/CreateNewSubformButtons.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import classes from './CreateNewSubformButtons.module.css';
+import { CheckmarkIcon, TrashIcon } from '@studio/icons';
+import { StudioButton, StudioSpinner } from '@studio/components';
+import { useTranslation } from 'react-i18next';
+
+type CreateNewSubformButtonsProps = {
+  isPendingLayoutSetMutation: boolean;
+  disableSaveButton: boolean;
+  displayCloseButton: boolean;
+  handleCloseButton: () => void;
+};
+
+export const CreateNewSubformButtons = ({
+  isPendingLayoutSetMutation,
+  disableSaveButton,
+  displayCloseButton,
+  handleCloseButton,
+}: CreateNewSubformButtonsProps) => {
+  const { t } = useTranslation();
+
+  const saveIcon = isPendingLayoutSetMutation ? (
+    <StudioSpinner size='sm' spinnerTitle={t('general.loading')} />
+  ) : (
+    <CheckmarkIcon />
+  );
+
+  return (
+    <div className={classes.buttonGroup}>
+      <StudioButton
+        icon={saveIcon}
+        type='submit'
+        title={t('general.save')}
+        disabled={disableSaveButton}
+        variant='secondary'
+        color='success'
+      />
+      {displayCloseButton && (
+        <StudioButton
+          onClick={handleCloseButton}
+          title={t('general.close')}
+          icon={<TrashIcon />}
+          variant='secondary'
+          color='danger'
+        />
+      )}
+    </div>
+  );
+};
