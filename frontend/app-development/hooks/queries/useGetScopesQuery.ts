cd8560be4db3e5a3c112276b47f80e48354d5c60
William Thorenfeldt <48119543+wrt95@users.noreply.github.com>
2024-11-27 09:33:06 +0100

    chore: connect get all scopes (#14176)
    Co-authored-by: davidovrelid.com <david@framit.no>
    Co-authored-by: David Ovrelid <46874830+framitdavid@users.noreply.github.com>
    Co-authored-by: Mirko Sekulic <misha.sekulic@gmail.com>

--- frontend/app-development/hooks/queries/useGetScopesQuery.ts
+++ frontend/app-development/hooks/queries/useGetScopesQuery.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { QueryKey } from 'app-shared/types/QueryKey';
 import { useServicesContext } from 'app-shared/contexts/ServicesContext';
-import { type MaskinportenScope } from 'app-shared/types/MaskinportenScope';
+import { type MaskinportenScopes } from 'app-shared/types/MaskinportenScope';
+import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 
 export const useGetScopesQuery = () => {
+  const { org, app } = useStudioEnvironmentParams();
   const { getMaskinportenScopes } = useServicesContext();
-  return useQuery<MaskinportenScope[]>({
+  return useQuery<MaskinportenScopes>({
     queryKey: [QueryKey.AppScopes],
-    queryFn: () => getMaskinportenScopes(),
+    queryFn: () => getMaskinportenScopes(org, app),
   });
 };
