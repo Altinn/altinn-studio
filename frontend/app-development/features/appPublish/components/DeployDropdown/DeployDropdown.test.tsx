f24cc17d67edc5718ebface3c7df932fad5e37bb
wrt95 <wrt_95@hotmail.com>
2024-11-27 14:27:23 +0100

    adding tests

--- frontend/app-development/features/appPublish/components/DeployDropdown/DeployDropdown.test.tsx
+++ frontend/app-development/features/appPublish/components/DeployDropdown/DeployDropdown.test.tsx
@@ -60,246 +60,138 @@ const imageOptions = [
 ];
 
 describe('DeployDropdown', () => {
-  describe('Dropdown', () => {
-    afterEach(jest.clearAllMocks);
+  afterEach(jest.clearAllMocks);
 
-    it('renders a spinner while loading data', () => {
-      render();
+  it('renders a spinner while loading data', () => {
+    render();
 
-      expect(screen.getByTitle(textMock('app_deployment.releases_loading'))).toBeInTheDocument();
-    });
+    expect(screen.getByTitle(textMock('app_deployment.releases_loading'))).toBeInTheDocument();
+  });
 
-    it('renders an error message if an error occurs while loading data', async () => {
-      render(
-        {},
-        {
-          getAppReleases: jest.fn().mockImplementation(() => Promise.reject()),
-        },
-      );
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+  it('renders an error message if an error occurs while loading data', async () => {
+    render(
+      {},
+      {
+        getAppReleases: jest.fn().mockImplementation(() => Promise.reject()),
+      },
+    );
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      expect(screen.getByText(textMock('app_deployment.releases_error'))).toBeInTheDocument();
-    });
+    expect(screen.getByText(textMock('app_deployment.releases_error'))).toBeInTheDocument();
+  });
 
-    it('render no image options message when image options are empty', async () => {
-      const user = userEvent.setup();
+  it('render no image options message when image options are empty', async () => {
+    const user = userEvent.setup();
 
-      render(
-        {},
-        {
-          getAppReleases: jest.fn().mockImplementation(() =>
-            Promise.resolve({
-              results: [],
-            }),
-          ),
-        },
-      );
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+    render(
+      {},
+      {
+        getAppReleases: jest.fn().mockImplementation(() =>
+          Promise.resolve({
+            results: [],
+          }),
+        ),
+      },
+    );
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      const select = await screen.findByLabelText(textMock('app_deployment.choose_version'));
-      await user.click(select);
+    const select = await screen.findByLabelText(textMock('app_deployment.choose_version'));
+    await user.click(select);
 
-      expect(screen.getByText(textMock('app_deployment.no_versions'))).toBeInTheDocument();
-    });
+    expect(screen.getByText(textMock('app_deployment.no_versions'))).toBeInTheDocument();
+  });
 
-    it('renders image options', async () => {
-      const user = userEvent.setup();
+  it('renders image options', async () => {
+    const user = userEvent.setup();
 
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+    render();
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      const select = screen.getByLabelText(textMock('app_deployment.choose_version'));
-      await user.click(select);
+    const select = screen.getByLabelText(textMock('app_deployment.choose_version'));
+    await user.click(select);
 
-      expect(screen.getByRole('option', { name: imageOptions[0].label })).toBeInTheDocument();
-      expect(screen.getByRole('option', { name: imageOptions[1].label })).toBeInTheDocument();
-    });
+    expect(screen.getByRole('option', { name: imageOptions[0].label })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: imageOptions[1].label })).toBeInTheDocument();
+  });
 
-    it('selects default image option', async () => {
-      render({ selectedImageTag: imageOptions[0].value });
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+  it('selects default image option', async () => {
+    render({ selectedImageTag: imageOptions[0].value });
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      expect(screen.getByRole('combobox')).toHaveValue(imageOptions[0].label);
-    });
+    expect(screen.getByRole('combobox')).toHaveValue(imageOptions[0].label);
+  });
 
-    it('selects new image option', async () => {
-      const user = userEvent.setup();
+  it('selects new image option', async () => {
+    const user = userEvent.setup();
 
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+    render();
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      const select = screen.getByLabelText(textMock('app_deployment.choose_version'));
-      await user.click(select);
+    const select = screen.getByLabelText(textMock('app_deployment.choose_version'));
+    await user.click(select);
 
-      const option = screen.getByRole('option', { name: imageOptions[1].label });
-      await user.click(option);
+    const option = screen.getByRole('option', { name: imageOptions[1].label });
+    await user.click(option);
 
-      await waitFor(() => {
-        expect(defaultProps.setSelectedImageTag).toHaveBeenCalledWith(imageOptions[1].value);
-      });
-    });
-
-    it('shows a loding spinner when mutation is pending', async () => {
-      render({ isPending: true });
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
-
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      expect(within(deployButton).getByTestId('spinner-test-id')).toBeInTheDocument();
-    });
-
-    it('disables both dropdown and button when deploy is not possible', async () => {
-      render({ disabled: true });
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
-
-      expect(screen.getByLabelText(textMock('app_deployment.choose_version'))).toBeDisabled();
-
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      expect(deployButton).toBeDisabled();
+    await waitFor(() => {
+      expect(defaultProps.setSelectedImageTag).toHaveBeenCalledWith(imageOptions[1].value);
     });
   });
 
-  describe('Confirmation dialog', () => {
-    afterEach(jest.clearAllMocks);
+  it('shows a loding spinner when mutation is pending', async () => {
+    render({ isPending: true });
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-    it('should open the confirmation dialog when clicking the deploy button', async () => {
-      const user = userEvent.setup();
-
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
-
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      await user.click(deployButton);
-
-      const dialog = screen.getByRole('dialog');
-      expect(dialog).toBeInTheDocument();
-
-      const text = await screen.findByText(
-        textMock('app_deployment.deploy_confirmation_short', {
-          selectedImageTag: imageOptions[0].value,
-        }),
-      );
-      expect(text).toBeInTheDocument();
-
-      const confirmButton = screen.getByRole('button', { name: textMock('general.yes') });
-      expect(confirmButton).toBeInTheDocument();
-
-      const cancelButton = screen.getByRole('button', { name: textMock('general.cancel') });
-      expect(cancelButton).toBeInTheDocument();
+    const deployButton = screen.getByRole('button', {
+      name: textMock('app_deployment.btn_deploy_new_version'),
     });
+    expect(within(deployButton).getByTestId('spinner-test-id')).toBeInTheDocument();
+  });
 
-    it('should open the confirmation dialog with a warning message when clicking the deploy button to an environment that alreadys has a deployed application', async () => {
-      const user = userEvent.setup();
+  it('disables both dropdown and button when deploy is not possible', async () => {
+    render({ disabled: true });
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      render({
-        appDeployedVersion: '1',
-      });
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+    expect(screen.getByLabelText(textMock('app_deployment.choose_version'))).toBeDisabled();
 
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      await user.click(deployButton);
-
-      const dialog = screen.getByRole('dialog');
-      expect(dialog).toBeInTheDocument();
-
-      const text = await screen.findByText(
-        textMock('app_deployment.deploy_confirmation', {
-          selectedImageTag: imageOptions[0].value,
-          appDeployedVersion: '1',
-        }),
-      );
-      expect(text).toBeInTheDocument();
-
-      const confirmButton = screen.getByRole('button', { name: textMock('general.yes') });
-      expect(confirmButton).toBeInTheDocument();
-
-      const cancelButton = screen.getByRole('button', { name: textMock('general.cancel') });
-      expect(cancelButton).toBeInTheDocument();
+    const deployButton = screen.getByRole('button', {
+      name: textMock('app_deployment.btn_deploy_new_version'),
     });
+    expect(deployButton).toBeDisabled();
+  });
 
-    it('should confirm and close the dialog when clicking the confirm button', async () => {
-      const user = userEvent.setup();
+  it('should confirm and close the dialog when clicking the confirm button', async () => {
+    const user = userEvent.setup();
 
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
+    render();
+    await waitForElementToBeRemoved(() =>
+      screen.queryByTitle(textMock('app_deployment.releases_loading')),
+    );
 
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      await user.click(deployButton);
-
-      const confirmButton = screen.getByRole('button', { name: textMock('general.yes') });
-      await user.click(confirmButton);
-
-      expect(defaultProps.startDeploy).toHaveBeenCalledTimes(1);
-      await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
+    const deployButton = screen.getByRole('button', {
+      name: textMock('app_deployment.btn_deploy_new_version'),
     });
+    await user.click(deployButton);
 
-    it('should close the confirmation dialog when clicking the cancel button', async () => {
-      const user = userEvent.setup();
+    const confirmButton = screen.getByRole('button', { name: textMock('general.yes') });
+    await user.click(confirmButton);
 
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
-
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      await user.click(deployButton);
-
-      const cancelButton = screen.getByRole('button', { name: textMock('general.cancel') });
-      await user.click(cancelButton);
-
-      expect(defaultProps.startDeploy).toHaveBeenCalledTimes(0);
-      await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
-    });
-
-    it('should close when clicking outside the popover', async () => {
-      const user = userEvent.setup();
-
-      render();
-      await waitForElementToBeRemoved(() =>
-        screen.queryByTitle(textMock('app_deployment.releases_loading')),
-      );
-
-      const deployButton = screen.getByRole('button', {
-        name: textMock('app_deployment.btn_deploy_new_version'),
-      });
-      await user.click(deployButton);
-
-      await user.click(document.body);
-
-      expect(defaultProps.startDeploy).toHaveBeenCalledTimes(0);
-      await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
-    });
+    expect(defaultProps.startDeploy).toHaveBeenCalledTimes(1);
+    await waitFor(() => expect(screen.queryByRole('dialog')).not.toBeInTheDocument());
   });
 });
 
