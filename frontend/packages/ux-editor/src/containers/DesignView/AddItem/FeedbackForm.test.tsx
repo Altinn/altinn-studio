65e4ff38b1d082122a66afe2880f59f1ebab39f8
Nina Kylstad <nkylstad@gmail.com>
2024-11-29 09:48:44 +0100

    remove broken tests and unused imports

--- frontend/packages/ux-editor/src/containers/DesignView/AddItem/FeedbackForm.test.tsx
+++ frontend/packages/ux-editor/src/containers/DesignView/AddItem/FeedbackForm.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByText, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { FeedbackForm } from './FeedbackForm';
 
@@ -24,30 +24,6 @@ describe('FeedbackForm', () => {
     expect(await screen.findByRole('dialog')).toBeInTheDocument();
     await user.click(screen.getByRole('button', { name: 'Send' }));
     expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
-  });
-
-  it('should call axios post when clicking send', async () => {
-    const user = userEvent.setup();
-    const postMock = jest.fn().mockImplementation(() => Promise.resolve({}));
-    jest.mock('app-shared/utils/networking', () => ({
-      post: postMock,
-    }));
-    renderFeedbackForm();
-    await user.click(screen.getByRole('button', { name: 'Gi tilbakemelding' }));
-    await user.click(screen.getByRole('button', { name: 'Send' }));
-    waitFor(() => expect(postMock).toHaveBeenCalledTimes(1));
-  });
-
-  it('should show success toast after clicking send', async () => {
-    const user = userEvent.setup();
-    const postMock = jest.fn().mockImplementation(() => Promise.resolve({}));
-    jest.mock('app-shared/utils/networking', () => ({
-      post: postMock,
-    }));
-    renderFeedbackForm();
-    await user.click(screen.getByRole('button', { name: 'Gi tilbakemelding' }));
-    await user.click(screen.getByRole('button', { name: 'Send' }));
-    waitFor(() => expect(screen.getByText('Takk for tilbakemeldingen!')).toBeInTheDocument());
   });
 });
 
