38d8646aad91f42e86b09b9f71d929a5d25b5460
Erling Hauan <148075168+ErlingHauan@users.noreply.github.com>
2024-12-05 08:41:34 +0100

    fix: Validation when creating a new data model (#14049)
    Co-authored-by: JamalAlabdullah <90609090+JamalAlabdullah@users.noreply.github.com>

--- frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/DeleteWrapper/DeleteWrapper.tsx
+++ frontend/app-development/features/dataModelling/SchemaEditorWithToolbar/TopToolbar/DeleteWrapper/DeleteWrapper.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { StudioButton } from '@studio/components';
 import { TrashIcon } from '@studio/icons';
-import { useDeleteDataModelMutation } from '../../../../hooks/mutations';
-import type { MetadataOption } from '../../../../types/MetadataOption';
+import { useDeleteDataModelMutation } from '../../../../../hooks/mutations';
+import type { MetadataOption } from '../../../../../types/MetadataOption';
 import { AltinnConfirmDialog } from 'app-shared/components';
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 import { useUpdateBpmn } from 'app-shared/hooks/useUpdateBpmn';
 import { removeDataTypeIdsToSign } from 'app-shared/utils/bpmnUtils';
+import classes from './DeleteWrapper.module.css';
+
 export interface DeleteWrapperProps {
   selectedOption: MetadataOption | null;
 }
@@ -36,6 +38,7 @@ export function DeleteWrapper({ selectedOption }: DeleteWrapperProps) {
 
   return (
     <AltinnConfirmDialog
+      className={classes.popover}
       open={dialogOpen}
       confirmText={t('schema_editor.confirm_deletion')}
       onConfirm={onDeleteConfirmClick}
