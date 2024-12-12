4a8fa9ec69adf555e4d9a1f102c3d5e5cd26731c
andreastanderen <andreastanderen@gmail.com>
2024-12-12 14:36:50 +0100

    adjust common hook for code list name error messages

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useInputCodeListNameErrorMessage.ts
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useInputCodeListNameErrorMessage.ts
@@ -1,7 +1,15 @@
-import type { FileNameErrorResult } from '@studio/pure-functions';
-import { useCodeListNameErrorMessages } from './useCodeListNameErrorMessages';
+import { FileNameErrorResult } from '@studio/pure-functions';
+import type { CommonMessageFileNameError } from './useCommonCodeListNameErrorMessages';
+import { useCommonCodeListNameErrorMessages } from './useCommonCodeListNameErrorMessages';
+import { useTranslation } from 'react-i18next';
 
 export function useInputCodeListNameErrorMessage(): (fileNameError: FileNameErrorResult) => string {
-  const errorMessages = useCodeListNameErrorMessages({ isNameFromUpload: false });
+  const { t } = useTranslation();
+  const commonErrorMessage: CommonMessageFileNameError = useCommonCodeListNameErrorMessages();
+  const errorMessages: Record<FileNameErrorResult, string> = {
+    ...commonErrorMessage,
+    [FileNameErrorResult.FileNameIsEmpty]: t('validation_errors.required'),
+    [FileNameErrorResult.FileExists]: t('validation_errors.file_name_occupied'),
+  };
   return (fileNameError: FileNameErrorResult): string => errorMessages[fileNameError];
 }
--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useUploadCodeListNameErrorMessage.ts
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useUploadCodeListNameErrorMessage.ts
@@ -1,9 +1,17 @@
-import type { FileNameErrorResult } from '@studio/pure-functions';
-import { useCodeListNameErrorMessages } from './useCodeListNameErrorMessages';
+import { FileNameErrorResult } from '@studio/pure-functions';
+import type { CommonMessageFileNameError } from './useCommonCodeListNameErrorMessages';
+import { useCommonCodeListNameErrorMessages } from './useCommonCodeListNameErrorMessages';
+import { useTranslation } from 'react-i18next';
 
 export function useUploadCodeListNameErrorMessage(): (
   fileNameError: FileNameErrorResult,
 ) => string {
-  const errorMessages = useCodeListNameErrorMessages({ isNameFromUpload: true });
+  const { t } = useTranslation();
+  const commonErrorMessage: CommonMessageFileNameError = useCommonCodeListNameErrorMessages();
+  const errorMessages: Record<FileNameErrorResult, string> = {
+    ...commonErrorMessage,
+    [FileNameErrorResult.FileNameIsEmpty]: t('validation_errors.upload_file_name_required'),
+    [FileNameErrorResult.FileExists]: t('validation_errors.upload_file_name_occupied'),
+  };
   return (fileNameError: FileNameErrorResult): string => errorMessages[fileNameError];
 }
