65f5690669573603786cd773be7a31780952560e
wrt95 <wrt_95@hotmail.com>
2024-12-12 18:01:38 +0100

    fixing story

--- frontend/libs/studio-components/src/components/StudioPaginatedContent/StudioPaginatedContent.stories.tsx
+++ frontend/libs/studio-components/src/components/StudioPaginatedContent/StudioPaginatedContent.stories.tsx
@@ -1,44 +1,26 @@
-import React, { useState } from 'react';
+import React, { type ChangeEvent, useState } from 'react';
 import type { Meta, StoryFn } from '@storybook/react';
 import { StudioPaginatedContent, type StudioPaginatedContentProps } from './StudioPaginatedContent';
+import { StudioParagraph } from '../StudioParagraph';
+import { StudioTextfield } from '../StudioTextfield';
 
-const ComplexChild = () => {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setInputValue(value);
-    if (value === '3') {
-      // Do something
-    }
-  };
-
+type ChildrenProps = {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+};
+const Children1 = ({ onChange, value }: ChildrenProps) => {
   return (
     <div>
-      <p>Type the number 3 to unlock the Next button:</p>
-      <input type='text' value={inputValue} onChange={handleInputChange} placeholder='Type here' />
+      <StudioParagraph>Children 1</StudioParagraph>
+      <StudioTextfield
+        size='sm'
+        label='Please enter the value "3" to proceed to the next page.'
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 };
-
-const items: StudioPaginatedContentProps['items'] = [
-  {
-    children: <div>Children 1</div>,
-    disableNext: false,
-  },
-  {
-    children: <div>Children 2</div>,
-    disableNext: false,
-  },
-  {
-    children: <ComplexChild />,
-    disableNext: true,
-  },
-  {
-    children: <div>Children 4</div>,
-    disableNext: false,
-  },
-];
 
 type Story = StoryFn<typeof StudioPaginatedContent>;
 
@@ -48,14 +30,40 @@ const meta: Meta = {
   argTypes: {},
 };
 
-export const Preview: Story = (args) => {
-  return <StudioPaginatedContent {...args} />;
-};
+export const Preview: Story = () => {
+  const [disableNext, setDisableNext] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
-Preview.args = {
-  items,
-  previousButtonText: 'Forrige',
-  nextButtonText: 'Neste',
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    if (value === '3') {
+      setDisableNext(false);
+    } else {
+      setDisableNext(true);
+    }
+  };
+
+  const items: StudioPaginatedContentProps['items'] = [
+    {
+      children: <Children1 value={inputValue} onChange={handleInputChange} />,
+      disableNext: disableNext,
+    },
+    {
+      children: <StudioParagraph size='sm'>Children 2</StudioParagraph>,
+      disableNext: false,
+    },
+    {
+      children: <StudioParagraph size='sm'>Children 3</StudioParagraph>,
+      disableNext: false,
+    },
+    {
+      children: <StudioParagraph size='sm'>Children 4</StudioParagraph>,
+      disableNext: false,
+    },
+  ];
+
+  return <StudioPaginatedContent items={items} previousButtonText='Back' nextButtonText='Next' />;
 };
 
 export default meta;
