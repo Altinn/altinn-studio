e887315c0f65081df10e715c89ecb020f7e490bf
wrt95 <wrt_95@hotmail.com>
2024-12-19 12:37:18 +0100

    adding tests

--- frontend/libs/studio-components/src/components/StudioPaginatedContent/hooks/useValidation.ts
+++ frontend/libs/studio-components/src/components/StudioPaginatedContent/hooks/useValidation.ts
@@ -1,9 +1,4 @@
 export const useValidation = (currentPage: number, validationRules: boolean[]) => {
   const isValid = validationRules[currentPage];
-
-  console.log('isValid', isValid);
-  console.log('currentPage', currentPage);
-  console.log('validationRules[currentPage]', validationRules[currentPage]);
-
   return { isValid };
 };
