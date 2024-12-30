d631b1eca1b6492a7efa457691083df259ee56bd
andreastanderen <71079896+standeren@users.noreply.github.com>
2024-12-30 08:38:23 +0100

    fix: 13738 fix dotnet build warnings (#14321)

--- backend/src/Designer/Models/CreateAccessListModel.cs
+++ backend/src/Designer/Models/CreateAccessListModel.cs
@@ -3,7 +3,7 @@ namespace Altinn.Studio.Designer.Models
 {
     public class CreateAccessListModel
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public string? Description { get; set; }
     }
 }
