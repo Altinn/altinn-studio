02d227defe899fabc806a2e9f38c83b35755c130
Erling Hauan <erling.hauan@digdir.no>
2025-01-06 10:28:29 +0100

    small fix - change export order

--- frontend/libs/studio-components/src/components/StudioInputTable/test-data/testTableData.ts
+++ frontend/libs/studio-components/src/components/StudioInputTable/test-data/testTableData.ts
@@ -5,16 +5,18 @@ import { textResourcesMock } from '../../../test-data/textResourcesMock';
 export const headerCheckboxLabel = 'Select all';
 export const textHeader = 'Text';
 export const textfieldHeader = 'Textfield';
+export const numberfieldHeader = 'Numberfield';
 export const textareaHeader = 'Textarea';
 export const buttonHeader = 'Button';
 export const textResourceHeader = 'Text Resource';
-export const numberfieldHeader = 'Numberfield';
 export const checkboxValue = (rowNumber: number) => `checkboxValue${rowNumber}`;
 export const checkboxName = (rowNumber: number) => `checkboxName${rowNumber}`;
 export const checkboxLabel = (rowNumber: number) => `Checkbox ${rowNumber}`;
 export const cleanText = (rowNumber: number) => `Text ${rowNumber}`;
 export const textfieldName = (rowNumber: number) => `textfield${rowNumber}`;
 export const textfieldLabel = (rowNumber: number) => `Textfield ${rowNumber}`;
+export const numberfieldName = (rowNumber: number) => `numberfield${rowNumber}`;
+export const numberfieldLabel = (rowNumber: number) => `Numberfield ${rowNumber}`;
 export const textareaName = (rowNumber: number) => `textarea${rowNumber}`;
 export const textareaLabel = (rowNumber: number) => `Textarea ${rowNumber}`;
 export const buttonLabel = (rowNumber: number) => `Button ${rowNumber}`;
@@ -22,8 +24,6 @@ export const textResourcePickerLabel = (rowNumber: number) => `Text resource ${r
 export const textResourceValueLabel = (rowNumber: number) => `Text value ${rowNumber}`;
 export const textResourceEditLabel = (rowNumber: number) => `Edit text ${rowNumber}`;
 export const textResourceSearchLabel = (rowNumber: number) => `Search for text ${rowNumber}`;
-export const numberfieldName = (rowNumber: number) => `numberfield${rowNumber}`;
-export const numberfieldLabel = (rowNumber: number) => `Numberfield ${rowNumber}`;
 
 export const textResourceProps = (rowNumber: number): CellTextResourceInputProps => ({
   textResources: textResourcesMock,
