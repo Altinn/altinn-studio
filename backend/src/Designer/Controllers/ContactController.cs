900aa3f6afbb0d7117595940d176c99a6d6e7b89
davidovrelid.com <david@framit.no>
2025-01-07 14:54:31 +0100

    format

--- backend/src/Designer/Controllers/ContactController.cs
+++ backend/src/Designer/Controllers/ContactController.cs
@@ -1,41 +1,41 @@
-using System;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Helpers;
 using Altinn.Studio.Designer.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Altinn.Studio.Designer.Controllers;
-
-[Route("designer/api/[controller]")]
-[ApiController]
-public class ContactController : ControllerBase
+namespace Altinn.Studio.Designer.Controllers
 {
-    private readonly IGitea _giteaService;
-
-    public ContactController(IGitea giteaService)
+    [Route("designer/api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
     {
-        _giteaService = giteaService;
-    }
+        private readonly IGitea _giteaService;
 
-    [AllowAnonymous]
-    [HttpGet("belongs-to-org")]
-    public async Task<IActionResult> BelongsToOrg()
-    {
-        bool isNotAuthenticated = string.IsNullOrEmpty(AuthenticationHelper.GetDeveloperUserName(HttpContext));
-        if (isNotAuthenticated)
+        public ContactController(IGitea giteaService)
         {
-            return Ok(new { belongsToOrg = false });
+            _giteaService = giteaService;
         }
 
-        try
+        [AllowAnonymous]
+        [HttpGet("belongs-to-org")]
+        public async Task<IActionResult> BelongsToOrg()
         {
-            var organizations = await _giteaService.GetUserOrganizations();
-            return Ok(new BelongsToOrgDto{ BelongsToOrg = organizations.Count > 0 });
-        }
-        catch
-        {
-            return Ok(new BelongsToOrgDto{ BelongsToOrg = false });
+            bool isNotAuthenticated = string.IsNullOrEmpty(AuthenticationHelper.GetDeveloperUserName(HttpContext));
+            if (isNotAuthenticated)
+            {
+                return Ok(new BelongsToOrgDto { BelongsToOrg = false });
+            }
+
+            try
+            {
+                var organizations = await _giteaService.GetUserOrganizations();
+                return Ok(new BelongsToOrgDto { BelongsToOrg = organizations.Count > 0 });
+            }
+            catch
+            {
+                return Ok(new BelongsToOrgDto { BelongsToOrg = false });
+            }
         }
     }
 }
