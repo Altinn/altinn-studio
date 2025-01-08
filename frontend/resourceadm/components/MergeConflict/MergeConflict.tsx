066cbc74e351b7ef61a0f4edb39df2431b540162
Martin Gunnerud <mgunnerud@gmail.com>
2025-01-08 16:11:32 +0100

    refactor merge conflict modal to a non-modal component (similar to how it is done in studio)

---
+++ frontend/resourceadm/components/MergeConflict/MergeConflict.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import classes from './MergeConflict.module.css';
+import { useTranslation } from 'react-i18next';
+import { repoDownloadPath } from 'app-shared/api/paths';
+import {
+  StudioButton,
+  StudioHeading,
+  StudioLabelAsParagraph,
+  StudioLink,
+  StudioParagraph,
+} from '@studio/components';
+import { useResetRepositoryMutation } from '../../hooks/mutations';
+
+type MergeConflictProps = {
+  /**
+   * The name of the organisation
+   */
+  org: string;
+  /**
+   * The name of the repo
+   */
+  repo: string;
+};
+
+/**
+ * @component
+ *    Displays the a message telling the user that there is a merge conflict
+ *
+ * @property {string}[org] - The name of the organisation
+ * @property {string}[repo] - The name of the repo
+ *
+ * @returns {React.JSX.Element} - The rendered component
+ */
+export const MergeConflict = ({ org, repo }: MergeConflictProps): React.JSX.Element => {
+  const { t } = useTranslation();
+
+  const { mutate: resetRepo, isPending: isRemovingChanges } = useResetRepositoryMutation(org, repo);
+
+  /**
+   * Function that resets the repo
+   */
+  const handleClickResetRepo = () => {
+    resetRepo(undefined, {
+      onSuccess: () => {
+        window.location.reload();
+      },
+    });
+  };
+
+  return (
+    <div className={classes.mergeConflictWrapper}>
+      <StudioHeading size='large'>{t('resourceadm.merge_conflict_header')}</StudioHeading>
+      <StudioParagraph size='small' spacing>
+        {t('resourceadm.merge_conflict_body')}
+      </StudioParagraph>
+      <div className={classes.downloadWrapper}>
+        <StudioLabelAsParagraph size='medium' weight='medium'>
+          {t('merge_conflict.download')}
+        </StudioLabelAsParagraph>
+        <StudioLink href={repoDownloadPath(org, repo)}>
+          {t('merge_conflict.download_edited_files')}
+        </StudioLink>
+        <StudioLink href={repoDownloadPath(org, repo, true)}>
+          {t('merge_conflict.download_entire_repo')}
+        </StudioLink>
+        <StudioParagraph size='small' spacing>
+          {t('resourceadm.merge_conflict_footer')}
+        </StudioParagraph>
+      </div>
+      <div>
+        <StudioButton
+          onClick={() => {
+            if (!isRemovingChanges) {
+              handleClickResetRepo();
+            }
+          }}
+        >
+          {t('merge_conflict.remove_my_changes')}
+        </StudioButton>
+      </div>
+    </div>
+  );
+};
