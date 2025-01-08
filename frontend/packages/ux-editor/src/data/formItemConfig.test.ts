7b805ee8bb7f96e02eba2c7513d96d88e05b3309
Nina Kylstad <nkylstad@gmail.com>
2025-01-08 15:02:21 +0100

    chore(ui-editor): remove custom component from toolbar list (#14362)

--- frontend/packages/ux-editor/src/data/formItemConfig.test.ts
+++ frontend/packages/ux-editor/src/data/formItemConfig.test.ts
@@ -14,9 +14,14 @@ describe('formItemConfig', () => {
     confOnScreenComponents,
   ];
   const allAvailableComponents = allAvailableLists.flat();
-  const excludedComponents = [ComponentType.Payment, ComponentType.Subform, ComponentType.Summary2];
+  const excludedComponents = [
+    ComponentType.Custom,
+    ComponentType.Payment,
+    ComponentType.Subform,
+    ComponentType.Summary2,
+  ];
 
-  /**  Test that all components, except Payment, Subform and Summary2 (since behind featureFlag), are available in one of the visible lists */
+  /**  Test that all components, except Custom, Payment, Subform and Summary2 (since behind featureFlag), are available in one of the visible lists */
   it.each(
     Object.values(ComponentType).filter(
       (componentType) => !excludedComponents.includes(componentType),
