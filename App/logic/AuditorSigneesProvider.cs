3860746a6551783e65f1b4e01f79a4fbd9ca499c
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-01-08 19:45:09 +0100

    adds radio button value check in auditor signees provider

--- App/logic/AuditorSigneesProvider.cs
+++ App/logic/AuditorSigneesProvider.cs
@@ -12,22 +12,21 @@ using Altinn.Platform.Storage.Interface.Models;
 
 namespace Altinn.App.logic;
 
-public class AuditorSigneesProvider : ISigneeProvider
+public class AuditorSigneesProvider(IDataClient dataClient) : ISigneeProvider
 {
-    private readonly IDataClient _dataClient;
+    private readonly IDataClient _dataClient = dataClient;
 
-    public AuditorSigneesProvider(IDataClient dataClient)
-    {
-        _dataClient = dataClient;
-    }
-    
     public string Id { get; init; } = "auditor";
-    
+
     public async Task<SigneesResult> GetSigneesAsync(Instance instance)
     {
         Skjemadata formData = await GetFormData(instance);
         Revisor revisor = formData.Revisor;
-        
+
+        if (formData.Revisor.HarRevisor == "nei")
+        {
+            return new SigneesResult { PersonSignees = [], OrganisationSignees = [] };
+        }
         var organisationSignee = new OrganisationSignee
         {
             DisplayName = revisor.Navn,
@@ -39,26 +38,31 @@ public class AuditorSigneesProvider : ISigneeProvider
                     Email = new Email
                     {
                         Subject = "Revisjonsoppdrag mottatt for signering i Altinn",
-                        Body = "Nytt revisjonsoppdrag for " + revisor.Navn + " er mottatt for signering i Altinn. Logg inn på Altinn for å signere.\n\nMed vennlig hilsen\nBrønnøysundregistrene"
+                        Body =
+                            "Nytt revisjonsoppdrag for "
+                            + revisor.Navn
+                            + " er mottatt for signering i Altinn. Logg inn på Altinn for å signere.\n\nMed vennlig hilsen\nBrønnøysundregistrene"
                     }
                 }
             }
         };
-            
-        return new SigneesResult
-        {
-            PersonSignees = [],
-            OrganisationSignees = [organisationSignee]
-        };
+
+        return new SigneesResult { PersonSignees = [], OrganisationSignees = [organisationSignee] };
     }
-    
+
     private async Task<Skjemadata> GetFormData(Instance instance)
     {
         DataElement modelData = instance.Data.Single(x => x.DataType == "Skjemadata");
         InstanceIdentifier instanceIdentifier = new(instance);
 
-        return (Skjemadata)await _dataClient.GetFormData(instanceIdentifier.InstanceGuid, typeof(Skjemadata), instance.Org,
-            instance.AppId,
-            instanceIdentifier.InstanceOwnerPartyId, new Guid(modelData.Id));
+        return (Skjemadata)
+            await _dataClient.GetFormData(
+                instanceIdentifier.InstanceGuid,
+                typeof(Skjemadata),
+                instance.Org,
+                instance.AppId,
+                instanceIdentifier.InstanceOwnerPartyId,
+                new Guid(modelData.Id)
+            );
     }
 }
