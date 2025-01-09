850d2eb1d7fee4e8eeea96697b9f41a4f81c2a0e
davidovrelid.com <david@framit.no>
2025-01-09 23:23:28 +0100

    testing

--- frontend/dashboard/components/NewApplicationForm/NewApplicationForm.test.tsx
+++ frontend/dashboard/components/NewApplicationForm/NewApplicationForm.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import {
   NewApplicationForm,
   type NewApplicationFormProps,
@@ -8,10 +8,12 @@ import {
 import { type User } from 'app-shared/types/Repository';
 import { type Organization } from 'app-shared/types/Organization';
 import userEvent from '@testing-library/user-event';
-import { textMock } from '../../../testing/mocks/i18nMock';
-import { ServicesContextProps, ServicesContextProvider } from 'app-shared/contexts/ServicesContext';
+import { textMock } from '@studio/testing/mocks/i18nMock';
+import type { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
+import { ServicesContextProvider } from 'app-shared/contexts/ServicesContext';
 import { renderWithProviders } from '../../testing/mocks';
 import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
+import { useUserOrgPermissionQuery } from '../../hooks/queries/useUserOrgPermissionsQuery';
 
 const mockOnSubmit = jest.fn();
 
@@ -53,6 +55,12 @@ const defaultProps: NewApplicationFormProps = {
   setFormError: jest.fn(),
   actionableElement: mockCancelComponentButton,
 };
+
+jest.mock('../../hooks/queries/useUserOrgPermissionsQuery');
+
+(useUserOrgPermissionQuery as jest.Mock).mockReturnValue({
+  data: { canCreateOrgRepo: true },
+});
 
 describe('NewApplicationForm', () => {
   afterEach(jest.clearAllMocks);
@@ -100,9 +108,32 @@ describe('NewApplicationForm', () => {
     expect(mockOnSubmit).toHaveBeenCalledTimes(0);
   });
 
-  it('should inform the user if he/she cannot create new application for the organization', () => {
-    renderNewApplicationForm();
-    expect(true).toBeTruthy();
+  it('should inform the user if he/she cannot create new application for the organization', async () => {
+    const user = userEvent.setup();
+    (useUserOrgPermissionQuery as jest.Mock).mockReturnValue({
+      data: { canCreateOrgRepo: false },
+    });
+    renderNewApplicationForm(defaultProps);
+
+    const serviceOwnerSelect = screen.getByLabelText(textMock('general.service_owner'));
+    await user.selectOptions(serviceOwnerSelect, mockOrg.username);
+    expect(
+      await screen.findByText(textMock('dashboard.missing_service_owner_rights_error_message')),
+    ).toBeInTheDocument();
+  });
+
+  it('should not display error if user has access to create org', async () => {
+    const user = userEvent.setup();
+    (useUserOrgPermissionQuery as jest.Mock).mockReturnValue({
+      data: { canCreateOrgRepo: true },
+    });
+    renderNewApplicationForm(defaultProps);
+
+    const serviceOwnerSelect = screen.getByLabelText(textMock('general.service_owner'));
+    await user.selectOptions(serviceOwnerSelect, mockOrg.username);
+    expect(
+      screen.queryByText(textMock('dashboard.missing_service_owner_rights_error_message')),
+    ).not.toBeInTheDocument();
   });
 });
 
