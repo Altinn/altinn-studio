2e99d2dcc16d800f2d1d83314e9cd63af7df9ff2
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-01-14 13:59:38 +0100

    Print studio url when starting dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,22 @@ import react from '@vitejs/plugin-react-swc';
 import alias from '@rollup/plugin-alias';
 import aliases from './studio.alias.js';
 import type { UserConfig } from 'vite';
+import colors from 'picocolors';
 
 export default {
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'url-override',
+      configureServer: (server) => {
+        server.printUrls = () => {
+          const { logger } = server.config;
+          const url = 'http://studio.localhost/';
+          logger.info(`  ${colors.green('➜')}  ${colors.bold('URL')}:     ${colors.cyan(url)}`);
+        };
+      },
+    },
+  ],
   server: {
     port: 2004,
   },
