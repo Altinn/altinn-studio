eb4921dad9ed33754f56618e66c298920d59e354
davidovrelid.com <david@framit.no>
2025-01-14 13:02:16 +0100

    dotnet format

--- backend/src/Designer/Models/AltinnRepoContext.cs
+++ backend/src/Designer/Models/AltinnRepoContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Altinn.Studio.Designer.Helpers;
+﻿using Altinn.Studio.Designer.Helpers;
 
 namespace Altinn.Studio.Designer.Models
 {
--- backend/src/Designer/RepositoryClient/Model/Team.cs
+++ backend/src/Designer/RepositoryClient/Model/Team.cs
@@ -1,5 +1,3 @@
-using System.Text.Json.Serialization;
-
 namespace Altinn.Studio.Designer.RepositoryClient.Model
 {
     /// <summary>
