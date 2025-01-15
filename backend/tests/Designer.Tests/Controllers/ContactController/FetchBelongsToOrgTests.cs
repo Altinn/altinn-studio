ffdf3f8a56d8793baf193c85f87bc4d283012ab3
davidovrelid.com <david@framit.no>
2025-01-15 15:32:45 +0100

    Format

--- backend/tests/Designer.Tests/Controllers/ContactController/FetchBelongsToOrgTests.cs
+++ backend/tests/Designer.Tests/Controllers/ContactController/FetchBelongsToOrgTests.cs
@@ -20,7 +20,6 @@ public class FetchBelongsToOrgTests : DesignerEndpointsTestsBase<FetchBelongsToO
     {
     }
 
-
     [Fact]
     public async Task UsersThatBelongsToOrg_ShouldReturn_True()
     {
