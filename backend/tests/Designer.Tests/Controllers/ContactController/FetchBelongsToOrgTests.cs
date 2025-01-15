2382643fd626eff2d34747ef0b2e93de2588f38b
davidovrelid.com <david@framit.no>
2025-01-15 15:30:02 +0100

    client id and clinet secret for tests

--- backend/tests/Designer.Tests/Controllers/ContactController/FetchBelongsToOrgTests.cs
+++ backend/tests/Designer.Tests/Controllers/ContactController/FetchBelongsToOrgTests.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Designer.Tests.Controllers.ApiTests;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -35,8 +38,16 @@ public class FetchBelongsToOrgTests : DesignerEndpointsTestsBase<FetchBelongsToO
     [Fact]
     public async Task UsersThatDoNotBelongsToOrg_ShouldReturn_False_IfAnonymousUser()
     {
+        string configPath = GetConfigPath();
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile(configPath, false, false)
+            .AddJsonStream(GenerateJsonOverrideConfig())
+            .AddEnvironmentVariables()
+            .Build();
+
         var anonymousClient = Factory.WithWebHostBuilder(builder =>
         {
+            builder.UseConfiguration(configuration);
             builder.ConfigureTestServices(services =>
             {
                 services.AddAuthentication("Anonymous")
@@ -54,5 +65,4 @@ public class FetchBelongsToOrgTests : DesignerEndpointsTestsBase<FetchBelongsToO
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.False(responseContent.BelongsToOrg);
     }
-
 }
