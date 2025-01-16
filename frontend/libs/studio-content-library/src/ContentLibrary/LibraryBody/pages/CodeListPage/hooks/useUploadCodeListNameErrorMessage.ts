724d18175c9ca6f3369a1542f9077e6cb3b6ed23
Erling Hauan <148075168+ErlingHauan@users.noreply.github.com>
2025-01-16 10:54:22 +0100

    Update useUploadCodeListNameErrorMessage.ts

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useUploadCodeListNameErrorMessage.ts
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/hooks/useUploadCodeListNameErrorMessage.ts
@@ -6,9 +6,9 @@ export function useUploadCodeListNameErrorMessage(): (
 ) => string {
   const { t } = useTranslation();
   const errorMessages: Record<FileNameErrorResult, string> = {
+    [FileNameErrorResult.NoRegExMatch]: t('validation_errors.file_name_invalid'),
     [FileNameErrorResult.FileNameIsEmpty]: t('validation_errors.upload_file_name_required'),
     [FileNameErrorResult.FileExists]: t('validation_errors.upload_file_name_occupied'),
-    [FileNameErrorResult.NoRegExMatch]: t('validation_errors.file_name_invalid'),
   };
   return (fileNameError: FileNameErrorResult): string => errorMessages[fileNameError];
 }
