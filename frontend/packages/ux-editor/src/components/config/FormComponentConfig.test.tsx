b9258b88e21a2bae977a45ce0cac8a0b9893143e
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-01-20 23:15:41 +0100

    add test

--- frontend/packages/ux-editor/src/components/config/FormComponentConfig.test.tsx
+++ frontend/packages/ux-editor/src/components/config/FormComponentConfig.test.tsx
@@ -483,6 +483,54 @@ describe('FormComponentConfig', () => {
     );
   });
 
+  it('should toggle close button and grid width text when the open and close buttons are clicked', async () => {
+    const user = userEvent.setup();
+    render({
+      props: {
+        schema: InputSchema,
+      },
+    });
+    const openGridButton = screen.getByRole('button', {
+      name: textMock('ux_editor.component_properties.grid'),
+    });
+    await user.click(openGridButton);
+    expect(screen.getByText(textMock('ux_editor.component_properties.grid'))).toBeInTheDocument();
+    const widthText = screen.getByText(textMock('ux_editor.modal_properties_grid'));
+    expect(widthText).toBeInTheDocument();
+
+    const closeGridButton = screen.getByRole('button', {
+      name: textMock('general.close'),
+    });
+    await user.click(closeGridButton);
+    expect(closeGridButton).not.toBeInTheDocument();
+    expect(widthText).not.toBeInTheDocument();
+  });
+
+  it('should not render grid width text if grid button is not clicked', async () => {
+    const user = userEvent.setup();
+    render({
+      props: {
+        schema: InputSchema,
+      },
+    });
+    expect(screen.queryByText(textMock('ux_editor.modal_properties_grid'))).not.toBeInTheDocument();
+    const openGridButton = screen.getByRole('button', {
+      name: textMock('ux_editor.component_properties.grid'),
+    });
+    await user.click(openGridButton);
+    expect(screen.getByText(textMock('ux_editor.component_properties.grid'))).toBeInTheDocument();
+
+    const widthText = screen.getByText(textMock('ux_editor.modal_properties_grid'));
+    expect(widthText).toBeInTheDocument();
+
+    const closeGridButton = screen.getByRole('button', {
+      name: textMock('general.close'),
+    });
+    await user.click(closeGridButton);
+    expect(closeGridButton).not.toBeInTheDocument();
+    expect(widthText).not.toBeInTheDocument();
+  });
+
   const render = ({
     props = {},
     queries = {},
