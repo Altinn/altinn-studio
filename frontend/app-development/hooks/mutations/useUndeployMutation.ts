88753ecaedd9deac675b6ae5a0b2c62863713030
davidovrelid.com <david@framit.no>
2025-01-23 16:30:44 +0100

    Invalidate queries

--- frontend/app-development/hooks/mutations/useUndeployMutation.ts
+++ frontend/app-development/hooks/mutations/useUndeployMutation.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useServicesContext } from 'app-shared/contexts/ServicesContext';
 import { QueryKey } from 'app-shared/types/QueryKey';
 
@@ -8,11 +8,12 @@ type UseUndeployMutation = {
 
 export const useUndeployMutation = (org: string, app: string) => {
   const { undeployAppFromEnv } = useServicesContext();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: ({ environment }: UseUndeployMutation) => undeployAppFromEnv(org, app, environment),
-    onSuccess: (): void => {
-      queryClient.invalidateQueries({ queryKey: [QueryKey.AppDeployments, org, app] });
+    onSuccess: async (): void => {
+      await queryClient.invalidateQueries({ queryKey: [QueryKey.AppDeployments, org, app] });
     },
   });
 };
