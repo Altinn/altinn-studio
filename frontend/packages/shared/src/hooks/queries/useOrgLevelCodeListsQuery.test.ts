a064d1ef91ffef967f13ec9507b36a62f4f2d5f4
wrt95 <wrt_95@hotmail.com>
2025-01-27 14:08:42 +0100

    adding test

---
+++ frontend/packages/shared/src/hooks/queries/useOrgLevelCodeListsQuery.test.ts
@@ -0,0 +1,18 @@
+import { queriesMock } from 'app-shared/mocks/queriesMock';
+import { renderHookWithProviders } from 'app-shared/mocks/renderHookWithProviders';
+import { waitFor } from '@testing-library/react';
+import { useOrgLevelCodeListsQuery } from './useOrgLevelCodeListsQuery';
+
+describe('useOrgLevelCodeListsQuery', () => {
+  it('calls getOptionListsReferences with the correct parameters', () => {
+    render();
+    expect(queriesMock.getOrgLevelCodeLists).toHaveBeenCalledWith();
+    expect(queriesMock.getOrgLevelCodeLists).toHaveBeenCalledTimes(1);
+  });
+});
+
+const render = async () => {
+  const { result } = renderHookWithProviders(() => useOrgLevelCodeListsQuery());
+  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+  return result;
+};
