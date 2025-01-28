261a5e02263fc964d09444ba8167d4b4250c6886
Mirko Sekulic <misha.sekulic@gmail.com>
2025-01-28 13:15:25 +0100

    fix build url for post

--- backend/src/Designer/TypedHttpClients/AzureDevOps/AzureDevOpsBuildClient.cs
+++ backend/src/Designer/TypedHttpClients/AzureDevOps/AzureDevOpsBuildClient.cs
@@ -69,7 +69,7 @@ namespace Altinn.Studio.Designer.TypedHttpClients.AzureDevOps
         {
             string requestBody = JsonSerializer.Serialize(queueBuildRequest);
             using StringContent httpContent = new(requestBody, Encoding.UTF8, MediaTypeNames.Application.Json);
-            string requestUri = "build/builds/?api-version=5.1";
+            string requestUri = "build/builds?api-version=5.1";
             _logger.LogInformation("Doing a request toward: {HttpClientBaseAddress}{RequestUri}", _httpClient.BaseAddress, requestUri);
 
             HttpResponseMessage response = await _httpClient.PostAsync(requestUri, httpContent);
