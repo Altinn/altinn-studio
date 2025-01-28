9662204ada44fed43b3746d6c8aab7a9e418b769
andreastanderen <andreastanderen@gmail.com>
2025-01-28 18:34:00 +0100

    feat: add org and apps menu items and additional routing path

--- frontend/dashboard/hooks/useSelectedContext/useSelectedContext.ts
+++ frontend/dashboard/hooks/useSelectedContext/useSelectedContext.ts
@@ -2,6 +2,6 @@ import { useParams } from 'react-router-dom';
 import { SelectedContextType } from 'dashboard/context/HeaderContext';
 
 export const useSelectedContext = () => {
-  const { selectedContext = SelectedContextType.None } = useParams();
+  const { selectedContext = SelectedContextType.Self } = useParams();
   return selectedContext;
 };
