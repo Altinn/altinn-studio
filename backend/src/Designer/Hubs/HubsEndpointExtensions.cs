2b3bf24d9f3318a90a9abe3f7019dd3b213ec27f
Mirko Sekulic <misha.sekulic@gmail.com>
2025-01-30 11:35:32 +0100

    organize hubs endpoints under /hubs

--- backend/src/Designer/Hubs/HubsEndpointExtensions.cs
+++ backend/src/Designer/Hubs/HubsEndpointExtensions.cs
@@ -1,4 +1,5 @@
 using Altinn.Studio.Designer.Hubs.EntityUpdate;
+using Altinn.Studio.Designer.Hubs.Preview;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 
@@ -9,8 +10,8 @@ public static class HubsEndpointExtensions
     public static void MapHubs(this IEndpointRouteBuilder endpoints)
     {
         // all hubs should be changed to have endpoint /hubs/hub-name
-        endpoints.MapHub<PreviewHub>("/previewHub");
-        endpoints.MapHub<SyncHub.SyncHub>("/sync-hub");
+        endpoints.MapHub<PreviewHub>("/hubs/preview");
+        endpoints.MapHub<SyncHub.SyncHub>("/hubs/sync");
         endpoints.MapHub<EntityUpdatedHub>("/hubs/entity-updated");
     }
 }
