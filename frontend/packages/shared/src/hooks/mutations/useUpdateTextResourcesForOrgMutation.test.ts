8df9abff7e28a08343df817c0eb92b2be17b603e
wrt95 <wrt_95@hotmail.com>
2025-02-03 10:28:35 +0100

    Refactoring code

--- frontend/packages/shared/src/hooks/mutations/useUpdateTextResourcesForOrgMutation.test.ts
+++ frontend/packages/shared/src/hooks/mutations/useUpdateTextResourcesForOrgMutation.test.ts
@@ -1,21 +1,21 @@
-import { queriesMock } from 'app-shared/mocks/queriesMock';
-import { renderHookWithProviders } from 'app-development/test/mocks';
+import { queriesMock } from '../../mocks/queriesMock';
+import { renderHookWithProviders } from '../../mocks/renderHookWithProviders';
 import { useUpdateTextResourcesForOrgMutation } from './useUpdateTextResourcesForOrgMutation';
 import { waitFor } from '@testing-library/react';
-import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
-import { QueryKey } from 'app-shared/types/QueryKey';
+import { createQueryClientMock } from '../../mocks/queryClientMock';
+import { QueryKey } from '../../types/QueryKey';
 import { org } from '@studio/testing/testids';
-import { type ITextResource } from 'app-shared/types/global';
-import { label1TextResource, label2TextResource } from 'app-shared/mocks/textResourcesMock';
+import { type ITextResource } from '../../types/global';
+import { label1TextResource, label2TextResource } from '../../mocks/textResourcesMock';
 
 const languageMock: string = 'nb';
 const textResourcesMock: ITextResource[] = [label1TextResource, label2TextResource];
 
 describe('useUpdateTextResourcesForOrgMutation', () => {
   it('Calls updateTextResourcesForOrg with correct arguments and payload', async () => {
-    const result = renderHookWithProviders()(() =>
+    const result = renderHookWithProviders(() =>
       useUpdateTextResourcesForOrgMutation(org, languageMock),
-    ).renderHookResult.result;
+    ).result;
 
     result.current.mutate(textResourcesMock);
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
@@ -32,9 +32,9 @@ describe('useUpdateTextResourcesForOrgMutation', () => {
     const client = createQueryClientMock();
     const invalidateQueriesSpy = jest.spyOn(client, 'invalidateQueries');
     const result = renderHookWithProviders(
-      {},
-      client,
-    )(() => useUpdateTextResourcesForOrgMutation(org, languageMock)).renderHookResult.result;
+      () => useUpdateTextResourcesForOrgMutation(org, languageMock),
+      { queryClient: client },
+    ).result;
 
     result.current.mutate(textResourcesMock);
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
--- frontend/packages/shared/src/hooks/mutations/useUpdateTextResourcesForOrgMutation.ts
+++ frontend/packages/shared/src/hooks/mutations/useUpdateTextResourcesForOrgMutation.ts
@@ -1,14 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useServicesContext } from 'app-shared/contexts/ServicesContext';
-import { type ITextResource } from 'app-shared/types/global';
-import { QueryKey } from 'app-shared/types/QueryKey';
+import { useServicesContext } from '../../contexts/ServicesContext';
+import { type ITextResource } from '../../types/global';
+import { QueryKey } from '../../types/QueryKey';
 
 export const useUpdateTextResourcesForOrgMutation = (org: string, language: string) => {
   const q = useQueryClient();
   const { updateTextResourcesForOrg } = useServicesContext();
   return useMutation({
     mutationFn: (payload: ITextResource[]) => updateTextResourcesForOrg(org, language, payload),
-    onSuccess: () =>
-      Promise.all([q.invalidateQueries({ queryKey: [QueryKey.TextResourcesForOrg] })]),
+    onSuccess: () => q.invalidateQueries({ queryKey: [QueryKey.TextResourcesForOrg] }),
   });
 };
