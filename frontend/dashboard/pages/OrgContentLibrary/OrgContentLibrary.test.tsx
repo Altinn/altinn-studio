21f7cf0316efab8bdf86b0d1a80bfad0acbad82b
Tomas <tomas.engebretsen@gmail.com>
2025-02-06 15:19:52 +0100

    Revert "feat: add context check when entering org library (#14556)"
    This reverts commit 45757ae9daf21862732c1a5bdf0650d9cdfec3fd.

--- frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.test.tsx
+++ frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.test.tsx
@@ -3,38 +3,8 @@ import { OrgContentLibrary } from './OrgContentLibrary';
 import { screen } from '@testing-library/react';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import { renderWithProviders } from '../../testing/mocks';
-import { SelectedContextType } from '../../context/HeaderContext';
-import { useParams } from 'react-router-dom';
-
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn(),
-}));
 
 describe('OrgContentLibrary', () => {
-  beforeEach(() => {
-    const selectedContext = 'some-org';
-    (useParams as jest.Mock).mockReturnValue({ selectedContext });
-  });
-
-  it.each([SelectedContextType.None, SelectedContextType.All, SelectedContextType.Self])(
-    'renders alert and omits library content when context is %s',
-    (selectedContext) => {
-      (useParams as jest.Mock).mockReturnValue({ selectedContext });
-      renderWithProviders(<OrgContentLibrary />);
-
-      const noOrgSelectedParagraph = screen.getByText(
-        textMock('dashboard.org_library.alert_no_org_selected'),
-      );
-      expect(noOrgSelectedParagraph).toBeInTheDocument();
-
-      const libraryTitle = screen.queryByRole('heading', {
-        name: textMock('app_content_library.library_heading'),
-      });
-      expect(libraryTitle).not.toBeInTheDocument();
-    },
-  );
-
   it('renders the library title', () => {
     renderWithProviders(<OrgContentLibrary />);
     const libraryTitle = screen.getByRole('heading', {
--- frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.tsx
+++ frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.tsx
@@ -1,42 +1,8 @@
 import type { ReactElement } from 'react';
 import React from 'react';
 import { ResourceContentLibraryImpl } from '@studio/content-library';
-import { useSelectedContext } from '../../hooks/useSelectedContext';
-import { StudioAlert, StudioCenter, StudioParagraph } from '@studio/components';
-import { SelectedContextType } from '../../context/HeaderContext';
-import classes from './OrgContentLibrary.module.css';
-import { useTranslation } from 'react-i18next';
 
 export function OrgContentLibrary(): ReactElement {
-  const selectedContext = useSelectedContext();
-  const contextWithNoLibraryAccess: string[] = [
-    SelectedContextType.Self,
-    SelectedContextType.All,
-    SelectedContextType.None,
-  ];
-
-  return contextWithNoLibraryAccess.includes(selectedContext) ? (
-    <ContextWithoutLibraryAccess />
-  ) : (
-    <OrgContentLibraryWithContext />
-  );
-}
-
-function ContextWithoutLibraryAccess(): ReactElement {
-  const { t } = useTranslation();
-  return (
-    <StudioCenter className={classes.noLibraryAccess}>
-      <StudioAlert className={classes.alert}>
-        <StudioParagraph>{t('dashboard.org_library.alert_no_org_selected')}</StudioParagraph>
-        <StudioParagraph>
-          {t('dashboard.org_library.alert_no_org_selected_no_access')}
-        </StudioParagraph>
-      </StudioAlert>
-    </StudioCenter>
-  );
-}
-
-function OrgContentLibraryWithContext(): ReactElement {
   const { getContentResourceLibrary } = new ResourceContentLibraryImpl({
     pages: {
       codeList: {
