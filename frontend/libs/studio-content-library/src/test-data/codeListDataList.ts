10d4b8a48a4b6e0dab2e0349da461d8085773cbd
Tomas <tomas.engebretsen@gmail.com>
2025-02-06 08:34:26 +0100

    Improve test data

--- frontend/libs/studio-content-library/src/test-data/codeListDataList.ts
+++ frontend/libs/studio-content-library/src/test-data/codeListDataList.ts
@@ -2,8 +2,14 @@ import type { CodeList } from '../types/CodeList';
 import type { CodeListData } from '../types/CodeListData';
 import {
   description1ResourceNb,
+  description2ResourceNb,
+  description3ResourceNb,
   helpText1ResourceNb,
+  helpText2ResourceNb,
+  helpText3ResourceNb,
   label1ResourceNb,
+  label2ResourceNb,
+  label3ResourceNb,
   label4ResourceNb,
 } from './textResources';
 
@@ -16,15 +22,15 @@ const codeList1: CodeList = [
   },
   {
     value: 'item2',
-    label: label1ResourceNb.id,
-    description: description1ResourceNb.id,
-    helpText: helpText1ResourceNb.id,
+    label: label2ResourceNb.id,
+    description: description2ResourceNb.id,
+    helpText: helpText2ResourceNb.id,
   },
   {
     value: 'item3',
-    label: label1ResourceNb.id,
-    description: description1ResourceNb.id,
-    helpText: helpText1ResourceNb.id,
+    label: label3ResourceNb.id,
+    description: description3ResourceNb.id,
+    helpText: helpText3ResourceNb.id,
   },
 ];
 const codeList1Name = 'codeList1';
