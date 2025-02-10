21d41415094a930890b63efc946b11325b4aa89c
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-02-10 08:38:39 +0100

    adds authentication to usercontext in useraction (#1094)

--- src/Altinn.App.Api/Controllers/ActionsController.cs
+++ src/Altinn.App.Api/Controllers/ActionsController.cs
@@ -140,7 +140,8 @@ public class ActionsController : ControllerBase
             user.UserId,
             actionRequest.ButtonId,
             actionRequest.Metadata,
-            language
+            language,
+            currentAuth
         );
         IUserAction? actionHandler = _userActionService.GetActionHandler(action);
         if (actionHandler == null)
