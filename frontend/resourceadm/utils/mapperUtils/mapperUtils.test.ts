09133669c0d8cfe6f680e64fc2e0b77efb3c2805
Martin Gunnerud <mgunnerud@gmail.com>
2025-02-10 19:48:02 +0100

    add tests for mapper function

--- frontend/resourceadm/utils/mapperUtils/mapperUtils.test.ts
+++ frontend/resourceadm/utils/mapperUtils/mapperUtils.test.ts
@@ -1,5 +1,9 @@
 import type { Altinn2LinkService } from 'app-shared/types/Altinn2LinkService';
-import { mapAltinn2LinkServiceToSelectOption } from './mapperUtils';
+import {
+  mapAltinn2LinkServiceToSelectOption,
+  setLastChangedAndSortResourceListByDate,
+} from './mapperUtils';
+import { LOCAL_RESOURCE_CHANGED_TIME } from '../resourceListUtils';
 
 describe('mapperUtils', () => {
   describe('mapAltinn2LinkServiceToSelectOption', () => {
@@ -24,6 +28,69 @@ describe('mapperUtils', () => {
       expect(result).toHaveLength(mockLinkServices.length);
       expect(result[0].value).toBe(JSON.stringify(mockLinkServices[0]));
       expect(result[0].label).toBe('name1 (code1/edition1)');
+    });
+  });
+
+  describe('setLastChangedAndSortResourceListByDate', () => {
+    it('should sort the list by date', () => {
+      const resource1Id = 'resource-1';
+      const resource2Id = 'resource-2';
+      const resource3Id = 'resource-3';
+      const loadedResourceList = [
+        {
+          title: { nb: resource1Id, en: '', nn: '' },
+          createdBy: '',
+          lastChanged: null,
+          identifier: resource1Id,
+          environments: ['tt02'],
+        },
+        {
+          title: { nb: resource2Id, en: '', nn: '' },
+          createdBy: '',
+          lastChanged: null,
+          identifier: resource2Id,
+          environments: ['gitea'],
+        },
+        {
+          title: { nb: resource3Id, en: '', nn: '' },
+          createdBy: 'ulrik user',
+          lastChanged: new Date('2023-08-29'),
+          identifier: resource3Id,
+          environments: ['gitea', 'tt02'],
+        },
+      ];
+      const resultResourceList = setLastChangedAndSortResourceListByDate(loadedResourceList);
+      expect(resultResourceList[0].identifier).toBe(resource2Id);
+      expect(resultResourceList[1].identifier).toBe(resource3Id);
+      expect(resultResourceList[2].identifier).toBe(resource1Id);
+    });
+
+    it('should set lastChanged to static value if lastChanged is null and environments includes gitea', () => {
+      const loadedResourceList = [
+        {
+          title: { nb: 'resource-1', en: '', nn: '' },
+          createdBy: '',
+          lastChanged: null,
+          identifier: 'resource-1',
+          environments: ['gitea'],
+        },
+      ];
+      const resultResourceList = setLastChangedAndSortResourceListByDate(loadedResourceList);
+      expect(resultResourceList[0].lastChanged).toBe(LOCAL_RESOURCE_CHANGED_TIME);
+    });
+
+    it('should sort environments', () => {
+      const loadedResourceList = [
+        {
+          title: { nb: 'resource-1', en: '', nn: '' },
+          createdBy: '',
+          lastChanged: null,
+          identifier: 'resource-1',
+          environments: ['gitea', 'at24', 'at22', 'prod', 'tt02'],
+        },
+      ];
+      const resultResourceList = setLastChangedAndSortResourceListByDate(loadedResourceList);
+      expect(resultResourceList[0].environments).toEqual(['prod', 'tt02', 'at22', 'at24', 'gitea']);
     });
   });
 });
