1ab24c876dff5198eb3b601c7abfeb8107b84977
wrt95 <wrt_95@hotmail.com>
2025-02-11 11:28:13 +0100

    feat: upload code list for org

--- frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.test.tsx
+++ frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.test.tsx
@@ -3,8 +3,50 @@ import { OrgContentLibrary } from './OrgContentLibrary';
 import { screen } from '@testing-library/react';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 import { renderWithProviders } from '../../testing/mocks';
+import userEvent, { type UserEvent } from '@testing-library/user-event';
+import type { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
+import type { QueryClient } from '@tanstack/react-query';
+import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
+import { QueryKey } from 'app-shared/types/QueryKey';
+import { org } from '@studio/testing/testids';
+import type { CodeListData } from '@studio/content-library';
+import type { CodeList } from '@studio/components';
+import { queriesMock } from 'app-shared/mocks/queriesMock';
+
+const uploadCodeListButtonTextMock = 'Upload Code List';
+const codeListNameMock = 'codeListNameMock';
+const codeListMock: CodeList = [{ value: '', label: '' }];
+const codeListsDataMock: CodeListData[] = [{ title: codeListNameMock, data: codeListMock }];
+
+jest.mock(
+  '../../../libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage',
+  () => ({
+    CodeListPage: ({ onUploadCodeList }: any) => (
+      <div>
+        <button
+          onClick={() =>
+            onUploadCodeList(
+              new File(['test'], `${codeListNameMock}.json`, { type: 'application/json' }),
+            )
+          }
+        >
+          {uploadCodeListButtonTextMock}
+        </button>
+      </div>
+    ),
+  }),
+);
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    selectedContext: 'testOrg',
+  }),
+}));
 
 describe('OrgContentLibrary', () => {
+  afterEach(jest.clearAllMocks);
+
   it('renders the library title', () => {
     renderWithProviders(<OrgContentLibrary />);
     const libraryTitle = screen.getByRole('heading', {
@@ -32,4 +74,79 @@ describe('OrgContentLibrary', () => {
     });
     expect(codeListMenuElement).toBeInTheDocument();
   });
+
+  it('calls onUploadCodeList when onUploadCodeList is triggered', async () => {
+    const user = userEvent.setup();
+    renderOrgContentLibraryWithCodeLists();
+    await goToLibraryPage(user, 'code_lists');
+    const uploadCodeListButton = screen.getByRole('button', { name: uploadCodeListButtonTextMock });
+    await user.click(uploadCodeListButton);
+    expect(queriesMock.uploadCodeListForOrg).toHaveBeenCalledTimes(1);
+    expect(queriesMock.uploadCodeListForOrg).toHaveBeenCalledWith(org, expect.any(FormData));
+  });
+
+  it('renders success toast when onUploadCodeList is called successfully', async () => {
+    const user = userEvent.setup();
+    renderOrgContentLibraryWithCodeLists();
+    await goToLibraryPage(user, 'code_lists');
+    const uploadCodeListButton = screen.getByRole('button', { name: uploadCodeListButtonTextMock });
+    await user.click(uploadCodeListButton);
+    const successToastMessage = screen.getByText(
+      textMock('dashboard.org_library.code_list_upload_success'),
+    );
+    expect(successToastMessage).toBeInTheDocument();
+  });
+
+  it('renders error toast when onUploadCodeList is rejected with unknown error code', async () => {
+    const user = userEvent.setup();
+    const uploadCodeListForOrg = jest
+      .fn()
+      .mockImplementation(() => Promise.reject({ response: {} }));
+    renderOrgContentLibraryWithCodeLists({ queries: { uploadCodeListForOrg } });
+    await goToLibraryPage(user, 'code_lists');
+    const uploadCodeListButton = screen.getByRole('button', { name: uploadCodeListButtonTextMock });
+    await user.click(uploadCodeListButton);
+    const errorToastMessage = screen.getByText(
+      textMock('dashboard.org_library.code_list_upload_generic_error'),
+    );
+    expect(errorToastMessage).toBeInTheDocument();
+  });
 });
+
+const getLibraryPageTile = (libraryPage: string) =>
+  screen.getByText(textMock(`app_content_library.${libraryPage}.page_name`));
+
+const goToLibraryPage = async (user: UserEvent, libraryPage: string) => {
+  const libraryPageNavTile = getLibraryPageTile(libraryPage);
+  await user.click(libraryPageNavTile);
+};
+
+type RenderOrgContentLibraryProps = {
+  queries?: Partial<ServicesContextProps>;
+  queryClient?: QueryClient;
+};
+
+const renderAppContentLibrary = ({
+  queries = {},
+  queryClient = createQueryClientMock(),
+}: RenderOrgContentLibraryProps = {}): void => {
+  renderWithProviders(<OrgContentLibrary />, {
+    queries,
+    queryClient,
+  });
+};
+
+function renderOrgContentLibraryWithCodeLists(
+  props?: Omit<RenderOrgContentLibraryProps, 'queryClient'>,
+): void {
+  const queryClient = createQueryClientWithOptionsDataList(codeListsDataMock);
+  renderAppContentLibrary({ ...props, queryClient });
+}
+
+function createQueryClientWithOptionsDataList(
+  codeListDataList: CodeListData[] | undefined,
+): QueryClient {
+  const queryClient = createQueryClientMock();
+  queryClient.setQueryData([QueryKey.OrgCodeLists, org], codeListDataList);
+  return queryClient;
+}
--- frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.tsx
+++ frontend/dashboard/pages/OrgContentLibrary/OrgContentLibrary.tsx
@@ -1,8 +1,19 @@
 import type { ReactElement } from 'react';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ResourceContentLibraryImpl } from '@studio/content-library';
+import { useTranslation } from 'react-i18next';
+import { isErrorUnknown } from 'app-shared/utils/ApiErrorUtils';
+import type { ApiError } from 'app-shared/types/api/ApiError';
+import { useUploadOrgCodeListMutation } from 'app-shared/hooks/mutations/useUploadOrgCodeListMutation';
+import { toast } from 'react-toastify';
+import type { AxiosError } from 'axios';
+import { useSelectedContext } from '../../hooks/useSelectedContext';
 
 export function OrgContentLibrary(): ReactElement {
+  const org = useSelectedContext();
+
+  const handleUpload = useUploadCodeList(org);
+
   const { getContentResourceLibrary } = new ResourceContentLibraryImpl({
     pages: {
       codeList: {
@@ -11,11 +22,33 @@ export function OrgContentLibrary(): ReactElement {
           onDeleteCodeList: () => {},
           onUpdateCodeListId: () => {},
           onUpdateCodeList: () => {},
-          onUploadCodeList: () => {},
+          onUploadCodeList: handleUpload,
         },
       },
     },
   });
 
   return <div>{getContentResourceLibrary()}</div>;
+}
+
+function useUploadCodeList(org: string): (file: File) => void {
+  const { mutate: uploadCodeList } = useUploadOrgCodeListMutation(org, {
+    hideDefaultError: (error: AxiosError<ApiError>) => isErrorUnknown(error),
+  });
+  const { t } = useTranslation();
+
+  return useCallback(
+    (file: File) =>
+      uploadCodeList(file, {
+        onSuccess: () => {
+          toast.success(t('dashboard.org_library.code_list_upload_success'));
+        },
+        onError: (error: AxiosError<ApiError>) => {
+          if (isErrorUnknown(error)) {
+            toast.error(t('dashboard.org_library.code_list_upload_generic_error'));
+          }
+        },
+      }),
+    [uploadCodeList, t],
+  );
 }
