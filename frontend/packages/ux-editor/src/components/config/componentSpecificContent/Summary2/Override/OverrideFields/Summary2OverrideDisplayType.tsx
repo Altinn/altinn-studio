2011cd36aece4d136788a0d84af5002985161fb1
lassopicasso <lars.walderhaug@digdir.no>
2025-02-11 10:07:00 +0100

    reset all override configs when reselecting component

--- frontend/packages/ux-editor/src/components/config/componentSpecificContent/Summary2/Override/OverrideFields/Summary2OverrideDisplayType.tsx
+++ frontend/packages/ux-editor/src/components/config/componentSpecificContent/Summary2/Override/OverrideFields/Summary2OverrideDisplayType.tsx
@@ -31,10 +31,6 @@ export const Summary2OverrideDisplayType = ({
     selectedComponentType?.includes(ComponentType.Checkboxes);
 
   if (!checkboxOrMultipleselect) {
-    if (override?.displayType) {
-      const { displayType, ...rest } = override;
-      onChange(rest);
-    }
     return null;
   }
 
