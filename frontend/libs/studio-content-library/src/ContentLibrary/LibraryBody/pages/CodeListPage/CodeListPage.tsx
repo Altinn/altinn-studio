35431d374ee9a40bfcdac429e4abe96fc089f9c6
Tomas <tomas.engebretsen@gmail.com>
2025-02-13 07:53:58 +0100

    Remove magic string

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/CodeListPage.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/CodeListPage.tsx
@@ -64,7 +64,7 @@ export function CodeListPage({
 
   const handleChangeTextResource = useCallback(
     (textResource: TextResource) => {
-      const updatedTextResource = createTextResourceWithLanguage('nb', textResource);
+      const updatedTextResource = createTextResourceWithLanguage(language, textResource);
       onUpdateTextResource?.(updatedTextResource);
     },
     [onUpdateTextResource],
