8eb8b8c414dd7944672a1303242f25341d818627
wrt95 <wrt_95@hotmail.com>
2025-02-17 13:08:33 +0100

    refactor: move ttd to variable

--- frontend/testing/playwright/tests/org-library/org-library.spec.ts
+++ frontend/testing/playwright/tests/org-library/org-library.spec.ts
@@ -6,10 +6,12 @@ import type { StorageState } from '../../types/StorageState';
 import { Gitea } from '../../helpers/Gitea';
 import { OrgLibraryPage } from '../../pages/OrgLibraryPage';
 
+const TEST_ORG: string = 'ttd';
+
 test.describe.configure({ mode: 'serial' });
 
 test.beforeAll(async ({ testAppName, request, storageState }) => {
-  const designerApi = new DesignerApi({ app: testAppName, org: 'ttd' });
+  const designerApi = new DesignerApi({ app: testAppName, org: TEST_ORG });
   const response = await designerApi.createApp(request, storageState as StorageState);
   expect(response.ok()).toBeTruthy();
 });
@@ -17,7 +19,7 @@ test.beforeAll(async ({ testAppName, request, storageState }) => {
 test.afterAll(async ({ request, testAppName }) => {
   const gitea = new Gitea();
   const response = await request.delete(
-    gitea.getDeleteAppEndpoint({ app: testAppName, org: 'ttd' }),
+    gitea.getDeleteAppEndpoint({ app: testAppName, org: TEST_ORG }),
   );
   expect(response.ok()).toBeTruthy();
 });
@@ -26,7 +28,7 @@ const setupAndVerifyOrgLibraryPage = async (
   page: Page,
   testAppName: string,
 ): Promise<OrgLibraryPage> => {
-  const orgLibraryPage = new OrgLibraryPage(page, { app: testAppName, org: 'ttd' });
+  const orgLibraryPage = new OrgLibraryPage(page, { app: testAppName, org: TEST_ORG });
   await orgLibraryPage.loadOrgLibraryPage();
   await orgLibraryPage.verifyOrgLibraryPage();
   await orgLibraryPage.waitForPageHeaderToBeVisible();
