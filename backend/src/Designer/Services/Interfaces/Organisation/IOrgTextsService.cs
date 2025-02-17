a963aba25641ea28203303cc37f298ab8c1e9147
Konrad-Simso <konrad.simso@digdir.no>
2025-02-17 09:48:12 +0100

    feat: text resource endpoints org level (#14655)

---
+++ backend/src/Designer/Services/Interfaces/Organisation/IOrgTextsService.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+
+namespace Altinn.Studio.Designer.Services.Interfaces.Organisation;
+
+public interface IOrgTextsService
+{
+    /// <summary>
+    /// Gets texts file in organisation repository according to specified language Code.
+    /// </summary>
+    /// <param name="org">Organisation</param>
+    /// <param name="developer">Username of developer</param>
+    /// <param name="languageCode">LanguageCode</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> that observes if operation is cancelled.</param>
+    /// <returns>The text file</returns>
+    public Task<TextResource> GetText(string org, string developer, string languageCode, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Saves text resource.
+    /// </summary>
+    /// <param name="org">Organisation</param>
+    /// <param name="developer">Username of developer</param>
+    /// <param name="textResource">The text resource to be saved</param>
+    /// <param name="languageCode">LanguageCode</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> that observes if operation is cancelled.</param>
+    /// <returns></returns>
+    public Task SaveText(string org, string developer, TextResource textResource, string languageCode, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Updates values for specified keys in the text resouce.
+    /// </summary>
+    /// <param name="org">Organisation</param>
+    /// <param name="developer">Username of developer</param>
+    /// <param name="keysTexts">KeysTexts</param>
+    /// <param name="languageCode">LanguageCode</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> that observes if operation is cancelled.</param>
+    /// <returns></returns>
+    public Task UpdateTextsForKeys(string org, string developer, Dictionary<string, string> keysTexts, string languageCode, CancellationToken cancellationToken = default);
+}
