e1cf07dce0f78d472bd384498e5a8c480d3eeca6
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-02-25 13:45:11 +0100

    added feature flag and new component FormDesignernavigation

--- frontend/app-development/router/routes.tsx
+++ frontend/app-development/router/routes.tsx
@@ -12,9 +12,12 @@ import { useAppVersionQuery } from 'app-shared/hooks/queries';
 import React from 'react';
 import { usePreviewContext } from '../contexts/PreviewContext';
 import { useLayoutContext } from '../contexts/LayoutContext';
-import { StudioPageSpinner } from '@studio/components';
+import { StudioPageSpinner, useLocalStorage } from '@studio/components';
 import { useTranslation } from 'react-i18next';
 import { AppContentLibrary } from 'app-development/features/appContentLibrary';
+import { FormDesignerNavigation } from '@altinn/ux-editor/containers/FormDesignNavigation';
+import { FeatureFlag, shouldDisplayFeature } from 'app-shared/utils/featureToggleUtils';
+import { useAppConfigQuery } from 'app-development/hooks/queries';
 
 interface IRouteProps {
   headerTextKey?: string;
@@ -45,6 +48,9 @@ const UiEditor = () => {
   const { data: version, isPending: fetchingVersionIsPending } = useAppVersionQuery(org, app);
   const { shouldReloadPreview, previewHasLoaded } = usePreviewContext();
   const { setSelectedLayoutSetName } = useLayoutContext();
+  const [selectedFormLayoutSetName] = useLocalStorage<string>('layoutSet/' + app);
+  const isTaskNavigationEnabled = shouldDisplayFeature(FeatureFlag.TaskNavigation);
+  const { data: appConfigData } = useAppConfigQuery(org, app);
 
   if (fetchingVersionIsPending) {
     return <StudioPageSpinner spinnerTitle={t('ux_editor.loading_page')} />;
@@ -53,11 +59,15 @@ const UiEditor = () => {
   if (!version) return null;
 
   return isLatestFrontendVersion(version) ? (
-    <UiEditorLatest
-      shouldReloadPreview={shouldReloadPreview}
-      previewHasLoaded={previewHasLoaded}
-      onLayoutSetNameChange={(layoutSetName) => setSelectedLayoutSetName(layoutSetName)}
-    />
+    isTaskNavigationEnabled && !selectedFormLayoutSetName ? (
+      <FormDesignerNavigation appConfig={appConfigData.serviceName} />
+    ) : (
+      <UiEditorLatest
+        shouldReloadPreview={shouldReloadPreview}
+        previewHasLoaded={previewHasLoaded}
+        onLayoutSetNameChange={(layoutSetName) => setSelectedLayoutSetName(layoutSetName)}
+      />
+    )
   ) : (
     <UiEditorV3 />
   );
