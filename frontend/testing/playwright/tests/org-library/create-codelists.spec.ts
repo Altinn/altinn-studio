57071771ece4647bc1a99773e70a77a75cc49fc9
wrt95 <wrt_95@hotmail.com>
2025-02-25 13:24:38 +0100

    fixing final piece

--- frontend/testing/playwright/tests/org-library/create-codelists.spec.ts
+++ frontend/testing/playwright/tests/org-library/create-codelists.spec.ts
@@ -5,9 +5,6 @@ import { DesignerApi } from '../../helpers/DesignerApi';
 import type { StorageState } from '../../types/StorageState';
 import { Gitea } from '../../helpers/Gitea';
 import { OrgLibraryPage } from '../../pages/OrgLibraryPage';
-import { DashboardPage } from '../../pages/DashboardPage';
-import { OverviewPage } from '../../pages/OverviewPage';
-import { AppDevelopmentHeader } from '../../components/AppDevelopmentHeader';
 
 const TEST_ORG: string = 'ttd';
 
@@ -42,8 +39,6 @@ const setupAndVerifyCodeListPage = async (
 };
 
 test('that it is possible to create a new codelist', async ({ page, testAppName }) => {
-  await navigateToAppAndPullLatestVersionAndNavigateBack(page);
-
   const orgLibraryPage: OrgLibraryPage = await setupAndVerifyCodeListPage(page, testAppName);
 
   await orgLibraryPage.codeLists.clickOnCreateNewCodelistButton();
@@ -61,24 +56,3 @@ test('that it is possible to create a new codelist', async ({ page, testAppName 
   await orgLibraryPage.codeLists.clickOnSaveCodelistButton();
   await orgLibraryPage.codeLists.verifyThatNewCodeListIsVisible(codelistTitle);
 });
-
-// TODO - Delete this function once https://github.com/Altinn/altinn-studio/pull/14793 is merged.
-const navigateToAppAndPullLatestVersionAndNavigateBack = async (page: Page) => {
-  const repoName: string = 'ttd-content';
-
-  const dashboardPage: DashboardPage = new DashboardPage(page, { app: repoName, org: TEST_ORG });
-  const overviewPage: OverviewPage = new OverviewPage(page, { app: repoName, org: TEST_ORG });
-  const appDevelopmentHeader = new AppDevelopmentHeader(page, { app: repoName, org: TEST_ORG });
-
-  await dashboardPage.loadDashboardPageAsOrg();
-  await dashboardPage.verifyDashboardPageAsOrg();
-
-  await dashboardPage.checkThatAppIsVisible(repoName);
-  await dashboardPage.clickOnTestAppEditButton(repoName);
-
-  const useTtdAsOrg: boolean = true;
-  await overviewPage.verifyOverviewPage(useTtdAsOrg);
-  await appDevelopmentHeader.clickOnPullFromGitea();
-  await appDevelopmentHeader.waitForFetchingToComplete();
-  await appDevelopmentHeader.clickOnNavigateToDashboard();
-};
