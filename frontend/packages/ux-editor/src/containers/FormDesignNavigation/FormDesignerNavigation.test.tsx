02f8d088af651cbb29ccf15560be409167cf4263
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-02-25 13:45:11 +0100

    added feature flag and new component FormDesignernavigation
    commit-id:280b7ec4

---
+++ frontend/packages/ux-editor/src/containers/FormDesignNavigation/FormDesignerNavigation.test.tsx
@@ -0,0 +1,5 @@
+//TODO: Add test case for featureFlags taskNavigation
+
+//TODO: Add test cases for FormDesignerNavigation component
+// 1- Add test for rendring the component
+// 2- Add test for the component with heading text sildeSortering
