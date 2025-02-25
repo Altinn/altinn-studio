45871c81089e7939d910a716d57a394694f00cc1
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-02-25 15:15:53 +0100

    added test for component FormDesignNavigation

--- frontend/packages/ux-editor/src/containers/FormDesignNavigation/FormDesignerNavigation.test.tsx
+++ frontend/packages/ux-editor/src/containers/FormDesignNavigation/FormDesignerNavigation.test.tsx
@@ -1,5 +1,23 @@
-//TODO: Add test case for featureFlags taskNavigation
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FormDesignerNavigation, type FormDesignerNavigationProps } from './FormDesignerNavigation';
+import { renderWithProviders } from 'app-development/test/mocks';
+import { textMock } from '@studio/testing/mocks/i18nMock';
 
-//TODO: Add test cases for FormDesignerNavigation component
-// 1- Add test for rendring the component
-// 2- Add test for the component with heading text sildeSortering
+const defaultProps = {
+  appConfig: 'test',
+};
+
+describe('FormDesignerNavigation', () => {
+  it('renders the component with heading text test', () => {
+    render({ appConfig: 'test' });
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+  it('renders the contact link', () => {
+    render();
+    expect(screen.getByRole('link', { name: textMock('general.contact') })).toBeInTheDocument();
+  });
+});
+
+const render = (props: Partial<FormDesignerNavigationProps> = {}) =>
+  renderWithProviders()(<FormDesignerNavigation {...defaultProps} {...props} />);
