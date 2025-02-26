21fd9b0865414c769dcf9b082023ccbfcbd6ab78
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-02-26 11:48:56 +0100

    fix: add helper method to check if version is below or equal to a specific version

---
+++ backend/tests/Designer.Tests/Helpers/Preview/NugetVersionHelperTests.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using Altinn.Studio.Designer.Helpers;
+using Altinn.Studio.Designer.Helpers.Preview;
+
+namespace Designer.Tests.Helpers.Preview {
+
+    public class NugetVersionHelperTests {
+        [Fact]
+        public void GetMockedAltinnNugetBuildFromVersion_ShouldReturnEmptyStringForVersionsBelowBreakpoint()
+        {
+            Assert.Equal("", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("1.1.1"));
+            Assert.Equal("", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("1.1.1-preview.150"));
+            Assert.Equal("", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("1.1.1-preview.0"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.1.1-preview.14"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.8.8-preview.0"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.18.1238"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.18.1238"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.8.8-preview.342"));
+            Assert.Equal("8.0.0.0", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.0.0-preview.15"));
+            Assert.Equal("", NugetVersionHelper.GetMockedAltinnNugetBuildFromVersion("8.0.0-preview.14"));
+        }
+    }
+}
