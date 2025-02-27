1b41a80feddf9fc53be6819c020763e7886aedd6
Konrad-Simso <konrad.simso@digdir.no>
2025-02-27 09:41:59 +0100

    test: tests for AltinnOrgGitRepository (#14771)

---
+++ backend/tests/Designer.Tests/Infrastructure/GitRepository/AltinnOrgGitRepositoryTests.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Infrastructure.GitRepository;
+using Altinn.Studio.Designer.Models;
+using Designer.Tests.Utils;
+using Xunit;
+
+namespace Designer.Tests.Infrastructure.GitRepository;
+
+public class AltinnOrgGitRepositoryTests
+{
+    private const string Org = "ttd";
+    private const string Developer = "testUser";
+
+    [Fact]
+    public void GetCodeListIds_WithRepoThatHasCodeLists_ShouldReturnCodeListPathNames()
+    {
+        // Arrange
+        const string repository = "org-content";
+        AltinnOrgGitRepository altinnOrgGitRepository = PrepareRepositoryForTest(Org, repository, Developer);
+
+        // Act
+        string[] codeListIds = altinnOrgGitRepository.GetCodeListIds();
+
+        // Assert
+        Assert.NotNull(codeListIds);
+        Assert.Equal(6, codeListIds.Length);
+    }
+
+    [Fact]
+    public void GetCodeListIds_WithRepoThatHasNoCodeLists_ShouldReturnEmptyList()
+    {
+        // Arrange
+        const string repository = "org-content-empty";
+        AltinnOrgGitRepository altinnOrgGitRepository = PrepareRepositoryForTest(Org, repository, Developer);
+
+        // Act
+        string[] codeListIds = altinnOrgGitRepository.GetCodeListIds();
+
+        // Assert
+        Assert.Empty(codeListIds);
+    }
+
+    [Fact]
+    public async Task GetCodeList_WithRepoThatHasCodeLists_ShouldReturnACodeListsWithCorrectValues()
+    {
+        // Arrange
+        const string repository = "org-content";
+        const string codeListId = "codeListString";
+        AltinnOrgGitRepository altinnOrgGitRepository = PrepareRepositoryForTest(Org, repository, Developer);
+
+        // Act
+        List<Option> codeLists = await altinnOrgGitRepository.GetCodeList(codeListId);
+
+        // Assert
+        Assert.NotNull(codeLists);
+        Assert.Equal(3, codeLists.Count);
+        Assert.Equal("norway", codeLists[0].Value);
+        Assert.Equal("denmark", codeLists[1].Value);
+        Assert.Equal("sweden", codeLists[2].Value);
+    }
+
+    [Fact]
+    public async Task GetCodeLists_WithSpecifiedCodeListIdDoesNotExistInOrg_ShouldThrowNotFoundException()
+    {
+        // Arrange
+        const string repository = "org-content";
+        const string codeListId = "none-existing-code-list";
+        AltinnOrgGitRepository altinnOrgGitRepository = PrepareRepositoryForTest(Org, repository, Developer);
+
+        // Assert
+        await Assert.ThrowsAsync<LibGit2Sharp.NotFoundException>(async () => await altinnOrgGitRepository.GetCodeList(codeListId));
+    }
+
+    private static AltinnOrgGitRepository PrepareRepositoryForTest(string org, string repository, string developer)
+    {
+
+        string repositoriesRootDirectory = TestDataHelper.GetTestDataRepositoriesRootDirectory();
+        string repositoryDirectory = TestDataHelper.GetTestDataRepositoryDirectory(org, repository, developer);
+        var altinnOrgGitRepository = new AltinnOrgGitRepository(org, repository, developer, repositoriesRootDirectory, repositoryDirectory);
+
+        return altinnOrgGitRepository;
+    }
+}
