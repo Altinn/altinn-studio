db730d91db14875f5398529e91248dd93b8cd9b3
Nina Kylstad <nkylstad@gmail.com>
2025-03-07 11:15:16 +0100

    ensure axios is mocked

--- frontend/libs/studio-feedback-form/src/FeedbackForm/FeedbackForm.test.tsx
+++ frontend/libs/studio-feedback-form/src/FeedbackForm/FeedbackForm.test.tsx
@@ -5,6 +5,7 @@ import type { ButtonTexts } from '../types/QuestionsProps';
 import { mockQuestions } from '../../mocks/mockQuestionsConfig';
 import { FeedbackFormContext } from '../contexts/FeedbackFormContext';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
 const buttonTexts: ButtonTexts = {
   submit: 'Submit',
@@ -14,6 +15,9 @@ const buttonTexts: ButtonTexts = {
 
 const heading = 'Heading';
 const description = 'Description';
+
+jest.mock('axios');
+var mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('FeedbackForm', () => {
   it('should render FeedbackForm', () => {
@@ -48,6 +52,7 @@ describe('FeedbackForm', () => {
   it('should close FeedbackForm modal when submit button is clicked', async () => {
     const user = userEvent.setup();
     renderFeedbackForm({ questions: mockQuestions });
+    mockedAxios.post.mockResolvedValueOnce({});
 
     const trigger = screen.getByRole('button', { name: buttonTexts.trigger });
     await user.click(trigger);
