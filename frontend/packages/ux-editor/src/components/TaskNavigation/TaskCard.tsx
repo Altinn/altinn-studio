12a6ed955333f59df8a68322e97d98d69c09da72
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-03-10 07:54:54 +0100

    feat: enable deletion of layout sets for subforms
    Adds a delete button for subforms on task cards.
    Removes the edit button dummy as it is not in use yet.
    
    Padding added to iconContainer to keep spacing correct even without
    context buttons. As StudioIcon has a css rule with padding on the first
    element inside.
    
    commit-id:e8b13a5a

--- frontend/packages/ux-editor/src/components/TaskNavigation/TaskCard.tsx
+++ frontend/packages/ux-editor/src/components/TaskNavigation/TaskCard.tsx
@@ -1,11 +1,12 @@
-import React, { type MouseEvent } from 'react';
+import React from 'react';
 import type { LayoutSetModel } from 'app-shared/types/api/dto/LayoutSetModel';
 import { StudioIconCard } from '@studio/components/src/components/StudioIconCard/StudioIconCard';
-import { PencilIcon } from '@studio/icons';
 import { getLayoutSetTypeTranslationKey } from 'app-shared/utils/layoutSetsUtils';
 import { useTranslation } from 'react-i18next';
 import { StudioButton, StudioDeleteButton, StudioParagraph } from '@studio/components';
 import { useLayoutSetIcon } from '../../hooks/useLayoutSetIcon';
+import { useDeleteLayoutSetMutation } from 'app-development/hooks/mutations/useDeleteLayoutSetMutation';
+import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 
 type TaskCardProps = {
   layoutSetModel: LayoutSetModel;
@@ -13,28 +14,22 @@ type TaskCardProps = {
 
 export const TaskCard = ({ layoutSetModel }: TaskCardProps) => {
   const { t } = useTranslation();
+  const { org, app } = useStudioEnvironmentParams();
+  const { mutate: deleteLayoutSet } = useDeleteLayoutSetMutation(org, app);
 
   const taskName = getLayoutSetTypeTranslationKey(layoutSetModel);
   const taskIcon = useLayoutSetIcon(layoutSetModel);
-  const contextButtons = (
-    <>
-      <StudioButton
-        variant='tertiary'
-        onClick={(event: MouseEvent<HTMLButtonElement>) => {
-          /* TODO: Implement editing mode */
-        }}
-      >
-        <PencilIcon /> {t('ux_editor.task_card.edit')}
-      </StudioButton>
-      <StudioDeleteButton
-        variant='tertiary'
-        onDelete={() => {
-          /* TODO: Call delete on task */
-        }}
-      >
-        {t('general.delete')}
-      </StudioDeleteButton>
-    </>
+
+  const contextButtons = layoutSetModel.type === 'subform' && (
+    <StudioDeleteButton
+      variant='tertiary'
+      confirmMessage={t('ux_editor.delete.subform.confirm')}
+      onDelete={() => {
+        deleteLayoutSet({ layoutSetIdToUpdate: layoutSetModel.id });
+      }}
+    >
+      {t('general.delete')}
+    </StudioDeleteButton>
   );
 
   return (
