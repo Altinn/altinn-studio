3c292f1c18cc78551b6b6357eb6050053189bd97
wrt95 <wrt_95@hotmail.com>
2025-03-11 09:45:12 +0100

    fix: fixing playwright so that it deletes before creation

--- frontend/testing/playwright/pages/OrgLibraryPage/CodeLists.ts
+++ frontend/testing/playwright/pages/OrgLibraryPage/CodeLists.ts
@@ -94,6 +94,16 @@ export class CodeLists extends BasePage {
     await expect(codeList).toBeVisible();
   }
 
+  public async getIfCodeListTitleExists(title: string): Promise<boolean> {
+    const codeList = this.page.getByTitle(
+      this.textMock('app_content_library.code_lists.code_list_accordion_title', {
+        codeListTitle: title,
+      }),
+    );
+
+    return codeList.isVisible();
+  }
+
   public async clickOnAddItemButton(): Promise<void> {
     await this.page
       .getByRole('button', {
