5c30f2b5846ce2bcebd4dabb4c5ed4a36d51c27b
adamhaeger <adamgullerud@gmail.com>
2025-03-13 10:40:20 +0100

    not allowing anonymous

--- App/logic/DataProcessing/ValidateQueryParamPrefill.cs
+++ App/logic/DataProcessing/ValidateQueryParamPrefill.cs
@@ -12,19 +12,10 @@ public class ValidateQueryParamPrefill : IValidateQueryParamPrefill
     {
         if (prefill.ContainsKey("JobTitle"))
         {
-            // No validation issue -> return null
             await Task.CompletedTask;
             return null;
         }
-
-        // If it's invalid, you can either throw or return a ValidationIssue:
+        
         throw new Exception("Tried to write invalid data via query params");
-
-        // Alternatively, return a ValidationIssue instead of throwing:
-        // return new ValidationIssue
-        // {
-        //     Severity = ValidationIssueSeverity.Error,
-        //     Description = "Tried to write invalid data via query params",
-        // };
     }
 }
