b7d2d3fcd1ad56097cf986e2cd8b89c72a6e9268
Konrad-Simso <konrad.simso@digdir.no>
2025-03-17 11:34:45 +0100

    Update frontend/dashboard/constants/index.ts
    Removing whitespace

--- frontend/dashboard/constants/index.ts
+++ frontend/dashboard/constants/index.ts
@@ -2,5 +2,4 @@ export type DATAGRID_PAGE_SIZE_TYPE = 5 | 10 | 20 | 50 | 100;
 export const DATAGRID_PAGE_SIZE_OPTIONS = [5, 10, 20, 50, 100];
 export const DATAGRID_DEFAULT_PAGE_SIZE = 5;
 export const DATA_MODEL_REPO_IDENTIFIER = '-datamodels';
-
 export const REPO_NAME_TTD_FOR_CODELISTS: string = 'ttd-content';
