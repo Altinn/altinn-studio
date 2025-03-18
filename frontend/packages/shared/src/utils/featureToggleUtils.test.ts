bfe207f6385e29ab10b17777b14701410f2bb437
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-03-18 14:18:04 +0100

    update

--- frontend/packages/shared/src/utils/featureToggleUtils.test.ts
+++ frontend/packages/shared/src/utils/featureToggleUtils.test.ts
@@ -107,6 +107,8 @@ describe('featureToggle url', () => {
   it('should return false if taskNavigationPageGroups is not enabled in the url', () => {
     window.history.pushState({}, 'PageUrl', '/?featureFlags=demo');
     expect(shouldDisplayFeature(FeatureFlag.TaskNavigationPageGroups)).toBeFalsy();
+  });
+
   it('should return true if TaskNavigationEditCards is enabled in the url', () => {
     window.history.pushState({}, 'PageUrl', '/?featureFlags=taskNavigationEditCards');
     expect(shouldDisplayFeature(FeatureFlag.TaskNavigationEditCards)).toBeTruthy();
