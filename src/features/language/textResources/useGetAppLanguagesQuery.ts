ef225f8b7de98926c18f8016e06f7306fa63c84b
Bjørnar Østtveit <47412359+bjosttveit@users.noreply.github.com>
2025-03-20 09:13:29 +0100

    log error when multiple resource files have the same language (#3139)

--- src/features/language/textResources/useGetAppLanguagesQuery.ts
+++ src/features/language/textResources/useGetAppLanguagesQuery.ts
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { useQuery } from '@tanstack/react-query';
 
 import { useAppQueries } from 'src/core/contexts/AppQueriesProvider';
@@ -5,12 +7,20 @@ import type { IAppLanguage } from 'src/types/shared';
 
 export function useGetAppLanguageQuery(enabled: boolean) {
   const { fetchAppLanguages } = useAppQueries();
-  return useQuery({
+  const utils = useQuery({
     queryKey: ['fetchAppLanguages'],
     queryFn: () => fetchAppLanguages(),
     enabled,
     select,
   });
+
+  useEffect(() => {
+    if (utils.data && new Set(utils.data).size < utils.data.length) {
+      window.logError(`Found multiple text resource files with the same 'language', languages found:\n`, utils.data);
+    }
+  }, [utils.data]);
+
+  return utils;
 }
 
 function select(appLanguages: IAppLanguage[]) {
