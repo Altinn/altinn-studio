666f2d56e9bf59e504b170e57f92d6eac6f50647
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2025-03-24 08:11:44 +0100

    fix: Fix useTextResourcesForOrgQuery (#15019)

--- frontend/packages/shared/src/hooks/queries/useTextResourcesForOrgQuery.test.ts
+++ frontend/packages/shared/src/hooks/queries/useTextResourcesForOrgQuery.test.ts
@@ -1,22 +1,42 @@
-import { queriesMock } from 'app-shared/mocks/queriesMock';
 import { renderHookWithProviders } from 'app-shared/mocks/renderHookWithProviders';
 import { waitFor } from '@testing-library/react';
-import { org } from '@studio/testing/testids';
 import { useTextResourcesForOrgQuery } from './useTextResourcesForOrgQuery';
+import { textResourcesMock } from 'app-shared/mocks/textResourcesMock';
+import { createQueryClientMock } from 'app-shared/mocks/queryClientMock';
+import type { QueryClient, QueryKey as TanstackQueryKey } from '@tanstack/react-query';
+import { QueryKey } from 'app-shared/types/QueryKey';
 
-const languageMock: string = 'nb';
+// Test data:
+const orgName = 'org';
+const language = 'nb';
+const textResources = textResourcesMock;
+
+// Mocks:
+const getTextResourcesForOrg = jest.fn(() => Promise.resolve(textResources));
 
 describe('useTextResourcesForOrgQuery', () => {
-  beforeEach(jest.clearAllMocks);
+  beforeEach(getTextResourcesForOrg.mockClear);
 
-  it('calls getTextResourcesForOrg with the correct parameters', () => {
-    renderAndWaitForResult();
-    expect(queriesMock.getTextResourcesForOrg).toHaveBeenCalledWith(org, languageMock);
-    expect(queriesMock.getTextResourcesForOrg).toHaveBeenCalledTimes(1);
+  it('calls getTextResourcesForOrg with the correct parameters', async () => {
+    await renderAndWaitForResult();
+    expect(getTextResourcesForOrg).toHaveBeenCalledTimes(1);
+    expect(getTextResourcesForOrg).toHaveBeenCalledWith(orgName, language);
+  });
+
+  it('Stores the result in the cache with correct keys', async () => {
+    const client = createQueryClientMock();
+    await renderAndWaitForResult(client);
+    const key: TanstackQueryKey = [QueryKey.TextResourcesForOrg, orgName, language];
+    expect(client.getQueryData(key)).toEqual(textResources);
   });
 });
 
-const renderAndWaitForResult = async (): Promise<void> => {
-  const { result } = renderHookWithProviders(() => useTextResourcesForOrgQuery(org, languageMock));
+const renderAndWaitForResult = async (
+  queryClient: QueryClient = createQueryClientMock(),
+): Promise<void> => {
+  const { result } = renderHookWithProviders(() => useTextResourcesForOrgQuery(orgName, language), {
+    queries: { getTextResourcesForOrg },
+    queryClient,
+  });
   await waitFor(() => expect(result.current.isSuccess).toBe(true));
 };
