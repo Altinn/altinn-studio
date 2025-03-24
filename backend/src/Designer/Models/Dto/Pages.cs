988992ba9ecfb5d154cb02f27db2bbfc5eefed1e
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-03-24 08:14:04 +0100

    Don't write pages/groups when they are null or empty

--- backend/src/Designer/Models/Dto/Pages.cs
+++ backend/src/Designer/Models/Dto/Pages.cs
@@ -6,8 +6,10 @@ namespace Altinn.Studio.Designer.Models.Dto;
 public class Pages
 {
     [JsonPropertyName("pages")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public List<Page> pages { get; set; }
 
     [JsonPropertyName("Groups")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public List<Group> groups { get; set; }
 }
