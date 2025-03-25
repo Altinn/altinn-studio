2d09ab039b26127965142ef166581c6953e751d4
davidovrelid.com <david@framit.no>
2025-03-25 11:00:38 +0100

    added the new studio-components lib

---
+++ frontend/libs/studio-components/.storybook/preview.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { Preview } from '@storybook/react';
+import { DocsContainer } from '@storybook/addon-docs';
+import { Unstyled } from '@storybook/blocks';
+
+const preview: Preview = {
+  parameters: {
+    layout: 'centered',
+    actions: { argTypesRegex: '^on.*' },
+    controls: {
+      default: 'expanded',
+      expanded: true,
+    },
+
+    docs: {
+      container: ({ children, context }) => (
+        <DocsContainer context={context}>
+          <Unstyled>{children}</Unstyled>
+        </DocsContainer>
+      ),
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+      },
+    },
+  },
+};
+
+export default preview;
