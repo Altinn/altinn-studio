5dfed0002136b1d9f4e0257545cce706941337cc
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-03-26 12:18:24 +0100

    added feature flages for adding subform

--- frontend/packages/ux-editor/src/components/TaskNavigation/TaskCardBar.tsx
+++ frontend/packages/ux-editor/src/components/TaskNavigation/TaskCardBar.tsx
@@ -4,6 +4,7 @@ import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmen
 import { TaskCard } from './TaskCard';
 import classes from './TaskCardBar.module.css';
 import { AddNewTask } from '@altinn/ux-editor/containers/AddNewTask';
+import { FeatureFlag, shouldDisplayFeature } from 'app-shared/utils/featureToggleUtils';
 
 export const TaskCardBar = () => {
   const { org, app } = useStudioEnvironmentParams();
@@ -11,6 +12,8 @@ export const TaskCardBar = () => {
     org,
     app,
   );
+
+  const isTaskNavigationSubformEnabled = shouldDisplayFeature(FeatureFlag.TaskNavigationSubform);
 
   if (layoutSetsPending) return null;
 
@@ -20,6 +23,7 @@ export const TaskCardBar = () => {
         <TaskCard key={layoutSetModel.id} layoutSetModel={layoutSetModel} />
       ))}
       <AddNewTask />
+      {isTaskNavigationSubformEnabled && true /* <AddSubformCard /> */}
     </div>
   );
 };
