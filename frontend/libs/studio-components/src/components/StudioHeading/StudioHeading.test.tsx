89bc940be609ede9cc9b3c23a1d23d30899c1f55
wrt95 <wrt_95@hotmail.com>
2025-03-27 13:25:19 +0100

    Pr comments

--- frontend/libs/studio-components/src/components/StudioHeading/StudioHeading.test.tsx
+++ frontend/libs/studio-components/src/components/StudioHeading/StudioHeading.test.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, type RenderResult, screen } from '@testing-library/react';
 import { StudioHeading, type StudioHeadingProps } from './StudioHeading';
 import { testRootClassNameAppending } from '../../test-utils/testRootClassNameAppending';
 import { testCustomAttributes } from '../../test-utils/testCustomAttributes';
 
-const mockHeading: string = 'Test Heading';
+const headingText: string = 'Test Heading';
+const defaultProps: StudioHeadingProps = {
+  children: headingText,
+};
 
 describe('StudioHeading', () => {
   it('renders children correctly', () => {
     renderStudioHeading();
-    expect(getHeading(mockHeading)).toBeInTheDocument();
+    expect(getHeading(headingText)).toBeInTheDocument();
   });
 
   it('applies default data-size correctly', () => {
     renderStudioHeading();
-    expect(getHeading(mockHeading).getAttribute('data-size')).toBe('sm');
+    expect(getHeading(headingText).getAttribute('data-size')).toBe('sm');
   });
 
   it('applies custom data-size correctly', () => {
     renderStudioHeading({ 'data-size': 'lg' });
-    expect(getHeading(mockHeading).getAttribute('data-size')).toBe('lg');
+    expect(getHeading(headingText).getAttribute('data-size')).toBe('lg');
   });
 
   it('applies custom level prop correctly', () => {
     renderStudioHeading({ level: 1 });
-    expect(getHeading(mockHeading, 1)).toBeInTheDocument();
+    expect(getHeading(headingText, 1)).toBeInTheDocument();
   });
 
   it('Appends given classname to internal classname', () => {
@@ -36,9 +39,9 @@ describe('StudioHeading', () => {
   });
 });
 
-const renderStudioHeading = (props: Partial<StudioHeadingProps> = {}) => {
-  return render(<StudioHeading {...props}>{mockHeading}</StudioHeading>);
+const renderStudioHeading = (props: Partial<StudioHeadingProps> = {}): RenderResult => {
+  return render(<StudioHeading {...defaultProps} {...props} />);
 };
 
-const getHeading = (name: string, level: number = 2) =>
-  screen.getByRole('heading', { name, level }) as HTMLElement;
+const getHeading = (name: string, level: number = 2): HTMLElement =>
+  screen.getByRole('heading', { name, level });
