0d349fae3cff66e48e6a2e72d160fc902bd9a570
lassopicasso <lars.walderhaug@digdir.no>
2025-03-27 09:24:48 +0100

    support keyboard on create task card

--- frontend/packages/ux-editor/src/containers/AddNewTask/AddNewTask.tsx
+++ frontend/packages/ux-editor/src/containers/AddNewTask/AddNewTask.tsx
@@ -12,8 +12,17 @@ export const AddNewTask = () => {
   const handleClick = () =>
     navigate(`../${RoutePaths.ProcessEditor}?returnTo=${RoutePaths.UIEditor}`);
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter' || event.key === ' ') handleClick();
+  };
+
   return (
-    <StudioCard onClick={handleClick} className={classes.card}>
+    <StudioCard
+      onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      className={classes.card}
+      tabIndex={0}
+    >
       <div className={classes.iconContainer}>{<PlusIcon />}</div>
       <div className={classes.content}>
         <StudioHeading size='2xs'>{t('ux_editor.task_card_add_new_task')}</StudioHeading>
