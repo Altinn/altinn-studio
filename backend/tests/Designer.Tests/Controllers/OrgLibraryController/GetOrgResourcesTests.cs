d8012c3c23c8d0d3052baeb4d812f2c0196fe7c5
Erling Hauan <erling.hauan@digdir.no>
2025-03-31 10:40:52 +0200

    refactor tests

--- backend/tests/Designer.Tests/Controllers/OrgLibraryController/GetOrgResourcesTests.cs
+++ backend/tests/Designer.Tests/Controllers/OrgLibraryController/GetOrgResourcesTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
@@ -8,7 +7,6 @@ using Designer.Tests.Controllers.ApiTests;
 using Designer.Tests.Utils;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
-using Task = System.Threading.Tasks.Task;
 
 namespace Designer.Tests.Controllers.OrgLibraryController;
 
@@ -18,62 +16,64 @@ public class GetOrgResourcesTests : DesignerEndpointsTestsBase<GetOrgResourcesTe
     {
     }
 
-    private const string Developer = "testUser";
-    private const string Org = "ttd";
-    private const string OrgRepo = "org-content";
-
     [Fact]
     public async Task GetOrgResources_GivenCodeListParameter_ShouldReturnOkWithCodeLists()
     {
         string apiBaseUrl = await PrepareOrgForTest();
-
         const LibraryResourceType resourceType = LibraryResourceType.CodeList;
         string apiUrlWithCodeListParameter = $"{apiBaseUrl}/{resourceType}";
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, apiUrlWithCodeListParameter);
+        var request = new HttpRequestMessage(HttpMethod.Get, apiUrlWithCodeListParameter);
 
-        var response = await HttpClient.SendAsync(httpRequestMessage);
-        List<string> responseContent = await response.Content.ReadAsAsync<List<string>>();
+        var response = await HttpClient.SendAsync(request);
+        List<string> codeLists = await response.Content.ReadAsAsync<List<string>>();
 
-        int numberOfCodeLists = responseContent.Count;
-        Assert.Equal(6, numberOfCodeLists);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(6, codeLists.Count);
     }
 
     [Fact]
     public async Task GetOrgResources_GivenTextResourceParameter_ShouldReturnOkWithTextResources()
     {
         string apiBaseUrl = await PrepareOrgForTest();
-
         const LibraryResourceType resourceType = LibraryResourceType.TextResource;
         string apiUrlWithTextResourceParameter = $"{apiBaseUrl}/{resourceType}";
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, apiUrlWithTextResourceParameter);
+        var request = new HttpRequestMessage(HttpMethod.Get, apiUrlWithTextResourceParameter);
 
-        var response = await HttpClient.SendAsync(httpRequestMessage);
-        List<string> responseContent = await response.Content.ReadAsAsync<List<string>>();
+        var response = await HttpClient.SendAsync(request);
+        List<string> textResources = await response.Content.ReadAsAsync<List<string>>();
 
-        int numberOfTextResources = responseContent.Count;
-        Assert.Equal(2, numberOfTextResources);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(2, textResources.Count);
     }
 
     [Fact]
     public async Task GetOrgResources_GivenInvalidTypeParameter_ShouldReturnBadRequest()
     {
         string apiBaseUrl = await PrepareOrgForTest();
-
         const string invalidResourceType = "invalidResourceType";
         string apiUrlWithInvalidResourceType = $"{apiBaseUrl}/{invalidResourceType}";
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, apiUrlWithInvalidResourceType);
+        var request = new HttpRequestMessage(HttpMethod.Get, apiUrlWithInvalidResourceType);
 
-        var response = await HttpClient.SendAsync(httpRequestMessage);
+        var response = await HttpClient.SendAsync(request);
 
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
     private async Task<string> PrepareOrgForTest()
     {
+        const string developer = "testUser";
+        const string org = "ttd";
+        const string orgRepo = "org-content";
+
         string targetOrgName = TestDataHelper.GenerateTestOrgName();
         string targetOrgRepo = TestDataHelper.GetOrgContentRepoName(targetOrgName);
-        await CopyOrgRepositoryForTest(Developer, Org, OrgRepo, targetOrgName, targetOrgRepo);
+        await CopyOrgRepositoryForTest(developer, org, orgRepo, targetOrgName, targetOrgRepo);
 
-        return $"designer/api/{targetOrgName}/resources";
+        return GetApiBaseUrl(targetOrgName);
+    }
+
+    private static string GetApiBaseUrl(string orgName)
+    {
+        return $"designer/api/{orgName}/resources";
     }
 }
