677d9359d17ffe26340af93434b363e1a5ed60e1
wrt95 <wrt_95@hotmail.com>
2025-03-31 12:11:40 +0200

    remove default data-size

--- frontend/libs/studio-components/src/components/StudioField/StudioField.tsx
+++ frontend/libs/studio-components/src/components/StudioField/StudioField.tsx
@@ -5,14 +5,6 @@ import type { FieldProps } from '@digdir/designsystemet-react';
 
 export type StudioFieldProps = FieldProps;
 
-export function StudioField({
-  'data-size': dataSize = 'sm',
-  children,
-  ...rest
-}: StudioFieldProps): ReactElement {
-  return (
-    <Field data-size={dataSize} {...rest}>
-      {children}
-    </Field>
-  );
+export function StudioField({ children, ...rest }: StudioFieldProps): ReactElement {
+  return <Field {...rest}>{children}</Field>;
 }
