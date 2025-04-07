4b6b05116e9db5193d6839757f88aa903aa111f3
Martin Gunnerud <mgunnerud@gmail.com>
2025-04-07 08:48:01 +0200

    rollback (model changes will be a separate PR)

--- backend/PolicyAdmin/Models/AccessPackageArea.cs
+++ backend/PolicyAdmin/Models/AccessPackageArea.cs
@@ -14,6 +14,8 @@ namespace PolicyAdmin.Models
 
         public string? Icon { get; set; }
 
+        public string? AreaGroup { get; set; }
+        
         public IEnumerable<AccessPackageOption> Packages { get; set; } = [];
     }
 }
