cadd7ac70a6684cb4943b3fe7ed20081a18e4147
Martin Othamar <martin.othamar@digdir.no>
2025-04-07 09:17:56 +0200

    Improve `InstanceResponse` XML doc (#1232)
    * Improve 'InstanceResponse' XML doc
    
    * Accept changes to swager documnetation
    
    ---------
    
    Co-authored-by: Ivar Nesje <ivarne@users.noreply.github.com>

--- src/Altinn.App.Api/Models/InstanceResponse.cs
+++ src/Altinn.App.Api/Models/InstanceResponse.cs
@@ -12,87 +12,87 @@ namespace Altinn.App.Api.Models;
 public sealed class InstanceResponse
 {
     /// <summary>
-    /// Gets or sets the unique id of the instance {instanceOwnerId}/{instanceGuid}.
+    /// The unique id of the instance {instanceOwnerId}/{instanceGuid}.
     /// </summary>
     public required string Id { get; init; }
 
     /// <summary>
-    /// Gets or sets the instance owner information.
+    /// The instance owner information.
     /// </summary>
     public required InstanceOwnerResponse InstanceOwner { get; init; }
 
     /// <summary>
-    /// Gets or sets the id of the application this is an instance of, e.g. {org}/{app22}.
+    /// The id of the application this is an instance of, e.g. {org}/{app22}.
     /// </summary>
     public required string AppId { get; init; }
 
     /// <summary>
-    /// Gets or sets application owner identifier, usually a abbreviation of organisation name. All in lower case.
+    /// Application owner identifier, usually a abbreviation of organisation name. All in lower case.
     /// </summary>
     public required string Org { get; init; }
 
     /// <summary>
-    /// Gets or sets a set of URLs to access the instance metadata resource.
+    /// A set of URLs to access the instance metadata resource.
     /// </summary>
     public required ResourceLinks SelfLinks { get; init; }
 
     /// <summary>
-    /// Gets or sets the due date to submit the instance to application owner.
+    /// The due date to submit the instance to application owner.
     /// </summary>
     public required DateTime? DueBefore { get; init; }
 
     /// <summary>
-    /// Gets or sets date and time for when the instance should first become visible for the instance owner.
+    /// Date and time for when the instance should first become visible for the instance owner.
     /// </summary>
     public required DateTime? VisibleAfter { get; init; }
 
     /// <summary>
-    /// Gets or sets an object containing the instance process state.
+    /// An object containing the instance process state.
     /// </summary>
     public required ProcessState Process { get; init; }
 
     /// <summary>
-    /// Gets or sets the type of finished status of the instance.
+    /// The type of finished status of the instance.
     /// </summary>
     public required InstanceStatus Status { get; init; }
 
     /// <summary>
-    /// Gets or sets a list of <see cref="CompleteConfirmation"/> elements.
+    /// A list of <see cref="CompleteConfirmation"/> elements.
     /// </summary>
     public required IReadOnlyList<CompleteConfirmation> CompleteConfirmations { get; init; }
 
     /// <summary>
-    /// Gets or sets a list of data elements associated with the instance
+    /// A list of data elements associated with the instance
     /// </summary>
     public required IReadOnlyList<DataElement> Data { get; init; }
 
     /// <summary>
-    /// Gets or sets the presentation texts for the instance.
+    /// The presentation texts for the instance.
     /// </summary>
     public required IReadOnlyDictionary<string, string> PresentationTexts { get; init; }
 
     /// <summary>
-    /// Gets or sets the data values for the instance.
+    /// The data values for the instance.
     /// </summary>
     public required IReadOnlyDictionary<string, string> DataValues { get; init; }
 
     /// <summary>
-    /// Gets or sets the date and time for when the element was created.
+    /// The date and time for when the element was created.
     /// </summary>
     public required DateTime? Created { get; init; }
 
     /// <summary>
-    /// Gets or sets the id of the user who created this element.
+    /// The id of the user who created this element.
     /// </summary>
     public required string CreatedBy { get; init; }
 
     /// <summary>
-    /// Gets or sets the date and time for when the element was last edited.
+    /// The date and time for when the element was last edited.
     /// </summary>
     public required DateTime? LastChanged { get; init; }
 
     /// <summary>
-    /// Gets or sets the id of the user who last changed this element.
+    /// The id of the user who last changed this element.
     /// </summary>
     public required string LastChangedBy { get; init; }
 
@@ -140,22 +140,22 @@ public sealed class InstanceResponse
 public sealed class InstanceOwnerResponse
 {
     /// <summary>
-    /// Gets or sets the party id of the instance owner (also called instance owner party id).
+    /// The party id of the instance owner (also called instance owner party id).
     /// </summary>
     public required string PartyId { get; init; }
 
     /// <summary>
-    /// Gets or sets person number (national identification number) of the party. Null if the party is not a person.
+    /// Person number (national identification number) of the party. Null if the party is not a person.
     /// </summary>
     public required string PersonNumber { get; init; }
 
     /// <summary>
-    /// Gets or sets the organisation number of the party. Null if the party is not an organisation.
+    /// The organisation number of the party. Null if the party is not an organisation.
     /// </summary>
     public required string OrganisationNumber { get; init; }
 
     /// <summary>
-    /// Gets or sets the username of the party. Null if the party is not self identified.
+    /// The username of the party. Null if the party is not self identified.
     /// </summary>
     public required string Username { get; init; }
 
@@ -171,42 +171,42 @@ public sealed class InstanceOwnerResponse
 public sealed class PartyResponse
 {
     /// <summary>
-    /// Gets or sets the ID of the party
+    /// The ID of the party
     /// </summary>
     public required int PartyId { get; init; }
 
     /// <summary>
-    /// Gets or sets the UUID of the party
+    /// The UUID of the party
     /// </summary>
     public required Guid? PartyUuid { get; init; }
 
     /// <summary>
-    /// Gets or sets the type of party
+    /// The type of party
     /// </summary>
     public required PartyType PartyTypeName { get; init; }
 
     /// <summary>
-    /// Gets the parties ssn
+    /// Person number (national identification number) of the party. Null if the party is not a person.
     /// </summary>
     public required string SSN { get; init; }
 
     /// <summary>
-    /// Gets the parties org number
+    /// The organisation number of the party. Null if the party is not an organisation.
     /// </summary>
     public required string OrgNumber { get; init; }
 
     /// <summary>
-    /// Gets or sets the UnitType
+    /// The UnitType
     /// </summary>
     public required string UnitType { get; init; }
 
     /// <summary>
-    /// Gets or sets the Name
+    /// The Name
     /// </summary>
     public required string Name { get; init; }
 
     /// <summary>
-    /// Gets or sets the IsDeleted
+    /// The IsDeleted
     /// </summary>
     public required bool IsDeleted { get; init; }
 
