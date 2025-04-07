9be1ea0a14a0181923e207c9920085fb34630da1
Erling Hauan <erling.hauan@digdir.no>
2025-04-07 15:04:50 +0200

    add taskType tag to table

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/CodeLists/EditCodeList/CodeListUsages/CodeListUsages.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListPage/CodeLists/EditCodeList/CodeListUsages/CodeListUsages.tsx
@@ -4,6 +4,7 @@ import { Table } from '@digdir/designsystemet-react';
 import { useTranslation } from 'react-i18next';
 import classes from './CodeListUsages.module.css';
 import { FileNameUtils } from '@studio/pure-functions';
+import { StudioTag, StudioTagProps } from '@studio/components-legacy';
 
 export type CodeListUsagesProps = {
   codeListSources: CodeListIdSource[];
@@ -16,7 +17,8 @@ export function CodeListUsages({ codeListSources }: CodeListUsagesProps): React.
     <Table zebra className={classes.table}>
       <Table.Head>
         <Table.Row>
-          <Table.HeaderCell>{t('general.task')}</Table.HeaderCell>
+          <Table.HeaderCell>Oppgavetype</Table.HeaderCell>
+          <Table.HeaderCell>Oppgavenavn</Table.HeaderCell>
           <Table.HeaderCell>
             {t('app_content_library.code_lists.code_list_usage_table_column_header_layout_set')}
           </Table.HeaderCell>
@@ -44,8 +46,21 @@ type CodeListUsageSourceRowProps = {
 function CodeListUsageSourceRow({
   codeListSource,
 }: CodeListUsageSourceRowProps): React.ReactElement {
+  const translatedTaskType = translateTaskType(codeListSource.taskType);
+  const colorClass =
+    codeListSource.taskType === 'data'
+      ? classes.tagTypeData
+      : codeListSource.taskType === 'signing'
+        ? classes.tagTypeSigning
+        : undefined;
+
   return (
     <Table.Row>
+      <Table.Cell>
+        <StudioTag className={`${classes.tag} ${colorClass}`} size={'md'}>
+          {translatedTaskType}
+        </StudioTag>
+      </Table.Cell>
       <Table.Cell>{codeListSource.taskId}</Table.Cell>
       <Table.Cell>{codeListSource.layoutSetId}</Table.Cell>
       <Table.Cell>{FileNameUtils.removeExtension(codeListSource.layoutName)}</Table.Cell>
@@ -56,4 +71,13 @@ function CodeListUsageSourceRow({
 
 const listComponentIds = (componentIds: string[]): string => {
   return componentIds.join(', ');
+};
+
+const translateTaskType = (taskType: string): string => {
+  switch (taskType) {
+    case 'data':
+      return 'Utfylling';
+    case 'signing':
+      return 'Signering';
+  }
 };
