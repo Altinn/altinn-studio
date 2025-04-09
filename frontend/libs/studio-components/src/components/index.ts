b228f654ff6d3ec62bf944cd3aa8d0e2066173d2
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-04-09 16:04:17 +0200

    update breadcrumbs

--- frontend/libs/studio-components/src/components/index.ts
+++ frontend/libs/studio-components/src/components/index.ts
@@ -3,6 +3,7 @@ import '@digdir/designsystemet-css/index.css';
 import '@digdir/designsystemet-theme';
 
 export { StudioDropdown } from './StudioDropdown';
+export { StudioBreadcrumbs } from './StudioBreadcrumbs';
 export { StudioButton } from './StudioButton';
 export { StudioHeading } from './StudioHeading';
 export { StudioParagraph } from './StudioParagraph';
