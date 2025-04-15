2f7a621ed728ddde15867e47b48e428d2fac2d29
lassopicasso <lars.walderhaug@digdir.no>
2025-04-15 11:12:33 +0200

    update test deleteModal

--- frontend/packages/shared/src/components/GiteaHeader/ThreeDotsMenu/LocalChangesModal/DeleteModal/DeleteModal.test.tsx
+++ frontend/packages/shared/src/components/GiteaHeader/ThreeDotsMenu/LocalChangesModal/DeleteModal/DeleteModal.test.tsx
@@ -12,12 +12,9 @@ import type { QueryClient } from '@tanstack/react-query';
 import { renderWithProviders } from 'app-shared/components/GiteaHeader/mocks/renderWithProviders';
 import { app, org } from '@studio/testing/testids';
 
-const onDelete: () => void = jest.fn();
-
 const defaultProps: DeleteModalProps = {
   app: app,
   org: org,
-  onDelete,
 };
 
 describe('DeleteModal', () => {
@@ -40,17 +37,21 @@ describe('DeleteModal', () => {
   });
 
   it('calls the handleDelete function when the Delete button is clicked with a matching app name', async () => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        ...window.location,
+        reload: jest.fn(),
+      },
+      writable: true,
+    });
+    jest.spyOn(window.location, 'reload').mockImplementation(() => {});
     const user = userEvent.setup();
-    const mockDelete = jest.fn().mockImplementation(() => Promise.resolve());
-    await renderDeleteModal({ resetRepoChanges: mockDelete });
+    await renderDeleteModal();
     expect(getDeleteButton()).toBeDisabled();
     await user.type(getNameField(), app);
     expect(getDeleteButton()).not.toBeDisabled();
-    expect(mockDelete).toHaveBeenCalledTimes(0);
     await user.click(getDeleteButton());
-    expect(mockDelete).toHaveBeenCalledTimes(1);
-    const toastSuccessText = await screen.findByText(successMessage);
-    expect(toastSuccessText).toBeInTheDocument();
+    expect(location.reload).toHaveBeenCalledTimes(1);
   });
 
   it('does not call the onClose function when the Delete button is clicked and an error is received', async () => {
@@ -93,4 +94,3 @@ const getButton = (name: string) => screen.getByRole('button', { name });
 
 const nameFieldLabel = textMock('local_changes.modal_delete_modal_textfield_label');
 const deleteButtonName = textMock('local_changes.modal_confirm_delete_button');
-const successMessage = textMock('local_changes.modal_deleted_success');
