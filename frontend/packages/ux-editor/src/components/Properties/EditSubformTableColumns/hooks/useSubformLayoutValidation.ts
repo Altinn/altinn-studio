49baa9cc93bb662919f14d02351d801aa76fc015
lassopicasso <lars.walderhaug@digdir.no>
2025-04-15 16:03:13 +0200

    fix display warning message for subform table

--- frontend/packages/ux-editor/src/components/Properties/EditSubformTableColumns/hooks/useSubformLayoutValidation.ts
+++ frontend/packages/ux-editor/src/components/Properties/EditSubformTableColumns/hooks/useSubformLayoutValidation.ts
@@ -1,13 +1,19 @@
 import { useFormLayoutsQuery } from '@altinn/ux-editor/hooks/queries/useFormLayoutsQuery';
 import { getAllLayoutComponents } from '@altinn/ux-editor/utils/formLayoutUtils';
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
+import { ComponentType } from 'app-shared/types/ComponentType';
 
 export const useSubformLayoutValidation = (subformLayoutSetName: string): boolean => {
   const { org, app } = useStudioEnvironmentParams();
   const { data: formLayout } = useFormLayoutsQuery(org, app, subformLayoutSetName);
+  console.log('useSubformLayoutValidation', formLayout);
+
+  const excludedComponents = [ComponentType.CustomButton];
 
   if (formLayout) {
-    return Object.values(formLayout).some((value) => getAllLayoutComponents(value).length > 0);
+    return Object.values(formLayout).some(
+      (value) => getAllLayoutComponents(value, excludedComponents).length > 0,
+    );
   }
   return false;
 };
