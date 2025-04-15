aca513a1780b5e092c97dfe9e7360c4f277a582d
wrt95 <wrt_95@hotmail.com>
2025-04-15 10:49:24 +0200

    fixing test

--- frontend/libs/studio-content-library/src/ContentLibrary/ContentLibrary.test.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/ContentLibrary.test.tsx
@@ -28,9 +28,7 @@ describe('ContentLibrary', () => {
     const codeListTitle = screen.getByRole('heading', {
       name: textMock('app_content_library.code_lists.page_name'),
     });
-    const infoBox = screen.getByTitle(textMock('app_content_library.info_box.title'));
     expect(codeListTitle).toBeInTheDocument();
-    expect(infoBox).toBeInTheDocument();
   });
 
   it('navigates to images content when clicking on images navigation', async () => {
