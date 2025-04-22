ca61d281c0ae0793e4a301654aa102d81d7d5bd8
Martin Gunnerud <mgunnerud@gmail.com>
2025-04-22 14:55:15 +0200

    fix mock

--- frontend/packages/shared/src/mocks/queriesMock.ts
+++ frontend/packages/shared/src/mocks/queriesMock.ts
@@ -207,6 +207,7 @@ export const queriesMock: ServicesContextProps = {
       Promise.resolve<ITextResourcesWithLanguage>(textResourcesWithLanguage),
     ),
   updateSelectedMaskinportenScopes: jest.fn().mockImplementation(() => Promise.resolve()),
+  getConsentTemplates: jest.fn().mockImplementation(() => Promise.resolve([])),
 
   // Queries - Contact
   fetchBelongsToGiteaOrg: jest
