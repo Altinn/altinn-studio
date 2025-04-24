834ac09db910575a80f67861e6a0a20146a33caa
lassopicasso <lars.walderhaug@digdir.no>
2025-04-24 15:19:10 +0200

    avoid line break when datamodel name is too long

--- frontend/packages/ux-editor/src/components/TaskNavigation/TaskCard.tsx
+++ frontend/packages/ux-editor/src/components/TaskNavigation/TaskCard.tsx
@@ -78,9 +78,11 @@ export const TaskCard = ({ layoutSetModel }: TaskCardProps) => {
       <div className={classes.details}>
         <div>
           <StudioParagraph size='sm'>{t(taskName)}</StudioParagraph>
-          <StudioHeading size='xs'>{layoutSetModel.id}</StudioHeading>
+          <StudioHeading size='xs' title={layoutSetModel.id}>
+            {layoutSetModel.id}
+          </StudioHeading>
         </div>
-        <StudioParagraph size='sm'>
+        <StudioParagraph size='sm' title={layoutSetModel.dataType}>
           {t('ux_editor.task_card.datamodel')}
           {layoutSetModel.dataType && ' ' + layoutSetModel.dataType}
         </StudioParagraph>
