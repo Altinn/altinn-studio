5b3a2be92112cc7b4603bbcbd07b1856245ae047
Tomas <tomas.engebretsen@gmail.com>
2025-05-01 14:06:37 +0200

    Remove redundant comment

--- frontend/packages/shared/src/hooks/useStudioEnvironmentParams.ts
+++ frontend/packages/shared/src/hooks/useStudioEnvironmentParams.ts
@@ -1,10 +1,6 @@
 import { useParams } from 'react-router-dom';
 import type { AppRouteParams } from 'app-shared/types/AppRouteParams';
 
-/**
- * Retrieves the org and app names from the URL.
- * @returns {StudioUrlParams} The org and app names.
- */
 export const useStudioEnvironmentParams = (): AppRouteParams => {
   const { org, app } = useParams<Partial<AppRouteParams>>();
   return { org, app };
