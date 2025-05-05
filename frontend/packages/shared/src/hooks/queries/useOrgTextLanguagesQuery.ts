35739e3c0ab85f82dcf46ae1b9f6801ab83832b4
Erling Hauan <erling.hauan@digdir.no>
2025-05-05 15:51:58 +0200

    fix rabbit comments

--- frontend/packages/shared/src/hooks/queries/useOrgTextLanguagesQuery.ts
+++ frontend/packages/shared/src/hooks/queries/useOrgTextLanguagesQuery.ts
@@ -2,9 +2,9 @@ import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 import { QueryKey } from '../../types/QueryKey';
 import { useServicesContext } from '../../contexts/ServicesContext';
 
-export const useOrgTextLanguagesQuery = (orgName: string): UseQueryResult<string[]> => {
+export const useOrgTextLanguagesQuery = (orgName: string): UseQueryResult<string[] | null> => {
   const { getOrgTextLanguages } = useServicesContext();
-  return useQuery<string[]>({
+  return useQuery<string[] | null>({
     queryKey: [QueryKey.OrgTextLanguages, orgName],
     queryFn: (): Promise<string[] | null> => getOrgTextLanguages(orgName),
   });
