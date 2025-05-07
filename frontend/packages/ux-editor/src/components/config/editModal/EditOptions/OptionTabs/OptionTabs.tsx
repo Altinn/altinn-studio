19c2de6c2526d1e7f848553c40fca3c150a51d2a
Konrad-Simso <konrad.simso@digdir.no>
2025-05-07 14:15:24 +0200

    feat: Remove feature flag OptionListEditor

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/OptionTabs.tsx
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/OptionTabs.tsx
@@ -2,18 +2,12 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StudioTabs } from '@studio/components-legacy';
 import { ReferenceTab } from './ReferenceTab/ReferenceTab';
-import { shouldDisplayFeature, FeatureFlag } from 'app-shared/utils/featureToggleUtils';
-import { ManualTab } from './ManualTab';
 import { EditTab } from './EditTab';
 import { SelectedOptionsType } from '../EditOptions';
 import type { IGenericEditComponent } from '../../../componentConfig';
 import type { SelectionComponentType } from '../../../../../types/FormComponent';
+import { getSelectedOptionsType } from './utils/optionsUtils';
 import classes from './OptionTabs.module.css';
-import { SelectTab } from './SelectTab';
-import {
-  getSelectedOptionsTypeWithManualSupport,
-  getSelectedOptionsType,
-} from './utils/optionsUtils';
 
 type OptionTabsProps = {
   optionListIds: string[];
@@ -21,21 +15,11 @@ type OptionTabsProps = {
 
 export function OptionTabs({ component, handleComponentChange, optionListIds }: OptionTabsProps) {
   return (
-    <>
-      {shouldDisplayFeature(FeatureFlag.OptionListEditor) ? (
-        <OptionTabsMergedTabs
-          component={component}
-          handleComponentChange={handleComponentChange}
-          optionListIds={optionListIds}
-        />
-      ) : (
-        <OptionTabsSplitTabs
-          component={component}
-          handleComponentChange={handleComponentChange}
-          optionListIds={optionListIds}
-        />
-      )}
-    </>
+    <OptionTabsMergedTabs
+      component={component}
+      handleComponentChange={handleComponentChange}
+      optionListIds={optionListIds}
+    />
   );
 }
 
@@ -70,48 +54,6 @@ function OptionTabsMergedTabs({
       </StudioTabs.List>
       <StudioTabs.Content className={classes.tabContent} value={SelectedOptionsType.CodeList}>
         <EditTab component={component} handleComponentChange={handleComponentChange} />
-      </StudioTabs.Content>
-      <StudioTabs.Content value={SelectedOptionsType.ReferenceId} className={classes.tabContent}>
-        <ReferenceTab component={component} handleComponentChange={handleComponentChange} />
-      </StudioTabs.Content>
-    </StudioTabs>
-  );
-}
-
-// Todo: Remove once featureFlag "optionListEditor" is removed.
-function OptionTabsSplitTabs({ component, handleComponentChange, optionListIds }: OptionTabsProps) {
-  const initialSelectedOptionsType = getSelectedOptionsTypeWithManualSupport(
-    component.optionsId,
-    component.options,
-    optionListIds || [],
-  );
-  const [selectedOptionsType, setSelectedOptionsType] = useState(initialSelectedOptionsType);
-  const { t } = useTranslation();
-
-  return (
-    <StudioTabs
-      value={selectedOptionsType}
-      size='small'
-      onChange={(value) => {
-        setSelectedOptionsType(value as SelectedOptionsType);
-      }}
-    >
-      <StudioTabs.List>
-        <StudioTabs.Tab value={SelectedOptionsType.CodeList}>
-          {t('ux_editor.options.tab_code_list')}
-        </StudioTabs.Tab>
-        <StudioTabs.Tab value={SelectedOptionsType.Manual}>
-          {t('ux_editor.options.tab_manual')}
-        </StudioTabs.Tab>
-        <StudioTabs.Tab value={SelectedOptionsType.ReferenceId}>
-          {t('ux_editor.options.tab_reference_id')}
-        </StudioTabs.Tab>
-      </StudioTabs.List>
-      <StudioTabs.Content className={classes.tabContent} value={SelectedOptionsType.CodeList}>
-        <SelectTab component={component} handleComponentChange={handleComponentChange} />
-      </StudioTabs.Content>
-      <StudioTabs.Content value={SelectedOptionsType.Manual} className={classes.tabContent}>
-        <ManualTab component={component} handleComponentChange={handleComponentChange} />
       </StudioTabs.Content>
       <StudioTabs.Content value={SelectedOptionsType.ReferenceId} className={classes.tabContent}>
         <ReferenceTab component={component} handleComponentChange={handleComponentChange} />
