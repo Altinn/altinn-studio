4cef41caa7b35f7d890c83584147d71c43be54ad
Konrad-Simso <konrad.simso@digdir.no>
2025-05-07 16:03:19 +0200

    feat: transform option labels into textResource values

--- frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/EditTab/OptionListEditor/hooks/useConcatOptionsLabels.ts
+++ frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/EditTab/OptionListEditor/hooks/useConcatOptionsLabels.ts
@@ -1,9 +1,26 @@
 import type { Option } from 'app-shared/types/Option';
+import type { TextResource } from '@studio/components-legacy';
 import { useTranslation } from 'react-i18next';
 
-export function useConcatOptionsLabels(optionsList: Option[]): string {
+export function useConcatOptionsLabels(
+  optionsList: Option[],
+  textResources: TextResource[],
+): string {
   const { t } = useTranslation();
-  return optionsList
-    .map((option: Option) => `${option.label || t('general.empty_string')}`)
-    .join(' | ');
+  const labels: string[] = [];
+  const optionLabels: string[] = optionsList.map((option: Option): string => option.label);
+
+  for (const label of optionLabels) {
+    const textResource: TextResource = textResources.find(
+      (resource: TextResource) => resource.id === label,
+    );
+
+    if (textResource) {
+      labels.push(textResource.value);
+    } else {
+      labels.push(label);
+    }
+  }
+
+  return labels.map((label: string) => `${label || t('general.empty_string')}`).join(' | ');
 }
