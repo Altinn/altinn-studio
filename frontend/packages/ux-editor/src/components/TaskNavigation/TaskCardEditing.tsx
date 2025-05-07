2027a2b8a57907475a2d6ef9b3bef4f5885adbdc
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-05-07 12:33:38 +0200

    pr feedback, move label condition to local const

--- frontend/packages/ux-editor/src/components/TaskNavigation/TaskCardEditing.tsx
+++ frontend/packages/ux-editor/src/components/TaskNavigation/TaskCardEditing.tsx
@@ -48,6 +48,11 @@ export const TaskCardEditing = ({ layoutSetModel, onClose }: TaskCardEditingProp
   const pendingMutation = updateProcessDataTypePending || mutateLayoutSetIdPending;
   const disableSaveButton = !fieldChanged || Boolean(idValidationError) || pendingMutation;
 
+  const taskNameFieldLabel =
+    layoutSetModel.type === 'subform'
+      ? t('ux_editor.task_card.subform_name_label')
+      : t('ux_editor.task_card.task_name_label');
+
   const onSettled = () => {
     if (!pendingMutation) onClose();
   };
@@ -80,11 +85,7 @@ export const TaskCardEditing = ({ layoutSetModel, onClose }: TaskCardEditingProp
     <StudioCard className={classes.editCard}>
       <StudioParagraph size='xs'>{t(taskName)}</StudioParagraph>
       <StudioTextfield
-        label={
-          layoutSetModel.type === 'subform'
-            ? t('ux_editor.task_card.subform_name_label')
-            : t('ux_editor.task_card.task_name_label')
-        }
+        label={taskNameFieldLabel}
         value={id}
         error={idValidationError}
         onKeyUp={(event) => {
