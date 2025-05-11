a6630b235e50175dff7347a9dcac3920331a1160
wrt95 <wrt_95@hotmail.com>
2025-05-11 19:10:53 +0200

    addign more

--- frontend/app-development/features/appSettings/components/TabsContent/TabsContent.tsx
+++ frontend/app-development/features/appSettings/components/TabsContent/TabsContent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { ReactElement } from 'react';
 import type { SettingsTabId } from '../../types/SettingsTabId';
 import { FeatureFlag, shouldDisplayFeature } from 'app-shared/utils/featureToggleUtils';
+import { PolicyTab } from './Tabs/PolicyTab';
 
 export type TabsContentProps = {
   currentTab: SettingsTabId;
@@ -16,7 +17,7 @@ export function TabsContent({ currentTab }: TabsContentProps): ReactElement {
       return <div>Setup tab</div>;
     }
     case 'policy': {
-      return <div>Policy tab</div>;
+      return <PolicyTab />;
     }
     case 'access_control': {
       return <div>Access Control tab</div>;
