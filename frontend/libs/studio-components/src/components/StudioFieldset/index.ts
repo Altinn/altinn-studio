5db2c1737d96ad3a6ac574656b76d8cf30720f86
wrt95 <wrt_95@hotmail.com>
2025-05-12 10:41:57 +0200

    initial commit

--- frontend/libs/studio-components/src/components/StudioFieldset/index.ts
+++ frontend/libs/studio-components/src/components/StudioFieldset/index.ts
@@ -1,1 +1,2 @@
 export { StudioFieldset } from './StudioFieldset';
+export type { StudioFieldsetProps } from './StudioFieldset';
---
+++ frontend/libs/studio-components/src/components/StudioFormGroup/StudioFormGroup.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react';
+import type { ReactElement, Ref } from 'react';
+import { StudioFieldset, type StudioFieldsetProps } from '../StudioFieldset';
+
+export type StudioFormGroupProps = StudioFieldsetProps & {
+  requiredText?: string;
+  required?: boolean;
+};
+
+function StudioFormGroup(
+  { children, requiredText, required, ...rest }: StudioFormGroupProps,
+  ref: Ref<HTMLFieldSetElement>,
+): ReactElement {
+  return (
+    <StudioFieldset>
+      <StudioFieldset.Legend></StudioFieldset.Legend>
+    </StudioFieldset>
+  );
+}
+
+const ForwardedStudioFormGroup = forwardRef(StudioFormGroup);
+
+export { ForwardedStudioFormGroup as StudioFormGroup };
