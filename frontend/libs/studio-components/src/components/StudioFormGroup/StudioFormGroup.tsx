41406ddcbaf5f1b14ab4edbd57ada297e67184a1
wrt95 <wrt_95@hotmail.com>
2025-05-12 13:02:44 +0200

    add more

--- frontend/libs/studio-components/src/components/StudioFormGroup/StudioFormGroup.tsx
+++ frontend/libs/studio-components/src/components/StudioFormGroup/StudioFormGroup.tsx
@@ -3,17 +3,24 @@ import type { ReactElement, Ref } from 'react';
 import { StudioFieldset, type StudioFieldsetProps } from '../StudioFieldset';
 
 export type StudioFormGroupProps = StudioFieldsetProps & {
+  legend: string;
+  description?: string;
   requiredText?: string;
   required?: boolean;
 };
 
 function StudioFormGroup(
-  { children, requiredText, required, ...rest }: StudioFormGroupProps,
+  { children, legend, description, requiredText, required, ...rest }: StudioFormGroupProps,
   ref: Ref<HTMLFieldSetElement>,
 ): ReactElement {
   return (
     <StudioFieldset {...rest} ref={ref}>
-      <StudioFieldset.Legend></StudioFieldset.Legend>
+      <StudioFieldset.Legend>
+        {legend}
+        {/* ADD TAG */}
+      </StudioFieldset.Legend>
+      {description && <StudioFieldset.Description>{description}</StudioFieldset.Description>}
+      {children}
     </StudioFieldset>
   );
 }
