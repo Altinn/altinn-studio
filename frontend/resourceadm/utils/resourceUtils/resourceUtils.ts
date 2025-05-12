a7b2ce8b278f4516b5900f28dc16bfeb962bfcc3
Martin Gunnerud <mgunnerud@gmail.com>
2025-05-12 14:16:21 +0200

    fix tests

--- frontend/resourceadm/utils/resourceUtils/resourceUtils.ts
+++ frontend/resourceadm/utils/resourceUtils/resourceUtils.ts
@@ -549,8 +549,8 @@ export const convertMetadataStringToConsentMetadata = (metadataString: string): 
 const getConsentMetadataValuesFromText = (text: string): string[] => {
   return text.match(/{([^{}]*?)}/g) ?? [];
 };
-const getUnknownMetadataValuesInText = (metadataValues: ConsentMetadata, consentText: string) => {
-  const metadataKeysInConsentText = getConsentMetadataValuesFromText(consentText);
+const getUnknownMetadataValuesInText = (metadataValues: ConsentMetadata, consentText?: string) => {
+  const metadataKeysInConsentText = getConsentMetadataValuesFromText(consentText ?? '');
   const unknownMetadataValueUsed = metadataKeysInConsentText
     .map((metadataKey) => metadataKey.slice(1, -1))
     .filter((metadataKey) => !metadataValues[metadataKey]);
@@ -562,8 +562,8 @@ const getUnknownMetadataValues = (
   consentTexts: SupportedLanguage,
 ) => {
   return {
-    nb: getUnknownMetadataValuesInText(metadataValues, consentTexts.nb),
-    nn: getUnknownMetadataValuesInText(metadataValues, consentTexts.nn),
-    en: getUnknownMetadataValuesInText(metadataValues, consentTexts.en),
+    nb: getUnknownMetadataValuesInText(metadataValues, consentTexts?.nb),
+    nn: getUnknownMetadataValuesInText(metadataValues, consentTexts?.nn),
+    en: getUnknownMetadataValuesInText(metadataValues, consentTexts?.en),
   };
 };
