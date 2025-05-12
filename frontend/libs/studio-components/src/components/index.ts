3f8e1ab99f59b705c30fad352ed56070ef93f89f
wrt95 <wrt_95@hotmail.com>
2025-05-12 08:34:38 +0200

    add export

--- frontend/libs/studio-components/src/components/index.ts
+++ frontend/libs/studio-components/src/components/index.ts
@@ -22,3 +22,4 @@ export { StudioSpinner } from './StudioSpinner';
 export { StudioTextfield } from './StudioTextfield';
 export { StudioContentMenu } from './StudioContentMenu';
 export type { StudioContentMenuButtonTabProps } from './StudioContentMenu';
+export { StudioCheckbox } from './StudioCheckbox';
