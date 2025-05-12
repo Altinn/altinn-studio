7350d0c392281f8e55d7b148b33636d794ff189e
wrt95 <wrt_95@hotmail.com>
2025-05-12 10:48:38 +0200

    adding legend and description

--- frontend/libs/studio-components/src/components/StudioFieldset/StudioFieldset.test.tsx
+++ frontend/libs/studio-components/src/components/StudioFieldset/StudioFieldset.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import type { ForwardedRef } from 'react';
-import type { StudioFieldsetProps } from './StudioFieldset';
-import { StudioFieldset } from './StudioFieldset';
+import type { StudioFieldsetProps } from './';
+import { StudioFieldset } from './';
 import type { RenderResult } from '@testing-library/react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { testRefForwarding } from '../../test-utils/testRefForwarding';
 import { testRootClassNameAppending } from '../../test-utils/testRootClassNameAppending';
 import { testCustomAttributes } from '../../test-utils/testCustomAttributes';
@@ -20,11 +20,29 @@ describe('StudioFieldset', () => {
   it('Appends custom attributes to the fieldset element', () => {
     testCustomAttributes(renderFieldset);
   });
+
+  it('Renders the legend ', () => {
+    renderFieldset();
+    expect(screen.getByText(legend)).toBeInTheDocument();
+  });
+
+  it('Renders the description ', () => {
+    renderFieldset();
+    expect(screen.getByText(description)).toBeInTheDocument();
+  });
 });
+
+const legend: string = 'Legend';
+const description: string = 'Description';
 
 function renderFieldset(
   props: Partial<StudioFieldsetProps> = {},
   ref?: ForwardedRef<HTMLFieldSetElement>,
 ): RenderResult {
-  return render(<StudioFieldset {...props} ref={ref} />);
+  return render(
+    <StudioFieldset {...props} ref={ref}>
+      <StudioFieldset.Legend>{legend}</StudioFieldset.Legend>
+      <StudioFieldset.Description>{description}</StudioFieldset.Description>
+    </StudioFieldset>,
+  );
 }
