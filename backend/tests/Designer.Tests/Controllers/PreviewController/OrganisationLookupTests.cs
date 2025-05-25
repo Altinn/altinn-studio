281a09dadd783faeb88dde5a467167e85eb15bfa
Michael Queyrichon <michael.queyrichon@digdir.no>
2025-05-25 18:55:44 +0200

    Fix dotnet format

--- backend/tests/Designer.Tests/Controllers/PreviewController/OrganisationLookupTests.cs
+++ backend/tests/Designer.Tests/Controllers/PreviewController/OrganisationLookupTests.cs
@@ -9,7 +9,7 @@ namespace Designer.Tests.Controllers.PreviewController
 {
     public class OrganisationLookupTests : PreviewControllerTestsBase<OrganisationLookupTests>, IClassFixture<WebApplicationFactory<Program>>
     {
-        protected const string organisationNumber = "123456789";
+        protected const string OrganisationNumber = "123456789";
 
         public OrganisationLookupTests(WebApplicationFactory<Program> factory) : base(factory)
         {
@@ -18,7 +18,7 @@ namespace Designer.Tests.Controllers.PreviewController
         [Fact]
         public async Task Get_OrganisationLookup_Ok()
         {
-            string dataPathWithData = $"{Org}/{AppV4}/api/v1/lookup/organisation/{organisationNumber}";
+            string dataPathWithData = $"{Org}/{AppV4}/api/v1/lookup/organisation/{OrganisationNumber}";
             using HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, dataPathWithData);
             httpRequestMessage.Headers.Referrer = new Uri($"{MockedReferrerUrl}?org={Org}&app={AppV4}&selectedLayoutSet=");
 
@@ -26,7 +26,7 @@ namespace Designer.Tests.Controllers.PreviewController
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
             string responseBody = await response.Content.ReadAsStringAsync();
-            Assert.Contains(organisationNumber, responseBody);
+            Assert.Contains(OrganisationNumber, responseBody);
             Assert.Contains("Test AS", responseBody);
         }
     }
