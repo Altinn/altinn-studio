7b73e80901758fd018cf48ec9fffd8360f72b92b
Nina Kylstad <nkylstad@gmail.com>
2025-05-27 21:07:49 +0200

    ensure text keys are generated consistently, and playwright

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/TextMainConfig.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/TextMainConfig.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { FormItem } from '@altinn/ux-editor/types/FormItem';
 import { TextResource } from '../../TextResource/TextResource';
 import { useTranslation } from 'react-i18next';
+import { useAppContext } from '../../../hooks';
 
 const TITLE_TEXT_RESOURCE_KEY = 'title';
 
@@ -17,6 +18,7 @@ export const TextMainConfig = ({
   handleComponentChange,
 }: TextMainConfigProps): React.JSX.Element => {
   const { t } = useTranslation();
+  const { selectedFormLayoutName } = useAppContext();
 
   if (
     componentSchemaTextKeys.length === 0 ||
@@ -48,6 +50,11 @@ export const TextMainConfig = ({
       handleRemoveTextResource={handleRemoveTextResource}
       label={t(`ux_editor.modal_properties_textResourceBindings_${TITLE_TEXT_RESOURCE_KEY}`)}
       textResourceId={component?.textResourceBindings?.[TITLE_TEXT_RESOURCE_KEY]}
+      generateIdOptions={{
+        componentId: component.id,
+        layoutId: selectedFormLayoutName,
+        textResourceKey: TITLE_TEXT_RESOURCE_KEY,
+      }}
     />
   );
 };
