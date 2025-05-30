e91c94a182e554944bb831fe76db5f003cc503ed
Nina Kylstad <nkylstad@gmail.com>
2025-05-30 10:51:35 +0200

    include selectedLayout as prop

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/SpecificMainConfig/OptionsMainConfig.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/SpecificMainConfig/OptionsMainConfig.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { EditOptions } from '../../../config/editModal/EditOptions';
 import type { FormItem } from '../../../../types/FormItem';
 import type { SelectionComponentType } from '../../../../types/FormComponent';
+import { useAppContext } from '../../../../hooks';
 
 export type OptionsMainConfigProps = {
   component: FormItem<SelectionComponentType>;
@@ -12,5 +13,12 @@ export function OptionsMainConfig({
   component,
   handleComponentChange,
 }: OptionsMainConfigProps): React.ReactElement {
-  return <EditOptions component={component} handleComponentChange={handleComponentChange} />;
+  const { selectedFormLayoutName } = useAppContext();
+  return (
+    <EditOptions
+      component={component}
+      handleComponentChange={handleComponentChange}
+      layoutName={selectedFormLayoutName}
+    />
+  );
 }
