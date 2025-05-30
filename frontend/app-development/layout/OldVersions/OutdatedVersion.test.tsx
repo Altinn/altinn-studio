7ad57cf5f3331210e8669e946c1812433000e760
Michael Queyrichon <michael.queyrichon@digdir.no>
2025-05-30 14:47:34 +0200

    Add tests

--- frontend/app-development/layout/OldVersions/OutdatedVersion.test.tsx
+++ frontend/app-development/layout/OldVersions/OutdatedVersion.test.tsx
@@ -7,6 +7,7 @@ import type { ServicesContextProps } from 'app-shared/contexts/ServicesContext';
 import { APP_DEVELOPMENT_BASENAME } from 'app-shared/constants';
 import { app, org } from '@studio/testing/testids';
 import { RoutePaths } from 'app-development/enums/RoutePaths';
+import userEvent from '@testing-library/user-event';
 
 describe('OutdatedVersion', () => {
   afterEach(() => {
@@ -55,6 +56,58 @@ describe('OutdatedVersion', () => {
       screen.queryByRole('heading', { name: textMock('versions.outdated_version') }),
     ).not.toBeInTheDocument();
     expect(screen.queryByRole(textMock('versions.supported_old_version'))).not.toBeInTheDocument();
+  });
+
+  describe('OutdatedVersionRemindChoiceDialog', () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should close popover and not set value in local storage when the "do show again" button is clicked', async () => {
+      render({
+        getAppVersion: () => Promise.resolve({ frontendVersion: '3', backendVersion: '7' }),
+      });
+
+      const user = userEvent.setup();
+
+      // Open popover
+      const closeButton = await screen.findByRole('button', { name: textMock('general.close') });
+      await user.click(closeButton);
+
+      const popover = screen.getByText(textMock('session.reminder'));
+      expect(popover).toBeInTheDocument();
+      const hidePopoverTemporaryButton = screen.getByRole('button', {
+        name: textMock('session.do_show_again'),
+      });
+
+      // Click hide temporary button
+      await user.click(hidePopoverTemporaryButton);
+
+      expect(window.localStorage.getItem('showOutdatedVersionDialog')).toBeNull();
+    });
+
+    it('should close popover and set value in local storage when the "do not show again" is clicked', async () => {
+      render({
+        getAppVersion: () => Promise.resolve({ frontendVersion: '3', backendVersion: '7' }),
+      });
+
+      const user = userEvent.setup();
+
+      // Open popover
+      const closeButton = await screen.findByRole('button', { name: textMock('general.close') });
+      await user.click(closeButton);
+
+      const popover = screen.getByText(textMock('session.reminder'));
+      expect(popover).toBeInTheDocument();
+      const hidePopoverForSessionButton = screen.getByRole('button', {
+        name: textMock('session.dont_show_again'),
+      });
+
+      // Click hide forever button
+      await user.click(hidePopoverForSessionButton);
+
+      expect(window.localStorage.getItem('showOutdatedVersionDialog')).toBe('false');
+    });
   });
 });
 
