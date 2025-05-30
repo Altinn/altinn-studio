e9ad5ef046db126fe5b7628f61734abc1ae99900
wrt95 <wrt_95@hotmail.com>
2025-05-30 08:46:01 +0200

    cleaning up

--- frontend/app-development/features/appSettings/components/TabsContent/Tabs/AboutTab/NewInputFields/ErrorSummary/ErrorSummary.tsx
+++ frontend/app-development/features/appSettings/components/TabsContent/Tabs/AboutTab/NewInputFields/ErrorSummary/ErrorSummary.tsx
@@ -23,19 +23,30 @@ export function ErrorSummary({
         {t('app_settings.about_tab_error_summary_header')}
       </StudioErrorSummary.Heading>
       <StudioErrorSummary.List>
-        {validationErrors.map((error: AppResourceFormError) => {
-          const href: string = getErrorSummaryHref(error);
-          return (
-            <StudioErrorSummary.Item key={JSON.stringify(error)}>
-              <StudioErrorSummary.Link href={href} onClick={() => onClickErrorLink(error.field)}>
-                {error.error}
-              </StudioErrorSummary.Link>
-            </StudioErrorSummary.Item>
-          );
-        })}
+        <ErrorListItems validationErrors={validationErrors} onClickErrorLink={onClickErrorLink} />
       </StudioErrorSummary.List>
     </StudioErrorSummary>
   );
+}
+
+type ErrorListItemsProps = {
+  validationErrors: AppResourceFormError[];
+  onClickErrorLink: (field: TranslationType) => void;
+};
+function ErrorListItems({
+  validationErrors,
+  onClickErrorLink,
+}: ErrorListItemsProps): ReactElement[] {
+  return validationErrors.map((error: AppResourceFormError) => {
+    const href: string = getErrorSummaryHref(error);
+    return (
+      <StudioErrorSummary.Item key={JSON.stringify(error)}>
+        <StudioErrorSummary.Link href={href} onClick={() => onClickErrorLink(error.field)}>
+          {error.error}
+        </StudioErrorSummary.Link>
+      </StudioErrorSummary.Item>
+    );
+  });
 }
 
 function getErrorSummaryHref(error: AppResourceFormError): string {
