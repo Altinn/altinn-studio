a3cf3243b3cce38e66e020589f0bb03120091016
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-06-02 11:03:13 +0200

    feat: mark as completed switch page group config panel (#15537)

--- frontend/packages/ux-editor/src/components/Properties/GroupConfigPanel/GroupConfigPanel.tsx
+++ frontend/packages/ux-editor/src/components/Properties/GroupConfigPanel/GroupConfigPanel.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import { StudioAlert } from '@studio/components';
-import classes from './GroupConfigPanel.module.css';
+import { StudioSpinner, StudioSwitch } from '@studio/components';
 import { useTranslation } from 'react-i18next';
 import { StudioSectionHeader } from '@studio/components-legacy';
 import { FileIcon } from '@studio/icons';
 import type { ItemType } from '../ItemType';
 import type { SelectedItem } from '../../../AppContext';
+import { useAppContext } from '../../../hooks';
+import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
+import { usePagesQuery } from '../../../hooks/queries/usePagesQuery';
+import { useChangePageGroupOrder } from '@altinn/ux-editor/hooks/mutations/useChangePageGroupOrder';
+import classes from './GroupConfigPanel.module.css';
 
-type GroupConfigPanelProps = {
+export type GroupConfigPanelProps = {
   selectedItem: Extract<SelectedItem, { type: ItemType.Group }>;
 };
 
 export const GroupConfigPanel = ({ selectedItem }: GroupConfigPanelProps) => {
   const { t } = useTranslation();
+  const { selectedFormLayoutSetName } = useAppContext();
+  const { org, app } = useStudioEnvironmentParams();
+  const { mutate: changePageGroup, isPending: mutatingPages } = useChangePageGroupOrder(
+    org,
+    app,
+    selectedFormLayoutSetName,
+  );
+  const { data: pages, isPending: pageQueryPending } = usePagesQuery(
+    org,
+    app,
+    selectedFormLayoutSetName,
+  );
+
+  if (pageQueryPending) return <StudioSpinner aria-label={t('general.loading')} />;
+  const selectedGroup = pages.groups[selectedItem.id];
+
+  const onMarkAsCompleted = (event: React.ChangeEvent<HTMLInputElement>) => {
+    selectedGroup.markWhenCompleted = event.target.checked;
+    pages.groups[selectedItem.id] = selectedGroup;
+    changePageGroup(pages);
+  };
 
   return (
     <>
@@ -20,13 +45,21 @@ export const GroupConfigPanel = ({ selectedItem }: GroupConfigPanelProps) => {
         data-testid='groupConfigPanel'
         icon={<FileIcon />}
         heading={{
-          text: selectedItem.id.toString(),
+          text: selectedGroup.name,
           level: 2,
         }}
       />
-      <StudioAlert data-color='info' className={classes.configPanel}>
-        {t('right_menu.content_group_message')}
-      </StudioAlert>
+      <div className={classes.configPanel}>
+        <div className={classes.fieldSetWrapper}>
+          <StudioSwitch
+            label={t('ux_editor.page_group.markAsCompleted_switch')}
+            position='end'
+            readOnly={mutatingPages}
+            checked={selectedGroup.markWhenCompleted || false}
+            onChange={onMarkAsCompleted}
+          ></StudioSwitch>
+        </div>
+      </div>
     </>
   );
 };
