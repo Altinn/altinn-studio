24fd0d1c102699152daae83da3c0cc3f594e6f12
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2025-06-03 11:08:02 +0200

    fix: Add function for updating an entire language (6) (#15548)

--- frontend/packages/shared/src/utils/textResourceUtils.ts
+++ frontend/packages/shared/src/utils/textResourceUtils.ts
@@ -1,4 +1,4 @@
-import type { ITextResource, ITextResources } from 'app-shared/types/global';
+import type { ITextResource, ITextResources, ITextResourcesWithLanguage } from '../types/global';
 import { TextResourceUtils } from '@studio/pure-functions';
 
 export const setTextResourcesForLanguage = (
@@ -14,3 +14,8 @@ export const setTextResourcesForLanguage = (
     [language]: updatedTextResources,
   };
 };
+
+export const updateEntireLanguage = (
+  current: ITextResources,
+  { language, resources }: ITextResourcesWithLanguage,
+): ITextResources => ({ ...current, [language]: resources });
