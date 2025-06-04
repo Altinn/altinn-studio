0ff04ed921162039f61f8531906c93b77fe0d92f
Ole Martin Handeland <github@olemartin.org>
2025-06-04 12:52:26 +0200

    Adding missing EmptyChildrenBoundary when rendering pages in Accordion (#3418)
    Co-authored-by: Ole Martin Handeland <git@olemartin.org>

--- src/layout/Summary2/CommonSummaryComponents/LayoutSetSummaryAccordion.tsx
+++ src/layout/Summary2/CommonSummaryComponents/LayoutSetSummaryAccordion.tsx
@@ -5,6 +5,7 @@ import { Accordion, Label } from '@digdir/designsystemet-react';
 import { Flex } from 'src/app-components/Flex/Flex';
 import { Lang } from 'src/features/language/Lang';
 import classes from 'src/layout/Summary2/CommonSummaryComponents/LayoutSetSummaryAccordion.module.css';
+import { EmptyChildrenBoundary } from 'src/layout/Summary2/isEmpty/EmptyChildrenContext';
 import { PageSummary } from 'src/layout/Summary2/SummaryComponent2/PageSummary';
 
 type LayoutSetAccordionSummaryProps = {
@@ -36,10 +37,12 @@ export function LayoutSetSummaryAccordion({ filteredPages }: LayoutSetAccordionS
               spacing={6}
               alignItems='flex-start'
             >
-              <PageSummary
-                pageId={layoutId}
-                key={layoutId}
-              />
+              <EmptyChildrenBoundary>
+                <PageSummary
+                  pageId={layoutId}
+                  key={layoutId}
+                />
+              </EmptyChildrenBoundary>
             </Flex>
           </Accordion.Content>
         </Accordion.Item>
