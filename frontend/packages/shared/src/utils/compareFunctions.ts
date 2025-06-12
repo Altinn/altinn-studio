b1f127e3d2c100558b7d5cf04fdd85b6a0a7d1ab
lassopicasso <lars.walderhaug@digdir.no>
2025-06-12 10:20:56 +0200

    use const instead of var

--- frontend/packages/shared/src/utils/compareFunctions.ts
+++ frontend/packages/shared/src/utils/compareFunctions.ts
@@ -12,6 +12,6 @@ export const alphabeticalCompareFunction: CompareFunction<string> = (a, b) => {
 export const isBelowSupportedVersion = (currentVersion: string, supportedVersion: number) => {
   if (!currentVersion || !supportedVersion) return true;
 
-  var majorVersion = parseInt(currentVersion.split('.')[0]);
+  const majorVersion = parseInt(currentVersion.split('.')[0]);
   return majorVersion < supportedVersion;
 };
