5bcf9477e59d09241a556b9fa463abc88fa030ef
Adam Haeger <adamgullerud@gmail.com>
2025-06-12 13:12:51 +0200

    added summary2 support for cards (#3431)
    * added summary2 support for cards
    * added hide empty fields components for cards

--- src/layout/Cards/CardsSummary.tsx
+++ src/layout/Cards/CardsSummary.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
 import { SummaryComponent } from 'src/layout/Summary/SummaryComponent';
+import { EmptyChildrenBoundary } from 'src/layout/Summary2/isEmpty/EmptyChildrenContext';
+import { ComponentSummaryById, SummaryFlexForContainer } from 'src/layout/Summary2/SummaryComponent2/ComponentSummary';
+import { useSummaryProp } from 'src/layout/Summary2/summaryStoreContext';
 import { useNode } from 'src/utils/layout/NodesContext';
 import { useNodeItem } from 'src/utils/layout/useNodeItem';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
+import type { Summary2Props } from 'src/layout/Summary2/SummaryComponent2/types';
 
 type Props = Pick<SummaryRendererProps<'Cards'>, 'targetNode' | 'summaryNode' | 'overrides'>;
+
+function Child({ id, summaryNode, overrides }: { id: string | undefined } & Pick<Props, 'summaryNode' | 'overrides'>) {
+  const child = useNode(id);
+  if (!child) {
+    return null;
+  }
+
+  return (
+    <SummaryComponent
+      key={child.id}
+      summaryNode={summaryNode}
+      overrides={{
+        ...overrides,
+        targetNode: child,
+        grid: {},
+        largeGroup: true,
+      }}
+    />
+  );
+}
 
 export function CardsSummary({ targetNode, summaryNode, overrides }: Props) {
   const cardsInternal = useNodeItem(targetNode, (i) => i.cardsInternal);
@@ -25,22 +49,24 @@ export function CardsSummary({ targetNode, summaryNode, overrides }: Props) {
   );
 }
 
-function Child({ id, summaryNode, overrides }: { id: string | undefined } & Pick<Props, 'summaryNode' | 'overrides'>) {
-  const child = useNode(id);
-  if (!child) {
-    return null;
-  }
+export function CardsSummary2({ target }: Summary2Props<'Cards'>) {
+  const item = useNodeItem(target, (i) => i.cardsInternal);
+  const hideEmptyFields = useSummaryProp('hideEmptyFields');
+  const childIds = item
+    .map((card) => card.childIds)
+    .filter((id) => !!id)
+    .flat();
 
   return (
-    <SummaryComponent
-      key={child.id}
-      summaryNode={summaryNode}
-      overrides={{
-        ...overrides,
-        targetNode: child,
-        grid: {},
-        largeGroup: true,
-      }}
-    />
+    <SummaryFlexForContainer
+      hideWhen={hideEmptyFields}
+      target={target}
+    >
+      {childIds.map((childId) => (
+        <EmptyChildrenBoundary key={childId}>
+          <ComponentSummaryById componentId={childId} />
+        </EmptyChildrenBoundary>
+      ))}
+    </SummaryFlexForContainer>
   );
 }
--- src/layout/Cards/index.tsx
+++ src/layout/Cards/index.tsx
@@ -2,10 +2,12 @@ import React, { forwardRef } from 'react';
 import type { JSX } from 'react';
 
 import { Cards as CardsComponent } from 'src/layout/Cards/Cards';
-import { CardsSummary } from 'src/layout/Cards/CardsSummary';
+import { CardsSummary, CardsSummary2 } from 'src/layout/Cards/CardsSummary';
 import { CardsDef } from 'src/layout/Cards/config.def.generated';
+import { EmptyChildrenBoundary } from 'src/layout/Summary2/isEmpty/EmptyChildrenContext';
 import type { PropsFromGenericComponent } from 'src/layout';
 import type { SummaryRendererProps } from 'src/layout/LayoutComponent';
+import type { Summary2Props } from 'src/layout/Summary2/SummaryComponent2/types';
 
 export class Cards extends CardsDef {
   render = forwardRef<HTMLElement, PropsFromGenericComponent<'Cards'>>(
@@ -16,6 +18,14 @@ export class Cards extends CardsDef {
 
   renderSummaryBoilerplate(): boolean {
     return false;
+  }
+
+  renderSummary2(props: Summary2Props<'Cards'>): React.JSX.Element | null {
+    return (
+      <EmptyChildrenBoundary>
+        <CardsSummary2 target={props.target} />
+      </EmptyChildrenBoundary>
+    );
   }
 
   renderSummary({ summaryNode, targetNode, overrides }: SummaryRendererProps<'Cards'>): JSX.Element | null {
