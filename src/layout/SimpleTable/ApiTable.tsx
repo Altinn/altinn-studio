a5afe0baf4d39cf683766b4cf3f5b0d69514a9de
Adam Haeger <adamgullerud@gmail.com>
2025-06-12 14:28:11 +0200

    Fix/api table open in new tab (#3442)
    * open in new tab working for simple table with external data

--- src/layout/SimpleTable/ApiTable.tsx
+++ src/layout/SimpleTable/ApiTable.tsx
@@ -76,7 +76,14 @@ export function ApiTable({ node, externalApi }: ApiTableProps) {
             if (component.type === 'link') {
               const href = pick(component.hrefPath, rowData);
               const text = pick(component.textPath, rowData);
-              return <Link href={href}>{text}</Link>;
+              return (
+                <Link
+                  href={href}
+                  target={component.openInNewTab ? '_blank' : undefined}
+                >
+                  {text}
+                </Link>
+              );
             }
           };
         }
