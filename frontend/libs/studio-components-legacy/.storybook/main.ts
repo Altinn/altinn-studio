76eb93d0703787e4b38115a5a99032cb26eae201
davidovrelid.com <david@framit.no>
2025-06-16 21:27:48 +0200

    removed unused function

--- frontend/libs/studio-components-legacy/.storybook/main.ts
+++ frontend/libs/studio-components-legacy/.storybook/main.ts
@@ -1,14 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
-import { join, dirname } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
