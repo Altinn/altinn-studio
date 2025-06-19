d0734758f4aa48834bbf19232a4a50fb65438e33
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-06-19 15:33:14 +0200

    change the fail service task a bit

--- App/Code/FailServiceTask.cs
+++ App/Code/FailServiceTask.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Altinn.App.Core.Features;
+using Altinn.App.Core.Internal.Data;
 using Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
+using Altinn.App.Core.Models;
+using Altinn.App.Models.Model2;
 using Altinn.Platform.Storage.Interface.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Altinn.App.Code;
 
 public class FailServiceTask : IServiceTask
 {
+    private readonly IDataClient _dataClient;
+
+    public FailServiceTask(IDataClient dataClient)
+    {
+        _dataClient = dataClient;
+    }
+    
     public Task Start(string taskId, Instance instance)
     {
         return Task.CompletedTask;
@@ -25,8 +37,16 @@ public class FailServiceTask : IServiceTask
 
     public string Type => "fail";
 
-    public Task Execute(string taskId, Instance instance, CancellationToken cancellationToken = new CancellationToken())
+    public async Task Execute(string taskId, Instance instance, CancellationToken cancellationToken = new CancellationToken())
     {
-        throw new Exception("This is a failing service task. It is used to test the process engine's error handling capabilities.");
+        DataElement dataElement = instance.Data.Find(x => x.DataType == "Model2");
+
+        var instanceIdentifier = new InstanceIdentifier(instance);
+        var formData = (Model2)await _dataClient.GetFormData(instanceIdentifier.InstanceGuid, typeof(Model2), instance.Org, instance.AppId, int.Parse(instance.InstanceOwner.PartyId), Guid.Parse(dataElement.Id));
+
+        if (formData.fail.HasValue && formData.fail.Value)
+        {
+            throw new Exception("Service task failed as expected.");
+        }
     }
 }
