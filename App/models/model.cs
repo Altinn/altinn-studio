142be0f1cb87de586ad50ecc382325831a735231
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-06-19 10:08:37 +0200

    process

--- App/models/model.cs
+++ App/models/model.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text.Json.Serialization;
 using System.Xml.Serialization;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
 
 namespace Altinn.App.Models.model
