1ffdb96d5125536543e4f642565b9d42f2ef7b18
Erling Hauan <erling.hauan@digdir.no>
2025-06-26 13:20:04 +0200

    feat: dynamic org/app library heading

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.tsx
@@ -4,16 +4,27 @@ import { studioBetaTagClasses, StudioHeading } from '@studio/components-legacy';
 import { useTranslation } from 'react-i18next';
 import { BookIcon } from '@studio/icons';
 import cn from 'classnames';
+import { UrlUtils, StringUtils } from '@studio/pure-functions';
+import { ORG_LIBRARY_BASENAME } from 'app-shared/constants';
 
 export function LibraryHeader(): React.ReactElement {
   const { t } = useTranslation();
+
+  const headingText = isOrgLibraryPage()
+    ? t('org_content_library.library_heading')
+    : t('app_content_library.library_heading');
 
   return (
     <div className={classes.libraryHeading}>
       <BookIcon className={classes.headingIcon} />
       <StudioHeading size='small' className={cn(classes.headingText, studioBetaTagClasses.isBeta)}>
-        {t('app_content_library.library_heading')}
+        {headingText}
       </StudioHeading>
     </div>
   );
+}
+
+function isOrgLibraryPage(): boolean {
+  const currentRoutePath: string = UrlUtils.extractSecondLastRouterParam(location.pathname);
+  return currentRoutePath === StringUtils.removeLeadingSlash(ORG_LIBRARY_BASENAME);
 }
