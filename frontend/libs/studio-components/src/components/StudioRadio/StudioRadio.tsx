73e09c78b609048b51dc6d8af693ecaf49052449
lassopicasso <lars.walderhaug@digdir.no>
2025-06-26 10:11:24 +0200

    migrate radio v1 as a studio component

---
+++ frontend/libs/studio-components/src/components/StudioRadio/StudioRadio.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react';
+import type { ReactElement, Ref } from 'react';
+import { Radio, type RadioProps } from '@digdir/designsystemet-react';
+import { hasAriaLabel, hasAriaLabelledBy } from '../../utils/labelUtils';
+import { StudioLabelWrapper } from '../StudioLabelWrapper';
+
+export type StudioRadioProps = RadioProps;
+
+function StudioRadio(
+  { label, ...rest }: StudioRadioProps,
+  ref: Ref<HTMLInputElement>,
+): ReactElement {
+  // Designsystemet has conditional types, so if we extract label from props, we must
+  // check if the usage has aria-labelledby or aria-label and if true not use the label.
+  if (hasAriaLabelledBy(rest) || hasAriaLabel(rest)) {
+    return <Radio ref={ref} {...rest} />;
+  }
+
+  return <Radio {...rest} ref={ref} label={<StudioLabelWrapper>{label}</StudioLabelWrapper>} />;
+}
+const ForwardedStudioRadio = forwardRef(StudioRadio);
+
+export { ForwardedStudioRadio as StudioRadio };
---
+++ frontend/libs/studio-components/src/components/StudioRadio/index.ts
@@ -0,0 +1,1 @@
+export { StudioRadio } from './StudioRadio';
