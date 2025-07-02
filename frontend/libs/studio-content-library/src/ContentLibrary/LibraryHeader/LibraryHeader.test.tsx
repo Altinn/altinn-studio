ebbe1296f86736a1a4598a9df565c1f2689b6bfa
Tomas <tomas.engebretsen@gmail.com>
2025-07-02 10:10:46 +0200

    Add heading text to config

--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.test.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.test.tsx
@@ -1,61 +1,37 @@
 import React from 'react';
-import { textMock } from '@studio/testing/mocks/i18nMock';
+import type { LibraryHeaderProps } from './LibraryHeader';
 import { LibraryHeader } from './LibraryHeader';
 import { render, screen } from '@testing-library/react';
 import { RouterContext } from '../../contexts/RouterContext';
-import { DASHBOARD_BASENAME, ORG_LIBRARY_BASENAME } from 'app-shared/constants';
 
 const navigateMock = jest.fn();
 
-const originalUrl = location.href;
-const originalHistoryData = history.state;
+// Test data:
+const children = 'The test library';
+const defaultProps: LibraryHeaderProps = { children };
 
 describe('LibraryHeader', () => {
-  afterEach(() => {
-    replaceCurrentUrl(originalUrl);
-  });
-
   it('renders header icon', () => {
     renderLibraryHeader();
     const libraryIcon = screen.getByRole('img');
     expect(libraryIcon).toBeInTheDocument();
   });
 
-  it('renders library header with app text as default', () => {
+  it('renders library header with the given text', () => {
     renderLibraryHeader();
-    const appLibraryHeader = screen.getByRole('heading', {
-      name: textMock('app_content_library.library_heading'),
-    });
-    expect(appLibraryHeader).toBeInTheDocument();
-  });
-
-  it('renders library header with org text for org paths', () => {
-    const orgPath = `${DASHBOARD_BASENAME}${ORG_LIBRARY_BASENAME}/test-org`;
-    replaceCurrentUrl(orgPath);
-    renderLibraryHeader();
-    const orgLibraryHeader = screen.getByRole('heading', {
-      name: textMock('org_content_library.library_heading'),
-    });
-    expect(orgLibraryHeader).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: children })).toBeInTheDocument();
   });
 
   it('renders the content library header with isBeta class', () => {
     renderLibraryHeader();
-    const libraryHeader = screen.getByRole('heading', {
-      name: textMock('app_content_library.library_heading'),
-    });
-    expect(libraryHeader).toHaveClass('isBeta');
+    expect(screen.getByRole('heading')).toHaveClass('isBeta');
   });
 });
 
-const renderLibraryHeader = () => {
+const renderLibraryHeader = (): void => {
   render(
     <RouterContext.Provider value={{ currentPage: 'codeList', navigate: navigateMock }}>
-      <LibraryHeader />
+      <LibraryHeader {...defaultProps} />
     </RouterContext.Provider>,
   );
-};
-
-const replaceCurrentUrl = (newUrl: string) => {
-  history.replaceState(originalHistoryData, '', newUrl);
 };
--- frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.tsx
+++ frontend/libs/studio-content-library/src/ContentLibrary/LibraryHeader/LibraryHeader.tsx
@@ -1,31 +1,19 @@
+import type { PropsWithChildren } from 'react';
 import React from 'react';
 import classes from './LibraryHeader.module.css';
 import { studioBetaTagClasses, StudioHeading } from '@studio/components-legacy';
-import { useTranslation } from 'react-i18next';
 import { BookIcon } from '@studio/icons';
 import cn from 'classnames';
-import { UrlUtils, StringUtils } from '@studio/pure-functions';
-import { ORG_LIBRARY_BASENAME } from 'app-shared/constants';
 
-export function LibraryHeader(): React.ReactElement {
-  const { t } = useTranslation();
+export type LibraryHeaderProps = PropsWithChildren<{}>;
 
-  const headingText = isOrgLibraryPage()
-    ? t('org_content_library.library_heading')
-    : t('app_content_library.library_heading');
-
+export function LibraryHeader({ children }: LibraryHeaderProps): React.ReactElement {
   return (
     <div className={classes.libraryHeading}>
       <BookIcon className={classes.headingIcon} />
       <StudioHeading size='small' className={cn(classes.headingText, studioBetaTagClasses.isBeta)}>
-        {headingText}
+        {children}
       </StudioHeading>
     </div>
   );
-}
-
-function isOrgLibraryPage(): boolean {
-  const orgLibraryPathSegment: string = StringUtils.removeLeadingSlash(ORG_LIBRARY_BASENAME);
-  const secondLastPathSegment: string = UrlUtils.extractSecondLastRouterParam(location.pathname);
-  return orgLibraryPathSegment === secondLastPathSegment;
 }
