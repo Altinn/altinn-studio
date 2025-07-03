ae18193ee5f1cb56b55c1b30e564aed3e68fb084
davidovrelid.com <david@framit.no>
2025-07-03 08:47:53 +0200

    refactor: simplify layout-set construction within unit-tests

--- frontend/packages/ux-editor/src/utils/getLayoutSetIcon.test.tsx
+++ frontend/packages/ux-editor/src/utils/getLayoutSetIcon.test.tsx
@@ -5,11 +5,11 @@ import { QuestionmarkIcon } from '@studio/icons';
 
 describe('getLayoutSetIcon', () => {
   it('should return default icon for unknown types', () => {
-    const layoutSet: LayoutSetModel = {
+    const layoutSet = constructLayoutSetModel({
       id: 'unknown-id',
       dataType: '',
       type: 'unknown-type',
-    };
+    });
     const result = getLayoutSetIcon(layoutSet);
 
     expect(result.icon.type).toBe((<QuestionmarkIcon />).type);
@@ -17,15 +17,12 @@ describe('getLayoutSetIcon', () => {
   });
 
   it('should return icon and iconColor for custom receipt', () => {
-    const layoutSet: LayoutSetModel = {
-      id: '',
-      dataType: '',
-      type: '',
+    const layoutSet = constructLayoutSetModel({
       task: {
         id: 'CustomReceipt',
         type: '',
       },
-    };
+    });
     const result = getLayoutSetIcon(layoutSet);
     expect(result.icon).toBeTruthy();
     expect(result.icon.type).not.toBe((<QuestionmarkIcon />).type);
@@ -33,11 +30,10 @@ describe('getLayoutSetIcon', () => {
   });
 
   it('should return icon and iconColor for subform', () => {
-    const layoutSet: LayoutSetModel = {
-      id: '',
-      dataType: '',
+    const layoutSet = constructLayoutSetModel({
       type: 'subform',
-    };
+    });
+
     const result = getLayoutSetIcon(layoutSet);
     expect(result.icon).toBeTruthy();
     expect(result.icon.type).not.toBe((<QuestionmarkIcon />).type);
@@ -45,15 +41,12 @@ describe('getLayoutSetIcon', () => {
   });
 
   it('should return icon and iconColor for data tasks', () => {
-    const layoutSet: LayoutSetModel = {
-      id: '',
-      dataType: '',
-      type: '',
+    const layoutSet = constructLayoutSetModel({
       task: {
         id: '',
         type: 'data',
       },
-    };
+    });
     const result = getLayoutSetIcon(layoutSet);
     expect(result.icon).toBeTruthy();
     expect(result.icon.type).not.toBe((<QuestionmarkIcon />).type);
@@ -61,18 +54,28 @@ describe('getLayoutSetIcon', () => {
   });
 
   it('should return icon and iconColor for signing tasks', () => {
-    const layoutSet: LayoutSetModel = {
-      id: '',
-      dataType: '',
-      type: '',
+    const layoutSet = constructLayoutSetModel({
       task: {
         id: '',
         type: 'signing',
       },
-    };
+    });
     const result = getLayoutSetIcon(layoutSet);
     expect(result.icon).toBeTruthy();
     expect(result.icon.type).not.toBe((<QuestionmarkIcon />).type);
     expect(result.iconColor).toBeTruthy();
   });
 });
+
+function constructLayoutSetModel(overrides: Partial<LayoutSetModel>): LayoutSetModel {
+  return {
+    id: '',
+    dataType: '',
+    type: '',
+    task: {
+      id: '',
+      type: '',
+    },
+    ...overrides,
+  };
+}
