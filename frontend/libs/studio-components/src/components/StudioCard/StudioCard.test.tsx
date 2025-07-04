d486d3b7abb22e6ae74b36e00d4b12003557c795
Tomas Engebretsen <tomas.engebretsen@digdir.no>
2025-07-04 12:26:46 +0200

    test: Ensure children are rendered in StudioCard test (#15827)
    Co-authored-by: Lars <74791975+lassopicasso@users.noreply.github.com>

--- frontend/libs/studio-components/src/components/StudioCard/StudioCard.test.tsx
+++ frontend/libs/studio-components/src/components/StudioCard/StudioCard.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import type { ForwardedRef } from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import type { RenderResult } from '@testing-library/react';
 import { StudioCard } from './StudioCard';
 import type { StudioCardProps } from './StudioCard';
 import { testRootClassNameAppending } from '../../test-utils/testRootClassNameAppending';
 import { testCustomAttributes } from '../../test-utils/testCustomAttributes';
 import { testRefForwarding } from '../../test-utils/testRefForwarding';
+
+// Test data:
+const children = 'Lorem ipsum dolor sit amet';
+const defaultProps: StudioCardProps = { children };
 
 describe('StudioCard', () => {
   it('Appends custom attributes to the card element', () => {
@@ -20,14 +24,14 @@ describe('StudioCard', () => {
   it('should support forwarding the ref', () => {
     testRefForwarding<HTMLDivElement>((ref) => renderCard({}, ref));
   });
+
+  it('Renders children', () => {
+    renderCard();
+    expect(screen.getByText(children)).toBeInTheDocument();
+  });
 });
 
 const renderCard = (
   props: Partial<StudioCardProps> = {},
   ref?: ForwardedRef<HTMLDivElement>,
-): RenderResult =>
-  render(
-    <StudioCard {...props} ref={ref}>
-      Children
-    </StudioCard>,
-  );
+): RenderResult => render(<StudioCard {...defaultProps} {...props} ref={ref} />);
