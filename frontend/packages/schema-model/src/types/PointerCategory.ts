e6b1e8c1f9c03d112416a31aa8f561ecf53445ea
davidovrelid.com <david@framit.no>
2025-07-04 09:57:23 +0200

    clean up

--- frontend/packages/schema-model/src/types/PointerCategory.ts
+++ frontend/packages/schema-model/src/types/PointerCategory.ts
@@ -1,12 +1,10 @@
 import type { Keyword } from './Keyword';
 import type { CombinationKind } from './CombinationKind';
-import type { Items } from '@altinn/schema-model/types/Items';
 
 export type PointerCategory =
   | Keyword.Properties
   | Keyword.Definitions
   | Keyword.Items
-  | Items
   | `${Keyword.Items}/${Keyword.Properties}`
   | CombinationKind
   | `${Keyword.Items}/${CombinationKind}`;
