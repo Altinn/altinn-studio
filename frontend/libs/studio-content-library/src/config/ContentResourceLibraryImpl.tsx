8f60f619cc366ecaef7702c6d55c7ea078046a28
Erling Hauan <148075168+ErlingHauan@users.noreply.github.com>
2025-07-07 14:35:56 +0200

    feat: dynamic org/app library heading (#15780)
    Co-authored-by: Gørild Døhl <gorild.dohl@digdir.no>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Tomas Engebretsen <tomas.engebretsen@digdir.no>
    Co-authored-by: Tomas <tomas.engebretsen@gmail.com>
    Co-authored-by: Konrad-Simso <konrad.simso@digdir.no>
    Co-authored-by: Lars <74791975+lassopicasso@users.noreply.github.com>

--- frontend/libs/studio-content-library/src/config/ContentResourceLibraryImpl.tsx
+++ frontend/libs/studio-content-library/src/config/ContentResourceLibraryImpl.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import type { PagesConfig } from '../types/PagesProps';
 import { RouterContextProvider } from '../contexts/RouterContext';
 import { ContentLibrary } from '../ContentLibrary/ContentLibrary';
+import type { ContentLibraryConfig } from '../types/ContentLibraryConfig';
 
 export class ResourceContentLibraryImpl {
   private readonly pages: PagesConfig;
+  private readonly heading: string;
 
-  constructor(config: { pages: PagesConfig }) {
+  constructor(config: ContentLibraryConfig) {
     this.pages = config.pages;
+    this.heading = config.heading;
     this.getContentResourceLibrary = this.getContentResourceLibrary.bind(this);
   }
 
   public getContentResourceLibrary(): React.ReactNode {
     return (
       <RouterContextProvider>
-        <ContentLibrary pages={this.pages} />
+        <ContentLibrary heading={this.heading} pages={this.pages} />
       </RouterContextProvider>
     );
   }
