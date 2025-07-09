dfec256fca4c26ab200fd8b9d2fd2563612030a6
Camilla Marie Dalan <camillamdalan@gmail.com>
2025-07-09 12:59:39 +0200

    adds logic for not failing service task if fail serivce task in model3 is false

--- App/Code/FailServiceTask.cs
+++ App/Code/FailServiceTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Altinn.App.Core.Features;
@@ -7,6 +8,7 @@ using Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
 using Altinn.App.Core.Models;
 using Altinn.App.Core.Models.Process;
 using Altinn.App.Models.Model2;
+using Altinn.App.Models.Model3;
 using Altinn.Platform.Storage.Interface.Models;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,19 +22,42 @@ public class FailServiceTask : IServiceTask
     {
         _dataClient = dataClient;
     }
-    
+
     public string Type => "fail";
 
     public async Task<ServiceTaskResult> Execute(ServiceTaskParameters parameters)
     {
         Instance instance = parameters.InstanceDataMutator.Instance;
-        
-        DataElement dataElement = instance.Data.Find(x => x.DataType == "Model2");
+
+        DataElement dataElement2 = instance.Data.Find(x => x.DataType == "Model2");
+        DataElement dataElement3 = instance.Data.Find(x => x.DataType == "Model3");
 
         var instanceIdentifier = new InstanceIdentifier(instance);
-        var formData = (Model2)await _dataClient.GetFormData(instanceIdentifier.InstanceGuid, typeof(Model2), instance.Org, instance.AppId, int.Parse(instance.InstanceOwner.PartyId), Guid.Parse(dataElement.Id));
+        var formDataModel2 = (Model2)
+            await _dataClient.GetFormData(
+                instanceIdentifier.InstanceGuid,
+                typeof(Model2),
+                instance.Org,
+                instance.AppId,
+                int.Parse(instance.InstanceOwner.PartyId),
+                Guid.Parse(dataElement2.Id)
+            );
 
-        if (formData.fail.HasValue && formData.fail.Value)
+        var formDataModel3 = (Model3)
+            await _dataClient.GetFormData(
+                instanceIdentifier.InstanceGuid,
+                typeof(Model3),
+                instance.Org,
+                instance.AppId,
+                int.Parse(instance.InstanceOwner.PartyId),
+                Guid.Parse(dataElement3.Id)
+            );
+
+        if (
+            formDataModel2.fail.HasValue
+            && formDataModel2.fail.Value
+            && (!formDataModel3.fail.HasValue || formDataModel3.fail.Value)
+        )
         {
             return new ServiceTaskFailedResult
             {
