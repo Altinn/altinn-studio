c94769eb57ddfbfc1c39525fa405da61ea22506d
Michael <michael.queyrichon@digdir.no>
2025-07-10 09:07:06 +0200

    feat: Add endpoints for application logs, operational logs and failed requests (#15838)

---
+++ src/KubernetesWrapper/src/Models/AppFailedRequest.cs
@@ -0,0 +1,8 @@
+namespace KubernetesWrapper.Models;
+
+public class AppFailedRequest
+{
+    public DateTime TimeGenerated { get; set; }
+    public string Url { get; set; }
+    public string ResultCode { get; set; }
+}
