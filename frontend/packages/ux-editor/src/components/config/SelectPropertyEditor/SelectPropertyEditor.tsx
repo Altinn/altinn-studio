c22a70a1c08d0ab4eb56f198bbb47f1ab7089355
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-07-10 11:43:11 +0200

    fixed a part of alignment

--- frontend/packages/ux-editor/src/components/config/SelectPropertyEditor/SelectPropertyEditor.tsx
+++ frontend/packages/ux-editor/src/components/config/SelectPropertyEditor/SelectPropertyEditor.tsx
@@ -3,7 +3,6 @@ import { XMarkIcon } from '@studio/icons';
 import { StudioButton, StudioProperty } from '@studio/components-legacy';
 import classes from './SelectPropertyEditor.module.css';
 import { useTranslation } from 'react-i18next';
-import cn from 'classnames';
 
 export type SelectPropertyEditorProps = {
   children?: React.ReactNode;
@@ -21,27 +20,30 @@ export const SelectPropertyEditor = ({
   const { t } = useTranslation();
   const [dataTypeSelectVisible, setDataTypeSelectVisible] = useState(false);
 
-  return (
-    <div className={cn(dataTypeSelectVisible ? classes.container : classes.viewMode)}>
-      {dataTypeSelectVisible ? (
-        <div className={classes.editSelectProperty}>
-          <div className={classes.selectProperty}>{children}</div>
-          <StudioButton
-            icon={<XMarkIcon />}
-            onClick={() => setDataTypeSelectVisible(false)}
-            title={t('general.close')}
-            variant='secondary'
-          />
-        </div>
-      ) : (
-        <StudioProperty.Button
-          onClick={() => setDataTypeSelectVisible(true)}
-          property={property}
-          title={title}
-          value={value}
-          className={classes.viewSelectProperty}
+  if (dataTypeSelectVisible) {
+    return (
+      <div className={classes.editSelectProperty}>
+        <div className={classes.selectProperty}>{children}</div>
+        <StudioButton
+          icon={<XMarkIcon />}
+          onClick={() => setDataTypeSelectVisible(false)}
+          title={t('general.close')}
+          variant='secondary'
+          className={classes.closeButton}
         />
-      )}
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.viewMode}>
+      <StudioProperty.Button
+        onClick={() => setDataTypeSelectVisible(true)}
+        property={property}
+        title={title}
+        value={value}
+        className={classes.viewSelectProperty}
+      />
     </div>
   );
 };
