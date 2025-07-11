6f7a4c7d1f4975ea85d3a21b5fa50219e85e5f41
Tomas <tomas.engebretsen@gmail.com>
2025-07-11 11:34:56 +0200

    fix: Remove deprecated Storybook addons

--- frontend/libs/studio-components-legacy/.storybook/main.ts
+++ frontend/libs/studio-components-legacy/.storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs',
-  ],
+  addons: ['@storybook/addon-links', '@chromatic-com/storybook', '@storybook/addon-docs'],
   features: {
     buildStoriesJson: true,
   },
