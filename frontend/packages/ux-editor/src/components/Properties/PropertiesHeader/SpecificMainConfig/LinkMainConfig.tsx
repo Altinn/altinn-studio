20f91f94528b98e6d9a4c03b5943bab67e62d757
lassopicasso <lars.walderhaug@digdir.no>
2025-07-18 11:47:19 +0200

    relative import

--- frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/SpecificMainConfig/LinkMainConfig.tsx
+++ frontend/packages/ux-editor/src/components/Properties/PropertiesHeader/SpecificMainConfig/LinkMainConfig.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import type { FormItem } from '@altinn/ux-editor/types/FormItem';
-import type { ComponentType } from 'app-shared/types/ComponentType';
-import { ConfigStringProperties } from '@altinn/ux-editor/components/config/ConfigProperties';
-import { useComponentSchemaQuery } from '@altinn/ux-editor/hooks/queries/useComponentSchemaQuery';
+import type { FormItem } from '../../../../types/FormItem';
+import type { ComponentType } from '../../../../../../shared/src/types/ComponentType';
+import { ConfigStringProperties } from '../../../config/ConfigProperties/ConfigStringProperties';
+import { useComponentSchemaQuery } from '../../../../hooks/queries/useComponentSchemaQuery';
 import type { properties } from '../../../../testing/schemas/json/component/Link.schema.v1.json';
 
 type LinkMainProperties = (keyof typeof properties)[];
