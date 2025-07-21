e03f3d80b8f4ae96f7ca621f7ae82f78415c0184
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-07-21 11:57:23 +0200

    changed color icon to green

--- frontend/packages/ux-editor/src/utils/getLayoutSetIcon.tsx
+++ frontend/packages/ux-editor/src/utils/getLayoutSetIcon.tsx
@@ -31,7 +31,7 @@ const iconByTaskTypeMap: Partial<Record<BpmnTaskType, IconMetaData>> = {
   data: { icon: <DataTaskIcon />, iconColor: 'blue' },
   signing: { icon: <SignTaskIcon />, iconColor: 'red' },
   payment: { icon: <CardIcon />, iconColor: 'yellow' },
-  userControlledSigning: { icon: <UserControlledSigningIcon />, iconColor: 'red' },
+  userControlledSigning: { icon: <UserControlledSigningIcon />, iconColor: 'green' },
 };
 
 const iconByTaskIdMap: Record<string, IconMetaData> = {
