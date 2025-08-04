fac533ac914d00d76b4cfce56d2e3f5caf430d5d
Erling Hauan <erling.hauan@digdir.no>
2025-08-04 14:15:50 +0200

    refactor renderUsePrevious

--- frontend/libs/studio-components/src/hooks/usePrevious.test.ts
+++ frontend/libs/studio-components/src/hooks/usePrevious.test.ts
@@ -1,32 +1,28 @@
+import type { RenderHookResult } from '@testing-library/react';
 import { renderHook } from '@testing-library/react';
 import { usePrevious } from './usePrevious';
 
+const renderUsePrevious = <T>(initialProps: {
+  state: T;
+}): RenderHookResult<T | undefined, { state: T }> =>
+  renderHook(({ state }) => usePrevious(state), { initialProps });
+
 describe('usePrevious', () => {
-  it('Returns undefined on initial render', () => {
-    const { result } = renderHook(() => usePrevious(0));
+  test('Returns undefined on initial render', () => {
+    const { result } = renderUsePrevious({ state: 0 });
     expect(result.current).toBeUndefined();
   });
 
-  it('Returns previous state after rerender', () => {
-    let state = 0;
-    const { result, rerender } = renderHook(() => usePrevious(state));
-
+  test('Returns previous state after rerender', () => {
+    const { result, rerender } = renderUsePrevious({ state: 0 });
     expect(result.current).toBeUndefined();
-
-    state = 1;
-    rerender();
+    rerender({ state: 1 });
     expect(result.current).toBe(0);
-
-    state = 2;
-    rerender();
+    rerender({ state: 2 });
     expect(result.current).toBe(1);
-
-    state = 3;
-    rerender();
+    rerender({ state: 4 });
     expect(result.current).toBe(2);
-
-    state = 4;
-    rerender();
-    expect(result.current).toBe(3);
+    rerender({ state: 8 });
+    expect(result.current).toBe(4);
   });
 });
