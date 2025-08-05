e13d61a3de0199408a799932f42ae3fc7e2abbfb
Erling Hauan <erling.hauan@digdir.no>
2025-08-05 13:12:00 +0200

    remove unused import

--- frontend/libs/studio-components-legacy/src/hooks/useMediaQuery.test.ts
+++ frontend/libs/studio-components-legacy/src/hooks/useMediaQuery.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { useMediaQuery } from './useMediaQuery';
 import MockedFunction = jest.MockedFunction;
 
