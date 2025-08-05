05b3c08c374793eab8dfd6670fef43a68059d9fe
Erling Hauan <erling.hauan@digdir.no>
2025-08-05 12:55:46 +0200

    refactor: useMediaQuery

--- frontend/libs/studio-components-legacy/src/hooks/useMediaQuery.ts
+++ frontend/libs/studio-components-legacy/src/hooks/useMediaQuery.ts
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react';
 
+function getMatches(query: string): boolean {
+  return window.matchMedia(query).matches ?? false;
+}
+
 export function useMediaQuery(query: string): boolean {
-  const getMatches = (query: string): boolean => window?.matchMedia(query).matches ?? false;
-
-  const [matches, setMatches] = useState<boolean>(getMatches(query));
-
-  const eventListener = () => {
-    setMatches(getMatches(query));
-  };
+  const [matches, setMatches] = useState<boolean>(() => getMatches(query));
 
   useEffect(() => {
-    const matchMedia = window.matchMedia(query);
-    eventListener();
-    matchMedia.addEventListener('change', eventListener);
-    return () => matchMedia.removeEventListener('change', eventListener);
-  }, [query]); // eslint-disable-line react-hooks/exhaustive-deps
+    const updateMatches = (): void => {
+      setMatches(getMatches(query));
+    };
+
+    updateMatches();
+
+    const mediaQueryList = window.matchMedia(query);
+    mediaQueryList.addEventListener('change', updateMatches);
+    return (): void => mediaQueryList.removeEventListener('change', updateMatches);
+  }, [query]);
 
   return matches;
 }
