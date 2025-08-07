353a514d1d7cd3e6b716a1ae8cca12e91bab5a07
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-08-07 10:43:49 +0200

    refactor: use the testapprouter in v3 rendering mock

--- frontend/packages/ux-editor-v3/src/components/TextResourceEdit.test.tsx
+++ frontend/packages/ux-editor-v3/src/components/TextResourceEdit.test.tsx
@@ -15,11 +15,6 @@ import { appContextMock } from '../testing/appContextMock';
 import { app, org } from '@studio/testing/testids';
 import { textMock } from '@studio/testing/mocks/i18nMock';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useParams: () => ({ org, app }),
-}));
-
 const user = userEvent.setup();
 
 // Test data:
