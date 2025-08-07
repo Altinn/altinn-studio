b3dc6a3702cc653e90e6ab4cf5dbd1c1d4ca15b5
Erling Hauan <erling.hauan@digdir.no>
2025-08-07 14:42:29 +0200

    add type for component args

--- frontend/libs/studio-components/src/components/StudioDetails/StudioDetails.stories.tsx
+++ frontend/libs/studio-components/src/components/StudioDetails/StudioDetails.stories.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import type { ReactElement } from 'react';
 import type { Meta } from '@storybook/react-vite';
 import { StudioDetails } from './index';
+import type { StudioDetailsProps } from './StudioDetails';
 import { StudioCard } from '../StudioCard';
 
-const ComposedComponent = (args): ReactElement => (
+const ComposedComponent = (args: StudioDetailsProps): ReactElement => (
   <StudioDetails {...args}>
     <StudioDetails.Summary>Details heading text</StudioDetails.Summary>
     <StudioDetails.Content>Details content</StudioDetails.Content>
   </StudioDetails>
 );
 
-const ComposedComponentInCard = (args): ReactElement => (
+const ComposedComponentInCard = (args: StudioDetailsProps): ReactElement => (
   <StudioCard>
     <StudioDetails {...args}>
       <StudioDetails.Summary>Details heading text</StudioDetails.Summary>
@@ -31,9 +32,11 @@ const meta: Meta<typeof StudioDetails> = {
   },
 };
 
-export const Preview = (args): ReactElement => <ComposedComponent {...args} />;
+export const Preview = (args: StudioDetailsProps): ReactElement => <ComposedComponent {...args} />;
 
-export const InCard = (args): ReactElement => <ComposedComponentInCard {...args} />;
+export const InCard = (args: StudioDetailsProps): ReactElement => (
+  <ComposedComponentInCard {...args} />
+);
 
 Preview.args = {
   'data-size': 'sm',
