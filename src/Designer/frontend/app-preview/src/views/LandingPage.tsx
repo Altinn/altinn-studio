b58dcd9c4747b5e4c3409fc2ae129b4623114f48
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-08-07 14:43:19 +0200

    fix: function return type

--- src/Designer/frontend/app-preview/src/views/LandingPage.tsx
+++ src/Designer/frontend/app-preview/src/views/LandingPage.tsx
@@ -120,7 +120,7 @@ export const LandingPage = () => {
   );
 };
 
-const getSelectedFormLayoutSetName = (layoutSet: string): string => {
+const getSelectedFormLayoutSetName = (layoutSet: string): string | undefined => {
   if (layoutSet === '') return undefined;
   return layoutSet;
 };
