0f5f164d38c60081ce0356965a590675a6e40e95
wrt95 <wrt_95@hotmail.com>
2025-08-07 14:30:06 +0200

    chore: Move StudioTabs to new studio-components

--- frontend/libs/studio-components/src/components/StudioTabs/index.ts
+++ frontend/libs/studio-components/src/components/StudioTabs/index.ts
@@ -1,8 +1,5 @@
 import { Tabs } from '@digdir/designsystemet-react';
 import type { TabsProps } from '@digdir/designsystemet-react';
 
-/**
- * @deprecated Use `StudioTabs` from `@studio/components` instead.
- */
 export const StudioTabs = Tabs;
 export type StudioTabsProps = TabsProps;
