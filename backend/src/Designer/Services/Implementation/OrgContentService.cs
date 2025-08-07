a376f9592663b20a5c357feb43bd86876660acfe
Konrad-Simso <konrad.simso@digdir.no>
2025-08-07 15:13:53 +0200

    Formatting, remove unused import.

--- backend/src/Designer/Services/Implementation/OrgContentService.cs
+++ backend/src/Designer/Services/Implementation/OrgContentService.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Altinn.Studio.Designer.Enums;
 using Altinn.Studio.Designer.Models;
