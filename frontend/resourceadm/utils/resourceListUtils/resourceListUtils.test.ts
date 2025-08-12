0a3b500d1453c02cc52ffed92646501c853812f3
Martin Gunnerud <mgunnerud@gmail.com>
2025-08-12 10:34:59 +0200

    handle resources without title property in dashboard search

--- frontend/resourceadm/utils/resourceListUtils/resourceListUtils.test.ts
+++ frontend/resourceadm/utils/resourceListUtils/resourceListUtils.test.ts
@@ -1,42 +1,42 @@
 import { filterTableData } from './resourceListUtils';
 import type { ResourceListItem } from 'app-shared/types/ResourceAdm';
 
+const dataToFilter: ResourceListItem[] = [
+  {
+    title: { nb: 'Test', nn: 'Test', en: 'Test' },
+    createdBy: 'William',
+    lastChanged: new Date('2023-08-24'),
+    identifier: 'resource-id',
+    environments: ['gitea'],
+  },
+  {
+    title: { nb: 'Test 2', nn: 'Test 2', en: 'Test 2' },
+    createdBy: 'William',
+    lastChanged: new Date('2023-08-24'),
+    identifier: 'res2',
+    environments: ['gitea'],
+  },
+  {
+    title: null,
+    createdBy: 'William',
+    lastChanged: new Date('2023-08-24'),
+    identifier: 'res3',
+    environments: ['gitea'],
+  },
+];
+
+const expectedResult: ResourceListItem[] = [
+  {
+    title: { nb: 'Test 2', nn: 'Test 2', en: 'Test 2' },
+    createdBy: 'William',
+    lastChanged: new Date('2023-08-24'),
+    identifier: 'res2',
+    environments: ['gitea'],
+  },
+];
+
 describe('filterTableData', () => {
   it('Filter out the data searched for', () => {
-    const dataToFilter: ResourceListItem[] = [
-      {
-        title: { nb: 'Test', nn: 'Test', en: 'Test' },
-        createdBy: 'William',
-        lastChanged: new Date('2023-08-24'),
-        identifier: 'resource-id',
-        environments: ['gitea'],
-      },
-      {
-        title: { nb: 'Test 2', nn: 'Test 2', en: 'Test 2' },
-        createdBy: 'William',
-        lastChanged: new Date('2023-08-24'),
-        identifier: 'res2',
-        environments: ['gitea'],
-      },
-      {
-        title: { nb: '123', nn: '123', en: '123' },
-        createdBy: 'William',
-        lastChanged: new Date('2023-08-24'),
-        identifier: 'res3',
-        environments: ['gitea'],
-      },
-    ];
-
-    const expectedResult: ResourceListItem[] = [
-      {
-        title: { nb: 'Test 2', nn: 'Test 2', en: 'Test 2' },
-        createdBy: 'William',
-        lastChanged: new Date('2023-08-24'),
-        identifier: 'resource-id',
-        environments: ['gitea'],
-      },
-    ];
-
     let result = filterTableData('sT 2', dataToFilter);
     expect(result).toEqual(expectedResult);
 
@@ -53,6 +53,11 @@ describe('filterTableData', () => {
     expect(result).toEqual(expectedResult);
 
     result = filterTableData('resource-id', dataToFilter);
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual([dataToFilter[0]]);
+  });
+
+  it('Retuns all resources when search string is empty', () => {
+    const result = filterTableData('', dataToFilter);
+    expect(result).toEqual(dataToFilter);
   });
 });
