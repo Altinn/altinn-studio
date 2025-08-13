eb1a4c1ac72f33c412298e19a8d39fc000feceed
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2025-08-13 13:20:20 +0200

    button migrations to v1

--- frontend/libs/studio-icons/src/index.ts
+++ frontend/libs/studio-icons/src/index.ts
@@ -6,4 +6,5 @@ export * from './react/types';
 
 export { CheckmarkIcon as StudioSaveIcon } from '@navikt/aksel-icons';
 export { XMarkIcon as StudioCancelIcon } from '@navikt/aksel-icons';
+export { XMarkIcon as StudioCloseIcon } from '@navikt/aksel-icons';
 export { PencilIcon as StudioEditIcon } from '@navikt/aksel-icons';
