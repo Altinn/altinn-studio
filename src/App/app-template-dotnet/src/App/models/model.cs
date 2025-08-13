998dee358a33316dc7e6733fd13e09fbd5cf8a2c
Mirko Sekulic <misha.sekulic@gmail.com>
2025-08-13 13:55:10 +0200

    sync with latest app template commit

--- src/App/app-template-dotnet/src/App/models/model.cs
+++ src/App/app-template-dotnet/src/App/models/model.cs
@@ -6,25 +6,25 @@ using System.Text.Json.Serialization;
 using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Newtonsoft.Json;
+
 namespace Altinn.App.Models.model
 {
-  [XmlRoot(ElementName="model")]
-  public class model
-  {
-    [XmlElement("property1", Order = 1)]
-    [JsonProperty("property1")]
-    [JsonPropertyName("property1")]
-    public string property1 { get; set; }
+    [XmlRoot(ElementName = "model")]
+    public class model
+    {
+        [XmlElement("property1", Order = 1)]
+        [JsonProperty("property1")]
+        [JsonPropertyName("property1")]
+        public string property1 { get; set; }
 
-    [XmlElement("property2", Order = 2)]
-    [JsonProperty("property2")]
-    [JsonPropertyName("property2")]
-    public string property2 { get; set; }
+        [XmlElement("property2", Order = 2)]
+        [JsonProperty("property2")]
+        [JsonPropertyName("property2")]
+        public string property2 { get; set; }
 
-    [XmlElement("property3", Order = 3)]
-    [JsonProperty("property3")]
-    [JsonPropertyName("property3")]
-    public string property3 { get; set; }
-
-  }
+        [XmlElement("property3", Order = 3)]
+        [JsonProperty("property3")]
+        [JsonPropertyName("property3")]
+        public string property3 { get; set; }
+    }
 }
