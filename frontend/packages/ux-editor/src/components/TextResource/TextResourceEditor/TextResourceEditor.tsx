a96dd13ec340d479d6790d2ff4df0a4eeb95a30a
Nina Kylstad <nkylstad@gmail.com>
2025-08-14 14:44:02 +0200

    fix: disable search for text resources for page name (#16016)
    Co-authored-by: JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>

--- frontend/packages/ux-editor/src/components/TextResource/TextResourceEditor/TextResourceEditor.tsx
+++ frontend/packages/ux-editor/src/components/TextResource/TextResourceEditor/TextResourceEditor.tsx
@@ -4,12 +4,14 @@ import { TextResourceValueEditor } from './TextResourceValueEditor';
 import { TextResourcePicker } from './TextResourcePicker';
 import classes from './TextResourceEditor.module.css';
 import { useTranslation } from 'react-i18next';
+import { StudioAlert } from '@studio/components';
 
 export interface TextResourceProps {
   textResourceId: string;
   onReferenceChange: (id: string) => void;
   onSetCurrentValue: (value: string) => void;
   textResourceValue?: string;
+  disableSearch?: boolean;
 }
 
 enum TextResourceTab {
@@ -22,6 +24,7 @@ export const TextResourceEditor = ({
   onReferenceChange,
   onSetCurrentValue,
   textResourceValue,
+  disableSearch = false,
 }: TextResourceProps) => {
   const { t } = useTranslation();
 
@@ -43,7 +46,17 @@ export const TextResourceEditor = ({
         />
       </Tabs.Content>
       <Tabs.Content value={TextResourceTab.Search}>
-        <TextResourcePicker onReferenceChange={onReferenceChange} textResourceId={textResourceId} />
+        {disableSearch && (
+          <StudioAlert data-color='info'>
+            {t('ux_editor.modal_properties_textResourceBindings_page_name_search_disabled')}
+          </StudioAlert>
+        )}
+        {!disableSearch && (
+          <TextResourcePicker
+            onReferenceChange={onReferenceChange}
+            textResourceId={textResourceId}
+          />
+        )}
       </Tabs.Content>
     </Tabs>
   );
