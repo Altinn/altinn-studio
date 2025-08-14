f05548c4c4610fc5315eebfde372b9c8344b17f0
Tomas <tomas.engebretsen@gmail.com>
2025-08-14 10:08:42 +0200

    refactor: Apply strict null checks to the hooks library

--- frontend/libs/studio-hooks/src/hooks/useForwardedRef.ts
+++ frontend/libs/studio-hooks/src/hooks/useForwardedRef.ts
@@ -1,8 +1,8 @@
 import type { ForwardedRef, MutableRefObject } from 'react';
 import { useImperativeHandle, useRef } from 'react';
 
-export function useForwardedRef<T>(forwardedRef: ForwardedRef<T>): MutableRefObject<T> {
-  const internalRef = useRef<T>();
-  useImperativeHandle<T, T>(forwardedRef, () => internalRef.current, []);
+export function useForwardedRef<T>(forwardedRef: ForwardedRef<T>): MutableRefObject<T | null> {
+  const internalRef = useRef<T>(null);
+  useImperativeHandle<T | null, T | null>(forwardedRef, () => internalRef?.current, []);
   return internalRef;
 }
