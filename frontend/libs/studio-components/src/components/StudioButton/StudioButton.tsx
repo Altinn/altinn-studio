f49725ef1640659ddd96293acafc8877b066370f
wrt95 <wrt_95@hotmail.com>
2025-08-18 13:12:58 +0200

    adding cast

--- frontend/libs/studio-components/src/components/StudioButton/StudioButton.tsx
+++ frontend/libs/studio-components/src/components/StudioButton/StudioButton.tsx
@@ -1,14 +1,12 @@
-import React, { forwardRef } from 'react';
-import type { ElementType, ReactElement, ReactNode } from 'react';
-import classes from './StudioButton.module.css';
-import cn from 'classnames';
+import React, { forwardRef, type ElementType, type ReactElement, type ReactNode } from 'react';
 import { Button, type ButtonProps } from '@digdir/designsystemet-react';
-import type { IconPlacement } from '../../types/IconPlacement';
+import cn from 'classnames';
+import classes from './StudioButton.module.css';
 import { TextWithIcon } from '../TextWithIcon';
+import type { IconPlacement } from '../../types/IconPlacement';
 import type { Override } from '../../types/Override';
 import type { OverridableComponentProps } from '../../types/OverridableComponentProps';
 import type { OverridableComponentRef } from '../../types/OverridableComponentRef';
-import type { OverridableComponent } from '../../types/OverridableComponent';
 
 export type StudioButtonProps = Override<
   {
@@ -19,47 +17,45 @@ export type StudioButtonProps = Override<
   Omit<ButtonProps, 'asChild' | 'icon'>
 >;
 
-const StudioButton: OverridableComponent<StudioButtonProps, HTMLButtonElement> = forwardRef(
-  <As extends ElementType = 'button'>(
-    {
-      as,
-      fullWidth,
-      icon,
-      iconPlacement = 'left',
-      'data-size': dataSize,
-      className: givenClassName,
-      children,
-      ...rest
-    }: OverridableComponentProps<StudioButtonProps, As>,
-    ref: OverridableComponentRef<As>,
-  ): ReactElement => {
-    const classNames = cn(givenClassName, classes.studioButton, {
-      [classes.smallWithIconOnly]: dataSize === 'sm' && !children,
-      [classes.fullWidth]: fullWidth,
-    });
+function StudioButtonImpl<As extends ElementType = 'button'>(
+  {
+    as,
+    fullWidth,
+    icon,
+    iconPlacement = 'left',
+    'data-size': dataSize,
+    className: givenClassName,
+    children,
+    ...rest
+  }: OverridableComponentProps<StudioButtonProps, As>,
+  ref: OverridableComponentRef<As>,
+): ReactElement {
+  const classNames = cn(givenClassName, classes.studioButton, {
+    [classes.smallWithIconOnly]: dataSize === 'sm' && !children,
+    [classes.fullWidth]: fullWidth,
+  });
 
-    const Component = as || 'button';
+  const Component = as || 'button';
 
-    return (
-      <Button asChild className={classNames} icon={!children} data-size={dataSize}>
-        <Component ref={ref} {...rest}>
-          {icon ? (
-            <TextWithIcon icon={icon} iconPlacement={iconPlacement}>
-              {children}
-            </TextWithIcon>
-          ) : (
-            children
-          )}
-        </Component>
-      </Button>
-    );
-  },
-) as <As extends ElementType = 'button'>(
+  return (
+    <Button asChild className={classNames} icon={!children} data-size={dataSize}>
+      <Component ref={ref} {...rest}>
+        {icon ? (
+          <TextWithIcon icon={icon} iconPlacement={iconPlacement}>
+            {children}
+          </TextWithIcon>
+        ) : (
+          children
+        )}
+      </Component>
+    </Button>
+  );
+}
+
+const StudioButton = forwardRef(StudioButtonImpl) as <As extends ElementType = 'button'>(
   props: OverridableComponentProps<StudioButtonProps, As> & {
     ref?: OverridableComponentRef<As>;
   },
 ) => ReactElement;
-
-StudioButton.displayName = 'StudioButton';
 
 export { StudioButton };
