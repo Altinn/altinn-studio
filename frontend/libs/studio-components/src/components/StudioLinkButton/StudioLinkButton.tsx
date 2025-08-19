1fd33dffb85736744671fa0e3c719fa29f61bd90
wrt95 <wrt_95@hotmail.com>
2025-08-19 12:35:42 +0200

    refactor: add styling for secondary and tertiaryg

--- frontend/libs/studio-components/src/components/StudioLinkButton/StudioLinkButton.tsx
+++ frontend/libs/studio-components/src/components/StudioLinkButton/StudioLinkButton.tsx
@@ -1,12 +1,16 @@
-import React, { forwardRef, type Ref, type ReactElement } from 'react';
+import React, { forwardRef } from 'react';
+import type { ReactElement, ReactNode, Ref } from 'react';
 import cn from 'classnames';
 import classes from './StudioLinkButton.module.css';
 import type { WithoutAsChild } from '../../types/WithoutAsChild';
-import { StudioLink, type StudioLinkProps } from '../StudioLink/StudioLink';
+import { StudioLink } from '../StudioLink/StudioLink';
+import type { StudioLinkProps } from '../StudioLink/StudioLink';
+import type { ButtonProps } from '@digdir/designsystemet-react';
 
 export type StudioLinkButtonProps = WithoutAsChild<StudioLinkProps> & {
-  icon?: React.ReactNode;
+  icon?: ReactNode;
   disabled?: boolean;
+  variant?: ButtonProps['variant'];
 };
 
 function StudioLinkButton(
@@ -14,17 +18,24 @@ function StudioLinkButton(
     children,
     icon,
     iconPlacement = 'left',
-    className = '',
+    className: givenClass = '',
     disabled = false,
+    'data-color': dataColor = 'neutral',
+    variant,
     ...rest
   }: StudioLinkButtonProps,
   ref?: Ref<HTMLAnchorElement>,
 ): ReactElement {
+  const className = cn(classes.linkButton, givenClass, {
+    [classes.disabled]: disabled,
+    [classes.secondary]: variant === 'secondary',
+    [classes.secondaryAndTertiary]: variant === 'secondary' || variant === 'tertiary',
+  });
   return (
     <StudioLink
       {...rest}
-      className={cn(classes.linkButton, className, { [classes.disabled]: disabled })}
-      data-color='neutral'
+      className={className}
+      data-color={dataColor}
       icon={icon}
       iconPlacement={iconPlacement}
       ref={ref}
