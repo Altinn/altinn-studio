853120c0ef27d003c8d09c8df114fe1c778835ab
wrt95 <wrt_95@hotmail.com>
2025-08-21 07:55:09 +0200

    removing more

--- frontend/app-development/layout/PageLayout.tsx
+++ frontend/app-development/layout/PageLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet, matchPath, useLocation } from 'react-router-dom';
 import { PageHeader } from './PageHeader';
-import { PageHeader as PageHeaderNew } from './PageHeaderNew';
 import { useRepoMetadataQuery, useRepoStatusQuery, useUserQuery } from 'app-shared/hooks/queries';
 import { ServerCodes } from 'app-shared/enums/ServerCodes';
 import { StudioCenter, StudioPageSpinner } from '@studio/components-legacy';
@@ -49,10 +48,6 @@ export const PageLayout = (): React.ReactNode => {
     <>
       <PageHeaderContextProvider user={user} repoOwnerIsOrg={repoOwnerIsOrg}>
         <PageHeader
-          showSubMenu={!repoStatus?.hasMergeConflict}
-          isRepoError={repoStatusError !== null}
-        />
-        <PageHeaderNew
           showSubMenu={!repoStatus?.hasMergeConflict}
           isRepoError={repoStatusError !== null}
         />
