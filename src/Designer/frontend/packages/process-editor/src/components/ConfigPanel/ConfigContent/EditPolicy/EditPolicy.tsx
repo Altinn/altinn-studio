3a3b33080a5202a1c26a119bfa22099cdda668c8
Erling Hauan <erling.hauan@digdir.no>
2025-09-02 15:55:32 +0200

    fix: deprecated components in EditPolicy

--- src/Designer/frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/EditPolicy/EditPolicy.tsx
+++ src/Designer/frontend/packages/process-editor/src/components/ConfigPanel/ConfigContent/EditPolicy/EditPolicy.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Alert } from '@digdir/designsystemet-react';
-import { StudioButton, StudioRedirectBox } from '@studio/components-legacy';
+import { StudioAlert, StudioLink, StudioRedirectBox } from '@studio/components';
 import { useTranslation } from 'react-i18next';
 import { ShieldLockIcon } from '@studio/icons';
 import classes from './EditPolicy.module.css';
@@ -29,24 +28,13 @@ export const EditPolicy = () => {
 
   return (
     <div className={classes.configContent}>
-      <Alert severity='info' className={classes.alert}>
-        {t('process_editor.configuration_panel.edit_policy_alert_message')}
-      </Alert>
+      <StudioAlert>{t('process_editor.configuration_panel.edit_policy_alert_message')}</StudioAlert>
       <StudioRedirectBox
         title={t('process_editor.configuration_panel.edit_policy_open_policy_editor_heading')}
       >
-        <StudioButton
-          as='a'
-          onClick={handleClick}
-          href={settingsPageHref}
-          className={classes.link}
-          variant='primary'
-          color='second'
-          icon={<ShieldLockIcon />}
-          iconPlacement='left'
-        >
+        <StudioLink onClick={handleClick} href={settingsPageHref} icon={<ShieldLockIcon />}>
           {t('process_editor.configuration_panel.edit_policy_open_policy_editor_button')}
-        </StudioButton>
+        </StudioLink>
       </StudioRedirectBox>
     </div>
   );
