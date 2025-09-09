6d08fe6106126a7cfcdccc0f5dc67ce0e30b39a4
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-09-09 12:55:47 +0200

    test

--- src/Designer/frontend/testing/playwright/helpers/BpmnJSQuery.ts
+++ src/Designer/frontend/testing/playwright/helpers/BpmnJSQuery.ts
@@ -14,6 +14,7 @@ export class BpmnJSQuery {
    */
   public async getTaskByIdAndType(id: string, type: SvgElement): Promise<string> {
     const elementSelector = `${type}[data-element-id="${id}"]`;
+    await this.page.waitForSelector(elementSelector);
     return elementSelector;
   }
 }
