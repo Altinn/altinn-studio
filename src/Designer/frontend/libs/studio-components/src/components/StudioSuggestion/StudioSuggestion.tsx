27a757951296f0030f5892d52a100559089a987b
Tomas <tomas.engebretsen@gmail.com>
2025-09-09 11:43:24 +0200

    Revert irrelevant changes

--- src/Designer/frontend/libs/studio-components/src/components/StudioSuggestion/StudioSuggestion.tsx
+++ src/Designer/frontend/libs/studio-components/src/components/StudioSuggestion/StudioSuggestion.tsx
@@ -3,7 +3,6 @@ import { forwardRef, type Ref, type ReactElement } from 'react';
 import {
   type SuggestionProps,
   EXPERIMENTAL_Suggestion as Suggestion,
-  ValidationMessage,
 } from '@digdir/designsystemet-react';
 import { StudioLabelWrapper } from '../StudioLabelWrapper';
 import type { StudioLabelWrapperProps } from '../StudioLabelWrapper/StudioLabelWrapper';
@@ -13,22 +12,12 @@ import { StudioLabel } from '../StudioLabel';
 export type StudioSuggestionProps = SuggestionProps &
   Pick<StudioLabelWrapperProps, 'tagText' | 'required'> & {
     emptyText: string;
-    error?: string;
     label: string;
     className?: string;
   };
 
 function StudioSuggestion(
-  {
-    children,
-    className,
-    emptyText,
-    error,
-    label,
-    required,
-    tagText,
-    ...rest
-  }: StudioSuggestionProps,
+  { required, tagText, label, children, emptyText, className, ...rest }: StudioSuggestionProps,
   ref: Ref<React.ElementRef<typeof Suggestion.Input>>,
 ): ReactElement {
   const inputId = useId();
@@ -45,7 +34,6 @@ function StudioSuggestion(
           {children}
         </Suggestion.List>
       </Suggestion>
-      {error && <ValidationMessage>{error}</ValidationMessage>}
     </StudioField>
   );
 }
