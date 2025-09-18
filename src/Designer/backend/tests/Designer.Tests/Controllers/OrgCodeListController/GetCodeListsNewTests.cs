b68d669008f45180157d7e9d1d319b3832ac7a09
Konrad-Simso <konrad.simso@digdir.no>
2025-09-18 08:31:22 +0200

    add tests for new code list controller encpoints.

---
+++ src/Designer/backend/tests/Designer.Tests/Controllers/OrgCodeListController/GetCodeListsNewTests.cs
@@ -0,0 +1,87 @@
+#nullable  enable
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+using Altinn.Studio.Designer.Models.Dto;
+using Altinn.Studio.Designer.Services.Interfaces.Organisation;
+using Designer.Tests.Controllers.ApiTests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Designer.Tests.Controllers.OrgCodeListController;
+
+public class GetCodeListsNewTests : DesignerEndpointsTestsBase<GetCodeListsNewTests>, IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly Mock<IOrgCodeListService> _orgCodeListService;
+    private const string Org = "ttd";
+
+    public GetCodeListsNewTests(WebApplicationFactory<Program> factory) : base(factory)
+    {
+        _orgCodeListService = new Mock<IOrgCodeListService>();
+    }
+
+    protected override void ConfigureTestServices(IServiceCollection services)
+    {
+        base.ConfigureTestServices(services);
+        services.AddSingleton(_orgCodeListService.Object);
+    }
+
+    [Fact]
+    public async Task Get_Returns200OK_WhenRetrievingCodeLists()
+    {
+        // Arrange
+        Dictionary<string, string> queryParameters = new() { { "key", "value" } };
+        Source source = new() { Name = "sourceName", Version = "1.0", QueryParameters = queryParameters };
+        CodeList codeList = new()
+        {
+            Codes =
+            [
+                new Code
+                {
+                    Value = "no",
+                    Label = new Dictionary<string, string> { { "nb", "Norge" } },
+                    Description = new Dictionary<string, string> { { "nb", "Et land i nord europa." } },
+                    HelpText = new Dictionary<string, string> { { "nb", "En hjelpe tekst." } },
+                    Tags = ["tag"]
+                }
+            ],
+            Source = source,
+            TagNames = ["tagName"]
+        };
+        List<CodeListWrapper> expected =
+        [
+            new()
+            {
+                Title = "",
+                CodeList = codeList,
+                HasError = false
+            }
+        ];
+
+        _orgCodeListService
+            .Setup(service => service.GetCodeListsNew(Org, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expected);
+
+        string apiUrl = ApiUrl();
+        using HttpRequestMessage request = new(HttpMethod.Get, apiUrl);
+
+        // Act
+        using HttpResponseMessage response = await HttpClient.SendAsync(request);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        List<CodeListWrapper>? result = JsonSerializer.Deserialize<List<CodeListWrapper>>(responseBody);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(expected, result);
+
+        _orgCodeListService.Verify(service => service.GetCodeListsNew(Org, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    private static string ApiUrl() => $"designer/api/{Org}/code-lists/new/";
+}
---
+++ src/Designer/backend/tests/Designer.Tests/Controllers/OrgCodeListController/UpdateCodeListsNewTests.cs
@@ -0,0 +1,112 @@
+#nullable  enable
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+using Altinn.Studio.Designer.Models.Dto;
+using Altinn.Studio.Designer.Services.Interfaces.Organisation;
+using Designer.Tests.Controllers.ApiTests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace Designer.Tests.Controllers.OrgCodeListController;
+
+public class UpdateCodeListsNewTests : DesignerEndpointsTestsBase<UpdateCodeListsNewTests>, IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly Mock<IOrgCodeListService> _orgCodeListService;
+    private const string Org = "ttd";
+    private const string Developer = "testUser";
+
+    public UpdateCodeListsNewTests(WebApplicationFactory<Program> factory) : base(factory)
+    {
+        _orgCodeListService = new Mock<IOrgCodeListService>();
+    }
+
+    protected override void ConfigureTestServices(IServiceCollection services)
+    {
+        base.ConfigureTestServices(services);
+        services.AddSingleton(_orgCodeListService.Object);
+    }
+
+    [Fact]
+    public async Task Put_Returns200OK_WhenUpdatingCodeList()
+    {
+        // Arrange
+        const string NewCodeListId = "countries";
+        const string DeleteCodeListId = "municipalities";
+        const string CommitMessage = "My commit message";
+        Dictionary<string, string> queryParameters = new() { { "key", "value" } };
+        Source source = new() { Name = "Klass",  Version = "1.0", QueryParameters = queryParameters };
+        CodeList newCodeList = new()
+        {
+            Codes =
+            [
+                new Code
+                {
+                    Value = "no",
+                    Label = new Dictionary<string, string> { { "nb", "Norge" } },
+                    Description = new Dictionary<string, string> { { "nb", "Et land i nord europa." } },
+                    HelpText = new Dictionary<string, string> { { "nb", "En hjelpe tekst." } },
+                    Tags = ["tag"]
+                }
+            ],
+            Source = source,
+            TagNames = ["tagName"]
+        };
+        CodeList deleteCodeList = new()
+        {
+            Codes =
+            [
+                new Code
+                {
+                    Value = "en",
+                    Label = new Dictionary<string, string> { { "nb", "Meløy" } },
+                    Description = new Dictionary<string, string> { { "nb", "En kommune i sør Salten." } },
+                    HelpText = new Dictionary<string, string> { { "nb", "en hjelpe tekst." } },
+                    Tags = ["tag"]
+                }
+            ],
+            Source = source,
+            TagNames = ["tagName"]
+        };
+        UpdateCodeListRequest requestBody = new()
+        {
+            CodeListWrappers =
+            [
+                new CodeListWrapper
+                {
+                    Title = NewCodeListId,
+                    CodeList = newCodeList,
+                    HasError = false
+                },
+                new CodeListWrapper
+                {
+                    Title = DeleteCodeListId,
+                    CodeList = deleteCodeList,
+                    HasError = false
+                }
+            ],
+            CommitMessage = CommitMessage
+        };
+
+        string apiUrl = ApiUrl();
+        using HttpRequestMessage request = new(HttpMethod.Put, apiUrl);
+        request.Content = new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, mediaType: MediaTypeNames.Application.Json);
+
+        // Act
+        using HttpResponseMessage response = await HttpClient.SendAsync(request);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        _orgCodeListService.Verify(service => service.UpdateCodeListsNew(Org, Developer, It.IsAny<List<CodeListWrapper>>(), CommitMessage, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    private static string ApiUrl() => $"designer/api/{Org}/code-lists/new/";
+}
