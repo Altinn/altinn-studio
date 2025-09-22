50a2068a16285d8b5759e16a6a8cd4b5d820d1d6
HauklandJ <hauklandjohannes@gmail.com>
2025-09-22 10:27:39 +0200

    chore: move fileoperation, fileoperationcontexts to spearate files

---
+++ src/Designer/backend/src/Designer/Models/Dto/FileOperationContext.cs
@@ -0,0 +1,82 @@
+#nullable enable
+using System;
+using System.Text.Json.Serialization;
+
+namespace Altinn.Studio.Designer.Models.Dto;
+
+
+/// <summary>
+/// Represents a single file change operation
+/// </summary>
+public class FileOperationContext
+{
+    /// <summary>
+    /// New or updated file content, must be base64 encoded
+    /// </summary>
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
+
+    /// <summary>
+    /// Old path of the file to move
+    /// </summary>
+    [JsonPropertyName("from_path")]
+    public string? FromPath { get; set; }
+
+    /// <summary>
+    /// Indicates what to do with the file
+    /// </summary>
+    [JsonPropertyName("operation")]
+    public required string Operation { get; set; }
+
+    /// <summary>
+    /// Path to the existing or new file
+    /// </summary>
+    [JsonPropertyName("path")]
+    public string Path { get; set; } = string.Empty;
+
+    /// <summary>
+    /// SHA for the file that already exists, required for update or delete
+    /// </summary>
+    [JsonPropertyName("sha")]
+    public string? Sha { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        FileOperationContext? other = obj as FileOperationContext;
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (Equals(other.Content, Content) is false)
+        {
+            return false;
+        }
+
+        if (Equals(other.FromPath, FromPath) is false)
+        {
+            return false;
+        }
+
+        if (Equals(other.Operation, Operation) is false)
+        {
+            return false;
+        }
+
+        if (Equals(other.Path, Path) is false)
+        {
+            return false;
+        }
+
+        if (Equals(other.Sha, Sha) is false)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Content, FromPath, Operation, Path, Sha);
+    }
+}
