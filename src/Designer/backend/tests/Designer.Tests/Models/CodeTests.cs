5b22ba9e2f7c85ba32c45f6cde87a6279faeeff9
HauklandJ <hauklandjohannes@gmail.com>
2025-09-24 09:36:18 +0200

    test: rm test testing c#

--- src/Designer/backend/tests/Designer.Tests/Models/CodeTests.cs
+++ src/Designer/backend/tests/Designer.Tests/Models/CodeTests.cs
@@ -199,15 +199,4 @@ public class CodeTests
         Code code2 = MakeCustomCode();
         Assert.True(code1.Equals(code2));
     }
-
-    [Fact]
-    public void GetHashCode_WhenAllFieldsEqual_IsSame()
-    {
-        Code code1 = MakeCustomCode();
-        Code code2 = MakeCustomCode();
-
-        bool result = ReferenceEquals(code1, code2);
-
-        Assert.True(result);
-    }
 }
