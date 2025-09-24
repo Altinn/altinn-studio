099d19e4ca5ce7645fda74b85499fc73209c57ce
HauklandJ <hauklandjohannes@gmail.com>
2025-09-24 09:46:31 +0200

    chore: remove code that does nothing

--- src/Designer/backend/src/Designer/Models/Dto/CodeListWrapper.cs
+++ src/Designer/backend/src/Designer/Models/Dto/CodeListWrapper.cs
@@ -1,32 +1,5 @@
 #nullable enable
-using System;
 
 namespace Altinn.Studio.Designer.Models.Dto;
 
-public sealed record CodeListWrapper(string Title, CodeList? CodeList = null, bool? HasError = null)
-{
-    public bool Equals(CodeListWrapper? other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        if (string.Equals(other.Title, Title) is false)
-        {
-            return false;
-        }
-
-        if (Equals(other.HasError, HasError) is false)
-        {
-            return false;
-        }
-
-        return Equals(other.CodeList, CodeList);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Title, CodeList, HasError);
-    }
-}
+public sealed record CodeListWrapper(string Title, CodeList? CodeList = null, bool? HasError = null);
