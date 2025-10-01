46c6e2e77d96a625b18380c61fc79346afe9fcb6
HauklandJ <hauklandjohannes@gmail.com>
2025-10-01 14:32:33 +0200

    chore: missing withespace

--- src/Designer/backend/tests/Designer.Tests/Mocks/ISourceControlMock.cs
+++ src/Designer/backend/tests/Designer.Tests/Mocks/ISourceControlMock.cs
@@ -166,6 +166,7 @@ namespace Designer.Tests.Mocks
         public void DeleteLocalBranchIfExists(AltinnRepoEditingContext editingContext, string branchName) => throw new NotImplementedException();
 
         public void CreateLocalBranch(AltinnRepoEditingContext editingContext, string branchName, string commitSha = null) => throw new NotImplementedException();
+
         public void MergeBranchIntoHead(AltinnRepoEditingContext editingContext, string featureBranch) => throw new NotImplementedException();
     }
 }
