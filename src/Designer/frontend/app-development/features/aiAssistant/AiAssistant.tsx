cb652025358d56895a3e7e69c68937494b23487c
Erling Hauan <erling.hauan@digdir.no>
2025-10-02 11:55:20 +0200

    add 'send' text to nb.json

--- src/Designer/frontend/app-development/features/aiAssistant/AiAssistant.tsx
+++ src/Designer/frontend/app-development/features/aiAssistant/AiAssistant.tsx
@@ -6,16 +6,16 @@ import type { ButtonTexts, Message } from '@studio/assistant/';
 
 export function AiAssistant(): ReactElement {
   const { t } = useTranslation();
+  const buttonTexts: ButtonTexts = { send: t('ai_assistant.button.send') };
+
   const { getAssistant } = new AssistantImpl({
     heading: t('ai_assistant.heading'),
-    buttonTexts,
+    buttonTexts: buttonTexts,
     onSubmitMessage,
   });
 
   return <div>{getAssistant()}</div>;
 }
-
-const buttonTexts: ButtonTexts = { send: 'Send' };
 
 const onSubmitMessage = (message: Message): void => {
   alert(`Du har trykket på send-knappen.\nMelding fra tekstfelt: ${message.content}`);
