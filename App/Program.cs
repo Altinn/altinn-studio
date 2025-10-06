07767f09f903754652d12fbc93ee5b2dea41b7bd
Bjørn Tore Gjerde <bjorn.tore.gjerde@vivende.no>
2025-10-06 08:21:34 +0200

    add missing eformidling stuff

--- App/Program.cs
+++ App/Program.cs
@@ -1,6 +1,8 @@
 using Altinn.App.Api.Extensions;
 using Altinn.App.Api.Helpers;
 using Altinn.App.Code;
+using Altinn.App.Core.EFormidling.Extensions;
+using Altinn.App.Core.EFormidling.Implementation;
 using Altinn.App.Core.Internal.Process.ProcessTasks.ServiceTasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +18,7 @@ void RegisterCustomAppServices(
 {
     // Register your apps custom service implementations here.
     services.AddTransient<IServiceTask, FailServiceTask>();
+    services.AddEFormidlingServices2<EFormidlingMetadata,DefaultEFormidlingReceivers>(config);
 }
 
 // ###########################################################################
