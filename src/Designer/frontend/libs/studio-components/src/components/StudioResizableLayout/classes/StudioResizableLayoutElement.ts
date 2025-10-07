8c6d303a2a393ab2b1a17aae50b7ee0f7dfbd036
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-10-07 12:44:06 +0200

    added type to StudioResizableLayoutContext

--- src/Designer/frontend/libs/studio-components/src/components/StudioResizableLayout/classes/StudioResizableLayoutElement.ts
+++ src/Designer/frontend/libs/studio-components/src/components/StudioResizableLayout/classes/StudioResizableLayoutElement.ts
@@ -6,29 +6,29 @@ export class StudioResizableLayoutArea {
     public orientation: 'horizontal' | 'vertical',
   ) {}
 
-  public get size() {
+  public get size(): number {
     return this.orientation === 'vertical'
       ? this.HTMLElementRef.offsetHeight!
       : this.HTMLElementRef.offsetWidth!;
   }
 
-  public get flexGrow() {
+  public get flexGrow(): number {
     return parseFloat(this.HTMLElementRef.style.flexGrow || '1');
   }
 
-  public get minimumSize() {
+  public get minimumSize(): number {
     return this.reactElement.props.minimumSize || 0;
   }
 
-  public get maximumSize() {
+  public get maximumSize(): number {
     return this.reactElement.props.maximumSize || Number.MAX_SAFE_INTEGER;
   }
 
-  public get collapsedSize() {
+  public get collapsedSize(): number {
     return this.reactElement.props.collapsedSize || 0;
   }
 
-  public get collapsed() {
+  public get collapsed(): boolean {
     return this.reactElement.props.collapsed;
   }
 }
