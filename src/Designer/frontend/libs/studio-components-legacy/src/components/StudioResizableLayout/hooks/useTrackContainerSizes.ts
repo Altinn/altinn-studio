4b24203af9212e06569c6d09e59719d6bd292e8b
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2025-10-08 09:30:12 +0200

    refactor

--- src/Designer/frontend/libs/studio-components-legacy/src/components/StudioResizableLayout/hooks/useTrackContainerSizes.ts
+++ src/Designer/frontend/libs/studio-components-legacy/src/components/StudioResizableLayout/hooks/useTrackContainerSizes.ts
@@ -10,7 +10,7 @@ export const useTrackContainerSizes = (localStorageContextKey: string) => {
   );
 
   useMemo(() => {
-    setContainerSizes(value ?? []);
+    setContainerSizes(value);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
