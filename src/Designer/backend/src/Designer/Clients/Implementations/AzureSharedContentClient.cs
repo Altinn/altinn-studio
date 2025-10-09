520ceb53c9968435f88a689e0eda194ba2d6f45d
HauklandJ <hauklandjohannes@gmail.com>
2025-10-09 10:32:38 +0200

    feat: use tempfilecreator to handle local file states

--- src/Designer/backend/src/Designer/Clients/Implementations/AzureSharedContentClient.cs
+++ src/Designer/backend/src/Designer/Clients/Implementations/AzureSharedContentClient.cs
@@ -12,15 +12,20 @@ using System.Threading.Tasks;
 using Altinn.Studio.Designer.Clients.Interfaces;
 using Altinn.Studio.Designer.Helpers;
 using Altinn.Studio.Designer.Models;
+using Azure.Storage.Blobs;
 using Microsoft.Extensions.Logging;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Altinn.Studio.Designer.Clients.Implementations;
 
-public class AzureSharedContentClient: ISharedContentClient
+public class AzureSharedContentClient(HttpClient httpClient, ILogger logger) : ISharedContentClient
 {
-    private readonly HttpClient _httpClient;
-    private readonly ILogger _logger;
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly ILogger _logger = logger;
+
+    private int _currentVersion = int.Parse(InitialVersion);
+
+    private Dictionary<string, string> _fileNamesAndContent = [];
 
     private const string SharedContentBaseUri = "https://<account>.blob.core.windows.net"; // should be a secret!
 
@@ -31,127 +36,43 @@ public class AzureSharedContentClient: ISharedContentClient
         AllowTrailingCommas = true
     };
 
-    public AzureSharedContentClient(HttpClient httpClient, ILogger logger)
-    {
-        _httpClient = httpClient;
-        _logger = logger;
-    }
-
-    // /{org-shortName}/{resourceType}/{resourceId}/{version}.json
-
     public async Task PublishCodeList(string orgName, string codeListId, CodeList codeList, CancellationToken cancellationToken = default)
     {
+        BlobContainerClient containerClient = GetContainerClient();
+
         // Sanity check
         string rootIndexUri = Path.Join(SharedContentBaseUri, IndexFileName);
         HttpResponseMessage rootIndexResponse = await _httpClient.GetAsync(rootIndexUri, cancellationToken);
-        if (rootIndexResponse.StatusCode == HttpStatusCode.NotFound)
-        {
-            _logger.LogError($"Shared content storage container not found, class: {nameof(AzureSharedContentClient)}");
-            throw new InvalidOperationException("Request failed.");
-        }
+        ThrowIfUnhealthyEndpoint(rootIndexResponse);
 
         using TempFileCreatorHelper tempFileHelper = new();
 
-        // Organisation
-        string rootIndexContent = await rootIndexResponse.Content.ReadAsStringAsync(cancellationToken);
-        if (string.IsNullOrEmpty(rootIndexContent))
-        {
-            await UpdateOrganizationIndexFile(tempFileHelper, [orgName]);
-        }
-        else
-        {
-            List<string>? organizations = JsonSerializer.Deserialize<List<string>>(rootIndexContent);
-            if (organizations?.Any(org => org == orgName) is false)
-            {
-                organizations.Add(orgName);
-                await UpdateOrganizationIndexFile(tempFileHelper, organizations);
-            }
-        }
+        await HandleOrganizationIndex(orgName, rootIndexResponse, tempFileHelper, cancellationToken);
+        string organisationDirectory = orgName;
 
-        // ResourceType
-        string organisationDirectory = Path.Join(LocalTemporaryDirectoryName, orgName);
-        string resourceTypeIndexUri = Path.Join(SharedContentBaseUri, orgName, IndexFileName);
-        HttpResponseMessage resourceTypeIndexResponse = await _httpClient.GetAsync(resourceTypeIndexUri, cancellationToken);
-        if (resourceTypeIndexResponse.StatusCode == HttpStatusCode.NotFound)
-        {
-            string contents = JsonSerializer.Serialize<List<string>>([CodeListDirectoryName]);
-            await tempFileHelper.CreateFileOnPath(organisationDirectory, IndexFileName, contents);
-        }
-        else
-        {
-            string resourceTypeIndexContent = await resourceTypeIndexResponse.Content.ReadAsStringAsync(cancellationToken);
-            List<string>? resourceTypes = JsonSerializer.Deserialize<List<string>>(resourceTypeIndexContent);
-            if (resourceTypes?.Any(resourceType => resourceType == CodeListDirectoryName) is false)
-            {
-                resourceTypes.Add(CodeListDirectoryName);
-                string contents = JsonSerializer.Serialize(resourceTypes);
-                await tempFileHelper.CreateFileOnPath(organisationDirectory, IndexFileName, contents);
-            }
-        }
+        await HandleResourceTypeIndex(orgName, tempFileHelper, organisationDirectory, cancellationToken);
 
-        // ResourceId
         string codeListDirectory = Path.Join(organisationDirectory, CodeListDirectoryName);
-        string codeListIdIndexUri = Path.Join(SharedContentBaseUri, orgName, CodeListDirectoryName, IndexFileName);
-        HttpResponseMessage codeListIdIndexResponse = await _httpClient.GetAsync(codeListIdIndexUri, cancellationToken);
-        if (codeListIdIndexResponse.StatusCode == HttpStatusCode.NotFound)
-        {
-            string contents = JsonSerializer.Serialize<List<string>>([codeListId]);
-            await tempFileHelper.CreateFileOnPath(codeListDirectory, IndexFileName, contents);
-        }
-        else
-        {
-            string codeListIdIndexContent = await codeListIdIndexResponse.Content.ReadAsStringAsync(cancellationToken);
-            List<string>? codeListIds = JsonSerializer.Deserialize<List<string>>(codeListIdIndexContent);
-            if (codeListIds?.Any(id => id == codeListId) is false)
-            {
-                codeListIds.Add(codeListId);
-                string contents = JsonSerializer.Serialize(codeListIds);
-                await tempFileHelper.CreateFileOnPath(codeListDirectory, IndexFileName, contents);
-            }
-        }
+        await HandleResourceIndex(orgName, codeListId, tempFileHelper, codeListDirectory, cancellationToken);
 
-        // Version
-        int version = int.Parse(InitialVersion);
         string versionDirectory = Path.Join(codeListDirectory, codeListId);
-        string versionIndexUri = Path.Join(SharedContentBaseUri, orgName, CodeListDirectoryName, codeListId, IndexFileName);
-        HttpResponseMessage versionIndexResponse = await _httpClient.GetAsync(versionIndexUri, cancellationToken);
-        if (versionIndexResponse.StatusCode == HttpStatusCode.NotFound)
-        {
-            string contents = JsonSerializer.Serialize<List<string>>([InitialVersion]);
-            await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
-        }
-        else
-        {
-            string versionIndexContent = await versionIndexResponse.Content.ReadAsStringAsync(cancellationToken);
-            List<int>? versions = JsonSerializer.Deserialize<List<int>>(versionIndexContent);
-            int? latestVersion = versions?.Max();
-            if (latestVersion is not null)
-            {
-                version = (int)latestVersion + 1;
-            }
+        await HandleVersionIndex(orgName, codeListId, tempFileHelper, versionDirectory, cancellationToken);
 
-            if (versions is null)
-            {
-                string contents = JsonSerializer.Serialize<List<string>>([version.ToString()]);
-                await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
-            }
-            else
-            {
-                versions.Add(version);
-                string contents = JsonSerializer.Serialize(versions);
-                await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
-            }
+        await CreateCodeListFile(codeList, tempFileHelper, versionDirectory);
+
+        List<string> allFiles = tempFileHelper.GetAllFilePaths();
+
+
+
+        foreach (var file in allFiles)
+        {
+            var blobClient = containerClient.GetBlobClient(file);
+            // blobClient.UploadAsync()
         }
 
-        // CodeList
-        string codeListFileName = $"{version}.json";
-        var codeListContents = codeList.TagNames is not null
-            ? new { codeList.Codes, TagNames = (List<string>?)codeList.TagNames }
-            : new { codeList.Codes, TagNames = (List<string>?)null};
+        // var blobClient = containerClient.GetBlobClient();
 
-        string contentsString = JsonSerializer.Serialize(codeListContents, s_jsonOptions);
-        await tempFileHelper.CreateFileOnPath(versionDirectory, codeListFileName, contentsString);
-
+        // await service.
 
         // Upload temp files to storage container
         /*
@@ -163,14 +84,140 @@ public class AzureSharedContentClient: ISharedContentClient
          */
     }
 
+    private static BlobContainerClient GetContainerClient()
+    {
+        string? containerName = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONTAINER_NAME");
+        string? accountUrl = Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_URL");
+        string? sasToken = Environment.GetEnvironmentVariable("AZURE_SAS_TOKEN");
+        BlobContainerClient containerClient = new BlobServiceClient(new Uri($"{accountUrl}?{sasToken}")).GetBlobContainerClient(containerName);
+        return containerClient;
+    }
+
+    private async Task CreateCodeListFile(CodeList codeList, TempFileCreatorHelper tempFileHelper, string versionDirectory)
+    {
+        string codeListFileName = $"{_currentVersion}.json";
+        var codeListContents = new SharedCodeList(codeList.Codes, codeList.TagNames);
+        string contentsString = JsonSerializer.Serialize(codeListContents, s_jsonOptions);
+        await tempFileHelper.CreateFileOnPath(versionDirectory, codeListFileName, contentsString);
+    }
+
+    private async Task HandleVersionIndex(string orgName, string codeListId, TempFileCreatorHelper tempFileHelper, string versionDirectory, CancellationToken cancellationToken)
+    {
+        string versionIndexUri = Path.Join(SharedContentBaseUri, orgName, CodeListDirectoryName, codeListId, IndexFileName);
+        HttpResponseMessage versionIndexResponse = await _httpClient.GetAsync(versionIndexUri, cancellationToken);
+
+        if (versionIndexResponse.StatusCode == HttpStatusCode.OK)
+        {
+            string versionIndexContent = await versionIndexResponse.Content.ReadAsStringAsync(cancellationToken);
+            List<int>? versions = JsonSerializer.Deserialize<List<int>>(versionIndexContent);
+            GetCurrentVersion(versions);
+
+            if (versions is not null)
+            {
+                versions.Add(_currentVersion);
+                string contents = JsonSerializer.Serialize(versions);
+                await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
+            }
+            else
+            {
+                string contents = JsonSerializer.Serialize<List<string>>([_currentVersion.ToString()]);
+                await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
+            }
+        }
+
+        if (versionIndexResponse.StatusCode == HttpStatusCode.NotFound)
+        {
+            string contents = JsonSerializer.Serialize<List<string>>([InitialVersion]);
+            await tempFileHelper.CreateFileOnPath(versionDirectory, IndexFileName, contents);
+        }
+    }
+
+    private async Task HandleResourceIndex(string orgName, string codeListId, TempFileCreatorHelper tempFileHelper, string codeListDirectory, CancellationToken cancellationToken)
+    {
+        string codeListIdIndexUri = Path.Join(SharedContentBaseUri, orgName, CodeListDirectoryName, IndexFileName);
+        HttpResponseMessage codeListIdIndexResponse = await _httpClient.GetAsync(codeListIdIndexUri, cancellationToken);
+        if (codeListIdIndexResponse.StatusCode == HttpStatusCode.NotFound)
+        {
+            string contents = JsonSerializer.Serialize<List<string>>([codeListId]);
+            await tempFileHelper.CreateFileOnPath(codeListDirectory, IndexFileName, contents);
+        }
+        else
+        {
+            string codeListIdIndexContent = await codeListIdIndexResponse.Content.ReadAsStringAsync(cancellationToken);
+            List<string>? codeListIds = JsonSerializer.Deserialize<List<string>>(codeListIdIndexContent);
+            if (codeListIds?.Contains(codeListId) is false)
+            {
+                codeListIds.Add(codeListId);
+                string contents = JsonSerializer.Serialize(codeListIds);
+                await tempFileHelper.CreateFileOnPath(codeListDirectory, IndexFileName, contents);
+            }
+        }
+    }
+
+    private async Task HandleResourceTypeIndex(string orgName, TempFileCreatorHelper tempFileHelper, string organisationDirectory, CancellationToken cancellationToken)
+    {
+        string resourceTypeIndexUri = Path.Join(SharedContentBaseUri, orgName, IndexFileName);
+        HttpResponseMessage resourceTypeIndexResponse = await _httpClient.GetAsync(resourceTypeIndexUri, cancellationToken);
+        if (resourceTypeIndexResponse.StatusCode == HttpStatusCode.NotFound)
+        {
+            string contents = JsonSerializer.Serialize<List<string>>([CodeListDirectoryName]);
+            await tempFileHelper.CreateFileOnPath(organisationDirectory, IndexFileName, contents);
+        }
+        else
+        {
+            string resourceTypeIndexContent = await resourceTypeIndexResponse.Content.ReadAsStringAsync(cancellationToken);
+            List<string>? resourceTypes = JsonSerializer.Deserialize<List<string>>(resourceTypeIndexContent);
+            if (resourceTypes?.Contains(CodeListDirectoryName) is false)
+            {
+                resourceTypes.Add(CodeListDirectoryName);
+                string contents = JsonSerializer.Serialize(resourceTypes);
+                await tempFileHelper.CreateFileOnPath(organisationDirectory, IndexFileName, contents);
+            }
+        }
+    }
+
+    private static async Task HandleOrganizationIndex(string orgName, HttpResponseMessage rootIndexResponse, TempFileCreatorHelper tempFileHelper, CancellationToken cancellationToken)
+    {
+        string rootIndexContent = await rootIndexResponse.Content.ReadAsStringAsync(cancellationToken);
+        if (string.IsNullOrEmpty(rootIndexContent))
+        {
+            await UpdateOrganizationIndexFile(tempFileHelper, [orgName]);
+        }
+        else
+        {
+            List<string>? organizations = JsonSerializer.Deserialize<List<string>>(rootIndexContent);
+            if (organizations?.Contains(orgName) is false)
+            {
+                organizations.Add(orgName);
+                await UpdateOrganizationIndexFile(tempFileHelper, organizations);
+            }
+        }
+    }
+
+    private void ThrowIfUnhealthyEndpoint(HttpResponseMessage rootIndexResponse)
+    {
+        if (rootIndexResponse.StatusCode == HttpStatusCode.NotFound)
+        {
+            _logger.LogError($"Shared content storage container not found, class: {nameof(AzureSharedContentClient)}");
+            throw new InvalidOperationException("Request failed.");
+        }
+    }
 
     private static async Task UpdateOrganizationIndexFile(TempFileCreatorHelper tempFileHelper, List<string> organizations)
     {
         string fileContents = JsonSerializer.Serialize(organizations, s_jsonOptions);
-        await tempFileHelper.CreateFileOnPath(LocalTemporaryDirectoryName, IndexFileName, fileContents);
+        await tempFileHelper.CreateFileOnPath("", IndexFileName, fileContents);
     }
 
-    private static string LocalTemporaryDirectoryName => Path.Join(Directory.GetCurrentDirectory(), "temp"); // TODO Move to TempFileCreatorHelper and generate GUID to make sure users have their own temp files.
+    private void GetCurrentVersion(List<int>? versions)
+    {
+        int? latestVersion = versions?.Max();
+        if (latestVersion is not null)
+        {
+            _currentVersion = (int)latestVersion + 1;
+        }
+    }
+
     private const string InitialVersion = "1";
     private const string CodeListDirectoryName = "code_lists";
     private const string IndexFileName = "_index.json";
--- src/Designer/backend/src/Designer/Helpers/TempFileCreatorHelper.cs
+++ src/Designer/backend/src/Designer/Helpers/TempFileCreatorHelper.cs
@@ -1,28 +1,40 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Altinn.Studio.Designer.Helpers;
 
-public class TempFileCreatorHelper : IDisposable
+public sealed class TempFileCreatorHelper : IDisposable
 {
     private bool _disposed;
-    private string _directoryName = string.Empty;
+    private readonly string _directoryName;
+
+    public TempFileCreatorHelper()
+    {
+        _directoryName = Guid.NewGuid().ToString().Substring(0, 16);
+    }
 
     public async Task CreateFileOnPath(string relativeDirectory, string fileName, string contents)
     {
         ThrowIfDisposed();
-        string? directory = Path.GetDirectoryName(relativeDirectory);
+        string combinedPath = Path.Join(_directoryName, relativeDirectory);
+        string? directory = Path.GetDirectoryName(combinedPath);
         if (string.IsNullOrEmpty(directory))
         {
-            Directory.CreateDirectory(relativeDirectory);
+            Directory.CreateDirectory(combinedPath);
         }
 
-        string absolutePath = Path.Join(directory, relativeDirectory, fileName);
+        string absolutePath = Path.Join(directory, combinedPath, fileName);
 
         await File.WriteAllTextAsync(absolutePath, contents);
-        _directoryName = relativeDirectory;
+    }
+
+    public List<string> GetAllFilePaths()
+    {
+        return [.. Directory.GetFiles(_directoryName, "*", SearchOption.AllDirectories).Select(f => f.ToString().Replace(_directoryName, ""))];
     }
 
     public void Dispose()
@@ -32,7 +44,6 @@ public class TempFileCreatorHelper : IDisposable
             Directory.Delete(_directoryName, true);
         }
 
-        GC.SuppressFinalize(this);
         _disposed = true;
     }
 
