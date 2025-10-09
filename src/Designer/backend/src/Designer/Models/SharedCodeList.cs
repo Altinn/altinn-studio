b67a59134fcdf873663885cbda1b85b2fb19491e
HauklandJ <hauklandjohannes@gmail.com>
2025-10-09 10:32:38 +0200

    feat: use tempfilecreator to handle local file states

---
+++ src/Designer/backend/src/Designer/Models/SharedCodeList.cs
@@ -0,0 +1,54 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Altinn.Studio.Designer.Models;
+
+public sealed record SharedCodeList(List<Code> Codes, List<string>? TagNames)
+{
+    public bool Equals(SharedCodeList? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other.Codes.SequenceEqual(Codes) is false)
+        {
+            return false;
+        }
+
+        if ((other.TagNames is null && TagNames is not null) ||
+            (TagNames is null && other.TagNames is not null))
+        {
+            return false;
+        }
+
+        if (other.TagNames is not null && TagNames is not null
+            && other.TagNames.SequenceEqual(TagNames) is false)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (Code code in Codes)
+        {
+            hash.Add(code);
+        }
+
+        if (TagNames is not null)
+        {
+            foreach (string tag in TagNames)
+            {
+                hash.Add(tag);
+            }
+        }
+        return hash.ToHashCode();
+    }
+}
