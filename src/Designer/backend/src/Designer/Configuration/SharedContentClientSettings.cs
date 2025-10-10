e6df4b6b88f657ea3431dd353e09dda3be8c95a2
Konrad-Simso <konrad.simso@digdir.no>
2025-10-10 14:19:02 +0200

    feat: setup credentials for blob service client.

---
+++ src/Designer/backend/src/Designer/Configuration/SharedContentClientSettings.cs
@@ -0,0 +1,7 @@
+namespace Altinn.Studio.Designer.Configuration;
+
+public class SharedContentClientSettings
+{
+    public string StorageContainerName { get; set; }
+    public string StorageAccountUrl { get; set; }
+}
