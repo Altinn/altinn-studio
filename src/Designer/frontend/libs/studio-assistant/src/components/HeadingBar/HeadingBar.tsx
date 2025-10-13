657566163a89f80e665fb0cb942079ccf9437bbb
Erling Hauan <erling.hauan@digdir.no>
2025-10-13 13:44:31 +0200

    replace ToggleGroup with StudioToggleGroup

--- src/Designer/frontend/libs/studio-assistant/src/components/HeadingBar/HeadingBar.tsx
+++ src/Designer/frontend/libs/studio-assistant/src/components/HeadingBar/HeadingBar.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StudioHeading } from '@studio/components';
+import { StudioHeading, StudioToggleGroup } from '@studio/components';
 import classes from './headingBar.module.css';
-import { ToggleGroup } from '@digdir/designsystemet-react';
 import { CodeIcon, PlayFillIcon } from '@studio/icons';
 import { ToolColumnMode } from '../../types/ToolColumnMode';
 import type { AssistantTexts } from '../../types/AssistantTexts';
@@ -19,21 +18,19 @@ export function HeadingBar({ texts, selectedToolColumnMode, onModeChange }: Head
     <div className={classes.headingBar}>
       <StudioHeading>{texts.heading}</StudioHeading>
       {shouldShowToggleGroup && (
-        // TODO: Create StudioToggleGroup component
-        <ToggleGroup
-          size='sm'
+        <StudioToggleGroup
           value={selectedToolColumnMode}
           onChange={(value) => onModeChange(value as ToolColumnMode)}
         >
-          <ToggleGroup.Item value={ToolColumnMode.Preview}>
+          <StudioToggleGroup.Item value={ToolColumnMode.Preview}>
             <PlayFillIcon />
             {texts.preview}
-          </ToggleGroup.Item>
-          <ToggleGroup.Item value={ToolColumnMode.FileExplorer}>
+          </StudioToggleGroup.Item>
+          <StudioToggleGroup.Item value={ToolColumnMode.FileExplorer}>
             <CodeIcon />
             {texts.fileBrowser}
-          </ToggleGroup.Item>
-        </ToggleGroup>
+          </StudioToggleGroup.Item>
+        </StudioToggleGroup>
       )}
     </div>
   );
