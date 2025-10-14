69de803d720d59ad2663ddc15ed00dce8159f382
Tomas <tomas.engebretsen@gmail.com>
2025-10-14 14:47:41 +0200

    Adjust implementation to recent refactorings

--- src/Designer/frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListsPage/CodeListsPage.tsx
+++ src/Designer/frontend/libs/studio-content-library/src/ContentLibrary/LibraryBody/pages/CodeListsPage/CodeListsPage.tsx
@@ -1,8 +1,11 @@
 import type { ReactElement } from 'react';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { StudioHeading } from '@studio/components';
 
 export type CodeListsPageProps = {};
 
 export function CodeListsPage(): ReactElement {
-  return <div>Her kommer nye kodelister</div>;
+  const { t } = useTranslation();
+  return <StudioHeading>{t('app_content_library.code_lists.page_name')}</StudioHeading>;
 }
