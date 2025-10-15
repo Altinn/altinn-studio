a69d383ddc76a84a5043499bf34f8ee653c2c342
Mirko Sekulic <misha.sekulic@gmail.com>
2025-10-15 12:50:28 +0200

    fix false warnings for aot

--- src/Runtime/StudioGateway/src/StudioGateway.Api/Flux/FluxWebhookEndpoints.cs
+++ src/Runtime/StudioGateway/src/StudioGateway.Api/Flux/FluxWebhookEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using StudioGateway.Api.Flux.Contracts;
 
 namespace StudioGateway.Api.Flux;
@@ -6,17 +7,18 @@ public static class FluxWebhookEndpoints
 {
     public static WebApplication MapFluxWebhookEndpoint(this WebApplication app)
     {
-        app.MapPost("/flux/webhook", (FluxEvent fluxEvent, ILogger<Program> logger) =>
-        {
-            logger.LogInformation("Received Flux event: {FluxEvent}", fluxEvent);
-
-            return Results.Ok();
-        })
+        app.MapPost("/flux/webhook", HandleFluxWebhook)
         .WithName("FluxWebhook")
         .WithSummary("Receive Flux CD webhook notifications")
         .WithDescription("Endpoint for receiving event notifications from Flux CD controllers")
         .WithTags("Flux");
 
         return app;
+    }
+
+    private static IResult HandleFluxWebhook(FluxEvent fluxEvent, ILogger<Program> logger)
+    {
+        logger.LogInformation("Received Flux event: {FluxEvent}", fluxEvent);
+        return Results.Ok();
     }
 }
