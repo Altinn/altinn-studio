a622a3ce65e9b853611724a9b123dddf0092d639
David Ovrelid <davidovrelid@David-sin-MacBook-Pro.local>
2025-10-16 10:01:16 +0200

    added a test

--- src/Designer/frontend/packages/process-editor/src/utils/taskUtils/taskUtils.test.ts
+++ src/Designer/frontend/packages/process-editor/src/utils/taskUtils/taskUtils.test.ts
@@ -1,5 +1,6 @@
 import { TaskUtils } from './taskUtils';
 import type { BpmnTaskType } from '../../types/BpmnTaskType';
+import { BpmnDetails } from '@altinn/process-editor/types/BpmnDetails';
 
 type TestCase = {
   input: BpmnTaskType;
@@ -9,10 +10,6 @@ type TestCase = {
 const testCases: Array<TestCase> = [
   {
     input: 'signing',
-    output: true,
-  },
-  {
-    input: 'userControlledSigning',
     output: true,
   },
   {
@@ -29,4 +26,41 @@ describe('taskUtils', () => {
   it.each(testCases)('should return true for signing-tasks %o', ({ input, output }) => {
     expect(TaskUtils.isSigningTask(input)).toEqual(output);
   });
+
+  it.each([
+    {
+      input: buildBpmnDetails('user-controlled-signatures'),
+      output: true,
+    },
+    {
+      input: buildBpmnDetails(''),
+      output: false,
+    },
+    {
+      input: buildBpmnDetails('unknown'),
+      output: false,
+    },
+  ])('should return true for user-controlled-signing %o', ({ input, output }) => {
+    expect(TaskUtils.isUserControlledSigning(input)).toEqual(output);
+  });
 });
+
+function buildBpmnDetails(signatureDataType: string): BpmnDetails {
+  return {
+    element: {
+      di: {
+        bpmnElement: {
+          extensionElements: {
+            values: [
+              {
+                signatureConfig: {
+                  signatureDataType,
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+  } as BpmnDetails;
+}
