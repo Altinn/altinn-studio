10e4a9dabd021c4d9678964cb2dbe0b527a8ac69
David Ovrelid <davidovrelid@David-sin-MacBook-Pro.local>
2025-10-16 09:52:25 +0200

    fix: user-controlled-signing task should be tasktype signing

--- src/Designer/frontend/packages/process-editor/src/utils/taskUtils/taskUtils.ts
+++ src/Designer/frontend/packages/process-editor/src/utils/taskUtils/taskUtils.ts
@@ -1,8 +1,16 @@
 import type { BpmnTaskType } from '../../types/BpmnTaskType';
+import type { BpmnDetails } from '../../types/BpmnDetails';
 
 export class TaskUtils {
   public static isSigningTask(taskType: BpmnTaskType): boolean {
-    const signingTasks: Array<BpmnTaskType> = ['signing', 'userControlledSigning'];
+    const signingTasks: Array<BpmnTaskType> = ['signing'];
     return signingTasks.includes(taskType);
+  }
+
+  public static isUserControlledSigning(bpmnDetails: BpmnDetails): boolean {
+    const dataType = 'user-controlled-signatures';
+    return bpmnDetails.element.di.bpmnElement.extensionElements.values[0].signatureConfig.signatureDataType.includes(
+      dataType,
+    );
   }
 }
