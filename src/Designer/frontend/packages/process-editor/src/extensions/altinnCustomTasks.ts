9cf09402d540e0228261a6c2b4015862b3b974b4
Tobias Netskar <51908451+vxkc@users.noreply.github.com>
2025-10-20 14:46:14 +0200

    wip

--- src/Designer/frontend/packages/process-editor/src/extensions/altinnCustomTasks.ts
+++ src/Designer/frontend/packages/process-editor/src/extensions/altinnCustomTasks.ts
@@ -31,6 +31,11 @@ export const altinnCustomTasks = {
           isMany: false,
           type: 'PaymentConfig',
         },
+        {
+          name: 'pdfConfig',
+          isMany: false,
+          type: 'PdfConfig',
+        },
       ],
     },
     {
@@ -162,6 +167,55 @@ export const altinnCustomTasks = {
           isMany: false,
           isBody: true,
           type: 'Boolean',
+        },
+      ],
+    },
+    {
+      name: 'PdfConfig',
+      properties: [
+        {
+          name: 'filename',
+          isMany: false,
+          isAttr: false,
+          type: 'Filename',
+        },
+        {
+          name: 'autoPdfTaskIds',
+          isMany: false,
+          type: 'AutoPdfTaskIds',
+        },
+      ],
+    },
+    {
+      name: 'Filename',
+      properties: [
+        {
+          name: 'value',
+          isMany: false,
+          isBody: true,
+          type: 'String',
+        },
+      ],
+    },
+    {
+      name: 'AutoPdfTaskIds',
+      properties: [
+        {
+          name: 'taskIds',
+          isMany: true,
+          isAttr: false,
+          type: 'TaskId',
+        },
+      ],
+    },
+    {
+      name: 'TaskId',
+      properties: [
+        {
+          name: 'value',
+          isMany: false,
+          isBody: true,
+          type: 'String',
         },
       ],
     },
