c0e74d0a3e16b072bbdd98dcb3e99aeee24d7560
Johannes Haukland <42615991+HauklandJ@users.noreply.github.com>
2025-10-23 12:10:24 +0200

    feat: use git to handle POST codelists in designer (#16477)
    Co-authored-by: Konrad-Simso <konrad.simso@digdir.no>
    Co-authored-by: Tomas Engebretsen <tomas.engebretsen@digdir.no>

---
+++ src/Designer/backend/src/Designer/Helpers/Extensions/CommandsExtensions.cs
@@ -0,0 +1,11 @@
+using LibGit2Sharp;
+
+namespace Altinn.Studio.Designer.Helpers.Extensions;
+
+public static class CommandsExtensions
+{
+    public static void StageAllChanges(LibGit2Sharp.Repository repository)
+    {
+        Commands.Stage(repository, "*");
+    }
+}
