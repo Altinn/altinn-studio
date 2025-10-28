b95610156873e54eb7b0de82d9cceee908f29232
Tomas <tomas.engebretsen@gmail.com>
2025-10-28 13:42:15 +0100

    Add wrapper component for translation hook

--- src/Designer/frontend/libs/studio-content-library/src/pages/Page.tsx
+++ src/Designer/frontend/libs/studio-content-library/src/pages/Page.tsx
@@ -1,5 +1,5 @@
 import type { PageName } from '../types/PageName';
-import React from 'react';
+import React, { ComponentProps, ReactElement } from 'react';
 import type { PagePropsMap } from '../types/PagesProps';
 import { useTranslation } from 'react-i18next';
 import { pageRouterQueryParamKey } from '../utils/router/QueryParamsRouter';
@@ -29,23 +29,15 @@ export abstract class Page<Name extends PageName> {
   abstract renderPageComponent(props: PagePropsMap<Name>): React.ReactElement;
 
   renderTab(): React.ReactElement {
-    const title = this.useTitle();
     return (
-      <StudioContentMenu.LinkTab
+      <LinkTab
         key={this.name}
         icon={this.renderIcon()}
         tabId={this.name}
-        tabName={title}
+        titleKey={this.titleKey}
         renderTab={this.renderLink}
       />
     );
-  }
-
-  private useTitle(): string {
-    /* Eslint misinterprets this as a class component, while it's really just a hook used in a functional component within a class */
-    /* eslint-disable-next-line react-hooks/rules-of-hooks */
-    const { t } = useTranslation();
-    return t(this.titleKey);
   }
 
   abstract renderIcon(): React.ReactElement;
@@ -57,4 +49,13 @@ export abstract class Page<Name extends PageName> {
   private get link(): string {
     return `?${pageRouterQueryParamKey}=${this.name}`;
   }
+}
+
+type LinkTabProps = Omit<ComponentProps<typeof StudioContentMenu.LinkTab>, 'tabName'> & {
+  titleKey: string;
+};
+
+function LinkTab({ titleKey, ...rest }: LinkTabProps): ReactElement {
+  const { t } = useTranslation();
+  return <StudioContentMenu.LinkTab tabName={t(titleKey)} {...rest} />;
 }
