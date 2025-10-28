c3a1db447a1971be0377121ef872d4cad41cab6d
lassopicasso <lars.walderhaug@digdir.no>
2025-10-28 18:41:25 +0100

    add story to new studio component

---
+++ src/Designer/frontend/libs/studio-components/src/components/StudioManualExpression/StudioManualExpression.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryFn } from '@storybook/react-vite';
+import { logicalExpression } from '../StudioExpression/test-data/expressions';
+import { texts } from '../StudioExpression/test-data/texts';
+import { StudioManualExpression } from './StudioManualExpression';
+
+type Story = StoryFn<typeof StudioManualExpression>;
+
+const meta: Meta = {
+  title: 'Components/StudioExpression',
+  component: StudioManualExpression,
+};
+export const Preview: Story = (args): React.ReactElement => <StudioManualExpression {...args} />;
+
+Preview.args = {
+  expression: logicalExpression,
+  texts: texts,
+};
+export default meta;
