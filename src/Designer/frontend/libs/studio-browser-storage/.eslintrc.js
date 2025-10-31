5eb34471a99b0a5698e3cbcbcf809a2963aa0ffb
David Ovrelid <davidovrelid@Mac.home>
2025-10-31 07:57:35 +0100

    PR feedback: added eslint

--- src/Designer/frontend/libs/studio-browser-storage/.eslintrc.js
+++ src/Designer/frontend/libs/studio-browser-storage/.eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   overrides: [
     {
       files: ['*.tsx', '*.ts'],
+      excludedFiles: ['*.test.ts', '*.test.tsx'],
       rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
         'no-restricted-imports': [
           'error',
           {
