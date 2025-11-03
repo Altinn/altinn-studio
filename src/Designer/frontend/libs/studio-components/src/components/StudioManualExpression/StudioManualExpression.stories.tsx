3615146673280ef0a02736dfbe30fd9e8428cb59
lassopicasso <lars.walderhaug@digdir.no>
2025-11-03 14:24:06 +0100

    code rabbit

--- src/Designer/frontend/libs/studio-components/src/components/StudioManualExpression/StudioManualExpression.stories.tsx
+++ src/Designer/frontend/libs/studio-components/src/components/StudioManualExpression/StudioManualExpression.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryFn } from '@storybook/react-vite';
 import { logicalExpression } from '../StudioExpression/test-data/expressions';
 import { texts } from '../StudioExpression/test-data/texts';
 import { StudioManualExpression } from './StudioManualExpression';
+import { fn } from 'storybook/internal/test';
 
 type Story = StoryFn<typeof StudioManualExpression>;
 
@@ -14,5 +15,7 @@ export const Preview: Story = (args): React.ReactElement => <StudioManualExpress
 Preview.args = {
   expression: logicalExpression,
   texts: texts,
+  onValidExpressionChange: fn(),
+  onValidityChange: fn(),
 };
 export default meta;
