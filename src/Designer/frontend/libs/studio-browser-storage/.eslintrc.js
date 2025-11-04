1aec6ba4f29ab287fc1d01c3cab3340d03aeb804
David Ovrelid <46874830+framitdavid@users.noreply.github.com>
2025-11-04 19:09:33 +0100

    feat: Add new studio-browser-storage library (#16798)
    Co-authored-by: David Ovrelid <davidovrelid@David-sin-MacBook-Pro.local>
    Co-authored-by: David Ovrelid <davidovrelid@Mac.home>
    Co-authored-by: Tomas Engebretsen <tomas.engebretsen@digdir.no>

---
+++ src/Designer/frontend/libs/studio-browser-storage/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: ['react'],
+                message: 'The @studio/browser-storage package should not depend on React.',
+              },
+              {
+                group: ['app-shared/*', '@altinn/*'],
+                message:
+                  'Files in the @studio/browser-storage package should not depend on app-specific packages.',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
