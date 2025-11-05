7135f771f46c1c53b0150dbb66837a1fec7c08c2
Tomas <tomas.engebretsen@gmail.com>
2025-11-05 07:46:39 +0100

    feat: Establish mutation query for code list endpoint

--- src/Designer/frontend/dashboard/pages/OrgContentLibraryPage/test-data/codeListsNewResponse.ts
+++ src/Designer/frontend/dashboard/pages/OrgContentLibraryPage/test-data/codeListsNewResponse.ts
@@ -1,49 +1,24 @@
 import type { CodeListDataNew } from 'app-shared/types/CodeListDataNew';
 import type { CodeListsNewResponse } from 'app-shared/types/api/CodeListsNewResponse';
+import { codeLists } from './codeLists';
 
-const animals: CodeListDataNew = {
-  title: 'animals',
+const animalsData: CodeListDataNew = {
+  title: 'animals' satisfies keyof typeof codeLists,
   codeList: {
-    codes: [
-      {
-        value: 'cat',
-        label: { nb: 'Katt', nn: 'Katt', en: 'Cat' },
-      },
-      {
-        value: 'dog',
-        label: { nb: 'Hund', nn: 'Hund', en: 'Dog' },
-      },
-      {
-        value: 'rabbit',
-        label: { nb: 'Kanin', nn: 'Kanin', en: 'Rabbit' },
-      },
-    ],
+    codes: codeLists.animals,
   },
   hasError: false,
 };
 
-const vehicles: CodeListDataNew = {
-  title: 'vehicles',
+export const vehiclesData: CodeListDataNew = {
+  title: 'vehicles' satisfies keyof typeof codeLists,
   codeList: {
-    codes: [
-      {
-        value: 'car',
-        label: { nb: 'Bil', nn: 'Bil', en: 'Car' },
-      },
-      {
-        value: 'bike',
-        label: { nb: 'Sykkel', nn: 'Sykkel', en: 'Bike' },
-      },
-      {
-        value: 'boat',
-        label: { nb: 'Båt', nn: 'Båt', en: 'Boat' },
-      },
-    ],
+    codes: codeLists.vehicles,
   },
   hasError: false,
 };
 
-const codeListWrappers: CodeListDataNew[] = [animals, vehicles];
+const codeListWrappers: CodeListDataNew[] = [animalsData, vehiclesData];
 
 export const codeListsNewResponse: CodeListsNewResponse = {
   codeListWrappers,
