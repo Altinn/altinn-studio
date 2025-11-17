87c03420e026a4e396b2ee970a94dfc326023820
Ole Martin Handeland <git@olemartin.org>
2025-11-17 16:01:38 +0100

    Adding upgrade command path for next, moving the legacy ones out of the way

---
+++ src/cli/src/altinn-studio-cli/Upgrade/Next/NextUpgrade.cs
@@ -0,0 +1,30 @@
+using System.CommandLine;
+
+namespace Altinn.Studio.Cli.Upgrade.Next;
+
+/// <summary>
+/// Defines the upgrade command for upgrading both backend and frontend to the next version
+/// </summary>
+internal static class NextUpgrade
+{
+    /// <summary>
+    /// Get the next upgrade command
+    /// </summary>
+    /// <param name="projectFolderOption">Option for setting the root folder of the project</param>
+    /// <returns>The command for upgrading to the next version</returns>
+    public static Command GetUpgradeCommand(Option<string> projectFolderOption)
+    {
+        var upgradeCommand = new Command("next", "Upgrade an app to the next version (both backend and frontend)")
+        {
+            projectFolderOption,
+        };
+
+        upgradeCommand.SetHandler(context =>
+        {
+            Console.WriteLine("Next upgrade not yet implemented");
+            Environment.Exit(0);
+        });
+
+        return upgradeCommand;
+    }
+}
