098b6bc57068649cf4e5a44e2f4fad823a400059
HauklandJ <hauklandjohannes@gmail.com>
2025-11-24 13:23:59 +0100

    refactor: enforce mutually exclusive nullability for Libraryfile

--- src/Designer/backend/src/Designer/Models/Dto/LibraryFile.cs
+++ src/Designer/backend/src/Designer/Models/Dto/LibraryFile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Altinn.Studio.Designer.Models.Dto;
@@ -7,14 +8,19 @@ public sealed record LibraryFile
     public string Path { get; set; }
     public string ContentType { get; set; }
 
-    [MemberNotNullWhen(false, nameof(Url))]
+    [MemberNotNullWhen(true, nameof(Url))]
     public string? Content { get; set; }
 
-    [MemberNotNullWhen(false, nameof(Content))]
+    [MemberNotNullWhen(true, nameof(Content))]
     public string? Url { get; set; }
 
     public LibraryFile(string path, string contentType, string? content, string? url)
     {
+        if ((content is null && url is null) || (content is not null && url is not null))
+        {
+            throw new ArgumentException("Exactly one of content or url must be provided.");
+        }
+
         Path = path;
         ContentType = contentType;
         Content = content;
