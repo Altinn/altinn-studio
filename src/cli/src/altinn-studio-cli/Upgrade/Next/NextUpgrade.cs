aece5028c4be0e74e772620eff16074f52ef076b
Ole Martin Handeland <git@olemartin.org>
2025-11-26 11:16:54 +0100

    Proper indentation, fixes to build, upgrading package file as well

--- src/cli/src/altinn-studio-cli/Upgrade/Next/NextUpgrade.cs
+++ src/cli/src/altinn-studio-cli/Upgrade/Next/NextUpgrade.cs
@@ -1,4 +1,6 @@
 using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
+using Altinn.Studio.Cli.Upgrade.Backend.v7Tov8.ProjectRewriters;
 using Altinn.Studio.Cli.Upgrade.Next.RuleAnalysis;
 
 namespace Altinn.Studio.Cli.Upgrade.Next;
@@ -15,20 +17,141 @@ internal static class NextUpgrade
     /// <returns>The command for upgrading to the next version</returns>
     public static Command GetUpgradeCommand(Option<string> projectFolderOption)
     {
+        var projectFileOption = new Option<string>(
+            name: "--project",
+            description: "The project file to read relative to --folder",
+            getDefaultValue: () => "App/App.csproj"
+        );
+        var targetVersionOption = new Option<string>(
+            name: "--target-version",
+            description: "The target version to upgrade to",
+            getDefaultValue: () => "8.9.0-preview.3"
+        );
+        var targetFrameworkOption = new Option<string>(
+            name: "--target-framework",
+            description: "The target dotnet framework version to upgrade to",
+            getDefaultValue: () => "net8.0"
+        );
+        var skipCsprojUpgradeOption = new Option<bool>(
+            name: "--skip-csproj-upgrade",
+            description: "Skip csproj file upgrade",
+            getDefaultValue: () => false
+        );
+
         var upgradeCommand = new Command("next", "Upgrade an app to the next version (both backend and frontend)")
         {
             projectFolderOption,
+            projectFileOption,
+            targetVersionOption,
+            targetFrameworkOption,
+            skipCsprojUpgradeOption,
         };
 
         // Add subcommands
         upgradeCommand.AddCommand(RuleConvertCommand.GetCommand(projectFolderOption));
 
-        upgradeCommand.SetHandler(context =>
+        int returnCode = 0;
+        upgradeCommand.SetHandler(async context =>
         {
-            Console.WriteLine("Next upgrade not yet implemented");
-            Environment.Exit(0);
+            var projectFolder = context.ParseResult.GetValueForOption(projectFolderOption);
+            var projectFile = context.ParseResult.GetValueForOption(projectFileOption);
+            var targetVersion = context.ParseResult.GetValueForOption(targetVersionOption);
+            var targetFramework = context.ParseResult.GetValueForOption(targetFrameworkOption);
+            var skipCsprojUpgrade = context.ParseResult.GetValueForOption(skipCsprojUpgradeOption);
+
+            if (projectFolder is null or "CurrentDirectory")
+                projectFolder = Directory.GetCurrentDirectory();
+
+            if (projectFile is null)
+                ExitWithError("Project file option is required but was not provided");
+
+            if (targetVersion is null)
+                ExitWithError("Target version option is required but was not provided");
+
+            if (targetFramework is null)
+                ExitWithError("Target framework option is required but was not provided");
+
+            if (!Directory.Exists(projectFolder))
+            {
+                ExitWithError(
+                    $"{projectFolder} does not exist. Please supply location of project with --folder [path/to/project]"
+                );
+            }
+
+            FileAttributes attr = File.GetAttributes(projectFolder);
+            if ((attr & FileAttributes.Directory) != FileAttributes.Directory)
+            {
+                ExitWithError(
+                    $"Project folder {projectFolder} is a file. Please supply location of project with --folder [path/to/project]"
+                );
+            }
+
+            if (!Path.IsPathRooted(projectFolder))
+            {
+                projectFile = Path.Combine(Directory.GetCurrentDirectory(), projectFolder, projectFile);
+            }
+            else
+            {
+                projectFile = Path.Combine(projectFolder, projectFile);
+            }
+
+            if (!File.Exists(projectFile))
+            {
+                ExitWithError(
+                    $"Project file {projectFile} does not exist. Please supply location of project with --project [path/to/project.csproj]"
+                );
+            }
+
+            // Job 1: Upgrade project file (csproj)
+            if (!skipCsprojUpgrade && returnCode == 0)
+            {
+                returnCode = await UpgradeProjectFile(projectFile, targetVersion, targetFramework);
+            }
+
+            // Job 2: Remove Swashbuckle.AspNetCore dependency
+            if (returnCode == 0)
+            {
+                returnCode = await RemoveSwashbucklePackage(projectFile);
+            }
+
+            // TODO: Add more upgrade jobs here (frontend, rules, etc.)
+
+            if (returnCode == 0)
+            {
+                Console.WriteLine(
+                    "Upgrade completed without errors. Please verify that the application is still working as expected."
+                );
+            }
+            else
+            {
+                Console.WriteLine("Upgrade completed with errors. Please check for errors in the log above.");
+            }
+            Environment.Exit(returnCode);
         });
 
         return upgradeCommand;
+    }
+
+    static async Task<int> UpgradeProjectFile(string projectFile, string targetVersion, string targetFramework)
+    {
+        var rewriter = new ProjectFileRewriter(projectFile, targetVersion, targetFramework);
+        await rewriter.Upgrade();
+        await Console.Out.WriteLineAsync("Nuget versions upgraded");
+        return 0;
+    }
+
+    static async Task<int> RemoveSwashbucklePackage(string projectFile)
+    {
+        var rewriter = new ProjectFileRewriter(projectFile);
+        await rewriter.RemovePackageReference("Swashbuckle.AspNetCore");
+        await Console.Out.WriteLineAsync("Swashbuckle.AspNetCore package reference removed");
+        return 0;
+    }
+
+    [DoesNotReturn]
+    private static void ExitWithError(string message, int exitCode = 1)
+    {
+        Console.Error.WriteLine(message);
+        Environment.Exit(exitCode);
     }
 }
