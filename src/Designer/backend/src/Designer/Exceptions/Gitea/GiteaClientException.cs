73b5b722668b5331ce496c332d30859d54299667
HauklandJ <hauklandjohannes@gmail.com>
2025-11-28 14:02:49 +0100

    docs: fix summary for gitea exception

--- src/Designer/backend/src/Designer/Exceptions/Gitea/GiteaClientException.cs
+++ src/Designer/backend/src/Designer/Exceptions/Gitea/GiteaClientException.cs
@@ -3,6 +3,6 @@
 namespace Altinn.Studio.Designer.Exceptions.Gitea;
 
 /// <summary>
-/// Used for exceptions occurring in the GiteaApi
+/// Used for exceptions occurring in the gitea client
 /// </summary>
 public class GiteaClientException(string message) : Exception(message);
