af626230bcb9d1b54d40059f07515188ced400e3
HauklandJ <hauklandjohannes@gmail.com>
2025-11-28 09:06:24 +0100

    refactor: primary constructor for exception

--- src/Designer/backend/src/Designer/Exceptions/Gitea/GiteaAPIWrapperException.cs
+++ src/Designer/backend/src/Designer/Exceptions/Gitea/GiteaAPIWrapperException.cs
@@ -5,9 +5,4 @@ namespace Altinn.Studio.Designer.Exceptions.Gitea;
 /// <summary>
 /// Used for exceptions occurring in the GiteaApi
 /// </summary>
-public class GiteaApiWrapperException : Exception
-{
-    public GiteaApiWrapperException(string message) : base(message)
-    {
-    }
-}
+public class GiteaApiWrapperException(string message) : Exception(message);
