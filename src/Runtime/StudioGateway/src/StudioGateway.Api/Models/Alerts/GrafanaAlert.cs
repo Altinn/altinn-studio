2529e0432ad679c9d18c089da0e9278a34bebc7d
Michael Queyrichon <24462611+mlqn@users.noreply.github.com>
2025-12-01 10:33:59 +0100

    Fix

--- src/Runtime/StudioGateway/src/StudioGateway.Api/Models/Alerts/GrafanaAlert.cs
+++ src/Runtime/StudioGateway/src/StudioGateway.Api/Models/Alerts/GrafanaAlert.cs
@@ -1,7 +1,11 @@
 namespace StudioGateway.Api.Models.Alerts;
 
-public record GrafanaAlert(
-    Dictionary<string, string> Labels,
-    Dictionary<string, string> Annotations,
-    string GeneratorURL,
-    string Fingerprint);
+public record GrafanaAlert
+{
+    public GrafanaAlert() { }
+
+    public Dictionary<string, string>? Labels { get; init; }
+    public Dictionary<string, string>? Annotations { get; init; }
+    public string? GeneratorURL { get; init; }
+    public string? Fingerprint { get; init; }
+}
