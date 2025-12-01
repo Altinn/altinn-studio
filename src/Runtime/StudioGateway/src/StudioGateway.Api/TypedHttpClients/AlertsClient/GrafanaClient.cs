354d0ad9f757b76acb1d229b9a5c9e246c33288b
Michael Queyrichon <24462611+mlqn@users.noreply.github.com>
2025-12-01 10:39:01 +0100

    Fix

--- src/Runtime/StudioGateway/src/StudioGateway.Api/TypedHttpClients/AlertsClient/GrafanaClient.cs
+++ src/Runtime/StudioGateway/src/StudioGateway.Api/TypedHttpClients/AlertsClient/GrafanaClient.cs
@@ -36,7 +36,7 @@ public class GrafanaClient(
                 Id = alert.Fingerprint,
                 RuleId = alert.Labels.TryGetValue("RuleId", out string? ruleId) ? ruleId : alert.Labels["__alert_rule_uid__"],
                 Name = alert.Labels["alertname"],
-                App = "ttd" + (alert.Labels.TryGetValue("__name__", out string? appName) ? appName : string.Empty),
+                App = alert.Labels.TryGetValue("__name__", out string? appName) ? appName : string.Empty,
                 // App = alert.Labels["cloud/rolename"],
                 Url = BuildAlertLink(baseUri, alert)
             };
