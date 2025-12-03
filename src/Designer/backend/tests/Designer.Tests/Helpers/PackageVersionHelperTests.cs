7761d8884f75f2debd10a2665f6469cf53ddadac
Olav Sørlie <olav.sorlie@digdir.no>
2025-12-03 13:49:49 +0100

    changed expected version in PackageVersionHelperTests

--- src/Designer/backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
+++ src/Designer/backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
@@ -9,7 +9,7 @@ namespace Designer.Tests.Helpers
     public class PackageVersionHelperTests
     {
         [Theory]
-        [InlineData("Altinn.App.Api", true, "8.6.4")]
+        [InlineData("Altinn.App.Api", true, "8.6.6")]
         [InlineData("NonExistingNuget", false)]
         public void TryGetPackageVersionFromCsprojFile_GivenValidCsprojFile_ReturnsTrue(string packageName, bool expectedResult, string expectedVersion = "")
         {
