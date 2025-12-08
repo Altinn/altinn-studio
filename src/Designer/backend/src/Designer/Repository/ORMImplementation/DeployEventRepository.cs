10d9b74fca02b09d3e44563b8d6cb807b6416089
Mirko Sekulic <misha.sekulic@gmail.com>
2025-12-08 15:33:13 +0100

    add event only when feature is enabled

--- src/Designer/backend/src/Designer/Repository/ORMImplementation/DeployEventRepository.cs
+++ src/Designer/backend/src/Designer/Repository/ORMImplementation/DeployEventRepository.cs
@@ -1,26 +1,34 @@
-#nullable disable
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Altinn.Studio.Designer.Constants;
 using Altinn.Studio.Designer.Repository.Models;
 using Altinn.Studio.Designer.Repository.ORMImplementation.Data;
 using Altinn.Studio.Designer.Repository.ORMImplementation.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.FeatureManagement;
 
 namespace Altinn.Studio.Designer.Repository.ORMImplementation;
 
 public class DeployEventRepository : IDeployEventRepository
 {
     private readonly DesignerdbContext _dbContext;
+    private readonly IFeatureManager _featureManager;
 
-    public DeployEventRepository(DesignerdbContext dbContext)
+    public DeployEventRepository(DesignerdbContext dbContext, IFeatureManager featureManager)
     {
         _dbContext = dbContext;
+        _featureManager = featureManager;
     }
 
     public async Task AddAsync(string org, string buildId, DeployEvent deployEvent, CancellationToken cancellationToken = default)
     {
-        var deploymentSequenceNo = await _dbContext.Deployments
+        if (!await _featureManager.IsEnabledAsync(StudioFeatureFlags.GitOpsDeploy))
+        {
+            return;
+        }
+
+        long deploymentSequenceNo = await _dbContext.Deployments
             .Include(d => d.Build)
             .AsNoTracking()
             .Where(d => d.Org == org && d.Build.ExternalId == buildId)
