21d0939c4f54607a4ee095cdde42be0a7874b250
HauklandJ <hauklandjohannes@gmail.com>
2025-12-09 09:04:09 +0100

    chore: remove unused code (problemdetails)

--- src/Designer/frontend/packages/shared/src/types/api/SharedResourcesResponse.ts
+++ src/Designer/frontend/packages/shared/src/types/api/SharedResourcesResponse.ts
@@ -1,13 +1,5 @@
 import type { LibraryFile } from '../LibraryFile';
 
-export interface ProblemDetails {
-  type?: string;
-  title?: string;
-  status?: number;
-  detail?: string;
-  instance?: string;
-}
-
 export interface SharedResourcesResponse {
   files: LibraryFile[];
   commitSha: string;
