c6336c8f3669a3832b2da73c3707f34feca42fa0
Ole Martin Handeland <git@olemartin.org>
2025-12-09 10:40:21 +0100

    Removing comment about original rulehandler function

--- src/test/apps/frontend-test/App/logic/ConvertedLegacyRules/ChangenameDataProcessor.cs
+++ src/test/apps/frontend-test/App/logic/ConvertedLegacyRules/ChangenameDataProcessor.cs
@@ -33,23 +33,6 @@ public class ChangenameDataProcessor : IDataWriteProcessor
 
     private async Task Rule_b9dcddd0deb411e99cfd6f791e4f04e5(IFormDataWrapper wrapper, Skjema? data)
     {
-        // Rule configuration:
-        //   SelectedFunction: nyttNavn
-        //   InputParams:
-        //     fornavn = NyttNavn-grp-9313.NyttNavn-grp-9314.PersonFornavnNytt-datadef-34758.value
-        //     mellomnavn = NyttNavn-grp-9313.NyttNavn-grp-9314.PersonMellomnavnNytt-datadef-34759.value
-        //     etternavn = NyttNavn-grp-9313.NyttNavn-grp-9314.PersonEtternavnNytt-datadef-34757.value
-        //   OutParams:
-        //     outParam0 = Innledning-grp-9309.NavneendringenGjelderFor-grp-9310.SubjektFornavnFolkeregistrert-datadef-34730.value
-
-        // Original JavaScript function: nyttNavn
-        // (function(obj) {
-        //         obj.fornavn = obj.fornavn ? obj.fornavn + ' ' : '';
-        //         obj.mellomnavn = obj.mellomnavn ? obj.mellomnavn + ' ' : '';
-        //         obj.etternavn = obj.etternavn ? obj.etternavn : '';
-        //         return obj.fornavn + obj.mellomnavn + obj.etternavn;
-        //     })
-
         var fornavn = (string?)wrapper.Get("NyttNavn-grp-9313.NyttNavn-grp-9314.PersonFornavnNytt-datadef-34758.value");
         var mellomnavn = (string?)wrapper.Get("NyttNavn-grp-9313.NyttNavn-grp-9314.PersonMellomnavnNytt-datadef-34759.value");
         var etternavn = (string?)wrapper.Get("NyttNavn-grp-9313.NyttNavn-grp-9314.PersonEtternavnNytt-datadef-34757.value");
