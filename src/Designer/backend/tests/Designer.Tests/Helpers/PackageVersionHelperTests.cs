94f2f4280ea5baac9e1c6f7087da58c733b39d94
Nina Kylstad <nkylstad@gmail.com>
2025-12-10 12:09:30 +0100

    handle pinned exact version

--- src/Designer/backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
+++ src/Designer/backend/tests/Designer.Tests/Helpers/PackageVersionHelperTests.cs
@@ -19,6 +19,32 @@ namespace Designer.Tests.Helpers
         {
             string testTemplateCsProjPath = Path.Combine(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath, "..", "..", "App", "template", "src", "App", "App.csproj");
 
+
+            string[] packages = [packageName, $"{packageName}.Experimental"];
+            string[][] inputs = [packages, packages.Reverse().ToArray()];
+            var outputs = new List<(bool, string?)>();
+            foreach (var input in inputs)
+            {
+                bool result = PackageVersionHelper.TryGetPackageVersionFromCsprojFile(testTemplateCsProjPath, input, out var version);
+
+                outputs.Add((result, version?.ToString()));
+            }
+
+            var snapshot = new
+            {
+                Inputs = inputs,
+                Outputs = outputs
+            };
+            await Verifier.Verify(snapshot).UseParameters(packageName);
+        }
+
+        [Theory]
+        [InlineData("Altinn.App.Api")]
+        public async Task TryGetPackageVersionFromCsprojFile_GivenValidCsprojFileWithPinnedVersions_ReturnsTrue(string packageName)
+        {
+            // In this directory, the csproj file has versions pinned with [ and ]
+            string testTemplateCsProjPath = Path.Combine(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath, "..", "testdata", "App", "App.csproj");
+
             string[] packages = [packageName, $"{packageName}.Experimental"];
             string[][] inputs = [packages, packages.Reverse().ToArray()];
             var outputs = new List<(bool, string?)>();
