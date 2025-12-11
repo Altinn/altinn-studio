e7fd0b7a669c3a60e914d43a9174ba50954d8b23
Mirko Sekulic <misha.sekulic@gmail.com>
2025-12-11 22:33:40 +0100

    app deployed endpoint

---
+++ src/Runtime/StudioGateway/src/StudioGateway.Api/Settings/GatewayContext.cs
@@ -0,0 +1,8 @@
+namespace StudioGateway.Api.Settings;
+
+internal class GatewayContext
+{
+    public string ServiceOwner { get; set; } = "";
+    public string UpgradeChannel { get; set; } = "";
+    public string Environment { get; set; } = "";
+}
