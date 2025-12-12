bf1a06fa7feb31c89484bb554a49556a0f941f18
lassopicasso <lars.walderhaug@digdir.no>
2025-12-12 07:45:04 +0100

    add tests

--- src/Designer/frontend/packages/ux-editor/src/components/config/ConfigProperties/ConfigObjectProperty/useDisplayObjectValues.test.tsx
+++ src/Designer/frontend/packages/ux-editor/src/components/config/ConfigProperties/ConfigObjectProperty/useDisplayObjectValues.test.tsx
@@ -1,6 +1,9 @@
 import { renderHook } from '@testing-library/react';
 import { useDisplayObjectValues } from './useDisplayObjectValues';
 import { textMock } from '@studio/testing/mocks/i18nMock';
+import { useComponentPropertyLabel } from '@altinn/ux-editor/hooks';
+
+jest.mock('@altinn/ux-editor/hooks/useComponentPropertyLabel');
 
 describe('useDisplayObjectValues', () => {
   it('should return null when no values are provided', () => {
@@ -9,14 +12,23 @@ describe('useDisplayObjectValues', () => {
   });
 
   it('should translate and format object values correctly', () => {
+    (useComponentPropertyLabel as jest.Mock).mockReturnValue(
+      (value: string) =>
+        ({
+          value1: textMock('ux_editor.component_properties.value1'),
+          value2: 'value2',
+          enum_value3: textMock('ux_editor.component_properties.enum_value3'),
+        })[value] || value,
+    );
+
     const valuesToBeSaved = {
       key1: 'value1',
       key2: 'value2',
+      key3: 'enum_value3',
     };
     const { result } = renderHook(() => useDisplayObjectValues(valuesToBeSaved));
-
     expect(result.current).toBe(
-      `${textMock('ux_editor.component_properties.value1')}, ${textMock('ux_editor.component_properties.value2')}`,
+      `${textMock('ux_editor.component_properties.value1')}, value2, ${textMock('ux_editor.component_properties.enum_value3')}`,
     );
   });
 });
