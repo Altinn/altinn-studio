8bcf2bbe6524a8a10a7444103eca63f6a92ce80a
Martin Othamar <martin.othamar@digdir.no>
2025-12-15 13:38:49 +0100

    fix tests?

--- src/Designer/backend/src/Designer/TypedHttpClients/RuntimeGateway/ServiceCollectionExtensions.cs
+++ src/Designer/backend/src/Designer/TypedHttpClients/RuntimeGateway/ServiceCollectionExtensions.cs
@@ -13,31 +13,26 @@ public static class ServiceCollectionExtensions
     internal static void AddRuntimeGatewayHttpClient(this IServiceCollection services, IConfiguration config)
     {
         var maskinportenClientForRuntime = config.GetSection(nameof(MaskinportenClientForRuntime)).Get<MaskinportenClientForRuntime>();
-        if (maskinportenClientForRuntime?.Count is null)
-        {
-            throw new System.Exception("Configuration for MaskinportenClientForRuntime is missing");
-        }
-        if (maskinportenClientForRuntime.Count is not 1)
-        {
-            throw new System.Exception("Configuration for MaskinportenClientForRuntime must contain exactly one client definition");
-        }
 
-        var kvp = maskinportenClientForRuntime.Single();
-        services.AddMaskinportenHttpClient<SettingsJwkClientDefinition>("runtime-gateway", kvp.Value)
-            .AddStandardResilienceHandler(options =>
-            {
-                options.Retry.MaxRetryAttempts = 3;
-                options.Retry.UseJitter = true;
-                options.Retry.ShouldHandle = args =>
-                    ValueTask.FromResult(
-                        args.Outcome switch
-                        {
-                            { Exception: not null } => true,
-                            { Result.IsSuccessStatusCode: false } => true,
-                            _ => false,
-                        }
-                    );
-            });
+        var settings = maskinportenClientForRuntime?.SingleOrDefault().Value;
+        if (settings is not null)
+        {
+            services.AddMaskinportenHttpClient<SettingsJwkClientDefinition>("runtime-gateway", settings)
+                .AddStandardResilienceHandler(options =>
+                {
+                    options.Retry.MaxRetryAttempts = 3;
+                    options.Retry.UseJitter = true;
+                    options.Retry.ShouldHandle = args =>
+                        ValueTask.FromResult(
+                            args.Outcome switch
+                            {
+                                { Exception: not null } => true,
+                                { Result.IsSuccessStatusCode: false } => true,
+                                _ => false,
+                            }
+                        );
+                });
+        }
 
         services.AddTransient<IRuntimeGatewayClient, RuntimeGatewayClient>();
     }
