74d1052e894891d2e724520682491c87a9e945df
Martin Gunnerud <mgunnerud@gmail.com>
2025-12-15 11:51:50 +0100

    rollback

--- src/Designer/backend/src/Designer/Services/Implementation/ResourceRegistryService.cs
+++ src/Designer/backend/src/Designer/Services/Implementation/ResourceRegistryService.cs
@@ -650,7 +650,7 @@ namespace Altinn.Studio.Designer.Services.Implementation
         public async Task<List<ConsentTemplate>> GetConsentTemplates(string org)
         {
             // Temp location. Will be moved to CDN
-            string url = "https://raw.githubusercontent.com/Altinn/altinn-studio-docs/feat/consent-bst-krav-v2/content/authorization/architecture/resourceregistry/consent_templates.json";
+            string url = "https://raw.githubusercontent.com/Altinn/altinn-studio-docs/master/content/authorization/architecture/resourceregistry/consent_templates.json";
 
             try
             {
