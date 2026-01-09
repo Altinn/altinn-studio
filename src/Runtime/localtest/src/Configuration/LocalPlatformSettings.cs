8aeda91e05a322c75f4fea65f02411b2090316d3
Martin Othamar <martin.othamar@digdir.no>
2026-01-09 01:03:52 +0100

    localtest - build/push container, migrate fully nginx -> yarp

--- src/Runtime/localtest/src/Configuration/LocalPlatformSettings.cs
+++ src/Runtime/localtest/src/Configuration/LocalPlatformSettings.cs
@@ -86,5 +86,11 @@ namespace LocalTest.Configuration
         public string InstanceDelegationsDataFolder { get; set; } = "instanceDelegations";
 
         public string LocalGrafanaUrl { get; set; }
+
+        public string LocalPdfServiceUrl { get; set; }
+
+        public string LocalReceiptUrl { get; set; }
+
+        public string LocalAccessManagementUrl { get; set; }
     }
 }
