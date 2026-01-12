3aeb3c9a56ecd811b38048b2e036d426bebbb873
David Ovrelid <davidovrelid@Mac.home>
2026-01-12 14:16:20 +0100

    polyfills

--- src/App/frontend/src/index.tsx
+++ src/App/frontend/src/index.tsx
@@ -1,3 +1,7 @@
+// Needed for "useBuiltIns": "entry" in babel.config.json to resolve
+// all the polyfills we need and inject them here
+import 'core-js';
+
 import { executeHashRouterRedirect } from 'src/utils/urls/hashRouterRedirect';
 if (executeHashRouterRedirect()) {
   throw new Error('HashRouterRedirect: Redirecting to browser-router route');
