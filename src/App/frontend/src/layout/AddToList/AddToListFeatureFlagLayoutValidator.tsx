0ae566da9e13eb58160e193fb65b9db4f263f17e
Adam Haeger <adamgullerud@gmail.com>
2026-01-14 14:12:45 +0100

    chore: fixed minor naming error

--- src/App/frontend/src/layout/AddToList/AddToListFeatureFlagLayoutValidator.tsx
+++ src/App/frontend/src/layout/AddToList/AddToListFeatureFlagLayoutValidator.tsx
@@ -5,10 +5,10 @@ import type { NodeValidationProps } from 'src/layout/layout';
 
 export function AddToListFeatureFlagLayoutValidator({ intermediateItem }: NodeValidationProps<'AddToList'>) {
   const applicationMetadata = useApplicationMetadata();
-  const simpleTableEnabled = getFeature('addToListEnabled', applicationMetadata);
+  const addToListEnabled = getFeature('addToListEnabled', applicationMetadata);
 
   const addError = NodesInternal.useAddError();
-  if (!simpleTableEnabled.value) {
+  if (!addToListEnabled.value) {
     const error = `You need to enable the feature flag addToListEnabled to use this component. Please note that the component is experimental
     and the configuration is likely to change.`;
     addError(error, intermediateItem.id, 'node');
