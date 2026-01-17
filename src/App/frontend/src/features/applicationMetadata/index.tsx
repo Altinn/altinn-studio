6474c0b30633748564766e13a7454eade2853307
Camilla Marie Dalan <camillamdalan@gmail.com>
2026-01-17 07:34:19 +0100

    make getApplicationMetadata a named function

--- src/App/frontend/src/features/applicationMetadata/index.tsx
+++ src/App/frontend/src/features/applicationMetadata/index.tsx
@@ -1,10 +1,12 @@
 import { useNavigationParam } from 'src/hooks/navigation';
 
+export function getApplicationMetadata() {
+  return window.altinnAppGlobalData.applicationMetadata;
+}
+
 export function useIsStateless() {
   const hasInstanceGuid = useNavigationParam('instanceGuid');
   const appMetadata = getApplicationMetadata();
   const show = appMetadata.onEntry.show;
   return hasInstanceGuid ? false : !!show && !['new-instance', 'select-instance'].includes(show);
 }
-
-export const getApplicationMetadata = () => window.altinnAppGlobalData.applicationMetadata;
