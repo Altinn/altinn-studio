84dd6a079ebfc45c8887bd82cf15b7a8c52379a3
Adam Haeger <adamgullerud@gmail.com>
2026-01-19 15:03:09 +0100

    added footer to init data

--- src/App/backend/src/Altinn.App.Core/Features/Bootstrap/BootstrapGlobalService.cs
+++ src/App/backend/src/Altinn.App.Core/Features/Bootstrap/BootstrapGlobalService.cs
@@ -1,13 +1,27 @@
+using System.Text.Json;
 using Altinn.App.Core.Features.Bootstrap.Models;
 using Altinn.App.Core.Internal.App;
 
 namespace Altinn.App.Core.Features.Bootstrap;
 
-internal sealed class BootstrapGlobalService(IAppMetadata appMetadata) : IBootstrapGlobalService
+internal sealed class BootstrapGlobalService(IAppMetadata appMetadata, IAppResources appResources)
+    : IBootstrapGlobalService
 {
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
     public async Task<BootstrapGlobalResponse> GetGlobalState()
     {
         var appMetadataTask = await appMetadata.GetApplicationMetadata();
-        return new BootstrapGlobalResponse { ApplicationMetadata = appMetadataTask };
+
+        var footer = await appResources.GetFooter();
+
+        var footerJson = string.IsNullOrEmpty(footer)
+            ? null
+            : JsonSerializer.Deserialize<object>(footer, _jsonSerializerOptions);
+
+        return new BootstrapGlobalResponse { ApplicationMetadata = appMetadataTask, Footer = footerJson };
     }
 }
