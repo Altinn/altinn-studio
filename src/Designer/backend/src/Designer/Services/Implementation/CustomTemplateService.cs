a4ba4d5734aaef7be0d51a1471e48f9c9bc7be89
acn-sbuad <stephanie.buadu@accenture.com>
2026-01-19 11:38:33 +0100

    list template API working

--- src/Designer/backend/src/Designer/Services/Implementation/CustomTemplateService.cs
+++ src/Designer/backend/src/Designer/Services/Implementation/CustomTemplateService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Altinn.Studio.Designer.Clients.Interfaces;
 using Altinn.Studio.Designer.Infrastructure.GitRepository;
 using Altinn.Studio.Designer.Models;
 using Altinn.Studio.Designer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using NJsonSchema;
 using NJsonSchema.Validation;
 
@@ -15,13 +19,19 @@ public class CustomTemplateService : ICustomTemplateService
 {
     private const string SchemaFileName = "templateManifest.schema.json";
     private const string AltinnStudioOrg = "als";
+    private const string TemplateFolder = "Templates/";
+    private const string TemplateFileName = "template.json";
+    private const string TemplateManifestFileName = "templateManifest.json";
+
     private readonly IAltinnGitRepositoryFactory _altinnGitRepositoryFactory;
+    private readonly IGiteaClient _giteaClient;
 
     public string AppTemplateManifestSchemaLocation { get; } = Path.Combine(AppContext.BaseDirectory, "Schemas", SchemaFileName);
 
-    public CustomTemplateService(IAltinnGitRepositoryFactory altinnGitRepositoryFactory)
+    public CustomTemplateService(IAltinnGitRepositoryFactory altinnGitRepositoryFactory, IGiteaClient giteaClient)
     {
         _altinnGitRepositoryFactory = altinnGitRepositoryFactory;
+        _giteaClient = giteaClient;
     }
 
     /// <summary>
@@ -55,9 +65,40 @@ public class CustomTemplateService : ICustomTemplateService
 
         List<CustomTemplate> templates = [];
 
-        templates.AddRange(await GetTemplateListForPublisher(AltinnStudioOrg, developer, cancellationToken));
-   
+        cancellationToken.ThrowIfCancellationRequested();
+
+        templates.AddRange(await GetTemplateListForAls(cancellationToken));
+
         return templates;
+    }
+
+    private async Task<List<CustomTemplate>> GetTemplateListForAls(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        string repository = GetContentRepoName(AltinnStudioOrg);
+        string baseCommitSha = await _giteaClient.GetLatestCommitOnBranch(AltinnStudioOrg, repository, cancellationToken: cancellationToken);
+
+        string path = Path.Combine(TemplateFolder, TemplateManifestFileName);
+        (FileSystemObject? file, ProblemDetails? problem) = await _giteaClient.GetFileAndErrorAsync(AltinnStudioOrg, repository, path, null, cancellationToken); // passing null as reference to get main branch and latest commit
+
+        if (problem != null)
+        {
+            // do something with the problem details. 
+
+        }
+
+        try
+        {
+            List<CustomTemplate> templates = JsonSerializer.Deserialize<List<CustomTemplate>>(Encoding.UTF8.GetString(Convert.FromBase64String(file.Content))) ?? [];
+            return templates;
+        }
+        catch
+        {
+            // do something with the exception
+        }
+
+        return [];
     }
 
     public async Task<CustomTemplate> GetCustomTemplate(string developer, string owner, string id, CancellationToken cancellationToken = default)
@@ -71,13 +112,13 @@ public class CustomTemplateService : ICustomTemplateService
     }
 
 
-    private async Task<List<CustomTemplate>> GetTemplateListForPublisher(string publisher, string developer, CancellationToken cancellationToken)
+    private async Task<List<CustomTemplate>> GetTemplateListForPublisher(string owner, string developer, CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        string repo = GetContentRepoName(publisher);
+        string repo = GetContentRepoName(owner);
 
-        AltinnOrgGitRepository altinnOrgGitRepository = _altinnGitRepositoryFactory.GetAltinnOrgGitRepository(publisher, repo, developer);
+        AltinnOrgGitRepository altinnOrgGitRepository = _altinnGitRepositoryFactory.GetAltinnOrgGitRepository(owner, repo, developer);
 
         return await altinnOrgGitRepository.GetTemplateManifest(cancellationToken);
     }
