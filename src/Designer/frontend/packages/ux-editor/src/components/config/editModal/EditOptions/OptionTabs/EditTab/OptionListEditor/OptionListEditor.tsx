99ec5c1e85ba4dcd6df7f8cf05d2f3567cdaa53b
Tomas <tomas.engebretsen@gmail.com>
2026-01-23 15:33:51 +0100

    feat: Published code lists in layout editor

--- src/Designer/frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/EditTab/OptionListEditor/OptionListEditor.tsx
+++ src/Designer/frontend/packages/ux-editor/src/components/config/editModal/EditOptions/OptionTabs/EditTab/OptionListEditor/OptionListEditor.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { StudioDeleteButton, StudioSpinner, StudioValidationMessage } from '@studio/components';
+import { Trans, useTranslation } from 'react-i18next';
+import {
+  StudioCodeFragment,
+  StudioDeleteButton,
+  StudioParagraph,
+  StudioSpinner,
+  StudioValidationMessage,
+} from '@studio/components';
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 import type { IGenericEditComponent } from '../../../../../componentConfig';
 import type { SelectionComponentType } from '../../../../../../../types/FormComponent';
@@ -10,16 +16,23 @@ import { ManualOptionsPanel } from './ManualOptionsPanel';
 import { handleOptionsChange, resetComponentOptions } from '../../utils/optionsUtils';
 import classes from './OptionListEditor.module.css';
 import type { ITextResources } from 'app-shared/types/global';
+import { retrieveOptionsType } from '../../utils/retrieveOptionsType';
+import { OptionsType } from '../../enums/OptionsType';
+import type { CodeListIdContextData } from '../../types/CodeListIdContextData';
+import { extractValuesFromPublishedCodeListReferenceString } from '../../utils/published-code-list-reference-utils';
+import { Guard } from '@studio/guard';
 
 export type OptionListEditorProps = Pick<
   IGenericEditComponent<SelectionComponentType>,
   'component' | 'handleComponentChange'
 > & {
+  codeListIdContextData: CodeListIdContextData;
   textResources: ITextResources;
   onEditButtonClick: () => void;
 };
 
 export function OptionListEditor({
+  codeListIdContextData,
   component,
   handleComponentChange,
   onEditButtonClick,
@@ -30,23 +43,26 @@ export function OptionListEditor({
     handleOptionsChange(updatedComponent, handleComponentChange);
   };
 
-  if (component.options !== undefined) {
-    return (
-      <ManualOptionsPanel
-        component={component}
-        onDeleteButtonClick={handleDeleteButtonClick}
-        onEditButtonClick={onEditButtonClick}
-        textResources={textResources}
-      />
-    );
-  } else {
-    return (
-      <OptionListResolver
-        optionsId={component.optionsId}
-        onDeleteButtonClick={handleDeleteButtonClick}
-        textResources={textResources}
-      />
-    );
+  switch (retrieveOptionsType(component, codeListIdContextData)) {
+    case OptionsType.Internal:
+      return (
+        <ManualOptionsPanel
+          component={component}
+          onDeleteButtonClick={handleDeleteButtonClick}
+          onEditButtonClick={onEditButtonClick}
+          textResources={textResources}
+        />
+      );
+    case OptionsType.FromAppLibrary:
+      return (
+        <OptionListResolver
+          optionsId={component.optionsId}
+          onDeleteButtonClick={handleDeleteButtonClick}
+          textResources={textResources}
+        />
+      );
+    case OptionsType.Published:
+      return <PublishedCodeListEditor referenceString={component.optionsId} />;
   }
 }
 
@@ -98,6 +114,28 @@ function OptionListResolver({
       );
     }
   }
+}
+
+type PublishedCodeListEditorProps = {
+  readonly referenceString: string;
+};
+
+function PublishedCodeListEditor({
+  referenceString,
+}: PublishedCodeListEditorProps): React.ReactElement {
+  const referenceValues = extractValuesFromPublishedCodeListReferenceString(referenceString);
+  Guard.againstNull(referenceValues);
+  const { codeListName, version } = referenceValues;
+
+  return (
+    <StudioParagraph>
+      <Trans
+        components={{ code: <StudioCodeFragment /> }}
+        i18nKey='ux_editor.options.published_code_list_in_use'
+        values={{ codeListName, version }}
+      />
+    </StudioParagraph>
+  );
 }
 
 OptionListEditor.displayName = 'OptionListEditor';
