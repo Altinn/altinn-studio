4c22b47dc51d4759902cc05b9e10abde31910232
Bjørnar Østtveit <47412359+bjosttveit@users.noreply.github.com>
2026-01-23 09:54:44 +0100

    fix: trigger reconciliation new app (#17494)

--- src/Runtime/StudioGateway/src/StudioGateway.Api/Application/HandleTriggerReconcile.cs
+++ src/Runtime/StudioGateway/src/StudioGateway.Api/Application/HandleTriggerReconcile.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using StudioGateway.Api.Clients.K8s;
 using StudioGateway.Contracts.Deploy;
@@ -18,31 +19,44 @@ internal static class HandleTriggerReconcile
         CancellationToken cancellationToken
     )
     {
-        if (request.IsNewApp || request.IsUndeploy)
+        if (!request.IsUndeploy)
         {
-            var (ociRepoName, kustomizationName) = GetSyncRootNames(originEnvironment);
+            try
+            {
+                await ociRepositoryClient.TriggerReconcileAsync(app, DefaultNamespace, cancellationToken);
+                await kustomizationClient.TriggerReconcileAsync(app, DefaultNamespace, cancellationToken);
 
-            await ociRepositoryClient.TriggerReconcileAsync(ociRepoName, DefaultNamespace, cancellationToken);
-            await kustomizationClient.TriggerReconcileAsync(kustomizationName, DefaultNamespace, cancellationToken);
+                logger.LogInformation(
+                    "Triggered reconciliation for app {App} (originEnv: {OriginEnv})",
+                    app,
+                    originEnvironment
+                );
 
-            logger.LogInformation(
-                "Triggered reconciliation for syncroot {OciRepo}/{Kustomization} (originEnv: {OriginEnv})",
-                ociRepoName,
-                kustomizationName,
-                originEnvironment
-            );
+                return Results.Ok();
+            }
+            catch (k8s.Autorest.HttpOperationException ex) when (ex.Response.StatusCode == HttpStatusCode.NotFound)
+            {
+#pragma warning disable S6667
+                logger.LogInformation(
+                    "Attempted to trigger reconciliation for app {App} (originEnv: {OriginEnv}), but resources were not found. This app is probably new.",
+                    app,
+                    originEnvironment
+                );
+#pragma warning restore S6667
+            }
         }
-        else
-        {
-            await ociRepositoryClient.TriggerReconcileAsync(app, DefaultNamespace, cancellationToken);
-            await kustomizationClient.TriggerReconcileAsync(app, DefaultNamespace, cancellationToken);
 
-            logger.LogInformation(
-                "Triggered reconciliation for app {App} (originEnv: {OriginEnv})",
-                app,
-                originEnvironment
-            );
-        }
+        var (ociRepoName, kustomizationName) = GetSyncRootNames(originEnvironment);
+
+        await ociRepositoryClient.TriggerReconcileAsync(ociRepoName, DefaultNamespace, cancellationToken);
+        await kustomizationClient.TriggerReconcileAsync(kustomizationName, DefaultNamespace, cancellationToken);
+
+        logger.LogInformation(
+            "Triggered reconciliation for syncroot {OciRepo}/{Kustomization} (originEnv: {OriginEnv})",
+            ociRepoName,
+            kustomizationName,
+            originEnvironment
+        );
 
         return Results.Ok();
     }
