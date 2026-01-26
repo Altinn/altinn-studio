d88318a003e2065996692a848f01efc3457c734f
acn-sbuad <stephanie.buadu@accenture.com>
2026-01-26 14:07:08 +0100

    fixed failing test

--- src/Designer/backend/tests/Designer.Tests/Controllers/RepositoryController/CreateAppTests.cs
+++ src/Designer/backend/tests/Designer.Tests/Controllers/RepositoryController/CreateAppTests.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Altinn.ResourceRegistry.Core.Models;
 using Altinn.Studio.Designer.Clients.Interfaces;
 using Altinn.Studio.Designer.Configuration;
 using Altinn.Studio.Designer.Models;
+using Altinn.Studio.Designer.Models.Dto;
 using Altinn.Studio.Designer.RepositoryClient.Model;
 using Altinn.Studio.Designer.Services.Interfaces;
 using Designer.Tests.Controllers.ApiTests;
 using Designer.Tests.Mocks;
+using LibGit2Sharp;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Polly.Caching;
 using Xunit;
+using IRepository = Altinn.Studio.Designer.Services.Interfaces.IRepository;
+using Repository = Altinn.Studio.Designer.RepositoryClient.Model.Repository;
 
 namespace Designer.Tests.Controllers.RepositoryController
 {
@@ -36,8 +43,16 @@ namespace Designer.Tests.Controllers.RepositoryController
         public async Task CreateApp_InvalidRepoName_BadRequest()
         {
             // Arrange
-            string uri = $"{VersionPrefix}/create-app?org=ttd&repository=2021-application";
+            string uri = $"{VersionPrefix}/create-app";
+            var content = new StringContent(JsonSerializer.Serialize(new CreateAppRequest
+            {
+                Org = "ttd",
+                Repository = "2021-application",
+
+            }), System.Text.Encoding.UTF8, "application/json");
+
             using HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+            httpRequestMessage.Content = content;
 
             // Act
             using HttpResponseMessage res = await HttpClient.SendAsync(httpRequestMessage);
@@ -50,13 +65,51 @@ namespace Designer.Tests.Controllers.RepositoryController
         public async Task CreateApp_ValidRepoName_Created()
         {
             // Arrange
-            string uri = $"{VersionPrefix}/create-app?org=ttd&repository=test";
+            string uri = $"{VersionPrefix}/create-app";
+            var content = new StringContent(JsonSerializer.Serialize(new CreateAppRequest
+            {
+                Org = "ttd",
+                Repository = "test",
+
+            }), System.Text.Encoding.UTF8, "application/json");
 
             _repositoryMock
                 .Setup(r => r.CreateService(It.IsAny<string>(), It.IsAny<ServiceConfiguration>(), It.IsAny<List<CustomTemplateReference>>()))
                 .ReturnsAsync(new Repository() { RepositoryCreatedStatus = HttpStatusCode.Created, CloneUrl = "https://some.site/this/is/not/relevant" });
 
             using HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+            httpRequestMessage.Content = content;
+
+            // Act
+            using HttpResponseMessage res = await HttpClient.SendAsync(httpRequestMessage);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, res.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateApp_TemplateIncluded_Created()
+        {
+
+            // Arrange
+            string uri = $"{VersionPrefix}/create-app";
+            var content = new StringContent(JsonSerializer.Serialize(new CreateAppRequest
+            {
+                Org = "ttd",
+                Repository = "test",
+                Template = new CustomTemplateReference
+                {
+                    Owner = "ttd",
+                    Id = "custom-template"                    
+                }
+            }), System.Text.Encoding.UTF8, "application/json");
+
+            _repositoryMock
+                .Setup(r => r.CreateService(It.IsAny<string>(), It.IsAny<ServiceConfiguration>(), It.IsAny<List<CustomTemplateReference>>()))
+                .ReturnsAsync(new Repository() { RepositoryCreatedStatus = HttpStatusCode.Created, CloneUrl = "https://some.site/this/is/not/relevant" });
+
+            using HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+            httpRequestMessage.Content = content;
 
             // Act
             using HttpResponseMessage res = await HttpClient.SendAsync(httpRequestMessage);
