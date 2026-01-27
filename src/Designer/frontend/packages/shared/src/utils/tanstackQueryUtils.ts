b37728a249732456c0af4abedcf453f00804cba5
ErlingHauan <erling.hauan@digdir.no>
2026-01-27 09:18:48 +0100

    WIP

--- src/Designer/frontend/packages/shared/src/utils/tanstackQueryUtils.ts
+++ src/Designer/frontend/packages/shared/src/utils/tanstackQueryUtils.ts
@@ -1,4 +1,5 @@
-import type { QueryStatus } from '@tanstack/react-query';
+import type { Query, QueryStatus } from '@tanstack/react-query';
+import { QueryKey } from 'app-shared/types/QueryKey';
 
 /**
  * Merges multiple query statuses into one.
@@ -9,4 +10,14 @@ export const mergeQueryStatuses = (...queryStatuses: QueryStatus[]): QueryStatus
   if (queryStatuses.includes('error')) return 'error';
   if (queryStatuses.includes('pending')) return 'pending';
   return 'success';
+};
+
+export const isAppSpecificQuery = (query: Query, org: string, app: string) => {
+  const key = query.queryKey;
+  return key[1] === org && key[2] === app;
+};
+
+export const isFormLayoutQuery = (query: Query): boolean => {
+  const formLayoutQueryKeys: string[] = [QueryKey.FormLayouts, QueryKey.FormLayoutSettings];
+  return formLayoutQueryKeys.includes(query.queryKey[0] as string);
 };
