87a2113f24004414b618087c6386875b76bf411c
Daniel Skovli <daniel.skovli@gmail.com>
2026-01-30 13:32:43 +0100

    Prettier formatting

--- src/Designer/frontend/packages/shared/src/getInTouch/providers/SlackContactProvider.test.ts
+++ src/Designer/frontend/packages/shared/src/getInTouch/providers/SlackContactProvider.test.ts
@@ -4,7 +4,9 @@ describe('SlackContactProvider', () => {
   it('should return correct Slack link based on selectedChannel', () => {
     const slackContactProvider = new SlackContactProvider();
 
-    expect(slackContactProvider.buildContactUrl('altinn')).toBe('https://digdir-samarbeid.slack.com');
+    expect(slackContactProvider.buildContactUrl('altinn')).toBe(
+      'https://digdir-samarbeid.slack.com',
+    );
     expect(slackContactProvider.buildContactUrl('product-altinn-studio')).toBe(
       'https://digdir-samarbeid.slack.com/archives/C02EJ9HKQA3',
     );
