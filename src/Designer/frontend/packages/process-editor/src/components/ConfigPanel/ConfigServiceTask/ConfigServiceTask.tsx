01edd68aa7e826a20241556c01028dfe394c3065
Tobias Netskar <git@t4s.no>
2026-02-04 12:39:00 +0100

    feat: add action and policy accordions to service task config panel

--- src/Designer/frontend/packages/process-editor/src/components/ConfigPanel/ConfigServiceTask/ConfigServiceTask.tsx
+++ src/Designer/frontend/packages/process-editor/src/components/ConfigPanel/ConfigServiceTask/ConfigServiceTask.tsx
@@ -6,6 +6,9 @@ import classes from './ConfigServiceTask.module.css';
 import { ConfigPdfServiceTask } from './ConfigPdfServiceTask';
 import { EditTaskId } from '../ConfigContent/EditTaskId/EditTaskId';
 import { StudioDisplayTile } from '@studio/components';
+import { Accordion } from '@digdir/designsystemet-react';
+import { EditActions } from '../ConfigContent/EditActions';
+import { EditPolicy } from '../ConfigContent/EditPolicy';
 
 export const ConfigServiceTask = (): React.ReactElement => {
   const { t } = useTranslation();
@@ -23,6 +26,24 @@ export const ConfigServiceTask = (): React.ReactElement => {
           showPadlock={false}
         />
         {isPdfTask && <ConfigPdfServiceTask />}
+        <Accordion color='neutral'>
+          <Accordion.Item>
+            <Accordion.Header>
+              {t('process_editor.configuration_panel_actions_title')}
+            </Accordion.Header>
+            <Accordion.Content className={classes.accordion}>
+              <EditActions />
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item>
+            <Accordion.Header>
+              {t('process_editor.configuration_panel_policy_title')}
+            </Accordion.Header>
+            <Accordion.Content className={classes.accordion}>
+              <EditPolicy />
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </ConfigContentContainer>
   );
