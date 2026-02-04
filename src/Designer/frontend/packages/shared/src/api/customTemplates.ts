78d9084e3bb074e00b36dd16124edeb6002132f5
Nina Kylstad <nkylstad@gmail.com>
2026-02-04 14:40:24 +0100

    small cleanup

--- src/Designer/frontend/packages/shared/src/api/customTemplates.ts
+++
@@ -1,7 +0,0 @@
-import { get } from 'app-shared/utils/networking';
-import type { CustomTemplate, CustomTemplateList } from '../types/CustomTemplate';
-
-export const fetchCustomTemplates = async (): Promise<CustomTemplate[]> => {
-  const response = await get<CustomTemplateList>('/designer/api/customtemplates');
-  return response.templates;
-};
