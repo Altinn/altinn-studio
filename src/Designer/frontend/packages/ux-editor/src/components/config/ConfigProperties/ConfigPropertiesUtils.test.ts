85347c9dbc70d3f072d1dcb03fde3ad33f71ce94
Lars <74791975+lassopicasso@users.noreply.github.com>
2026-02-06 09:53:23 +0100

    chore: Replace inline actions in config panel with controlled (#17316)
    Co-authored-by: JamalAlabdullah <jamal.mustafa.alabdullah@digdir.no>

---
+++ src/Designer/frontend/packages/ux-editor/src/components/config/ConfigProperties/ConfigPropertiesUtils.test.ts
@@ -0,0 +1,25 @@
+import { componentMocks } from '@altinn/ux-editor/testing/componentMocks';
+import { componentComparison } from './ConfigPropertiesUtils';
+
+describe('ConfigPropertiesUtils', () => {
+  const componentA = componentMocks.Input;
+  const updatedComponentA = {
+    ...componentMocks.Input,
+    dataModelBindings: {
+      ...componentMocks.Input.dataModelBindings,
+      simpleBinding: { field: 'newField', dataType: 'string' },
+    },
+  };
+
+  it('componentComparison should return true for identical components', () => {
+    expect(
+      componentComparison({ initialComponent: componentA, currentComponent: componentA }),
+    ).toBe(true);
+  });
+
+  it('componentComparison should return false for different components', () => {
+    expect(
+      componentComparison({ initialComponent: componentA, currentComponent: updatedComponentA }),
+    ).toBe(false);
+  });
+});
