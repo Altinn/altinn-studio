bc984b05154afa73d2d1f4657cb3ec4551202a86
Ole Martin Handeland <git@olemartin.org>
2026-02-13 11:31:24 +0100

    Updating snapshots, removing page order test that tests functionality that hasn't been supported since v7

--- src/App/backend/test/Altinn.App.Core.Tests/Implementation/DefaultPageOrderTest.cs
+++ src/App/backend/test/Altinn.App.Core.Tests/Implementation/DefaultPageOrderTest.cs
@@ -20,7 +20,6 @@ public class DefaultPageOrderTest
     {
         // Arrange
         AppIdentifier appIdentifier = new AppIdentifier("ttd", "best-app");
-        Guid guid = Guid.NewGuid();
         InstanceIdentifier instanceIdentifier = InstanceIdentifier.NoInstance;
         string layoutSetId = "layoutSetId";
         string currentPage = "page1";
@@ -47,41 +46,6 @@ public class DefaultPageOrderTest
         // Assert
         Assert.Equal(expected, actual);
         appResourcesMock.Verify(ar => ar.GetLayoutSettingsForSet(layoutSetId), Times.Once);
-        appResourcesMock.VerifyNoOtherCalls();
-    }
-
-    [Fact]
-    public async Task GetPageOrder_Returns_LayoutSettings_layoutSetId_is_null()
-    {
-        // Arrange
-        AppIdentifier appIdentifier = new AppIdentifier("ttd", "best-app");
-        Guid guid = Guid.NewGuid();
-        InstanceIdentifier instanceIdentifier = InstanceIdentifier.NoInstance;
-        string layoutSetId = null;
-        string currentPage = "page1";
-        string dataTypeId = "dataTypeId";
-        object formData = new object();
-
-        List<string> expected = new List<string> { "page2", "page3" };
-        appResourcesMock
-            .Setup(ar => ar.GetLayoutSettings())
-            .Returns(new LayoutSettings { Pages = new Pages { Order = expected } });
-
-        // Act
-        DefaultPageOrder target = new DefaultPageOrder(appResourcesMock.Object);
-
-        List<string> actual = await target.GetPageOrder(
-            appIdentifier,
-            instanceIdentifier,
-            layoutSetId,
-            currentPage,
-            dataTypeId,
-            formData
-        );
-
-        // Assert
-        Assert.Equal(expected, actual);
-        appResourcesMock.Verify(ar => ar.GetLayoutSettings(), Times.Once);
         appResourcesMock.VerifyNoOtherCalls();
     }
 }
