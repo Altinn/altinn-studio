14b810826c13bc2300c3bbe309becefe1ba9ad5e
Camilla Marie Dalan <camillamdalan@gmail.com>
2026-02-19 16:56:45 +0100

    implement translation in DisplayDate

--- src/App/frontend/src/app-components/Date/DisplayDate.tsx
+++ src/App/frontend/src/app-components/Date/DisplayDate.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useTranslation } from 'src/app-components/AppComponentsProvider';
 import classes from 'src/app-components/Date/Date.module.css';
 
 interface DateProps {
@@ -9,15 +10,18 @@ interface DateProps {
   labelId?: string;
 }
 
-export const DisplayDate = ({ value, iconUrl, iconAltText, labelId }: DateProps) => (
-  <>
-    {iconUrl && (
-      <img
-        src={iconUrl}
-        className={classes.icon}
-        alt={iconAltText}
-      />
-    )}
-    <span aria-labelledby={labelId}>{value}</span>
-  </>
-);
+export const DisplayDate = ({ value, iconUrl, iconAltText, labelId }: DateProps) => {
+  const t = useTranslation();
+  return (
+    <>
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          className={classes.icon}
+          alt={iconAltText ? t(iconAltText) : undefined}
+        />
+      )}
+      <span aria-labelledby={labelId}>{value}</span>
+    </>
+  );
+};
