e30853c6d1d2ec6efb0cda8fe8b6c3659816643e
Camilla Marie Dalan <camillamdalan@gmail.com>
2026-02-19 15:56:19 +0100

    move IGridStyling to generalt types in app-components so Flex also can use it

--- src/App/frontend/src/app-components/Label/types.ts
+++
@@ -1,9 +0,0 @@
-export type IGridSize = 'auto' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-
-export interface IGridStyling {
-  xs?: IGridSize;
-  sm?: IGridSize;
-  md?: IGridSize;
-  lg?: IGridSize;
-  xl?: IGridSize;
-}
