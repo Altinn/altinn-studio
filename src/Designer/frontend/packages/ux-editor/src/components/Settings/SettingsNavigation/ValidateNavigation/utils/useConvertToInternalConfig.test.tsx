2f59846eeeeae1a688884cf63d9e950b31df1b5e
lassopicasso <lars.walderhaug@digdir.no>
2026-02-22 18:28:33 +0100

    add variables to tests

--- src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/utils/useConvertToInternalConfig.test.tsx
+++ src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/utils/useConvertToInternalConfig.test.tsx
@@ -1,35 +1,44 @@
+import { renderHook } from '@testing-library/react';
 import { useConvertToInternalConfig } from './useConvertToInternalConfig';
 
 jest.mock('@altinn/ux-editor/hooks', () => ({
   useComponentPropertyEnumValue: () => (value: string) => value,
 }));
 
+const show = ['type1', 'type2'];
+const page = 'current';
+const tasks = ['task1'];
+const task = 'task2';
+const pages = ['page1', 'page2'];
+
 describe('useConvertToInternalConfig', () => {
   it('should return undefined when externalConfig is undefined', () => {
-    const result = useConvertToInternalConfig(undefined);
-    expect(result).toBeUndefined();
+    const { result } = renderHook(() => useConvertToInternalConfig(undefined));
+    expect(result.current).toBeUndefined();
   });
 
   it('should convert externalConfig to internalConfig correctly', () => {
     const externalConfig = {
-      show: ['type1', 'type2'],
-      page: 'current',
-      tasks: ['task1'],
-      task: 'task2',
-      pages: ['page1', 'page2'],
+      show,
+      page,
+      tasks,
+      task,
+      pages,
     };
-    const result = useConvertToInternalConfig(externalConfig);
-    expect(result).toEqual({
+
+    const { result } = renderHook(() => useConvertToInternalConfig(externalConfig));
+
+    expect(result.current).toEqual({
       types: [
-        { value: 'type1', label: 'type1' },
-        { value: 'type2', label: 'type2' },
+        { value: show[0], label: show[0] },
+        { value: show[1], label: show[1] },
       ],
-      pageScope: { value: 'current', label: 'current' },
-      tasks: [{ value: 'task1', label: 'task1' }],
-      task: { value: 'task2', label: 'task2' },
+      pageScope: { value: page, label: page },
+      tasks: [{ value: tasks[0], label: tasks[0] }],
+      task: { value: task, label: task },
       pages: [
-        { value: 'page1', label: 'page1' },
-        { value: 'page2', label: 'page2' },
+        { value: pages[0], label: pages[0] },
+        { value: pages[1], label: pages[1] },
       ],
     });
   });
