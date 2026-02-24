3b228dd16d984b55e0a5ab28214662cb002b6f44
Daniel Skovli <daniel.skovli@gmail.com>
2026-02-24 15:09:53 +0100

    Nicer output for PersistentItemStatus

--- src/Runtime/workflow-engine/src/WorkflowEngine.Models/PersistentItemStatus.cs
+++ src/Runtime/workflow-engine/src/WorkflowEngine.Models/PersistentItemStatus.cs
@@ -1,5 +1,9 @@
+using System.Text.Json.Serialization;
+using WorkflowEngine.Resilience.JsonConverters;
+
 namespace WorkflowEngine.Models;
 
+[JsonConverter(typeof(FlexibleEnumConverter<PersistentItemStatus>))]
 public enum PersistentItemStatus
 {
     /// <summary>
