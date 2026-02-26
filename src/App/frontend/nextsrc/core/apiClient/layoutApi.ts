9c6b6b2314769981e9210c7dd975fba2580381e7
Ole Martin Handeland <git@olemartin.org>
2026-02-26 13:00:34 +0100

    Fixing compatibility with main (layout-sets to ui folders refactor)

--- src/App/frontend/nextsrc/core/apiClient/layoutApi.ts
+++ src/App/frontend/nextsrc/core/apiClient/layoutApi.ts
@@ -1,13 +1,8 @@
 import { axiosInstance } from 'nextsrc/core/axiosInstance';
 
-import type { ILayoutSettings } from 'src/layout/common.generated';
 import type { ILayoutCollection } from 'src/layout/layout';
 
 export class LayoutApi {
-  public static async getLayoutSettings(layoutSetId: string) {
-    return axiosInstance.get<ILayoutSettings>(`/api/layoutsettings/${layoutSetId}`).then((response) => response.data);
-  }
-
   public static async getLayout(layoutSetId: string) {
     return axiosInstance.get<ILayoutCollection>(`/api/layouts/${layoutSetId}`).then((response) => response.data);
   }
