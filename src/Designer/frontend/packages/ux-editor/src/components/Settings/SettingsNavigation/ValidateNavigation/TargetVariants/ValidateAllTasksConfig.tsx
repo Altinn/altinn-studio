40beaa1195b0131d5e3f68ff83841626e74c9694
lassopicasso <lars.walderhaug@digdir.no>
2026-02-27 12:24:05 +0100

    connect backend to frontend for get, post and delete

--- src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/TargetVariants/ValidateAllTasksConfig.tsx
+++ src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/TargetVariants/ValidateAllTasksConfig.tsx
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ValidateNavigationConfig } from '../ValidateNavigationConfig';
 import { convertToExternalConfig, Scope } from '../utils/ValidateNavigationUtils';
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
-import { useLayoutSetsQuery } from 'app-shared/hooks/queries/useLayoutSetsQuery';
 import type { InternalConfigState } from '../utils/ValidateNavigationTypes';
 import { useConvertToInternalConfig } from '../utils/useConvertToInternalConfig';
-import { useValidationOnNavigationLayoutSetsMutation } from '@altinn/ux-editor/hooks/mutations/useValidationOnNavigationLayoutSetsMutation';
+import { useSaveValidationOnNavigationLayoutSets } from '@altinn/ux-editor/hooks/mutations/useSaveValidationOnNavigationLayoutSets';
+import { useDeleteValidationOnNavigationLayoutSets } from '@altinn/ux-editor/hooks/mutations/useDeleteValidationOnNavigationLayoutSets';
+import { useValidationOnNavigationLayoutSetsQuery } from '@altinn/ux-editor/hooks/queries/useValidationOnNavigationLayoutSetsQuery';
 
 export const ValidateAllTasksConfig = () => {
   const { org, app } = useStudioEnvironmentParams();
-  const { data: layoutSetsSchema } = useLayoutSetsQuery(org, app);
-  const configData = layoutSetsSchema?.validationOnNavigation;
-  const [tempExtConfig, setTempExtConfig] = useState(configData); // This is just to simulate the save functionality, in real implementation this would be handled differently
-  const config = useConvertToInternalConfig(tempExtConfig);
-  const { mutate: updateLayoutSets } = useValidationOnNavigationLayoutSetsMutation(org, app);
+
+  const { data: externalConfig } = useValidationOnNavigationLayoutSetsQuery(org, app);
+  const { mutate: updateExternalConfig } = useSaveValidationOnNavigationLayoutSets(org, app);
+  const { mutate: deleteExternalConfig } = useDeleteValidationOnNavigationLayoutSets(org, app);
+
+  const internalConfig = useConvertToInternalConfig(externalConfig);
   const handleSave = (updatedConfig: InternalConfigState) => {
-    setTempExtConfig(convertToExternalConfig(updatedConfig));
+    updateExternalConfig(convertToExternalConfig(updatedConfig));
   };
 
   const handleDelete = () => {
-    updateLayoutSets(null);
-    setTempExtConfig(undefined); // This is just to simulate the delete functionality, in real implementation this would be handled differently
+    deleteExternalConfig();
   };
 
   return (
     <ValidateNavigationConfig
       scope={Scope.AllTasks}
-      config={config}
+      config={internalConfig}
       onSave={handleSave}
       onDelete={handleDelete}
     />
