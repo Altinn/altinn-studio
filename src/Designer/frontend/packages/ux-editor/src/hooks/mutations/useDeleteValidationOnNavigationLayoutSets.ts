28f1f16fe52b5489e0c680a388ca66c9c6efd740
lassopicasso <lars.walderhaug@digdir.no>
2026-02-27 12:25:15 +0100

    frontend hooks for post and delete

--- src/Designer/frontend/packages/ux-editor/src/hooks/mutations/useDeleteValidationOnNavigationLayoutSets.ts
+++ src/Designer/frontend/packages/ux-editor/src/hooks/mutations/useDeleteValidationOnNavigationLayoutSets.ts
@@ -1,15 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useServicesContext } from 'app-shared/contexts/ServicesContext';
 import { QueryKey } from 'app-shared/types/QueryKey';
-import type { IValidationOnNavigationLayoutSets } from 'app-shared/types/global';
 
-export const useValidationOnNavigationLayoutSetsMutation = (org: string, app: string) => {
-  const { updateValidationOnNavigationLayoutSets } = useServicesContext();
+export const useDeleteValidationOnNavigationLayoutSets = (org: string, app: string) => {
+  const { deleteValidationOnNavigationLayoutSets } = useServicesContext();
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (updatedConfig?: IValidationOnNavigationLayoutSets) =>
-      updateValidationOnNavigationLayoutSets(org, app, updatedConfig),
+    mutationFn: () => deleteValidationOnNavigationLayoutSets(org, app),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: [QueryKey.ValidationOnNavigationLayoutSets, org, app],
