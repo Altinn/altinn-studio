ec813beb2dd20f6217148c5f50ddf45151a5f30b
Tomas <tomas.engebretsen@gmail.com>
2026-02-27 09:48:51 +0100

    build: Optimise Typescript configuration in Designer

--- src/Designer/frontend/libs/studio-components/src/components/StudioResizableLayout/StudioResizableLayout.stories.tsx
+++ src/Designer/frontend/libs/studio-components/src/components/StudioResizableLayout/StudioResizableLayout.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryFn, Meta } from '@storybook/react/*';
+import type { StoryFn, Meta } from '@storybook/react';
 import React from 'react';
 import type { StudioResizableLayoutContainerProps } from './StudioResizableLayoutContainer/StudioResizableLayoutContainer';
 import { StudioResizableLayoutContainer } from './StudioResizableLayoutContainer/StudioResizableLayoutContainer';
