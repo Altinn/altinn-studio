fb9c404e78c8ca2bd85a8629ee4f6088ff4a2cca
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-04 19:23:29 +0100

    add grant to seq

--- src/Designer/backend/src/Designer/Migrations/SqlScripts/UserAccountsAndPersonalAccessTokens/setup-grants.sql
+++ src/Designer/backend/src/Designer/Migrations/SqlScripts/UserAccountsAndPersonalAccessTokens/setup-grants.sql
@@ -1,2 +1,3 @@
 GRANT ALL ON TABLE designer.user_accounts TO designer;
 GRANT ALL ON TABLE designer.personal_access_tokens TO designer;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA designer TO designer;
