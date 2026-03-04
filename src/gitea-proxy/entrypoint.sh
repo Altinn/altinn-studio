650400420845dc2d0a1d40d37a7d60edb94c3180
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-04 19:06:23 +0100

    support otel export in the proxy service

--- src/gitea-proxy/entrypoint.sh
+++ src/gitea-proxy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${DESIGNER_HOST} ${GITEA_HOST} ${STUDIO_OIDC_ENABLED}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${DESIGNER_HOST} ${GITEA_HOST} ${STUDIO_OIDC_ENABLED} ${OTEL_ENDPOINT} ${OTEL_SERVICE_NAME} ${OTEL_TRACE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec nginx -g 'daemon off;'
