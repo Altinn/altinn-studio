0224288505328d21471007faf0a13ae1fc94ea6d
Jamal Alabdullah <jamal.mustafa.alabdullah@digdir.no>
2026-03-04 10:39:08 +0100

    refactor

--- src/Designer/frontend/packages/shared/src/utils/formatDateAndTime.test.ts
+++ src/Designer/frontend/packages/shared/src/utils/formatDateAndTime.test.ts
@@ -2,15 +2,21 @@ import { formatDateAndTime } from './formatDateAndTime';
 import { DateUtils } from '@studio/pure-functions';
 
 describe('formatDateAndTime', () => {
+  let toLocaleTimeStringSpy: jest.SpyInstance;
+  afterEach(() => {
+    toLocaleTimeStringSpy?.mockRestore();
+  });
+
   it('formats timestamp as date, time with dots and AM/PM', () => {
     const timestamp = new Date('2026-03-03T16:27:15Z').getTime();
     const isoString = new Date(timestamp).toISOString();
     const expectedDatePart = DateUtils.formatDateDDMMYYYY(isoString);
-    const toLocaleTimeStringSpy = jest
+
+    toLocaleTimeStringSpy = jest
       .spyOn(Date.prototype, 'toLocaleTimeString')
       .mockReturnValue('4:27:15 PM');
+
     const result = formatDateAndTime(timestamp);
     expect(result).toBe(`${expectedDatePart}, 4.27.15 PM`);
-    toLocaleTimeStringSpy.mockRestore();
   });
 });
--- src/Designer/frontend/packages/shared/src/utils/formatDateAndTime.ts
+++ src/Designer/frontend/packages/shared/src/utils/formatDateAndTime.ts
@@ -1,6 +1,6 @@
 import { DateUtils } from '@studio/pure-functions';
 
-export const formatDateAndTime = (timestamp: number) => {
+export const formatDateAndTime = (timestamp: number, locale: string = 'en-US') => {
   const date = new Date(timestamp);
   const isoString = date.toISOString();
   const datePart = DateUtils.formatDateDDMMYYYY(isoString);
