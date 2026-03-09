35a4771312fb6b99c53c4d99f605433a3a9545a5
Ole Martin Handeland <git@olemartin.org>
2026-03-09 12:06:21 +0100

    Fixing test that failed. The data model is embedded in the form bootstrap query now

--- src/App/frontend/test/e2e/integration/subform-test/pdf.ts
+++ src/App/frontend/test/e2e/integration/subform-test/pdf.ts
@@ -118,8 +118,8 @@ describe('Subform test', () => {
       });
     });
 
-    // Block subform data element to provoke unknown error, intercept 3 times, 1 (main data) + 2 (subform data)
-    cy.intercept({ method: 'GET', url: '**/data/**includeRowId=true*', times: 3 }, (req) => {
+    // Block subform data element to provoke unknown error
+    cy.intercept({ method: 'GET', url: '**/data/**includeRowId=true*', times: 2 }, (req) => {
       if (req.url.includes(data.dataElementIdToBlock)) {
         req.reply({ statusCode: 404, body: 'Not Found' });
       }
