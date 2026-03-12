a2b83c7bd79b7596be90aa888613a77f2993ec5f
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-12 08:05:15 +0100

    feat: standalone loadbalancer (#18115)
    Co-authored-by: Jonas Dyrlie <jonas.dyrlie@gmail.com>

---
+++ src/load-balancer/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+VARS='${DESIGNER_HOST} ${DASHBOARD_HOST} ${EDITOR_HOST} ${PREVIEW_HOST} ${ADMIN_HOST} ${INFO_HOST} ${RESOURCEADM_HOST} ${REPOS_HOST} ${SERVER_NAME} ${LISTEN_DIRECTIVE} ${OTEL_ENDPOINT} ${OTEL_SERVICE_NAME} ${OTEL_TRACE}'
+
+for tpl in /etc/nginx/*.template /etc/nginx/extra-*/*.conf.template; do
+  [ -f "$tpl" ] && envsubst "$VARS" < "$tpl" > "${tpl%.template}"
+done
+
+exec nginx -g 'daemon off;'
