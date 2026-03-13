77ebcc93eee43aa978b919e4eb9895eca428271f
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-13 16:34:22 +0100

    rewrite string keywords as IKeywordHandler singletons

--- src/Designer/backend/src/DataModeling/Json/Keywords/XsdMaxOccursKeyword.cs
+++ src/Designer/backend/src/DataModeling/Json/Keywords/XsdMaxOccursKeyword.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Json.Keywords;
@@ -9,89 +6,14 @@ namespace Altinn.Studio.DataModeling.Json.Keywords;
 /// <summary>
 /// Adds @xsdMaxOccurs keyword to schema.
 /// </summary>
-[SchemaKeyword(Name)]
-[SchemaSpecVersion(SpecVersion.Draft6)]
-[SchemaSpecVersion(SpecVersion.Draft7)]
-[SchemaSpecVersion(SpecVersion.Draft201909)]
-[SchemaSpecVersion(SpecVersion.Draft202012)]
-[SchemaSpecVersion(SpecVersion.DraftNext)]
-[JsonConverter(typeof(XsdMaxOccursKeywordJsonConverter))]
-public sealed class XsdMaxOccursKeyword : IJsonSchemaKeyword, IEquatable<XsdMaxOccursKeyword>
+public sealed class XsdMaxOccursKeyword : IKeywordHandler
 {
-    /// <summary>
-    /// The name of the keyword
-    /// </summary>
-    private const string Name = "@xsdMaxOccurs";
+    public static readonly XsdMaxOccursKeyword Instance = new();
+    public string Name => "@xsdMaxOccurs";
 
-    /// <summary>
-    /// Content
-    /// </summary>
-    public string Value { get; }
+    public object ValidateKeywordValue(JsonElement value) => value.GetString();
 
-    /// <summary>
-    /// Create instance with defined value
-    /// </summary>
-    public XsdMaxOccursKeyword(string value)
-    {
-        Value = value;
-    }
+    public void BuildSubschemas(KeywordData keyword, BuildContext context) { }
 
-    public KeywordConstraint GetConstraint(
-        SchemaConstraint schemaConstraint,
-        IReadOnlyList<KeywordConstraint> localConstraints,
-        EvaluationContext context
-    )
-    {
-        return new KeywordConstraint(Name, (e, c) => { });
-    }
-
-    /// <inheritdoc/>
-    public bool Equals(XsdMaxOccursKeyword other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return ReferenceEquals(this, other) || Equals(Value, other.Value);
-    }
-
-    /// <inheritdoc/>
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as XsdMaxOccursKeyword);
-    }
-
-    /// <inheritdoc/>
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Serializer for the @xsdMaxOccurs keyword
-    /// </summary>
-    internal class XsdMaxOccursKeywordJsonConverter : JsonConverter<XsdMaxOccursKeyword>
-    {
-        /// <inheritdoc/>
-        public override XsdMaxOccursKeyword Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options
-        )
-        {
-            if (reader.TokenType != JsonTokenType.String)
-            {
-                throw new JsonException("Expected string");
-            }
-
-            return new XsdMaxOccursKeyword(reader.GetString());
-        }
-
-        /// <inheritdoc/>
-        public override void Write(Utf8JsonWriter writer, XsdMaxOccursKeyword value, JsonSerializerOptions options)
-        {
-            writer.WriteString(Name, value.Value);
-        }
-    }
+    public KeywordEvaluation Evaluate(KeywordData keyword, EvaluationContext context) => KeywordEvaluation.Ignore;
 }
