ec86420c3e49fbfb04b3e8b29fd598f1b79f053c
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-13 16:49:20 +0100

    rewrite remaining simple keywords as IKeywordHandler

--- src/Designer/backend/src/DataModeling/Json/Keywords/FormatExclusiveMaximumKeyword.cs
+++ src/Designer/backend/src/DataModeling/Json/Keywords/FormatExclusiveMaximumKeyword.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Json.Keywords;
@@ -9,98 +6,15 @@ namespace Altinn.Studio.DataModeling.Json.Keywords;
 /// <summary>
 /// Used to represent exclusive maximum on the date types
 /// </summary>
-[SchemaKeyword(Name)]
-[SchemaSpecVersion(SpecVersion.Draft6)]
-[SchemaSpecVersion(SpecVersion.Draft7)]
-[SchemaSpecVersion(SpecVersion.Draft201909)]
-[SchemaSpecVersion(SpecVersion.Draft202012)]
-[SchemaSpecVersion(SpecVersion.DraftNext)]
-[JsonConverter(typeof(FormatExclusiveMaximumKeywordJsonConverter))]
-public sealed class FormatExclusiveMaximumKeyword : IJsonSchemaKeyword, IEquatable<FormatExclusiveMaximumKeyword>
+public sealed class FormatExclusiveMaximumKeyword : IKeywordHandler
 {
-    /// <summary>
-    /// The name of the keyword
-    /// </summary>
-    internal const string Name = "formatExclusiveMaximum";
+    public static readonly FormatExclusiveMaximumKeyword Instance = new();
+    internal const string KeywordName = "formatExclusiveMaximum";
+    public string Name => KeywordName;
 
-    /// <summary>
-    /// The value, format of maximum
-    /// </summary>
-    public string Value { get; }
+    public object ValidateKeywordValue(JsonElement value) => value.GetString();
 
-    /// <summary>
-    /// Create a new instance with the specified value
-    /// </summary>
-    /// <param name="value">ExclusiveMaximum format</param>
-    public FormatExclusiveMaximumKeyword(string value)
-    {
-        Value = value;
-    }
+    public void BuildSubschemas(KeywordData keyword, BuildContext context) { }
 
-    public KeywordConstraint GetConstraint(
-        SchemaConstraint schemaConstraint,
-        IReadOnlyList<KeywordConstraint> localConstraints,
-        EvaluationContext context
-    )
-    {
-        return new KeywordConstraint(Name, (e, c) => { });
-    }
-
-    /// <inheritdoc />
-    public bool Equals(FormatExclusiveMaximumKeyword other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return ReferenceEquals(this, other) || Equals(Value, other.Value);
-    }
-
-    /// <inheritdoc />
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as FormatExclusiveMaximumKeyword);
-    }
-
-    /// <inheritdoc />
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Serializer for the FormatExclusiveMaximumKeyword keyword
-    /// </summary>
-    internal class FormatExclusiveMaximumKeywordJsonConverter : JsonConverter<FormatExclusiveMaximumKeyword>
-    {
-        /// <summary>
-        /// Read formatExclusiveMaximum keyword from json schema
-        /// </summary>
-        public override FormatExclusiveMaximumKeyword Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options
-        )
-        {
-            if (reader.TokenType != JsonTokenType.String)
-            {
-                throw new JsonException("Expected string");
-            }
-
-            return new FormatExclusiveMaximumKeyword(reader.GetString());
-        }
-
-        /// <summary>
-        /// Write formatExclusiveMaximum keyword to json
-        /// </summary>
-        public override void Write(
-            Utf8JsonWriter writer,
-            FormatExclusiveMaximumKeyword value,
-            JsonSerializerOptions options
-        )
-        {
-            writer.WriteString(Name, value.Value);
-        }
-    }
+    public KeywordEvaluation Evaluate(KeywordData keyword, EvaluationContext context) => KeywordEvaluation.Ignore;
 }
--- src/Designer/backend/src/DataModeling/Json/Keywords/FormatMinimumKeyword.cs
+++ src/Designer/backend/src/DataModeling/Json/Keywords/FormatMinimumKeyword.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Json.Keywords;
@@ -9,94 +6,15 @@ namespace Altinn.Studio.DataModeling.Json.Keywords;
 /// <summary>
 /// Used to represent minimum on the date types
 /// </summary>
-[SchemaKeyword(Name)]
-[SchemaSpecVersion(SpecVersion.Draft6)]
-[SchemaSpecVersion(SpecVersion.Draft7)]
-[SchemaSpecVersion(SpecVersion.Draft201909)]
-[SchemaSpecVersion(SpecVersion.Draft202012)]
-[SchemaSpecVersion(SpecVersion.DraftNext)]
-[JsonConverter(typeof(FormatMinimumKeywordJsonConverter))]
-public sealed class FormatMinimumKeyword : IJsonSchemaKeyword, IEquatable<FormatMinimumKeyword>
+public sealed class FormatMinimumKeyword : IKeywordHandler
 {
-    /// <summary>
-    /// The name of the keyword
-    /// </summary>
-    internal const string Name = "formatMinimum";
+    public static readonly FormatMinimumKeyword Instance = new();
+    internal const string KeywordName = "formatMinimum";
+    public string Name => KeywordName;
 
-    /// <summary>
-    /// The value, format of minimum
-    /// </summary>
-    public string Value { get; }
+    public object ValidateKeywordValue(JsonElement value) => value.GetString();
 
-    /// <summary>
-    /// Create a new instance with the specified value
-    /// </summary>
-    /// <param name="value">Minimum format</param>
-    public FormatMinimumKeyword(string value)
-    {
-        Value = value;
-    }
+    public void BuildSubschemas(KeywordData keyword, BuildContext context) { }
 
-    public KeywordConstraint GetConstraint(
-        SchemaConstraint schemaConstraint,
-        IReadOnlyList<KeywordConstraint> localConstraints,
-        EvaluationContext context
-    )
-    {
-        return new KeywordConstraint(Name, (e, c) => { });
-    }
-
-    /// <inheritdoc />
-    public bool Equals(FormatMinimumKeyword other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return ReferenceEquals(this, other) || Equals(Value, other.Value);
-    }
-
-    /// <inheritdoc />
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as FormatMinimumKeyword);
-    }
-
-    /// <inheritdoc />
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Serializer for the FormatMinimumKeyword keyword
-    /// </summary>
-    internal class FormatMinimumKeywordJsonConverter : JsonConverter<FormatMinimumKeyword>
-    {
-        /// <summary>
-        /// Read formatExclusiveMaximum keyword from json schema
-        /// </summary>
-        public override FormatMinimumKeyword Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options
-        )
-        {
-            if (reader.TokenType != JsonTokenType.String)
-            {
-                throw new JsonException("Expected string");
-            }
-
-            return new FormatMinimumKeyword(reader.GetString());
-        }
-
-        /// <summary>
-        /// Write formatExclusiveMaximum keyword to json
-        /// </summary>
-        public override void Write(Utf8JsonWriter writer, FormatMinimumKeyword value, JsonSerializerOptions options)
-        {
-            writer.WriteString(Name, value.Value);
-        }
-    }
+    public KeywordEvaluation Evaluate(KeywordData keyword, EvaluationContext context) => KeywordEvaluation.Ignore;
 }
