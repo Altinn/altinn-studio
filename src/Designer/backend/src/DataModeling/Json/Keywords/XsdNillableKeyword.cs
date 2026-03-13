50616aa6cb5d58333dc91d4c7ffc2a6bc52159db
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-13 16:29:06 +0100

    rewrite boolean keywords as IKeywordHandler singletons

--- src/Designer/backend/src/DataModeling/Json/Keywords/XsdNillableKeyword.cs
+++ src/Designer/backend/src/DataModeling/Json/Keywords/XsdNillableKeyword.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Json.Keywords;
@@ -9,89 +6,14 @@ namespace Altinn.Studio.DataModeling.Json.Keywords;
 /// <summary>
 /// Adds @xsdNillable keyword to schema indicating if member should be treated as XML text when serialized or deserialized.
 /// </summary>
-[SchemaKeyword(Name)]
-[SchemaSpecVersion(SpecVersion.Draft6)]
-[SchemaSpecVersion(SpecVersion.Draft7)]
-[SchemaSpecVersion(SpecVersion.Draft201909)]
-[SchemaSpecVersion(SpecVersion.Draft202012)]
-[SchemaSpecVersion(SpecVersion.DraftNext)]
-[JsonConverter(typeof(XsdNillableKeywordJsonConverter))]
-public sealed class XsdNillableKeyword : IJsonSchemaKeyword, IEquatable<XsdNillableKeyword>
+public sealed class XsdNillableKeyword : IKeywordHandler
 {
-    /// <summary>
-    /// The name of the keyword
-    /// </summary>
-    private const string Name = "@xsdNillable";
+    public static readonly XsdNillableKeyword Instance = new();
+    public string Name => "@xsdNillable";
 
-    /// <summary>
-    /// Content
-    /// </summary>
-    public bool Value { get; }
+    public object ValidateKeywordValue(JsonElement value) => value.GetBoolean();
 
-    /// <summary>
-    /// Create a new instance of <see cref="XsdNillableKeyword"/> with the specified false value.
-    /// </summary>
-    public XsdNillableKeyword(bool value)
-    {
-        Value = value;
-    }
+    public void BuildSubschemas(KeywordData keyword, BuildContext context) { }
 
-    public KeywordConstraint GetConstraint(
-        SchemaConstraint schemaConstraint,
-        IReadOnlyList<KeywordConstraint> localConstraints,
-        EvaluationContext context
-    )
-    {
-        return new KeywordConstraint(Name, (e, c) => { });
-    }
-
-    /// <inheritdoc/>
-    public bool Equals(XsdNillableKeyword other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return ReferenceEquals(this, other) || Equals(Value, other.Value);
-    }
-
-    /// <inheritdoc/>
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as XsdNillableKeyword);
-    }
-
-    /// <inheritdoc/>
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Serializer for the @xsdText keyword
-    /// </summary>
-    internal class XsdNillableKeywordJsonConverter : JsonConverter<XsdNillableKeyword>
-    {
-        /// <inheritdoc/>
-        public override XsdNillableKeyword Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options
-        )
-        {
-            if (reader.TokenType != JsonTokenType.True && reader.TokenType != JsonTokenType.False)
-            {
-                throw new JsonException("Expected boolean");
-            }
-
-            return new XsdNillableKeyword(reader.GetBoolean());
-        }
-
-        /// <inheritdoc/>
-        public override void Write(Utf8JsonWriter writer, XsdNillableKeyword value, JsonSerializerOptions options)
-        {
-            writer.WriteBoolean(Name, value.Value);
-        }
-    }
+    public KeywordEvaluation Evaluate(KeywordData keyword, EvaluationContext context) => KeywordEvaluation.Ignore;
 }
--- src/Designer/backend/src/DataModeling/Json/Keywords/XsdTextKeyword.cs
+++ src/Designer/backend/src/DataModeling/Json/Keywords/XsdTextKeyword.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Json.Schema;
 
 namespace Altinn.Studio.DataModeling.Json.Keywords;
@@ -9,95 +6,14 @@ namespace Altinn.Studio.DataModeling.Json.Keywords;
 /// <summary>
 /// Adds @XsdText keyword to schema indicating if member should be treated as XML text when serialized or deserialized.
 /// </summary>
-[SchemaKeyword(Name)]
-[SchemaSpecVersion(SpecVersion.Draft6)]
-[SchemaSpecVersion(SpecVersion.Draft7)]
-[SchemaSpecVersion(SpecVersion.Draft201909)]
-[SchemaSpecVersion(SpecVersion.Draft202012)]
-[SchemaSpecVersion(SpecVersion.DraftNext)]
-[JsonConverter(typeof(XsdTextKeywordJsonConverter))]
-public sealed class XsdTextKeyword : IJsonSchemaKeyword, IEquatable<XsdTextKeyword>
+public sealed class XsdTextKeyword : IKeywordHandler
 {
-    /// <summary>
-    /// The name of the keyword
-    /// </summary>
-    private const string Name = "@xsdText";
+    public static readonly XsdTextKeyword Instance = new();
+    public string Name => "@xsdText";
 
-    /// <summary>
-    /// Content
-    /// </summary>
-    public bool Value { get; }
+    public object ValidateKeywordValue(JsonElement value) => value.GetBoolean();
 
-    /// <summary>
-    /// Create a new instance of XsdTextKeyword with the default false value.
-    /// </summary>
-    public XsdTextKeyword()
-        : this(false) { }
+    public void BuildSubschemas(KeywordData keyword, BuildContext context) { }
 
-    /// <summary>
-    /// Create a new instance of XsdTextKeyword with the specified false value.
-    /// </summary>
-    public XsdTextKeyword(bool value)
-    {
-        Value = value;
-    }
-
-    public KeywordConstraint GetConstraint(
-        SchemaConstraint schemaConstraint,
-        IReadOnlyList<KeywordConstraint> localConstraints,
-        EvaluationContext context
-    )
-    {
-        return new KeywordConstraint(Name, (e, c) => { });
-    }
-
-    /// <inheritdoc/>
-    public bool Equals(XsdTextKeyword other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return ReferenceEquals(this, other) || Equals(Value, other.Value);
-    }
-
-    /// <inheritdoc/>
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as XsdTextKeyword);
-    }
-
-    /// <inheritdoc/>
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
-    }
-
-    /// <summary>
-    /// Serializer for the @xsdText keyword
-    /// </summary>
-    internal class XsdTextKeywordJsonConverter : JsonConverter<XsdTextKeyword>
-    {
-        /// <inheritdoc/>
-        public override XsdTextKeyword Read(
-            ref Utf8JsonReader reader,
-            Type typeToConvert,
-            JsonSerializerOptions options
-        )
-        {
-            if (reader.TokenType != JsonTokenType.True && reader.TokenType != JsonTokenType.False)
-            {
-                throw new JsonException("Expected boolean");
-            }
-
-            return new XsdTextKeyword(reader.GetBoolean());
-        }
-
-        /// <inheritdoc/>
-        public override void Write(Utf8JsonWriter writer, XsdTextKeyword value, JsonSerializerOptions options)
-        {
-            writer.WriteBoolean(Name, value.Value);
-        }
-    }
+    public KeywordEvaluation Evaluate(KeywordData keyword, EvaluationContext context) => KeywordEvaluation.Ignore;
 }
