54563f49efcd83880b68cb3033e55d5b5379b444
Nina Kylstad <nkylstad@gmail.com>
2026-03-17 12:47:21 +0100

    remove unnecessary imports

--- src/Designer/frontend/dashboard/components/CreateFromTemplate/TemplateDetails.test.tsx
+++ src/Designer/frontend/dashboard/components/CreateFromTemplate/TemplateDetails.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TemplateDetails, type TemplateDetailsProps } from './TemplateDetails';
 import { render, screen } from '@testing-library/react';
 
--- src/Designer/frontend/dashboard/components/CreateFromTemplate/TemplateSelector.test.tsx
+++ src/Designer/frontend/dashboard/components/CreateFromTemplate/TemplateSelector.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, within } from '@testing-library/react';
 import { TemplateSelector, type TemplateSelectorProps } from './TemplateSelector';
 import { textMock } from '@studio/testing/mocks/i18nMock';
