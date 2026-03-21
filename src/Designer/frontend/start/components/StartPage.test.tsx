40498bb1107c032b19cfaad375d7f0a48a04c9f4
Mirko Sekulic <misha.sekulic@gmail.com>
2026-03-21 20:09:23 +0100

    registration button tests

---
+++ src/Designer/frontend/start/components/StartPage.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { StartPage } from './StartPage';
+
+const mockUseEnvironmentConfig = jest.fn();
+jest.mock('app-shared/contexts/EnvironmentConfigContext', () => ({
+  useEnvironmentConfig: () => mockUseEnvironmentConfig(),
+}));
+
+describe('StartPage', () => {
+  it('should show registration link when studioOidc is disabled', () => {
+    mockUseEnvironmentConfig.mockReturnValue({
+      environment: { featureFlags: { studioOidc: false } },
+    });
+
+    renderStartPage();
+
+    expect(screen.getByRole('link', { name: 'Opprett ny bruker' })).toBeInTheDocument();
+  });
+
+  it('should not show registration link when studioOidc is enabled', () => {
+    mockUseEnvironmentConfig.mockReturnValue({
+      environment: { featureFlags: { studioOidc: true } },
+    });
+
+    renderStartPage();
+
+    expect(screen.queryByRole('link', { name: 'Opprett ny bruker' })).not.toBeInTheDocument();
+  });
+
+  it('should show login button', () => {
+    mockUseEnvironmentConfig.mockReturnValue({
+      environment: { featureFlags: { studioOidc: true } },
+    });
+
+    renderStartPage();
+
+    expect(screen.getByDisplayValue('Logg inn')).toBeInTheDocument();
+  });
+});
+
+const renderStartPage = () => render(<StartPage />);
