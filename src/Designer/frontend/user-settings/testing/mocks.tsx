3a25767a7963dc23f693f753acfe2431a376e6c8
Tomas <tomas.engebretsen@gmail.com>
2026-03-24 13:18:24 +0100

    Fix user-settings tests

--- src/Designer/frontend/user-settings/testing/mocks.tsx
+++ src/Designer/frontend/user-settings/testing/mocks.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import { render } from '@testing-library/react';
+import { renderAndRunTimers } from '@studio/ui-test';
 import type { RenderOptions } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import type { MemoryRouterProps } from 'react-router-dom';
@@ -27,5 +27,5 @@ export function renderWithProviders(
       </MemoryRouter>
     ),
   };
-  return render(component, renderOptions);
+  return renderAndRunTimers(component, renderOptions);
 }
