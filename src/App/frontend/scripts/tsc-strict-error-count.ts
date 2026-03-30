2964a0d455cb01df92d79bb3c98b8bb32c8647ad
Philip Bruvoll <philip.johannes.bruvoll@digdir.no>
2026-03-30 10:18:23 +0200

    docs

--- src/App/frontend/scripts/tsc-strict-error-count.ts
+++ src/App/frontend/scripts/tsc-strict-error-count.ts
@@ -1,5 +1,7 @@
 import { spawnSync } from 'node:child_process';
 
+// This script outputs the number of typescript errors when using the strict settings in the tsconfig-strict.json file.
+// It is used in the CI pipeline to check that the number of strict errors does not increase. Then end goal is to get rid of all strict errors and enable the strict settings in the main tsconfig.json file.
 const result = spawnSync('tsc', ['--project', 'tsconfig-strict.json', '--pretty', 'false'], {
   encoding: 'utf-8',
 });
