c8ba2ab7f759718dfd52b9e9779d5b73984e80b7
Philip Bruvoll <philip.johannes.bruvoll@digdir.no>
2026-03-31 15:45:25 +0200

    Implement validationApi

--- src/App/frontend/src/core/api-client/validation.api.ts
+++ src/App/frontend/src/core/api-client/validation.api.ts
@@ -1,5 +1,22 @@
+import { axiosInstance } from 'src/core/axiosInstance';
+import { getQueryStringFromObject } from 'src/utils/urls/urlHelper';
+import type { BackendValidationIssue } from 'src/features/validation';
+
 export const validationApi = {
-  dummy: () => {},
+  async fetchBackendValidations(
+    instanceId: string,
+    language: string,
+    onlyIncrementalValidators?: boolean,
+  ): Promise<BackendValidationIssue[]> {
+    const queryString = getQueryStringFromObject({
+      language,
+      onlyIncrementalValidators: onlyIncrementalValidators?.toString(),
+    });
+    const { data } = await axiosInstance.get<BackendValidationIssue[]>(
+      `/instances/${instanceId}/validate${queryString}`,
+    );
+    return data;
+  },
 };
 
 export type ValidationApi = typeof validationApi;
