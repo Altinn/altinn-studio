30766314d6d3a859347e23e9c270b63419a7ec1b
ErlingHauan <erling.hauan@digdir.no>
2026-04-01 07:54:11 +0200

    revert: remove unintended Runtime files from branch

--- src/Runtime/workflow-engine/src/WorkflowEngine.Core/Metadata/InboundMetadata.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkflowEngine.Core.Metadata;
-
-/// <summary>
-/// Metadata extracted from inbound HTTP headers and/or query parameters.
-/// </summary>
-internal sealed record InboundMetadata(string Namespace, string IdempotencyKey, Guid? CorrelationId);
