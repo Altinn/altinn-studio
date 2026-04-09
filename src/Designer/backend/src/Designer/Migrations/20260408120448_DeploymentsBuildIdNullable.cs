2eeebce915014fa74d246892041382f221e78b4a
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2026-04-09 13:10:32 +0200

    Add database migration making buildid on deployments nullable

---
+++ src/Designer/backend/src/Designer/Migrations/20260408120448_DeploymentsBuildIdNullable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Studio.Designer.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeploymentsBuildIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "buildid",
+                table: "deployments",
+                schema: "designer",
+                nullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "buildid",
+                table: "deployments",
+                schema: "designer",
+                nullable: false
+            );
+        }
+    }
+}
