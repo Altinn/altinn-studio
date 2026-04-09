4f0b3763c71364a42df335b679bbe2b61e0fd70c
Michael Queyrichon <24462611+mlqn@users.noreply.github.com>
2026-04-09 11:01:35 +0200

    fix query mock

--- src/Designer/frontend/packages/shared/src/mocks/queriesMock.ts
+++ src/Designer/frontend/packages/shared/src/mocks/queriesMock.ts
@@ -387,16 +387,9 @@ export const queriesMock: ServicesContextProps = {
   deleteContactPoint: jest.fn().mockImplementation(() => Promise.resolve()),
 
   // Mutations - Org settings - Bot accounts
-  createBotAccount: jest.fn().mockImplementation(() =>
-    Promise.resolve({
-      id: 'mock-bot-id',
-      username: 'mock-bot',
-      organizationName: 'ttd',
-      created: '2024-01-01T00:00:00Z',
-    }),
-  ),
+  createBotAccount: jest.fn().mockImplementation(() => Promise.resolve({})),
   deactivateBotAccount: jest.fn().mockImplementation(() => Promise.resolve()),
-  createBotAccountApiKey: jest.fn().mockImplementation(() => Promise.resolve()),
+  createBotAccountApiKey: jest.fn().mockImplementation(() => Promise.resolve({})),
   revokeBotAccountApiKey: jest.fn().mockImplementation(() => Promise.resolve()),
   updateBotAccount: jest.fn().mockImplementation(() => Promise.resolve()),
 };
