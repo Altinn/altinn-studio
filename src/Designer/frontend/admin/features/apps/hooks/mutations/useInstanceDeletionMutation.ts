af1791f26edc15033ba29e42ae42882c65a968be
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2026-04-27 10:35:39 +0200

    fix: invalidate instance details query when deleting instance

--- src/Designer/frontend/admin/features/apps/hooks/mutations/useInstanceDeletionMutation.ts
+++ src/Designer/frontend/admin/features/apps/hooks/mutations/useInstanceDeletionMutation.ts
@@ -1,4 +1,5 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QueryKey } from 'app-shared/types/QueryKey';
 import { instanceDeletePath } from 'admin/features/apps/utils/apiPaths';
 import axios from 'axios';
 
@@ -8,8 +9,17 @@ export const useInstanceDeletionMutation = (
   app: string,
   instanceId: string,
 ) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async () =>
       await axios.delete(instanceDeletePath(org, environment, app, instanceId)),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKey.AppInstanceDetails, org, environment, app, instanceId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [QueryKey.AppInstances, org, environment, app],
+      });
+    },
   });
 };
