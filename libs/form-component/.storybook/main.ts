952122326211b2a70ad94b3661d2fcf2247cb4e6
Philip Bruvoll <philip.johannes.bruvoll@digdir.no>
2026-05-18 13:37:01 +0200

    Add a11y addon for storybook in form-component

--- libs/form-component/.storybook/main.ts
+++ libs/form-component/.storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@chromatic-com/storybook', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-links',
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
