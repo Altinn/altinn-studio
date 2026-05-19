f25a06a8483bd0087bb676b96a5ba18d0af9ae51
lassopicasso <lars.walderhaug@digdir.no>
2026-05-19 10:43:14 +0200

    update ILayoutsetsService

--- src/Designer/backend/src/Designer/Services/Interfaces/ILayoutsetsService.cs
+++ src/Designer/backend/src/Designer/Services/Interfaces/ILayoutsetsService.cs
@@ -1,9 +1,29 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Studio.Designer.Models;
+using Altinn.Studio.Designer.Models.Dto;
+
 namespace Altinn.Studio.Designer.Services.Interfaces;
 
 public interface ILayoutsetsService
 {
-    Task<ValidationOnNavigation> GetGlobalValidationOnNavigationSettings(AltinnRepoEditingContext editingContext, CancellationToken cancellationToken);
-    Task SaveGlobalValidationOnNavigationSettings(AltinnRepoEditingContext editingContext, ValidationOnNavigation? config, CancellationToken cancellationToken);
-    Task<IEnumerable<TaskNavigationGroupDto>> GetGlobalTaskNavigationSettings(AltinnRepoEditingContext editingContext, CancellationToken cancellationToken);
-    Task UpdateGlobalTaskNavigationSettings(AltinnRepoEditingContext editingContext, IEnumerable<TaskNavigationGroupDto> taskNavigationGroupDtoList, CancellationToken cancellationToken);
+    public Task<ValidationOnNavigation> GetGlobalValidationOnNavigationSettings(
+        AltinnRepoEditingContext editingContext,
+        CancellationToken cancellationToken
+    );
+    public Task SaveGlobalValidationOnNavigationSettings(
+        AltinnRepoEditingContext editingContext,
+        ValidationOnNavigation? validationOnNavigation,
+        CancellationToken cancellationToken
+    );
+    Task<IEnumerable<TaskNavigationGroupDto>> GetGlobalTaskNavigationSettings(
+        AltinnRepoEditingContext editingContext,
+        CancellationToken cancellationToken
+    );
+    public Task UpdateGlobalTaskNavigationSettings(
+        AltinnRepoEditingContext editingContext,
+        IEnumerable<TaskNavigationGroup> taskNavigationGroupList,
+        CancellationToken cancellationToken
+    );
 }
