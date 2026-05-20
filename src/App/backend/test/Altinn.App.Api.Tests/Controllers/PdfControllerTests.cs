19722fc0142ee5f54f54df608d4e417aecabf83f
Ole Martin Handeland <git@olemartin.org>
2026-05-20 11:52:38 +0200

    Removing assertions on cookie that is no longer in use in v9

--- src/App/backend/test/Altinn.App.Api.Tests/Controllers/PdfControllerTests.cs
+++ src/App/backend/test/Altinn.App.Api.Tests/Controllers/PdfControllerTests.cs
@@ -159,7 +159,6 @@ public class PdfControllerTests
             .Contain(
                 @"url"":""http://local.altinn.cloud/org/app/instance/12345/e11e3e0b-a45c-48fb-a968-8d4ddf868c80?pdf=1"
             );
-        requestBody.Should().NotContain(@"name"":""frontendVersion");
     }
 
     [Fact]
@@ -227,6 +226,5 @@ public class PdfControllerTests
             .Contain(
                 @"url"":""http://org.apps.tt02.altinn.no/org/app/instance/12345/e11e3e0b-a45c-48fb-a968-8d4ddf868c80?pdf=1"
             );
-        requestBody.Should().NotContain(@"name"":""frontendVersion");
     }
 }
