23deb5938d5f43e9d547527edf262f30e834b524
Adam Haeger <adamgullerud@gmail.com>
2026-05-22 11:00:41 +0200

    refactor: move loading components to form-component lib (#18917)

--- libs/form-component/src/app-components/loading/AltinnContentLoader/AltinnContentLoader.test.tsx
+++ libs/form-component/src/app-components/loading/AltinnContentLoader/AltinnContentLoader.test.tsx
@@ -1,29 +1,14 @@
-import React from 'react';
+import { render, screen } from '@testing-library/react';
 
-import { screen } from '@testing-library/react';
+import { AltinnContentLoader, type IAltinnContentLoaderProps } from './AltinnContentLoader';
 
-import {
-  AltinnContentLoader,
-  IAltinnContentLoaderProps,
-} from 'src/app-components/loading/AltinnContentLoader/AltinnContentLoader';
-import { renderWithAppComponentsProvider } from 'src/app-components/test/renderWithAppComponentsProvider';
-
-const render = (props: Omit<IAltinnContentLoaderProps, 'reason'> = {}) => {
-  const allProps = {
-    ...props,
-  };
-
-  renderWithAppComponentsProvider(
-    <AltinnContentLoader
-      reason='testing'
-      {...allProps}
-    />,
-  );
+const renderLoader = (props: Omit<IAltinnContentLoaderProps, 'reason'> = {}) => {
+  render(<AltinnContentLoader reason='testing' {...props} />);
 };
 
 describe('AltinnContentLoader', () => {
   it('should show default loader when no variant is set', () => {
-    render();
+    renderLoader();
 
     expect(screen.getByTestId('AltinnContentIcon')).toBeInTheDocument();
     expect(screen.queryByTestId('AltinnContentIconFormData')).not.toBeInTheDocument();
@@ -31,7 +16,7 @@ describe('AltinnContentLoader', () => {
   });
 
   it('should show form loader when variant=form', () => {
-    render({ variant: 'form' });
+    renderLoader({ variant: 'form' });
 
     expect(screen.queryByTestId('AltinnContentIconFormData')).toBeInTheDocument();
     expect(screen.queryByTestId('AltinnContentIconReceipt')).not.toBeInTheDocument();
@@ -39,7 +24,7 @@ describe('AltinnContentLoader', () => {
   });
 
   it('should show receipt loader when variant=receipt', () => {
-    render({ variant: 'receipt' });
+    renderLoader({ variant: 'receipt' });
 
     expect(screen.queryByTestId('AltinnContentIconReceipt')).toBeInTheDocument();
     expect(screen.queryByTestId('AltinnContentIconFormData')).not.toBeInTheDocument();
--- libs/form-component/src/app-components/loading/LoadingWrapper/LoadingWrapper.tsx
+++ libs/form-component/src/app-components/loading/LoadingWrapper/LoadingWrapper.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import type { HTMLAttributes, PropsWithChildren } from 'react';
 
 /**
  * The `data-loading` signals that something is pending and we should not print PDF yet.
  */
-export function LoadingWrapper({ children, ...props }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+export function LoadingWrapper({
+  children,
+  ...props
+}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
   return (
-    <div
-      data-loading
-      {...props}
-    >
+    <div data-loading {...props}>
       {children}
     </div>
   );
