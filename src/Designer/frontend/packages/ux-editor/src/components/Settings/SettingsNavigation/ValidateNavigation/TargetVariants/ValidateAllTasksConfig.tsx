72aff153af4b3dccf4444e7f360e80cc00adf63d
lassopicasso <lars.walderhaug@digdir.no>
2026-05-22 12:28:29 +0200

    update the frontend code that use global validation on navigation hooks

--- src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/TargetVariants/ValidateAllTasksConfig.tsx
+++ src/Designer/frontend/packages/ux-editor/src/components/Settings/SettingsNavigation/ValidateNavigation/TargetVariants/ValidateAllTasksConfig.tsx
@@ -3,16 +3,16 @@ import { convertToExternalConfig, Scope } from '../utils/ValidateNavigationUtils
 import { useStudioEnvironmentParams } from 'app-shared/hooks/useStudioEnvironmentParams';
 import type { InternalConfigState } from '../utils/ValidateNavigationTypes';
 import { useConvertToInternalConfig } from '../utils/useConvertToInternalConfig';
-import { useSaveValidationOnNavigationLayoutSets } from '@altinn/ux-editor/hooks/mutations/useSaveValidationOnNavigationLayoutSets';
-import { useDeleteValidationOnNavigationLayoutSets } from '@altinn/ux-editor/hooks/mutations/useDeleteValidationOnNavigationLayoutSets';
-import { useValidationOnNavigationLayoutSetsQuery } from '@altinn/ux-editor/hooks/queries/useValidationOnNavigationLayoutSetsQuery';
+import { useGlobalValidationOnNavigationQuery } from '@altinn/ux-editor/hooks/queries/useGlobalValidationOnNavigationQuery';
+import { useSaveGlobalValidationOnNavigation } from '@altinn/ux-editor/hooks/mutations/useSaveGlobalValidationOnNavigation';
+import { useDeleteGlobalValidationOnNavigation } from '@altinn/ux-editor/hooks/mutations/useDeleteGlobalValidationOnNavigation';
 
 export const ValidateAllTasksConfig = () => {
   const { org, app } = useStudioEnvironmentParams();
 
-  const { data: externalConfig } = useValidationOnNavigationLayoutSetsQuery(org, app);
-  const { mutate: updateExternalConfig } = useSaveValidationOnNavigationLayoutSets(org, app);
-  const { mutate: deleteExternalConfig } = useDeleteValidationOnNavigationLayoutSets(org, app);
+  const { data: externalConfig } = useGlobalValidationOnNavigationQuery(org, app);
+  const { mutate: updateExternalConfig } = useSaveGlobalValidationOnNavigation(org, app);
+  const { mutate: deleteExternalConfig } = useDeleteGlobalValidationOnNavigation(org, app);
 
   const internalConfig = useConvertToInternalConfig(externalConfig);
   const handleSave = (updatedConfig: InternalConfigState) => {
