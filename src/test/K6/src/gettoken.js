71cde0fcb90843d5f30dd82e47027d37fbabb956
Jonas Dyrlie <jonas.dyrlie@gmail.com>
2026-06-05 15:18:02 +0200

    failure checks for login

--- src/test/K6/src/gettoken.js
+++ src/test/K6/src/gettoken.js
@@ -1,3 +1,4 @@
+import { check } from 'k6';
 import * as setUpData from './setup.js';
 
 export const options = {
@@ -7,7 +8,17 @@ export const options = {
 };
 
 export default function () {
-  const runtimeToken = setUpData.getAltinnTokenForUser();
-  // Sets environment variable in Azure DevOps pipeline.
-  console.log(`##vso[task.setvariable variable=runtimetoken;issecret=true]${runtimeToken}`);
+  let runtimeToken;
+  try {
+    runtimeToken = setUpData.getAltinnTokenForUser();
+  } catch (e) {
+    runtimeToken = null;
+  }
+  const success = check(runtimeToken, {
+    'Acquired token is a JWT': (t) => typeof t === 'string' && t.split('.').length === 3,
+  });
+  if (success) {
+    // Sets environment variable in Azure DevOps pipeline.
+    console.log(`##vso[task.setvariable variable=runtimetoken;issecret=true]${runtimeToken}`);
+  }
 }
