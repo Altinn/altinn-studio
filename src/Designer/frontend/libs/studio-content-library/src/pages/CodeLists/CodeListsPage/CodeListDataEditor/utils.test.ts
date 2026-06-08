c6fba8403f41dcacf3cd7cfe6281be4cd8ec765e
Tomas <tomas.engebretsen@gmail.com>
2026-06-08 14:26:27 +0200

    feat: Indicate unsaved code list changes

--- src/Designer/frontend/libs/studio-content-library/src/pages/CodeLists/CodeListsPage/CodeListDataEditor/utils.test.ts
+++ src/Designer/frontend/libs/studio-content-library/src/pages/CodeLists/CodeListsPage/CodeListDataEditor/utils.test.ts
@@ -1,6 +1,9 @@
-import { updateCodes, updateName } from './utils';
-import { fruitsFile } from '../test-data/codeLists';
+import { fileState, updateCodes, updateName } from './utils';
+import type { FileState } from './utils';
+import { coloursFile, fruitsFile } from '../test-data/codeLists';
 import type { CodeList } from '../../../../types/CodeList';
+import { ObjectUtils } from '@studio/pure-functions';
+import type { CodeListFileWithProblem } from '../../../../types/CodeListFile';
 
 describe('CodeListDataEditor utils', () => {
   describe('updateName', () => {
@@ -19,6 +22,37 @@ describe('CodeListDataEditor utils', () => {
       ];
       const result = updateCodes(fruitsFile, newCodes);
       expect(result.content).toBe(JSON.stringify(newCodes));
+    });
+  });
+
+  describe('fileState', () => {
+    it('Returns "added" when savedFile is null', () => {
+      expect(fileState(fruitsFile, null)).toBe('added' satisfies FileState);
+    });
+
+    it('Returns "changed" when savedFile is different from currentFile', () => {
+      expect(fileState(fruitsFile, coloursFile)).toBe('changed' satisfies FileState);
+    });
+
+    it('Returns "saved" when savedFile and currentFile are equal', () => {
+      const fruitsFileCopy = ObjectUtils.deepCopy(fruitsFile);
+      expect(fileState(fruitsFile, fruitsFileCopy)).toBe('saved' satisfies FileState);
+    });
+
+    it('Returns "withProblem" when there is a problem in savedFile', () => {
+      const fileWithProblem: CodeListFileWithProblem = {
+        name: fruitsFile.name,
+        problem: {},
+      };
+      expect(fileState(fruitsFile, fileWithProblem)).toBe('withProblem' satisfies FileState);
+    });
+
+    it('Returns "withProblem" when there is a problem in currentFile', () => {
+      const fileWithProblem: CodeListFileWithProblem = {
+        name: fruitsFile.name,
+        problem: {},
+      };
+      expect(fileState(fileWithProblem, fruitsFile)).toBe('withProblem' satisfies FileState);
     });
   });
 });
