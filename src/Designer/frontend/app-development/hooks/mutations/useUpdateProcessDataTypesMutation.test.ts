be506f8f9a0713b477e5f6ba876f67f38e86f889
lassopicasso <lars.walderhaug@digdir.no>
2026-06-08 15:22:24 +0200

    4 calls since it invalidates both v4 and v9 apps

--- src/Designer/frontend/app-development/hooks/mutations/useUpdateProcessDataTypesMutation.test.ts
+++ src/Designer/frontend/app-development/hooks/mutations/useUpdateProcessDataTypesMutation.test.ts
@@ -43,7 +43,7 @@ describe('useUpdateProcessDataTypeMutation', () => {
 
     await renderHook({ queryClient });
 
-    expect(invalidateQueriesSpy).toHaveBeenCalledTimes(3);
+    expect(invalidateQueriesSpy).toHaveBeenCalledTimes(4);
     expect(invalidateQueriesSpy).toHaveBeenCalledWith({
       queryKey: [QueryKey.AppMetadataModelIds, org, app],
     });
