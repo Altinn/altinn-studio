1774f53ce74f210f410da8c78da5c282fc6932c8
Tomas <tomas.engebretsen@gmail.com>
2026-06-08 14:26:27 +0200

    feat: Indicate unsaved code list changes

--- src/Designer/frontend/libs/studio-content-library/src/pages/CodeLists/CodeListsPage/CodeListDataEditor/CodeListDataEditor.tsx
+++ src/Designer/frontend/libs/studio-content-library/src/pages/CodeLists/CodeListsPage/CodeListDataEditor/CodeListDataEditor.tsx
@@ -9,59 +9,68 @@ import {
 } from '@studio/components';
 import type { CodeList } from '../../../../types/CodeList';
 import type { CodeListData } from '../../../../types/CodeListData';
-import { codeListFileToData, updateCodes, updateName } from './utils';
+import { codeListFileToData, fileState, updateCodes, updateName } from './utils';
+import type { FileState } from './utils';
 import { useTranslation } from 'react-i18next';
 import classes from './CodeListDataEditor.module.css';
 import { Publishing } from './Publishing';
 import type { CodeListFile } from '../../../../types/CodeListFile';
+import cn from 'classnames';
 
 export type CodeListDataEditorProps = Readonly<{
-  file: CodeListFile;
+  currentFile: CodeListFile;
   isPublishing: boolean;
   onDelete: () => void;
   onPublish: (data: CodeListData) => void;
   onUpdate: (newFile: CodeListFile) => void;
   publishedCodeLists: string[];
+  savedFile: CodeListFile | null;
 }>;
 
 export function CodeListDataEditor({
-  file,
+  currentFile,
   isPublishing,
   onDelete,
   onPublish,
   onUpdate,
   publishedCodeLists,
+  savedFile,
 }: CodeListDataEditorProps): ReactElement {
   const texts = useCodeListEditorTexts();
   const { t } = useTranslation();
 
-  const data = useMemo<CodeListData>(() => codeListFileToData(file), [file]);
+  const data = useMemo<CodeListData>(() => codeListFileToData(currentFile), [currentFile]);
 
   const handleNameChange: ChangeEventHandler<HTMLInputElement> = useCallback(
     (event) => {
       const { value } = event.target;
-      const newFile = updateName(file, value);
+      const newFile = updateName(currentFile, value);
       onUpdate(newFile);
     },
-    [file, onUpdate],
+    [currentFile, onUpdate],
   );
 
   const handleCodeListUpdate = useCallback(
     (newCodeList: CodeList): void => {
-      const newFile = updateCodes(file, newCodeList);
+      const newFile = updateCodes(currentFile, newCodeList);
       onUpdate(newFile);
     },
-    [file, onUpdate],
+    [currentFile, onUpdate],
   );
 
   const handlePublish = useCallback((): void => onPublish(data), [data, onPublish]);
 
+  const state = useMemo<FileState>(
+    () => fileState(currentFile, savedFile),
+    [currentFile, savedFile],
+  );
+
   return (
     <StudioDetails>
-      <StudioDetails.Summary>
+      <StudioDetails.Summary className={cn(classes.summary, fileStateToClassMap[state])}>
         <Name name={data.name} />
       </StudioDetails.Summary>
-      <StudioDetails.Content className={classes.content}>
+      <StudioDetails.Content className={classes.content} data-color='info'>
         <StudioTextfield
           className={classes.nameField}
           label={t('app_content_library.code_lists.name')}
@@ -98,3 +107,10 @@ function Name({ name }: Readonly<{ name: string }>): ReactNode {
   else
     return <span className={classes.unnamed}>{t('app_content_library.code_lists.unnamed')}</span>;
 }
+
+const fileStateToClassMap: Record<FileState, string | null> = {
+  added: classes.added,
+  changed: classes.changed,
+  saved: null,
+  withProblem: null,
+};
