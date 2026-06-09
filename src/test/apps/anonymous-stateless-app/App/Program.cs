0b5be001da789e767cbcbc84d1a9180d80e77a03
Ivar Nesje <ivarne@users.noreply.github.com>
2026-06-09 10:07:04 +0200

    Remove .Models from Microsoft.OpenApi imports

--- src/test/apps/anonymous-stateless-app/App/Program.cs
+++ src/test/apps/anonymous-stateless-app/App/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 void RegisterCustomAppServices(IServiceCollection services, IConfiguration config, IWebHostEnvironment env)
 {
