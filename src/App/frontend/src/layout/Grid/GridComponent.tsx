51ec71fd776da32d0fdc63ed6101ee1d7e357fc9
Adam Haeger <adamgullerud@gmail.com>
2026-06-09 10:53:44 +0200

    moved HelpTextContainer to lib

--- src/App/frontend/src/layout/Grid/GridComponent.tsx
+++ src/App/frontend/src/layout/Grid/GridComponent.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useMemo } from 'react';
 import type { PropsWithChildren } from 'react';
 
-import { ConditionalWrapper, Fieldset, FullWidthWrapper } from '@app/form-component';
+import { ConditionalWrapper, Fieldset, FullWidthWrapper, HelpTextContainer } from '@app/form-component';
 import { Table } from '@digdir/designsystemet-react';
 import cn from 'classnames';
 
 import { Caption } from 'src/components/form/caption/Caption';
-import { HelpTextContainer } from 'src/components/form/HelpTextContainer';
 import { LabelContent } from 'src/components/label/LabelContent';
 import { evalExpr } from 'src/features/expressions';
 import { ExprVal } from 'src/features/expressions/types';
