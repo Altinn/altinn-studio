306d257d7ffd2b5f8d9a3604ba7179e7e2a4efc7
Adam Haeger <adamgullerud@gmail.com>
2026-06-25 12:27:58 +0200

    making debug run that runs a single test

--- src/App/backend/src/Altinn.App.Core/Internal/App/IndexPageGenerator.cs
+++ src/App/backend/src/Altinn.App.Core/Internal/App/IndexPageGenerator.cs
@@ -71,7 +71,7 @@ internal sealed class IndexPageGenerator : IIndexPageGenerator
               <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
               <title>{{org}} - {{app}}</title>
               <link rel="icon" href="https://altinncdn.no/favicon.ico">
-              <link rel="stylesheet" type="text/css" href="{{appFrontendAssetBaseUrl}}/altinn-app-frontend.css" crossorigin="anonymous">
+              <link rel="stylesheet" type="text/css" href="{{appFrontendAssetBaseUrl}}/altinn-app-frontend.css">
             {{externalStylesheets}}{{customCssLinks}}</head>
             <body>
               <div id="root"></div>
