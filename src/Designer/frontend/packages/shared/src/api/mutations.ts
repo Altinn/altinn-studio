a7c306cb836ae2f8c92c899d12c50224aae174b6
lassopicasso <lars.walderhaug@digdir.no>
2026-06-26 14:01:06 +0200

    remove unused import

--- src/Designer/frontend/packages/shared/src/api/mutations.ts
+++ src/Designer/frontend/packages/shared/src/api/mutations.ts
@@ -67,7 +67,6 @@ import {
   orgCodeListUpdateIdPath,
   orgLibraryUpdatePath,
   orgCodeListPublishPath,
-  layoutSetsPath,
   studioctlAuthRequestCancelPath,
   studioctlAuthRequestConfirmPath,
   userApiKeyPath,
