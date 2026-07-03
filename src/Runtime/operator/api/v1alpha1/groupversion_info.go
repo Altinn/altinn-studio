bc26917763de95cc29fad82a10b4309d5a4873d9
Martin Othamar (Agent) <martinothamar.agent@gmail.com>
2026-07-03 15:25:32 +0000

    chore: include safe Kubebuilder scaffold updates

--- src/Runtime/operator/api/v1alpha1/groupversion_info.go
+++ src/Runtime/operator/api/v1alpha1/groupversion_info.go
@@ -9,12 +9,21 @@ import (
 )
 
 var (
-	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "resources.altinn.studio", Version: "v1alpha1"}
+	// SchemeGroupVersion is group version used to register these objects.
+	// This name is used by applyconfiguration generators (e.g. controller-gen).
+	SchemeGroupVersion = schema.GroupVersion{Group: "resources.altinn.studio", Version: "v1alpha1"}
+
+	// GroupVersion is an alias for SchemeGroupVersion, for backward compatibility.
+	GroupVersion = SchemeGroupVersion
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Resource takes an unqualified resource and returns a Group-qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
