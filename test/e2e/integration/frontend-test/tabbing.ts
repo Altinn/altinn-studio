6ec2e340eb6db00ea67c68d57acb3662d6eb5cb3
Ole Martin Handeland <github@olemartin.org>
2026-07-22 10:22:37 +0200

    Fixing two flaky Cypress tests (#4324)
    * Fix 1: This test would sometimes fail because the length was 2 already when the page had loaded. That's because Cypress itself was too late to start the assertions, because preselectedOptionIndex works immediately when the page loads. Removing those assertions, since it's impossible for them to become 2 without having been 1 at some point anyway...
    
    * Fix 2: This test ran into a failure mode where the whole body would be focused. I saw this happened before preselectedOptionIndex and the option effects had run, so I'm adding some assertions to make it wait to begin tabbing until the page has properly loaded first.
    
    ---------
    
    Co-authored-by: Ole Martin Handeland <git@olemartin.org>

--- test/e2e/integration/frontend-test/tabbing.ts
+++ test/e2e/integration/frontend-test/tabbing.ts
@@ -5,7 +5,15 @@ const appFrontend = new AppFrontend();
 
 describe('Tabbing', () => {
   it('Tab through the fields in change name form', () => {
+    cy.intercept('GET', '**/api/options/references?language=nb&source=altinn').as('fetchReferences');
     cy.goto('changename');
+
+    // Making extra sure we've finished waiting for the page to load. Sometimes, if we start too early (unrealistically
+    // early, compared to real users) the focus might have been reset before we get to simulate tabbing. This was
+    // somewhat flaky for a while.
+    cy.wait('@fetchReferences');
+    cy.waitUntilSaved();
+
     cy.get(appFrontend.changeOfName.newFirstName).focus().tab();
     cy.focused().should('have.text', 'Nytt mellomnavn');
     cy.tab();
