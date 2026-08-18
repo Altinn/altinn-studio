0580c1158baef4f7a5e4f5cbd2e4742444f52a79
David Ovrelid <davidovrelid@David-sin-MacBook-Pro.local>
2026-08-18 10:22:35 +0200

    clean ups

--- src/Designer/development/db/init.sql
+++ src/Designer/development/db/init.sql
@@ -1,4 +1,6 @@
+-- designerdb is created by the postgres entrypoint itself (POSTGRES_DB in compose.yaml), so it
+-- must not be created here: this script runs with ON_ERROR_STOP, and a duplicate CREATE DATABASE
+-- aborts the first container start.
 CREATE ROLE gitea WITH LOGIN PASSWORD 'gitea';
 CREATE ROLE designer WITH LOGIN PASSWORD 'designer';
 CREATE DATABASE giteadb WITH OWNER gitea TEMPLATE template0 ENCODING UTF8 LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8';
-CREATE DATABASE designerdb WITH OWNER designer TEMPLATE template0 ENCODING UTF8 LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8';
