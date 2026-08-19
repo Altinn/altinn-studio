bb20ea479fe2d6d98fce87ffaf19c3bad138935d
Ole Martin Handeland <git@olemartin.org>
2026-08-19 14:13:57 +0200

    Fixing workaround. It's not the parent component we should be looking for

--- src/App/frontend/src/features/options/RunOptionsEffectsForNode.tsx
+++ src/App/frontend/src/features/options/RunOptionsEffectsForNode.tsx
@@ -57,9 +57,7 @@ function RunVisibleOptionsEffects({ valueType, node }: RunOptionEffectsProps) {
   // we don't store option values here so it makes no sense to do this,
   // consider solving this more elegantly in the future.
   // AFAIK, stale values are not removed from attachment tags, maybe they should?
-  const parentComponent = node.parent.type === 'node' ? lookups.getComponent(node.parent.baseId) : undefined;
-  const shouldRemoveStaleValues =
-    parentComponent?.type !== 'FileUpload' && !('renderAsSummary' in item && item.renderAsSummary);
+  const shouldRemoveStaleValues = item?.type !== 'FileUpload' && !('renderAsSummary' in item && item.renderAsSummary);
 
   return (
     <>
