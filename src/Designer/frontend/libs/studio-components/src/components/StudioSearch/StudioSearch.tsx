0c0047257a82059c655b9b24e1469c2c8a3dfcd9
lassopicasso <lars.walderhaug@digdir.no>
2026-08-28 13:06:59 +0200

    type fix in new version for studioSearch and StudioErrorSummary

--- src/Designer/frontend/libs/studio-components/src/components/StudioSearch/StudioSearch.tsx
+++ src/Designer/frontend/libs/studio-components/src/components/StudioSearch/StudioSearch.tsx
@@ -1,10 +1,10 @@
 import React, { forwardRef, useId } from 'react';
-import { Label, Search, type SearchProps } from '@digdir/designsystemet-react';
+import { Label, Search, type SearchInputProps } from '@digdir/designsystemet-react';
 import type { WithoutAsChild } from '../../types/WithoutAsChild';
 import { StudioValidationMessage } from '../StudioValidationMessage';
 import classes from './StudioSearch.module.css';
 
-export type StudioSearchProps = WithoutAsChild<SearchProps> & {
+export type StudioSearchProps = WithoutAsChild<SearchInputProps> & {
   label: React.ReactNode;
   id?: string;
   value?: string;
