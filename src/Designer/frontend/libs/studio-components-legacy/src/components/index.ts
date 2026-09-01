9aae8d2102d81e7cf27afbf5c47ae5e7f07de58e
lassopicasso <lars.walderhaug@digdir.no>
2026-09-01 12:08:00 +0200

    remove legacy StudioTextarea

--- src/Designer/frontend/libs/studio-components-legacy/src/components/index.ts
+++ src/Designer/frontend/libs/studio-components-legacy/src/components/index.ts
@@ -2,5 +2,4 @@ export * from './StudioDecimalInput';
 export * from './StudioGridSelector';
 export * from './StudioLabelWrapper';
 export * from './StudioTable';
-export * from './StudioTextarea';
 export * from './StudioTextfield';
