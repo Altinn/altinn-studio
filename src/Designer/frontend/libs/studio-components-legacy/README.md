9470a1e6a7c1d8650202bf4935d2862281573ef5
lassopicasso <lars.walderhaug@digdir.no>
2026-09-01 15:55:59 +0200

    delete the @studio/components-legacy package
    The package had no consumers left after the last components,
    types and studioBetaTagClasses were removed.
    
    Unwires the package from jest, eslint, the Designer Dockerfile
    and the frontend AGENTS.md. studio-feedback-form declared only
    @studio/components-legacy while importing @studio/components and
    @studio/icons, so its manifest now declares what it actually uses.

--- src/Designer/frontend/libs/studio-components-legacy/README.md
+++
@@ -1,4 +0,0 @@
-# @studio/components-legacy
-
-Should not be used anymore, if you need a component from this library (`studio-components-legacy`), please migrate it
-and use `studio-components` instead
--- src/Designer/frontend/libs/studio-components-legacy/src/index.ts
+++
@@ -1,5 +0,0 @@
-// These imports are here to make sure that the CSS of the components are rendered correctly
-import '@digdir/designsystemet-css/dist/index.css';
-import '@digdir/designsystemet-theme/brand/altinn/tokens.css';
-
-export * from './style/studio-variables.css';
