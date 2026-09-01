a4236ca277b64cf1a185e9126a09b56b80fdb4ad
lassopicasso <lars.walderhaug@digdir.no>
2026-09-01 11:52:23 +0200

    remove legacy studiocheckbox and studiocheckboxGroup

--- src/Designer/frontend/libs/studio-components-legacy/src/components/StudioCheckbox/StudioCheckboxGroupContext.ts
+++
@@ -1,9 +0,0 @@
-import type { CheckboxGroupProps } from '@digdir/designsystemet-react';
-import { createContext } from 'react';
-import { DEFAULT_CHECKBOX_SIZE } from './constants';
-
-export type StudioCheckboxGroupContextProps = Pick<CheckboxGroupProps, 'size'>;
-
-export const StudioCheckboxGroupContext = createContext<StudioCheckboxGroupContextProps>({
-  size: DEFAULT_CHECKBOX_SIZE,
-});
--- src/Designer/frontend/libs/studio-components-legacy/src/components/index.ts
+++ src/Designer/frontend/libs/studio-components-legacy/src/components/index.ts
@@ -1,4 +1,3 @@
-export * from './StudioCheckbox';
 export * from './StudioCodeFragment';
 export * from './StudioCombobox';
 export * from './StudioDecimalInput';
