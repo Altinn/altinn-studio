78ddfcc076aae3b2660ade926024827f8c38fdd2
Ivar Nesje <ivarne@users.noreply.github.com>
2026-09-12 22:35:14 +0200

    Revert exception when self identified users sign

--- src/App/backend/src/Altinn.App.Core/Features/Action/SigningUserAction.cs
+++ src/App/backend/src/Altinn.App.Core/Features/Action/SigningUserAction.cs
@@ -295,9 +295,7 @@ internal class SigningUserAction : IUserAction
                 return new Signee
                 {
                     UserId = userProfile.UserId.ToString(CultureInfo.InvariantCulture),
-                    PersonNumber =
-                        userProfile.Party?.SSN
-                        ?? throw new InvalidOperationException("User profile does not contain a party with SSN."),
+                    PersonNumber = userProfile.Party?.SSN,
                     OrganizationNumber = context.OnBehalfOf,
                 };
             }
