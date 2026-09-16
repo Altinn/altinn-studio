0f147f2f88ecad87136a575c4af2319e955354c4
lassopicasso <lars.walderhaug@digdir.no>
2026-09-16 09:57:12 +0200

    simplify backend tests

--- src/Designer/backend/tests/Designer.Tests/Services/TaskDefaultDataTypeBindingValidatorTests.cs
+++ src/Designer/backend/tests/Designer.Tests/Services/TaskDefaultDataTypeBindingValidatorTests.cs
@@ -14,193 +14,104 @@ public class TaskDefaultDataTypeBindingValidatorTests
 {
     private const string Org = "ttd";
     private const string Developer = "testUser";
+    private const string SourceRepository = "app-with-layoutsets-v9";
+    private const string SecondTaskId = "Task_2";
+
+    private const string SettingsWithoutDefaultDataType = """
+        {
+          "$schema": "https://altinncdn.no/toolkits/altinn-app-frontend/4/schemas/json/layout/layoutSettings.schema.v1.json",
+          "pages": { "order": ["Side1"] }
+        }
+        """;
+
+    private const string ApplicationMetadataWithoutDataTypes = """
+        {
+          "id": "ttd/app-with-layoutsets-v9",
+          "org": "ttd",
+          "title": { "nb": "app-with-layoutsets-v9" },
+          "dataTypes": [],
+          "partyTypesAllowed": {
+            "bankruptcyEstate": true,
+            "organisation": true,
+            "person": true,
+            "subUnit": true
+          },
+          "autoDeleteOnProcessEnd": false
+        }
+        """;
 
     [Fact]
-    public async Task ValidateAsync_V9AppWithValidBinding_ReturnsNoErrors()
+    public async Task ValidateAsync_ValidBinding_ReturnsNoErrors()
     {
-        TaskDefaultDataTypeBindingValidator validator = CreateValidator();
-        AltinnRepoEditingContext editingContext = AltinnRepoEditingContext.FromOrgRepoDeveloper(
-            Org,
-            "app-with-layoutsets-v9",
-            Developer
-        );
-
-        IReadOnlyDictionary<string, string[]> errors = await validator.ValidateAsync(editingContext);
+        IReadOnlyDictionary<string, string[]> errors = await ValidateAsync(SourceRepository);
 
         Assert.Empty(errors);
     }
 
     [Fact]
-    public async Task ValidateAsync_MissingDefaultDataType_ReturnsError()
+    public async Task ValidateAsync_SettingsWithoutDefaultDataType_ReturnsMissingError()
     {
-        string targetRepository = TestDataHelper.GenerateTestRepoName();
-        await TestDataHelper.CopyRepositoryForTest(Org, "app-with-layoutsets-v9", Developer, targetRepository);
-        string settingsPath = Path.Combine(
-            TestDataHelper.GetTestDataRepositoriesRootDirectory(),
-            Developer,
-            Org,
-            targetRepository,
+        string repository = await CopyRepositoryAsync();
+        await WriteRepositoryFileAsync(
+            repository,
+            SettingsWithoutDefaultDataType,
             "App",
             "ui",
             "Task_1",
             "Settings.json"
         );
-        await File.WriteAllTextAsync(
-            settingsPath,
-            """
-            {
-              "$schema": "https://altinncdn.no/toolkits/altinn-app-frontend/4/schemas/json/layout/layoutSettings.schema.v1.json",
-              "pages": { "order": ["Side1"] }
-            }
-            """
-        );
 
-        TaskDefaultDataTypeBindingValidator validator = CreateValidator();
-        AltinnRepoEditingContext editingContext = AltinnRepoEditingContext.FromOrgRepoDeveloper(
-            Org,
-            targetRepository,
-            Developer
-        );
-
-        IReadOnlyDictionary<string, string[]> errors = await validator.ValidateAsync(editingContext);
+        IReadOnlyDictionary<string, string[]> errors = await ValidateAsync(repository);
 
         KeyValuePair<string, string[]> error = Assert.Single(errors);
         Assert.Equal("taskSettings[Task_1].defaultDataType.missing", error.Key);
     }
 
     [Fact]
-    public async Task ValidateAsync_DanglingDefaultDataType_ReturnsError()
+    public async Task ValidateAsync_DefaultDataTypeNotInApplicationMetadata_ReturnsNotFoundError()
     {
-        string targetRepository = TestDataHelper.GenerateTestRepoName();
-        await TestDataHelper.CopyRepositoryForTest(Org, "app-with-layoutsets-v9", Developer, targetRepository);
-        string metadataPath = Path.Combine(
-            TestDataHelper.GetTestDataRepositoriesRootDirectory(),
-            Developer,
-            Org,
-            targetRepository,
+        string repository = await CopyRepositoryAsync();
+        await WriteRepositoryFileAsync(
+            repository,
+            ApplicationMetadataWithoutDataTypes,
             "App",
             "config",
             "applicationmetadata.json"
         );
-        await File.WriteAllTextAsync(
-            metadataPath,
-            """
-            {
-              "id": "ttd/app-with-layoutsets-v9",
-              "org": "ttd",
-              "title": { "nb": "app-with-layoutsets-v9" },
-              "dataTypes": [],
-              "partyTypesAllowed": {
-                "bankruptcyEstate": true,
-                "organisation": true,
-                "person": true,
-                "subUnit": true
-              },
-              "autoDeleteOnProcessEnd": false
-            }
-            """
-        );
 
-        TaskDefaultDataTypeBindingValidator validator = CreateValidator();
-        AltinnRepoEditingContext editingContext = AltinnRepoEditingContext.FromOrgRepoDeveloper(
-            Org,
-            targetRepository,
-            Developer
-        );
-
-        IReadOnlyDictionary<string, string[]> errors = await validator.ValidateAsync(editingContext);
+        IReadOnlyDictionary<string, string[]> errors = await ValidateAsync(repository);
 
         KeyValuePair<string, string[]> error = Assert.Single(errors);
         Assert.Equal("taskSettings[Task_1].defaultDataType.notFound.model", error.Key);
     }
 
-    [Fact]
-    public async Task ValidateAsync_FeedbackTaskWithoutSettings_ReturnsNoErrors()
+    [Theory]
+    [InlineData("data", true)]
+    [InlineData("payment", true)]
+    [InlineData("signing", true)]
+    [InlineData("feedback", false)]
+    [InlineData("confirmation", false)]
+    public async Task ValidateAsync_TaskWithoutSettings_ReportsOnlyBindableTaskTypes(string taskType, bool expectsError)
     {
-        string targetRepository = TestDataHelper.GenerateTestRepoName();
-        await TestDataHelper.CopyRepositoryForTest(Org, "app-with-layoutsets-v9", Developer, targetRepository);
-        string processPath = Path.Combine(
-            TestDataHelper.GetTestDataRepositoriesRootDirectory(),
-            Developer,
-            Org,
-            targetRepository,
+        string repository = await CopyRepositoryAsync();
+        await WriteRepositoryFileAsync(
+            repository,
+            ProcessWithSecondTask(taskType),
             "App",
             "config",
             "process",
             "process.bpmn"
         );
-        await File.WriteAllTextAsync(
-            processPath,
-            """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:altinn="http://altinn.no/process" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Altinn_SingleDataTask_Process_Definition" targetNamespace="http://bpmn.io/schema/bpmn">
-              <bpmn:process id="SingleDataTask" isExecutable="false">
-                <bpmn:startEvent id="StartEvent_1">
-                  <bpmn:outgoing>SequenceFlow_1n56yn5</bpmn:outgoing>
-                </bpmn:startEvent>
-                <bpmn:task id="Task_1" name="Utfylling">
-                  <bpmn:extensionElements>
-                    <altinn:taskExtension>
-                      <altinn:taskType>data</altinn:taskType>
-                    </altinn:taskExtension>
-                  </bpmn:extensionElements>
-                  <bpmn:incoming>SequenceFlow_1n56yn5</bpmn:incoming>
-                  <bpmn:outgoing>Flow_to_feedback</bpmn:outgoing>
-                </bpmn:task>
-                <bpmn:task id="Activity_feedback" name="Altinn feedback task">
-                  <bpmn:extensionElements>
-                    <altinn:taskExtension>
-                      <altinn:taskType>feedback</altinn:taskType>
-                    </altinn:taskExtension>
-                  </bpmn:extensionElements>
-                  <bpmn:incoming>Flow_to_feedback</bpmn:incoming>
-                  <bpmn:outgoing>Flow_0gbacsh</bpmn:outgoing>
-                </bpmn:task>
-                <bpmn:endEvent id="EndEvent_1">
-                  <bpmn:incoming>Flow_0gbacsh</bpmn:incoming>
-                </bpmn:endEvent>
-                <bpmn:sequenceFlow id="SequenceFlow_1n56yn5" sourceRef="StartEvent_1" targetRef="Task_1" />
-                <bpmn:sequenceFlow id="Flow_to_feedback" sourceRef="Task_1" targetRef="Activity_feedback" />
-                <bpmn:sequenceFlow id="Flow_0gbacsh" sourceRef="Activity_feedback" targetRef="EndEvent_1" />
-              </bpmn:process>
-            </bpmn:definitions>
-            """
-        );
 
-        TaskDefaultDataTypeBindingValidator validator = CreateValidator();
-        AltinnRepoEditingContext editingContext = AltinnRepoEditingContext.FromOrgRepoDeveloper(
-            Org,
-            targetRepository,
-            Developer
-        );
+        IReadOnlyDictionary<string, string[]> errors = await ValidateAsync(repository);
 
-        IReadOnlyDictionary<string, string[]> errors = await validator.ValidateAsync(editingContext);
-
-        Assert.Empty(errors);
+        Assert.Equal(expectsError, errors.ContainsKey($"taskSettings[{SecondTaskId}].defaultDataType.missing"));
     }
 
-    [Theory]
-    [InlineData("payment", "Activity_payment")]
-    [InlineData("signing", "Activity_signing")]
-    public async Task ValidateAsync_LayoutSetTaskWithoutDefaultDataType_ReturnsError(string taskType, string taskId)
-    {
-        string targetRepository = TestDataHelper.GenerateTestRepoName();
-        await TestDataHelper.CopyRepositoryForTest(Org, "app-with-layoutsets-v9", Developer, targetRepository);
-        string processPath = Path.Combine(
-            TestDataHelper.GetTestDataRepositoriesRootDirectory(),
-            Developer,
-            Org,
-            targetRepository,
-            "App",
-            "config",
-            "process",
-            "process.bpmn"
-        );
-        await File.WriteAllTextAsync(
-            processPath,
-            $$"""
+    private static string ProcessWithSecondTask(string taskType) =>
+        $$"""
             <?xml version="1.0" encoding="UTF-8"?>
-            <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:altinn="http://altinn.no/process" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Altinn_SingleDataTask_Process_Definition" targetNamespace="http://bpmn.io/schema/bpmn">
+            <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:altinn="http://altinn.no/process" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" id="Altinn_SingleDataTask_Process_Definition" targetNamespace="http://bpmn.io/schema/bpmn">
               <bpmn:process id="SingleDataTask" isExecutable="false">
                 <bpmn:startEvent id="StartEvent_1">
                   <bpmn:outgoing>SequenceFlow_1n56yn5</bpmn:outgoing>
@@ -214,7 +125,7 @@ public class TaskDefaultDataTypeBindingValidatorTests
                   <bpmn:incoming>SequenceFlow_1n56yn5</bpmn:incoming>
                   <bpmn:outgoing>Flow_to_task</bpmn:outgoing>
                 </bpmn:task>
-                <bpmn:task id="{{taskId}}" name="Altinn {{taskType}} task">
+                <bpmn:task id="{{SecondTaskId}}" name="Altinn {{taskType}} task">
                   <bpmn:extensionElements>
                     <altinn:taskExtension>
                       <altinn:taskType>{{taskType}}</altinn:taskType>
@@ -227,50 +138,41 @@ public class TaskDefaultDataTypeBindingValidatorTests
                   <bpmn:incoming>Flow_0gbacsh</bpmn:incoming>
                 </bpmn:endEvent>
                 <bpmn:sequenceFlow id="SequenceFlow_1n56yn5" sourceRef="StartEvent_1" targetRef="Task_1" />
-                <bpmn:sequenceFlow id="Flow_to_task" sourceRef="Task_1" targetRef="{{taskId}}" />
-                <bpmn:sequenceFlow id="Flow_0gbacsh" sourceRef="{{taskId}}" targetRef="EndEvent_1" />
+                <bpmn:sequenceFlow id="Flow_to_task" sourceRef="Task_1" targetRef="{{SecondTaskId}}" />
+                <bpmn:sequenceFlow id="Flow_0gbacsh" sourceRef="{{SecondTaskId}}" targetRef="EndEvent_1" />
               </bpmn:process>
             </bpmn:definitions>
-            """
-        );
-        string settingsDir = Path.Combine(
+            """;
+
+    private static async Task<string> CopyRepositoryAsync()
+    {
+        string repository = TestDataHelper.GenerateTestRepoName();
+        await TestDataHelper.CopyRepositoryForTest(Org, SourceRepository, Developer, repository);
+        return repository;
+    }
+
+    private static async Task WriteRepositoryFileAsync(string repository, string content, params string[] segments)
+    {
+        string[] pathSegments =
+        [
             TestDataHelper.GetTestDataRepositoriesRootDirectory(),
             Developer,
             Org,
-            targetRepository,
-            "App",
-            "ui",
-            taskId
-        );
-        Directory.CreateDirectory(settingsDir);
-        await File.WriteAllTextAsync(
-            Path.Combine(settingsDir, "Settings.json"),
-            """
-            {
-              "$schema": "https://altinncdn.no/toolkits/altinn-app-frontend/4/schemas/json/layout/layoutSettings.schema.v1.json",
-              "pages": { "order": ["Side1"] }
-            }
-            """
-        );
-
-        TaskDefaultDataTypeBindingValidator validator = CreateValidator();
-        AltinnRepoEditingContext editingContext = AltinnRepoEditingContext.FromOrgRepoDeveloper(
-            Org,
-            targetRepository,
-            Developer
-        );
-
-        IReadOnlyDictionary<string, string[]> errors = await validator.ValidateAsync(editingContext);
-
-        Assert.Contains($"taskSettings[{taskId}].defaultDataType.missing", errors.Keys);
+            repository,
+            .. segments,
+        ];
+        string path = Path.Combine(pathSegments);
+        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+        await File.WriteAllTextAsync(path, content);
     }
 
-    private static TaskDefaultDataTypeBindingValidator CreateValidator()
+    private static Task<IReadOnlyDictionary<string, string[]>> ValidateAsync(string repository)
     {
         string repositoriesRoot = TestDataHelper.GetTestDataRepositoriesRootDirectory();
-        return new TaskDefaultDataTypeBindingValidator(
+        var validator = new TaskDefaultDataTypeBindingValidator(
             new AltinnGitRepositoryFactory(repositoriesRoot),
             new AppVersionService(new AltinnGitRepositoryFactory(repositoriesRoot))
         );
+        return validator.ValidateAsync(AltinnRepoEditingContext.FromOrgRepoDeveloper(Org, repository, Developer));
     }
 }
