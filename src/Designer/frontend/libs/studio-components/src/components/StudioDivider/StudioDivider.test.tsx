ba77bd0b138890604f928cfee672006f0f7ea94c
lassopicasso <lars.walderhaug@digdir.no>
2026-09-21 13:05:38 +0200

    support orientation in studio divider

--- src/Designer/frontend/libs/studio-components/src/components/StudioDivider/StudioDivider.test.tsx
+++ src/Designer/frontend/libs/studio-components/src/components/StudioDivider/StudioDivider.test.tsx
@@ -9,6 +9,12 @@ describe('StudioDivider', () => {
     expect(divider).toBeInTheDocument();
   });
 
+  it('should render a vertical divider when the orientation is vertical', () => {
+    render(<StudioDivider orientation='vertical' />);
+    const divider = screen.getByRole('separator', { hidden: true });
+    expect(divider).toHaveAttribute('aria-orientation', 'vertical');
+  });
+
   it('should forward ref correctly', () => {
     const ref = createRef<HTMLHRElement>();
     render(<StudioDivider ref={ref} />);
