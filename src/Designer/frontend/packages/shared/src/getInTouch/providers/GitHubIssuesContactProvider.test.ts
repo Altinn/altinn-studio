166b7a87aca87e859370af8cc2336966930c9f91
Ronny Birkeli <ronny.birkeli@gmail.com>
2026-09-25 15:57:52 +0200

    chore: Removed any reference to kind/bug label (#20724)

--- src/Designer/frontend/packages/shared/src/getInTouch/providers/GitHubIssuesContactProvider.test.ts
+++ src/Designer/frontend/packages/shared/src/getInTouch/providers/GitHubIssuesContactProvider.test.ts
@@ -8,7 +8,7 @@ describe('GitHubIssuesContactProvider', () => {
     );
 
     expect(gitHubIssuesContactProvider.buildContactUrl('bugReport')).toBe(
-      'https://github.com/Altinn/altinn-studio/issues/new?labels=kind/bug,status/triage&template=bug_report.yml',
+      'https://github.com/Altinn/altinn-studio/issues/new?labels=status/triage&template=bug_report.yml',
     );
 
     expect(gitHubIssuesContactProvider.buildContactUrl('choose')).toBe(
@@ -24,7 +24,7 @@ describe('GitHubIssuesContactProvider', () => {
         'additional-information': 'cannot read property of undefined, reading id',
       }),
     ).toBe(
-      'https://github.com/Altinn/altinn-studio/issues/new?labels=kind/bug,status/triage&template=bug_report.yml&title=title%20of%20the%20issue&additional-information=cannot%20read%20property%20of%20undefined%2C%20reading%20id',
+      'https://github.com/Altinn/altinn-studio/issues/new?labels=status/triage&template=bug_report.yml&title=title%20of%20the%20issue&additional-information=cannot%20read%20property%20of%20undefined%2C%20reading%20id',
     );
   });
 });
